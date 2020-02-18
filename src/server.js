55e13055d7ae2f6aec865689eb8e5c91fd6f20f6
Pierre Duchateau <pad@dabla.com>
2020-02-18 18:28:13 +0100

    Use template.data.gouv.fr to create a simple landing page

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('src/public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
