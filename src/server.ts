e9b49c0c761367118df412072a1f19d659016227
Pierre Duchateau <pad@dabla.com>
2020-02-20 18:10:47 +0100

    Add typescript with ts-node-dev for developpement transpiling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port: number = 3000
 
 app.use(express.static('src/public'))
+
+const router = express.Router()
+
+router.get('/ssr', async (req, res) => {
+  res.status(201).send('Hello world')
+})
+
+app.use(router)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
