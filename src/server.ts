a2d1c2ed2fdf3a646d647efea48aa9319801c7ed
Pierre Duchateau <pad@dabla.com>
2020-02-24 10:57:09 +0100

    Build page from header/footer partials and jsx content

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,15 @@ const port: number = 3000
 import Component from './component'
 import * as ReactDOMServer from 'react-dom/server'
 
+import buildFromTemplate from './views/buildFromTemplate'
+
 app.use(express.static('src/public'))
 
 const router = express.Router()
 
 router.get('/ssr', async (req, res) => {
   const htmlOutput = ReactDOMServer.renderToString(Component('john'))
-  res.status(201).send(htmlOutput)
+  res.status(201).send(buildFromTemplate(htmlOutput))
 })
 
 app.use(router)
