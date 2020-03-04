0c8d9c5f4aaedfb22b7a6f485999c62a7f55c18a
Pierre Duchateau <pad@dabla.com>
2020-03-04 18:53:03 +0100

    Add Storybook to tests UI elements and pages

---
+++ src/views/pages/adminDashboard.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import AdminDashboard from './adminDashboard'
+
+import '../../public/main.css'
+
+export default { title: 'Button' }
+
+export const empty = () => <AdminDashboard adminName="Roger Rabbit" />
+
+export const withError = () => (
+  <AdminDashboard adminName="Roger Rabbit" error="This is an error message!" />
+)
+
+export const withSuccess = () => (
+  <AdminDashboard
+    adminName="Roger Rabbit"
+    success="This is a success message!"
+  />
+)
+
+const projects = [
+  {
+    periode: 'periode',
+    numeroCRE: 'numeroCRE',
+    famille: 'famille',
+    nomCandidat: 'nomCandidat',
+    nomProjet: 'nomProjet',
+    puissance: 12765,
+    prixReference: 87.9,
+    evaluationCarbone: 4.4,
+    note: 11,
+    nomRepresentantLegal: 'nomRepresentantLegal',
+    email: 'email',
+    adresseProjet: 'adresseProjet',
+    codePostalProjet: 'codePostalProjet',
+    communeProjet: 'communeProjet',
+    departementProjet: 'departementProjet',
+    regionProjet: 'regionProjet',
+    classe: 'Eliminé' as 'Eliminé',
+    motifsElimination: 'motifsElimination'
+  },
+  {
+    periode: 'periode',
+    numeroCRE: 'numeroCRE',
+    famille: 'famille',
+    nomCandidat: 'nomCandidat',
+    nomProjet: 'nomProjet',
+    puissance: 12765,
+    prixReference: 87.9,
+    evaluationCarbone: 4.4,
+    note: 11,
+    nomRepresentantLegal: 'nomRepresentantLegal',
+    email: 'email',
+    adresseProjet: 'adresseProjet',
+    codePostalProjet: 'codePostalProjet',
+    communeProjet: 'communeProjet',
+    departementProjet: 'departementProjet',
+    regionProjet: 'regionProjet',
+    classe: 'Classé' as 'Classé',
+    motifsElimination: ''
+  }
+]
+
+export const withProjects = () => (
+  <AdminDashboard adminName="Roger Rabbit" projects={projects} />
+)
