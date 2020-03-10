04517dd10de871fd02e1a3c93ef02c6204423117
Pierre Duchateau <pad@dabla.com>
2020-03-10 14:54:22 +0100

    Plug end to end tests into cucumber (jest/cucumber/puppeteer)

--- features/test.e2e.ts
+++ features/test.e2e.ts
@@ -1,14 +1,34 @@
-describe('Local', () => {
-  let page
+import { defineFeature, loadFeature } from 'jest-cucumber'
+import { Page } from 'puppeteer'
+
+const feature = loadFeature('features/test.feature')
+
+defineFeature(feature, test => {
+  let page: Page
+
   beforeAll(async () => {
     page = await global['__BROWSER__'].newPage()
-    await page.goto('http://localhost:3001/hello')
   })
 
-  it('should be titled "Google"', async () => {
-    await expect(page.content()).resolves.toMatch(
-      '<html><head></head><body>Hello, World!</body></html>'
-    )
-    console.log('done')
+  test('juste un truc pour voir', ({ given, when, then }) => {
+    given('je suis un utilisateur', async () => {
+      console.log('given je suis un utilisateur')
+    })
+
+    given('je visite la bonne page', async () => {
+      console.log('given je visite la bonne page')
+      await page.goto('http://localhost:3001/hello')
+    })
+
+    when('je lance une action', async () => {
+      console.log('when je lance une action')
+    })
+
+    then("l'appli doit faire quelque chose", async () => {
+      console.log("then l'appli doit faire quelque chose")
+      await expect(page.content()).resolves.toMatch(
+        '<html><head></head><body>Hello, World!</body></html>'
+      )
+    })
   })
 })
