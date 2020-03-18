b622b683dcbf29be981a17b5bfb30f460f47b8ec
Pierre Duchateau <pad@dabla.com>
2020-03-18 19:15:49 +0100

    simplify dataAccess index

--- src/dataAccess/index.ts
+++ src/dataAccess/index.ts
@@ -1,39 +1,10 @@
-import {
-  userRepo,
-  credentialsRepo,
-  projectRepo,
-  candidateNotificationRepo,
-  projectAdmissionKeyRepo,
-  initDatabase,
-  resetDatabase
-} from './db'
-
 if (process.env.NODE_ENV === 'unit-test') {
   console.log('DO NOT USE THESE DEFAULT DATA ACCESS REPOS (DB) IN UNIT TESTS')
   console.log("Use: import { xxxRepo } from 'dataAccess/inMemory'")
   process.exit(1)
 }
 
-const dataAccess = Object.freeze({
-  credentialsRepo,
-  userRepo,
-  projectRepo,
-  candidateNotificationRepo,
-  projectAdmissionKeyRepo,
-  initDatabase,
-  resetDatabase
-})
-
-export default dataAccess
-export {
-  credentialsRepo,
-  userRepo,
-  projectRepo,
-  candidateNotificationRepo,
-  projectAdmissionKeyRepo,
-  initDatabase,
-  resetDatabase
-}
+export * from './db'
 
 export * from './user'
 export * from './credentials'
