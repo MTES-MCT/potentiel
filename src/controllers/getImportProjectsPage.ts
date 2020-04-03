e039323cbf01b0084d5a4467eba3de49d903998c
Pierre Duchateau <pad@dabla.com>
2020-04-03 18:04:02 +0200

    Plug the getImportProjectsPage to the appelOffre repo

--- src/controllers/getImportProjectsPage.ts
+++ src/controllers/getImportProjectsPage.ts
@@ -4,11 +4,18 @@ import { listProjects } from '../useCases'
 import { ImportCandidatesPage } from '../views/pages'
 import { Success } from '../helpers/responses'
 
+import { appelOffreRepo } from '../dataAccess'
+
 const getImportProjectsPage = async (request: HttpRequest) => {
   // console.log('getAdminDashboardPage request.query', request.query)
+
+  // TODO: Move this to a use-case
+  const appelsOffre = await appelOffreRepo.findAll()
+
   return Success(
     ImportCandidatesPage({
-      request
+      request,
+      appelsOffre
     })
   )
 }
