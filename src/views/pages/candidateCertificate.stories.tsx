f02ea62360bfcecee0123c35b743482f44c25456
Pierre Duchateau <pad@dabla.com>
2020-04-07 23:39:28 +0200

    WIP: create pdf for candidate certificate

---
+++ src/views/pages/candidateCertificate.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import makeFakeRequest from '../../__tests__/fixtures/request'
+import makeFakeUser from '../../__tests__/fixtures/user'
+import LoginPage from './login'
+
+import { PDFViewer } from '@react-pdf/renderer'
+
+import { MyDocument } from './candidateCertificate'
+
+export default { title: 'Certificate' }
+
+export const Blank = () => (
+  <PDFViewer width="100%" height="800">
+    <MyDocument />
+  </PDFViewer>
+)
