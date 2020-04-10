7d06acf2699d77fb3974dae119c7f0e8c6609f33
Pierre Duchateau <pad@dabla.com>
2020-04-10 14:42:43 +0200

    If user goes to login page while logged in, redirect him to dashboard

--- src/controllers/getLoginPage.ts
+++ src/controllers/getLoginPage.ts
@@ -1,11 +1,16 @@
-import { Success } from '../helpers/responses'
+import { Success, Redirect } from '../helpers/responses'
 import { HttpRequest } from '../types'
 import { LoginPage } from '../views/pages'
+import routes from '../routes'
 
 const getLoginPage = async (request: HttpRequest) => {
+  if (request.user) {
+    return Redirect(routes.REDIRECT_BASED_ON_ROLE)
+  }
+
   return Success(
     LoginPage({
-      request
+      request,
     })
   )
 }
