a316e64734901079acb5cae27ca1a841385d15df
Pierre Duchateau <pad@dabla.com>
2020-05-11 18:44:08 +0200

    Create an integration test for the importer candidats feature

---
+++ cypress/integration/importer-candidats.feature
@@ -0,0 +1,26 @@
+# language: fr
+Fonctionnalité: Import d'un fichier de candidats
+
+  @admin
+  Scénario: Fichier au bon format
+    Etant donné que je me rends sur la page d'import de candidats
+    Lorsque je selectionne le fichier "candidats.csv"
+    Et que je valide le formulaire
+    Alors je suis dirigé vers la page qui liste les projets à notifier
+    Et on me notifie la réussite par "Les candidats ont bien été importés."
+    Et je trouve bien le projet "Nom du projet" dans la liste des projets
+
+
+
+# Scénario: Fichier au mauvais format
+#   Etant donné que je suis un administrateur DGEC
+#   Et que je suis sur la page d'import de candidats
+#   Lorsque je saisis la période "Période 3T Batiment"
+#   Et que je selectionne le fichier csv de la forme
+#     """
+#     mauvaiseColonne;etEncoreMauvais
+#     1;3
+#     """
+#   Et que je valide le formulaire
+#   Alors le site reste sur la page d'import de candidats
+#   Et me notifie l'échec par "Format du fichier erroné (vérifier conformité des colonnes)"
---
+++ cypress/integration/importer-candidats/index.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+import {
+  Before,
+  Given,
+  When,
+  And,
+  Then,
+} from 'cypress-cucumber-preprocessor/steps'
+import testid from '../../helpers/testid'
+
+Given("je me rends sur la page d'import de candidats", () => {
+  cy.visit('/admin/importer-candidats.html')
+})
+
+When('je selectionne le fichier {string}', async function (filename) {
+  // Don't ask me why base64 works...
+  cy.fixture(filename, 'base64').then((fileContent) => {
+    cy.get(testid('candidats-field')).upload({
+      fileContent,
+      fileName: 'candidats.csv',
+      mimeType: 'text/plain',
+      encoding: 'base64',
+    })
+  })
+})
+
+When('je valide le formulaire', () => {
+  cy.get(testid('submit-button')).click()
+})
+
+Then('je suis dirigé vers la page qui liste les projets à notifier', () => {
+  cy.url().should('include', '/admin/notifier-candidats.html')
+})
+
+Then('on me notifie la réussite par {string}', (successMessage) => {
+  cy.get(testid('success-message')).should('contain', successMessage)
+})
+
+Then(
+  'je trouve bien le projet {string} dans la liste des projets',
+  (projectName) => {
+    cy.get(testid('projectList-item-nomProjet')).should('contain', projectName)
+  }
+)
