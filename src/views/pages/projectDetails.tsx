71215e61b4c9549ef739aa6a47b9db942b594f85
Pierre Duchateau <pad@dabla.com>
2020-05-26 17:00:30 +0200

    User can toggle opening of frise item by clicking on arrow

--- src/views/pages/projectDetails.tsx
+++ src/views/pages/projectDetails.tsx
@@ -198,6 +198,12 @@ const FriseItem = ({
                 height="20"
                 stroke="var(--blue)"
                 viewBox="0 0 24 24"
+                {...(action && action.openHiddenContent
+                  ? {
+                      ...dataId('frise-action'),
+                      className: 'frise-content-toggle',
+                    }
+                  : {})}
               >
                 <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
