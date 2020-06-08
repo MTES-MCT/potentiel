223cd11e8e72c09c65e4c16a3f4b7394ee583396
Pierre Duchateau <pad@dabla.com>
2020-06-08 18:06:12 +0200

    Add a check to index.ts to verify presence of environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { makeServer } from './server'
 
+// Check env variables
+
+const mandatoryVariables = ['NODE_ENV', 'BASE_URL', 'SEND_EMAILS_FROM']
+
+mandatoryVariables.forEach((variable) => {
+  if (!process.env[variable]) {
+    console.log(`Missing ${variable} environment variable`)
+    process.exit(1)
+  }
+})
+
 makeServer()
