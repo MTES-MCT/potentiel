f710c8137c5338c9ac5b33f8c8688ee48c0454ee
Pierre Duchateau <pad@dabla.com>
2020-09-16 12:09:14 +0200

    create sequelize seed for test users

---
+++ src/infra/sequelize/seeds/20200916093931-users.js
@@ -0,0 +1,104 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    try {
+      await queryInterface.bulkInsert(
+        'users',
+        [
+          {
+            id: '5c3bc7a0-95f1-11ea-b350-bb5aa5faa992',
+            fullName: 'Admin Test',
+            email: 'admin@test.test',
+            role: 'admin',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: '5c3c3cd0-95f1-11ea-b350-bb5aa5faa992',
+            fullName: 'Dreal Test',
+            email: 'dreal@test.test',
+            role: 'dreal',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            id: '5c3c3cd4-95f1-11ea-b350-bb5aa5faa992',
+            fullName: 'Porteur Projet Test',
+            email: 'porteur@test.test',
+            role: 'porteur-projet',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        {}
+      )
+
+      await queryInterface.bulkInsert(
+        'credentials',
+        [
+          {
+            id: '5c3c15c0-95f1-11ea-b350-bb5aa5faa992',
+            userId: '5c3bc7a0-95f1-11ea-b350-bb5aa5faa992',
+            email: 'admin@test.test',
+            hash: '098f6bcd4621d373cade4e832627b4f6',
+          },
+          {
+            id: '5c3c3cd1-95f1-11ea-b350-bb5aa5faa992',
+            userId: '5c3c3cd0-95f1-11ea-b350-bb5aa5faa992',
+            email: 'dreal@test.test',
+            hash: '098f6bcd4621d373cade4e832627b4f6',
+          },
+          {
+            id: '5c3c3cd5-95f1-11ea-b350-bb5aa5faa992',
+            userId: '5c3c3cd4-95f1-11ea-b350-bb5aa5faa992',
+            email: 'porteur@test.test',
+            hash: '098f6bcd4621d373cade4e832627b4f6',
+          },
+        ],
+        {}
+      )
+    } catch (error) {
+      console.log('Error', error)
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('users', {
+      id: {
+        [Sequelize.Op.in]: [
+          '5c3bc7a0-95f1-11ea-b350-bb5aa5faa992',
+          '5c3c3cd0-95f1-11ea-b350-bb5aa5faa992',
+          '5c3c3cd4-95f1-11ea-b350-bb5aa5faa992',
+        ],
+      },
+    })
+
+    await queryInterface.bulkDelete('credentials', {
+      id: {
+        [Sequelize.Op.in]: [
+          '5c3c15c0-95f1-11ea-b350-bb5aa5faa992',
+          '5c3c3cd1-95f1-11ea-b350-bb5aa5faa992',
+          '5c3c3cd5-95f1-11ea-b350-bb5aa5faa992',
+        ],
+      },
+    })
+  },
+}
