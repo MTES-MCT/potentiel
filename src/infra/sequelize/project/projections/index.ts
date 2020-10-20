38d2a76972a81aec42de32fe8fd410aa7ce00cbc
Pierre Duchateau <pad@dabla.com>
2020-10-20 22:03:48 +0200

    Implement onProjectNotificationDateSet projection

--- src/infra/sequelize/project/projections/index.ts
+++ src/infra/sequelize/project/projections/index.ts
@@ -4,12 +4,14 @@ import {
   ProjectDataCorrected,
   ProjectDCRDueDateSet,
   ProjectGFDueDateSet,
+  ProjectNotificationDateSet,
   ProjectNotified,
 } from '../../../../modules/project/events'
 import { onProjectCertificateGenerated } from './onProjectCertificateGenerated'
 import { onProjectDataCorrected } from './onProjectDataCorrected'
 import { onProjectDCRDueDateSet } from './onProjectDCRDueDateSet'
 import { onProjectGFDueDateSet } from './onProjectGFDueDateSet'
+import { onProjectNotificationDateSet } from './onProjectNotificationDateSet'
 import { onProjectNotified } from './onProjectNotified'
 
 export const initProjectProjections = (eventBus: EventBus, models) => {
@@ -17,6 +19,10 @@ export const initProjectProjections = (eventBus: EventBus, models) => {
   eventBus.subscribe(ProjectNotified.type, onProjectNotified(models))
   eventBus.subscribe(ProjectDCRDueDateSet.type, onProjectDCRDueDateSet(models))
   eventBus.subscribe(ProjectGFDueDateSet.type, onProjectGFDueDateSet(models))
+  eventBus.subscribe(
+    ProjectNotificationDateSet.type,
+    onProjectNotificationDateSet(models)
+  )
   eventBus.subscribe(
     ProjectCertificateGenerated.type,
     onProjectCertificateGenerated(models)
