66c55d5c621b576106f0bba7255b9ee5b3404b85
Pierre Duchateau <pad@dabla.com>
2020-11-19 13:26:50 +0100

    fix(stats): tweak wording

--- src/views/pages/statistiques.tsx
+++ src/views/pages/statistiques.tsx
@@ -53,7 +53,7 @@ export default function StatistiquesPages(props: Props) {
     <main role="main">
       <section className="section section-color">
         <div className="container">
-          <h2 className="section__title">Potentiel en nombres</h2>
+          <h2 className="section__title">Potentiel en chiffres</h2>
           <p className="section__subtitle">Au service des porteurs de projets</p>
 
           <div className="row">
@@ -67,15 +67,15 @@ export default function StatistiquesPages(props: Props) {
           <h2>Utilisateurs</h2>
 
           <div className="row">
-            <Card value={props.porteursProjetNotifiesInscrits} title='inscriptions' subtitle={{prefix: 'soit', value: ratio(props.porteursProjetNotifiesInscrits, props.porteursProjetNotifies), suffix: 'des invités'}}/>
+            <Card value={props.porteursProjetNotifiesInscrits} title='porteurs de projet' subtitle={{prefix: 'soit', value: ratio(props.porteursProjetNotifiesInscrits, props.porteursProjetNotifies)}}/>
             <Card value={props.parrainages} title='parrainages'/>
-            <Card value={18} title='DREAL' subtitle={{prefix: 'soit', value: '100%', suffix: 'des régions'}}/>
+            <Card value={18} title='DREAL' subtitle={{prefix: 'soit', value: '100%'}}/>
           </div>
 
-          <h2>Suivi des étapes</h2>
+          <h2>Avancement des projets</h2>
 
           <div className="row">
-            <Card value={props.telechargementsAttestation} titleColor='var(--theme-dark-text)' title="téléchargements d'attestations" subtitle={{prefix: 'soit', value: ratio(props.telechargementsAttestation,props.projetsAvecAttestation), suffix: 'des projets'}}/>
+            <Card value={props.telechargementsAttestation} titleColor='var(--theme-dark-text)' title="attestations téléchargées" subtitle={{prefix: 'soit', value: ratio(props.telechargementsAttestation,props.projetsAvecAttestation)}}/>
             <Card value={props.gfDeposees} titleColor='var(--theme-dark-text)' title='GF déposées' subtitle={{prefix: 'soit', value: ratio(props.gfDeposees, props.gfDues)}}/>
             <Card value={props.dcrDeposees} titleColor='var(--theme-dark-text)' title='DCR déposées' subtitle={{prefix: 'soit', value: ratio(props.dcrDeposees, props.dcrDues)}}/>
           </div>
