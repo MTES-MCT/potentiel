1b0e013c20ce986fbd4793d3fa4c6485ee0eee00
Pierre Duchateau <pad@dabla.com>
2020-11-26 15:08:31 +0100

    feat(filestorage): add S3FileStorageService

---
+++ src/infra/file/S3FileStorageService.integration.ts
@@ -0,0 +1,111 @@
+import { Readable } from 'stream'
+import { makeS3FileStorageService } from './S3FileStorageService'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const bucket = process.env.S3_BUCKET
+const endpoint = process.env.S3_ENDPOINT
+
+describe('S3FileStorageService', () => {
+  const fakePath = `test/fakeFile-${Date.now()}.txt`
+
+  describe('upload', () => {
+    let uploadedFileId: string
+
+    describe('given a proper bucket', () => {
+      expect(bucket).toBeDefined()
+      if (!bucket) return
+
+      expect(endpoint).toBeDefined()
+      if (!endpoint) return
+
+      const fileStorageService = makeS3FileStorageService({ endpoint, bucket })
+
+      afterAll(async () => {
+        if (uploadedFileId) await fileStorageService.remove(uploadedFileId)
+      })
+
+      it('should call the client upload', async () => {
+        const fakeContents = Readable.from(['test'])
+
+        const result = await fileStorageService.upload({ contents: fakeContents, path: fakePath })
+
+        if (result.isErr()) console.log('error on save', result.error)
+        expect(result.isOk()).toBe(true)
+
+        if (result.isErr()) return
+
+        uploadedFileId = result.value
+
+        expect(result.value).toEqual('S3:' + bucket + ':' + fakePath)
+      })
+    })
+
+    describe('given a wrong bucket', () => {
+      it('should return an error', async () => {
+        expect(endpoint).toBeDefined()
+        if (!endpoint) return
+
+        const fileStorageService = makeS3FileStorageService({
+          endpoint,
+          bucket: 'CONTAINERTHATDOESNTEXIST',
+        })
+        const fakeContents = Readable.from(['test'])
+        const result = await fileStorageService.upload({ contents: fakeContents, path: fakePath })
+
+        expect(result.isErr()).toBe(true)
+      })
+    })
+  })
+
+  describe('download', () => {
+    expect(bucket).toBeDefined()
+    if (!bucket) return
+
+    expect(endpoint).toBeDefined()
+    if (!endpoint) return
+
+    const fileStorageService = makeS3FileStorageService({ endpoint, bucket })
+
+    describe('given an existing file', () => {
+      let uploadedFileId: string
+
+      beforeAll(async () => {
+        const fakeContents = Readable.from(['test'])
+        const result = await fileStorageService.upload({ contents: fakeContents, path: fakePath })
+
+        expect(result.isOk()).toBe(true)
+        if (result.isErr()) return
+
+        uploadedFileId = result.value
+      })
+
+      afterAll(async () => {
+        if (uploadedFileId) await fileStorageService.remove(uploadedFileId)
+      })
+
+      it('should retrieve the file from the S3 storage', async () => {
+        const result = await fileStorageService.download(uploadedFileId)
+
+        if (result.isErr()) console.log('error on load', result.error)
+        expect(result.isOk()).toBe(true)
+        if (result.isErr()) return
+
+        const downloadedFile = await new Promise((resolve, reject) => {
+          let fileContents = ''
+          result.value.on('data', (chunk) => {
+            fileContents += chunk
+          })
+          result.value.on('error', (err) => {
+            reject(err)
+          })
+          result.value.on('end', () => {
+            resolve(fileContents)
+          })
+        })
+
+        expect(downloadedFile).toEqual('test')
+      })
+    })
+  })
+})
---
+++ src/infra/file/S3FileStorageService.ts
@@ -0,0 +1,94 @@
+import AWS from 'aws-sdk'
+import { err, ok, okAsync, Result, ResultAsync } from '../../core/utils/Result'
+import { FileStorageService } from '../../modules/file'
+import { InfraNotAvailableError } from '../../modules/shared'
+
+class WrongIdentifierFormat extends Error {
+  constructor() {
+    super('Identifier is not recognized as S3.')
+  }
+}
+
+class WrongBucket extends Error {
+  constructor() {
+    super('The S3 bucket does not match the current bucket.')
+  }
+}
+
+const IDENTIFIER_PREFIX = 'S3'
+
+function makeIdentifier(filePath: string, bucket: string): string {
+  return `${IDENTIFIER_PREFIX}:${bucket}:${filePath}`
+}
+
+function parseIdentifier(fileId: string, _bucket: string): Result<string, WrongIdentifierFormat> {
+  if (!fileId || fileId.indexOf(IDENTIFIER_PREFIX) !== 0) {
+    return err(new WrongIdentifierFormat())
+  }
+
+  const bucket = fileId.substring(IDENTIFIER_PREFIX.length + 1, fileId.lastIndexOf(':'))
+
+  if (bucket !== _bucket) {
+    return err(new WrongBucket())
+  }
+
+  return ok(fileId.substring(fileId.lastIndexOf(':') + 1))
+}
+
+export const makeS3FileStorageService = (args: {
+  endpoint: string
+  bucket: string
+}): FileStorageService => {
+  const { endpoint, bucket } = args
+  const _client = new AWS.S3({ endpoint })
+
+  return {
+    upload({ contents, path: filePath }) {
+      return ResultAsync.fromPromise(
+        _client
+          .upload({
+            Bucket: bucket,
+            Key: filePath,
+            Body: contents,
+          })
+          .promise(),
+        (e: any) => {
+          console.error('S3FileStorageService.upload', e)
+          return new InfraNotAvailableError()
+        }
+      ).map(() => makeIdentifier(filePath, bucket))
+    },
+
+    download(storedAt) {
+      return parseIdentifier(storedAt, bucket)
+        .map((remote: string) =>
+          _client
+            .getObject({
+              Bucket: bucket,
+              Key: remote,
+            })
+            .createReadStream()
+        )
+        .asyncAndThen((stream) => okAsync(stream))
+    },
+
+    remove(storedAt) {
+      return parseIdentifier(storedAt, bucket)
+        .asyncAndThen((remote) =>
+          ResultAsync.fromPromise(
+            _client
+              .deleteObject({
+                Bucket: bucket,
+                Key: remote,
+              })
+              .promise(),
+            (e: any) => {
+              console.error('S3FileStorageService.remove', e)
+              return new InfraNotAvailableError()
+            }
+          )
+        )
+        .map(() => null)
+    },
+  }
+}
