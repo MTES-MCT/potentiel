bf0b460dda8029e1b4c7603c83e5144dfd773aab
Pierre Duchateau <pad@dabla.com>
2020-11-26 15:09:05 +0100

    feat(filestorage): switch production fileStorage to S3, remove ObjectStorage

--- src/config/fileStorage.config.ts
+++ src/config/fileStorage.config.ts
@@ -1,37 +1,21 @@
-import { makeLocalFileStorageService, makeObjectStorageFileStorageService } from '../infra/file'
+import { makeLocalFileStorageService, makeS3FileStorageService } from '../infra/file'
 import { FileStorageService } from '../modules/file'
 import { isProdEnv, isStagingEnv } from './env.config'
 
 let fileStorageService: FileStorageService
 if (isStagingEnv || isProdEnv) {
-  const authUrl = process.env.OS_AUTH_URL
-  const region = process.env.OS_REGION
-  const username = process.env.OS_USERNAME
-  const password = process.env.OS_PASSWORD
-  const container = process.env.OS_CONTAINER
+  const { S3_ENDPOINT, S3_BUCKET, AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID } = process.env
 
-  if (!authUrl || !region || !username || !password || !container) {
+  if (!S3_BUCKET || !S3_ENDPOINT || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
     console.log(
-      'Cannot start ObjectStorageFileStorageService because of missing environment variables (OS_AUTH_URL, OS_REGION, OS_USERNAME, OS_PASSWORD, OS_CONTAINER)'
+      'Cannot start S3FileStorageService because of missing environment variables (S3_ENDPOINT, S3_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)'
     )
     process.exit(1)
   }
 
-  fileStorageService = makeObjectStorageFileStorageService(
-    {
-      provider: 'openstack',
-      keystoneAuthVersion: 'v3',
-      authUrl,
-      region,
-      username,
-      password,
-      // @ts-ignore
-      domainId: 'default',
-    },
-    container
-  )
+  fileStorageService = makeS3FileStorageService({ endpoint: S3_ENDPOINT, bucket: S3_BUCKET })
 
-  console.log('FileService will be using ObjectStorage on container ' + container)
+  console.log('FileService will be using S3 on bucket ' + S3_BUCKET)
 } else {
   console.log('FileService will be using LocalFileStorage is userData/')
   fileStorageService = makeLocalFileStorageService('userData')
