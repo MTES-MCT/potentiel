f020d8b8276e40c97295e1d34522c0fa3e61c3d7
Pierre Duchateau <pad@dabla.com>
2021-01-19 17:05:26 +0100

    :recycle:  create and implement GetModificationRequestListForUser query

--- src/modules/modificationRequest/queries/index.ts
+++ src/modules/modificationRequest/queries/index.ts
@@ -1,3 +1,4 @@
 export * from './GetModificationRequestDetails'
 export * from './GetModificationRequestInfoForStatusNotification'
+export * from './GetModificationRequestListForUser'
 export * from './GetModificationRequestStatus'
