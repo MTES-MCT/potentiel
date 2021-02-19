a1551511b72e252f0f6ebdc0afeaabbe6850cae7
Julien HAIRAPIAN <julien.hairapian@gmail.com>
2021-02-19 03:45:46 +0100

    :recycle:  Factorize shared code in csv.ts

---
+++ src/helpers/csv.ts
@@ -0,0 +1,24 @@
+import { promises as fsPromises } from 'fs'
+import { encode } from 'iconv-lite'
+import { v4 as uuid } from 'uuid'
+import { dataFieldsFlattened } from '../dataAccess/inMemory/appelsOffres'
+
+export async function writeCsvOnDisk(csv: string, csvSaveDir: string): Promise<string> {
+  const csvUtf8Bom = encode(csv, 'utf-8', { addBOM: true })
+  const fileName = `${csvSaveDir}/${uuid()}.csv`
+  await fsPromises.writeFile(fileName, csvUtf8Bom)
+  return fileName
+}
+
+export function formatField(field: {
+  name: string
+  fn?: Function
+}): { label: string; value: string | Function } {
+  const { name, fn } = field
+
+  if (fn) return { label: name, value: fn }
+
+  return dataFieldsFlattened.has(name)
+    ? { label: dataFieldsFlattened.get(name) as string, value: name }
+    : { label: name, value: `details.${name}` }
+}
