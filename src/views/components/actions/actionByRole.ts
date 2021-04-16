0cb6c802f3c43bf0a6c73a74df2674b36d67336b
Pierre Duchateau <pad@dabla.com>
2021-04-16 16:24:57 +0200

    :star2:  Disable project actions for acheteur-obligé

--- src/views/components/actions/actionByRole.ts
+++ src/views/components/actions/actionByRole.ts
@@ -1,10 +1,13 @@
 import { adminActions } from './admin'
 import { porteurProjetActions } from './porteurProjet'
 import { drealActions } from './dreal'
+import { User } from '../../../entities'
 
-export const ACTION_BY_ROLE = {
+export const ACTION_BY_ROLE: Record<User['role'], (project: any) => any[]> = {
   admin: adminActions,
   dgec: adminActions,
   'porteur-projet': porteurProjetActions,
   dreal: drealActions,
+  'acheteur-obligé': () => [],
+  ademe: () => [],
 }
