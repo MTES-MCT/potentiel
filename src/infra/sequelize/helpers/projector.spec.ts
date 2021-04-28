ea9fbcf2709ac386f1b87c5cd91059f9ade29611
Pierre Duchateau <pad@dabla.com>
2021-04-28 19:39:34 +0200

    :recycle:  create new generic projector with create/update/delete ops

---
+++ src/infra/sequelize/helpers/projector.spec.ts
@@ -0,0 +1,142 @@
+import { BaseDomainEvent, DomainEvent } from '../../../core/domain'
+import { makeProjector } from './projector'
+
+export interface FakeEventPayload {
+  param: string
+}
+export class FakeEvent extends BaseDomainEvent<FakeEventPayload> implements DomainEvent {
+  public static type: 'FakeEvent' = 'FakeEvent'
+  public type = FakeEvent.type
+  currentVersion = 1
+
+  aggregateIdFromPayload(payload: FakeEventPayload) {
+    return undefined
+  }
+}
+
+describe('projector', () => {
+  describe('on(Event).create', () => {
+    const projector = makeProjector()
+    const fakeModel = {
+      create: jest.fn(),
+    }
+
+    const fakeEventBus = {
+      subscribe: jest.fn(),
+      publish: jest.fn(),
+    }
+    projector.initModel(fakeModel)
+    projector.initEventBus(fakeEventBus)
+
+    const fakeCallback = jest.fn((event: FakeEvent) => ({ param1: 'value1' }))
+
+    beforeAll(() => {
+      projector.on(FakeEvent).create(fakeCallback)
+    })
+
+    it('should add a listener for the FakeEvent', () => {
+      expect(fakeEventBus.subscribe).toHaveBeenCalledTimes(1)
+
+      const eventType = fakeEventBus.subscribe.mock.calls[0][0]
+      expect(eventType).toEqual(FakeEvent.type)
+    })
+
+    it('should add a listener that calls model.create with the value returned by the callback', () => {
+      const listener = fakeEventBus.subscribe.mock.calls[0][1]
+
+      // Call the listener with a fake event
+      const fakeEvent = new FakeEvent({ payload: { param: '123' } })
+      listener(fakeEvent)
+
+      expect(fakeCallback).toHaveBeenCalledWith(fakeEvent)
+      expect(fakeModel.create).toHaveBeenCalledWith({ param1: 'value1' })
+    })
+  })
+
+  describe('on(Event).delete', () => {
+    const projector = makeProjector()
+    const fakeModel = {
+      destroy: jest.fn(),
+    }
+
+    const fakeEventBus = {
+      subscribe: jest.fn(),
+      publish: jest.fn(),
+    }
+    projector.initModel(fakeModel)
+    projector.initEventBus(fakeEventBus)
+
+    const fakeWhere = jest.fn((event: FakeEvent) => ({ id: 'value1' }))
+
+    beforeAll(() => {
+      projector.on(FakeEvent).delete(fakeWhere)
+    })
+
+    it('should add a listener for the FakeEvent', () => {
+      expect(fakeEventBus.subscribe).toHaveBeenCalledTimes(1)
+
+      const eventType = fakeEventBus.subscribe.mock.calls[0][0]
+      expect(eventType).toEqual(FakeEvent.type)
+    })
+
+    it('should add a listener that calls model.destroy with the where clause', () => {
+      const listener = fakeEventBus.subscribe.mock.calls[0][1]
+
+      // Call the listener with a fake event
+      const fakeEvent = new FakeEvent({ payload: { param: '123' } })
+      listener(fakeEvent)
+
+      expect(fakeWhere).toHaveBeenCalledWith(fakeEvent)
+      expect(fakeModel.destroy).toHaveBeenCalledWith({ where: { id: 'value1' } })
+    })
+  })
+
+  describe('on(Event).update', () => {
+    const projector = makeProjector()
+
+    const fakeInstance = {
+      save: jest.fn(() => Promise.resolve()),
+      set: jest.fn((key: string, value: any) => {}),
+      get: jest.fn(() => undefined),
+    }
+    const fakeModel = {
+      findOne: jest.fn((args: any) => Promise.resolve(fakeInstance)),
+    }
+
+    const fakeEventBus = {
+      subscribe: jest.fn(),
+      publish: jest.fn(),
+    }
+    projector.initModel(fakeModel)
+    projector.initEventBus(fakeEventBus)
+
+    const fakeDeltaCallback = jest.fn((event: FakeEvent) => ({ param1: 'value1' }))
+    const fakeWhereCallback = jest.fn((event: FakeEvent) => ({ id: '1234' }))
+
+    beforeAll(() => {
+      projector.on(FakeEvent).update({ where: fakeWhereCallback, delta: fakeDeltaCallback })
+    })
+
+    it('should add a listener for the FakeEvent', () => {
+      expect(fakeEventBus.subscribe).toHaveBeenCalledTimes(1)
+
+      const eventType = fakeEventBus.subscribe.mock.calls[0][0]
+      expect(eventType).toEqual(FakeEvent.type)
+    })
+
+    it('should add a listener that calls model.findOne with the where value from the callback, apply the delta value from the callback to it and then call instance.save', async () => {
+      const listener = fakeEventBus.subscribe.mock.calls[0][1]
+
+      // Call the listener with a fake event
+      const fakeEvent = new FakeEvent({ payload: { param: '123' } })
+      await listener(fakeEvent)
+
+      expect(fakeDeltaCallback).toHaveBeenCalledWith(fakeEvent)
+      expect(fakeWhereCallback).toHaveBeenCalledWith(fakeEvent)
+
+      expect(fakeModel.findOne).toHaveBeenCalledWith({ where: { id: '1234' } })
+      expect(fakeInstance.set).toHaveBeenCalledWith('param1', 'value1')
+      expect(fakeInstance.save).toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/infra/sequelize/helpers/projector.ts
@@ -0,0 +1,112 @@
+import { merge } from 'lodash'
+
+import { BaseDomainEvent, DomainEvent } from '../../../core/domain'
+import { logger } from '../../../core/utils'
+import { EventBus } from '../../../modules/eventStore'
+
+interface HasType {
+  type: string
+}
+
+interface Constructor<T> {
+  new (args: any): T
+}
+
+interface Handler<Event> {
+  (event: Event): any
+}
+
+export const makeProjector = () => {
+  const handlers: any[] = []
+  let eventBus: EventBus
+  let model: any
+
+  function subscribe<Event extends DomainEvent>(
+    eventType: string,
+    handler: (event: Event) => unknown
+  ) {
+    if (eventBus) {
+      eventBus.subscribe(eventType, handler)
+    } else {
+      handlers.push({
+        eventType,
+        handler,
+      })
+    }
+
+    // Returning handler allows projection handlers to export the result of the subscription
+    return handler
+  }
+
+  const on = <Payload, Event extends BaseDomainEvent<Payload>>(
+    eventClass: Constructor<Event> & HasType
+  ) => ({
+    create(cb: Handler<Event>) {
+      const eventHandler = async (event) => {
+        try {
+          await model.create(cb(event))
+        } catch (e) {
+          logger.error(e)
+        }
+      }
+      return subscribe(eventClass.type, eventHandler) as Handler<Event>
+    },
+    update(args: { where: Handler<Event>; delta: Handler<Event> }) {
+      const { where, delta } = args
+      const eventHandler = async (event) => {
+        try {
+          const instance = await model.findOne({ where: where(event) })
+
+          if (instance === null) {
+            logger.error(
+              `${model?.name}.on${eventClass.type} failed to find item to update (where: ${where(
+                event
+              )})`
+            )
+            return
+          }
+
+          for (const [key, value] of Object.entries(delta(event))) {
+            instance.set(key, isObject(value) ? merge({}, instance.get(key) || {}, value) : value)
+          }
+
+          await instance.save()
+        } catch (e) {
+          logger.error(e)
+        }
+      }
+      return subscribe(eventClass.type, eventHandler) as Handler<Event>
+    },
+    delete(where: Handler<Event>) {
+      const eventHandler = async (event) => {
+        try {
+          await model.destroy({ where: where(event) })
+        } catch (e) {
+          logger.error(e)
+        }
+      }
+      return subscribe(eventClass.type, eventHandler) as Handler<Event>
+    },
+  })
+
+  const initModel = (_model: any) => {
+    model = _model
+  }
+
+  const initEventBus = (_eventBus: EventBus) => {
+    eventBus = _eventBus
+    handlers.forEach(({ eventType, handler }) => {
+      eventBus.subscribe(eventType, handler)
+    })
+  }
+
+  return {
+    on,
+    initModel,
+    initEventBus,
+  }
+}
+
+function isObject(obj) {
+  return Object.getPrototypeOf(obj) === Object.prototype
+}
