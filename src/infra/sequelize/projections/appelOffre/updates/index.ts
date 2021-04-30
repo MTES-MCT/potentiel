4c4752e5fe44e7f051e56156095a95ed50fd333b
Pierre A. Duchateau <pad@dabla.com>
2021-04-30 17:19:32 +0200

    :bug:  Fix appelOffre projection init (#144)

--- src/infra/sequelize/projections/appelOffre/updates/index.ts
+++ src/infra/sequelize/projections/appelOffre/updates/index.ts
@@ -1,24 +1,12 @@
+import { logger } from '../../../../../core/utils'
+import { PeriodeCreated, PeriodeUpdated } from '../../../../../modules/appelOffre'
 import { EventBus } from '../../../../../modules/eventStore'
-import {
-  PeriodeCreated,
-  PeriodeUpdated,
-  AppelOffreUpdated,
-  AppelOffreCreated,
-  AppelOffreRemoved,
-} from '../../../../../modules/appelOffre'
 import { onPeriodeCreated } from './onPeriodeCreated'
 import { onPeriodeUpdated } from './onPeriodeUpdated'
-import { onAppelOffreUpdated } from './onAppelOffreUpdated'
-import { onAppelOffreCreated } from './onAppelOffreCreated'
-import { onAppelOffreRemoved } from './onAppelOffreRemoved'
-import { logger } from '../../../../../core/utils'
 
 export const initAppelOffreProjections = (eventBus: EventBus, models) => {
   eventBus.subscribe(PeriodeCreated.type, onPeriodeCreated(models))
   eventBus.subscribe(PeriodeUpdated.type, onPeriodeUpdated(models))
-  eventBus.subscribe(AppelOffreUpdated.type, onAppelOffreUpdated(models))
-  eventBus.subscribe(AppelOffreCreated.type, onAppelOffreCreated(models))
-  eventBus.subscribe(AppelOffreRemoved.type, onAppelOffreRemoved(models))
 
   logger.info('Initialized AppelOffre projections')
 }
