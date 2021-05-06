8f201df45b30b92b19d4718e05c68615b6a5bd0d
Pierre Duchateau <pad@dabla.com>
2021-05-06 18:31:28 +0200

    :star2:  cancelModificationRequest use-case

---
+++ src/modules/modificationRequest/useCases/cancelModificationRequest.spec.ts
@@ -0,0 +1,73 @@
+import { logger } from '../../../core/utils'
+import {
+  fakeTransactionalRepo,
+  makeFakeModificationRequest,
+} from '../../../__tests__/fixtures/aggregates'
+import makeFakeUser from '../../../__tests__/fixtures/user'
+import { UnauthorizedError } from '../../shared'
+import { ModificationRequest } from '../ModificationRequest'
+import { makeCancelModificationRequest } from './cancelModificationRequest'
+
+describe('cancelModificationRequest use-case', () => {
+  const fakeUser = makeFakeUser({ role: 'porteur-project' })
+
+  describe('when user has rights to this project', () => {
+    const shouldUserAccessProject = jest.fn(async () => true)
+
+    const fakeModificationRequest = {
+      ...makeFakeModificationRequest(),
+    }
+    const modificationRequestRepo = fakeTransactionalRepo(
+      fakeModificationRequest as ModificationRequest
+    )
+
+    const cancelModificationRequest = makeCancelModificationRequest({
+      modificationRequestRepo,
+      shouldUserAccessProject,
+    })
+
+    beforeAll(async () => {
+      const res = await cancelModificationRequest({
+        modificationRequestId: fakeModificationRequest.id,
+        cancelledBy: fakeUser,
+      })
+
+      if (res.isErr()) logger.error(res.error)
+      expect(res.isOk()).toEqual(true)
+    })
+
+    it('should call cancel on modificationRequest', () => {
+      expect(fakeModificationRequest.cancel).toHaveBeenCalledTimes(1)
+      expect(fakeModificationRequest.cancel).toHaveBeenCalledWith(fakeUser)
+    })
+  })
+
+  describe('when user does not have rights to this project', () => {
+    const shouldUserAccessProject = jest.fn(async () => false)
+
+    const fakeModificationRequest = {
+      ...makeFakeModificationRequest(),
+    }
+    const modificationRequestRepo = fakeTransactionalRepo(
+      fakeModificationRequest as ModificationRequest
+    )
+
+    const cancelModificationRequest = makeCancelModificationRequest({
+      modificationRequestRepo,
+      shouldUserAccessProject,
+    })
+
+    it('should return UnauthorizedError', async () => {
+      const res = await cancelModificationRequest({
+        modificationRequestId: fakeModificationRequest.id,
+        cancelledBy: fakeUser,
+      })
+
+      expect(res.isErr()).toEqual(true)
+      if (res.isOk()) return
+
+      expect(res.error).toBeInstanceOf(UnauthorizedError)
+      expect(fakeModificationRequest.cancel).not.toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/modules/modificationRequest/useCases/cancelModificationRequest.ts
@@ -0,0 +1,34 @@
+import { TransactionalRepository, UniqueEntityID } from '../../../core/domain'
+import { err, ResultAsync, wrapInfra } from '../../../core/utils'
+import { User } from '../../../entities'
+import { EntityNotFoundError, InfraNotAvailableError, UnauthorizedError } from '../../shared'
+import { ModificationRequest } from '../ModificationRequest'
+
+interface CancelModificationRequestDeps {
+  modificationRequestRepo: TransactionalRepository<ModificationRequest>
+  shouldUserAccessProject: (args: { user: User; projectId: string }) => Promise<boolean>
+}
+
+interface CancelModificationRequestArgs {
+  modificationRequestId: UniqueEntityID
+  cancelledBy: User
+}
+
+export const makeCancelModificationRequest = (deps: CancelModificationRequestDeps) => (
+  args: CancelModificationRequestArgs
+): ResultAsync<null, InfraNotAvailableError | EntityNotFoundError | UnauthorizedError> => {
+  const { modificationRequestRepo, shouldUserAccessProject } = deps
+  const { modificationRequestId, cancelledBy } = args
+
+  return modificationRequestRepo.transaction(modificationRequestId, (modificationRequest) => {
+    return wrapInfra(
+      shouldUserAccessProject({
+        user: cancelledBy,
+        projectId: modificationRequest.projectId.toString(),
+      })
+    ).andThen((isUserAuthorized) => {
+      if (!isUserAuthorized) return err(new UnauthorizedError())
+      return modificationRequest.cancel(cancelledBy)
+    })
+  })
+}
