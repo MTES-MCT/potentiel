b77fee77dbbc64a896cc06f32b45e3022c3da554
Pierre Duchateau <pad@dabla.com>
2021-05-20 16:01:50 +0200

    :star2:  Auth with keycloak-connect

--- src/controllers/auth/authentication.ts
+++ src/controllers/auth/authentication.ts
@@ -1,83 +1,117 @@
-import passport from 'passport'
-import { Strategy } from 'passport-local'
-import { ensureLoggedIn as _ensureLoggedIn } from 'connect-ensure-login'
-import { Application, Request, Response, NextFunction } from 'express'
-
-import { User } from '../../entities'
-import { login } from '../../useCases'
-import { userRepo } from '../../dataAccess'
-import { logger } from '../../core/utils'
-import routes from '../../routes'
+import { Application, response } from 'express'
+import makeSequelizeStore from 'connect-session-sequelize'
+import session from 'express-session'
+import Keycloak from 'keycloak-connect'
+import { User, USER_ROLES } from '../../entities'
+import { getUserByEmail } from '../../config'
+import { sequelizeInstance } from '../../sequelize.config'
 import { v1Router } from '../v1Router'
+import routes from '../../routes'
 
-interface RegisterAuthProps {
-  app: Application
-  loginRoute: string
-  successRoute: string
+const SequelizeStore = makeSequelizeStore(session.Store)
+
+const store = new SequelizeStore({
+  db: sequelizeInstance,
+  tableName: 'sessions',
+  checkExpirationInterval: 15 * 60 * 1000, // 15 minutes
+  expiration: 24 * 60 * 60 * 1000, // 1 day
+})
+
+const {
+  KEYCLOAK_SERVER,
+  KEYCLOAK_REALM,
+  KEYCLOAK_USER_CLIENT_ID,
+  KEYCLOAK_USER_CLIENT_SECRET,
+} = process.env
+
+if (
+  !KEYCLOAK_SERVER ||
+  !KEYCLOAK_REALM ||
+  !KEYCLOAK_USER_CLIENT_ID ||
+  !KEYCLOAK_USER_CLIENT_SECRET
+) {
+  console.error('Missing KEYCLOAK env vars')
+  process.exit(1)
 }
+export const keycloak = new Keycloak(
+  {
+    store,
+  },
+  {
+    'confidential-port': 0,
+    'auth-server-url': KEYCLOAK_SERVER,
+    resource: KEYCLOAK_USER_CLIENT_ID,
+    'ssl-required': 'external',
+    'bearer-only': false,
+    realm: KEYCLOAK_REALM,
+    // @ts-ignore
+    credentials: {
+      secret: KEYCLOAK_USER_CLIENT_SECRET,
+    },
+  }
+)
 
 // Method to be called first
 // Sets up passport middleware in the express app
-const registerAuth = ({ app }: RegisterAuthProps) => {
-  //
-  // Configure Passport authenticated session persistence
-  //
-  passport.serializeUser(function (user: User, done) {
-    done(null, user.id)
-  })
-
-  passport.deserializeUser(async function (id: User['id'], done) {
-    const userResult = await userRepo.findById(id)
-
-    if (userResult.is_none()) {
-      logger.error('Authentication: Found user session id but no matching user')
-      return done(null, null)
-    }
-
-    done(null, userResult.unwrap())
-  })
-
-  passport.use(
-    new Strategy(
-      {
-        usernameField: 'email',
-        passwordField: 'password',
-      },
-      function (username: string, password: string, done) {
-        login({ email: username, password })
-          .then((userResult) => {
-            if (userResult.is_err()) {
-              logger.info(userResult.unwrap_err().toString())
-              return done(null, false)
-            }
-
-            return done(null, userResult.unwrap())
-          })
-          .catch((err) => {
-            // Should never happen because login shouldn't throw
-            logger.error(err)
-            return done(err)
-          })
-      }
-    )
+interface RegisterAuthProps {
+  app: Application
+  sessionSecret: string
+}
+export const registerAuth = ({ app, sessionSecret }: RegisterAuthProps) => {
+  app.use(
+    session({
+      secret: sessionSecret,
+      store,
+      resave: false,
+      proxy: true,
+      saveUninitialized: false,
+    })
   )
 
-  //
-  // Initialize authentication state from session, if any
-  //
-  app.use(passport.initialize())
-  app.use(passport.session())
+  app.use(keycloak.middleware())
 
-  v1Router.post(routes.LOGIN_ACTION, postLogin())
-  v1Router.get(routes.LOGOUT_ACTION, logoutMiddleware, (req, res) => {
-    res.redirect('/')
+  // Add a middleware to attach the User object on the request (if logged-in)
+  app.use((request, response, next) => {
+    const token = request.kauth?.grant?.access_token
+    const userEmail = token?.content?.email
+    if (userEmail) {
+      return getUserByEmail(userEmail).then((userResult) => {
+        if (userResult.isOk() && userResult.value !== null) {
+          const kRole = USER_ROLES.find((role) => token.hasRealmRole(role))
+          if (kRole) {
+            request.user = userResult.value
+            request.user.role = kRole
+          }
+        }
+        next()
+      })
+    }
+
+    next()
   })
+
+  v1Router.get('/test/auth', (request, response) => {
+    console.log('user', request.user)
+
+    response.send('ok')
+  })
+
+  v1Router.get(routes.LOGIN, keycloak.protect(), (req, res) => {
+    res.redirect(routes.REDIRECT_BASED_ON_ROLE)
+  })
+
   v1Router.get(routes.REDIRECT_BASED_ON_ROLE, async (req, res) => {
     const user = req.user as User
 
     if (!user) {
       // Sometimes, the user session is not immediately available in the req object
       // In that case, wait a bit and redirect to the same url
+
+      if (req.kauth) {
+        // This user has a session but no user was found, log him out
+        res.redirect('/logout')
+        return
+      }
 
       // Use a retry counter to avoid infinite loop
       const retryCount = Number(req.query.retry || 0)
@@ -100,42 +134,14 @@ const registerAuth = ({ app }: RegisterAuthProps) => {
   })
 }
 
-// Handler for the login route
-const postLogin = () => {
-  return passport.authenticate('local', {
-    successReturnToOrRedirect: routes.REDIRECT_BASED_ON_ROLE,
-    failureRedirect: `${routes.LOGIN}?error=Identifiant ou mot de passe erroné.`,
-  })
+export const ensureLoggedIn = () => {
+  return keycloak.protect()
 }
 
-const logoutMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  req.logout()
-
-  next()
-}
-
-const ensureLoggedIn = () => _ensureLoggedIn(routes.LOGIN)
-
-const ensureRole = (roles: User['role'] | Array<User['role']>) => (req, res, next) => {
-  const user = req.user as User
-
-  if (!user) {
-    return res.redirect(routes.LOGIN)
-  }
-
+export const ensureRole = (roles: User['role'] | User['role'][]) => {
   const roleList = Array.isArray(roles) ? roles : [roles]
 
-  if (!roleList.includes(user.role)) {
-    return res.redirect(routes.REDIRECT_BASED_ON_ROLE)
-  }
-
-  // Ok to move forward
-  next()
-}
-
-export {
-  registerAuth,
-  // Handler for all auth enabled routes
-  ensureLoggedIn,
-  ensureRole,
+  return keycloak.protect((token) => {
+    return roleList.some((role) => token.hasRealmRole(role))
+  })
 }
