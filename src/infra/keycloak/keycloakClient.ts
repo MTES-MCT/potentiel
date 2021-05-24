977eba57307cfa8c879b85d13b0d9205309c26b0
Pierre Duchateau <pad@dabla.com>
2021-05-24 19:40:37 +0200

    :star2:  Plug all user facing routes to keycloak for auth

---
+++ src/infra/keycloak/keycloakClient.ts
@@ -0,0 +1,23 @@
+import KeycloakAdmin from 'keycloak-admin'
+
+const {
+  KEYCLOAK_SERVER,
+  KEYCLOAK_REALM,
+  KEYCLOAK_ADMIN_CLIENT_ID,
+  KEYCLOAK_ADMIN_CLIENT_SECRET,
+} = process.env
+
+if (
+  !KEYCLOAK_SERVER ||
+  !KEYCLOAK_REALM ||
+  !KEYCLOAK_ADMIN_CLIENT_ID ||
+  !KEYCLOAK_ADMIN_CLIENT_SECRET
+) {
+  console.error('Missing KEYCLOAK env vars')
+  process.exit(1)
+}
+
+export const keycloakAdminClient = new KeycloakAdmin({
+  baseUrl: KEYCLOAK_SERVER,
+  realmName: KEYCLOAK_REALM,
+})
