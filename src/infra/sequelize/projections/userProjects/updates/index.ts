64bd79392f6ffa8dd68544ebdf1f5c654eb3a959
Pierre Duchateau <pad@dabla.com>
2021-06-11 15:00:38 +0200

    :bug:  Hookup onUserInvitedToProject projector

--- src/infra/sequelize/projections/userProjects/updates/index.ts
+++ src/infra/sequelize/projections/userProjects/updates/index.ts
@@ -2,8 +2,10 @@ import { EventBus } from '../../../../../modules/eventStore'
 
 import { onUserRightsToProjectRevoked } from './onUserRightsToProjectRevoked'
 import { onUserRightsToProjectGranted } from './onUserRightsToProjectGranted'
+import { onUserInvitedToProject } from './onUserInvitedToProject'
 import { logger } from '../../../../../core/utils'
 import {
+  UserInvitedToProject,
   UserRightsToProjectRevoked,
   UserRightsToProjectGranted,
 } from '../../../../../modules/authorization'
@@ -15,6 +17,7 @@ export const initUserProjectsProjections = (eventBus: EventBus, models) => {
   eventBus.subscribe(UserRightsToProjectGranted.type, onUserRightsToProjectGranted(models))
   eventBus.subscribe(ProjectClaimed.type, onProjectClaimed(models))
   eventBus.subscribe(ProjectClaimedByOwner.type, onProjectClaimed(models))
+  eventBus.subscribe(UserInvitedToProject.type, onUserInvitedToProject(models))
 
   logger.info('Initialized User Projects projections')
 }
