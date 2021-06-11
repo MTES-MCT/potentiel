31e265c5e56d6102dd4166c2d0c43fd33bff39c8
Pierre Duchateau <pad@dabla.com>
2021-06-11 16:06:09 +0200

    :star2:  when invited partner user, emit PartnerUserInvited

---
+++ src/modules/authorization/events/PartnerUserInvited.ts
@@ -0,0 +1,18 @@
+import { BaseDomainEvent, DomainEvent } from '../../../core/domain/DomainEvent'
+
+export interface PartnerUserInvitedPayload {
+  userId: string
+  role: string
+  invitedBy: string
+}
+export class PartnerUserInvited
+  extends BaseDomainEvent<PartnerUserInvitedPayload>
+  implements DomainEvent {
+  public static type: 'PartnerUserInvited' = 'PartnerUserInvited'
+  public type = PartnerUserInvited.type
+  currentVersion = 1
+
+  aggregateIdFromPayload(payload: PartnerUserInvitedPayload) {
+    return undefined
+  }
+}
