6662613ddae7dd788a4700e453d798dff12125cb
Pierre Duchateau <pad@dabla.com>
2021-06-16 16:23:12 +0200

    :recycle:  Update candidate notification for keycloak process

--- src/modules/candidateNotification/eventHandlers/handleCandidateNotifiedForPeriode.ts
+++ src/modules/candidateNotification/eventHandlers/handleCandidateNotifiedForPeriode.ts
@@ -1,85 +1,62 @@
-import { errAsync, fromOldResult, fromOldResultAsync, ResultAsync } from '../../../core/utils'
-import { ProjectAdmissionKeyRepo } from '../../../dataAccess'
-import { makeProjectAdmissionKey, ProjectAdmissionKey } from '../../../entities'
+import { logger, wrapInfra } from '../../../core/utils'
 import routes from '../../../routes'
 import { GetPeriodeTitle } from '../../appelOffre'
-import { EventBus } from '../../eventStore'
 import { NotificationService } from '../../notification'
-import { OtherError } from '../../shared'
-import { CandidateNotificationForPeriodeFailed } from '../events'
+import { GetUserByEmail, makeCreateUser } from '../../users'
 import { CandidateNotifiedForPeriode } from '../events/CandidateNotifiedForPeriode'
 
 export const handleCandidateNotifiedForPeriode = (deps: {
-  eventBus: EventBus
   sendNotification: NotificationService['sendNotification']
-  saveProjectAdmissionKey: ProjectAdmissionKeyRepo['save']
+  createUser: ReturnType<typeof makeCreateUser>
+  getUserByEmail: GetUserByEmail
   getPeriodeTitle: GetPeriodeTitle
 }) => async (event: CandidateNotifiedForPeriode) => {
-  const { eventBus, sendNotification, saveProjectAdmissionKey, getPeriodeTitle } = deps
+  const { sendNotification, createUser, getPeriodeTitle, getUserByEmail } = deps
   const {
     payload: { periodeId, appelOffreId, candidateEmail, candidateName },
-    requestId,
   } = event
 
-  const notificationResult = await _sendCandidateNotification()
+  await getUserByEmail(candidateEmail)
+    .andThen((userOrNull) => {
+      if (userOrNull === null) {
+        return createUser({
+          role: 'porteur-projet',
+          email: candidateEmail,
+          fullName: candidateName,
+        })
+      }
 
-  if (notificationResult.isErr()) {
-    await eventBus.publish(
-      new CandidateNotificationForPeriodeFailed({
-        payload: {
-          candidateEmail,
-          periodeId,
-          appelOffreId,
-          error: notificationResult.error.message,
-        },
-        requestId,
-      })
+      return _sendCandidateNotification()
+    })
+    .match(
+      () => {},
+      (e) => {
+        logger.info(`Failed to notify candidate for periode ${appelOffreId} - ${periodeId}`)
+        logger.error(e)
+      }
     )
-  }
 
-  function _sendCandidateNotification(): ResultAsync<null, Error> {
-    let projectAdmissionKey: ProjectAdmissionKey | undefined
-    return fromOldResult(
-      makeProjectAdmissionKey({
-        email: candidateEmail,
-        fullName: candidateName,
-        appelOffreId,
-        periodeId,
-      })
-    )
-      .asyncAndThen((_projectAdmissionKey) => {
-        projectAdmissionKey = _projectAdmissionKey
-        return fromOldResultAsync(saveProjectAdmissionKey(_projectAdmissionKey))
-      })
-      .andThen(() => getPeriodeTitle(appelOffreId, periodeId))
-      .andThen(({ periodeTitle, appelOffreTitle }) => {
-        const subject = `Résultats de la ${periodeTitle} période de l'appel d'offres ${appelOffreTitle}`
+  function _sendCandidateNotification() {
+    return getPeriodeTitle(appelOffreId, periodeId).andThen(({ periodeTitle, appelOffreTitle }) => {
+      const subject = `Résultats de la ${periodeTitle} période de l'appel d'offres ${appelOffreTitle}`
 
-        if (!projectAdmissionKey) return errAsync(new OtherError('Impossibru!!!')) // Not actually possible because of chaining above
-
-        return ResultAsync.fromPromise(
-          sendNotification({
-            type: 'designation',
-            context: {
-              projectAdmissionKeyId: projectAdmissionKey.id,
-              appelOffreId,
-              periodeId,
-            },
-            variables: {
-              invitation_link: routes.PROJECT_INVITATION({
-                projectAdmissionKey: projectAdmissionKey.id,
-              }),
-            },
-            message: {
-              subject,
-              email: candidateEmail,
-              name: candidateName,
-            },
-          }),
-          (e: any) => {
-            return new OtherError(e.message)
-          }
-        )
-      })
+      return wrapInfra(
+        sendNotification({
+          type: 'designation',
+          context: {
+            appelOffreId,
+            periodeId,
+          },
+          variables: {
+            invitation_link: routes.USER_DASHBOARD,
+          },
+          message: {
+            subject,
+            email: candidateEmail,
+            name: candidateName,
+          },
+        })
+      )
+    })
   }
 }
