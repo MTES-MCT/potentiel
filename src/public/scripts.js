0270a1c78bb09e1569169391a5ed066f0a7113d5
Julien HAIRAPIAN <julien.hairapian@gmail.com>
2021-07-30 13:53:28 +0200

    :star2:  Add checkbox multi select for pot commun in script.js

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -20,6 +20,7 @@ window.initHandlers = function () {
   addProjectListSelectionHandler()
   addConfirmHandlers()
   addGoToOnClickHandlers()
+  addMissingOwnerProjectListSelectionHandler()
 }
 
 document.addEventListener('DOMContentLoaded', () => window.initHandlers())
@@ -313,6 +314,79 @@ function addProjectListSelectionHandler() {
   if (selectAllCheckbox) {
     selectAllCheckbox.addEventListener('change', function (event) {
       projectCheckboxes.forEach((item) => toggleProjectBox(item, event.target.checked))
+    })
+  }
+}
+
+function addMissingOwnerProjectListSelectionHandler() {
+  const projectCheckboxes = document.querySelectorAll(
+    '[data-testid=missingOwnerProjectList-item-checkbox]'
+  )
+
+  const selectAllCheckbox = document.querySelector(
+    '[data-testid=missingOwnerProjectList-selectAll-checkbox]'
+  )
+
+  const claimProjectsSubmitButton = document.querySelector(
+    '[data-testid=claim-projects-submit-button]'
+  )
+
+  const swornStatementCheckbox = document.querySelector('[data-testid=sworn-statement]')
+
+  const claimedProjectList = document.querySelector('[data-testid=claimed-project-list]')
+
+  function updateAccessFormVisibility() {
+    claimedProjectList?.options.length && swornStatementCheckbox?.checked
+      ? (claimProjectsSubmitButton.disabled = false)
+      : (claimProjectsSubmitButton.disabled = true)
+  }
+
+  function findOption(options, value) {
+    for (const option of options) {
+      if (option.value === value) return option
+    }
+  }
+
+  function toggleProjectInList(projectId, isSelected) {
+    if (claimedProjectList) {
+      const projectOption = findOption(claimedProjectList.options, projectId)
+      if (isSelected && !projectOption) {
+        claimedProjectList.options.add(new Option(projectId, projectId, true, true))
+      }
+
+      if (!isSelected && projectOption) {
+        projectOption.remove()
+      }
+    }
+
+    updateAccessFormVisibility()
+  }
+
+  function toggleProjectBox(item, isSelected) {
+    const projectId = item.getAttribute('data-projectid')
+
+    item.checked = isSelected
+
+    toggleProjectInList(projectId, isSelected)
+  }
+
+  projectCheckboxes.forEach((item) =>
+    item.addEventListener('change', function (event) {
+      if (selectAllCheckbox) {
+        selectAllCheckbox.checked = false
+      }
+
+      toggleProjectBox(item, event.target.checked)
+    })
+  )
+
+  if (selectAllCheckbox) {
+    selectAllCheckbox.addEventListener('change', function (event) {
+      projectCheckboxes.forEach((item) => toggleProjectBox(item, event.target.checked))
+    })
+
+    swornStatementCheckbox.addEventListener('change', function () {
+      updateAccessFormVisibility()
     })
   }
 }
