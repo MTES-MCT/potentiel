2d4cec19e1a8e8811e970047fb4e0a06ea05a9f4
Pierre Duchateau <pad@dabla.com>
2021-08-17 12:27:12 +0200

    :wrench:  fix Statistiques stories file

--- src/views/pages/statistiques.stories.tsx
+++ src/views/pages/statistiques.stories.tsx
@@ -5,25 +5,9 @@ import makeFakeRequest from '../../__tests__/fixtures/request'
 export default { title: 'Statistiques' }
 
 const props = {
-  projetsTotal: 962,
-  projetsLaureats: 558,
-  porteursProjetNotifies: 162,
-  porteursProjetNotifiesInscrits: 140,
-  parrainages: 45,
-  telechargementsAttestation: 863,
-  projetsAvecAttestation: 925,
-  gfDeposees: 100,
-  gfDues: 152,
-  dcrDeposees: 73,
-  dcrDues: 260,
-  demandes: {
-    actionnaire: 0,
-    producteur: 0,
-    fournisseur: 0,
-    puissance: 2,
-    abandon: 1,
-    recours: 7,
-    delai: 0
-  }}
+  request: makeFakeRequest(),
+  mainIframeUrl: '',
+  mapIframeUrl: '',
+}
 
-export const empty = () => <Statistiques {...props} request={makeFakeRequest()}/>
+export const empty = () => <Statistiques {...props} />
