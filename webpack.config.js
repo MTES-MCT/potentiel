b8af3ce0960bb558b791eafa1f5c30aa1d68aa66
Pierre Duchateau <pad@dabla.com>
2021-08-17 13:17:13 +0200

    :wrench:  Webpack: use production build for staging

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const pageEntries = fs.readdirSync(pageDir)
                 }), {})
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging' ? 'production' : 'development',
   entry: {
     ...pageEntries,
     shared: ['react', 'react-dom']
