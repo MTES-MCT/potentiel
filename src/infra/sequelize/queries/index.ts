MTES-MCT/enr

:star2:  Use keycloak to display/relance pending candidate invitations

src/infra/sequelize/queries/index.ts
@@ -57,3 +57,5 @@ export const getModificationRequestDataForResponseTemplate = makeGetModification
   }
 )
 export const getModificationRequestRecipient = makeGetModificationRequestRecipient(models)
+
+export * from './candidateNotification'

MTES-MCT/potentiel

src/infra/sequelize/queries/index.ts
@@ -1,7 +1,7 @@
-export * from './appelOffre';
-export * from './authorization';
-export * from './file';
-export * from './modificationRequest';
-export * from './notification';
-export * from './project';
-export * from './users';
+export * from './appelOffre'
+export * from './authorization'
+export * from './file'
+export * from './modificationRequest'
+export * from './notification'
+export * from './project'
+export * from './users'