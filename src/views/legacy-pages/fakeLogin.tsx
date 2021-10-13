af32e724a8e32cc2868240876dbb3c3b6175b772
Pierre Duchateau <pad@dabla.com>
2021-10-13 11:39:00 +0200

    :bug:  Fix fake login case where email is not known

--- src/views/legacy-pages/fakeLogin.tsx
+++ src/views/legacy-pages/fakeLogin.tsx
@@ -9,12 +9,18 @@ interface Props {
 
 /* Pure component */
 export default function FakeLoginPage({ request }: Props) {
+  const { error } = (request.query as any) || {}
   return (
     <main role="main">
       <section className="section section-grey">
         <div className="container">
           <form action={routes.LOGIN_ACTION} method="post" name="form">
             <h3 id="login">Je m‘identifie</h3>
+            {!!error && (
+              <div className="notification error" {...dataId('error-message')}>
+                {error}
+              </div>
+            )}
             <div className="form__group">
               <label htmlFor="email">Courrier électronique</label>
               <input type="email" name="email" id="email" {...dataId('email-field')} />
