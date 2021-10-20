f66bf5e4ee933879a579731362ab70bc5b6a59aa
Pierre Duchateau <pad@dabla.com>
2021-10-20 12:18:35 +0200

    :bug:  handleLegacyModif* should retry after s…
    It calls findProjectByIdentifiers to get the
    projectId for this modification and sometimes
    the project is not in the database yet. If the
    first check returns null, try again after 5
    seconds.

--- src/modules/modificationRequest/eventHandlers/handleLegacyModificationRawDataImported.ts
+++ src/modules/modificationRequest/eventHandlers/handleLegacyModificationRawDataImported.ts
@@ -1,37 +1,48 @@
-import { logger, errAsync } from '../../../core/utils'
+import { logger, errAsync, okAsync } from '../../../core/utils'
+import { withDelay } from '../../../core/utils/withDelay'
 import { EventBus } from '../../eventStore'
 import { FindProjectByIdentifiers } from '../../project'
 import { EntityNotFoundError } from '../../shared'
 import { LegacyModificationImported, LegacyModificationRawDataImported } from '../events'
 
-export const handleLegacyModificationRawDataImported =
-  (deps: { findProjectByIdentifiers: FindProjectByIdentifiers; eventBus: EventBus }) =>
-  async (event: LegacyModificationRawDataImported) => {
-    const {
-      payload: { appelOffreId, periodeId, familleId, numeroCRE, importId, modifications },
-    } = event
+export const handleLegacyModificationRawDataImported = (deps: {
+  findProjectByIdentifiers: FindProjectByIdentifiers
+  eventBus: EventBus
+}) => async (event: LegacyModificationRawDataImported) => {
+  const {
+    payload: { appelOffreId, periodeId, familleId, numeroCRE, importId, modifications },
+  } = event
 
-    await deps
-      .findProjectByIdentifiers({ appelOffreId, periodeId, familleId, numeroCRE })
-      .andThen((projectIdOrNull) => {
-        if (projectIdOrNull !== null) {
-          return deps.eventBus.publish(
-            new LegacyModificationImported({
-              payload: {
-                projectId: projectIdOrNull,
-                modifications,
-                importId,
-              },
-            })
-          )
-        }
+  const findProject = () =>
+    deps.findProjectByIdentifiers({ appelOffreId, periodeId, familleId, numeroCRE })
 
-        return errAsync(new EntityNotFoundError())
-      })
-      .match(
-        () => {},
-        (e: Error) => {
-          logger.error(e)
-        }
-      )
-  }
+  findProject()
+    .andThen(
+      (projectIdOrNull): ReturnType<typeof findProject> => {
+        if (projectIdOrNull !== null) return okAsync(projectIdOrNull)
+
+        return withDelay(5000, () => findProject())
+      }
+    )
+    .andThen((projectIdOrNull) => {
+      if (projectIdOrNull !== null) {
+        return deps.eventBus.publish(
+          new LegacyModificationImported({
+            payload: {
+              projectId: projectIdOrNull,
+              modifications,
+              importId,
+            },
+          })
+        )
+      }
+
+      return errAsync(new EntityNotFoundError())
+    })
+    .match(
+      () => {},
+      (e: Error) => {
+        logger.error(e)
+      }
+    )
+}
