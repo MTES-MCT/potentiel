ee7711266c9a8ebeedddfd86ed1cb7fca2c0d2f8
HanaeY <72154904+HanaeY@users.noreply.github.com>
2021-10-29 14:47:39 +0200

    597 Help link added to the header when user is logged in
    * :star2:  help link added to the header when user is logged in
    * :star2:  PR-follow-up

--- src/views/components/header.tsx
+++ src/views/components/header.tsx
@@ -29,6 +29,13 @@ const Header = ({ request }: HeaderProps) => {
             <li className="nav__item">
               {user ? (
                 <>
+                  <a 
+                    href="https://docs.potentiel.beta.gouv.fr/info/guide-dutilisation-potentiel" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Aide <img src="/images/icons/external/external-link-alt-solid.svg" style={{width: 12, height: 12}} alt="Ouvrir dans un nouvel onglet"></img>
+                  </a>
                   <span>{user.fullName}</span>
                   <a href={routes.LOGOUT_ACTION}>Me déconnecter</a>
                 </>
