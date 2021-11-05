ab55e512f4d977d5a3a48c581f2a647088bcb741
Hanae <hanae.yamashita@lilo.org>
2021-11-05 14:08:44 +0100

    :wrench:  New CDC choice form component

--- src/views/pages/newModificationRequestPage/index.tsx
+++ src/views/pages/newModificationRequestPage/index.tsx
@@ -12,6 +12,7 @@ import { getAutoAcceptRatiosForAppelOffre } from '../../../modules/modificationR
 
 import moment from 'moment'
 import ModificationRequestActionTitles from '../../components/ModificationRequestActionTitles'
+import { CDCChoiceForm } from '../../components/CDCChoiceForm'
 import toNumber from '../../../helpers/toNumber';
 import { isStrictlyPositiveNumber } from '../../../helpers/formValidators'
 
@@ -156,63 +157,7 @@ export const NewModificationRequest = PageLayout(({
                 </a>
                 .
               </p>
-              {!project.newRulesOptIn && (
-                <>
-                  <div style={{border: "1px solid #adb9c9", padding: 20, borderRadius: 3, marginBottom: 15}}>
-                    <div className="inline-radio-option">
-                      <input
-                          type="radio"
-                          name="newRulesOptIn"
-                          value="Anciennes règles"
-                          {...dataId('modificationRequest-oldRules')}
-                          disabled={project.newRulesOptIn}
-                      />
-                      <label htmlFor="Anciennes règles" style={{flex: 1}}>
-                        <strong>Instruction selon les dispositions du cahier des charges en vigueur au moment de
-                        la candidature &nbsp;</strong>
-                        {cahiersChargesURLs?.oldCahierChargesURL && (
-                          <a href={cahiersChargesURLs?.oldCahierChargesURL}>
-                            (voir le cahier des charges)
-                          </a>
-                        )}.
-                      </label>
-                    </div>
-                    <div>
-                      <ul>
-                        <li style={{listStyleImage: 'URL(/images/icons/external/arrow-right.svg)'}}>Je dois envoyer ma demande ou mon signalement au format papier.</li>
-                        <li style={{listStyleImage: 'URL(/images/icons/external/arrow-right.svg)'}}>Je pourrai changer de mode d'instruction lors de ma prochaine demande si je le souhaite.</li>
-                      </ul>
-                    </div>
-                  </div>
-                </>
-              )}
-              <div style={{border: "1px solid #adb9c9", padding: 20, borderRadius: 3, marginBottom: 15}}>
-                <div className="inline-radio-option">
-                  <input
-                      type="radio"
-                      name="newRulesOptIn"
-                      value="Nouvelles règles"
-                      defaultChecked={project.newRulesOptIn}
-                      {...dataId('modificationRequest-newRules')}
-                      disabled={project.newRulesOptIn}
-                  />
-                  <label htmlFor="Nouvelles règles" style={{flex: 1}}>
-                  <strong>Instruction selon le cahier des charges modifié rétroactivement et publié le 30/07/2021, 
-                  pris en application du décret n° 2019-1175 du 14 novembre 2019&nbsp;</strong>
-                    {cahiersChargesURLs?.newCahierChargesURL && (
-                      <a href={cahiersChargesURLs?.newCahierChargesURL}>(voir le cahier des charges)</a>
-                    )}.
-                  </label>
-                </div>
-                <div>
-                      <ul>
-                        <li style={{listStyleImage: 'URL(/images/icons/external/arrow-right.svg)'}}>Ce choix s'appliquera à toutes les futures demandes faites sous Potentiel.</li>
-                        <li style={{listStyleImage: 'URL(/images/icons/external/arrow-right.svg)'}}>Une modification ultérieure pourra toujours être instruite selon le cahier des
-                            charges en vigueur au moment du dépôt de l'offre, à condition qu'elle soit soumise
-                            au format papier en précisant ce choix.</li>
-                      </ul>
-                    </div>
-              </div>
+              <CDCChoiceForm project={project} cahiersChargesURLs={cahiersChargesURLs}/>
             </div>
 
             <div {...dataId('modificationRequest-demandesInputs')}>
