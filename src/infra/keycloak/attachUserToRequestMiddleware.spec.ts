693cdec2c1f664223a08bcce92cf2e5f85b1d39e
Julien Pavon <julien.pavon@gmail.com>
2021-11-08 15:09:17 +0100

    :white_check_mark: Add test when user and role exists

--- src/infra/keycloak/attachUserToRequestMiddleware.spec.ts
+++ src/infra/keycloak/attachUserToRequestMiddleware.spec.ts
@@ -30,18 +30,18 @@ describe(`attachUserToRequestMiddleware`, () => {
   })
 
   describe(`when the path is not a static one`, () => {
-    const hasRealmRole = jest.fn()
-
-    const request = {
-      path: '/a-protected-path',
-    } as express.Request
-    const token = {
-      content: {},
-      hasRealmRole,
-    }
-    request['kauth'] = { grant: { access_token: token } }
-
     describe(`when there is no user email in the keycloak access token`, () => {
+      const hasRealmRole = jest.fn()
+
+      const request = {
+        path: '/a-protected-path',
+      } as express.Request
+      const token = {
+        content: {},
+        hasRealmRole,
+      }
+      request['kauth'] = { grant: { access_token: token } }
+
       token.content['email'] = undefined
       const nextFunction = jest.fn()
 
@@ -54,21 +54,21 @@ describe(`attachUserToRequestMiddleware`, () => {
     })
 
     describe(`when there is a user email in the keycloak access token`, () => {
-      const user: User = {
-        email: 'user@email.com',
-        fullName: 'User',
-        id: 'user-id',
-        isRegistered: true,
-        role: 'admin',
-      }
-      const { email: userEmail } = user
-
-      token.content['email'] = userEmail
-      getUserByEmail.mockImplementation((email) =>
-        email === userEmail ? okAsync(user) : okAsync(null)
-      )
-
       describe(`when no role in the keycloak access token`, () => {
+        const hasRealmRole = jest.fn()
+
+        const request = {
+          path: '/a-protected-path',
+        } as express.Request
+
+        const token = {
+          content: {},
+          hasRealmRole,
+        }
+        request['kauth'] = { grant: { access_token: token } }
+
+        token.content['email'] = 'user@email.com'
+
         hasRealmRole.mockReturnValue(false)
         const nextFunction = jest.fn()
 
@@ -77,6 +77,46 @@ describe(`attachUserToRequestMiddleware`, () => {
         it('should not attach the user to the request and execute the next function', () => {
           expect(request.user).toBeUndefined()
           expect(nextFunction).toHaveBeenCalled()
+        })
+      })
+
+      describe(`when a user role is in the keycloak access token`, () => {
+        describe(`when the user exists in Potentiel`, () => {
+          const hasRealmRole = jest.fn()
+
+          const request = {
+            path: '/a-protected-path',
+          } as express.Request
+
+          const token = {
+            content: {},
+            hasRealmRole,
+          }
+          request['kauth'] = { grant: { access_token: token } }
+
+          const user: User = {
+            email: 'user@email.com',
+            fullName: 'User',
+            id: 'user-id',
+            isRegistered: true,
+            role: 'admin',
+          }
+          const { email: userEmail } = user
+
+          token.content['email'] = userEmail
+          getUserByEmail.mockImplementation((email) =>
+            email === userEmail ? okAsync(user) : okAsync(null)
+          )
+
+          hasRealmRole.mockReturnValue(true)
+          const nextFunction = jest.fn()
+
+          middleware(request, {} as express.Response, nextFunction)
+
+          it('should attach the user to the request and execute the next function', () => {
+            expect(request.user).toMatchObject(user)
+            expect(nextFunction).toHaveBeenCalled()
+          })
         })
       })
     })
