acc8dbcf4fec9ffdb24d99ab2b95b9d18e909d7b
Julien Pavon <julien.pavon@gmail.com>
2021-11-08 14:51:33 +0100

    :white_check_mark: Add test when there is no user email in the token

--- src/infra/keycloak/attachUserToRequestMiddleware.spec.ts
+++ src/infra/keycloak/attachUserToRequestMiddleware.spec.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { okAsync } from '../../core/utils'
+import { User } from '../../entities'
 import { makeAttachUserToRequestMiddleware } from './attachUserToRequestMiddleware'
 
 describe(`attachUserToRequestMiddleware`, () => {
@@ -16,6 +18,31 @@ describe(`attachUserToRequestMiddleware`, () => {
       const request = {
         path,
       } as express.Request
+      const nextFunction = jest.fn()
+
+      middleware(request, {} as express.Response, nextFunction)
+
+      it('should not attach the user to the request and execute the next function', () => {
+        expect(request.user).toBeUndefined()
+        expect(nextFunction).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe(`when the path is not a static one`, () => {
+    const hasRealmRole = jest.fn()
+
+    const request = {
+      path: '/a-protected-path',
+    } as express.Request
+    const token = {
+      content: {},
+      hasRealmRole,
+    }
+    request['kauth'] = { grant: { access_token: token } }
+
+    describe(`when there is no user email in the keycloak access token`, () => {
+      token.content['email'] = undefined
       const nextFunction = jest.fn()
 
       middleware(request, {} as express.Response, nextFunction)
