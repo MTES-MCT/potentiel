ba74eb08426578f550589f257d6a4bfcb085e829
Hanae <hanae.yamashita@lilo.org>
2021-11-16 14:42:56 +0100

    :wrench:  Update migration script to fill projects table with PotentielId

--- src/infra/sequelize/migrations/20211116112634-add-project-potentielIdentifier.js
+++ src/infra/sequelize/migrations/20211116112634-add-project-potentielIdentifier.js
@@ -1,11 +1,61 @@
 'use strict';
+const crypto = require('crypto')
+
+const makeProjectIdentifier = (project) => {
+  const nakedIdentifier =
+    project.appelOffreId.replace(/ /g, '') +
+    '-P' +
+    project.periodeId +
+    (project.familleId ? '-F' + project.familleId : '') +
+    '-' +
+    project.numeroCRE
+
+  return (
+    nakedIdentifier +
+    '-' +
+    crypto.createHash('md5').update(project.id).digest('hex').substring(0, 3).toUpperCase()
+  )
+}
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('projects', 'potentielIdentifier', {
-      type: Sequelize.DataTypes.STRING,
-      allowNull: false,
-    })
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+
+      await queryInterface.addColumn('projects', 'potentielIdentifier', {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        transaction,
+      })
+
+      const projects = await queryInterface.sequelize.query(
+        'SELECT * FROM "projects', 
+        {
+          type: queryInterface.sequelize.QueryTypes.SELECT,
+          transaction,
+        }
+      )
+
+      for(const project of projects) {
+        const potentielIdentifier = makeProjectIdentifier(project)
+        await queryInterface.sequelize.query(
+          'UPDATE "projects" SET "potentielIdentifier" = ? ', 
+          {
+            type: queryInterface.sequelize.UPDATE,
+            replacements: potentielIdentifier,
+            transaction,
+          }
+        )
+      }
+
+      console.log(`Updated potentielIdentifierfor ${projects.length} projects`)
+      await transaction.commit()
+
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
