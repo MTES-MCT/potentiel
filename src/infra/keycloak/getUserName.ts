141e60b03e4beb895467f6d3cac1c6d1dfb71f76
Julien Pavon <julien.pavon@gmail.com>
2021-11-17 16:43:10 +0100

    :recycle: Use makeKeycloakClient function

--- src/infra/keycloak/getUserName.ts
+++ src/infra/keycloak/getUserName.ts
@@ -1,12 +1,14 @@
+import { makeKeycloakClient } from './keycloakClient'
 import { logger, ResultAsync } from '../../core/utils'
 import { OtherError } from '../../modules/shared'
 import { GetUserName } from '../../modules/users'
-import { keycloakAdminClient } from './keycloakClient'
 
 const { KEYCLOAK_ADMIN_CLIENT_ID, KEYCLOAK_ADMIN_CLIENT_SECRET, KEYCLOAK_REALM } = process.env
 
 export const getUserName: GetUserName = (id) => {
   async function getKeycloakUsername(): Promise<string> {
+    const keycloakAdminClient = makeKeycloakClient()
+
     await keycloakAdminClient.auth({
       grantType: 'client_credentials',
       clientId: KEYCLOAK_ADMIN_CLIENT_ID!,
