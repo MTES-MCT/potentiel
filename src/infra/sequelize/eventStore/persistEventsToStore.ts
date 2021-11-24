48cd2f89213a5cf0869aacd7345d850c64e46535
Pierre Duchateau <pad@dabla.com>
2021-11-24 16:18:54 +0100

    :recycle:  create sequelize.persistEventsToStore

---
+++ src/infra/sequelize/eventStore/persistEventsToStore.ts
@@ -0,0 +1,8 @@
+import { MakeEventStoreDeps, okAsync, wrapInfra } from '../../../core/utils'
+import { toPersistance } from '../helpers'
+import models from '../models'
+const { EventStore } = models
+
+export const persistEventsToStore: MakeEventStoreDeps['persistEventsToStore'] = (events) => {
+  return wrapInfra(EventStore.bulkCreate(events.map(toPersistance)))
+}
