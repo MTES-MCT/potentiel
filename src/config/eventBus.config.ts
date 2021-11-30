b81d8de6c856b675b324ee06768dc9d4eb6f2291
Julien Pavon <julien.pavon@gmail.com>
2021-11-30 15:15:24 +0100

    :white_check_mark: Update eventbus config to use in memory only for integration tests

--- src/config/eventBus.config.ts
+++ src/config/eventBus.config.ts
@@ -2,20 +2,31 @@ import { makeDualEventBus } from '../infra/dualEventBus'
 import { makeInMemoryEventBus } from '../infra/inMemoryEventBus'
 import { makeRedisEventBus } from '../infra/redis'
 import Redis from 'ioredis'
+import { isTestEnv } from './env.config'
+import { EventBus } from '../core/domain'
 
-const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_EVENT_BUS_STREAM_NAME } = process.env
+let eventBus: EventBus
 
-if (!REDIS_PORT || !REDIS_HOST || !REDIS_EVENT_BUS_STREAM_NAME || !REDIS_PASSWORD) {
-  console.error('Missing REDIS env variables. Aborting.')
-  process.exit(1)
+if (isTestEnv) {
+  eventBus = makeInMemoryEventBus()
+  console.log(`EventBus will be using in-memory for the eventbus`)
+} else {
+  const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_EVENT_BUS_STREAM_NAME } = process.env
+
+  if (!REDIS_PORT || !REDIS_HOST || !REDIS_EVENT_BUS_STREAM_NAME || !REDIS_PASSWORD) {
+    console.error('Missing REDIS env variables. Aborting.')
+    process.exit(1)
+  }
+
+  const port = Number(REDIS_PORT)
+  const redis = new Redis({ port, host: REDIS_HOST, password: REDIS_PASSWORD })
+
+  console.log(`EventBus will be using both in-memory and redis for the eventbus`)
+
+  eventBus = makeDualEventBus({
+    inMemoryEventBus: makeInMemoryEventBus(),
+    redisEventBus: makeRedisEventBus({ redis, streamName: REDIS_EVENT_BUS_STREAM_NAME }),
+  })
 }
 
-const port = Number(REDIS_PORT)
-const redis = new Redis({ port, host: REDIS_HOST, password: REDIS_PASSWORD })
-
-console.log(`EventBus will be using both in-memory and redis for the eventbus`)
-
-export const eventBus = makeDualEventBus({
-  inMemoryEventBus: makeInMemoryEventBus(),
-  redisEventBus: makeRedisEventBus({ redis, streamName: REDIS_EVENT_BUS_STREAM_NAME }),
-})
+export { eventBus }
