fa902b942f99124b5caa82e682f4042c2f7658fc
Pierre Duchateau <pad@dabla.com>
2021-11-30 16:48:27 +0100

    :star2:  Add frise GetProjectEvents query interface

---
+++ src/modules/frise/queries/GetProjectEvents.ts
@@ -0,0 +1,9 @@
+import { ProjectEventListDTO } from '..'
+import { ResultAsync } from '../../../core/utils'
+import { User } from '../../../entities'
+import { InfraNotAvailableError, UnauthorizedError } from '../../shared'
+
+export type GetProjectEvents = (args: {
+  projectId: string
+  user: User
+}) => ResultAsync<ProjectEventListDTO, UnauthorizedError | InfraNotAvailableError>
