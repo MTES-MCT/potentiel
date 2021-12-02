6f95ca4e3568003b3dafed9060cd594cabe78efb
Pierre Duchateau <pad@dabla.com>
2021-12-02 14:49:36 +0100

    :wrench:  fix instantiation of eventbus in config

--- src/config/eventBus.config.ts
+++ src/config/eventBus.config.ts
@@ -4,14 +4,15 @@ import { makePublishInRedisEventBus } from '../infra/redis'
 import Redis from 'ioredis'
 import { isTestEnv } from './env.config'
 import { EventEmitter } from 'stream'
+import { EventBus } from '../core/domain'
 
 const eventEmitter = new EventEmitter()
-let publishToEventBus
+let publishToEventBus: EventBus['publish']
 
 const subscribe = makeSubscribeToMemory({ eventEmitter })
 
 if (isTestEnv) {
-  publishToEventBus = () => makePublishInMemory({ eventEmitter })
+  publishToEventBus = makePublishInMemory({ eventEmitter })
   console.log(`EventBus will be using in-memory for the eventbus`)
 } else {
   const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD, REDIS_EVENT_BUS_STREAM_NAME } = process.env
@@ -24,7 +25,7 @@ if (isTestEnv) {
   const redis = new Redis({
     port: Number(REDIS_PORT),
     host: REDIS_HOST,
-    password: 'REDIS_PASSWORD',
+    password: REDIS_PASSWORD,
     showFriendlyErrorStack: true,
     lazyConnect: true,
   })
@@ -35,14 +36,13 @@ if (isTestEnv) {
 
   console.log(`EventBus will be using both in-memory and redis for the eventbus`)
 
-  publishToEventBus = () =>
-    makePublishEvent({
-      publishInRedisEventBus: makePublishInRedisEventBus({
-        redis,
-        streamName: REDIS_EVENT_BUS_STREAM_NAME,
-      }),
-      publishInMemory: makePublishInMemory({ eventEmitter }),
-    })
+  publishToEventBus = makePublishEvent({
+    publishInRedisEventBus: makePublishInRedisEventBus({
+      redis,
+      streamName: REDIS_EVENT_BUS_STREAM_NAME,
+    }),
+    publishInMemory: makePublishInMemory({ eventEmitter }),
+  })
 }
 
 export { publishToEventBus, subscribe }
