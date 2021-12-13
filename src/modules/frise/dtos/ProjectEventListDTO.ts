04da78572e530ab30acda6d579fdeafd8fb611f7
Hanae <hanae.yamashita@lilo.org>
2021-12-13 17:30:44 +0100

    :wrench:  Fix ProjectEventDTO after rebasing ...

--- src/modules/frise/dtos/ProjectEventListDTO.ts
+++ src/modules/frise/dtos/ProjectEventListDTO.ts
@@ -2,16 +2,16 @@ import { UserRole } from '../../users'
 
 export type ProjectEventDTO = ProjectNotifiedDTO | ProjectImportedDTO
 
-export type ProjectNotifiedDTO =
-  | {
-      type: 'ProjectNotified'
-      variant: Exclude<UserRole, 'ademe'>
-      date: number
-    }
-  | {
-      type: 'ProjectImported'
-      variant: 'dgec' | 'admin'
-      date: number
-    }
+export type ProjectNotifiedDTO = {
+  type: 'ProjectNotified'
+  variant: Exclude<UserRole, 'ademe'>
+  date: number
+}
+
+export type ProjectImportedDTO = {
+  type: 'ProjectImported'
+  variant: 'dgec' | 'admin'
+  date: number
+}
 
 export type ProjectEventListDTO = { events: ProjectEventDTO[] }
