8309c4de99683bc83f84b6af14fa00e22ebe9b6f
Pierre Duchateau <pad@dabla.com>
2021-12-13 19:46:26 +0100

    :recycle:  Remove useless props of BaseDomainEvent (version, requestId)

--- src/core/domain/DomainEvent.ts
+++ src/core/domain/DomainEvent.ts
@@ -4,7 +4,6 @@ export interface DomainEvent {
   id: string
   occurredAt: Date
   type: string
-  getVersion: () => number
   aggregateId: string[] | string | undefined
   requestId?: string
   payload: any
@@ -15,7 +14,6 @@ export interface BaseDomainEventProps<P> {
   requestId?: DomainEvent['requestId']
   original?: {
     occurredAt: DomainEvent['occurredAt']
-    version: ReturnType<DomainEvent['getVersion']>
     eventId?: string
   }
 }
@@ -25,25 +23,13 @@ export abstract class BaseDomainEvent<P> {
   public readonly requestId: DomainEvent['requestId']
   public readonly aggregateId: DomainEvent['aggregateId']
   public readonly occurredAt: DomainEvent['occurredAt']
-  private originalVersion: ReturnType<DomainEvent['getVersion']>
 
-  abstract readonly currentVersion: number
-
-  constructor({ payload, requestId, original }: BaseDomainEventProps<P>) {
+  constructor({ payload }: BaseDomainEventProps<P>) {
     this.payload = payload
-    this.occurredAt = original?.occurredAt || new Date()
-    this.id = original?.eventId || new UniqueEntityID().toString()
-    this.requestId = requestId
+    this.occurredAt = new Date()
+    this.id = new UniqueEntityID().toString()
     this.aggregateId = this.aggregateIdFromPayload(payload)
-
-    if (original?.version) {
-      this.originalVersion = original.version
-    }
   }
 
   abstract aggregateIdFromPayload(payload: P): DomainEvent['aggregateId']
-
-  getVersion() {
-    return this.originalVersion || this.currentVersion
-  }
 }
