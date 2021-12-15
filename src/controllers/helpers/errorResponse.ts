796c719949ef70411d67e1bf680de31a8be83202
Pierre Duchateau <pad@dabla.com>
2021-12-15 11:34:07 +0100

    :wrench:  Add generic controllers/helpers/errorResponse to return ErrorPage

---
+++ src/controllers/helpers/errorResponse.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from 'express'
+import { ErrorPage } from '../../views'
+
+export const errorResponse = (args: {
+  response: Response
+  request: Request
+  customStatus?: number
+  customTitle?: string
+  customMessage?: string
+}) => {
+  const { response, request, customTitle, customMessage, customStatus } = args
+  return response.status(customStatus || 500).send(
+    ErrorPage({
+      request,
+      errorTitle: customTitle || 'Erreur',
+      errorMessage:
+        customMessage ||
+        'Une erreur est survenue. Merci de réessayer ou de contacter un administrateur.',
+    })
+  )
+}
