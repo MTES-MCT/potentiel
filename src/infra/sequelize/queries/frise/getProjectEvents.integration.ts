e4a677d78e9ca988053f6b27e3be9e8f6d84f280
Hanae <hanae.yamashita@lilo.org>
2021-12-16 10:41:09 +0100

    :white_check_mark:  Cover all cases for ProjectImported event

--- src/infra/sequelize/queries/frise/getProjectEvents.integration.ts
+++ src/infra/sequelize/queries/frise/getProjectEvents.integration.ts
@@ -8,51 +8,55 @@ import { getProjectEvents } from './getProjectEvents'
 describe('frise.getProjectEvents', () => {
   const projectId = new UniqueEntityID().toString()
 
-  describe('when the user is dgec', () => {
-    it('should return the ProjectImported event', async () => {
-      const fakeUser = { role: 'dgec' } as User
-      await resetDatabase()
+  for (const role of USER_ROLES.filter((role) => role === 'dgec' || role === 'admin')) {
+    describe(`when the user is ${role}`, () => {
+      it('should return the ProjectImported event', async () => {
+        const fakeUser = { role } as User
+        await resetDatabase()
 
-      await ProjectEvent.create({
-        id: new UniqueEntityID().toString(),
-        projectId,
-        type: 'ProjectImported',
-        valueDate: 1234,
-      })
+        await ProjectEvent.create({
+          id: new UniqueEntityID().toString(),
+          projectId,
+          type: 'ProjectImported',
+          valueDate: 1234,
+        })
 
-      const res = await getProjectEvents({ projectId, user: fakeUser })
+        const res = await getProjectEvents({ projectId, user: fakeUser })
 
-      expect(res._unsafeUnwrap()).toMatchObject({
-        events: [
-          {
-            type: 'ProjectImported',
-            date: 1234,
-            variant: 'dgec',
-          },
-        ],
-      })
-    })
-  })
-
-  describe('when the user is porteur-projet', () => {
-    it('should not return the ProjectImported event', async () => {
-      const fakeUser = { role: 'porteur-projet' } as User
-      await resetDatabase()
-
-      await ProjectEvent.create({
-        id: new UniqueEntityID().toString(),
-        projectId,
-        type: 'ProjectImported',
-        valueDate: 1234,
-      })
-
-      const res = await getProjectEvents({ projectId, user: fakeUser })
-
-      expect(res._unsafeUnwrap()).toMatchObject({
-        events: [],
+        expect(res._unsafeUnwrap()).toMatchObject({
+          events: [
+            {
+              type: 'ProjectImported',
+              date: 1234,
+              variant: role,
+            },
+          ],
+        })
       })
     })
-  })
+  }
+
+  for (const role of USER_ROLES.filter((role) => role !== 'dgec' && role !== 'admin')) {
+    describe(`when the user is ${role}`, () => {
+      it('should NOT return the ProjectImported event', async () => {
+        const fakeUser = { role } as User
+        await resetDatabase()
+
+        await ProjectEvent.create({
+          id: new UniqueEntityID().toString(),
+          projectId,
+          type: 'ProjectImported',
+          valueDate: 1234,
+        })
+
+        const res = await getProjectEvents({ projectId, user: fakeUser })
+
+        expect(res._unsafeUnwrap()).toMatchObject({
+          events: [],
+        })
+      })
+    })
+  }
 
   for (const role of USER_ROLES.filter((role) => role !== 'ademe')) {
     describe(`when the user is ${role}`, () => {
