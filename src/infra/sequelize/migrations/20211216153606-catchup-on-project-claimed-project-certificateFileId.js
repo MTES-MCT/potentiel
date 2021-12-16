99700a258ada4c2caef555bac1d91009130810cd
Pierre Duchateau <pad@dabla.com>
2021-12-16 16:44:36 +0100

    :star2:  catchup on all the past ProjectClaimed events
    projects.certificateFileId was empty

---
+++ src/infra/sequelize/migrations/20211216153606-catchup-on-project-claimed-project-certificateFileId.js
@@ -0,0 +1,55 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      const projectClaimedEvents = await queryInterface.sequelize.query(
+        `SELECT * FROM "eventStores" WHERE type = 'ProjectClaimed'`,
+        {
+          type: queryInterface.sequelize.QueryTypes.SELECT,
+          transaction,
+        }
+      )
+
+      for (const event of projectClaimedEvents) {
+        const { id, payload } = event
+
+        const { projectId, attestationDesignationFileId } = payload
+
+        const project = await queryInterface.sequelize.query(
+          `SELECT "certificateFileId" FROM "projects" WHERE id = ?`,
+          {
+            type: queryInterface.sequelize.QueryTypes.SELECT,
+            replacements: [projectId],
+            transaction,
+          }
+        )
+
+        if (project && !project.certificateFileId) {
+          await queryInterface.sequelize.query(
+            'UPDATE "projects" SET "certificateFileId" = ? WHERE id = ?',
+            {
+              type: queryInterface.sequelize.UPDATE,
+              replacements: [attestationDesignationFileId, projectId],
+              transaction,
+            }
+          )
+        }
+      }
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
