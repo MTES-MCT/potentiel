e6c60c37de32b2832eff65667bddfc0143e644f8
Hanae <hanae.yamashita@lilo.org>
2021-12-22 18:28:46 +0100

    :white_check_mark:  Fix tests

--- src/views/pages/projectDetailsPage/components/timeline/helpers/getLatestCertificateEvent.spec.ts
+++ src/views/pages/projectDetailsPage/components/timeline/helpers/getLatestCertificateEvent.spec.ts
@@ -11,46 +11,29 @@ import { getLatestCertificateEvent } from './getLatestCertificateEvent'
 describe('getLatestCertificateEvent', () => {
   const events1: ProjectEventDTO[] = [
     {
-      type: 'ProjectNotified' as 'ProjectNotified',
-      variant: 'admin' as 'admin',
+      type: 'ProjectNotified',
+      variant: 'admin',
       date: 14,
-    },
+    } as ProjectNotifiedDTO,
     {
       type: 'ProjectCertificateUpdated',
       variant: 'admin',
       date: 10,
-      potentielIdentifier: 'pot-id',
-      certificateFileId: 'file-id',
-      nomProjet: 'nom-projet',
-      email: 'email',
     } as ProjectCertificateUpdatedDTO,
     {
       type: 'ProjectCertificateGenerated',
       variant: 'admin',
       date: 13,
-      potentielIdentifier: 'pot-id',
-      certificateFileId: 'file-id',
-      nomProjet: 'nom-projet',
-      email: 'email',
     } as ProjectCertificateGeneratedDTO,
     {
       type: 'ProjectCertificateRegenerated',
       variant: 'admin',
-      date: 12,
-      potentielIdentifier: 'pot-id',
-      certificateFileId: 'file-id',
-      nomProjet: 'nom-projet',
-      email: 'email',
+      date: 15,
     } as ProjectCertificateRegeneratedDTO,
     {
       type: 'ProjectClaimed',
       variant: 'admin',
       date: 14,
-      potentielIdentifier: 'pot-id',
-      certificateFileId: 'file-id',
-      nomProjet: 'nom-projet',
-      email: 'email',
-      claimedBy: 'someone',
     } as ProjectClaimedDTO,
   ]
 
@@ -62,16 +45,16 @@ describe('getLatestCertificateEvent', () => {
     } as ProjectNotifiedDTO,
   ]
 
-  describe('when "events" contains events of types ProjectCertificateGenerated, ProjectCertificateRegenerated or ProjectCertificateUpdated', () => {
-    it('should return the latest event from events of type ProjectCertificateGenerated, ProjectCertificateRegenerated and ProjectCertificateUpdated', () => {
+  describe('when "events" contains events of types ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
+    it('should return the latest event from events of type ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
       const result1 = getLatestCertificateEvent(events1)
       expect(result1).not.toBeNull
-      expect(result1?.date).toEqual(14)
-      expect(result1?.type).toEqual('ProjectClaimed')
+      expect(result1?.date).toEqual(15)
+      expect(result1?.type).toEqual('ProjectCertificateRegenerated')
     })
   })
 
-  describe('when "events" does not contain events of type ProjectCertificateGenerated, ProjectCertificateRegenerated or ProjectCertificateUpdated', () => {
+  describe('when "events" does not contain events of type ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
     it('should return null', () => {
       const result2 = getLatestCertificateEvent(events2)
       expect(result2).toEqual(undefined)
