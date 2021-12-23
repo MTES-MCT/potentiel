e3502c286f36480ce653183970b4bfe99eaac20a
Julien Pavon <julien.pavon@gmail.com>
2021-12-23 10:42:06 +0100

    :white_check_mark: Test all Certificate events in getLatest function

--- src/views/pages/projectDetailsPage/components/timeline/helpers/getLatestCertificateEvent.spec.ts
+++ src/views/pages/projectDetailsPage/components/timeline/helpers/getLatestCertificateEvent.spec.ts
@@ -1,4 +1,5 @@
 import {
+  ProjectCertificateDTO,
   ProjectCertificateGeneratedDTO,
   ProjectCertificateRegeneratedDTO,
   ProjectCertificateUpdatedDTO,
@@ -9,52 +10,89 @@ import {
 import { getLatestCertificateEvent } from './getLatestCertificateEvent'
 
 describe('getLatestCertificateEvent', () => {
-  const events1: ProjectEventDTO[] = [
-    {
-      type: 'ProjectNotified',
-      variant: 'admin',
-      date: 14,
-    } as ProjectNotifiedDTO,
-    {
-      type: 'ProjectCertificateUpdated',
-      variant: 'admin',
-      date: 10,
-    } as ProjectCertificateUpdatedDTO,
-    {
-      type: 'ProjectCertificateGenerated',
-      variant: 'admin',
-      date: 13,
-    } as ProjectCertificateGeneratedDTO,
-    {
-      type: 'ProjectCertificateRegenerated',
-      variant: 'admin',
-      date: 15,
-    } as ProjectCertificateRegeneratedDTO,
-    {
-      type: 'ProjectClaimed',
-      variant: 'admin',
-      date: 14,
-    } as ProjectClaimedDTO,
-  ]
+  describe('when "events" contains events of many different types', () => {
+    const events1: ProjectEventDTO[] = [
+      {
+        type: 'ProjectNotified',
+        variant: 'admin',
+        date: 16,
+      } as ProjectNotifiedDTO,
+      {
+        type: 'ProjectCertificateUpdated',
+        variant: 'admin',
+        date: 10,
+      } as ProjectCertificateUpdatedDTO,
+      {
+        type: 'ProjectCertificateGenerated',
+        variant: 'admin',
+        date: 13,
+      } as ProjectCertificateGeneratedDTO,
+      {
+        type: 'ProjectCertificateRegenerated',
+        variant: 'admin',
+        date: 15,
+      } as ProjectCertificateRegeneratedDTO,
+      {
+        type: 'ProjectClaimed',
+        variant: 'admin',
+        date: 14,
+      } as ProjectClaimedDTO,
+    ]
 
-  const events2 = [
-    {
-      type: 'ProjectNotified',
-      variant: 'admin',
-      date: 14,
-    } as ProjectNotifiedDTO,
-  ]
-
-  describe('when "events" contains events of types ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
-    it('should return the latest event from events of type ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
+    it('should return the latest event of type ProjectCertificateRegenerated', () => {
       const result1 = getLatestCertificateEvent(events1)
       expect(result1).not.toBeNull
       expect(result1?.date).toEqual(15)
       expect(result1?.type).toEqual('ProjectCertificateRegenerated')
     })
+
+    describe('when "events" contains 2 events with same date', () => {
+      const fixtures: ProjectCertificateDTO[] = [
+        {
+          type: 'ProjectCertificateUpdated',
+          variant: 'admin',
+        } as ProjectCertificateUpdatedDTO,
+        {
+          type: 'ProjectCertificateGenerated',
+          variant: 'admin',
+        } as ProjectCertificateGeneratedDTO,
+        {
+          type: 'ProjectCertificateRegenerated',
+          variant: 'admin',
+        } as ProjectCertificateRegeneratedDTO,
+        {
+          type: 'ProjectClaimed',
+          variant: 'admin',
+        } as ProjectClaimedDTO,
+      ]
+
+      for (const certificateEvent of fixtures) {
+        it(`should return the latest event of type ${certificateEvent.type}`, () => {
+          const result1 = getLatestCertificateEvent([
+            {
+              type: 'ProjectNotified',
+              variant: 'admin',
+              date: 14,
+            } as ProjectNotifiedDTO,
+            { ...certificateEvent, date: 14 },
+          ])
+          expect(result1).not.toBeNull
+          expect(result1?.date).toEqual(14)
+          expect(result1?.type).toEqual(certificateEvent.type)
+        })
+      }
+    })
   })
 
   describe('when "events" does not contain events of type ProjectCertificateGenerated, ProjectCertificateRegenerated, ProjectCertificateUpdated or ProjectClaimed', () => {
+    const events2 = [
+      {
+        type: 'ProjectNotified',
+        variant: 'admin',
+        date: 14,
+      } as ProjectNotifiedDTO,
+    ]
+
     it('should return null', () => {
       const result2 = getLatestCertificateEvent(events2)
       expect(result2).toEqual(undefined)
