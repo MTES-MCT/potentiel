67751baabab65a81a655c7a55640f8df1671302d
Hanae <hanae.yamashita@lilo.org>
2021-12-23 12:27:28 +0100

    :wrench:  Add filename for GF event if getProjectEvents query

--- src/infra/sequelize/queries/frise/getProjectEvents.ts
+++ src/infra/sequelize/queries/frise/getProjectEvents.ts
@@ -10,75 +10,82 @@ export const getProjectEvents: GetProjectEvents = ({ projectId, user }) => {
     .andThen((rawProject: any) =>
       getEvents(projectId).map((rawEvents) => ({ rawProject, rawEvents }))
     )
-    .map(({ rawProject, rawEvents }) => {
+    .map(async ({ rawProject, rawEvents }) => {
       const { email, nomProjet, potentielIdentifier } = rawProject.get()
 
       return {
-        events: rawEvents
+        events: await rawEvents
           .map((item) => item.get())
-          .reduce<ProjectEventDTO[]>((events, { type, valueDate, payload }) => {
-            switch (type) {
-              case 'ProjectImported':
-                if (user.role === 'dgec' || user.role === 'admin') {
-                  events.push({
-                    type,
-                    date: valueDate,
-                    variant: user.role,
-                  })
-                }
-                break
-              case 'ProjectNotified':
-                if (user.role !== 'ademe') {
-                  events.push({
-                    type,
-                    date: valueDate,
-                    variant: user.role,
-                  })
-                }
-                break
-              case 'ProjectCertificateGenerated':
-              case 'ProjectCertificateRegenerated':
-              case 'ProjectCertificateUpdated':
-              case 'ProjectClaimed':
-                if (user.role !== 'ademe' && user.role !== 'dreal') {
-                  events.push({
-                    type,
-                    potentielIdentifier,
-                    email: ['admin', 'dgec'].includes(user.role) ? email : undefined,
-                    nomProjet,
-                    date: valueDate,
-                    variant: user.role,
-                    certificateFileId:
-                      type === 'ProjectClaimed'
-                        ? payload.attestationDesignationFileId
-                        : payload.certificateFileId,
-                    ...(type === 'ProjectClaimed' && { claimedBy: payload.claimedBy }),
-                  })
-                }
-                break
-              case 'ProjectGFSubmitted':
-                if (
-                  user.role === 'porteur-projet' ||
-                  user.role === 'admin' ||
-                  user.role === 'dgec' ||
-                  user.role === 'dreal'
-                ) {
-                  const fileId = payload.fileId
-                  const file = File.findOne({ attributes: ['filename'], where: { id: fileId } })
-                  const filename = file.filename
-                  events.push({
-                    type,
-                    date: valueDate,
-                    variant: user.role,
-                    fileId,
-                    submittedBy: payload.submittedBy,
-                    filename,
-                  })
-                }
-            }
+          .reduce<Promise<ProjectEventDTO[]>>(
+            async (eventsPromise, { type, valueDate, payload }) => {
+              const events: ProjectEventDTO[] = await eventsPromise
+              switch (type) {
+                case 'ProjectImported':
+                  if (user.role === 'dgec' || user.role === 'admin') {
+                    events.push({
+                      type,
+                      date: valueDate,
+                      variant: user.role,
+                    })
+                  }
+                  break
+                case 'ProjectNotified':
+                  if (user.role !== 'ademe') {
+                    events.push({
+                      type,
+                      date: valueDate,
+                      variant: user.role,
+                    })
+                  }
+                  break
+                case 'ProjectCertificateGenerated':
+                case 'ProjectCertificateRegenerated':
+                case 'ProjectCertificateUpdated':
+                case 'ProjectClaimed':
+                  if (user.role !== 'ademe' && user.role !== 'dreal') {
+                    events.push({
+                      type,
+                      potentielIdentifier,
+                      email: ['admin', 'dgec'].includes(user.role) ? email : undefined,
+                      nomProjet,
+                      date: valueDate,
+                      variant: user.role,
+                      certificateFileId:
+                        type === 'ProjectClaimed'
+                          ? payload.attestationDesignationFileId
+                          : payload.certificateFileId,
+                      ...(type === 'ProjectClaimed' && { claimedBy: payload.claimedBy }),
+                    })
+                  }
+                  break
+                case 'ProjectGFSubmitted':
+                  if (
+                    user.role === 'porteur-projet' ||
+                    user.role === 'admin' ||
+                    user.role === 'dgec' ||
+                    user.role === 'dreal'
+                  ) {
+                    const fileId = payload.fileId
+                    const rawFilename = await File.findOne({
+                      attributes: ['filename'],
+                      where: { id: fileId },
+                    })
+                    const filename = rawFilename.filename
+                    events.push({
+                      type,
+                      date: valueDate,
+                      variant: user.role,
+                      fileId,
+                      submittedBy: payload.submittedBy,
+                      filename,
+                    })
+                  }
+              }
 
-            return events
-          }, [] as ProjectEventDTO[]),
+              return Promise.resolve(events)
+            },
+            Promise.resolve([] as ProjectEventDTO[])
+          ),
       }
     })
 }
