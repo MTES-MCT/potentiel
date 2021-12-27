fa67a19f0fc79a5a57ab46c226f22d9802217ed2
Hanae <hanae.yamashita@lilo.org>
2021-12-27 19:03:13 +0100

    :star2:  Manage GFDueDateSet and GFSubmitted events to show rele…

--- src/views/pages/projectDetailsPage/components/timeline/GarantiesFinancieresItem.tsx
+++ src/views/pages/projectDetailsPage/components/timeline/GarantiesFinancieresItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   ProjectEventDTO,
+  ProjectGFDueDateSetDTO,
   ProjectGFSubmittedDTO,
 } from '../../../../../modules/frise/dtos/ProjectEventListDTO'
 import { TimelineItem, ItemTitle, ItemDate, ContentArea, PassedIcon } from './components'
@@ -8,11 +9,12 @@ import { GFDocumentLinkItem } from './GFDocumentLinkItem'
 
 export const GarantieFinanciereItem = (props: {
   isLastItem: boolean
-  events: ProjectGFSubmittedDTO[]
+  events: (ProjectGFSubmittedDTO | ProjectGFDueDateSetDTO)[]
   groupIndex: number
   date: number
 }) => {
   const { isLastItem, events, groupIndex, date } = props
+  const projectGFDueDateSet = events.find(isProjectGFDueDateSet)
   const projectGFSubmittedEvent = events.find(isProjectGFSubmitted)
 
   return (
@@ -21,6 +23,7 @@ export const GarantieFinanciereItem = (props: {
       <ContentArea>
         <ItemDate date={date} />
         <ItemTitle title="Constitution des garanties Financières" />
+        {projectGFDueDateSet && <p className="mt-0 mb-0">Garanties financières en attente</p>}
         {projectGFSubmittedEvent && <GFDocumentLinkItem event={projectGFSubmittedEvent} />}
       </ContentArea>
     </TimelineItem>
@@ -29,3 +32,6 @@ export const GarantieFinanciereItem = (props: {
 
 const isProjectGFSubmitted = (event: ProjectEventDTO): event is ProjectGFSubmittedDTO =>
   event.type === 'ProjectGFSubmitted'
+
+const isProjectGFDueDateSet = (event: ProjectEventDTO): event is ProjectGFDueDateSetDTO =>
+  event.type === 'ProjectGFDueDateSet'
