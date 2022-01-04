dbd2d32fe0ca88677578f41c8a9f14f500f25ce5
HanaeY <hanae.yamashita@lilo.org>
2022-01-04 09:41:45 +0100

    :wrench:  Fix wording mistake

--- src/views/components/DateInput.tsx
+++ src/views/components/DateInput.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DateInput = ({ setErrorMessage, setDisableSubmit, maxDate }) => {
   const dateRegex = new RegExp(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/)
   const errorFormat =
-    'La date saisie doit être de la forme JJ/MM/AAAA, soit par exemple 25/04/2021 pour le 25 mai 2021.'
+    'La date saisie doit être de la forme JJ/MM/AAAA, soit par exemple 25/04/2021 pour le 25 avril 2021.'
   const errorMaxDate = "La date saisie doit être antérieure à la date d'aujourd'hui."
 
   const handleDateInput = (e: any) => {
