94ade09d3ac83f3525086d8dd9ca2ecf1219a2fc
Julien Pavon <julien.pavon@gmail.com>
2022-01-04 16:40:56 +0100

    :sparkles: Set eventPublishedAt for ProjectCertificateGenerated events

--- src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectCertificateGenerated.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectCertificateGenerated.ts
@@ -10,6 +10,7 @@ export default ProjectEvent.projector.on(
         projectId,
         type: ProjectCertificateGenerated.type,
         valueDate: occurredAt.getTime(),
+        eventPublishedAt: occurredAt.getTime(),
       },
       defaults: {
         id: new UniqueEntityID().toString(),
