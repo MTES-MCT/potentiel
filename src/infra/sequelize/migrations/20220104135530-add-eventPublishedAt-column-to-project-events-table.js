f6b21adb1b69ec00231435540aec160daa1c0e8c
Julien Pavon <julien.pavon@gmail.com>
2022-01-07 15:33:08 +0100

    :card_file_box: Remove useless scripts and use rebuild function when adding new column

--- src/infra/sequelize/migrations/20220104135530-add-eventPublishedAt-column-to-project-events-table.js
+++ src/infra/sequelize/migrations/20220104135530-add-eventPublishedAt-column-to-project-events-table.js
@@ -4,37 +4,25 @@ const { ProjectEvent } = require('../projectionsNext')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const tableDefinition = await queryInterface.describeTable('project_events')
+    const transaction = await queryInterface.sequelize.transaction()
 
-    if (!tableDefinition.eventPublishedAt) {
-      await queryInterface.addColumn('project_events', 'eventPublishedAt', {
+    try {
+      const tableDefinition = await queryInterface.describeTable('project_events')
+
+      if (!tableDefinition.eventPublishedAt) {
+        await queryInterface.addColumn('project_events', 'eventPublishedAt', {
+          type: Sequelize.DataTypes.BIGINT,
+          transaction,
+        })
+      }
+
+      await ProjectEvent.projector.rebuild(transaction)
+
+      await queryInterface.changeColumn('project_events', 'eventPublishedAt', {
         type: Sequelize.DataTypes.BIGINT,
         allowNull: false,
         default: 0,
       })
-    }
-
-    const transaction = await queryInterface.sequelize.transaction()
-    try {
-      await queryInterface.sequelize.query(`TRUNCATE TABLE "project_events"`, {
-        type: queryInterface.sequelize.QueryTypes.DELETE,
-      })
-
-      const events = ProjectEvent.projector.getListenedEvents()
-
-      if (events.length > 0) {
-        const projectEvents = await queryInterface.sequelize.query(
-          `SELECT * FROM "eventStores" 
-           WHERE type in (${events.map((event) => `'${event}'`).join(',')}) 
-           ORDER BY "occurredAt" ASC`,
-          {
-            type: queryInterface.sequelize.QueryTypes.SELECT,
-            transaction,
-          }
-        )
-
-        await Promise.all(projectEvents.map((event) => ProjectEvent.projector.handleEvent(event)))
-      }
 
       await transaction.commit()
     } catch (error) {
