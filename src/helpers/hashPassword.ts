2b9e3f79cf1d38e542856fe3bfc139f37f78dc62
Pierre Duchateau <pad@dabla.com>
2022-01-13 19:41:28 +0100

    :wrench:  remove deprecated crypto lib

--- src/helpers/hashPassword.ts
+++
@@ -1,6 +0,0 @@
-import crypto from 'crypto'
-
-// TODO: this is too weak, needs to be changed for production
-export default function (str: string): string {
-  return crypto.createHash('md5').update(str).digest('hex')
-}
