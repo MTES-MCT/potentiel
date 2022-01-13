3f69771c3f8c177e0e270e07fb4dc58cc44e8b34
Julien Pavon <julien.pavon@gmail.com>
2022-01-13 09:49:50 +0100

    :sparkles: Add ProjectEvents event handler for ProjectDCRDueDateSet event

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectDCRDueDateSet.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityID } from '../../../../../core/domain'
+import { ProjectDCRDueDateSet } from '../../../../../modules/project'
+import { ProjectEvent } from '../projectEvent.model'
+
+export default ProjectEvent.projector.on(
+  ProjectDCRDueDateSet,
+  async ({ payload: { projectId, dcrDueOn }, occurredAt }, transaction) => {
+    await ProjectEvent.create(
+      {
+        projectId,
+        type: ProjectDCRDueDateSet.type,
+        eventPublishedAt: occurredAt.getTime(),
+        valueDate: dcrDueOn,
+        id: new UniqueEntityID().toString(),
+      },
+      { transaction }
+    )
+  }
+)
