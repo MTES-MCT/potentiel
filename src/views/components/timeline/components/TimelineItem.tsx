9ef4ea3a584d8a11565e70fe6a4d3389ce6f999e
Julien Pavon <julien.pavon@gmail.com>
2022-01-13 14:02:43 +0100

    :lipstick: Update style of timeline icons

--- src/views/components/timeline/components/TimelineItem.tsx
+++ src/views/components/timeline/components/TimelineItem.tsx
@@ -9,7 +9,7 @@ export const TimelineItem = ({ children, isLastItem }: TimelineItemProps) => (
   <li className={classNames(isLastItem ? '' : 'pb-6', 'relative')}>
     {isLastItem ? null : (
       <div
-        className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-green-700"
+        className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
         aria-hidden="true"
       />
     )}
