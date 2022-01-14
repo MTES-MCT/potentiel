0f77b216a4d618f6bcb72c09b0ed75b516e0a465
Julien Pavon <julien.pavon@gmail.com>
2022-01-14 15:01:16 +0100

    :sparkles: Add ProjectEvents event handler for ProjectPTFRemoved event

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectPTFRemoved.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityID } from '../../../../../core/domain'
+import { ProjectPTFRemoved } from '../../../../../modules/project'
+import { ProjectEvent } from '../projectEvent.model'
+
+export default ProjectEvent.projector.on(
+  ProjectPTFRemoved,
+  async ({ payload: { projectId }, occurredAt }, transaction) => {
+    await ProjectEvent.create(
+      {
+        projectId,
+        type: ProjectPTFRemoved.type,
+        valueDate: occurredAt.getTime(),
+        eventPublishedAt: occurredAt.getTime(),
+        id: new UniqueEntityID().toString(),
+      },
+      { transaction }
+    )
+  }
+)
