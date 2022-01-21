04a45fe6ba1d87bc052728599a117285a4c4845f
Julien Pavon <julien.pavon@gmail.com>
2022-01-21 11:03:24 +0100

    :hammer: Fix storybook configuration with tsconfig paths plugin (#316)
    * :hammer: Fix storybook configuration with tsconfig paths plugin
    
    * :construction_worker: Build storybook on PRs
    
    * :construction_worker: Cache npm dependencies
    
    * :construction_worker: Ignore tests files to build storybook on PR

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,18 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: ['../src/views/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        plugins: [new TsconfigPathsPlugin()],
+      },
+    }
   },
 }
