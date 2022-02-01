f8cb192996b0996d011f0703817cfc8bd6cc2a97
Julien Pavon <julien.pavon@gmail.com>
2022-02-01 15:58:50 +0100

    :white_check_mark: Use a fake AO Eolien PPE2 for the laureat story

--- src/views/certificates/ppe2/ppe2.laureat.stories.tsx
+++ src/views/certificates/ppe2/ppe2.laureat.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Font, PDFViewer } from '@react-pdf/renderer'
-import { ProjectAppelOffre } from '@entities'
+import { Famille, Periode, ProjectAppelOffre } from '@entities'
 import { ProjectDataForCertificate } from '@modules/project'
 import { makeLaureat } from './components/Laureat'
 import { Certificate } from './Certificate'
-import { eolienPPE2 } from '@dataAccess/inMemory/appelsOffres'
+import { makeParagrapheAchevementForDelai } from '@dataAccess/inMemory/appelsOffres'
 
 export default { title: 'Attestations PDF' }
 
@@ -25,12 +25,40 @@ Font.register({
   ],
 })
 
+const fakeAoEolienPPE2 = {
+  id: 'PPE2 - Eolien',
+  title:
+    '2021/S 146-386083 portant sur la réalisation et l’exploitation d’Installations de production d’électricité à partir de l’énergie mécanique du vent implantées à terre',
+  shortTitle: 'PPE2 - Eolien 2021/S 146-386083',
+  dossierSuiviPar: 'violaine.tarizzo@developpement-durable.gouv.fr',
+  launchDate: 'Août 2021',
+  unitePuissance: 'MW',
+  tarifOuPrimeRetenue: 'le prix de référence T de l’électricité retenu',
+  tarifOuPrimeRetenueAlt: 'ce prix de référence',
+  paragraphePrixReference: '7',
+  affichageParagrapheECS: true,
+  paragrapheEngagementIPFP: '3.3.7, 4.3 et 6.5.2',
+  renvoiEngagementIPFP: '3.3.7',
+  renvoiDemandeCompleteRaccordement: '6.1',
+  renvoiRetraitDesignationGarantieFinancieres: '5.1',
+  soumisAuxGarantiesFinancieres: true,
+  paragrapheDelaiDerogatoire: '6.3',
+  delaiRealisationEnMois: 36,
+  contenuParagrapheAchevement: makeParagrapheAchevementForDelai(36, '7.1'),
+  delaiRealisationTexte: 'trente-six (36) mois',
+  paragrapheAttestationConformite: '6.5',
+  afficherParagrapheInstallationMiseEnServiceModification: true,
+  renvoiModification: '5.2',
+  paragrapheClauseCompetitivite: '2.11',
+  afficherPhraseRegionImplantation: false,
+  afficherValeurEvaluationCarbone: true,
+  periodes: [] as Periode[],
+  familles: [] as Famille[],
+  periode: { id: 'periodeId', title: 'periodeTitle' } as Periode,
+} as ProjectAppelOffre
+
 const fakeProject: ProjectDataForCertificate = {
-  appelOffre: {
-    ...eolienPPE2,
-    affichageParagrapheECS: true,
-    periode: { id: 'periodeId', title: 'periodeTitle' },
-  } as ProjectAppelOffre,
+  appelOffre: fakeAoEolienPPE2,
   isClasse: true,
   familleId: 'famille',
   prixReference: 42,
