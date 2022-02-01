54310479598c64319e8f07aa814f50ee11666600
Julien Pavon <julien.pavon@gmail.com>
2022-02-01 18:08:20 +0100

    :sparkles: Add new field notethresholdByCategroy in Periode type

--- src/entities/periode.ts
+++ src/entities/periode.ts
@@ -6,12 +6,18 @@ type NoteThreshold = {
   territoire?: Territoire
 }
 
+type NoteThresholdByCategory = {
+  volumesReserves: number
+  autres: number
+}
+
 export type CertificateTemplate = 'v0' | 'v1' | 'ppe2.v0'
 
 export type Periode = {
   id: string
   title: string
   noteThresholdByFamily?: NoteThreshold[]
+  noteThresholdByCategory?: NoteThresholdByCategory
   paragrapheAchevement: string
 } & (
   | {
