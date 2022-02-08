e67d8c81532e10059b51454f8af483d07a98f6c5
HanaeY <hanae.yamashita@lilo.org>
2022-02-08 18:32:12 +0100

    :wrench:  Save ProjecrGFUploaded event in the ProjectEvent t…

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectGFUploaded.ts
@@ -0,0 +1,39 @@
+import { UniqueEntityID } from '@core/domain'
+import { ProjectGFUploaded } from '@modules/project'
+import { ProjectEvent } from '../projectEvent.model'
+import models from '../../../models'
+import { logger } from '@core/utils'
+
+export default ProjectEvent.projector.on(
+  ProjectGFUploaded,
+  async ({ payload: { projectId, fileId, gfDate }, occurredAt }, transaction) => {
+    const { File } = models
+    const rawFilename = await File.findOne({
+      attributes: ['filename'],
+      where: { id: fileId },
+    })
+
+    if (!rawFilename) {
+      logger.error(
+        new Error(
+          `Impossible de trouver le fichier (id = ${fileId}) d'attestation GF pour le project ${projectId})`
+        )
+      )
+    }
+
+    const filename: string | undefined = rawFilename?.filename
+    const file = filename && { id: fileId, name: filename }
+
+    await ProjectEvent.create(
+      {
+        projectId,
+        type: ProjectGFUploaded.type,
+        valueDate: gfDate.getTime(),
+        eventPublishedAt: occurredAt.getTime(),
+        id: new UniqueEntityID().toString(),
+        payload: { file },
+      },
+      { transaction }
+    )
+  }
+)
