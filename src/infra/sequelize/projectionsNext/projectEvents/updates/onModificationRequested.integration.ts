6782c4478f333e5ad2ffed8edb7172da16b2516e
HanaeY <hanae.yamashita@lilo.org>
2022-02-10 17:20:35 +0100

    :wrench: ProjectEvent proj for ModificationRequested event of type delai

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onModificationRequested.integration.ts
@@ -0,0 +1,41 @@
+import { resetDatabase } from '@dataAccess'
+import { UniqueEntityID } from '@core/domain'
+import { ModificationRequested, ModificationRequestedPayload } from '@modules/modificationRequest'
+import { ProjectEvent } from '..'
+import onModificationRequested from './onModificationRequested'
+
+describe('onModificationRequested', () => {
+  const projectId = new UniqueEntityID().toString()
+  const modificationRequestId = new UniqueEntityID().toString()
+  beforeEach(async () => {
+    await resetDatabase()
+  })
+  describe('when modification type is "delay"', () => {
+    it('should create a new project event of type "DelayRequested"', async () => {
+      await onModificationRequested(
+        new ModificationRequested({
+          payload: {
+            type: 'delai',
+            modificationRequestId,
+            projectId,
+            requestedBy: 'user-id',
+            authority: 'dgec',
+            fileId: 'file-id',
+            justification: 'en retard',
+            delayInMonths: 10,
+          } as ModificationRequestedPayload,
+          original: {
+            version: 1,
+            occurredAt: new Date('2022-02-09'),
+          },
+        })
+      )
+      const projectEvent = await ProjectEvent.findOne({ where: { projectId } })
+      expect(projectEvent).toMatchObject({
+        type: 'ModificationRequested',
+        projectId,
+        payload: { modificationType: 'delai', modificationRequestId, delayInMonths: 10 },
+      })
+    })
+  })
+})
