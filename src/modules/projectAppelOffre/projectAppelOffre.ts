5d0d34c69873dc429201891a3536aa164b3bdd28
Julien Pavon <julien.pavon@gmail.com>
2022-02-10 18:14:44 +0100

    :fire: Remove useless file

--- src/modules/projectAppelOffre/projectAppelOffre.ts
+++
@@ -1,7 +0,0 @@
-import { Periode, Famille, AppelOffre } from '@entities'
-
-// export type ProjectAppelOffre = AppelOffre & {
-//   periode: Periode
-//   famille: Famille | undefined
-//   isSoumisAuxGFs: boolean
-// }
