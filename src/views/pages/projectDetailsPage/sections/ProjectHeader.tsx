dcd444a1e5746d4a61a2630b9bf7b995af4de2d2
Julien Pavon <julien.pavon@gmail.com>
2022-02-21 14:21:22 +0100

    :bug: Add the link about the rules of the instruction on the project

--- src/views/pages/projectDetailsPage/sections/ProjectHeader.tsx
+++ src/views/pages/projectDetailsPage/sections/ProjectHeader.tsx
@@ -3,6 +3,7 @@ import { ProjectDataForProjectPage } from '@modules/project/dtos'
 import React from 'react'
 import { ProjectStatusLabel } from '../../../components/ProjectStatusLabel'
 import NewProjectActions from '../../projectDetailsPage/components/ProjectActions'
+import { ExternalLinkIcon } from '@heroicons/react/solid'
 
 type ProjectHeaderProps = {
   project: ProjectDataForProjectPage
@@ -10,7 +11,7 @@ type ProjectHeaderProps = {
   cahiersChargesURLs?: { oldCahierChargesURL?: string; newCahierChargesURL?: string }
 }
 
-export const ProjectHeader = ({ project, user }: ProjectHeaderProps) => (
+export const ProjectHeader = ({ project, user, cahiersChargesURLs }: ProjectHeaderProps) => (
   <div className="w-full pt-3 md:pt-0 lg:flex justify-between gap-2">
     <div className="pl-3 mb-3">
       <div className="flex justify-start items-center">
@@ -31,10 +32,29 @@ export const ProjectHeader = ({ project, user }: ProjectHeaderProps) => (
         {project.communeProjet}, {project.departementProjet}, {project.regionProjet}
       </p>
       <div className="text-sm">{project.potentielIdentifier}</div>
-      <div className="text-sm">
-        Instruction des demandes selon les règles{' '}
-        {project.newRulesOptIn ? `modifiées par l'avis modificatif de juillet 2021` : 'initiales'}
-      </div>
+
+      {user.role !== 'porteur-projet' && (
+        <div className="text-sm">
+          Instruction des demandes selon les règles du{' '}
+          {project.newRulesOptIn ? (
+            cahiersChargesURLs?.newCahierChargesURL ? (
+              <a target="_blank" href={cahiersChargesURLs.newCahierChargesURL}>
+                cahier des charges modifié (option choisie par le candidat){' '}
+                <ExternalLinkIcon className="w-4" />
+              </a>
+            ) : (
+              `cahier des charges modifié (option choisie par le candidat)`
+            )
+          ) : cahiersChargesURLs?.oldCahierChargesURL ? (
+            <a target="_blank" href={cahiersChargesURLs.oldCahierChargesURL}>
+              cahier des charges initial (en vigueur à la candidature){' '}
+              <ExternalLinkIcon className="w-4" />
+            </a>
+          ) : (
+            `cahier des charges initial (en vigueur à la candidature)`
+          )}
+        </div>
+      )}
     </div>
     <NewProjectActions project={project} role={user.role} />
   </div>
