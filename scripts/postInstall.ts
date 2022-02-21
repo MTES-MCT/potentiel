4e4bfb56cb457804a3567f7a575a8e2cf56b592d
Julien Pavon <julien.pavon@gmail.com>
2022-02-21 16:15:59 +0100

    :wrench: 896 Retablir le déploiement en Demo et Dev (#353)
    * :wrench: Execute postInstall from a TS file
    
    Only build and migrate for environment different from local or test
    
    * :hammer: Handle errors in postInstall script
    
    * :hammer: Do not build and migrate if no NODE_ENV value
    
    * :wrench: Run build script for ci.bild github workflow
    
    * :wrench: Add local env as a developement environment

---
+++ scripts/postInstall.ts
@@ -0,0 +1,16 @@
+//if test \"$NODE_ENV\" = \"production\" || test \"$NODE_ENV\" = \"staging\" ; then npm run build:front && npm run build:css && npm run build && npm run migrate; fi
+
+import dotenv from 'dotenv'
+import { spawnSync } from 'child_process'
+
+process.env.NODE_ENV ?? dotenv.config()
+
+const NODE_ENV = process.env.NODE_ENV || ''
+
+if (!['', 'local', 'test'].includes(NODE_ENV)) {
+  const build = spawnSync('npm', ['run', 'build'], { stdio: 'inherit' })
+  build.status && build.status > 0 && process.exit(build.status)
+
+  const migrate = spawnSync('npm', ['run', 'migrate'], { stdio: 'inherit' })
+  process.exit(migrate.status ?? 0)
+}
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-if (!['test', 'development', 'staging', 'production'].includes(process.env.NODE_ENV || '')) {
+if (
+  !['local', 'test', 'development', 'staging', 'production'].includes(process.env.NODE_ENV || '')
+) {
   console.error('ERROR: NODE_ENV not recognized')
   process.exit(1)
 }
 
 export const isTestEnv = process.env.NODE_ENV === 'test'
-export const isDevEnv = process.env.NODE_ENV === 'development'
+export const isDevEnv = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local'
 export const isStagingEnv = process.env.NODE_ENV === 'staging'
 export const isProdEnv = process.env.NODE_ENV === 'production'
 
