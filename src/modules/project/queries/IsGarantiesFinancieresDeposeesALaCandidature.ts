138e5e2a4d9d7d480cbd041f3948ad148bf9d1ef
HanaeY <hanae.yamashita@lilo.org>
2022-02-23 14:06:54 +0100

    remove unused query

--- src/modules/project/queries/IsGarantiesFinancieresDeposeesALaCandidature.ts
+++
@@ -1,6 +0,0 @@
-import { ResultAsync } from '@core/utils'
-import { EntityNotFoundError, InfraNotAvailableError } from '../../shared'
-
-export type IsGarantiesFinancieresDeposeesALaCandidature = (
-  projectId: string
-) => ResultAsync<boolean, EntityNotFoundError | InfraNotAvailableError>
