a4579cef062d11b71db9ba96d0ea807cc3ce7863
HanaeY <72154904+HanaeY@users.noreply.github.com>
2022-02-28 16:08:10 +0100

    :lipstick: split page elements into two cols (#357)
    * :wrench: split page elements into two cols
    
    * :wrench: add top margin to page main element

--- src/views/pages/projectDetailsPage/ProjectDetails.tsx
+++ src/views/pages/projectDetailsPage/ProjectDetails.tsx
@@ -39,161 +39,167 @@ export const ProjectDetails = PageLayout(
       <RoleBasedDashboard role={user.role} currentPage="list-projects">
         <ProjectHeader {...{ project, user, cahiersChargesURLs }} />
         <SuccessErrorBox success={success} error={error} />
-        <div className={'flex flex-row flex-wrap gap-3 justify-start items-stretch mt-5'}>
-          <div className="panel p-4 mt-0 flex-auto">
-            <h3 className="section--title">
-              <CalendarIcon className="w-5 h-5 mr-2" />
-              Étapes du projet
-            </h3>
-            <Timeline
-              {...{
-                projectEventList,
-                now,
-              }}
-            />
-          </div>
-          <Section title="Projet" icon="building">
-            <div>
-              <h5 style={{ marginBottom: 5 }}>Performances</h5>
-              <div>
-                Puissance installée: {project.puissance} {project.appelOffre?.unitePuissance}
-              </div>
+        <main className="flex flex-col gap-3 mt-5">
+          <div className="flex flex-col lg:flex-row gap-3">
+            <div className="panel p-4 mt-0 flex-auto">
+              <h3 className="section--title">
+                <CalendarIcon className="w-5 h-5 mr-2" />
+                Étapes du projet
+              </h3>
+              <Timeline
+                {...{
+                  projectEventList,
+                  now,
+                }}
+              />
             </div>
-            <div>
-              <h5 style={{ marginBottom: 5, marginTop: 10 }}>Site de production</h5>
-              <div>{project.adresseProjet}</div>
-              <div>
-                {project.codePostalProjet} {project.communeProjet}
-              </div>
-              <div>
-                {project.departementProjet}, {project.regionProjet}
-              </div>
-            </div>
-          </Section>
-          <Section title="Contact" icon="user-circle">
-            <div style={{ marginBottom: 10 }}>{project.nomCandidat}</div>
-            <div>
-              <h5 style={{ marginBottom: 5 }}>Représentant légal</h5>
-              <div>{project.nomRepresentantLegal}</div>
-              <div>{project.email}</div>
-            </div>
-            {project.notifiedOn &&
-            ['admin', 'dgec', 'porteur-projet', 'dreal'].includes(user.role) ? (
-              <div>
-                <h5 style={{ marginBottom: 5, marginTop: 15 }}>Comptes ayant accès à ce projet</h5>
-                <ul style={{ marginTop: 5, marginBottom: 5 }}>
-                  {project.users.map(({ id, fullName, email }) => (
-                    <li key={'project_user_' + id}>
-                      {fullName && `${fullName} - `}
-                      {email}
-                      {id !== user.id ? (
-                        <a
-                          href={ROUTES.REVOKE_USER_RIGHTS_TO_PROJECT_ACTION({
-                            projectId: project.id,
-                            userId: id,
-                          })}
-                          style={{ marginLeft: 5 }}
-                          data-confirm={`Etes-vous sur de vouloir retirer les droits à ce projet à ${fullName} ?`}
-                        >
-                          retirer
-                        </a>
+            <div className="flex flex-col gap-3">
+              <Section title="Projet" icon="building">
+                <div>
+                  <h5 style={{ marginBottom: 5 }}>Performances</h5>
+                  <div>
+                    Puissance installée: {project.puissance} {project.appelOffre?.unitePuissance}
+                  </div>
+                </div>
+                <div>
+                  <h5 style={{ marginBottom: 5, marginTop: 10 }}>Site de production</h5>
+                  <div>{project.adresseProjet}</div>
+                  <div>
+                    {project.codePostalProjet} {project.communeProjet}
+                  </div>
+                  <div>
+                    {project.departementProjet}, {project.regionProjet}
+                  </div>
+                </div>
+              </Section>
+              <Section title="Contact" icon="user-circle">
+                <div style={{ marginBottom: 10 }}>{project.nomCandidat}</div>
+                <div>
+                  <h5 style={{ marginBottom: 5 }}>Représentant légal</h5>
+                  <div>{project.nomRepresentantLegal}</div>
+                  <div>{project.email}</div>
+                </div>
+                {project.notifiedOn &&
+                ['admin', 'dgec', 'porteur-projet', 'dreal'].includes(user.role) ? (
+                  <div>
+                    <h5 style={{ marginBottom: 5, marginTop: 15 }}>
+                      Comptes ayant accès à ce projet
+                    </h5>
+                    <ul style={{ marginTop: 5, marginBottom: 5 }}>
+                      {project.users.map(({ id, fullName, email }) => (
+                        <li key={'project_user_' + id}>
+                          {fullName && `${fullName} - `}
+                          {email}
+                          {id !== user.id ? (
+                            <a
+                              href={ROUTES.REVOKE_USER_RIGHTS_TO_PROJECT_ACTION({
+                                projectId: project.id,
+                                userId: id,
+                              })}
+                              style={{ marginLeft: 5 }}
+                              data-confirm={`Etes-vous sur de vouloir retirer les droits à ce projet à ${fullName} ?`}
+                            >
+                              retirer
+                            </a>
+                          ) : (
+                            ''
+                          )}
+                        </li>
+                      ))}
+                      {!project.users.length ? (
+                        <>
+                          <li>Aucun utilisateur n‘a accès à ce projet pour le moment.</li>
+                        </>
                       ) : (
                         ''
                       )}
-                    </li>
-                  ))}
-                  {!project.users.length ? (
-                    <>
-                      <li>Aucun utilisateur n‘a accès à ce projet pour le moment.</li>
-                    </>
-                  ) : (
-                    ''
-                  )}
-                </ul>
-              </div>
-            ) : (
-              ''
-            )}
-            {['admin', 'dgec', 'porteur-projet'].includes(user.role) ? (
-              <div {...dataId('invitation-form')}>
-                <a
-                  href="#"
-                  {...dataId('invitation-form-show-button')}
-                  className="invitationFormToggle"
-                >
-                  Donner accès à un autre utilisateur
-                </a>
-                <form
-                  action={ROUTES.INVITE_USER_TO_PROJECT_ACTION}
-                  method="post"
-                  name="form"
-                  className="invitationForm"
-                >
-                  <h5 style={{ marginBottom: 5 }}>Gestion des accès à ce projet</h5>
-                  <input type="hidden" name="projectId" id="projectId" value={project.id} />
-                  <label htmlFor="email">
-                    Courrier électronique de la personne habilitée à suivre ce projet
-                  </label>
-                  <input type="email" name="email" id="email" {...dataId('email-field')} />
-                  <button
-                    className="button"
-                    type="submit"
-                    name="submit"
-                    id="submit"
-                    {...dataId('submit-button')}
-                  >
-                    Accorder les droits sur ce projet
-                  </button>
-                  <a href="#" {...dataId('invitation-form-hide-button')}>
-                    Annuler
-                  </a>
-                </form>
-              </div>
-            ) : (
-              ''
-            )}
-          </Section>
-          <Section title="Matériels et technologies" icon="cog">
-            <div>Fournisseur: {project.fournisseur}</div>
-            <div>Evaluation carbone simplifiée: {project.evaluationCarbone} kg eq CO2/kWc</div>
-          </Section>
-          {project.appelOffre?.type === 'innovation' && user.role !== 'dreal' ? (
-            <Section title="Résultats de l'appel d'offres" icon="clipboard-check">
-              <div style={{ marginBottom: 10, fontSize: 18 }} {...dataId('project-note')}>
-                <b>Note totale</b>: {project.note || 'N/A'}
-              </div>
-              <ul>
-                <NoteElement project={project} column={'Note prix'} />
-                <NoteElement project={project} column={'Note innovation\n(AO innovation)'} />
-                <ul>
-                  <NoteElement
-                    project={project}
-                    column={'Note degré d’innovation (/20pt)\n(AO innovation)'}
-                  />
-                  <NoteElement
-                    project={project}
-                    column={'Note positionnement sur le marché (/10pt)\n(AO innovation)'}
-                  />
-                  <NoteElement
-                    project={project}
-                    column={'Note qualité technique (/5pt)\n(AO innovation)'}
-                  />
-                  <NoteElement
-                    project={project}
-                    column={
-                      'Note adéquation du projet avec les ambitions industrielles (/5pt)\n(AO innovation)'
-                    }
-                  />
-                  <NoteElement
-                    project={project}
-                    column={'Note aspects environnementaux et sociaux (/5pt)\n(AO innovation)'}
-                  />
-                </ul>
-              </ul>
-            </Section>
-          ) : (
-            ''
-          )}
+                    </ul>
+                  </div>
+                ) : (
+                  ''
+                )}
+                {['admin', 'dgec', 'porteur-projet'].includes(user.role) ? (
+                  <div {...dataId('invitation-form')}>
+                    <a
+                      href="#"
+                      {...dataId('invitation-form-show-button')}
+                      className="invitationFormToggle"
+                    >
+                      Donner accès à un autre utilisateur
+                    </a>
+                    <form
+                      action={ROUTES.INVITE_USER_TO_PROJECT_ACTION}
+                      method="post"
+                      name="form"
+                      className="invitationForm"
+                    >
+                      <h5 style={{ marginBottom: 5 }}>Gestion des accès à ce projet</h5>
+                      <input type="hidden" name="projectId" id="projectId" value={project.id} />
+                      <label htmlFor="email">
+                        Courrier électronique de la personne habilitée à suivre ce projet
+                      </label>
+                      <input type="email" name="email" id="email" {...dataId('email-field')} />
+                      <button
+                        className="button"
+                        type="submit"
+                        name="submit"
+                        id="submit"
+                        {...dataId('submit-button')}
+                      >
+                        Accorder les droits sur ce projet
+                      </button>
+                      <a href="#" {...dataId('invitation-form-hide-button')}>
+                        Annuler
+                      </a>
+                    </form>
+                  </div>
+                ) : (
+                  ''
+                )}
+              </Section>
+              <Section title="Matériels et technologies" icon="cog">
+                <div>Fournisseur: {project.fournisseur}</div>
+                <div>Evaluation carbone simplifiée: {project.evaluationCarbone} kg eq CO2/kWc</div>
+              </Section>
+              {project.appelOffre?.type === 'innovation' && user.role !== 'dreal' ? (
+                <Section title="Résultats de l'appel d'offres" icon="clipboard-check">
+                  <div style={{ marginBottom: 10, fontSize: 18 }} {...dataId('project-note')}>
+                    <b>Note totale</b>: {project.note || 'N/A'}
+                  </div>
+                  <ul>
+                    <NoteElement project={project} column={'Note prix'} />
+                    <NoteElement project={project} column={'Note innovation\n(AO innovation)'} />
+                    <ul>
+                      <NoteElement
+                        project={project}
+                        column={'Note degré d’innovation (/20pt)\n(AO innovation)'}
+                      />
+                      <NoteElement
+                        project={project}
+                        column={'Note positionnement sur le marché (/10pt)\n(AO innovation)'}
+                      />
+                      <NoteElement
+                        project={project}
+                        column={'Note qualité technique (/5pt)\n(AO innovation)'}
+                      />
+                      <NoteElement
+                        project={project}
+                        column={
+                          'Note adéquation du projet avec les ambitions industrielles (/5pt)\n(AO innovation)'
+                        }
+                      />
+                      <NoteElement
+                        project={project}
+                        column={'Note aspects environnementaux et sociaux (/5pt)\n(AO innovation)'}
+                      />
+                    </ul>
+                  </ul>
+                </Section>
+              ) : (
+                ''
+              )}
+            </div>
+          </div>
           {user.role == 'porteur-projet' && project.isClasse ? (
             <Section title="Cahier des charges" icon="clipboard-check">
               <form action={ROUTES.CHANGER_CDC} method="post" className={'m-0 max-w-full'}>
@@ -225,7 +231,7 @@ export const ProjectDetails = PageLayout(
           ) : (
             ''
           )}
-        </div>
+        </main>
       </RoleBasedDashboard>
     )
   }
