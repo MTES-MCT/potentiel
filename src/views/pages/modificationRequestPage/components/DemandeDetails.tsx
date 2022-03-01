2127429a7e88d86cbdabc4a61658240b2a3f8513
Pierre Duchateau <pad@dabla.com>
2022-03-01 15:26:03 +0100

    :recycle:  Separate each type of details in a component

--- src/views/pages/modificationRequestPage/components/DemandeDetails.tsx
+++ src/views/pages/modificationRequestPage/components/DemandeDetails.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import moment from 'moment'
-
-import { formatDate } from '../../../../helpers/formatDate'
 import { ModificationRequestPageDTO } from '@modules/modificationRequest'
-import { DownloadIcon } from '../../../components'
-import ROUTES from '../../../../routes'
+import moment from 'moment'
+import React from 'react'
+import { formatDate } from '../../../../helpers/formatDate'
 import { dataId } from '../../../../helpers/testId'
+import ROUTES from '../../../../routes'
+import { DownloadIcon } from '../../../components'
 
 interface DemandeDetailsProps {
   modificationRequest: ModificationRequestPageDTO
 }
 
 export const DemandeDetails = ({ modificationRequest }: DemandeDetailsProps) => {
-  const { requestedBy, requestedOn, justification, project, attachmentFile, status } =
-    modificationRequest
+  const { requestedBy, requestedOn, justification, attachmentFile } = modificationRequest
+
   return (
     <div className="panel__header">
       <div>
@@ -26,77 +25,7 @@ export const DemandeDetails = ({ modificationRequest }: DemandeDetailsProps) => 
           {'"'}
         </div>
       )}
-      {modificationRequest.type === 'delai' ? (
-        status === 'envoyée' || status === 'en instruction' ? (
-          <div style={{ marginTop: 5 }}>
-            La date de mise en service théorique est au <b>{formatDate(project.completionDueOn)}</b>
-            .
-            <br />
-            Le porteur demande un délai de <b>{modificationRequest.delayInMonths} mois</b>, ce qui
-            reporterait la mise en service au{' '}
-            <b>
-              {formatDate(
-                +moment(project.completionDueOn).add(modificationRequest.delayInMonths, 'month')
-              )}
-            </b>
-            .
-          </div>
-        ) : (
-          <div style={{ marginTop: 5 }}>
-            Le porteur a demandé un délai de <b>{modificationRequest.delayInMonths} mois</b>.{' '}
-          </div>
-        )
-      ) : null}
-      {modificationRequest.type === 'puissance' && (
-        <div className="flex flex-col" style={{ marginTop: 5 }}>
-          <span>
-            Puissance à la notification : {modificationRequest.project.puissanceInitiale}{' '}
-            {modificationRequest.project.unitePuissance}
-          </span>
-          <span>
-            Puissance actuelle : {modificationRequest.project.puissance}{' '}
-            {modificationRequest.project.unitePuissance}
-          </span>
-          <span>
-            Nouvelle puissance demandée : {modificationRequest.puissance}{' '}
-            {modificationRequest.project.unitePuissance}
-          </span>
-        </div>
-      )}
-      {modificationRequest.type === 'actionnaire' && (
-        <div style={{ marginTop: 5 }}>
-          <span>Nouvel actionnaire : {modificationRequest.actionnaire}</span>
-        </div>
-      )}
-      {modificationRequest.type === 'producteur' && (
-        <div style={{ marginTop: 5 }}>
-          <span>Nouveau producteur : {modificationRequest.producteur}</span>
-        </div>
-      )}
-      {modificationRequest.type === 'fournisseur' && (
-        <div style={{ marginTop: 5 }}>
-          {modificationRequest.fournisseurs?.length > 0 && (
-            <>
-              <span>Nouveau(x) fournisseur(s) : </span>
-              <ul>
-                {modificationRequest.fournisseurs?.map((fournisseur, index) => (
-                  <li key={index}>
-                    {fournisseur.kind} : {fournisseur.name}
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {modificationRequest.evaluationCarbone && (
-            <>
-              <br />
-              <span>
-                Nouvelle évaluation carbone : {modificationRequest.evaluationCarbone} kg eq CO2/kWc
-              </span>
-            </>
-          )}
-        </div>
-      )}
+      <DetailsByType modificationRequest={modificationRequest} />
       {attachmentFile && (
         <div style={{ marginTop: 10 }}>
           <DownloadIcon />
@@ -108,6 +37,133 @@ export const DemandeDetails = ({ modificationRequest }: DemandeDetailsProps) => 
             Télécharger la pièce-jointe
           </a>
         </div>
+      )}
+    </div>
+  )
+}
+
+interface DetailsByTypeProps {
+  modificationRequest: ModificationRequestPageDTO
+}
+const DetailsByType = ({ modificationRequest }: DetailsByTypeProps) => {
+  switch (modificationRequest.type) {
+    case 'delai':
+      return <DelaiDetails modificationRequest={modificationRequest} />
+    case 'puissance':
+      return <PuissanceDetails modificationRequest={modificationRequest} />
+    case 'actionnaire':
+      return <ActionnaireDetails modificationRequest={modificationRequest} />
+    case 'producteur':
+      return <ProducteurDetails modificationRequest={modificationRequest} />
+    case 'fournisseur':
+      return <FournisseurDetails modificationRequest={modificationRequest} />
+    default:
+      return null
+  }
+}
+
+interface DelaiDetailsProps {
+  modificationRequest: ModificationRequestPageDTO & { type: 'delai' }
+}
+const DelaiDetails = ({ modificationRequest }: DelaiDetailsProps) => {
+  const { project, status } = modificationRequest
+
+  return status === 'envoyée' || status === 'en instruction' ? (
+    <div style={{ marginTop: 5 }}>
+      La date de mise en service théorique est au <b>{formatDate(project.completionDueOn)}</b>
+      .
+      <br />
+      Le porteur demande un délai de <b>{modificationRequest.delayInMonths} mois</b>, ce qui
+      reporterait la mise en service au{' '}
+      <b>
+        {formatDate(
+          +moment(project.completionDueOn).add(modificationRequest.delayInMonths, 'month')
+        )}
+      </b>
+      .
+    </div>
+  ) : (
+    <div style={{ marginTop: 5 }}>
+      Le porteur a demandé un délai de <b>{modificationRequest.delayInMonths} mois</b>.{' '}
+    </div>
+  )
+}
+
+interface PuissanceDetailsProps {
+  modificationRequest: ModificationRequestPageDTO & { type: 'puissance' }
+}
+const PuissanceDetails = ({ modificationRequest }: PuissanceDetailsProps) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      <span>
+        Puissance à la notification : {modificationRequest.project.puissanceInitiale}{' '}
+        {modificationRequest.project.unitePuissance}
+      </span>
+
+      <br />
+
+      <span>
+        Puissance actuelle : {modificationRequest.project.puissance}{' '}
+        {modificationRequest.project.unitePuissance}
+      </span>
+
+      <br />
+
+      <span>
+        Nouvelle puissance demandée : {modificationRequest.puissance}{' '}
+        {modificationRequest.project.unitePuissance}
+      </span>
+    </div>
+  )
+}
+
+interface ActionnaireDetailsProps {
+  modificationRequest: ModificationRequestPageDTO & { type: 'actionnaire' }
+}
+const ActionnaireDetails = ({ modificationRequest }: ActionnaireDetailsProps) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      <span>Nouvel actionnaire : {modificationRequest.actionnaire}</span>
+    </div>
+  )
+}
+
+interface ProducteurDetailsProps {
+  modificationRequest: ModificationRequestPageDTO & { type: 'producteur' }
+}
+const ProducteurDetails = ({ modificationRequest }: ProducteurDetailsProps) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      <span>Nouveau producteur : {modificationRequest.producteur}</span>
+    </div>
+  )
+}
+
+interface FournisseurDetailsProps {
+  modificationRequest: ModificationRequestPageDTO & { type: 'fournisseur' }
+}
+const FournisseurDetails = ({ modificationRequest }: FournisseurDetailsProps) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      {modificationRequest.fournisseurs?.length > 0 && (
+        <>
+          <span>Nouveau(x) fournisseur(s) : </span>
+          <ul>
+            {modificationRequest.fournisseurs?.map((fournisseur, index) => (
+              <li key={index}>
+                {fournisseur.kind} : {fournisseur.name}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      {modificationRequest.evaluationCarbone && (
+        <>
+          <br />
+          <span>
+            Nouvelle évaluation carbone : {modificationRequest.evaluationCarbone} kg eq CO2/kWc
+          </span>
+        </>
       )}
     </div>
   )
