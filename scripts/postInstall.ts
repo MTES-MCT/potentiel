7872aea62b4487abd4a5679a0fb86222fdc2a6f6
Pierre Duchateau <pad@dabla.com>
2022-03-08 11:39:52 +0100

    :star2:  Inject umami tracker in home page (on post-install)

--- scripts/postInstall.ts
+++ scripts/postInstall.ts
@@ -2,18 +2,57 @@
 
 import dotenv from 'dotenv'
 import { spawnSync } from 'child_process'
+import fs from 'fs/promises'
+import path from 'path'
+import { getTrackerScript } from '../src/infra/umami'
 
 process.env.NODE_ENV ?? dotenv.config()
 
 const NODE_ENV = process.env.NODE_ENV || ''
-const executeMigrateScript = process.env.DO_NOT_EXECUTE_MIGRATE ? false : true 
+const executeMigrateScript = process.env.DO_NOT_EXECUTE_MIGRATE ? false : true
+const trackerWebsiteId = process.env.TRACKER_WEBSITE_ID
 
-if (!['', 'local', 'test'].includes(NODE_ENV)) {
-  const build = spawnSync('npm', ['run', 'build'], { stdio: 'inherit' })
-  build.status && build.status > 0 && process.exit(build.status)
+postInstall()
 
-  if (executeMigrateScript) {
-    const migrate = spawnSync('npm', ['run', 'migrate'], { stdio: 'inherit' })
-    process.exit(migrate.status ?? 0)
+async function postInstall() {
+  console.log('postInstall')
+  if (!['', 'local', 'test'].includes(NODE_ENV)) {
+    console.log('postInstall executes')
+    const build = spawnSync('npm', ['run', 'build'], { stdio: 'inherit' })
+    if (build.status && build.status > 0) process.exit(build.status)
+
+    await insertTracker()
+
+    if (executeMigrateScript) {
+      const migrate = spawnSync('npm', ['run', 'migrate'], { stdio: 'inherit' })
+      process.exit(migrate.status ?? 0)
+    }
+
+    process.exit(0)
+  }
+}
+
+async function insertTracker() {
+  console.log('postInstall insertTracker start', { trackerWebsiteId })
+  if (!trackerWebsiteId) return
+
+  const trackerScript = getTrackerScript(trackerWebsiteId)
+
+  const indexPath = path.resolve('dist/src/public/index.html')
+
+  const trackerRegexp = /<!-- TRACKER -->/g
+
+  const indexHtml = await fs.readFile(indexPath, { encoding: 'utf-8' })
+
+  if (!indexHtml.match(trackerRegexp)) {
+    console.warn(
+      'WARNING ! TRACKER COULD NOT BE INJECTED, index.html does not contain <!-- TRACKER -->'
+    )
+  } else {
+    console.log('Inserting tracker script to', indexPath)
+    await fs.writeFile(indexPath, indexHtml.replace(trackerRegexp, trackerScript))
+
+    const oldIndexPath = path.resolve('src/public/index.html')
+    await fs.writeFile(oldIndexPath, 'TEST')
   }
 }
