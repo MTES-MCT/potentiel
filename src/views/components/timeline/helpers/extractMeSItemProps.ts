6fbfb61862482b257e0bea860457c05893324cf3
HanaeY <hanae.yamashita@lilo.org>
2022-03-14 15:11:41 +0100

    :wrench: Update extractMeSItemProps

--- src/views/components/timeline/helpers/extractMeSItemProps.ts
+++ src/views/components/timeline/helpers/extractMeSItemProps.ts
@@ -1,4 +1,4 @@
-import { ProjectEventDTO } from '@modules/frise'
+import { ProjectEventDTO, ProjectEventListDTO } from '@modules/frise'
 
 export type MeSItemProps = {
   type: 'mise-en-service'
@@ -9,10 +9,10 @@ export type MeSItemProps = {
 export const extractMeSItemProps = (
   events: ProjectEventDTO[],
   project: {
-    isLaureat: boolean
+    status: ProjectEventListDTO['project']['status']
   }
 ): MeSItemProps | null => {
-  if (!events.length || !project.isLaureat) {
+  if (!events.length || project.status !== 'Classé') {
     return null
   }
 
