a84f0fd2f5a0c743b6f97bd9ceb83dbbe5691ab0
Julien Pavon <julien.pavon@gmail.com>
2022-03-18 15:32:48 +0100

    :card_file_box: Rebuild legacy overwritten project

---
+++ src/infra/sequelize/migrations/20220318143025-rebuild-overwritten-project.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const { models } = require('../models')
+const { fromPersistance } = require('../helpers/fromPersistance')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const projectIdToRebuild = 'ed3ccfda-fb6f-4410-b040-ac325abadc94'
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      const events = await queryInterface.sequelize.query(
+        `SELECT * FROM "eventStores" WHERE "aggregateId" && ? ORDER BY "occurredAt" ASC`,
+        {
+          type: queryInterface.sequelize.QueryTypes.SELECT,
+          replacements: [`{${projectIdToRebuild}}`],
+          transaction,
+        }
+      )
+
+      const importedEvent = fromPersistance(
+        events.filter((e) => e.type === 'ProjectImported').pop()
+      )
+      const { projectId, data, potentielIdentifier } = importedEvent.payload
+      const importedProject = {
+        id: projectId,
+        ...data,
+        potentielIdentifier,
+      }
+
+      const eventsWithoutImported = events
+        .filter((e) => e.type !== 'ProjectImported')
+        .map(fromPersistance)
+
+      const projectToUpdate = eventsWithoutImported.reduce((project, event) => {
+        switch (event.type) {
+          case 'ProjectNotificationDateSet':
+            return {
+              ...project,
+              notifiedOn: event.payload.notifiedOn,
+            }
+          case 'ProjectDCRDueDateSet':
+            return {
+              ...project,
+              dcrDueOn: event.payload.dcrDueOn,
+            }
+          case 'ProjectGFDueDateSet':
+            return {
+              ...project,
+              garantiesFinancieresDueOn: event.payload.garantiesFinancieresDueOn,
+            }
+          case 'ProjectCompletionDueDateSet':
+            return {
+              ...project,
+              completionDueOn: event.payload.completionDueOn,
+            }
+        }
+      }, importedProject)
+
+      const { Project } = models
+      await Project.update(projectToUpdate, { where: { id: projectId } }, { transaction })
+
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
