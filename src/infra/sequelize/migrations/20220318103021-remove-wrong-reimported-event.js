bf43c3d69cb8b46b9d766570b5cda8ec0857d6c2
Julien Pavon <julien.pavon@gmail.com>
2022-03-18 14:15:21 +0100

    :card_file_box: Remove wrong reimported event from 2 projects

--- src/infra/sequelize/migrations/20220318103021-remove-wrong-reimported-event.js
+++ src/infra/sequelize/migrations/20220318103021-remove-wrong-reimported-event.js
@@ -6,22 +6,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const projectIds = [
       'ed3ccfda-fb6f-4410-b040-ac325abadc94',
-      '5ae063cf-3f31-421b-9d76-553e14f2b405',
-      '0169d9c8-148a-4764-8b52-021685144cad',
-      '6c4ce2b7-81d2-486c-9d94-bb5417093be0',
+      'ea60df43-a026-11ea-b05c-11293d839ea9',
     ]
 
     const transaction = await queryInterface.sequelize.transaction()
     try {
       for (const projectId of projectIds) {
-        await queryInterface.sequelize.query(`DELETE FROM "projects" WHERE "id" = ?`, {
-          type: queryInterface.sequelize.QueryTypes.DELETE,
-          replacements: [projectId],
-          transaction,
-        })
-
         const events = await queryInterface.sequelize.query(
-          `SELECT * FROM "eventStores" WHERE "aggregateId" && ?`,
+          `SELECT * FROM "eventStores" WHERE "aggregateId" && ? ORDER BY "occurredAt" ASC`,
           {
             type: queryInterface.sequelize.QueryTypes.SELECT,
             replacements: [`{${projectId}}`],
@@ -29,23 +21,19 @@ module.exports = {
           }
         )
 
-        if (events && events.length) {
-          const eventsToDelete = events
-            .map((event) => fromPersistance(event))
-            .filter((event) => event !== null)
+        const lastReimportedEvent = events.filter((e) => e.type === 'ProjectReimported').pop()
 
+        if (lastReimportedEvent) {
           await queryInterface.sequelize.query(
-            `DELETE FROM "eventStores" WHERE "id" IN (${eventsToDelete
-              .map((e) => `'${e.id}'`)
-              .join(',')})`,
+            `DELETE FROM "eventStores" WHERE "id" = '${lastReimportedEvent.id}'`,
             {
               type: queryInterface.sequelize.QueryTypes.DELETE,
-              replacements: [`{${projectId}}`],
               transaction,
             }
           )
         }
       }
+
       await transaction.commit()
     } catch (err) {
       await transaction.rollback()
