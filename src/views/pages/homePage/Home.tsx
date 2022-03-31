c4a5d83398f7174d459b34e365302c70ce36e328
Julien Pavon <julien.pavon@gmail.com>
2022-03-31 11:08:05 +0200

    :lipstick: 1005 Mise à jour de la page d'accueil avec la proposition de valeur de Potentiel (#410)
    * :lipstick: Change the first section in the home page
    
    * :lipstick: Arrange styles for the login section
    
    * :bento: Update image
    
    * :recycle: Extract some css class applying tailwind styles
    
    * :lipstick: Use the same blue color from DSFR configured in tailwind
    
    * :recycle: Extract Container and LinkButton component in HomePage

--- src/views/pages/homePage/Home.tsx
+++ src/views/pages/homePage/Home.tsx
@@ -1,47 +1,72 @@
 import type { Request } from 'express'
 import React from 'react'
+import { RiAccountCircleLine } from 'react-icons/ri'
 import { PageLayout } from '../../components/PageLayout'
 import { hydrateOnClient } from '../../helpers/hydrateOnClient'
 
-interface Props {
+type Props = {
   request: Request
 }
 
-/* Pure component */
 export const Home = PageLayout(function (props: Props) {
   return (
     <>
-      <div
-        className="hero"
-        role="banner"
-        style={{ background: "url('images/PV.jpg') no-repeat center", backgroundSize: 'cover' }}
-      >
-        <div className="hero__container text-center">
-          <h1 style={{ color: 'white', textShadow: '1px 1px 3px rgba(0, 0, 0, 0.7)' }}>
-            POTENTIEL
-          </h1>
-          <p style={{ color: 'white', textShadow: '1px 1px 3px rgba(0, 0, 0, 0.7)' }}>
-            Faciliter le parcours des producteurs d'énergies renouvelables électriques
-          </p>
-        </div>
-      </div>
-
-      <main role="main">
-        <section className="section section-grey" style={{ minHeight: 'calc(100vh - 810px)' }}>
-          <div className="container">
-            <p className="section__subtitle">
-              Potentiel est un nouveau service du ministère de la transition écologique. <br />
-              Cette plateforme, en cours de développement, permettra la gestion des projets
-              d'énergie renouvelable électrique soutenus par les appels d’offres de l’Etat. <br />
-              <a className="button button-primary" style={{ marginTop: '50px' }} href="/login.html">
-                Je m'identifie
-              </a>
+      <main role="main" style={{ fontFamily: 'Marianne, arial, sans-serif' }}>
+        <section className="bg-blue-france-sun-base text-white">
+          <Container className="flex flex-col p-6 gap-6 xl:py-10">
+            <div className="flex flex-col md:flex-row gap-6">
+              <p className="m-0 text-3xl lg:text-5xl xl:text-6xl font-semibold">
+                Suivez efficacement vos projets EnRE*, transmettez vos documents, demandez des
+                modifications.
+                <br />
+                <span className="text-sm lg:text-base font-light">
+                  *Énergies renouvelables électriques
+                </span>
+              </p>
+              <img className="w-full md:w-1/2" src="/images/home/proposition_valeur.png" />
+            </div>
+            <p className="text-lg md:text-base lg:text-xl font-medium md:font-semibold md:text-center md:m-0 md:mt-10 md:mb-0 lg:px-16 lg:leading-loose">
+              Potentiel est le service du Ministère de la Transition Écologique qui connecte
+              <br className="hidden md:inline" /> tous les acteurs du parcours administratif des
+              projets d'EnRE soumis à appel d'offre en France
             </p>
-          </div>
+          </Container>
+        </section>
+
+        <section>
+          <Container className="flex p-6">
+            <LinkButton className="mx-auto" href="/login.html">
+              <RiAccountCircleLine className="mr-4" />
+              Je m'identifie
+            </LinkButton>
+          </Container>
         </section>
       </main>
     </>
   )
 })
+
+type ContainerProps = {
+  className?: string
+  children?: React.ReactNode
+}
+const Container = ({ className, children }: ContainerProps) => (
+  <div className={`flex xl:mx-auto xl:max-w-7xl ${className}`}>{children}</div>
+)
+
+type LinkButtonProps = {
+  href: string
+  className?: string
+  children?: React.ReactNode
+}
+const LinkButton = ({ href, className, children }: LinkButtonProps) => (
+  <a
+    className={`no-underline inline-flex items-center px-6 py-3 border border-transparent text-base lg:text-lg font-medium shadow-sm text-white bg-blue-france-sun-base hover:bg-blue-france-sun-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-blue-france-sun-active ${className}`}
+    style={{ color: 'white', textDecoration: 'none' }}
+    href={href}
+  >
+    {children}
+  </a>
+)
 
 hydrateOnClient(Home)
