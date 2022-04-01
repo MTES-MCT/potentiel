0d7013ad170d2237c94f3b74c643e3f0def79505
Pierre Duchateau <pad@dabla.com>
2022-04-01 09:30:00 +0200

    fix postCancelGF...

--- src/controllers/admin/postCancelGFDueDateForPPE2AOs.ts
+++ src/controllers/admin/postCancelGFDueDateForPPE2AOs.ts
@@ -6,6 +6,7 @@ import { v1Router } from '../v1Router'
 import models from '../../infra/sequelize/models'
 import { InfraNotAvailableError } from '../../modules/shared'
 import { ok } from 'assert'
+import { errorResponse } from '../helpers'
 
 // Ce point d'entrée est destiné à n'être appelé qu'une seule fois - à supprimer
 const { Project } = models
@@ -14,22 +15,29 @@ v1Router.get(
   '/admin/cancel-GFDueDate-PPE2',
   ensureRole(['admin']),
   asyncHandler(async (request, response) => {
-    await getProjectToFix().andThen(async (projectIds) => {
-      try {
-        for (const projectId of projectIds) {
-          const res = await projectRepo.transaction(new UniqueEntityID(projectId), (project) => {
-            return project.cancelGFDueDateSet()
-          })
-          if (res.isErr()) {
-            throw res.error
-          }
+    const result = await getProjectToFix()
+
+    if (result.isErr()) {
+      return errorResponse({ response, request })
+    }
+
+    const projectIds = result.value
+
+    try {
+      for (const projectId of projectIds) {
+        const res = await projectRepo.transaction(new UniqueEntityID(projectId), (project) => {
+          return project.cancelGFDueDateSet()
+        })
+        if (res.isErr()) {
+          throw res.error
         }
-      } catch (e) {
-        logger.error(e)
-        response.send('Une erreur est survenue')
       }
-      response.send('Les projets ont bien été corrigés')
-    })
+    } catch (e) {
+      logger.error(e)
+      return errorResponse({ response, request })
+    }
+
+    response.send('Les projets ont bien été corrigés')
   })
 )
 
