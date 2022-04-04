dd2f92266b98a52cb738174df9dbd36482e4207f
HanaeY <hanae.yamashita@lilo.org>
2022-04-04 17:23:48 +0200

    :lipstick: wording

--- src/views/components/timeline/components/ACItem.tsx
+++ src/views/components/timeline/components/ACItem.tsx
@@ -13,7 +13,8 @@ export const ACItem = ({ date, covidDelay }: ACItemProps) => {
         </span>
         {covidDelay && (
           <p className="p-0 m-0">
-            Prolongation de délai d'achèvement ou de mise en service (
+            Ce projet bénéficie d'une prolongation de délai d'achèvement ou de mise en service
+            compte tenu de la crise liée au coronavirus (covid-19) (
             <a
               href="https://www.ecologie.gouv.fr/sites/default/files/2004%20-%20SR%20-%20Note%20EDF%20OA%20D%C3%A9finition%20des%20d%C3%A9lais%20COVID%202019_v5.pdf"
               download
