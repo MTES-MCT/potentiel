6e53e51c938ca283e36ca3dee1fb776355d689a0
HanaeY <hanae.yamashita@lilo.org>
2022-05-05 10:44:19 +0200

    :recycle:  Use yup to validat request body

--- src/controllers/garantiesFinancieres/postSubmitGF.ts
+++ src/controllers/garantiesFinancieres/postSubmitGF.ts
@@ -2,130 +2,139 @@ import asyncHandler from '../helpers/asyncHandler'
 import fs from 'fs'
 import { ensureRole } from '@config'
 import { submitGF } from '@config/useCases.config'
-import { logger } from '@core/utils'
+import { logger, Result, ok, err } from '@core/utils'
 import { addQueryParams } from '../../helpers/addQueryParams'
-import { pathExists } from '../../helpers/pathExists'
-import { validateUniqueId } from '../../helpers/validateUniqueId'
 import { UnauthorizedError } from '@modules/shared'
 import routes from '../../routes'
 import { errorResponse, unauthorizedResponse } from '../helpers'
 import { upload } from '../upload'
 import { v1Router } from '../v1Router'
-import { GFCertificateHasAlreadyBeenSentError } from '../../modules/project'
-import { parse, isDate } from 'date-fns'
+import {
+  CertificateFileIsMissingError,
+  GFCertificateHasAlreadyBeenSentError,
+} from '../../modules/project'
+import { parse } from 'date-fns'
+import * as yup from 'yup'
+import { ValidationError, BaseSchema, InferType } from 'yup'
+import { pathExists } from '../../helpers/pathExists'
+
+const parseStringDate = (value, originalValue) => {
+  return parse(originalValue, 'yyyy-MM-dd', new Date())
+}
+
+const requestBodySchema = yup.object({
+  projectId: yup.string().uuid().required(),
+  stepDate: yup
+    .date()
+    .transform(parseStringDate)
+    .max(
+      new Date().toLocaleDateString('en-ca'),
+      "La date de constitution ne doit dépasser la date d'aujourd'hui."
+    )
+    .required('Vous devez renseigner la date de constitution.')
+    .typeError(`La date de constitution n'est pas valide.`),
+  expirationDate: yup
+    .date()
+    .transform(parseStringDate)
+    .required("Vous devez renseigner la date d'échéance.")
+    .typeError(`La date d'échéance saisie n'est pas valide.`),
+  type: yup.mixed().oneOf(['garanties-financieres']).required('Ce champ est obligatoire.'),
+})
+class RequestValidationError extends Error {
+  constructor(public errors: Array<string>) {
+    super("L'attestation de constitution des garanties financières n'a pas pu être envoyée.")
+  }
+}
+
+const validateRequestBody = (
+  body: Request['body'],
+  schema: BaseSchema
+): Result<InferType<typeof schema>, RequestValidationError | Error> => {
+  try {
+    return ok(schema.validateSync(body, { abortEarly: false }))
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return err(new RequestValidationError(error.errors))
+    }
+    return err(error)
+  }
+}
 
 v1Router.post(
   routes.SUBMIT_GARANTIES_FINANCIERES(),
   ensureRole(['porteur-projet']),
   upload.single('file'),
   asyncHandler(async (request, response) => {
-    const { type, stepDate, projectId, expirationDate } = request.body
-
-    if (!validateUniqueId(projectId)) {
-      return errorResponse({
-        request,
-        response,
-        customMessage:
-          'Il y a eu une erreur lors de le la transmission de votre attestation de garanties financières. Merci de recommencer.',
+    validateRequestBody(request.body, requestBodySchema)
+      .andThen((body) => {
+        if (!request.file || !pathExists(request.file.path)) {
+          return err(new CertificateFileIsMissingError())
+        }
+        return ok(body)
       })
-    }
+      .asyncAndThen((body) => {
+        const { stepDate, projectId, expirationDate } = body
+        const { user: submittedBy } = request
+        const file = {
+          contents: fs.createReadStream(request.file!.path),
+          filename: `${Date.now()}-${request.file!.originalname}`,
+        }
 
-    if (type !== 'garanties-financieres') {
-      return errorResponse({ request, response, customStatus: 400 })
-    }
-
-    if (!stepDate) {
-      return response.redirect(
-        addQueryParams(routes.PROJECT_DETAILS(projectId), {
-          error:
-            "Votre attestation de garanties financières n'a pas pu être soumise. La date est obligatoire.",
-        })
-      )
-    }
-
-    const parsedStepDate = isDate(stepDate) ? stepDate : parse(stepDate, 'yyyy-MM-dd', new Date())
-
-    if (!isDate(parsedStepDate)) {
-      return response.redirect(
-        addQueryParams(routes.PROJECT_DETAILS(projectId), {
-          error:
-            "Votre attestation de garanties financières n'a pas pu être soumise. La date envoyée n'est pas au bon format (JJ/MM/AAAA)",
-        })
-      )
-    }
-
-    if (!expirationDate) {
-      return response.redirect(
-        addQueryParams(routes.PROJECT_DETAILS(projectId), {
-          error:
-            "Votre attestation de garanties financières n'a pas pu être soumise. La date d'échéance est obligatoire.",
-        })
-      )
-    }
-
-    const parsedExpirationDate = isDate(expirationDate)
-      ? expirationDate
-      : parse(expirationDate, 'yyyy-MM-dd', new Date())
-
-    if (!isDate(parsedExpirationDate)) {
-      return response.redirect(
-        addQueryParams(routes.PROJECT_DETAILS(projectId), {
-          error:
-            "Votre attestation de garanties financières n'a pas pu être soumise. La date d'échéance n'est pas au bon format (JJ/MM/AAAA)",
-        })
-      )
-    }
-
-    const attestationExists: boolean = !!request.file && (await pathExists(request.file.path))
-
-    if (!attestationExists) {
-      return response.redirect(
-        addQueryParams(routes.PROJECT_DETAILS(projectId), {
-          error:
-            "Votre attestation de garanties financières n'a pas pu être soumise. Merci d'attacher l'attestation en pièce-jointe.",
-        })
-      )
-    }
-
-    const file = {
-      contents: fs.createReadStream(request.file!.path),
-      filename: `${Date.now()}-${request.file!.originalname}`,
-    }
-
-    ;(
-      await submitGF({
-        projectId,
-        stepDate: parsedStepDate,
-        expirationDate: parsedExpirationDate,
-        file,
-        submittedBy: request.user,
+        return submitGF({ projectId, stepDate, expirationDate, file, submittedBy }).map(() => ({
+          projectId,
+        }))
       })
-    ).match(
-      () =>
-        response.redirect(
-          routes.SUCCESS_OR_ERROR_PAGE({
-            success: 'Votre attestation de garanties financières a bien été soumise.',
-            redirectUrl: routes.PROJECT_DETAILS(projectId),
-            redirectTitle: 'Retourner à la page projet',
-          })
-        ),
-      (e) => {
-        if (e instanceof GFCertificateHasAlreadyBeenSentError) {
+      .match(
+        ({ projectId }) => {
           return response.redirect(
-            addQueryParams(routes.PROJECT_DETAILS(projectId), {
-              error:
-                "Il semblerait qu'il y ait déjà une garantie financière en cours de validité sur ce projet.",
+            routes.SUCCESS_OR_ERROR_PAGE({
+              success: 'Votre attestation de garanties financières a bien été enregistrée.',
+              redirectUrl: routes.PROJECT_DETAILS(projectId),
+              redirectTitle: 'Retourner à la page projet',
             })
           )
-        }
+        },
+        (error) => {
+          if (error instanceof RequestValidationError) {
+            return response.redirect(
+              addQueryParams(routes.PROJECT_DETAILS(request.body.projectId), {
+                ...request.body,
+                error: `${error.message} ${error.errors.join(' ')}`,
+              })
+            )
+          }
 
-        if (e instanceof UnauthorizedError) {
-          return unauthorizedResponse({ request, response })
-        }
+          if (error instanceof CertificateFileIsMissingError) {
+            return response.redirect(
+              addQueryParams(routes.PROJECT_DETAILS(request.body.projectId), {
+                error:
+                  "L'attestation de constitution des garanties financières n'a pas pu être envoyée. Vous devez joindre un fichier.",
+              })
+            )
+          }
 
-        logger.error(e)
-        return errorResponse({ request, response })
-      }
-    )
+          if (error instanceof GFCertificateHasAlreadyBeenSentError) {
+            return response.redirect(
+              addQueryParams(routes.PROJECT_DETAILS(request.body.projectId), {
+                error:
+                  "Il semblerait qu'il y ait déjà une garantie financière en cours de validité sur ce projet.",
+              })
+            )
+          }
+
+          if (error instanceof UnauthorizedError) {
+            return unauthorizedResponse({ request, response })
+          }
+
+          logger.error(error)
+
+          return errorResponse({
+            request,
+            response,
+            customMessage:
+              'Il y a eu une erreur lors de la soumission de votre demande. Merci de recommencer.',
+          })
+        }
+      )
   })
 )
