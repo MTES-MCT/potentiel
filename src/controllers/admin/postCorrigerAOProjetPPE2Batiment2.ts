c1300e094464f0c7a4ed1e0d5e42edb0ab047bbd
Julien Pavon <julien.pavon@gmail.com>
2022-05-18 09:10:20 +0200

    :sparkles: Ajout du point d'entree pour la correction des projets de l'AO "PPE2 - Bâtiment 2"

--- src/controllers/admin/postCorrigerAOProjetPPE2Batiment2.ts
+++ src/controllers/admin/postCorrigerAOProjetPPE2Batiment2.ts
@@ -1,16 +1,42 @@
-import { importAppelOffreData } from '@config'
-import { logger } from '@core/utils'
 import asyncHandler from '../helpers/asyncHandler'
-import { addQueryParams } from '../../helpers/addQueryParams'
-import { parseCsv } from '../../helpers/parseCsv'
-import { InfraNotAvailableError, UnauthorizedError } from '@modules/shared'
-import routes from '../../routes'
-import { ensureRole } from '@config'
-import { upload } from '../upload'
+import { ensureRole, getProjectIdsForPeriode, modifierAppelOffreProjet } from '@config'
 import { v1Router } from '../v1Router'
+import { combine, logger } from '@core/utils'
+import routes from '../../routes'
+import { addQueryParams } from '../../helpers/addQueryParams'
 
 v1Router.post(
   'admin/corriger-ao-projet-ppe2-batiment2',
   ensureRole(['admin']),
-  asyncHandler(async (request, response) => {})
+  asyncHandler(async (request, response) => {
+    getProjectIdsForPeriode({ appelOffreId: 'PPE2 - Bâtiment 2', periodeId: '2' })
+      .andThen((projectIds) =>
+        combine(
+          projectIds.map((projectId) =>
+            modifierAppelOffreProjet({ projectId, appelOffreId: 'PPE2 - Bâtiment' })
+          )
+        )
+      )
+      .match(
+        () =>
+          response.redirect(
+            routes.SUCCESS_OR_ERROR_PAGE({
+              success: ``,
+              redirectUrl: addQueryParams(routes.ADMIN_LIST_PROJECTS, {
+                appelOffreId: 'PPE2 - Bâtiment',
+                periodeId: '2',
+              }),
+              redirectTitle: `Voir le listing des projets corrigés pour la période 2 de l'AO "PPE2 - Bâtiment"`,
+            })
+          ),
+        (e: Error) => {
+          logger.error(e)
+          response.redirect(
+            routes.SUCCESS_OR_ERROR_PAGE({
+              error: `Une erreur est survenue lors de la correction de l'AO des projets pour "PPE2 - Bâtiment 2"`,
+            })
+          )
+        }
+      )
+  })
 )
