8235708e94a28f98bc45de19c3e6e62a2e928453
Pierre Duchateau <pad@dabla.com>
2022-05-19 15:09:04 +0200

    Add table of projections with all the events that update

--- devtools/generateEventsDoc/EventsDocument.template.ts
+++ devtools/generateEventsDoc/EventsDocument.template.ts
@@ -16,19 +16,45 @@ export function EventsDocument(events: EventResult[]) {
     return moduleMap
   }, {})
 
+  const projections = events.reduce<Record<string, EventResult[]>>((projectionMap, event) => {
+    const { projectionUpdates } = event
+
+    for (const projectionUpdate of projectionUpdates) {
+      const { projection } = projectionUpdate
+      if (!projectionMap[projection]) {
+        projectionMap[projection] = []
+      }
+
+      projectionMap[projection].push(event)
+    }
+
+    return projectionMap
+  }, {})
+
   return `
 # Evenements
 
-${Sommaire(modules)}
+## Sommaire
+${SommaireProjections(projections)}
+${SommaireEvenementsParModule(modules)}
 
+${Projections(projections)}
 ${Modules(modules)}
   `
 }
 
-export function Sommaire(modules: Record<string, EventResult[]>) {
+export function SommaireProjections(projections: Record<string, EventResult[]>) {
   return `
-## Sommaire
+- Projections (tables)
+${Object.keys(projections)
+  .sort((a, b) => a.localeCompare(b))
+  .map((projection) => `  - [${projection}](#table-${projection.toLowerCase()})`)
+  .join('\n')}`
+}
 
+export function SommaireEvenementsParModule(modules: Record<string, EventResult[]>) {
+  return `
+- Événements par module
 ${Object.entries(modules)
   .map(([module, events]) => SommaireModuleItem(module, events))
   .join('\n')}`
@@ -36,13 +62,38 @@ ${Object.entries(modules)
 
 function SommaireModuleItem(module: string, events: EventResult[]) {
   return `
-- ${module}
+  - ${module}
 ${events.map(SommaireEventItem).join('\n')}
 `
 }
 
 function SommaireEventItem({ name }: EventResult) {
-  return `  - [${name}](#${name.toLowerCase()})`
+  return `    - [${name}](#${name.toLowerCase()})`
+}
+
+export function Projections(projections: Record<string, EventResult[]>) {
+  return `
+## Projections
+
+${Object.entries(projections)
+  .sort((a, b) => a[0].localeCompare(b[0]))
+  .map(([projection, events]) => DetailedProjectionItem(projection, events))
+  .join('\n')}
+  `
+}
+
+function DetailedProjectionItem(projection: string, events: EventResult[]) {
+  return `
+### Table ${projection}
+Mise à jour par:
+${events
+  .map(
+    ({ name }) => `
+  - [${name}](#${name.toLowerCase()})
+`
+  )
+  .join('\n\n')}
+`
 }
 
 export function Modules(modules: Record<string, EventResult[]>) {
