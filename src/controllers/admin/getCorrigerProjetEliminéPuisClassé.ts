8fa0d6c10bcea1789c774cdb4e330252b988d48c
HanaeY <hanae.yamashita@lilo.org>
2022-06-16 14:36:00 +0200

    :wrench: ajout point entrée et méthode temporaires

---
+++ src/controllers/admin/getCorrigerProjetEliminéPuisClassé.ts
@@ -0,0 +1,32 @@
+import { ensureRole, projectRepo } from '@config'
+import { UniqueEntityID } from 'src/core/domain'
+import { logger } from 'src/core/utils'
+import { addQueryParams } from 'src/helpers/addQueryParams'
+import routes from 'src/routes'
+import asyncHandler from '../helpers/asyncHandler'
+import { v1Router } from '../v1Router'
+
+v1Router.get(
+  '/admin/corriger-projet-elimine-puis-classe',
+  ensureRole('admin'),
+  asyncHandler(async (request, response) => {
+    const projectId = 'c599d37b-641e-49b2-a21f-2c1bb4261476'
+    try {
+      const res = await projectRepo.transaction(new UniqueEntityID(projectId), (project) => {
+        return project.corrigerProjetSansDatesDues()
+      })
+
+      if (res.isErr()) {
+        throw res.error
+      }
+    } catch (e) {
+      logger.error(e)
+      response.send('Une erreur est survenue')
+    }
+    response.redirect(
+      addQueryParams(routes.PROJECT_DETAILS(projectId), {
+        success: 'Le projet a bien été corrigé.',
+      })
+    )
+  })
+)
