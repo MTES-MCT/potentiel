f88c1320f199cc422d7984ae65d40b8619bcad0c
HanaeY <hanae.yamashita@lilo.org>
2022-06-23 16:16:48 +0200

    :star2: Enregistrer une nouvelle demande de délai dans ProjectEvents

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onDélaiDemandé.ts
@@ -0,0 +1,27 @@
+import { DélaiDemandé } from '@modules/modificationRequest'
+import { UniqueEntityID } from '@core/domain'
+import { ProjectEvent } from '../projectEvent.model'
+
+export default ProjectEvent.projector.on(
+  DélaiDemandé,
+  async ({ payload, occurredAt }, transaction) => {
+    const { demandeDélaiId, projetId, autorité, dateAchèvementDemandée, porteurId } = payload
+
+    await ProjectEvent.create(
+      {
+        id: demandeDélaiId,
+        projectId: projetId,
+        type: 'DemandeDélai',
+        valueDate: occurredAt.getTime(),
+        eventPublishedAt: occurredAt.getTime(),
+        payload: {
+          statut: 'demande-envoyée',
+          autorité,
+          dateAchèvementDemandée,
+          demandeur: porteurId,
+        },
+      },
+      { transaction }
+    )
+  }
+)
