9078e2d47da082b2321b356bf1de2e0f562d5936
HanaeY <hanae.yamashita@lilo.org>
2022-06-27 13:34:50 +0200

    :star2: événement DélaiAnnulé

---
+++ src/modules/modificationRequest/events/DélaiAnnulé.ts
@@ -0,0 +1,16 @@
+import { BaseDomainEvent, DomainEvent } from '@core/domain'
+
+export type DélaiAnnuléPayload = {
+  demandeDélaiId: string
+  annuléPar: string
+}
+
+export class DélaiAnnulé extends BaseDomainEvent<DélaiAnnuléPayload> implements DomainEvent {
+  public static type: 'DélaiAnnulé' = 'DélaiAnnulé'
+  public type = DélaiAnnulé.type
+  currentVersion = 1
+
+  aggregateIdFromPayload(payload: DélaiAnnuléPayload) {
+    return payload.demandeDélaiId
+  }
+}
--- src/modules/modificationRequest/events/index.ts
+++ src/modules/modificationRequest/events/index.ts
@@ -1,5 +1,6 @@
 export * from './ConfirmationRequested'
 export * from './DélaiAccordé'
+export * from './DélaiAnnulé'
 export * from './DélaiDemandé'
 export * from './DélaiEnInstruction'
 export * from './DélaiRefusé'
