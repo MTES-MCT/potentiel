ffdfb63b94d8a4826b6968fde305b37a7ae716d3
Julien Pavon <julien.pavon@gmail.com>
2022-07-07 14:00:14 +0200

    ✨ accorder toutes les demandes de delai avec une date

--- src/views/pages/modificationRequestPage/components/AdminRéponseDélaiForm.tsx
+++ src/views/pages/modificationRequestPage/components/AdminRéponseDélaiForm.tsx
@@ -1,54 +1,36 @@
-import moment from 'moment'
-import React from 'react'
-import { formatDate } from '../../../../helpers/formatDate'
-import { dataId } from '../../../../helpers/testId'
 import { ModificationRequestPageDTO } from '@modules/modificationRequest'
-import { Input } from '../../../components'
 import { format } from 'date-fns'
-interface AdminRéponseDélaiForm {
+import React from 'react'
+import { Input } from '../../../components'
+
+type AdminRéponseDélaiFormProps = {
   modificationRequest: ModificationRequestPageDTO & { type: 'delai' }
 }
-export const AdminRéponseDélaiForm = ({ modificationRequest }: AdminRéponseDélaiForm) => {
+
+export const AdminRéponseDélaiForm = ({ modificationRequest }: AdminRéponseDélaiFormProps) => {
   const { project, delayInMonths, dateAchèvementDemandée } = modificationRequest
   const dateDemandée = dateAchèvementDemandée
     ? new Date(dateAchèvementDemandée)
-    : moment(project.completionDueOn).add(delayInMonths, 'month').toDate()
+    : delayInMonths
+    ? new Date(
+        new Date(project.completionDueOn).setMonth(
+          new Date(project.completionDueOn).getMonth() + delayInMonths
+        )
+      )
+    : null
 
   return (
     <div className="mt-4 mb-4">
-      {dateAchèvementDemandée && (
-        <>
-          <label htmlFor="dateAchèvementAccordée">Date limite d'achèvement accordée</label>
-          <Input
-            type="date"
-            name="dateAchèvementAccordée"
-            id="dateAchèvementAccordée"
-            {...(dateAchèvementDemandée && {
-              defaultValue: format(dateDemandée, 'yyyy-MM-dd'),
-            })}
-            required
-          />
-        </>
-      )}
-      {delayInMonths && (
-        <div className="form__group mt-4 mb-4">
-          <label htmlFor="delayInMonths">Délai accordé (en mois)</label>
-          <input
-            type="number"
-            name="delayInMonths"
-            id="delayInMonths"
-            defaultValue={delayInMonths}
-            data-initial-date={project.completionDueOn}
-            {...dataId('delayInMonthsField')}
-            style={{ width: 75 }}
-          />
-          <span style={{ marginLeft: 10 }} {...dataId('delayEstimateBox')}>
-            {`Date d'achèvement projetée: ${formatDate(
-              +moment(project.completionDueOn).add(delayInMonths, 'month')
-            )}`}
-          </span>
-        </div>
-      )}
+      <label htmlFor="dateAchèvementAccordée">Date limite d'achèvement accordée</label>
+      <Input
+        type="date"
+        name="dateAchèvementAccordée"
+        id="dateAchèvementAccordée"
+        {...(dateDemandée && {
+          defaultValue: format(dateDemandée, 'yyyy-MM-dd'),
+        })}
+        required
+      />
     </div>
   )
 }
