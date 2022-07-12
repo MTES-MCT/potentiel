0640b579745dd7675661354d6877be279a90978d
Julien Pavon <julien.pavon@gmail.com>
2022-07-12 16:40:13 +0200

    ✅ Reecriture des scenarios pour onDelaiDemande

--- src/modules/notification/eventHandlers/demandeDélai/onDélaiDemandé.spec.ts
+++ src/modules/notification/eventHandlers/demandeDélai/onDélaiDemandé.spec.ts
@@ -1,10 +1,148 @@
+import { okAsync } from '@core/utils'
+import { Project, User } from '@entities'
+import { DélaiDemandé } from '@modules/demandeModification'
+import routes from '@routes'
+import { makeOnDélaiDemandé } from './onDélaiDemandé'
+
 describe(`Notifier lorsqu'un délai est demandé`, () => {
   describe(`Notifier le porteur de projet`, () => {
     it(`  Quand un délai est demandé
-          Alors le porteur ayant fait la demande devrait être notifié`, () => {})
+          Alors le porteur ayant fait la demande devrait être notifié`, async () => {
+      const sendNotification = jest.fn()
+      const getInfoForModificationRequested = () =>
+        okAsync({
+          porteurProjet: { email: 'porteur@test.test', fullName: 'Porteur de Projet' },
+          nomProjet: 'nom-du-projet',
+        })
+
+      const onDélaiDemandé = makeOnDélaiDemandé({
+        sendNotification,
+        getInfoForModificationRequested,
+        findUsersForDreal: jest.fn(),
+        findProjectById: jest.fn(),
+      })
+
+      await onDélaiDemandé(
+        new DélaiDemandé({
+          payload: {
+            demandeDélaiId: 'la-demande',
+            projetId: 'le-projet',
+            porteurId: 'le-porteur',
+            autorité: 'dgec',
+            dateAchèvementDemandée: '2022-07-12',
+          },
+        })
+      )
+
+      expect(sendNotification).toHaveBeenCalledTimes(1)
+      expect(sendNotification).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'modification-request-status-update',
+          message: expect.objectContaining({
+            email: 'porteur@test.test',
+            name: 'Porteur de Projet',
+          }),
+          context: expect.objectContaining({
+            modificationRequestId: 'la-demande',
+            userId: 'le-porteur',
+          }),
+          variables: expect.objectContaining({
+            status: 'envoyée',
+            nom_projet: 'nom-du-projet',
+            type_demande: 'delai',
+            document_absent: '',
+          }),
+        })
+      )
+    })
   })
   describe(`Notifier les DREALs`, () => {
     it(`  Quand un délai est demandé à l'autorité DREAL
-          Alors tous les agents de la DREAL de la région di projet devraient être notifiés`, () => {})
+          Alors tous les agents de la DREAL de la région du projet devraient être notifiés`, async () => {
+      const sendNotification = jest.fn()
+      const getInfoForModificationRequested = () =>
+        okAsync({
+          porteurProjet: { email: 'porteur@test.test', fullName: 'Porteur de Projet' },
+          nomProjet: 'nom-du-projet',
+        })
+
+      const findProjectById = (region: string) =>
+        Promise.resolve({
+          id: 'le-projet',
+          nomProjet: 'nom-du-projet',
+          regionProjet: 'regionA / regionB',
+          departementProjet: 'département',
+        } as Project)
+
+      const findUsersForDreal = (region: string) =>
+        Promise.resolve(
+          region === 'regionA'
+            ? [{ email: 'drealA@test.test', fullName: 'drealA' } as User]
+            : [{ email: 'drealB@test.test', fullName: 'drealB' } as User]
+        )
+
+      const onDélaiDemandé = makeOnDélaiDemandé({
+        sendNotification,
+        getInfoForModificationRequested,
+        findUsersForDreal,
+        findProjectById,
+      })
+
+      await onDélaiDemandé(
+        new DélaiDemandé({
+          payload: {
+            demandeDélaiId: 'la-demande',
+            projetId: 'le-projet',
+            porteurId: 'le-porteur',
+            autorité: 'dreal',
+            dateAchèvementDemandée: '2022-07-12',
+          },
+        })
+      )
+
+      expect(sendNotification).toHaveBeenCalledTimes(3)
+      expect(sendNotification).toHaveBeenNthCalledWith(
+        2,
+        expect.objectContaining({
+          type: 'admin-modification-requested',
+          message: expect.objectContaining({
+            email: 'drealA@test.test',
+            name: 'drealA',
+          }),
+          context: expect.objectContaining({
+            modificationRequestId: 'la-demande',
+            dreal: 'regionA',
+            projectId: 'le-projet',
+          }),
+          variables: expect.objectContaining({
+            nom_projet: 'nom-du-projet',
+            modification_request_url: routes.DEMANDE_PAGE_DETAILS('la-demande'),
+            type_demande: 'delai',
+            departement_projet: 'département',
+          }),
+        })
+      )
+      expect(sendNotification).toHaveBeenNthCalledWith(
+        3,
+        expect.objectContaining({
+          type: 'admin-modification-requested',
+          message: expect.objectContaining({
+            email: 'drealB@test.test',
+            name: 'drealB',
+          }),
+          context: expect.objectContaining({
+            modificationRequestId: 'la-demande',
+            dreal: 'regionB',
+            projectId: 'le-projet',
+          }),
+          variables: expect.objectContaining({
+            nom_projet: 'nom-du-projet',
+            modification_request_url: routes.DEMANDE_PAGE_DETAILS('la-demande'),
+            type_demande: 'delai',
+            departement_projet: 'département',
+          }),
+        })
+      )
+    })
   })
 })
--- src/modules/notification/eventHandlers/demandeDélai/onDélaiDemandé.ts
+++ src/modules/notification/eventHandlers/demandeDélai/onDélaiDemandé.ts
@@ -5,22 +5,38 @@ import routes from '@routes'
 import { NotificationService } from '../..'
 import { GetInfoForModificationRequested } from '../../queries'
 
-export const makeOnDélaiDemandé =
-  (deps: {
-    sendNotification: NotificationService['sendNotification']
-    getInfoForModificationRequested: GetInfoForModificationRequested
-    findUsersForDreal: UserRepo['findUsersForDreal']
-    findProjectById: ProjectRepo['findById']
-  }) =>
-  async (event: DélaiDemandé) => {
-    const { demandeDélaiId, projetId: projectId, porteurId: userId, autorité } = event.payload
+type OnDélaiDemandé = (evenement: DélaiDemandé) => Promise<void>
 
-    await deps.getInfoForModificationRequested({ projectId, userId }).match(
+type MakeOnDélaiDemandé = (dépendances: {
+  sendNotification: NotificationService['sendNotification']
+  getInfoForModificationRequested: GetInfoForModificationRequested
+  findUsersForDreal: UserRepo['findUsersForDreal']
+  findProjectById: ProjectRepo['findById']
+}) => OnDélaiDemandé
+
+export const makeOnDélaiDemandé: MakeOnDélaiDemandé =
+  ({ sendNotification, getInfoForModificationRequested, findUsersForDreal, findProjectById }) =>
+  async ({ payload: { demandeDélaiId, projetId: projectId, porteurId: userId, autorité } }) => {
+    await getInfoForModificationRequested({ projectId, userId }).match(
       async ({ nomProjet, porteurProjet: { fullName, email } }) => {
-        await _sendPPUpdateNotification({
-          email,
-          fullName,
-          nomProjet,
+        await sendNotification({
+          type: 'modification-request-status-update',
+          message: {
+            email,
+            name: fullName,
+            subject: `Votre demande de délai pour le projet ${nomProjet}`,
+          },
+          context: {
+            modificationRequestId: demandeDélaiId,
+            userId,
+          },
+          variables: {
+            nom_projet: nomProjet,
+            type_demande: 'delai',
+            status: 'envoyée',
+            modification_request_url: routes.DEMANDE_PAGE_DETAILS(demandeDélaiId),
+            document_absent: '', // injecting an empty string will prevent the default "with document" message to be injected in the email body
+          },
         })
       },
       (e: Error) => {
@@ -28,18 +44,17 @@ export const makeOnDélaiDemandé =
       }
     )
 
-    const project = await deps.findProjectById(projectId)
+    const project = await findProjectById(projectId)
 
     if (project && autorité === 'dreal') {
-      // Send dreal email for each dreal of each region
       const regions = project.regionProjet.split(' / ')
       await Promise.all(
         regions.map(async (region) => {
-          const drealUsers = await deps.findUsersForDreal(region)
+          const drealUsers = await findUsersForDreal(region)
 
           await Promise.all(
             drealUsers.map((drealUser) =>
-              deps.sendNotification({
+              sendNotification({
                 type: 'admin-modification-requested',
                 message: {
                   email: drealUser.email,
@@ -63,32 +78,5 @@ export const makeOnDélaiDemandé =
           )
         })
       )
-    }
-
-    const _sendPPUpdateNotification = (args: {
-      email: string
-      fullName: string
-      nomProjet: string
-    }) => {
-      const { email, fullName, nomProjet } = args
-      return deps.sendNotification({
-        type: 'modification-request-status-update',
-        message: {
-          email,
-          name: fullName,
-          subject: `Votre demande de délai pour le projet ${nomProjet}`,
-        },
-        context: {
-          modificationRequestId: demandeDélaiId,
-          userId,
-        },
-        variables: {
-          nom_projet: nomProjet,
-          type_demande: 'delai',
-          status: 'envoyée',
-          modification_request_url: routes.DEMANDE_PAGE_DETAILS(demandeDélaiId),
-          document_absent: '', // injecting an empty string will prevent the default "with document" message to be injected in the email body
-        },
-      })
     }
   }
