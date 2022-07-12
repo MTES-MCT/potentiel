c5c5e7cc5e2cbd2a12f5695946789a3bfc406850
Julien Pavon <julien.pavon@gmail.com>
2022-07-12 15:27:57 +0200

    🧪 Reecriture des spec pour les notifications des delais demandes

---
+++ src/modules/notification/eventHandlers/demandeDélai/onDélaiDemandé.spec.ts
@@ -0,0 +1,10 @@
+describe(`Notifier lorsqu'un délai est demandé`, () => {
+  describe(`Notifier le porteur de projet`, () => {
+    it(`  Quand un délai est demandé
+          Alors le porteur ayant fait la demande devrait être notifié`, () => {})
+  })
+  describe(`Notifier les DREALs`, () => {
+    it(`  Quand un délai est demandé à l'autorité DREAL
+          Alors tous les agents de la DREAL de la région di projet devraient être notifiés`, () => {})
+  })
+})
