a671f994613c9b186ede69067db3d1ad4721fdd5
HanaeY <hanae.yamashita@lilo.org>
2022-08-08 16:10:55 +0200

    :star2: Mise à jour de ModificationRequestAccepted pour les délais

--- src/infra/sequelize/projectionsNext/projectEvents/updates/onModificationRequestAccepted.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onModificationRequestAccepted.ts
@@ -2,16 +2,20 @@ import { UniqueEntityID } from '@core/domain'
 import { ModificationRequestAccepted } from '@modules/modificationRequest'
 import models from '../../../models'
 import { ProjectEvent } from '../projectEvent.model'
+import { logger } from '@core/utils'
+import { ProjectionEnEchec } from '@modules/shared'
 
 export default ProjectEvent.projector.on(
   ModificationRequestAccepted,
-  async (
-    { payload: { modificationRequestId, responseFileId, params }, occurredAt },
-    transaction
-  ) => {
+  async (événement, transaction) => {
     const { ModificationRequest } = models
     const { File } = models
     let file: {} | undefined = {}
+
+    const {
+      payload: { modificationRequestId, responseFileId, params, acceptedBy },
+      occurredAt,
+    } = événement
 
     if (responseFileId) {
       const rawFilename = await File.findByPk(responseFileId, {
@@ -27,23 +31,82 @@ export default ProjectEvent.projector.on(
       transaction,
     })
 
-    if (projectId) {
-      await ProjectEvent.create(
-        {
-          projectId,
-          type: 'ModificationRequestAccepted',
-          valueDate: occurredAt.getTime(),
-          eventPublishedAt: occurredAt.getTime(),
-          id: new UniqueEntityID().toString(),
-          payload: {
-            modificationRequestId,
-            file,
-            ...(params &&
-              params.type === 'delai' && { delayInMonthsGranted: params.delayInMonths }),
-          },
-        },
-        { transaction }
+    if (!projectId) {
+      logger.error(
+        new ProjectionEnEchec(
+          `Erreur lors du traitement de l'événement ModificationRequestAccepted : modificationRequest non trouvée`,
+          {
+            evenement: événement,
+            nomProjection: 'ProjectEvent.onModificationRequestAccepted',
+          }
+        )
       )
+      return
     }
+
+    if (params?.type === 'delai') {
+      const demandeDélai = await ProjectEvent.findOne({
+        where: { id: modificationRequestId },
+        transaction,
+      })
+
+      if (!demandeDélai) {
+        logger.error(
+          new ProjectionEnEchec(
+            `Erreur lors du traitement de l'événement ModificationRequestAccepted : demande non trouvée`,
+            {
+              evenement: événement,
+              nomProjection: 'ProjectEvent.onModificationRequestAccepted',
+            }
+          )
+        )
+        return
+      }
+
+      try {
+        await ProjectEvent.update(
+          {
+            valueDate: occurredAt.getTime(),
+            eventPublishedAt: occurredAt.getTime(),
+            payload: {
+              //@ts-ignore
+              ...demandeDélai.payload,
+              statut: 'accordée',
+              accordéePar: acceptedBy,
+              délaiEnMoisAccordé: params.delayInMonths,
+              file,
+            },
+          },
+          { where: { id: modificationRequestId }, transaction }
+        )
+      } catch (e) {
+        logger.error(
+          new ProjectionEnEchec(
+            `Erreur lors du traitement de l'événement ModificationRequestAccepted à la mise à jour de l'événement de type DemandeDélai`,
+            {
+              evenement: événement,
+              nomProjection: 'ProjectEvent.onModificationRequestAccepted',
+            },
+            e
+          )
+        )
+      }
+      return
+    }
+
+    await ProjectEvent.create(
+      {
+        projectId,
+        type: 'ModificationRequestAccepted',
+        valueDate: occurredAt.getTime(),
+        eventPublishedAt: occurredAt.getTime(),
+        id: new UniqueEntityID().toString(),
+        payload: {
+          modificationRequestId,
+          file,
+        },
+      },
+      { transaction }
+    )
   }
 )
