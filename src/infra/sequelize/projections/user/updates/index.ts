73cfe3506023b8bc0c03516d40e47ffd20911cf1
HanaeY <hanae.yamashita@lilo.org>
2022-08-12 15:10:40 +0200

    ✨ (Projection Users): Ajout colonne fonction + handlers pour les nouveaux événements

--- src/infra/sequelize/projections/user/updates/index.ts
+++ src/infra/sequelize/projections/user/updates/index.ts
@@ -1,1 +1,3 @@
+export * from './onFonctionUtilisateurModifiée'
+export * from './onRôleUtilisateurModifié'
 export * from './onUserCreated'
---
+++ src/infra/sequelize/projections/user/updates/onRôleUtilisateurModifié.ts
@@ -0,0 +1,9 @@
+import { RôleUtilisateurModifié } from '@modules/users'
+import { userProjector } from '../user.model'
+
+export const onRôleUtilisateurModifié = userProjector
+  .on(RôleUtilisateurModifié)
+  .update({
+    where: ({ payload: { userId } }) => ({ id: userId }),
+    delta: ({ payload: { role } }) => ({ role }),
+  })
