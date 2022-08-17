0cf4e091e762d7b7d891023d7be55749cf2dab09
HanaeY <hanae.yamashita@lilo.org>
2022-08-17 16:46:30 +0200

    🎨 (User projection event handler): Prettier warning

--- src/infra/sequelize/projections/user/updates/onRôleUtilisateurModifié.ts
+++ src/infra/sequelize/projections/user/updates/onRôleUtilisateurModifié.ts
@@ -1,9 +1,7 @@
 import { RôleUtilisateurModifié } from '@modules/users'
 import { userProjector } from '../user.model'
 
-export const onRôleUtilisateurModifié = userProjector
-  .on(RôleUtilisateurModifié)
-  .update({
-    where: ({ payload: { userId } }) => ({ id: userId }),
-    delta: ({ payload: { role } }) => ({ role }),
-  })
+export const onRôleUtilisateurModifié = userProjector.on(RôleUtilisateurModifié).update({
+  where: ({ payload: { userId } }) => ({ id: userId }),
+  delta: ({ payload: { role } }) => ({ role }),
+})
