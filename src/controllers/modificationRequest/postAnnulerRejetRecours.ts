78e13ba861f76b52c1e35151efb1463a51f664f0
HubM <hubmoncenis@gmail.com>
2022-08-29 10:45:02 +0200

    🎨 (1029): ajout de yup pour la validation du body dans le controller postAnnulerRejetRecours

--- src/controllers/modificationRequest/postAnnulerRejetRecours.ts
+++ src/controllers/modificationRequest/postAnnulerRejetRecours.ts
@@ -1,48 +1,53 @@
+import * as yup from 'yup'
+
 import { annulerRejetRecours, ensureRole } from '@config'
 import { logger } from '@core/utils'
 import { EntityNotFoundError, UnauthorizedError } from '@modules/shared'
-import { validateUniqueId } from '../../helpers/validateUniqueId'
 import routes from '../../routes'
 import { errorResponse, notFoundResponse, unauthorizedResponse } from '../helpers'
 import asyncHandler from '../helpers/asyncHandler'
+import { validateRequestBodyForErrorArray } from '../helpers'
 import { v1Router } from '../v1Router'
+
+const requestBodySchema = yup.object({
+  modificationRequestId: yup.string().uuid().required(),
+})
 
 v1Router.post(
   routes.ADMIN_ANNULER_RECOURS_REJETE(),
   ensureRole(['admin', 'dgec-validateur']),
   asyncHandler(async (request, response) => {
-    const {
-      body: { modificationRequestId },
-      user,
-    } = request
-    if (!validateUniqueId(modificationRequestId)) {
-      return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-    }
+    validateRequestBodyForErrorArray(request.body, requestBodySchema)
+      .asyncAndThen((body) => {
+        const { user } = request
+        const { modificationRequestId } = body
 
-    return annulerRejetRecours({
-      user,
-      demandeRecoursId: modificationRequestId,
-    }).match(
-      () => {
-        return response.redirect(
-          routes.SUCCESS_OR_ERROR_PAGE({
-            success: 'La réponse à la demande de recours a bien été annulée.',
-            redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
-            redirectTitle: 'Retourner à la demande',
-          })
-        )
-      },
-      (e) => {
-        if (e instanceof EntityNotFoundError) {
-          return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-        } else if (e instanceof UnauthorizedError) {
-          return unauthorizedResponse({ request, response })
+        return annulerRejetRecours({
+          user,
+          demandeRecoursId: modificationRequestId,
+        }).map(() => ({ modificationRequestId }))
+      })
+      .match(
+        ({ modificationRequestId }) => {
+          return response.redirect(
+            routes.SUCCESS_OR_ERROR_PAGE({
+              success: 'La réponse à la demande de recours a bien été annulée.',
+              redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
+              redirectTitle: 'Retourner à la demande',
+            })
+          )
+        },
+        (e) => {
+          if (e instanceof EntityNotFoundError) {
+            return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
+          } else if (e instanceof UnauthorizedError) {
+            return unauthorizedResponse({ request, response })
+          }
+
+          logger.error(e)
+
+          return errorResponse({ request, response })
         }
-
-        logger.error(e)
-
-        return errorResponse({ request, response })
-      }
-    )
+      )
   })
 )
