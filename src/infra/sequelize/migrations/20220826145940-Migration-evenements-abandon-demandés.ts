7b2071ee696c88015bfdea3b5940598720838c20
HanaeY <hanae.yamashita@lilo.org>
2022-08-29 16:16:51 +0200

    WIP

--- src/infra/sequelize/migrations/20220826145940-Migration-evenements-abandon-demandés.ts
+++ src/infra/sequelize/migrations/20220826145940-Migration-evenements-abandon-demandés.ts
@@ -8,87 +8,75 @@ import onAbandonDemandé from '../projectionsNext/projectEvents/updates/abandon/
 
 export default {
   up: async (queryInterface: QueryInterface, Sequelize: Sequelize) => {
+    console.log('TEST')
     const transaction = await queryInterface.sequelize.transaction()
-
     try {
-      const { ModificationRequest, EventStore } = models
-
-      const demandesAbandonAMigrer: Array<{ id: string }> = await ModificationRequest.findAll(
+      const { EventStore } = models
+      const demandesAbandonAMigrer: ModificationRequested[] = await EventStore.findAll(
         {
           where: {
-            type: 'abandon',
-            status: ['envoyée', 'en instruction'],
+            type: 'ModificationRequested',
+            'payload.type': { [Op.eq]: 'abandon' },
           },
-          attributes: ['id'],
         },
         { transaction }
       )
-
-      console.log(`${demandesAbandonAMigrer.length} demandes d'abandon envoyées à migrer`)
-
+      console.log(
+        `${demandesAbandonAMigrer.length} modification requested de type abandon à migrer`
+      )
       const nouveauxÉvénements = (
         await Promise.all(
           demandesAbandonAMigrer.map(async (demandeAbandonAMigrer) => {
-            const modificationRequestedEvent:
-              | (ModificationRequested & { payload: { type: 'abandon' } })
-              | undefined = await EventStore.findOne(
-              {
-                where: {
-                  aggregateId: { [Op.overlap]: [demandeAbandonAMigrer.id] },
-                  type: 'ModificationRequested',
-                  'payload.type': { [Op.eq]: 'abandon' },
-                },
+            const {
+              payload: { modificationRequestId, requestedBy, fileId, justification, projectId },
+              occurredAt,
+            } = demandeAbandonAMigrer
+            const événementAbandonExistant = await EventStore.findOne({
+              where: {
+                'payload.demandeAbandonId': demandeAbandonAMigrer.id,
+                type: AbandonDemandé.type,
               },
-              { transaction }
-            )
-
-            if (modificationRequestedEvent) {
-              const { payload, occurredAt } = modificationRequestedEvent
-
-              return new AbandonDemandé({
-                payload: {
-                  demandeAbandonId: payload.modificationRequestId,
-                  projetId: payload.projectId,
-                  autorité: 'dgec',
-                  fichierId: payload.fileId,
-                  justification: payload.justification,
-                  porteurId: payload.requestedBy,
-                },
-                original: {
-                  occurredAt,
-                  version: 1,
-                },
-              })
-            }
+            })
+            if (événementAbandonExistant) return
+            return new AbandonDemandé({
+              payload: {
+                demandeAbandonId: modificationRequestId,
+                projetId: projectId,
+                autorité: 'dgec',
+                fichierId: fileId,
+                justification,
+                porteurId: requestedBy,
+              },
+              original: {
+                occurredAt,
+                version: 1,
+              },
+            })
           })
         )
       ).filter((e): e is AbandonDemandé => e?.type === 'AbandonDemandé')
-
       console.log(
         `${nouveauxÉvénements.length} nouveaux événements AbandonDemandé vont être ajoutés`
       )
-
       await EventStore.bulkCreate(nouveauxÉvénements.map(toPersistance), { transaction })
-
       await Promise.all(
         nouveauxÉvénements.map((AbandonDemandé) => onAbandonDemandé(AbandonDemandé, transaction))
       )
-
       const modificationRequestIds = nouveauxÉvénements.map(
         (demande) => demande.payload.demandeAbandonId
       )
-
       await ProjectEvent.destroy({
         where: {
+          type: 'ModificationRequested',
           'payload.modificationRequestId': {
             [Op.in]: modificationRequestIds,
           },
         },
         transaction,
       })
-
       await transaction.commit()
     } catch (error) {
+      console.log('TEST 1')
       await transaction.rollback()
       throw error
     }
