8ac22110bd30e63d3a691cf4fe2bc94e03010771
HubM <hubmoncenis@gmail.com>
2022-08-30 10:08:51 +0200

    🎨 (1048): utilisation de yup pour validation postAnullerDemandeAbandon

--- src/controllers/modificationRequest/demandeAbandon/postAnnulerDemandeAbandon.ts
+++ src/controllers/modificationRequest/demandeAbandon/postAnnulerDemandeAbandon.ts
@@ -1,44 +1,55 @@
-import { annulerDemandeAbandon, annulerDemandeDélai, ensureRole } from '@config'
+import * as yup from 'yup'
+
+import { annulerDemandeAbandon, ensureRole } from '@config'
 import { logger } from '@core/utils'
 import asyncHandler from '../../helpers/asyncHandler'
-import { validateUniqueId } from '../../../helpers/validateUniqueId'
 import { EntityNotFoundError, UnauthorizedError } from '@modules/shared'
 import routes from '../../../routes'
-import { errorResponse, notFoundResponse, unauthorizedResponse } from '../../helpers'
+import {
+  errorResponse,
+  notFoundResponse,
+  unauthorizedResponse,
+  validateRequestBody,
+} from '../../helpers'
 import { v1Router } from '../../v1Router'
+
+const requestBodySchema = yup.object({
+  modificationRequestId: yup.string().uuid().required(),
+})
 
 v1Router.post(
   routes.ANNULER_DEMANDE_ABANDON_ACTION,
   ensureRole(['porteur-projet']),
   asyncHandler(async (request, response) => {
-    const { modificationRequestId } = request.body
-    const user = request.user
-    if (!validateUniqueId(modificationRequestId)) {
-      return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-    }
+    validateRequestBody(request.body, requestBodySchema)
+      .asyncAndThen((body) => {
+        const { user } = request
+        const { modificationRequestId } = body
 
-    return annulerDemandeAbandon({
-      user,
-      demandeAbandonId: modificationRequestId,
-    }).match(
-      () =>
-        response.redirect(
-          routes.SUCCESS_OR_ERROR_PAGE({
-            success: 'Votre demande a bien été annulée.',
-            redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
-            redirectTitle: 'Retourner à la demande',
-          })
-        ),
-      (e) => {
-        if (e instanceof EntityNotFoundError) {
-          return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-        } else if (e instanceof UnauthorizedError) {
-          return unauthorizedResponse({ request, response })
+        return annulerDemandeAbandon({
+          user,
+          demandeAbandonId: modificationRequestId,
+        }).map(() => modificationRequestId)
+      })
+      .match(
+        (modificationRequestId) =>
+          response.redirect(
+            routes.SUCCESS_OR_ERROR_PAGE({
+              success: 'Votre demande a bien été annulée.',
+              redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
+              redirectTitle: 'Retourner à la demande',
+            })
+          ),
+        (e) => {
+          if (e instanceof EntityNotFoundError) {
+            return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
+          } else if (e instanceof UnauthorizedError) {
+            return unauthorizedResponse({ request, response })
+          }
+
+          logger.error(e)
+          return errorResponse({ request, response })
         }
-
-        logger.error(e)
-        return errorResponse({ request, response })
-      }
-    )
+      )
   })
 )
