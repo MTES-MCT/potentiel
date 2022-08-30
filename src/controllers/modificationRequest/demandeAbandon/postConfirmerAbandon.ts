26467a3d0f70cc40a17a3aedfeb9d20d31a45f7c
HubM <hubmoncenis@gmail.com>
2022-08-30 10:20:42 +0200

    🎨 (1048): utilisation de yup pour validation postConfirmerAbandon

--- src/controllers/modificationRequest/demandeAbandon/postConfirmerAbandon.ts
+++ src/controllers/modificationRequest/demandeAbandon/postConfirmerAbandon.ts
@@ -1,58 +1,65 @@
-import { confirmerDemandeAbandon, confirmRequest } from '@config'
+import * as yup from 'yup'
+
+import { confirmerDemandeAbandon } from '@config'
 import { logger } from '@core/utils'
 import asyncHandler from '../../helpers/asyncHandler'
 import { addQueryParams } from '../../../helpers/addQueryParams'
-import {
-  AggregateHasBeenUpdatedSinceError,
-  EntityNotFoundError,
-  UnauthorizedError,
-} from '@modules/shared'
+import { EntityNotFoundError, UnauthorizedError } from '@modules/shared'
 import routes from '@routes'
 import { ensureRole } from '@config'
 import { v1Router } from '../../v1Router'
-import { validateUniqueId } from '../../../helpers/validateUniqueId'
-import { errorResponse, notFoundResponse, unauthorizedResponse } from '../../helpers'
+import {
+  errorResponse,
+  notFoundResponse,
+  unauthorizedResponse,
+  validateRequestBody,
+} from '../../helpers'
 import { ConfirmerDemandeAbandonError } from '../../../modules/demandeModification/demandeAbandon/confirmerAbandon/ConfirmerDemandeAbandonError'
+
+const requestBodySchema = yup.object({
+  demandeAbandonId: yup.string().uuid().required(),
+})
 
 v1Router.post(
   routes.CONFIRMER_DEMANDE_ABANDON,
   ensureRole(['porteur-projet']),
   asyncHandler(async (request, response) => {
-    const { demandeAbandonId } = request.body
+    validateRequestBody(request.body, requestBodySchema)
+      .asyncAndThen((body) => {
+        const { user } = request
+        const { demandeAbandonId } = body
 
-    if (!validateUniqueId(demandeAbandonId)) {
-      return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-    }
-
-    return confirmerDemandeAbandon({
-      demandeAbandonId,
-      confirméPar: request.user,
-    }).match(
-      () => {
-        response.redirect(
-          routes.SUCCESS_OR_ERROR_PAGE({
-            success: 'Votre confirmation a bien été enregistrée.',
-            redirectUrl: routes.DEMANDE_PAGE_DETAILS(demandeAbandonId),
-            redirectTitle: 'Retourner à la demande',
-          })
-        )
-      },
-      (e) => {
-        if (e instanceof ConfirmerDemandeAbandonError) {
-          return response.redirect(
-            addQueryParams(routes.DEMANDE_PAGE_DETAILS(demandeAbandonId), {
-              error: e.message,
+        return confirmerDemandeAbandon({
+          demandeAbandonId,
+          confirméPar: user,
+        }).map(() => demandeAbandonId)
+      })
+      .match(
+        (demandeAbandonId) => {
+          response.redirect(
+            routes.SUCCESS_OR_ERROR_PAGE({
+              success: 'Votre confirmation a bien été enregistrée.',
+              redirectUrl: routes.DEMANDE_PAGE_DETAILS(demandeAbandonId),
+              redirectTitle: 'Retourner à la demande',
             })
           )
-        } else if (e instanceof UnauthorizedError) {
-          return unauthorizedResponse({ request, response })
-        } else if (e instanceof EntityNotFoundError) {
-          return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-        }
+        },
+        (e) => {
+          if (e instanceof ConfirmerDemandeAbandonError) {
+            return response.redirect(
+              addQueryParams(routes.DEMANDE_PAGE_DETAILS(request.body.demandeAbandonId), {
+                error: e.message,
+              })
+            )
+          } else if (e instanceof UnauthorizedError) {
+            return unauthorizedResponse({ request, response })
+          } else if (e instanceof EntityNotFoundError) {
+            return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
+          }
 
-        logger.error(e)
-        return errorResponse({ request, response })
-      }
-    )
+          logger.error(e)
+          return errorResponse({ request, response })
+        }
+      )
   })
 )
