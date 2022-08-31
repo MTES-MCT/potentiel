94214d07711336710ac7d97f2bfbfff3eb148755
Julien Pavon <julien.pavon@gmail.com>
2022-08-31 11:30:49 +0200

    🔨 (DX): Ne configurer l'env de dev que s'il n'y a pas d'environnement de configuré

--- scripts/postInstall.ts
+++ scripts/postInstall.ts
@@ -11,12 +11,14 @@ const executeMigrateScript = process.env.DO_NOT_EXECUTE_MIGRATE ? false : true
 postInstall()
 
 async function postInstall() {
-  if (['', 'local'].includes(NODE_ENV)) {
-    const hooks = spawnSync('npm', ['run', 'configure-hooks'], { stdio: 'inherit' })
-    if (hooks.status && hooks.status > 0) process.exit(hooks.status)
-
+  if (NODE_ENV === '') {
     const devEnv = spawnSync('npm', ['run', 'configure-dev-env-variables'], { stdio: 'inherit' })
     if (devEnv.status && devEnv.status > 0) process.exit(devEnv.status)
+  }
+
+  if (NODE_ENV === 'local') {
+    const hooks = spawnSync('npm', ['run', 'configure-hooks'], { stdio: 'inherit' })
+    if (hooks.status && hooks.status > 0) process.exit(hooks.status)
   }
 
   if (!['', 'local', 'test'].includes(NODE_ENV)) {
