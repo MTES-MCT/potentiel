719fb479645bf92b6fb504585d2f5cb8c2c0136e
flodlc <f.delacomble@gmail.com>
2022-08-31 11:18:06 +0200

    ✨ (1027): notifier les porteurs de projet en cas d'annulation du rejet d'abandon

---
+++ src/modules/notification/eventHandlers/demandeAbandon/onRejetAbandonAnnulé.spec.ts
@@ -0,0 +1,76 @@
+import { okAsync } from '@core/utils'
+import { RejetAbandonAnnulé } from '@modules/demandeModification'
+import { makeOnRejetAbandonAnnulé } from './onRejetAbandonAnnulé'
+
+describe(`Notifier lors de l'annulation du rejet d'une demande d'abandon`, () => {
+  describe(`Notifier les porteurs ayant accès au projet`, () => {
+    it(`Quand un rejet de demande d'abandon est annulé
+        alors tous les porteurs ayant accès au projet devrait être notifiés`, async () => {
+      const notifierPorteurChangementStatutDemande = jest.fn()
+      const getModificationRequestInfoForStatusNotification = () =>
+        okAsync({
+          porteursProjet: [
+            {
+              role: 'porteur-projet',
+              id: 'porteur-1',
+              email: 'porteur1@test.test',
+              fullName: 'Porteur de projet 1',
+            },
+            {
+              role: 'porteur-projet',
+              id: 'porteur-2',
+              email: 'porteur2@test.test',
+              fullName: 'Porteur de projet 2',
+            },
+          ],
+          nomProjet: 'nom-du-projet',
+          regionProjet: 'region',
+          departementProjet: 'departement',
+          type: 'abandon',
+        })
+
+      const onRejetDemandeAbandonAnnulé = makeOnRejetAbandonAnnulé({
+        notifierPorteurChangementStatutDemande,
+        getModificationRequestInfoForStatusNotification,
+      })
+
+      await onRejetDemandeAbandonAnnulé(
+        new RejetAbandonAnnulé({
+          payload: {
+            demandeAbandonId: 'la-demande',
+            projetId: 'le-projet',
+            annuléPar: 'dgec',
+          },
+        })
+      )
+
+      expect(notifierPorteurChangementStatutDemande).toHaveBeenCalledTimes(2)
+      expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
+        1,
+        expect.objectContaining({
+          email: 'porteur1@test.test',
+          status: 'repassée en statut "envoyée"',
+          fullName: 'Porteur de projet 1',
+          porteurId: 'porteur-1',
+          typeDemande: 'abandon',
+          nomProjet: 'nom-du-projet',
+          modificationRequestId: 'la-demande',
+          hasDocument: false,
+        })
+      )
+      expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
+        2,
+        expect.objectContaining({
+          email: 'porteur2@test.test',
+          status: 'repassée en statut "envoyée"',
+          fullName: 'Porteur de projet 2',
+          porteurId: 'porteur-2',
+          typeDemande: 'abandon',
+          nomProjet: 'nom-du-projet',
+          modificationRequestId: 'la-demande',
+          hasDocument: false,
+        })
+      )
+    })
+  })
+})
