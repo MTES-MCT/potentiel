a2bc27f982479ada253198c7a320935953a92a50
HanaeY <72154904+HanaeY@users.noreply.github.com>
2022-09-07 10:21:37 +0200

    ✨ 1101 (Script de migration): Ajout de la fonction d'un nouveau dgec-validateur (#607)
    * ✨ (Script de migration): Ajout de la fonction d'un nouveau dgec-validateur
    
    * 🗃️ (User): Ne pas lever d'erreur si utilisateur inexistant
    
    Co-authored-by: Julien Pavon <julien.pavon@gmail.com>

---
+++ src/infra/sequelize/migrations/20220906153001-Ajouter-fonction-nouveau-validateur.ts
@@ -0,0 +1,68 @@
+'use strict'
+
+import { QueryInterface, Sequelize } from 'sequelize'
+import { FonctionUtilisateurModifiée } from '@modules/users'
+import { logger } from '@core/utils'
+import { toPersistance } from '../helpers'
+import models from '../models'
+
+module.exports = {
+  async up(queryInterface: QueryInterface, Sequelize: Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      const { User, EventStore } = models
+
+      const userId = 'e3b6e352-da66-493a-9bef-9a4bd172bf79'
+
+      // Ajout des événements dans l'EventStore
+      const utilisateurCible = await User.findOne(
+        { where: { id: userId }, attributes: ['email'] },
+        { transaction }
+      )
+
+      if (!utilisateurCible) {
+        logger.error(`L'utilisateur cible avec l'id ${userId} n'a pas été trouvé`)
+      } else {
+        // Mise à jour de la projection Users
+        await User.update(
+          {
+            fonction: 'Sous-directeur du système électrique et des énergies renouvelables',
+          },
+          { where: { id: userId }, transaction }
+        )
+
+        const {
+          dataValues: { email },
+        } = utilisateurCible
+
+        await EventStore.create(
+          toPersistance(
+            new FonctionUtilisateurModifiée({
+              payload: {
+                userId,
+                email,
+                fonction: 'Sous-directeur du système électrique et des énergies renouvelables',
+              },
+            })
+          ),
+          { transaction }
+        )
+      }
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
