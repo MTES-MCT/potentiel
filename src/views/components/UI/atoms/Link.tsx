06c0a7f86ba2d3bb3841c50dd36bc5e0e09a4755
Julien Pavon <julien.pavon@gmail.com>
2022-09-08 15:48:55 +0200

    💄 (UI): Correction de la couleur du texte des liens (#612)
    💄 (UI): Correction des la couleur du texte des liens

--- src/views/components/UI/atoms/Link.tsx
+++ src/views/components/UI/atoms/Link.tsx
@@ -6,6 +6,7 @@ export const Link: FC<LinkProps> = ({ className = '', children, ...props }) => (
   <a
     className={`text-blue-france-sun-base underline hover:text-blue-france-sun-base hover:underline focus:text-blue-france-sun-base focus:underline 
                 ${className}`}
+    style={{ color: '#000091' }} // Style permettant de surcharger les styles css legacy appliqué sur l'élément a
     {...props}
   >
     {children}
