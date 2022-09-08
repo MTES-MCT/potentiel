1de89b2ef5841d83c4bbe1e1a734bc93ba99d70a
HanaeY <hanae.yamashita@lilo.org>
2022-09-08 18:09:48 +0200

    🐛 (Conflit de rebase): Correction après rebase

--- src/views/components/UI/atoms/icons.ts
+++ src/views/components/UI/atoms/icons.ts
@@ -1,8 +1,5 @@
 export { RiFileDownloadLine as FileDownloadIcon } from '@react-icons/all-files/ri/RiFileDownloadLine'
 export { RiFileExcelLine as ExcelFileIcon } from '@react-icons/all-files/ri/RiFileExcelLine'
-<<<<<<< HEAD
 export { FiExternalLink as ExternalLinkIcon } from '@react-icons/all-files/fi/FiExternalLink'
 export { FiChevronLeft as ChevronLeftIcon } from '@react-icons/all-files/fi/FiChevronLeft'
-=======
 export { IoIosWarning as WarningIcon } from '@react-icons/all-files/io/IoIosWarning'
->>>>>>> 💄 (Molecules): Ajout d'une AlertBox
