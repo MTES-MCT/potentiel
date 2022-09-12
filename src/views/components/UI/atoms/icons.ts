1a67488d704af42714f5e0d20f3c30b6d49b3b89
HubM <hubmoncenis@gmail.com>
2022-09-12 16:31:29 +0200

    🎨 (1110): utilisation du bon icon externalLink

--- src/views/components/UI/atoms/icons.ts
+++ src/views/components/UI/atoms/icons.ts
@@ -1,5 +1,5 @@
 export { RiFileDownloadLine as FileDownloadIcon } from '@react-icons/all-files/ri/RiFileDownloadLine'
 export { RiFileExcelLine as ExcelFileIcon } from '@react-icons/all-files/ri/RiFileExcelLine'
-export { FiExternalLink as ExternalLinkIcon } from '@react-icons/all-files/fi/FiExternalLink'
+export { RiExternalLinkLine as ExternalLinkIcon } from '@react-icons/all-files/ri/RiExternalLinkLine'
 export { FiChevronLeft as ChevronLeftIcon } from '@react-icons/all-files/fi/FiChevronLeft'
 export { IoIosWarning as WarningIcon } from '@react-icons/all-files/io/IoIosWarning'
