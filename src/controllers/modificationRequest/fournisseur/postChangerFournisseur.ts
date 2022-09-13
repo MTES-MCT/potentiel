b93efc140764f2ddf8dc0faf3391012913280b1a
HanaeY <hanae.yamashita@lilo.org>
2022-09-13 11:46:09 +0200

    🎨 (Contrôleur): Utilisation du safeAsyncHandler

--- src/controllers/modificationRequest/fournisseur/postChangerFournisseur.ts
+++ src/controllers/modificationRequest/fournisseur/postChangerFournisseur.ts
@@ -1,108 +1,100 @@
 import { ensureRole, requestFournisseurModification, choisirNouveauCahierDesCharges } from '@config'
-import { logger, okAsync } from '@core/utils'
+import { logger } from '@core/utils'
 import { Fournisseur, isFournisseurKind } from '@modules/project'
 import routes from '@routes'
 import fs from 'fs'
-import {
-  errorResponse,
-  RequestValidationErrorArray,
-  unauthorizedResponse,
-  validateRequestBodyForErrorArray,
-} from '../../helpers'
-import asyncHandler from '../../helpers/asyncHandler'
+import { errorResponse, unauthorizedResponse } from '../../helpers'
 import { upload } from '../../upload'
 import { v1Router } from '../../v1Router'
 import * as yup from 'yup'
 import { UniqueEntityID } from '@core/domain'
 import { addQueryParams } from '../../../helpers/addQueryParams'
-import { ChampFournisseurManquantError } from '@modules/shared/errors/ChampFournisseurManquantError'
 import { UnauthorizedError } from '@modules/shared'
+import safeAsyncHandler from 'src/controllers/helpers/safeAsyncHandler'
 
-type RequestBodyType = {
-  projectId: string
-  newEvaluationCarbone: number | undefined
-  justification: string | undefined
-  newRulesOptIn: boolean | undefined
-  'Fournisseur modules ou films': string | undefined
-  'Fournisseur cellules': string | undefined
-  'Fournisseur plaquettes de silicium (wafers)': string | undefined
-  'Fournisseur polysilicium': string | undefined
-  'Fournisseur postes de conversion': string | undefined
-  'Fournisseur structure': string | undefined
-  'Fournisseur dispositifs de stockage de l’énergie': string | undefined
-  'Fournisseur dispositifs de suivi de la course du soleil': string | undefined
-  'Fournisseur autres technologies': string | undefined
-}
-
-const requestBodySchema: yup.SchemaOf<RequestBodyType> = yup
-  .object({
-    projectId: yup.string().uuid().required(),
-    newEvaluationCarbone: yup.number().positive().optional(),
-    justification: yup.string().optional(),
-    newRulesOptIn: yup.boolean().optional(),
-    'Fournisseur modules ou films': yup.string().optional(),
-    'Fournisseur cellules': yup.string().optional(),
-    'Fournisseur plaquettes de silicium (wafers)': yup.string().optional(),
-    'Fournisseur polysilicium': yup.string().optional(),
-    'Fournisseur postes de conversion': yup.string().optional(),
-    'Fournisseur structure': yup.string().optional(),
-    'Fournisseur dispositifs de stockage de l’énergie': yup.string().optional(),
-    'Fournisseur dispositifs de suivi de la course du soleil': yup.string().optional(),
-    'Fournisseur autres technologies': yup.string().optional(),
-  })
-  .test(
-    'vérification-globale-fournisseurs',
-    `Vous devez modifier au moins l'un des fournisseurs.`,
-    function (fields) {
-      return (
-        !!fields['Fournisseur modules ou films'] ||
-        !!fields['Fournisseur cellules'] ||
-        !!fields['Fournisseur plaquettes de silicium (wafers)'] ||
-        !!fields['Fournisseur polysilicium'] ||
-        !!fields['Fournisseur postes de conversion'] ||
-        !!fields['Fournisseur structure'] ||
-        !!fields['Fournisseur dispositifs de stockage de l’énergie'] ||
-        !!fields['Fournisseur dispositifs de suivi de la course du soleil'] ||
-        !!fields['Fournisseur autres technologies']
-      )
-    }
-  )
+const schema = yup.object({
+  body: yup
+    .object({
+      projectId: yup.string().uuid().required(),
+      newEvaluationCarbone: yup
+        .number()
+        .typeError('Le champ Evaluation carbone doit contenir un nombre.')
+        .min(0, 'Le champ Evaluation Carbone doit contenir un nombre strictement positif.')
+        .optional(),
+      justification: yup.string().optional(),
+      newRulesOptIn: yup.boolean().optional(),
+      'Fournisseur modules ou films': yup.string().optional(),
+      'Fournisseur cellules': yup.string().optional(),
+      'Fournisseur plaquettes de silicium (wafers)': yup.string().optional(),
+      'Fournisseur polysilicium': yup.string().optional(),
+      'Fournisseur postes de conversion': yup.string().optional(),
+      'Fournisseur structure': yup.string().optional(),
+      'Fournisseur dispositifs de stockage de l’énergie': yup.string().optional(),
+      'Fournisseur dispositifs de suivi de la course du soleil': yup.string().optional(),
+      'Fournisseur autres technologies': yup.string().optional(),
+    })
+    .test(
+      'vérification-globale-fournisseurs',
+      `Vous devez modifier au moins l'un des fournisseurs.`,
+      function (fields) {
+        return (
+          !!fields['Fournisseur modules ou films'] ||
+          !!fields['Fournisseur cellules'] ||
+          !!fields['Fournisseur plaquettes de silicium (wafers)'] ||
+          !!fields['Fournisseur polysilicium'] ||
+          !!fields['Fournisseur postes de conversion'] ||
+          !!fields['Fournisseur structure'] ||
+          !!fields['Fournisseur dispositifs de stockage de l’énergie'] ||
+          !!fields['Fournisseur dispositifs de suivi de la course du soleil'] ||
+          !!fields['Fournisseur autres technologies']
+        )
+      }
+    ),
+})
 
 v1Router.post(
   routes.CHANGEMENT_FOURNISSEUR_ACTION,
   ensureRole('porteur-projet'),
   upload.single('file'),
-  asyncHandler(async (request, response) => {
-    validateRequestBodyForErrorArray(request.body, requestBodySchema)
-      .asyncAndThen((body) => {
-        const { user } = request
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({ request, response, error }) => {
+        console.log(error)
+        return response.redirect(
+          addQueryParams(routes.CHANGER_FOURNISSEUR(request.body.projectId), {
+            error: `${error.errors.join(' ')}`,
+          })
+        )
+      },
+    },
+    async (request, response) => {
+      const { user } = request
+      const {
+        body: { newRulesOptIn, projectId, newEvaluationCarbone, justification },
+      } = request
 
-        const file = request.file && {
-          contents: fs.createReadStream(request.file.path),
-          filename: `${Date.now()}-${request.file.originalname}`,
-        }
+      const file = request.file && {
+        contents: fs.createReadStream(request.file.path),
+        filename: `${Date.now()}-${request.file.originalname}`,
+      }
 
-        if (body.newRulesOptIn) {
-          return choisirNouveauCahierDesCharges({
-            utilisateur: user,
-            projetId: body.projectId,
-          }).map(() => ({ user, file, ...body }))
-        }
-        return okAsync({ user, file, ...body })
-      })
-      .andThen(({ user, file, ...body }) => {
-        const { projectId, justification, newEvaluationCarbone } = body
-        return requestFournisseurModification({
-          projectId: new UniqueEntityID(projectId),
-          requestedBy: user,
-          newFournisseurs: getFournisseurs(body),
-          newEvaluationCarbone,
-          justification,
-          file,
-        }).map(() => ({ projectId }))
-      })
-      .match(
-        ({ projectId }) => {
+      if (newRulesOptIn) {
+        return choisirNouveauCahierDesCharges({
+          utilisateur: user,
+          projetId: projectId,
+        })
+      }
+
+      return requestFournisseurModification({
+        projectId: new UniqueEntityID(projectId),
+        requestedBy: user,
+        newFournisseurs: getFournisseurs(request.body),
+        newEvaluationCarbone,
+        justification,
+        file,
+      }).match(
+        () => {
           return response.redirect(
             routes.SUCCESS_OR_ERROR_PAGE({
               success: 'Vos changements ont bien été enregistrés.',
@@ -112,22 +104,6 @@ v1Router.post(
           )
         },
         (error) => {
-          if (error instanceof RequestValidationErrorArray) {
-            return response.redirect(
-              addQueryParams(routes.CHANGER_FOURNISSEUR(request.body.projectId), {
-                error: `${error.message} ${error.errors.join(' ')}`,
-              })
-            )
-          }
-
-          if (error instanceof ChampFournisseurManquantError) {
-            return response.redirect(
-              addQueryParams(routes.CHANGER_FOURNISSEUR(request.body.projectId), {
-                error: error.message,
-              })
-            )
-          }
-
           if (error instanceof UnauthorizedError) {
             return unauthorizedResponse({ request, response })
           }
@@ -141,10 +117,11 @@ v1Router.post(
           })
         }
       )
-  })
+    }
+  )
 )
 
-function getFournisseurs(validatedBody: RequestBodyType): Fournisseur[] {
+function getFournisseurs(validatedBody: yup.InferType<typeof schema>['body']): Fournisseur[] {
   let nouveauFournisseurs: Fournisseur[] = []
   for (const [key, value] of Object.entries(validatedBody)) {
     if (typeof value === 'string' && value !== '' && isFournisseurKind(key)) {
