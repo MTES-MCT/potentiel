4b2f4cdf4098d9a87019422e18b7e9862b462da8
HanaeY <hanae.yamashita@lilo.org>
2022-09-14 08:00:07 +0200

    ♻️ (PR follow up): Utilisation d'un tableau CHAMPS_FOURNISSEURS réutilisable pour fournisseurKind

--- src/modules/project/types/fournisseur.ts
+++ src/modules/project/types/fournisseur.ts
@@ -1,26 +1,19 @@
 export type Fournisseur = { kind: FournisseurKind; name: string }
 
-export type FournisseurKind =
-  | 'Fournisseur modules ou films'
-  | 'Fournisseur cellules'
-  | 'Fournisseur plaquettes de silicium (wafers)'
-  | 'Fournisseur polysilicium'
-  | 'Fournisseur postes de conversion'
-  | 'Fournisseur structure'
-  | 'Fournisseur dispositifs de stockage de l’énergie'
-  | 'Fournisseur dispositifs de suivi de la course du soleil'
-  | 'Fournisseur autres technologies'
+export const CHAMPS_FOURNISSEURS: ReadonlyArray<string> = [
+  'Fournisseur modules ou films',
+  'Fournisseur cellules',
+  'Fournisseur plaquettes de silicium (wafers)',
+  'Fournisseur polysilicium',
+  'Fournisseur postes de conversion',
+  'Fournisseur structure',
+  'Fournisseur dispositifs de stockage de l’énergie',
+  'Fournisseur dispositifs de suivi de la course du soleil',
+  'Fournisseur autres technologies',
+]
+
+export type FournisseurKind = typeof CHAMPS_FOURNISSEURS[number]
 
 export const isFournisseurKind = (value: string): value is FournisseurKind => {
-  return [
-    'Fournisseur modules ou films',
-    'Fournisseur cellules',
-    'Fournisseur plaquettes de silicium (wafers)',
-    'Fournisseur polysilicium',
-    'Fournisseur postes de conversion',
-    'Fournisseur structure',
-    'Fournisseur dispositifs de stockage de l’énergie',
-    'Fournisseur dispositifs de suivi de la course du soleil',
-    'Fournisseur autres technologies',
-  ].includes(value)
+  return CHAMPS_FOURNISSEURS.includes(value)
 }
