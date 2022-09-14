871a724f66c5b94ce2dcf667fc4f9de4263c8abf
HubM <hubmoncenis@gmail.com>
2022-09-14 15:31:21 +0200

    🎨 (1115): adapter la page ChangerFournisseur au nouveau système du choix CDC

--- src/views/pages/changerFournisseurPage/ChangerFournisseur.tsx
+++ src/views/pages/changerFournisseurPage/ChangerFournisseur.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Project } from '@entities'
-import ROUTES from '@routes'
+import routes from '@routes'
 import { dataId } from '../../../helpers/testId'
 import { Request } from 'express'
 
 import {
   PageLayout,
-  CDCChoiceForm,
+  ChoisirCahierDesChargesFormulaire,
   UserDashboard,
   ProjectInfo,
   SuccessErrorBox,
   Button,
   Label,
   SecondaryLinkButton,
+  ExternalLink,
+  InfoBox,
 } from '@components'
 import { hydrateOnClient } from '../../helpers'
 import {
@@ -33,9 +35,6 @@ export const ChangerFournisseur = PageLayout(
     const doitChoisirCahierDesCharges =
       project.appelOffre?.choisirNouveauCahierDesCharges &&
       !project.nouvellesRèglesDInstructionChoisies
-    const [newRulesOptInSelectionné, setNewRulesOptInSelectionné] = useState(
-      project.nouvellesRèglesDInstructionChoisies
-    )
 
     return (
       <UserDashboard currentPage={'list-requests'}>
@@ -44,116 +43,117 @@ export const ChangerFournisseur = PageLayout(
             <h3>Je signale un changement de fournisseur</h3>
           </div>
 
-          <form
-            action={ROUTES.CHANGEMENT_FOURNISSEUR_ACTION}
-            method="post"
-            encType="multipart/form-data"
-          >
-            <input type="hidden" name="projectId" value={project.id} />
-            <div className="form__group">
-              <div className="mb-2">Concernant le projet:</div>
-              <ProjectInfo project={project} className="mb-3"></ProjectInfo>
-              <SuccessErrorBox success={success} error={error} />
-              {doitChoisirCahierDesCharges && (
-                <div>
-                  <Label required>
-                    <strong>
-                      Veuillez saisir les modalités d'instruction à appliquer à ce changement
-                    </strong>
-                  </Label>
-                  <CDCChoiceForm
-                    nouvellesRèglesDInstructionChoisies={
-                      project.nouvellesRèglesDInstructionChoisies
-                    }
-                    cahiersChargesURLs={cahiersChargesURLs}
-                    onChoiceChange={(isNewRule: boolean) => {
-                      setNewRulesOptInSelectionné(isNewRule)
-                    }}
-                  />
-                </div>
-              )}
-
-              {(newRulesOptInSelectionné || !doitChoisirCahierDesCharges) && (
-                <>
-                  {CHAMPS_FOURNISSEURS.map((champ) => {
-                    return (
-                      <div>
-                        <h3 style={{ marginTop: 15, marginBottom: 3 }}>{champ}</h3>
-                        <label>Ancien fournisseur</label>
-                        <input
-                          type="text"
-                          disabled
-                          defaultValue={
-                            project.details?.[
-                              CORRESPONDANCE_CHAMPS_FOURNISSEURS_COLONNE_IMPORT[champ]
-                            ]
-                          }
-                        />
-                        <label htmlFor={champ} className="mt-2">
-                          Nouveau fournisseur
-                        </label>
-                        <input type="text" name={champ} id={champ} />
-                      </div>
-                    )
-                  })}
-                  {project.evaluationCarbone > 0 && (
-                    <div>
-                      <h3 style={{ marginTop: 15, marginBottom: 3 }}>évaluation carbone</h3>
-                      <label>Ancienne évaluation carbone (kg eq CO2/kWc)</label>
-                      <input
-                        type="number"
-                        disabled
-                        defaultValue={project.evaluationCarbone}
-                        {...dataId('modificationRequest-oldEvaluationCarboneField')}
-                      />
-                      <label htmlFor="evaluationCarbone">
-                        Nouvelle évaluation carbone (kg eq CO2/kWc)
-                      </label>
-                      <input
-                        type="number"
-                        name="evaluationCarbone"
-                        id="evaluationCarbone"
-                        {...dataId('modificationRequest-evaluationCarboneField')}
-                      />
-                    </div>
-                  )}
-                  <label htmlFor="candidats" className="mt-6">
-                    Pièce-jointe
-                  </label>
-                  <input
-                    type="file"
-                    name="file"
-                    {...dataId('modificationRequest-fileField')}
-                    id="file"
-                  />
-                  <Label htmlFor="justification" className="mt-4">
-                    <strong>Veuillez nous indiquer les raisons qui motivent votre demande</strong>
-                    <br />
-                    Pour faciliter le traitement de votre demande, veillez à détailler les raisons
-                    ayant conduit à ce besoin de modification (contexte, facteurs extérieurs, etc)
-                  </Label>
-                  <textarea
-                    name="justification"
-                    id="justification"
-                    defaultValue={justification || ''}
-                    {...dataId('modificationRequest-justificationField')}
-                  />
-
-                  <Button
-                    className="mt-3 mr-1"
-                    type="submit"
-                    id="submit"
-                    {...dataId('submit-button')}
-                  >
-                    Envoyer
-                  </Button>
-                  <SecondaryLinkButton href={ROUTES.USER_LIST_PROJECTS}>
-                    Annuler
-                  </SecondaryLinkButton>
-                </>
-              )}
+          {doitChoisirCahierDesCharges ? (
+            <div>
+              <InfoBox
+                title="Afin d'accéder au formulaire de changement de fournisseur, vous devez d'abord changer le
+                  cahier des charges à appliquer"
+                className="mb-5"
+              >
+                <p className="m-0">
+                  Pour plus d'informations sur les modalités d'instruction veuillez consulter cette
+                  &nbsp;
+                  <ExternalLink href="https://docs.potentiel.beta.gouv.fr/info/guide-dutilisation-potentiel/comment-faire-une-demande-de-modification-ou-informer-le-prefet-dun-changement">
+                    page d'aide
+                  </ExternalLink>
+                  .
+                </p>
+              </InfoBox>
+              <ChoisirCahierDesChargesFormulaire
+                cahiersChargesURLs={cahiersChargesURLs}
+                projet={project}
+                redirectUrl={routes.CHANGER_FOURNISSEUR(project.id)}
+              />
             </div>
-          </form>
+          ) : (
+            <form
+              action={routes.CHANGEMENT_FOURNISSEUR_ACTION}
+              method="post"
+              encType="multipart/form-data"
+            >
+              <input type="hidden" name="projectId" value={project.id} />
+              <input type="hidden" name="type" value="fournisseur" />
+              <div className="form__group">
+                <div className="mb-2">Concernant le projet:</div>
+                <ProjectInfo project={project} className="mb-3"></ProjectInfo>
+                <SuccessErrorBox success={success} error={error} />
+
+                {CHAMPS_FOURNISSEURS.map((champ) => {
+                  return (
+                    <div>
+                      <h3 style={{ marginTop: 15, marginBottom: 3 }}>{champ}</h3>
+                      <label>Ancien fournisseur</label>
+                      <input
+                        type="text"
+                        disabled
+                        defaultValue={
+                          project.details?.[
+                            CORRESPONDANCE_CHAMPS_FOURNISSEURS_COLONNE_IMPORT[champ]
+                          ]
+                        }
+                      />
+                      <label htmlFor={champ} className="mt-2">
+                        Nouveau fournisseur
+                      </label>
+                      <input type="text" name={champ} id={champ} />
+                    </div>
+                  )
+                })}
+                {project.evaluationCarbone > 0 && (
+                  <div>
+                    <h3 style={{ marginTop: 15, marginBottom: 3 }}>évaluation carbone</h3>
+                    <label>Ancienne évaluation carbone (kg eq CO2/kWc)</label>
+                    <input
+                      type="number"
+                      disabled
+                      defaultValue={project.evaluationCarbone}
+                      {...dataId('modificationRequest-oldEvaluationCarboneField')}
+                    />
+                    <label htmlFor="evaluationCarbone">
+                      Nouvelle évaluation carbone (kg eq CO2/kWc)
+                    </label>
+                    <input
+                      type="number"
+                      name="evaluationCarbone"
+                      id="evaluationCarbone"
+                      {...dataId('modificationRequest-evaluationCarboneField')}
+                    />
+                  </div>
+                )}
+                <label htmlFor="candidats" className="mt-6">
+                  Pièce-jointe
+                </label>
+                <input
+                  type="file"
+                  name="file"
+                  {...dataId('modificationRequest-fileField')}
+                  id="file"
+                />
+                <Label htmlFor="justification" className="mt-4">
+                  <strong>Veuillez nous indiquer les raisons qui motivent votre demande</strong>
+                  <br />
+                  Pour faciliter le traitement de votre demande, veillez à détailler les raisons
+                  ayant conduit à ce besoin de modification (contexte, facteurs extérieurs, etc)
+                </Label>
+                <textarea
+                  name="justification"
+                  id="justification"
+                  defaultValue={justification || ''}
+                  {...dataId('modificationRequest-justificationField')}
+                />
+
+                <Button
+                  className="mt-3 mr-1"
+                  type="submit"
+                  id="submit"
+                  {...dataId('submit-button')}
+                >
+                  Envoyer
+                </Button>
+                <SecondaryLinkButton href={routes.USER_LIST_PROJECTS}>Annuler</SecondaryLinkButton>
+              </div>
+            </form>
+          )}
         </div>
       </UserDashboard>
     )
