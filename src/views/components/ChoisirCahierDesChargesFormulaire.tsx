d3fe2c43fb52c04c1a417def53b1e227f45982dc
HanaeY <72154904+HanaeY@users.noreply.github.com>
2022-09-19 17:53:24 +0200

    💄 1124 (Formulaire CDC): Mise en avant du CDC choisi (#630)
    💄 (Formulaire CDC): Mise en avant du CDC choisi

--- src/views/components/ChoisirCahierDesChargesFormulaire.tsx
+++ src/views/components/ChoisirCahierDesChargesFormulaire.tsx
@@ -24,47 +24,45 @@ const CahierDesChargesInitial: React.FC<CahierDesChargesInitialProps> = ({
   appelOffre,
 }) => {
   return (
-    <div className={'border border-gray-400 border-solid rounded p-5 mb-5'}>
-      <div className="inline-radio-option">
-        <input
-          type="radio"
-          name="choixCDC"
-          value="initial"
-          id="Anciennes règles"
-          disabled={true}
-          defaultChecked={cahierDesChargesActuel === 'initial'}
-        />
-        <label htmlFor="Anciennes règles" className="flex-1">
-          <span className="font-bold">
-            Instruction selon les dispositions du cahier des charges en vigueur au moment de la
-            candidature &nbsp;
-          </span>
-          {appelOffre.periode.cahierDesCharges.url && (
-            <>
-              {'('}
-              <ExternalLink href={appelOffre.periode.cahierDesCharges.url}>
-                voir le cahier des charges
-              </ExternalLink>
-              {')'}
-            </>
-          )}
-          .
-        </label>
-      </div>
-      {appelOffre.choisirNouveauCahierDesCharges && (
-        <div>
-          <ul>
-            <li style={{ listStyleImage: 'URL(/images/icons/external/arrow-right.svg)' }}>
-              Je dois envoyer ma demande ou mon signalement au format papier.
-            </li>
-            <li style={{ listStyleImage: 'URL(/images/icons/external/arrow-right.svg)' }}>
+    <li className="inline-radio-option relative">
+      <input
+        type="radio"
+        name="choixCDC"
+        value="initial"
+        id="Anciennes règles"
+        disabled={true}
+        defaultChecked={cahierDesChargesActuel === 'initial'}
+        className="peer absolute left-4"
+      />
+      <label
+        htmlFor="Anciennes règles"
+        className="flex-1 border border-gray-400 border-solid rounded p-5 mb-5 pl-10 peer-checked:border-2 peer-checked:border-blue-france-main-525-base hover:cursor-pointer peer-checked:bg-blue-france-975-base peer-disabled:cursor-not-allowed"
+      >
+        <span className="font-bold">
+          Instruction selon les dispositions du cahier des charges en vigueur au moment de la
+          candidature &nbsp;
+        </span>
+        {appelOffre.periode.cahierDesCharges.url && (
+          <>
+            {'('}
+            <ExternalLink href={appelOffre.periode.cahierDesCharges.url}>
+              voir le cahier des charges
+            </ExternalLink>
+            {')'}
+          </>
+        )}
+        .
+        {appelOffre.choisirNouveauCahierDesCharges && (
+          <ul className="mt-2 list-none md:list-disc p-1 md:pl-10">
+            <li>Je dois envoyer ma demande ou mon signalement au format papier.</li>
+            <li>
               Je pourrai changer de mode d'instruction lors de ma prochaine demande si je le
               souhaite.
             </li>
           </ul>
-        </div>
-      )}
-    </div>
+        )}
+      </label>
+    </li>
   )
 }
 
@@ -83,41 +81,37 @@ const CahierDesChargesModifiéDisponible: React.FC<CahierDesChargesModifiéDispo
   const coché = cahierDesChargesActuel === getIdCahierDesCharges(cdc)
 
   return (
-    <div className={'border border-gray-400 border-solid rounded p-5 mb-5'}>
-      <div className="inline-radio-option">
-        <input
-          type="radio"
-          name="choixCDC"
-          value={idCdc}
-          id="Nouvelles règles"
-          defaultChecked={coché}
-          disabled={coché}
-          onChange={() => onCahierDesChargesChoisi(idCdc)}
-        />
-
-        <label htmlFor="Nouvelles règles" className="flex-1">
-          <span className="font-bold">
-            Instruction selon le cahier des charges modifié rétroactivement et publié le{' '}
-            {cdc.paruLe}{' '}
-          </span>
-          {'('}
-          <ExternalLink href={cdc.url}>voir le cahier des charges</ExternalLink>
-          {')'}.
-        </label>
-      </div>
-      <div>
-        <ul>
-          <li style={{ listStyleImage: 'URL(/images/icons/external/arrow-right.svg)' }}>
-            Ce choix s'appliquera à toutes les futures demandes faites sous Potentiel.
-          </li>
-          <li style={{ listStyleImage: 'URL(/images/icons/external/arrow-right.svg)' }}>
+    <li className="inline-radio-option relative">
+      <input
+        type="radio"
+        name="choixCDC"
+        value={idCdc}
+        id="Nouvelles règles"
+        defaultChecked={coché}
+        disabled={coché}
+        onChange={() => onCahierDesChargesChoisi(idCdc)}
+        className="peer absolute left-4"
+      />
+      <label
+        htmlFor="Nouvelles règles"
+        className="flex-1 border border-gray-400 border-solid rounded p-5 mb-5 pl-10 peer-checked:border-2 peer-checked:border-blue-france-main-525-base peer-checked:bg-blue-france-975-base hover:cursor-pointer peer-disabled:cursor-not-allowed"
+      >
+        <span className="font-bold">
+          Instruction selon le cahier des charges modifié rétroactivement et publié le {cdc.paruLe}{' '}
+        </span>
+        {'('}
+        <ExternalLink href={cdc.url}>voir le cahier des charges</ExternalLink>
+        {')'}.
+        <ul className="mt-2 list-none p-1 md:list-disc md:pl-10">
+          <li>Ce choix s'appliquera à toutes les futures demandes faites sous Potentiel.</li>
+          <li>
             Une modification ultérieure pourra toujours être instruite selon le cahier des charges
             en vigueur au moment du dépôt de l'offre, à condition qu'elle soit soumise au format
             papier en précisant ce choix.
           </li>
         </ul>
-      </div>
-    </div>
+      </label>
+    </li>
   )
 }
 
@@ -135,18 +129,20 @@ export const ChoisirCahierDesChargesFormulaire = ({
       <input type="hidden" name="projectId" value={id} />
       {type && <input type="hidden" name="type" value={type} />}
 
-      <CahierDesChargesInitial {...{ cahierDesChargesActuel, appelOffre }} />
+      <ul className="list-none pl-0">
+        <CahierDesChargesInitial {...{ cahierDesChargesActuel, appelOffre }} />
 
-      {appelOffre.cahiersDesChargesModifiésDisponibles.map((cdc) => (
-        <CahierDesChargesModifiéDisponible
-          {...{
-            key: getIdCahierDesCharges(cdc),
-            cdc,
-            cahierDesChargesActuel,
-            onCahierDesChargesChoisi: () => pouvoirEnregistrerLeChangement(true),
-          }}
-        />
-      ))}
+        {appelOffre.cahiersDesChargesModifiésDisponibles.map((cdc) => (
+          <CahierDesChargesModifiéDisponible
+            {...{
+              key: getIdCahierDesCharges(cdc),
+              cdc,
+              cahierDesChargesActuel,
+              onCahierDesChargesChoisi: () => pouvoirEnregistrerLeChangement(true),
+            }}
+          />
+        ))}
+      </ul>
 
       <div className="flex items-center justify-center">
         <Button type="submit" disabled={!peutEnregistrerLeChangement}>
