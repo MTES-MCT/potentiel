3c40532392553082852c1b3cad5853c174be342b
Hubert Moncenis <hubmoncenis@gmail.com>
2022-09-19 12:03:28 +0200

    🚧 (1073): création d'une documentation MDX pour les icones de l'app (#605)
    * 🚧 (1073): création d'une documentation MDX pour les icones de l'app
    
    * ✨ (1073): rollback maj de dépendances + ajout d'une story qui présente l'iconography dynamiquement
    
    * ⏪️ (1073): revert package.json dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
-  stories: ['../src/views/**/*.stories.tsx'],
+  stories: ['../src/views/**/*.stories.(tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5',
