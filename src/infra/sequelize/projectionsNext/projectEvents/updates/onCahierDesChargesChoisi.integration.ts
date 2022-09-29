bbfd160cb84f7863f06545d5eed312f70ec2ca1f
HubM <hubmoncenis@gmail.com>
2022-09-29 10:32:29 +0200

    🎨 (1148): ajout d'un handler onCahierDesChargesChoisi pour la mise à jour de la frise projet

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onCahierDesChargesChoisi.integration.ts
@@ -0,0 +1,73 @@
+import { UniqueEntityID } from '@core/domain'
+import { CahierDesChargesChoisi, CahierDesChargesChoisiPayload } from '@modules/project'
+import { resetDatabase } from '../../../helpers'
+import { ProjectEvent } from '../projectEvent.model'
+import onCahierDesChargesChoisi from './onCahierDesChargesChoisi'
+
+describe('Projecteur de ProjectEvent onCahierDesChargesChoisi', () => {
+  describe(`Étant donné des évènements de type "CahierDesChargesChoisi" pour un changement de cahier des charges`, () => {
+    describe(`Lorsqu'on émet un évènement CahierDesChargesChoisi pour un CDC`, () => {
+      beforeEach(async () => {
+        await resetDatabase()
+      })
+      it(`Si le CDC choisi est de type initial
+          Alors le CDC du projet devrait être initial`, async () => {
+        const projectId = new UniqueEntityID().toString()
+        const occurredAt = new Date('2022-09-29')
+        await onCahierDesChargesChoisi(
+          new CahierDesChargesChoisi({
+            payload: {
+              projetId: projectId,
+              choisiPar: 'utilisateur',
+              type: 'initial',
+            } as CahierDesChargesChoisiPayload,
+            original: {
+              version: 1,
+              occurredAt,
+            },
+          })
+        )
+
+        const projectEvent = await ProjectEvent.findOne({ where: { projectId } })
+
+        expect(projectEvent).not.toBeNull()
+        expect(projectEvent).toMatchObject({
+          type: 'CahierDesChargesChoisi',
+          valueDate: occurredAt.getTime(),
+          eventPublishedAt: occurredAt.getTime(),
+          payload: { choisiPar: 'utilisateur' },
+        })
+      })
+
+      it(`Si le CDC choisi est de type modifié
+          Alors le CDC du projet devrait être modifié`, async () => {
+        const projectId = new UniqueEntityID().toString()
+        const occurredAt = new Date('2022-09-29')
+        await onCahierDesChargesChoisi(
+          new CahierDesChargesChoisi({
+            payload: {
+              projetId: projectId,
+              choisiPar: 'utilisateur',
+              type: 'modifié',
+              paruLe: '30/07/2021',
+            } as CahierDesChargesChoisiPayload,
+            original: {
+              version: 1,
+              occurredAt,
+            },
+          })
+        )
+
+        const projectEvent = await ProjectEvent.findOne({ where: { projectId } })
+
+        expect(projectEvent).not.toBeNull()
+        expect(projectEvent).toMatchObject({
+          type: 'CahierDesChargesChoisi',
+          valueDate: occurredAt.getTime(),
+          eventPublishedAt: occurredAt.getTime(),
+          payload: { choisiPar: 'utilisateur', paruLe: '30/07/2021' },
+        })
+      })
+    })
+  })
+})
