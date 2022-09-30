6bc16f39342f9160fdbdfe67b35c06e9d1b14126
Julien Pavon <julien.pavon@gmail.com>
2022-09-30 15:18:08 +0200

    💄 (UI): Ajout du menu PP dans le header (manque page active)

--- src/views/components/PageLayout.tsx
+++ src/views/components/PageLayout.tsx
@@ -2,6 +2,7 @@ import type { Request } from 'express'
 import React from 'react'
 import { Footer } from './Footer'
 import { Header } from './Header'
+import { HeaderForPP } from './UI'
 
 interface HasRequest {
   request: Request
@@ -10,10 +11,34 @@ interface HasRequest {
 export const PageLayout =
   <T extends HasRequest>(Component: (props: T) => JSX.Element) =>
   (props: T) => {
+    const {
+      request: { user },
+    } = props
     return (
       <>
-        <Header {...{ user: props.request.user }} />
-        <Component {...props} />
+        {user.role === 'porteur-projet' ? (
+          <HeaderForPP
+            {...{
+              user: { ...user, role: 'porteur-projet' },
+              currentPage: '',
+            }}
+          />
+        ) : (
+          <Header {...{ user: props.request.user }} />
+        )}
+
+        {user.role === 'porteur-projet' ? (
+          <main
+            role="main"
+            className="flex flex-col py-6 xl:pt-12 xl:mx-auto xl:max-w-7xl"
+            style={{ fontFamily: 'Marianne, arial, sans-serif' }}
+          >
+            <Component {...props} />
+          </main>
+        ) : (
+          <Component {...props} />
+        )}
+
         <Footer />
       </>
     )
