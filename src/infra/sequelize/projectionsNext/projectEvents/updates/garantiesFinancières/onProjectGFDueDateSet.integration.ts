7ef76627f388c19c8425f4b7cad008853a9afaf7
HanaeY <hanae.yamashita@lilo.org>
2022-10-14 17:01:46 +0200

    ✅ (onProjectGFDueDateSet pour ProjectEvent): Mise à jour des tests d'intégration (et notamment pour vérifier que le fichier est conservé en cas de mise à jour de la data due)

--- src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFDueDateSet.integration.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFDueDateSet.integration.ts
@@ -1,43 +1,210 @@
 import { UniqueEntityID } from '@core/domain'
 import { ProjectGFDueDateSet, ProjectGFDueDateSetPayload } from '@modules/project'
 import { resetDatabase } from '../../../../helpers'
+import { GarantiesFinancièresDueEventPayload } from '../../events/GarantiesFinancièresEvent'
 import { ProjectEvent } from '../../projectEvent.model'
 import onProjectGFDueDateSet from './onProjectGFDueDateSet'
 
 describe('onProjectGFDueDateSet', () => {
   const projectId = new UniqueEntityID().toString()
-  const garantiesFinancieresDueOn = new Date('2022-01-27').getTime()
+  const garantiesFinancieresDuesLe = new Date('2022-01-27').getTime()
   const occurredAt = new Date('2021-11-27')
+  const fichier = { id: 'id', name: 'name' }
 
   beforeEach(async () => {
     await resetDatabase()
   })
 
-  it('should create a new project event of type ProjectGFDueDateSet', async () => {
-    await onProjectGFDueDateSet(
-      new ProjectGFDueDateSet({
-        payload: {
-          projectId,
-          garantiesFinancieresDueOn,
-        } as ProjectGFDueDateSetPayload,
-        original: {
-          version: 1,
-          occurredAt,
-        },
-      })
-    )
+  describe(`Pas d'élément de type GF correspondant dans ProjectEvent`, () => {
+    it(`Si aucun élément de type GF n'est trouvé dans ProjectEvent,
+    alors un nouvel élément devrait être créé`, async () => {
+      await onProjectGFDueDateSet(
+        new ProjectGFDueDateSet({
+          payload: {
+            projectId,
+            garantiesFinancieresDueOn: garantiesFinancieresDuesLe,
+          } as ProjectGFDueDateSetPayload,
+          original: {
+            version: 1,
+            occurredAt,
+          },
+        })
+      )
 
-    const projectEvent = await ProjectEvent.findOne({
-      where: { projectId, type: 'GarantiesFinancières' },
+      const élémentGFFinal = await ProjectEvent.findOne({
+        where: { projectId, type: 'GarantiesFinancières' },
+      })
+
+      expect(élémentGFFinal).toMatchObject({
+        type: 'GarantiesFinancières',
+        eventPublishedAt: occurredAt.getTime(),
+        payload: { dateLimiteDEnvoi: garantiesFinancieresDuesLe },
+      })
+    })
+  })
+  describe(`Elément de type GF correspondant dans ProjectEvent`, () => {
+    it(`Etant donné un élément de statut 'due' sans date limite d'envoi,
+    alors la date limite d'envoi devrait être ajoutée`, async () => {
+      const élémentId = new UniqueEntityID().toString()
+      const élémentGFInitial = {
+        id: élémentId,
+        type: 'GarantiesFinancières',
+        projectId,
+        valueDate: 123,
+        eventPublishedAt: 123,
+        payload: {
+          statut: 'due',
+        } as GarantiesFinancièresDueEventPayload,
+      }
+      await ProjectEvent.create(élémentGFInitial)
+
+      await onProjectGFDueDateSet(
+        new ProjectGFDueDateSet({
+          payload: {
+            projectId,
+            garantiesFinancieresDueOn: garantiesFinancieresDuesLe,
+          } as ProjectGFDueDateSetPayload,
+          original: {
+            version: 1,
+            occurredAt,
+          },
+        })
+      )
+
+      const élémentGFFinal = await ProjectEvent.findOne({
+        where: { projectId, type: 'GarantiesFinancières', id: élémentId },
+      })
+
+      expect(élémentGFFinal).toMatchObject({
+        type: 'GarantiesFinancières',
+        eventPublishedAt: occurredAt.getTime(),
+        payload: { dateLimiteDEnvoi: garantiesFinancieresDuesLe },
+      })
     })
 
-    expect(projectEvent).not.toBeNull()
-    expect(projectEvent).toMatchObject({
-      projectId,
-      type: 'GarantiesFinancières',
-      eventPublishedAt: occurredAt.getTime(),
-      valueDate: garantiesFinancieresDueOn,
-      payload: { statut: 'due' },
+    it(`Etant donné un élément de statut 'due' avec date limite d'envoi,
+    alors la date limite d'envoi devrait être mise à jour`, async () => {
+      const élémentId = new UniqueEntityID().toString()
+      const élémentGFInitial = {
+        id: élémentId,
+        type: 'GarantiesFinancières',
+        projectId,
+        valueDate: 123,
+        eventPublishedAt: 123,
+        payload: {
+          statut: 'due',
+          dateLimiteDEnvoi: 456,
+        } as GarantiesFinancièresDueEventPayload,
+      }
+      await ProjectEvent.create(élémentGFInitial)
+
+      await onProjectGFDueDateSet(
+        new ProjectGFDueDateSet({
+          payload: {
+            projectId,
+            garantiesFinancieresDueOn: garantiesFinancieresDuesLe,
+          } as ProjectGFDueDateSetPayload,
+          original: {
+            version: 1,
+            occurredAt,
+          },
+        })
+      )
+
+      const élémentGFFinal = await ProjectEvent.findOne({
+        where: { projectId, type: 'GarantiesFinancières', id: élémentId },
+      })
+
+      expect(élémentGFFinal).toMatchObject({
+        type: 'GarantiesFinancières',
+        eventPublishedAt: occurredAt.getTime(),
+        payload: { dateLimiteDEnvoi: garantiesFinancieresDuesLe },
+      })
+    })
+
+    for (const statut of ['uploaded', 'validated', 'pending-validation']) {
+      it(`Etant donné un élément de statut '${statut}' avec date limite d'envoi,
+      alors la date limite d'envoi devrait être mise à jour 
+      et le fichier conservé`, async () => {
+        const élémentId = new UniqueEntityID().toString()
+        const élémentGFInitial = {
+          id: élémentId,
+          type: 'GarantiesFinancières',
+          projectId,
+          valueDate: 123,
+          eventPublishedAt: 123,
+          payload: {
+            statut,
+            dateLimiteDEnvoi: 456,
+            fichier,
+          },
+        }
+        await ProjectEvent.create(élémentGFInitial)
+
+        await onProjectGFDueDateSet(
+          new ProjectGFDueDateSet({
+            payload: {
+              projectId,
+              garantiesFinancieresDueOn: garantiesFinancieresDuesLe,
+            } as ProjectGFDueDateSetPayload,
+            original: {
+              version: 1,
+              occurredAt,
+            },
+          })
+        )
+
+        const élémentGFFinal = await ProjectEvent.findOne({
+          where: { projectId, type: 'GarantiesFinancières', id: élémentId },
+        })
+
+        expect(élémentGFFinal).toMatchObject({
+          type: 'GarantiesFinancières',
+          eventPublishedAt: occurredAt.getTime(),
+          payload: { dateLimiteDEnvoi: garantiesFinancieresDuesLe, fichier },
+        })
+      })
+    }
+
+    it(`Etant donné un élément de statut 'uploaded' sans date limite d'envoi,
+    alors la date limite d'envoi devrait être ajoutée 
+    et le fichier conservé`, async () => {
+      const élémentId = new UniqueEntityID().toString()
+      const élémentGFInitial = {
+        id: élémentId,
+        type: 'GarantiesFinancières',
+        projectId,
+        valueDate: 123,
+        eventPublishedAt: 123,
+        payload: {
+          statut: 'uploaded',
+          fichier,
+        },
+      }
+      await ProjectEvent.create(élémentGFInitial)
+
+      await onProjectGFDueDateSet(
+        new ProjectGFDueDateSet({
+          payload: {
+            projectId,
+            garantiesFinancieresDueOn: garantiesFinancieresDuesLe,
+          } as ProjectGFDueDateSetPayload,
+          original: {
+            version: 1,
+            occurredAt,
+          },
+        })
+      )
+
+      const élémentGFFinal = await ProjectEvent.findOne({
+        where: { projectId, type: 'GarantiesFinancières', id: élémentId },
+      })
+
+      expect(élémentGFFinal).toMatchObject({
+        type: 'GarantiesFinancières',
+        eventPublishedAt: occurredAt.getTime(),
+        payload: { dateLimiteDEnvoi: garantiesFinancieresDuesLe, fichier },
+      })
     })
   })
 })
