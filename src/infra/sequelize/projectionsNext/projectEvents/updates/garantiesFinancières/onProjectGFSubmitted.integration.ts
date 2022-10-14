a39a53f0fc9a4ff065dd734f5c4681f93a030405
HanaeY <hanae.yamashita@lilo.org>
2022-10-14 17:20:33 +0200

    ✅ (onProjectGFSubmitted ProjectEvent): Mise à jour du scenario testé

--- src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFSubmitted.integration.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFSubmitted.integration.ts
@@ -1,11 +1,10 @@
 import { UniqueEntityID } from '@core/domain'
 import { ProjectGFSubmitted, ProjectGFSubmittedPayload } from '@modules/project'
-import { resetDatabase } from '../../../../helpers'
 import models from '../../../../models'
 import { ProjectEvent } from '../../projectEvent.model'
 import onProjectGFSubmitted from './onProjectGFSubmitted'
 
-describe('onProjectGFSubmitted', () => {
+describe('Handler onProjectGFSubmitted', () => {
   const { File } = models
   const projectId = new UniqueEntityID().toString()
   const fileId = new UniqueEntityID().toString()
@@ -14,55 +13,55 @@ describe('onProjectGFSubmitted', () => {
   const gfDate = new Date('2021-12-26')
   const filename = 'my-file'
   const expirationDate = new Date('2025-01-01')
+  const id = new UniqueEntityID().toString()
 
-  beforeEach(async () => {
-    await resetDatabase()
-  })
+  it(`Etant donné un élément GF avec le statut 'due' dans ProjectEvent,
+  alors il devrait être mis à jour avec le fichier soumis`, async () => {
+    await File.create({
+      id: fileId,
+      filename,
+      designation: 'designation',
+    })
 
-  describe('when there is a corresponding file is the File projection', () => {
-    it('should create a new project event of type ProjectGFSubmitted', async () => {
-      await File.create({
-        id: fileId,
-        filename,
-        designation: 'designation',
+    await ProjectEvent.create({
+      id,
+      type: 'GarantiesFinancières',
+      projectId,
+      valueDate: new Date('2020-01-01').getTime(),
+      eventPublishedAt: new Date('2020-01-01').getTime(),
+      payload: { statut: 'due' },
+    })
+
+    await onProjectGFSubmitted(
+      new ProjectGFSubmitted({
+        payload: {
+          projectId,
+          fileId,
+          submittedBy,
+          gfDate,
+          expirationDate,
+        } as ProjectGFSubmittedPayload,
+        original: {
+          version: 1,
+          occurredAt,
+        },
       })
+    )
 
-      await ProjectEvent.create({
-        id: new UniqueEntityID().toString(),
-        type: 'GarantiesFinancières',
-        projectId,
-        valueDate: new Date('2020-01-01').getTime(),
-        eventPublishedAt: new Date('2020-01-01').getTime(),
-        payload: { statut: 'due' },
-      })
+    const projectEvent = await ProjectEvent.findOne({
+      where: { type: 'GarantiesFinancières', projectId },
+    })
 
-      await onProjectGFSubmitted(
-        new ProjectGFSubmitted({
-          payload: {
-            projectId,
-            fileId,
-            submittedBy,
-            gfDate,
-            expirationDate,
-          } as ProjectGFSubmittedPayload,
-          original: {
-            version: 1,
-            occurredAt,
-          },
-        })
-      )
-
-      const projectEvent = await ProjectEvent.findOne({
-        where: { type: 'GarantiesFinancières', projectId },
-      })
-
-      expect(projectEvent).not.toBeNull()
-      expect(projectEvent).toMatchObject({
-        type: 'GarantiesFinancières',
-        valueDate: gfDate.getTime(),
-        eventPublishedAt: occurredAt.getTime(),
-        payload: { statut: 'pending-validation' },
-      })
+    expect(projectEvent).toMatchObject({
+      id,
+      type: 'GarantiesFinancières',
+      valueDate: occurredAt.getTime(),
+      eventPublishedAt: occurredAt.getTime(),
+      payload: {
+        statut: 'pending-validation',
+        fichier: { id: fileId, name: filename },
+        dateConstitution: gfDate.getTime(),
+      },
     })
   })
 })
