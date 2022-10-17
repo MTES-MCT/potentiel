a929cdec8226fcabdb43ee64e8cef210cc95561a
flodlc <f.delacomble@gmail.com>
2022-10-17 17:51:53 +0200

    ✅ ajout d'une scénario de test pour le handler onProjectGFWithdrawn

--- src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFWithdrawn.integration.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/garantiesFinancières/onProjectGFWithdrawn.integration.ts
@@ -8,10 +8,11 @@ describe('Handler onProjectGFWithdrawn', () => {
   beforeEach(async () => {
     await resetDatabase()
   })
-  it('should create a new event of type ProjectGFWithdrawn', async () => {
+  it(`Etant donné un élément GF avec le statut 'uploaded' et une date limite d'envoi dans ProjectEvent,
+  alors il devrait être mis à jour avec le statut 'due'.`, async () => {
     const projectId = new UniqueEntityID().toString()
-    const removedBy = 'user-id'
     const occurredAt = new Date('2022-01-12')
+    const dateLimiteDEnvoi = new Date('2023-01-01')
 
     await ProjectEvent.create({
       id: new UniqueEntityID().toString(),
@@ -19,12 +20,12 @@ describe('Handler onProjectGFWithdrawn', () => {
       projectId,
       valueDate: new Date('2020-01-01').getTime(),
       eventPublishedAt: new Date('2020-01-01').getTime(),
-      payload: { statut: 'pending-validation' },
+      payload: { statut: 'uploaded', dateLimiteDEnvoi: dateLimiteDEnvoi.getTime() },
     })
 
     await onProjectGFWithdrawn(
       new ProjectGFWithdrawn({
-        payload: { projectId, removedBy } as ProjectGFWithdrawnPayload,
+        payload: { projectId } as ProjectGFWithdrawnPayload,
         original: {
           version: 1,
           occurredAt,
@@ -41,7 +42,38 @@ describe('Handler onProjectGFWithdrawn', () => {
       type: 'GarantiesFinancières',
       valueDate: occurredAt.getTime(),
       eventPublishedAt: occurredAt.getTime(),
-      payload: { statut: 'due' },
+      payload: { statut: 'due', dateLimiteDEnvoi: dateLimiteDEnvoi.getTime() },
     })
+  })
+
+  it(`Etant donné un élément GF avec le statut 'uploaded' et aucunue date limite d'envoi dans ProjectEvent,
+  alors il devrait être supprimé.`, async () => {
+    const projectId = new UniqueEntityID().toString()
+    const occurredAt = new Date('2022-01-12')
+
+    await ProjectEvent.create({
+      id: new UniqueEntityID().toString(),
+      type: 'GarantiesFinancières',
+      projectId,
+      valueDate: new Date('2020-01-01').getTime(),
+      eventPublishedAt: new Date('2020-01-01').getTime(),
+      payload: { statut: 'uploaded' },
+    })
+
+    await onProjectGFWithdrawn(
+      new ProjectGFWithdrawn({
+        payload: { projectId } as ProjectGFWithdrawnPayload,
+        original: {
+          version: 1,
+          occurredAt,
+        },
+      })
+    )
+
+    const projectEvent = await ProjectEvent.findOne({
+      where: { type: 'GarantiesFinancières', projectId },
+    })
+
+    expect(projectEvent).toBeNull()
   })
 })
