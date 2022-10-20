3432605bb9ba7b4573b76bdc7737db078f6ea577
Julien Pavon <julien.pavon@gmail.com>
2022-10-20 13:48:45 +0200

    ✨ (1220): Ajout de l'atom Tile

---
+++ src/views/components/UI/atoms/Tile.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Tile } from './Tile'
+
+export default {
+  title: 'Components/Atoms/Tile',
+  component: Tile,
+  argTypes: {
+    children: { control: 'text' },
+  },
+}
+
+const Template = (args) => <Tile {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  children: `Exemple de tuile`,
+}
