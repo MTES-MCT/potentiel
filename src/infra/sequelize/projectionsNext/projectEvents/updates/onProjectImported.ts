1348f11af5ff78bd3ddad6412ef24a50fd6afe16
HanaeY <hanae.yamashita@lilo.org>
2022-10-25 11:14:28 +0200

    ✨ (ProjectEvent): onProjectImported : ajout d'un item MeS

--- src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectImported.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/onProjectImported.ts
@@ -1,19 +1,18 @@
 import { UniqueEntityID } from '@core/domain'
+import { logger } from '@core/utils'
 import { ProjectImported } from '@modules/project'
+import { ProjectionEnEchec } from '@modules/shared'
 import { ProjectEvent, ProjectEventProjector } from '../projectEvent.model'
 
-export default ProjectEventProjector.on(
-  ProjectImported,
-  async (
-    {
-      payload: {
-        projectId,
-        data: { notifiedOn },
-      },
-      occurredAt,
+export default ProjectEventProjector.on(ProjectImported, async (évènement, transaction) => {
+  const {
+    payload: {
+      projectId,
+      data: { notifiedOn, classe },
     },
-    transaction
-  ) => {
+    occurredAt,
+  } = évènement
+  try {
     await ProjectEvent.create(
       {
         projectId,
@@ -25,5 +24,43 @@ export default ProjectEventProjector.on(
       },
       { transaction }
     )
+  } catch (error) {
+    logger.error(
+      new ProjectionEnEchec(
+        `Erreur lors du traitement de l'évènement ProjectImported (projectEvent.update) : ajout de ProjectImported`,
+        {
+          évènement,
+          nomProjection: 'ProjectEvent.onProjectImported',
+        },
+        error
+      )
+    )
   }
-)
+
+  if (classe === 'Classé') {
+    try {
+      await ProjectEvent.create(
+        {
+          projectId,
+          type: 'DateMiseEnService',
+          valueDate: occurredAt.getTime(),
+          eventPublishedAt: occurredAt.getTime(),
+          id: new UniqueEntityID().toString(),
+          payload: { statut: 'non-renseignée' },
+        },
+        { transaction }
+      )
+    } catch (error) {
+      logger.error(
+        new ProjectionEnEchec(
+          `Erreur lors du traitement de l'évènement ProjectImported (projectEvent.update) : ajout de DateMiseEnService`,
+          {
+            évènement,
+            nomProjection: 'ProjectEvent.onProjectImported',
+          },
+          error
+        )
+      )
+    }
+  }
+})
