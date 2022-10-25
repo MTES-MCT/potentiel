2747d2402b24dcbe1c2684cb233b9b77ba8ce58c
HanaeY <hanae.yamashita@lilo.org>
2022-10-25 15:45:50 +0200

    🎨 (PR FOLLOW UP): Ajout try/catch sur une modification de ProjectEvent

--- src/infra/sequelize/projectionsNext/projectEvents/updates/abandon/onAbandonAccordé.ts
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/abandon/onAbandonAccordé.ts
@@ -46,8 +46,21 @@ export default ProjectEventProjector.on(AbandonAccordé, async (évènement, tra
     )
   }
 
-  await ProjectEvent.destroy({
-    where: { projectId: projetId, type: 'DateMiseEnService' },
-    transaction,
-  })
+  try {
+    await ProjectEvent.destroy({
+      where: { projectId: projetId, type: 'DateMiseEnService' },
+      transaction,
+    })
+  } catch (e) {
+    logger.error(
+      new ProjectionEnEchec(
+        `Erreur lors du traitement de l'événement AbandonAccordé: suppression de l'élément DateMiseEnService`,
+        {
+          évènement,
+          nomProjection: 'ProjectEventProjector.onAbandonAccordé',
+        },
+        e
+      )
+    )
+  }
 })
