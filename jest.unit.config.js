2e640e6c5651ee8b3fe355f11fa6789315907e58
HubM <hubmoncenis@gmail.com>
2022-10-27 15:38:11 +0200

    ⬆️ (1209): upgrade jest

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-      isolatedModules: true,
-    },
-  },
   moduleFileExtensions: ['ts', 'js', 'tsx'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        isolatedModules: true,
+      },
+    ],
   },
   testMatch: ['**/*.spec.(ts|js)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
