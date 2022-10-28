a870033b87b12b2ea84d0236f8dba37799cde41d
HanaeY <hanae.yamashita@lilo.org>
2022-10-28 17:11:34 +0200

    ✅ (fix tests): Update fake project

--- src/modules/project/eventHandlers/onDateMiseEnServiceRenseignée.spec.ts
+++ src/modules/project/eventHandlers/onDateMiseEnServiceRenseignée.spec.ts
@@ -1,11 +1,11 @@
 import { okAsync } from '@core/utils'
 import { InfraNotAvailableError } from '@modules/shared'
-import makeFakeProject from '../../../__tests__/fixtures/project'
-import { fakeTransactionalRepo } from '../../../__tests__/fixtures/aggregates'
+import { fakeTransactionalRepo, makeFakeProject } from '../../../__tests__/fixtures/aggregates'
 import { DateMiseEnServiceRenseignée } from '../events'
 import { makeOnDateMiseEnServiceRenseignée } from './onDateMiseEnServiceRenseignée'
 import { DomainEvent } from '@core/domain'
 import { ProjectAppelOffre } from '@entities'
+import { Project } from '../Project'
 
 describe(`Handler onDateMiseEnServiceRenseignée`, () => {
   const publishToEventStore = jest.fn((event: DomainEvent) =>
@@ -18,6 +18,8 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
   const nouvelleDateAchèvementAttendue = new Date(
     new Date(dateAchèvementInitiale).setMonth(new Date(dateAchèvementInitiale).getMonth() + 18)
   )
+  const fakeProject = { ...makeFakeProject() }
+  const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
   for (const type of ['autoconso', 'batiment', 'innovation', 'neutre', 'sol', 'zni', 'autre']) {
     describe(`Projets PV ${type}`, () => {
@@ -25,12 +27,6 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
       describe(`Projet PV ${type} ne pouvant pas bénéficier du délai de 18 mois`, () => {
         it(`Etant donné un projet PV ${type} dont la date de mise en service est supérieure au 31 décembre 2024
       alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-          const projectRepo = fakeTransactionalRepo(
-            makeFakeProject({
-              id: 'projetId',
-            })
-          )
-
           const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
             projectRepo,
             publishToEventStore,
@@ -38,21 +34,15 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           })
 
           const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-            payload: { projetId: 'projetId', dateMiseEnService: '01/01/2050' },
+            payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2050' },
           })
 
           await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
           expect(publishToEventStore).not.toHaveBeenCalled()
         })
 
-        it(`Etant donné un projet PV ${type} dont la date de mise en service est inférieure au 1er septembre 2022, 
+        it(`Etant donné un projet PV ${type} dont la date de mise en service est inférieure au 1er septembre 2022,
       alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-          const projectRepo = fakeTransactionalRepo(
-            makeFakeProject({
-              id: 'projetId',
-            })
-          )
-
           const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
             projectRepo,
             publishToEventStore,
@@ -60,24 +50,23 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           })
 
           const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-            payload: { projetId: 'projetId', dateMiseEnService: '01/01/2020' },
+            payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2020' },
           })
 
           await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
           expect(publishToEventStore).not.toHaveBeenCalled()
         })
 
-        it(`Etant donné un projet PV ${type} : 
+        it(`Etant donné un projet PV ${type} :
       - dont la date de mise en service est comprise entre le 1er septembre 2022 et le 31 décembre 2024
       - MAIS n'ayant pas souscrit au CDC 2022
       alors le projet ne doit pas être modifié et aucun événement n'est émis
       `, async () => {
-          const projectRepo = fakeTransactionalRepo(
-            makeFakeProject({
-              id: 'projetId',
-              cahierDesCharges: { type: 'modifié', paruLe: '30/07/2022' },
-            })
-          )
+          const fakeProject = {
+            ...makeFakeProject(),
+            cahierDesCharges: { type: 'modifié', paruLe: '30/07/2022' },
+          }
+          const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
           const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
             projectRepo,
@@ -86,25 +75,25 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           })
 
           const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-            payload: { projetId: 'projetId', dateMiseEnService: '01/01/2023' },
+            payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2023' },
           })
 
           await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
           expect(publishToEventStore).not.toHaveBeenCalled()
         })
 
-        it(`Etant donné un projet PV ${type} : 
+        it(`Etant donné un projet PV ${type} :
       - dont la date de mise en service est comprise entre le 1er septembre 2022 et le 31 décembre 2024
       - MAIS ayant déjà bénéficié du délai CDC 2022,
       alors le projet ne doit pas être modifié et aucun événement n'est émis
       `, async () => {
-          const projectRepo = fakeTransactionalRepo(
-            makeFakeProject({
-              id: 'projetId',
-              cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
-              délaiCDC2022appliqué: true,
-            })
-          )
+          const fakeProject = {
+            ...makeFakeProject(),
+            cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
+            délaiCDC2022appliqué: true,
+          }
+
+          const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
           const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
             projectRepo,
@@ -113,7 +102,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           })
 
           const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-            payload: { projetId: 'projetId', dateMiseEnService: '01/01/2023' },
+            payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2023' },
           })
 
           await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
@@ -122,17 +111,17 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
       })
 
       describe(`Projet PV ${type} pouvant bénéficier du délai de 18 mois`, () => {
-        it(`Etant donné un projet PV ${type} : 
+        it(`Etant donné un projet PV ${type} :
       - ayant souscrit au CDC 2022,
       - dont la date de mise en service est comprise entre le 1er septembre 2022 et le 31 décembre 2024,
       alors le délai de 18 mois en lien avec le CDC 2022 devrait être appliqué`, async () => {
-          const projectRepo = fakeTransactionalRepo(
-            makeFakeProject({
-              id: 'projetId',
-              cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
-              completionDueOn: dateAchèvementInitiale,
-            })
-          )
+          const fakeProject = {
+            ...makeFakeProject(),
+            cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
+            completionDueOn: dateAchèvementInitiale,
+          }
+
+          const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
           const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
             projectRepo,
@@ -141,7 +130,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           })
 
           const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-            payload: { projetId: 'projetId', dateMiseEnService: '01/01/2023' },
+            payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2023' },
           })
 
           await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
@@ -151,7 +140,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           expect(évènement1.type).toEqual('ProjectCompletionDueDateSet')
           expect(évènement1.payload).toEqual(
             expect.objectContaining({
-              projectId: 'projetId',
+              projectId: fakeProject.id.toString(),
               completionDueOn: nouvelleDateAchèvementAttendue.getTime(),
             })
           )
@@ -160,7 +149,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
           expect(évènement2.type).toEqual('DélaiCDC2022Appliqué')
           expect(évènement2.payload).toEqual(
             expect.objectContaining({
-              projetId: 'projetId',
+              projetId: fakeProject.id.toString(),
               nouvelleDateLimiteAchèvement: nouvelleDateAchèvementAttendue.toISOString(),
               ancienneDateLimiteAchèvement: new Date(dateAchèvementInitiale).toISOString(),
             })
@@ -175,12 +164,6 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
     describe(`Projet éolien ne pouvant pas bénéficier du délai de 18 mois`, () => {
       it(`Etant donné un projet éolien dont la date de mise en service est antérieure au 1er juin 2022
       alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-        const projectRepo = fakeTransactionalRepo(
-          makeFakeProject({
-            id: 'projetId',
-          })
-        )
-
         const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
           projectRepo,
           publishToEventStore,
@@ -188,7 +171,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         })
 
         const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-          payload: { projetId: 'projetId', dateMiseEnService: '01/01/2021' },
+          payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2021' },
         })
 
         await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
@@ -197,12 +180,6 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
 
       it(`Etant donné un projet éolien dont la date de mise en service est postérieure au 30 septembre 2024
       alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-        const projectRepo = fakeTransactionalRepo(
-          makeFakeProject({
-            id: 'projetId',
-          })
-        )
-
         const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
           projectRepo,
           publishToEventStore,
@@ -210,23 +187,22 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         })
 
         const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-          payload: { projetId: 'projetId', dateMiseEnService: '01/01/2050' },
+          payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2050' },
         })
 
         await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
         expect(publishToEventStore).not.toHaveBeenCalled()
       })
 
-      it(`Etant donné un projet éolien 
+      it(`Etant donné un projet éolien
       dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
       MAIS qui n'a pas souscrit au CDC 2022,
       alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-        const projectRepo = fakeTransactionalRepo(
-          makeFakeProject({
-            id: 'projetId',
-            cahierDesCharges: { type: 'modifié', paruLe: '30/07/2022' },
-          })
-        )
+        const fakeProject = {
+          ...makeFakeProject(),
+          cahierDesCharges: { type: 'modifié', paruLe: '30/07/2022' },
+        }
+        const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
         const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
           projectRepo,
@@ -235,7 +211,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         })
 
         const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-          payload: { projetId: 'projetId', dateMiseEnService: '01/01/2023' },
+          payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2023' },
         })
 
         await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
@@ -245,15 +221,14 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
 
     describe(`Projet éolien pouvant bénéficier du délai de 18 mois`, () => {
       it(`dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
-      et ayant souscrit au CDC 2022, 
+      et ayant souscrit au CDC 2022,
       alors le délai de 18 mois en lien avec le CDC 2022 devrait être appliqué`, async () => {
-        const projectRepo = fakeTransactionalRepo(
-          makeFakeProject({
-            id: 'projetId',
-            cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
-            completionDueOn: dateAchèvementInitiale,
-          })
-        )
+        const fakeProject = {
+          ...makeFakeProject(),
+          cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
+          completionDueOn: dateAchèvementInitiale,
+        }
+        const projectRepo = fakeTransactionalRepo(fakeProject as Project)
 
         const onDateMiseEnServiceRenseignée = makeOnDateMiseEnServiceRenseignée({
           projectRepo,
@@ -262,7 +237,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         })
 
         const événementMeSRenseignée = new DateMiseEnServiceRenseignée({
-          payload: { projetId: 'projetId', dateMiseEnService: '01/01/2023' },
+          payload: { projetId: fakeProject.id.toString(), dateMiseEnService: '01/01/2023' },
         })
 
         await onDateMiseEnServiceRenseignée(événementMeSRenseignée)
@@ -272,7 +247,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         expect(évènement1.type).toEqual('ProjectCompletionDueDateSet')
         expect(évènement1.payload).toEqual(
           expect.objectContaining({
-            projectId: 'projetId',
+            projectId: fakeProject.id.toString(),
             completionDueOn: nouvelleDateAchèvementAttendue.getTime(),
           })
         )
@@ -281,7 +256,7 @@ describe(`Handler onDateMiseEnServiceRenseignée`, () => {
         expect(évènement2.type).toEqual('DélaiCDC2022Appliqué')
         expect(évènement2.payload).toEqual(
           expect.objectContaining({
-            projetId: 'projetId',
+            projetId: fakeProject.id.toString(),
             nouvelleDateLimiteAchèvement: nouvelleDateAchèvementAttendue.toISOString(),
             ancienneDateLimiteAchèvement: new Date(dateAchèvementInitiale).toISOString(),
           })
