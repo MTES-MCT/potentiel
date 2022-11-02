ecdf01a33a70c71f4585621049f1864a463ae6f1
Hubert Moncenis <hubmoncenis@gmail.com>
2022-11-02 11:43:03 +0100

    🎨 (storybook): ajout d'une storie pour le composant AlertBox (#707)

---
+++ src/views/components/UI/molecules/AlertBox.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AlertBox } from './AlertBox'
+
+export default {
+  title: 'Components/Molecules/AlertBox',
+  component: AlertBox,
+  argTypes: {
+    title: { control: 'text', defaultValue: 'titre' },
+    children: { control: 'text', defaultValue: '-' },
+  },
+}
+
+const Template = (args) => <AlertBox {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  title: `Exemple de titre d'une AlertBox`,
+  children: `Exemple de description d'une AlertBox`,
+}
