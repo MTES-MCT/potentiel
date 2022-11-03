ac55769198548640192152f05834f0dcc397c487
HanaeY <hanae.yamashita@lilo.org>
2022-11-03 17:10:34 +0100

    🐛 (Page Import gestionnaire): Remettre le bouton "actualiser"

--- src/views/pages/importGestionnaireReseauPage/ImportGestionnaireReseau.tsx
+++ src/views/pages/importGestionnaireReseauPage/ImportGestionnaireReseau.tsx
@@ -10,6 +10,8 @@ import {
   ErrorIcon,
   SuccessIcon,
   Link,
+  SecondaryLinkButton,
+  RefreshIcon,
 } from '@components'
 import routes from '@routes'
 import { Request } from 'express'
@@ -207,6 +209,12 @@ export const ImportGestionnaireReseau = ({
       </form>
     </div>
     <div className="panel flex flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <h4 className="m-0 p-0">Liste des mises à jour :</h4>
+        <SecondaryLinkButton href={request.path}>
+          <RefreshIcon className="h-4 w-4 mr-2" /> Actualiser
+        </SecondaryLinkButton>
+      </div>
       {tâches.length === 0 ? (
         <div className="flex p-16 border border-dashed border-grey-625-base">
           <span className="mx-auto text-center">Aucune mise à jour n'a encore été démarrée</span>
