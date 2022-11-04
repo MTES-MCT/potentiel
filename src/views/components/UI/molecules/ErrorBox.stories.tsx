72ca751e087027de541992c8fd34fc137d658f5e
HubM <hubmoncenis@gmail.com>
2022-11-04 15:58:02 +0100

    ✨ (1163): création d'une errorbox conforme dsfr

---
+++ src/views/components/UI/molecules/ErrorBox.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ErrorBox } from './ErrorBox'
+
+export default {
+  title: 'Components/Molecules/ErrorBox',
+  component: ErrorBox,
+  argTypes: {
+    title: { control: 'text', defaultValue: 'titre' },
+    children: { control: 'text', defaultValue: '-' },
+  },
+}
+
+const Template = (args) => <ErrorBox {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  title: `Exemple de titre d'une ErrorBox`,
+  children: `Exemple de description d'une ErrorBox`,
+}
