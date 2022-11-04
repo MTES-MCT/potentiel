3368fc3f5f12f17421631bd452283c523a66fd8e
HanaeY <hanae.yamashita@lilo.org>
2022-11-04 11:41:25 +0100

    🔥 (contrôleurs helpers): suppression de stringToDateYupTransformation

--- src/controllers/helpers/yupTransformations.ts
+++ src/controllers/helpers/yupTransformations.ts
@@ -1,7 +1,4 @@
 import { parse } from 'date-fns'
 
 export const iso8601DateToDateYupTransformation = (_, originalValue) =>
-  stringToDateYupTransformation(originalValue, 'yyyy-MM-dd')
-
-export const stringToDateYupTransformation = (date: string, format: string) =>
-  date !== '' ? parse(date, format, new Date()) : null
+  originalValue !== '' ? parse(originalValue, 'yyyy-MM-dd', new Date()) : null
