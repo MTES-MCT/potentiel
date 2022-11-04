d56acc1060c0a0c8392a0f90dad08632d4f9a6b6
Julien Pavon <julien.pavon@gmail.com>
2022-11-04 14:29:23 +0100

    💄 (1244): Affichage des mise a jour ignorees

--- src/views/pages/importGestionnaireReseauPage/components/Tâche.tsx
+++ src/views/pages/importGestionnaireReseauPage/components/Tâche.tsx
@@ -1,4 +1,4 @@
-import { Badge, SuccessIcon, ErrorIcon, Link } from '@components'
+import { Badge, SuccessIcon, ErrorIcon, Link, SkipIcon } from '@components'
 import routes from '@routes'
 import React, { FC, useState } from 'react'
 
@@ -12,15 +12,35 @@ export type TâcheProps = {
   | {
       état: 'terminée'
       dateDeFin: Date
-      nombreDeSucces: number
-      nombreDEchecs: number
-      résultatErreurs: RésultatErreurs
+      détail: Détail
     }
 )
 
+type Succès = {
+  projetId: string
+  identifiantGestionnaireRéseau: string
+}
+
+type Erreur = {
+  raison: string
+  projetId?: string
+  identifiantGestionnaireRéseau: string
+}
+
+type Ignorés = {
+  raison: string
+  projetId: string
+  identifiantGestionnaireRéseau: string
+}
+
+type Détail = {
+  succès?: Array<Succès>
+  ignorés?: Array<Ignorés>
+  erreurs?: Array<Erreur>
+}
+
 export const Tâche: FC<TâcheProps> = (props) => {
   const { état, dateDeDébut } = props
-  const [afficherDétailErreurs, setAfficherDétailErreurs] = useState(false)
   return (
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-1">
@@ -35,68 +55,102 @@ export const Tâche: FC<TâcheProps> = (props) => {
           {état === 'terminée' && `Terminée le ${new Date(props.dateDeFin).toLocaleString()}`}
         </p>
       </div>
-      {état === 'terminée' && (
-        <>
-          <div className="flex flex-col lg:flex-row gap-1">
-            {props.nombreDeSucces > 0 && (
-              <div className="flex items-center text-sm lg:mr-4">
-                <SuccessIcon className="w-4 h-4 text-success-425-base mr-1" />
-                {`${props.nombreDeSucces} ${
-                  props.nombreDeSucces === 1 ? 'projet a' : 'projets ont'
-                } été mis à jour`}
-              </div>
-            )}
-            {props.nombreDEchecs > 0 && (
-              <Link
-                className="flex items-center text-sm"
-                onClick={() => {
-                  setAfficherDétailErreurs(!afficherDétailErreurs)
-                }}
-              >
-                <ErrorIcon className="w-4 h-4 text-error-425-base mr-1" />
-                {`${props.nombreDEchecs} ${
-                  props.nombreDEchecs === 1 ? 'mise à jour a' : 'mises à jour ont'
-                } échoué`}{' '}
-                ({afficherDétailErreurs ? 'masquer le détail' : 'voir le détail'})
-              </Link>
-            )}
-          </div>
-          {afficherDétailErreurs && <RapportErreurs erreurs={props.résultatErreurs} />}
-        </>
+      {props.état === 'terminée' && (
+        <div className="flex flex-col lg:flex-row gap-1">
+          {props.détail.succès && <DétailSuccès {...{ succès: props.détail.succès }} />}
+          {props.détail.ignorés && <DétailIgnorés {...{ ignorés: props.détail.ignorés }} />}
+          {props.détail.erreurs && <DétailErreurs {...{ erreurs: props.détail.erreurs }} />}
+        </div>
       )}
     </div>
   )
 }
 
-type RésultatErreurs = Array<{
-  raison: string
-  projetId?: string
-  identifiantGestionnaireRéseau: string
-}>
+const DétailSuccès: FC<{ succès: Array<Succès> }> = ({ succès }) => {
+  return succès.length > 0 ? (
+    <div className="flex text-sm lg:mr-4">
+      <SuccessIcon className="w-4 h-4 text-success-425-base mr-1" />
+      {`${succès.length} ${succès.length === 1 ? 'projet a' : 'projets ont'} été mis à jour`}
+    </div>
+  ) : null
+}
 
-const RapportErreurs: FC<{ erreurs: RésultatErreurs }> = ({ erreurs }) => (
-  <ul className="p-0 list-none">
-    {erreurs.map((erreur, index) => {
-      return (
-        <li className="mt-3 text-sm">
-          <span className="block">
-            Identifiant gestionnaire réseau :
-            <code className="font-bold">{erreur.identifiantGestionnaireRéseau}</code>
-          </span>
-          <span className="block">Erreur : {erreur.raison}</span>
-          {erreur.projetId && (
-            <Link className="block" href={routes.PROJECT_DETAILS(erreur.projetId)}>
-              Voir page projet
-            </Link>
-          )}
-          {index !== erreurs.length - 1 && (
-            <hr
-              style={{ borderTopWidth: '1px', borderTopStyle: 'solid' }}
-              className="my-4 border-t-grey-925-base"
-            />
-          )}
-        </li>
-      )
-    })}
-  </ul>
-)
+const DétailIgnorés: FC<{ ignorés: Array<Ignorés> }> = ({ ignorés }) => {
+  const [afficherDétail, setAfficherDétail] = useState(false)
+
+  return ignorés.length > 0 ? (
+    <div>
+      <Link
+        className="flex text-sm lg:mr-4"
+        onClick={() => {
+          setAfficherDétail(!afficherDétail)
+        }}
+      >
+        <SkipIcon className="w-4 h-4 text-grey-625-base mr-1" />
+        {`${ignorés.length} ${
+          ignorés.length === 1 ? 'mise à jour a été ignorée' : 'mises à jour ont été ignorées'
+        } (${afficherDétail ? 'masquer le détail' : 'voir le détail'})`}
+      </Link>
+      {afficherDétail && (
+        <ul className="p-0 list-none">
+          {ignorés.map((ignorés) => {
+            return (
+              <li className="py-3 text-sm border-0 border-solid border-b-grey-925-base border-b-[1px] last:border-b-0 last:pb-0">
+                <div>
+                  Identifiant gestionnaire réseau :
+                  <code className="font-bold">{ignorés.identifiantGestionnaireRéseau}</code>
+                </div>
+                <div>Raison : {ignorés.raison}</div>
+                {ignorés.projetId && (
+                  <Link className="block" href={routes.PROJECT_DETAILS(ignorés.projetId)}>
+                    Voir page projet
+                  </Link>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </div>
+  ) : null
+}
+
+const DétailErreurs: FC<{ erreurs: Array<Erreur> }> = ({ erreurs }) => {
+  const [afficherDétail, setAfficherDétail] = useState(false)
+
+  return erreurs.length > 0 ? (
+    <div>
+      <Link
+        className="flex items-center text-sm"
+        onClick={() => {
+          setAfficherDétail(!afficherDétail)
+        }}
+      >
+        <ErrorIcon className="w-4 h-4 text-error-425-base mr-1" />
+        {`${erreurs.length} ${
+          erreurs.length === 1 ? 'mise à jour a' : 'mises à jour ont'
+        } échoué (${afficherDétail ? 'masquer le détail' : 'voir le détail'})`}
+      </Link>
+      {afficherDétail && (
+        <ul className="p-0 list-none">
+          {erreurs.map((erreur) => {
+            return (
+              <li className="py-3 text-sm border-0 border-solid border-b-grey-925-base border-b-[1px] last:border-b-0 last:pb-0">
+                <div>
+                  Identifiant gestionnaire réseau :
+                  <code className="font-bold">{erreur.identifiantGestionnaireRéseau}</code>
+                </div>
+                <div>Erreur : {erreur.raison}</div>
+                {erreur.projetId && (
+                  <Link className="block" href={routes.PROJECT_DETAILS(erreur.projetId)}>
+                    Voir page projet
+                  </Link>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </div>
+  ) : null
+}
