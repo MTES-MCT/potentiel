1fcab8245c661b1990d31899f00c70a9cb426767
flodlc <f.delacomble@gmail.com>
2022-11-08 16:36:15 +0100

    💄 wip

--- src/views/components/ProjectList.tsx
+++ src/views/components/ProjectList.tsx
@@ -2,99 +2,16 @@ import { ACTION_BY_ROLE, DownloadIcon, PaginationPanel, ProjectActions } from '@
 import { Project } from '@entities'
 import { UserRole } from '@modules/users'
 import routes from '@routes'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { formatDate } from '../../helpers/formatDate'
 import { dataId } from '../../helpers/testId'
 import { PaginatedList } from '../../types'
 import { Button, Tile } from './UI'
 import { PowerIcon, EuroIcon, CloudIcon } from './UI/atoms/icons'
 
-type Columns =
-  | 'Candidat'
-  | 'Puissance'
-  | 'Prix'
-  | 'Evaluation Carbone'
-  | 'Garanties Financières'
-  | 'Classé'
-
 type ColumnRenderer = (props: { project: Project; GFPastDue?: boolean }) => React.ReactNode
 
-const ColumnComponent: Record<Columns, ColumnRenderer> = {
-  Candidat: function CandidatColumn({ project }) {
-    return (
-      <td valign="top" className="projectList-candidat-column">
-        <div {...dataId('projectList-item-nomCandidat')}>{project.nomCandidat}</div>
-        <div
-          style={{
-            fontStyle: 'italic',
-            lineHeight: 'normal',
-            fontSize: 12,
-          }}
-        >
-          <span {...dataId('projectList-item-nomRepresentantLegal')}>
-            {project.nomRepresentantLegal}
-          </span>{' '}
-          <span {...dataId('projectList-item-email')}>{project.email}</span>
-        </div>
-      </td>
-    )
-  } as ColumnRenderer,
-  Puissance: function PuissanceColumn({ project }) {
-    return (
-      <td valign="top" className="projectList-puissance-column">
-        <span {...dataId('projectList-item-puissance')}>{project.puissance}</span>{' '}
-        <span
-          style={{
-            fontStyle: 'italic',
-            lineHeight: 'normal',
-            fontSize: 12,
-          }}
-        >
-          {project.appelOffre?.unitePuissance}
-        </span>
-      </td>
-    )
-  } as ColumnRenderer,
-  Prix: function PrixColumn({ project }) {
-    return (
-      <td valign="top" className="projectList-prix-column">
-        <span {...dataId('projectList-item-prixReference')}>{project.prixReference}</span>{' '}
-        <span
-          style={{
-            fontStyle: 'italic',
-            lineHeight: 'normal',
-            fontSize: 12,
-          }}
-        >
-          €/MWh
-        </span>
-      </td>
-    )
-  } as ColumnRenderer,
-  'Evaluation Carbone': function EvaluationCarboneColumn({ project }) {
-    return (
-      <td valign="top" className="projectList-evaluation-column">
-        {project.evaluationCarbone > 0 ? (
-          <>
-            <span {...dataId('projectList-item-evaluationCarbone')}>
-              {project.evaluationCarbone}
-            </span>{' '}
-            <span
-              style={{
-                fontStyle: 'italic',
-                lineHeight: 'normal',
-                fontSize: 12,
-              }}
-            >
-              kg eq CO2/kWc
-            </span>
-          </>
-        ) : (
-          ''
-        )}
-      </td>
-    )
-  } as ColumnRenderer,
+const ColumnComponent: Record<string, ColumnRenderer> = {
   'Garanties Financières': function GarantieFinanciereColumn({ project, GFPastDue }) {
     return (
       <td valign="top">
@@ -253,6 +170,18 @@ interface Props {
   GFPastDue?: boolean
 }
 
+const Currency = ({ children }: { children: ReactNode }) => (
+  <span
+    style={{
+      fontStyle: 'italic',
+      lineHeight: 'normal',
+      fontSize: 12,
+    }}
+  >
+    {children}
+  </span>
+)
+
 export const ProjectList = ({ projects, displayColumns, role, GFPastDue }: Props) => {
   let items: Array<Project>
   if (Array.isArray(projects)) {
@@ -290,109 +219,13 @@ export const ProjectList = ({ projects, displayColumns, role, GFPastDue }: Props
               />
             </div>
 
-            <div className="mb-4" {...dataId('projectList-item-nomProjet')}>
-              <a href={routes.PROJECT_DETAILS(project.id)}>{project.nomProjet}</a>{' '}
-              <div className="bg-success-950-base bg-sucess inline-block ml-2 px-2 rounded-md">
-                Classé
-              </div>
-            </div>
-            <div className="flex flex-row justify-between gap-4">
-              <div
-                style={{
-                  fontStyle: 'italic',
-                  lineHeight: 'normal',
-                  fontSize: 12,
-                }}
-              >
-                <span {...dataId('projectList-item-communeProjet')}>{project.communeProjet}</span>,{' '}
-                <span {...dataId('projectList-item-departementProjet')}>
-                  {project.departementProjet}
-                </span>
-                , <span {...dataId('projectList-item-regionProjet')}>{project.regionProjet}</span>
-                <div style={{ marginTop: 5, fontStyle: 'normal' }}>
-                  {project.potentielIdentifier}
-                </div>
-              </div>
-
-              <div className="projectList-candidat-column">
-                <div {...dataId('projectList-item-nomCandidat')}>{project.nomCandidat}</div>
-                <div
-                  style={{
-                    fontStyle: 'italic',
-                    lineHeight: 'normal',
-                    fontSize: 12,
-                  }}
-                >
-                  <span {...dataId('projectList-item-nomRepresentantLegal')}>
-                    {project.nomRepresentantLegal}
-                  </span>{' '}
-                  <span {...dataId('projectList-item-email')}>{project.email}</span>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center gap-2">
-                <div className="text-grey-425-base flex flex-row items-center">
-                  <PowerIcon className="mr-2" />
-                </div>
-                <div>
-                  <span {...dataId('projectList-item-puissance')}>{project.puissance}</span>{' '}
-                  <span
-                    style={{
-                      fontStyle: 'italic',
-                      lineHeight: 'normal',
-                      fontSize: 12,
-                    }}
-                  >
-                    {project.appelOffre?.unitePuissance}
-                  </span>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center gap-2">
-                <div className="text-grey-425-base flex flex-row items-center">
-                  <EuroIcon />
-                </div>
-                <div>
-                  <span {...dataId('projectList-item-prixReference')}>{project.prixReference}</span>{' '}
-                  <span
-                    style={{
-                      fontStyle: 'italic',
-                      lineHeight: 'normal',
-                      fontSize: 12,
-                    }}
-                  >
-                    €/MWh
-                  </span>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center gap-2 mr-4">
-                <div className="text-grey-425-base flex flex-row items-center">
-                  <CloudIcon />
-                </div>{' '}
-                <div>
-                  {project.evaluationCarbone > 0 ? (
-                    <>
-                      <span {...dataId('projectList-item-evaluationCarbone')}>
-                        {project.evaluationCarbone}
-                      </span>{' '}
-                      <span
-                        style={{
-                          fontStyle: 'italic',
-                          lineHeight: 'normal',
-                          fontSize: 12,
-                        }}
-                      >
-                        kg eq CO2/kWc
-                      </span>
-                    </>
-                  ) : (
-                    ''
-                  )}
-                </div>
-              </div>
-            </div>
             <div className="flex flex-row gap-4 items-center mt-4">
+              <div className="mb-4" {...dataId('projectList-item-nomProjet')}>
+                <a href={routes.PROJECT_DETAILS(project.id)}>{project.nomProjet}</a>{' '}
+                <div className="bg-success-950-base bg-sucess inline-block ml-2 px-2 rounded-md">
+                  Classé
+                </div>
+              </div>
               {ACTION_BY_ROLE[role] ? (
                 <div className="ml-auto" {...dataId('item-actions-container')}>
                   <ProjectActions
@@ -409,6 +242,82 @@ export const ProjectList = ({ projects, displayColumns, role, GFPastDue }: Props
                 ''
               )}
               <Button>Voir</Button>
+            </div>
+
+            <div className="flex flex-row justify-between gap-4 items-center">
+              <div>
+                <div
+                  style={{
+                    fontStyle: 'italic',
+                    lineHeight: 'normal',
+                    fontSize: 12,
+                  }}
+                >
+                  <span {...dataId('projectList-item-communeProjet')}>{project.communeProjet}</span>
+                  ,{' '}
+                  <span {...dataId('projectList-item-departementProjet')}>
+                    {project.departementProjet}
+                  </span>
+                  , <span {...dataId('projectList-item-regionProjet')}>{project.regionProjet}</span>
+                  <div style={{ marginTop: 5, fontStyle: 'normal' }}>
+                    {project.potentielIdentifier}
+                  </div>
+                </div>
+
+                <div className="mt-2">
+                  <div {...dataId('projectList-item-nomCandidat')}>{project.nomCandidat}</div>
+                  <div
+                    style={{
+                      fontStyle: 'italic',
+                      lineHeight: 'normal',
+                      fontSize: 12,
+                    }}
+                  >
+                    <span {...dataId('projectList-item-nomRepresentantLegal')}>
+                      {project.nomRepresentantLegal}
+                    </span>{' '}
+                    <span {...dataId('projectList-item-email')}>{project.email}</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-2">
+                <div className="text-grey-425-base flex flex-row items-center">
+                  <PowerIcon />
+                </div>
+                <div>
+                  <span {...dataId('projectList-item-puissance')}>{project.puissance}</span>{' '}
+                  <Currency>{project.appelOffre?.unitePuissance}</Currency>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-2">
+                <div className="text-grey-425-base flex flex-row items-center">
+                  <EuroIcon />
+                </div>
+                <div>
+                  <span {...dataId('projectList-item-prixReference')}>{project.prixReference}</span>{' '}
+                  <Currency>€/MWh</Currency>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-2 mr-4">
+                <div className="text-grey-425-base flex flex-row items-center">
+                  <CloudIcon />
+                </div>
+                <div>
+                  {project.evaluationCarbone > 0 ? (
+                    <>
+                      <span {...dataId('projectList-item-evaluationCarbone')}>
+                        {project.evaluationCarbone}
+                      </span>{' '}
+                      <Currency>kg eq CO2/kWc</Currency>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
             </div>
           </Tile>
         )
