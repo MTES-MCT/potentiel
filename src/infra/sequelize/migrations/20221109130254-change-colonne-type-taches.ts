3bbb2e98fb6428811c50debd0417a5a0656bf46d
Julien Pavon <julien.pavon@gmail.com>
2022-11-09 14:48:17 +0100

    🗃️ (1172): Changement de l'enum de type de tache

---
+++ src/infra/sequelize/migrations/20221109130254-change-colonne-type-taches.ts
@@ -0,0 +1,28 @@
+import { QueryInterface, DataTypes } from 'sequelize'
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.removeConstraint('taches', 'tache_unique_gestionnaire_type_dateDeDébut')
+    await queryInterface.changeColumn('taches', 'type', {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    })
+    await queryInterface.sequelize.query(`drop type enum_taches_type;`)
+    await queryInterface.sequelize.query(`update taches set type = 'maj-données-de-raccordement';`)
+    await queryInterface.changeColumn('taches', 'type', {
+      type: DataTypes.ENUM('maj-données-de-raccordement'),
+      allowNull: false,
+    })
+    // await queryInterface.changeColumn('taches', 'type', {
+    //   type: DataTypes.ENUM('maj-date-mise-en-service'),
+    //   allowNull: false,
+    // })
+    queryInterface.addConstraint('taches', {
+      fields: ['gestionnaire', 'type', 'dateDeDébut'],
+      type: 'unique',
+      name: 'tache_unique_gestionnaire_type_dateDeDébut',
+    })
+  },
+
+  async down() {},
+}
