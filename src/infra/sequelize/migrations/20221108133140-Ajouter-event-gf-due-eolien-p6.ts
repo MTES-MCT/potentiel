bbfa4a25492b293edc5b401b1798e52bd1894c69
HanaeY <72154904+HanaeY@users.noreply.github.com>
2022-11-15 14:15:58 +0100

    1204 :bug: Ajout events ProjectGFDueDateSet Eolien P6 (#722)
    * 🗃️ (EventStore): Ajout de ProjectGFDueDateSet manquants
    
    * 🧐 (ProjectEvent): Rebuild

---
+++ src/infra/sequelize/migrations/20221108133140-Ajouter-event-gf-due-eolien-p6.ts
@@ -0,0 +1,78 @@
+'use strict'
+
+import { QueryInterface } from 'sequelize'
+import { logger } from '@core/utils'
+import { ProjectGFDueDateSet } from '@modules/project'
+import { toPersistance } from '../helpers'
+import models from '../models'
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      const { EventStore, Project } = models
+      const projects = await Project.findAll(
+        {
+          where: { appelOffreId: 'Eolien', periodeId: '6', classe: 'Classé' },
+          attributes: ['id', 'garantiesFinancieresDueOn', 'notifiedOn'],
+        },
+        { transaction }
+      )
+
+      if (!projects.length) {
+        logger.error(`aucun projet trouvé`)
+      } else {
+        console.log(`Nombre de projets lauréats de la période : ${projects.length}`)
+
+        let événementsAjoutés = 0
+        let projetNonModifiés = 0
+
+        for (const project of projects) {
+          const { id: projectId, garantiesFinancieresDueOn, notifiedOn } = project
+
+          const projectGFDueDateSetEvent = await EventStore.findOne(
+            {
+              where: { type: 'ProjectGFDueDateSet', 'payload.projectId': projectId },
+            },
+            { transaction }
+          )
+          if (projectGFDueDateSetEvent) {
+            projetNonModifiés++
+            return
+          }
+          await EventStore.create(
+            toPersistance(
+              new ProjectGFDueDateSet({
+                payload: { projectId, garantiesFinancieresDueOn },
+                original: {
+                  occurredAt: new Date(notifiedOn),
+                  version: 1,
+                },
+              })
+            ),
+            { transaction }
+          )
+          événementsAjoutés++
+        }
+
+        console.log(`Nombre d'événements créés : ${événementsAjoutés}`)
+        console.log(`Nombre de projets non modifiés : ${projetNonModifiés}`)
+      }
+
+      transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+}
