90eb220d13d90917b89a62cd11d60d5ff0cdef97
HanaeY <hanae.yamashita@lilo.org>
2022-11-17 12:02:37 +0100

    ✨ (ProjectEvent): Supprimer l'événement Abandon

---
+++ src/infra/sequelize/projectionsNext/projectEvents/updates/abandon/onLegacyAbandonSupprimé.ts
@@ -0,0 +1,28 @@
+import { ProjectEvent, ProjectEventProjector } from '../../projectEvent.model'
+import { ProjectionEnEchec } from '@modules/shared'
+import { logger } from '@core/utils'
+import { LegacyAbandonSupprimé } from '@modules/project'
+
+export default ProjectEventProjector.on(LegacyAbandonSupprimé, async (évènement, transaction) => {
+  const {
+    payload: { projetId },
+  } = évènement
+
+  try {
+    await ProjectEvent.destroy({
+      where: { 'payload.modificationType': 'abandon', projectId: projetId },
+      transaction,
+    })
+  } catch (e) {
+    logger.error(
+      new ProjectionEnEchec(
+        `Erreur lors du traitement de l'événement LegacyAbandonSupprimé`,
+        {
+          évènement,
+          nomProjection: 'ProjectEventProjector.onLegacyAbandonSupprimé',
+        },
+        e
+      )
+    )
+  }
+})
