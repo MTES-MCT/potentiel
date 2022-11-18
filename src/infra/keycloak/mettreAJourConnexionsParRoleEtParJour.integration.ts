6d461c392330b171ee5b877aa1eb6e1b88271f94
HubM <hubmoncenis@gmail.com>
2022-11-18 14:23:26 +0100

    🎨 (retour de reviews): amélioration de la description du test d'intégration

--- src/infra/keycloak/mettreAJourConnexionsParRoleEtParJour.integration.ts
+++ src/infra/keycloak/mettreAJourConnexionsParRoleEtParJour.integration.ts
@@ -2,11 +2,12 @@ import { resetDatabase } from '@infra/sequelize/helpers'
 import { ConnexionsParRoleEtParJour } from '@infra/sequelize/tableModels'
 import { mettreAJourConnexionsParRoleEtParJour } from './mettreAJourConnexionsParRoleEtParJour'
 
-describe(`helper mettreAJourConnexionsParRoleEtParJour`, () => {
+describe(`Mettre à jour le nombre de connexion par rôle et par jour`, () => {
   const date = new Date('2022-01-01')
   beforeEach(async () => await resetDatabase())
-  describe(`Lorsqu'il n'y a pas d'entrée dans la table ConnexionsParRoleEtParJour pour le rôle à la date du jour`, () => {
-    it(`Alors, une nouvelle entrée devrait être insérée dans la table ConnexionsParRoleEtParJour`, async () => {
+  describe(`Étant donné qu'aucune connexion n'a été faite le 01/01/2022 pour le rôle admin
+            Lorsqu'un utilisateur avec le rôle admin se connecte le 01/01/2022`, () => {
+    it(`Alors le compteur de connexion devrait être de 1 pour le rôle admin le 01/01/2022`, async () => {
       await mettreAJourConnexionsParRoleEtParJour({ role: 'admin', date })
       const entrée = await ConnexionsParRoleEtParJour.findOne({ where: { role: 'admin' } })
       expect(entrée?.compteur).toEqual(1)
@@ -14,8 +15,9 @@ describe(`helper mettreAJourConnexionsParRoleEtParJour`, () => {
     })
   })
 
-  describe(`Lorsqu'il y a déjà une entrée dans la table ConnexionsParRoleEtParJour pour le rôle à la date du jour`, () => {
-    it(`Alors, le compteur de cette entrée devrait être incrémenté`, async () => {
+  describe(`Étant donné qu'aucune connexion n'a été faite le 01/01/2022 pour le rôle admin
+            Lorsqu'un utilisateur avec le rôle admin se connecte le 01/01/2022`, () => {
+    it(`Alors le compteur de connexion devrait être de 2 pour le rôle admin le 01/01/2022`, async () => {
       const entréeInitiale = await ConnexionsParRoleEtParJour.create({
         role: 'admin',
         date: new Date('2022-01-01'),
