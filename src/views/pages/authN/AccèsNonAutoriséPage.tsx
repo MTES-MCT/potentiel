11eab1e23d2a2f1ef537a7e37109fca0e2cff6b9
Julien Pavon <julien.pavon@gmail.com>
2022-11-23 17:02:31 +0100

    ✏️ (841): Correction typo

--- src/views/pages/authN/AccèsNonAutoriséPage.tsx
+++ src/views/pages/authN/AccèsNonAutoriséPage.tsx
@@ -10,6 +10,6 @@ export const AccèsNonAutorisé = ({ request }: AccèsNonAutoriséProps) => (
   <PageTemplate {...request}>
     <h1>Accès non autorisé</h1>
     <p className="mt-0 text-sm">Erreur 403</p>
-    <p>Votre compte ne vous permet pas d'accèder à cette ressource.</p>
+    <p>Votre compte ne vous permet pas d'accéder à cette ressource.</p>
   </PageTemplate>
 )
