e830cb5c372d399730c202615948bc022a5276b3
HubM <hubmoncenis@gmail.com>
2022-11-29 17:00:09 +0100

    :card_file_box: (1320): ajout des projets lauréats entre le 15/04/2020 et le 21/10/2021 dans les stats d'utilisation (attestation téléchargée)

---
+++ src/infra/sequelize/migrations/20221129162000-migrer-lauréats-pre-21102021-statistiques-utilisation.ts
@@ -0,0 +1,63 @@
+import { Op, QueryInterface, Sequelize } from 'sequelize'
+import { models } from '../models'
+import { StatistiquesUtilisation } from '../tableModels'
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      const { Project } = models
+
+      const projetsÀAjouter = await Project.findAll(
+        {
+          where: {
+            classe: 'Classé',
+            notifiedOn: {
+              [Op.and]: {
+                [Op.gte]: 1586960864000, // 15/04/2020
+                [Op.lte]: 1634767201000, // 21/10/2021
+              },
+            },
+          },
+          attributes: [
+            [Sequelize.fn('DISTINCT', Sequelize.col('id')), 'id'],
+            'appelOffreId',
+            'periodeId',
+            'familleId',
+            'numeroCRE',
+            'notifiedOn',
+          ],
+        },
+        { transaction }
+      )
+
+      for (const projet of projetsÀAjouter) {
+        await StatistiquesUtilisation.create(
+          {
+            type: 'AttestationTéléchargée',
+            date: new Date(projet.notifiedOn),
+            données: {
+              utilisateur: { role: 'porteur-projet' },
+              projet: {
+                appelOffreId: projet.appelOffreId,
+                periodeId: projet.periodeId,
+                familleId: projet.familleId,
+                numeroCRE: projet.numeroCRE,
+              },
+            },
+          },
+          {
+            transaction,
+          }
+        )
+      }
+
+      await transaction.commit()
+    } catch (e) {
+      console.error(e)
+      await transaction.rollback()
+    }
+  },
+
+  async down(queryInterface: QueryInterface) {},
+}
