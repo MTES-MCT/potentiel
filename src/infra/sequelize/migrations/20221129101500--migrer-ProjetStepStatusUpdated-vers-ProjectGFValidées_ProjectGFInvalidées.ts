f851f92a4092304e4fb7860d297b07df513dd1f8
flodlc <f.delacomble@gmail.com>
2022-11-29 16:12:32 +0100

    ♻️ migration des evenements

---
+++ src/infra/sequelize/migrations/20221129101500--migrer-ProjetStepStatusUpdated-vers-ProjectGFValidées_ProjectGFInvalidées.ts
@@ -0,0 +1,78 @@
+import { ProjectGFInvalidées, ProjectGFValidées, ProjectStepStatusUpdated } from '@modules/project'
+import { QueryInterface } from 'sequelize'
+import { toPersistance } from '../helpers'
+import { models } from '../models'
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      const { EventStore, ProjectStep } = models
+
+      const evenementsAMigrer: ProjectStepStatusUpdated[] = await EventStore.findAll(
+        {
+          where: {
+            type: 'ProjectStepStatusUpdated',
+          },
+        },
+        { transaction }
+      )
+      console.log(`${evenementsAMigrer.length} événements ProjectStepStatusUpdated`)
+
+      const nouveauxÉvénements = (
+        await Promise.all(
+          evenementsAMigrer.map(async (evenement) => {
+            const {
+              occurredAt,
+              payload: { projectStepId, newStatus, statusUpdatedBy },
+            } = evenement
+
+            const projectStep = await ProjectStep.findByPk(projectStepId)
+
+            if (projectStep.type !== 'garantie-financiere') return
+
+            const { projectId } = projectStep
+
+            if (newStatus === 'validé') {
+              return new ProjectGFValidées({
+                payload: {
+                  projetId: projectId,
+                  validéesPar: statusUpdatedBy,
+                },
+                original: {
+                  occurredAt,
+                  version: 1,
+                },
+              })
+            } else {
+              return new ProjectGFInvalidées({
+                payload: {
+                  projetId: projectId,
+                  invalidéesPar: statusUpdatedBy,
+                },
+                original: {
+                  occurredAt,
+                  version: 1,
+                },
+              })
+            }
+          })
+        )
+      ).filter((item): item is Exclude<typeof item, undefined> => !!item)
+
+      console.log(
+        `${nouveauxÉvénements.length} nouveaux événements ${ProjectGFInvalidées.type} ou ${ProjectGFValidées.type} vont être ajoutés`
+      )
+
+      await EventStore.bulkCreate(nouveauxÉvénements.map(toPersistance), { transaction })
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  down: async () => {},
+}
