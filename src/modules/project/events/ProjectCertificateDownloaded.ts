112e017abfccd02e54020faf9d7afcf9e2595617
HanaeY <72154904+HanaeY@users.noreply.github.com>
2022-12-01 16:14:57 +0100

    Revert ":card_file_box: (1320): ajout des projets lauréats entre le 15/04/2020 et le 21/10/2020 dans les stats d'utilisation (attestation téléchargée) (#775)"
    This reverts commit a13ae91f97a083eb00679b5d8d8b8d9c17db061f.

---
+++ src/modules/project/events/ProjectCertificateDownloaded.ts
@@ -0,0 +1,20 @@
+import { BaseDomainEvent, DomainEvent } from '@core/domain'
+
+export interface ProjectCertificateDownloadedPayload {
+  certificateFileId: string
+  projectId: string
+  downloadedBy: string
+}
+export class ProjectCertificateDownloaded
+  extends BaseDomainEvent<ProjectCertificateDownloadedPayload>
+  implements DomainEvent
+{
+  public static type: 'ProjectCertificateDownloaded' = 'ProjectCertificateDownloaded'
+
+  public type = ProjectCertificateDownloaded.type
+  currentVersion = 1
+
+  aggregateIdFromPayload(payload: ProjectCertificateDownloadedPayload) {
+    return payload.projectId
+  }
+}
