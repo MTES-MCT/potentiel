eae366816e5fbfebeb5702e6579d08f80c803c9f
flodlc <f.delacomble@gmail.com>
2022-12-08 14:18:33 +0100

    ♻️ suppression d'un test et traduction

--- src/views/components/actions/dreal.spec.ts
+++ src/views/components/actions/dreal.spec.ts
@@ -3,53 +3,40 @@ import ROUTES from '@routes'
 import makeFakeProject from '../../../__tests__/fixtures/project'
 
 describe('drealActions', () => {
-  describe('when GF has no status', () => {
-    it('should return a link for the action "Marquer la garantie financière comme validée"', () => {
-      const fakeProject = makeFakeProject({
-        id: '1',
-        garantiesFinancières: { id: '1', statut: null },
-      })
-      const result = drealActions(fakeProject)
-      expect(result).toEqual([])
+  it(`Etant donné un projet avec garanties financières dont le statut est "à traiter"
+      Alors l'appel à drealActions devrait retourner une seul action 'Marquer la garantie financière comme validée'`, () => {
+    const fakeProject = makeFakeProject({
+      id: '1',
+      garantiesFinancières: { id: '1', statut: 'à traiter' },
+    })
+    const result = drealActions(fakeProject)
+    expect(result).toHaveLength(1)
+    expect(result[0]).toMatchObject({
+      title: 'Marquer la garantie financière comme validée',
+      link: ROUTES.VALIDER_GF({
+        projetId: fakeProject.id,
+      }),
     })
   })
-  describe('when GF status is "à traiter"', () => {
-    it('should return a link for the action "Marquer la garantie financière comme validée"', () => {
-      const fakeProject = makeFakeProject({
-        id: '1',
-        garantiesFinancières: { id: '1', statut: 'à traiter' },
-      })
-      const result = drealActions(fakeProject)
-      expect(result).toHaveLength(1)
-      expect(result[0]).toMatchObject({
-        title: 'Marquer la garantie financière comme validée',
-        link: ROUTES.VALIDER_GF({
-          projetId: fakeProject.id,
-        }),
-      })
+  it(`Etant donné un projet avec garanties financières dont le statut est "validé"
+      Alors l'appel à drealActions devrait retourner une seul action 'Marquer la garantie financière comme à traiter'`, () => {
+    const fakeProject = makeFakeProject({
+      id: '1',
+      garantiesFinancières: { id: '1', statut: 'validé' },
+    })
+    const result = drealActions(fakeProject)
+    expect(result).toHaveLength(1)
+    expect(result[0]).toMatchObject({
+      title: 'Marquer la garantie financière comme à traiter',
+      link: ROUTES.INVALIDER_GF({
+        projetId: fakeProject.id,
+      }),
     })
   })
-  describe('when GF status is "validé"', () => {
-    it('should return a link for the action "Marquer la garantie financière comme à traiter"', () => {
-      const fakeProject = makeFakeProject({
-        id: '1',
-        garantiesFinancières: { id: '1', statut: 'validé' },
-      })
-      const result = drealActions(fakeProject)
-      expect(result).toHaveLength(1)
-      expect(result[0]).toMatchObject({
-        title: 'Marquer la garantie financière comme à traiter',
-        link: ROUTES.INVALIDER_GF({
-          projetId: fakeProject.id,
-        }),
-      })
-    })
-  })
-  describe('when the project has no gf property', () => {
-    it('should return an empty action array', () => {
-      const fakeProject = makeFakeProject()
-      const result = drealActions(fakeProject)
-      expect(result).toEqual([])
-    })
+  it(`Etant donné un projet sans garanties financières
+      Alors l'appel à drealActions devrait retourner un tableau vide`, () => {
+    const fakeProject = makeFakeProject()
+    const result = drealActions(fakeProject)
+    expect(result).toEqual([])
   })
 })
