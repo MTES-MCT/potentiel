4b8b55c4d89278040b4ab61c612ce233f073da78
flodlc <f.delacomble@gmail.com>
2022-12-08 10:15:21 +0100

    🐛 correction migration

--- src/infra/sequelize/migrations/20221208151500-suppression-colonnes-gf-dans-project.ts
+++ src/infra/sequelize/migrations/20221208151500-suppression-colonnes-gf-dans-project.ts
@@ -1,16 +1,10 @@
-import { DataTypes, QueryInterface } from 'sequelize'
+import { QueryInterface } from 'sequelize'
 
 export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.removeColumn('projects', 'garantiesFinancieresDueOn')
     await queryInterface.removeColumn('projects', 'garantiesFinancieresRelanceOn')
-    await queryInterface.removeColumn('projects', 'garantiesFinancieresSubmittedOn')
-    await queryInterface.removeColumn('projects', 'garantiesFinancieresFile')
-    await queryInterface.removeColumn('projects', 'garantiesFinancieresSubmittedBy')
-    await queryInterface.addColumn('projects', 'garantiesFinancieresDate', {
-      type: DataTypes.JSON,
-      allowNull: true,
-    })
+    await queryInterface.removeColumn('projects', 'garantiesFinancieresFileId')
   },
 
   down: async () => {},
