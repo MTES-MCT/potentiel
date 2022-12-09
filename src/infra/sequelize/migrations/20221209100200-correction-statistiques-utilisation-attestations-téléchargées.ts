7b7db05d933bd1318dd731281ae0e46564432060
Julien Pavon <julien.pavon@gmail.com>
2022-12-09 10:41:55 +0100

    🗃️ (DB): Correction du type des stats d'utilisation "attestationTéléchargée" (#802)

---
+++ src/infra/sequelize/migrations/20221209100200-correction-statistiques-utilisation-attestations-téléchargées.ts
@@ -0,0 +1,24 @@
+import { QueryInterface } from 'sequelize'
+import { StatistiquesUtilisation } from '../tableModels'
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await StatistiquesUtilisation.update(
+        { type: 'attestationTéléchargée' },
+        {
+          where: { type: 'AttestationTéléchargée' },
+          transaction,
+        }
+      )
+
+      await transaction.commit()
+    } catch (e) {
+      console.error(e)
+      await transaction.rollback()
+    }
+  },
+
+  async down(queryInterface: QueryInterface) {},
+}
