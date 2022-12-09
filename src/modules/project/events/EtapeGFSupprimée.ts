3fbdb462bd2a92df85aac6f7d5663f941dec862b
HanaeY <hanae.yamashita@lilo.org>
2022-12-09 16:07:25 +0100

    🎨 (PR FOLLOW UP): modifs diverses dans EtapeGFSupprimée + prettier

--- src/modules/project/events/EtapeGFSupprimée.ts
+++ src/modules/project/events/EtapeGFSupprimée.ts
@@ -1,9 +1,9 @@
 import { BaseDomainEvent, DomainEvent } from '@core/domain'
 
-// Suppression des GF quelque soient leur statut pour un projet non soumis à GF
+// Suppression des GF quelque soit leur statut pour un projet non soumis à GF
 // Evénement émis dans une migration pour corriger des projets
 
-export interface EtapeGFSuppriméePayload {
+export type EtapeGFSuppriméePayload = {
   projetId: string
 }
 export class EtapeGFSupprimée
