e9833e41de701d141575d5a03e154e34b2af1686
Julien Pavon <julien.pavon@gmail.com>
2022-12-09 15:58:06 +0100

    🗃️ (1328): Ajout d'une valeur par defaut pour fullName

--- src/infra/sequelize/projections/user/user.model.ts
+++ src/infra/sequelize/projections/user/user.model.ts
@@ -16,6 +16,7 @@ export const MakeUserModel = (sequelize) => {
       fullName: {
         type: DataTypes.STRING,
         allowNull: false,
+        defaultValue: '',
       },
       email: {
         type: DataTypes.STRING,
