723e5aa15a3c45ea7a9b98196d69e01ca0e9d329
flodlc <f.delacomble@gmail.com>
2022-12-12 17:11:20 +0100

    ⚰️ suppression de code mort de GF dans ProjectStep

--- src/infra/sequelize/projections/projectStep/updates/onProjectStepRemoved.ts
+++ src/infra/sequelize/projections/projectStep/updates/onProjectStepRemoved.ts
@@ -1,36 +1,17 @@
-import { Op } from 'sequelize'
 import { logger } from '@core/utils'
-import { ProjectGFRemoved, ProjectGFWithdrawn, ProjectPTFRemoved } from '@modules/project'
+import { ProjectPTFRemoved } from '@modules/project'
 
-type StepRemovedEvent = ProjectPTFRemoved | ProjectGFRemoved | ProjectGFWithdrawn
-
-const StepTypeByEventType: Record<StepRemovedEvent['type'], string> = {
-  [ProjectPTFRemoved.type]: 'ptf',
-  [ProjectGFRemoved.type]: 'garantie-financiere',
-  [ProjectGFWithdrawn.type]: 'garantie-financiere',
-}
-
-export const onProjectStepRemoved = (models) => async (event: StepRemovedEvent) => {
+export const onProjectStepRemoved = (models) => async (event: ProjectPTFRemoved) => {
   const { ProjectStep } = models
 
   const { projectId } = event.payload
   try {
-    if (event.type === ProjectGFWithdrawn.type) {
-      await ProjectStep.destroy({
-        where: {
-          type: StepTypeByEventType[event.type],
-          projectId,
-        },
-      })
-    } else {
-      await ProjectStep.destroy({
-        where: {
-          type: StepTypeByEventType[event.type],
-          projectId,
-          [Op.or]: [{ status: null }, { status: 'à traiter' }],
-        },
-      })
-    }
+    await ProjectStep.destroy({
+      where: {
+        type: 'ptf',
+        projectId,
+      },
+    })
   } catch (e) {
     logger.error(e)
   }
