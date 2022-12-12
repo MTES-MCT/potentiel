6b7e17b71821d45cb87636c1a20abb92a5b93253
HanaeY <hanae.yamashita@lilo.org>
2022-12-12 14:55:57 +0100

    🎨 (PR FOLLOW UP): suppression export type payload event

--- src/modules/project/events/EtapeGFSupprimée.ts
+++ src/modules/project/events/EtapeGFSupprimée.ts
@@ -3,7 +3,7 @@ import { BaseDomainEvent, DomainEvent } from '@core/domain'
 // Suppression des GF quelque soit leur statut pour un projet non soumis à GF
 // Evénement émis dans une migration pour corriger des projets
 
-export type EtapeGFSuppriméePayload = {
+type EtapeGFSuppriméePayload = {
   projetId: string
 }
 export class EtapeGFSupprimée
