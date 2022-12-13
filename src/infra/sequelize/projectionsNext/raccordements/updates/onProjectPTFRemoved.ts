940c0bf735881f9e168f2e430533a36f1720e8bb
flodlc <f.delacomble@gmail.com>
2022-12-13 17:34:51 +0100

    ♻️ gestion des erreurs

--- src/infra/sequelize/projectionsNext/raccordements/updates/onProjectPTFRemoved.ts
+++ src/infra/sequelize/projectionsNext/raccordements/updates/onProjectPTFRemoved.ts
@@ -1,13 +1,28 @@
 import { ProjectPTFRemoved } from '@modules/project'
 import { Raccordements, RaccordementsProjector } from '../raccordements.model'
+import { logger } from '@core/utils'
+import { ProjectionEnEchec } from '@modules/shared'
 
 export default RaccordementsProjector.on(ProjectPTFRemoved, async (évènement, transaction) => {
   const {
     payload: { projectId },
   } = évènement
 
-  await Raccordements.update(
-    { ptfDateDeSignature: null, ptfEnvoyéePar: null, ptfFichierId: null },
-    { where: { projetId: projectId }, transaction }
-  )
+  try {
+    await Raccordements.update(
+      { ptfDateDeSignature: null, ptfEnvoyéePar: null, ptfFichierId: null },
+      { where: { projetId: projectId }, transaction }
+    )
+  } catch (error) {
+    logger.error(
+      new ProjectionEnEchec(
+        `Erreur lors du traitement de l'évènement ProjectPTFRemoved`,
+        {
+          évènement,
+          nomProjection: 'Raccordements',
+        },
+        error
+      )
+    )
+  }
 })
--- src/infra/sequelize/projectionsNext/raccordements/updates/onProjectPTFSubmitted.ts
+++ src/infra/sequelize/projectionsNext/raccordements/updates/onProjectPTFSubmitted.ts
@@ -1,19 +1,32 @@
 import { ProjectPTFSubmitted } from '@modules/project'
 import { Raccordements, RaccordementsProjector } from '../raccordements.model'
+import { logger } from '@core/utils'
+import { ProjectionEnEchec } from '@modules/shared'
 
 export default RaccordementsProjector.on(ProjectPTFSubmitted, async (évènement, transaction) => {
   const {
     payload: { projectId, fileId, submittedBy, ptfDate },
   } = évènement
 
-  const raccordement = await Raccordements.findOne({ where: { projetId: projectId }, transaction })
-
-  await Raccordements.update(
-    {
-      ptfFichierId: fileId,
-      ptfDateDeSignature: ptfDate,
-      ptfEnvoyéePar: submittedBy,
-    },
-    { where: { projetId: projectId }, transaction }
-  )
+  try {
+    await Raccordements.update(
+      {
+        ptfFichierId: fileId,
+        ptfDateDeSignature: ptfDate,
+        ptfEnvoyéePar: submittedBy,
+      },
+      { where: { projetId: projectId }, transaction }
+    )
+  } catch (error) {
+    logger.error(
+      new ProjectionEnEchec(
+        `Erreur lors du traitement de l'évènement ProjectPTFSubmitted`,
+        {
+          évènement,
+          nomProjection: 'Raccordements',
+        },
+        error
+      )
+    )
+  }
 })
