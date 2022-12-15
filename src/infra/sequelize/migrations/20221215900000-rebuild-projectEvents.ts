b6fd127fca442fab7e4cde0d2b8f8b7a8920808a
flodlc <f.delacomble@gmail.com>
2022-12-15 19:11:03 +0100

    ♻️ suppression d'un test et rebuild de la projection ProjectEvents

---
+++ src/infra/sequelize/migrations/20221215900000-rebuild-projectEvents.ts
@@ -0,0 +1,19 @@
+import { QueryInterface } from 'sequelize'
+import { ProjectEventProjector } from '../projectionsNext/projectEvents'
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      await ProjectEventProjector.rebuild(transaction)
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  down: async () => {},
+}
