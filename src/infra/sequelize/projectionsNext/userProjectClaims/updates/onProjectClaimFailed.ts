5e3bda4464dbd03411f672f129ad0d9aec6ddf12
Julien Pavon <julien.pavon@gmail.com>
2022-12-27 11:48:23 +0100

    🔥 (1375): Suppression code comment

--- src/infra/sequelize/projectionsNext/userProjectClaims/updates/onProjectClaimFailed.ts
+++ src/infra/sequelize/projectionsNext/userProjectClaims/updates/onProjectClaimFailed.ts
@@ -1,25 +1,3 @@
-// import { logger } from '@core/utils'
-// import { ProjectClaimFailed } from '@modules/projectClaim/events'
-
-// export const onProjectClaimFailed = (models) => async (event: ProjectClaimFailed) => {
-//   const { UserProjectClaims } = models
-//   const { claimedBy, projectId } = event.payload
-
-//   try {
-//     const [userProjectClaim, created] = await UserProjectClaims.findOrCreate({
-//       where: { userId: claimedBy, projectId },
-//       defaults: { failedAttempts: 1 },
-//     })
-
-//     if (!created) {
-//       userProjectClaim.failedAttempts++
-//       await userProjectClaim.save()
-//     }
-//   } catch (e) {
-//     logger.error(e)
-//   }
-// }
-
 import { UserProjectClaims, UserProjectClaimsProjector } from '../userProjectClaims.model'
 import { ProjectClaimFailed } from '@modules/projectClaim'
 import { logger } from '@core/utils'
