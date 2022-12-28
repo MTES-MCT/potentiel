3ab7a2f396f552830c41895813ed6f6a0cdefb3e
Julien Pavon <julien.pavon@gmail.com>
2022-12-28 09:33:44 +0100

    🔀 🐛 (BUG): Correction du lien d'aide de modalités d'instruction (#854)
    🐛 (BUG): Correction du lien d'aide de modalites d'instruction

--- src/views/components/InfoLienGuideUtilisationCDC.tsx
+++ src/views/components/InfoLienGuideUtilisationCDC.tsx
@@ -4,7 +4,7 @@ import { ExternalLink } from './UI'
 export const InfoLienGuideUtilisationCDC = () => (
   <span>
     Pour plus d'informations sur les modalités d'instruction veuillez consulter cette&nbsp;
-    <ExternalLink href="https://docs.potentiel.beta.gouv.fr/info/guide-dutilisation-potentiel/comment-faire-une-demande-de-modification-ou-informer-le-prefet-dun-changement">
+    <ExternalLink href="https://docs.potentiel.beta.gouv.fr/faire-une-demande-de-modification">
       page d'aide
     </ExternalLink>
     .
