9ce3c45d58241484f92681c861540a57e8e82df4
Julien Pavon <julien.pavon@gmail.com>
2023-01-06 09:33:19 +0100

    🐛 (Projet): Correction d'erreur lors de la mise à jour des fournisseurs (#876)
    * 🚧 (FIX): Ajout scenario si pas de details dans le projet
    
    * 🐛 (Projet): Correction erreur quand le projet n'a pas de details

--- src/infra/sequelize/projections/project/updates/onProjectFournisseursUpdated.integration.ts
+++ src/infra/sequelize/projections/project/updates/onProjectFournisseursUpdated.integration.ts
@@ -6,23 +6,23 @@ import { ProjectFournisseursUpdated, Fournisseur } from '@modules/project'
 import { UniqueEntityID } from '@core/domain'
 
 describe('project.onProjectFournisseursUpdated', () => {
-  const { Project } = models
-  const projectId = new UniqueEntityID().toString()
-  const project = makeFakeProject({
-    id: projectId,
-    details: {
-      'Nom du fabricant \n(Modules ou films)': 'oldFabricant1',
-      'Nom du fabricant \n(Postes de conversion)': 'oldFabricant2',
-    },
-    evaluationCarbone: 50,
-  })
-
   beforeAll(async () => {
     await resetDatabase()
-    await Project.bulkCreate([project])
   })
 
   it('should update the project fournisseurs and evaluation carbone', async () => {
+    const { Project } = models
+    const projectId = new UniqueEntityID().toString()
+    const project = makeFakeProject({
+      id: projectId,
+      details: {
+        'Nom du fabricant \n(Modules ou films)': 'oldFabricant1',
+        'Nom du fabricant \n(Postes de conversion)': 'oldFabricant2',
+      },
+      evaluationCarbone: 50,
+    })
+    await Project.bulkCreate([project])
+
     const newEvaluationCarbone = 100
     const newFournisseurs: Fournisseur[] = [
       {
@@ -41,6 +41,39 @@ describe('project.onProjectFournisseursUpdated', () => {
     const updatedProject = await Project.findByPk(projectId)
     expect(updatedProject.details).toMatchObject({
       'Nom du fabricant \n(Postes de conversion)': 'oldFabricant2',
+      'Nom du fabricant \n(Modules ou films)': 'newFabricant1',
+      'Nom du fabricant (Cellules)': 'newFabricant2',
+    })
+    expect(updatedProject.evaluationCarbone).toEqual(newEvaluationCarbone)
+  })
+
+  it('should update the project fournisseurs and evaluation carbone even if no details on the initial project', async () => {
+    const { Project } = models
+    const projectId = new UniqueEntityID().toString()
+    const project = makeFakeProject({
+      id: projectId,
+      details: undefined,
+      evaluationCarbone: 50,
+    })
+    await Project.bulkCreate([project])
+
+    const newEvaluationCarbone = 100
+    const newFournisseurs: Fournisseur[] = [
+      {
+        kind: 'Nom du fabricant \n(Modules ou films)',
+        name: 'newFabricant1',
+      },
+      { kind: 'Nom du fabricant (Cellules)', name: 'newFabricant2' },
+    ]
+
+    await onProjectFournisseursUpdated(models)(
+      new ProjectFournisseursUpdated({
+        payload: { projectId, newFournisseurs, newEvaluationCarbone, updatedBy: 'someone' },
+      })
+    )
+
+    const updatedProject = await Project.findByPk(projectId)
+    expect(updatedProject.details).toMatchObject({
       'Nom du fabricant \n(Modules ou films)': 'newFabricant1',
       'Nom du fabricant (Cellules)': 'newFabricant2',
     })
