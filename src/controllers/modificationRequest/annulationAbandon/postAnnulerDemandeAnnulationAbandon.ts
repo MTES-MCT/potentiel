b4ff010ca6041b1d74880444e4cf5510f296154a
Julien Pavon <julien.pavon@gmail.com>
2023-01-18 15:23:41 +0100

    ♻️ (Controller): Passage du controller en safeAsync

--- src/controllers/modificationRequest/annulationAbandon/postAnnulerDemandeAnnulationAbandon.ts
+++ src/controllers/modificationRequest/annulationAbandon/postAnnulerDemandeAnnulationAbandon.ts
@@ -2,54 +2,64 @@ import * as yup from 'yup'
 
 import { annulerDemandeAnnulationAbandon, ensureRole } from '@config'
 import { logger } from '@core/utils'
-import asyncHandler from '../../helpers/asyncHandler'
+import safeAsyncHandler from '../../helpers/safeAsyncHandler'
 import { EntityNotFoundError, UnauthorizedError } from '@modules/shared'
 import routes from '../../../routes'
-import {
-  errorResponse,
-  notFoundResponse,
-  unauthorizedResponse,
-  validateRequestBody,
-} from '../../helpers'
+import { errorResponse, notFoundResponse, unauthorizedResponse } from '../../helpers'
 import { v1Router } from '../../v1Router'
+import { addQueryParams } from 'src/helpers/addQueryParams'
 
-const requestBodySchema = yup.object({
-  demandeId: yup.string().uuid().required(),
+const schema = yup.object({
+  body: yup.object({
+    demandeId: yup.string().uuid().required(),
+  }),
 })
 
 v1Router.post(
   routes.POST_ANNULER_DEMANDE_ANNULATION_ABANDON,
   ensureRole(['porteur-projet']),
-  asyncHandler(async (request, response) => {
-    validateRequestBody(request.body, requestBodySchema)
-      .asyncAndThen((body) => {
-        const { user } = request
-        const { demandeId } = body
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({ request, response, error }) =>
+        response.redirect(
+          addQueryParams(routes.USER_LIST_REQUESTS, {
+            ...error.errors,
+          })
+        ),
+    },
+    async (request, response) => {
+      const { user } = request
+      const { demandeId } = request.body
 
-        return annulerDemandeAnnulationAbandon({
-          user,
-          demandeId,
-        }).map(() => demandeId)
-      })
-      .match(
-        (demandeId) =>
-          response.redirect(
+      return annulerDemandeAnnulationAbandon({
+        user,
+        demandeId,
+      }).match(
+        () => {
+          return response.redirect(
             routes.SUCCESS_OR_ERROR_PAGE({
-              success: 'Votre demande a bien été annulée.',
+              success: `Votre demande a bien été annulée.`,
               redirectUrl: routes.DEMANDE_PAGE_DETAILS(demandeId),
-              redirectTitle: 'Retourner à la demande',
+              redirectTitle: 'Retourner à la page de la demande',
             })
-          ),
-        (e) => {
-          if (e instanceof EntityNotFoundError) {
+          )
+        },
+        (error) => {
+          if (error instanceof EntityNotFoundError) {
             return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-          } else if (e instanceof UnauthorizedError) {
+          } else if (error instanceof UnauthorizedError) {
             return unauthorizedResponse({ request, response })
           }
 
-          logger.error(e)
-          return errorResponse({ request, response })
+          logger.error(error)
+          return errorResponse({
+            request,
+            response,
+            customMessage: `Il y a eu une erreur lors de l'annulation de votre demande. Merci de recommencer.`,
+          })
         }
       )
-  })
+    }
+  )
 )
