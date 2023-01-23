7a8c6340ddf80507e530f82269db212682225d05
HubM <hubmoncenis@gmail.com>
2023-01-23 14:25:42 +0100

    🔥 supprimer code inutile

--- src/views/components/UI/organisms/PaginationPanel.tsx
+++ src/views/components/UI/organisms/PaginationPanel.tsx
@@ -16,28 +16,7 @@ export function PaginationPanel({
   titreItems,
   pagination: { limiteParPage, page },
 }: Props) {
-  const pageNumbers = [0, 1]
   const limitePageOptions = [5, 10, 20, 50, 100]
-
-  if (nombreDePage <= 5) {
-    pageNumbers.push(2, 3, 4)
-  } else {
-    if (page > 1 && page < nombreDePage - 2) {
-      if (page > 2) {
-        pageNumbers.push(-1) // Will be "..."
-      }
-
-      pageNumbers.push(page)
-
-      if (page < nombreDePage - 3) {
-        pageNumbers.push(-1) // Will be "..."
-      }
-    } else {
-      pageNumbers.push(2, -1)
-    }
-
-    pageNumbers.push(nombreDePage - 2, nombreDePage - 1)
-  }
 
   return (
     <div className="flex justify-between items-center flex-wrap mt-6">
