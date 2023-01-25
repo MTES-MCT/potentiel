09d6d68ff9fd1cad9673dc06ed5f2bf54d77140f
HubM <hubmoncenis@gmail.com>
2023-01-25 16:00:28 +0100

    🎨 (controller): utilisation de safeAsyncHandler pour le controlleur de réponse à une demande de changement de puissance

--- src/controllers/modificationRequest/puissance/postRepondreDemandeChangementDePuissance.ts
+++ src/controllers/modificationRequest/puissance/postRepondreDemandeChangementDePuissance.ts
@@ -1,16 +1,14 @@
-import fs from 'fs'
+import * as yup from 'yup'
+import { createReadStream } from 'fs'
 import {
   acceptModificationRequest,
   ensureRole,
+  getModificationRequestAuthority,
   rejectModificationRequest,
-  requestConfirmation,
   updateModificationRequestStatus,
 } from '@config'
-import { logger } from '@core/utils'
-import { getModificationRequestAuthority } from '@infra/sequelize/queries'
+import { isStrictlyPositiveNumber, logger } from '@core/utils'
 import { addQueryParams } from '../../../helpers/addQueryParams'
-import { isStrictlyPositiveNumber } from '../../../helpers/formValidators'
-import { validateUniqueId } from '../../../helpers/validateUniqueId'
 import {
   ProjetDéjàClasséError,
   PuissanceVariationWithDecisionJusticeError,
@@ -22,184 +20,188 @@ import {
 } from '@modules/shared'
 import routes from '@routes'
 import { errorResponse, notFoundResponse, unauthorizedResponse } from '../../helpers'
-import asyncHandler from '../../helpers/asyncHandler'
 import { upload } from '../../upload'
 import { v1Router } from '../../v1Router'
+import safeAsyncHandler from '../../helpers/safeAsyncHandler'
+import { UniqueEntityID } from '@core/domain'
+
+const _handleSuccess = (response, modificationRequestId) => () => {
+  response.redirect(
+    routes.SUCCESS_OR_ERROR_PAGE({
+      success: 'Votre réponse a bien été enregistrée.',
+      redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
+      redirectTitle: 'Retourner à la demande',
+    })
+  )
+}
+
+const _handleErrors = (request, response, modificationRequestId) => (e) => {
+  if (e instanceof AggregateHasBeenUpdatedSinceError) {
+    return response.redirect(
+      addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
+        error: `Votre réponse n'a pas pu être prise en compte parce que la demande a été mise à jour entre temps. Merci de réessayer.`,
+      })
+    )
+  }
+
+  if (e instanceof PuissanceVariationWithDecisionJusticeError) {
+    return response.redirect(
+      addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
+        error: e.message,
+      })
+    )
+  }
+
+  if (e instanceof ProjetDéjàClasséError) {
+    return response.redirect(
+      addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
+        error: `Vous ne pouvez pas accepter cette demande de recours car le projet est déjà "classé". Le porteur a la possibilité d'annuler sa demande, ou bien vous pouvez la rejeter.`,
+      })
+    )
+  }
+
+  if (e instanceof EntityNotFoundError) {
+    return notFoundResponse({ request, response })
+  }
+
+  if (e instanceof UnauthorizedError) {
+    return unauthorizedResponse({ request, response })
+  }
+
+  logger.error(e)
+
+  return errorResponse({ request, response })
+}
+
+const schema = yup.object({
+  body: yup.object({
+    modificationRequestId: yup.string().uuid().required(),
+    type: yup
+      .mixed()
+      .oneOf(['puissance'])
+      .required('Le type est obligatoire')
+      .typeError(`Le type n'est pas valide`),
+    versionDate: yup.string().required(),
+    submitAccept: yup.string().nullable(),
+    submitRefuse: yup.string().nullable(),
+    isDecisionJustice: yup.boolean(),
+    statusUpdateOnly: yup.string().nullable(),
+    puissance: yup.string().required('La puissance est obligatoire'),
+  }),
+})
 
 v1Router.post(
   routes.POST_REPONDRE_DEMANDE_CHANGEMENT_PUISSANCE,
   ensureRole(['admin', 'dgec-validateur', 'dreal']),
   upload.single('file'),
-  asyncHandler(async (request, response) => {
-    // validateRequestBodyForErrorArray(request.body, requestBodySchema)
-    const {
-      user: { role },
-      body: {
-        modificationRequestId,
-        type,
-        versionDate,
-        submitAccept,
-        submitConfirm,
-        statusUpdateOnly,
-        puissance,
-        isDecisionJustice,
-      },
-    } = request
-
-    if (!validateUniqueId(modificationRequestId)) {
-      return notFoundResponse({ request, response, ressourceTitle: 'Demande' })
-    }
-
-    if (role === 'dreal') {
-      const authority = await getModificationRequestAuthority(modificationRequestId)
-
-      if (authority && authority !== role) {
-        return unauthorizedResponse({ request, response })
-      }
-    }
-
-    // There are two submit buttons on the form, named submitAccept and submitReject
-    // We know which one has been clicked when it has a string value
-    const estAccordé = typeof submitAccept === 'string'
-    const demanderConfirmation = typeof submitConfirm === 'string'
-
-    if (statusUpdateOnly) {
-      if (demanderConfirmation) {
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({
+        request: {
+          body: { modificationRequestId },
+        },
+        response,
+        error,
+      }) => {
+        console.log(error)
         return response.redirect(
           addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-            error: `Votre réponse n'a pas pu être prise en compte parce qu'il n'est pas possible de demander une confirmation si la demande a été traitée en dehors de Potentiel.`,
+            ...error.errors,
+          })
+        )
+      },
+    },
+    async (request, response) => {
+      const {
+        file,
+        user: { role },
+        body: {
+          modificationRequestId,
+          type,
+          versionDate,
+          submitAccept,
+          statusUpdateOnly,
+          puissance,
+          isDecisionJustice,
+        },
+      } = request
+
+      console.log('FINALLY')
+
+      if (role === 'dreal') {
+        const authority = await getModificationRequestAuthority(modificationRequestId)
+        if (authority && authority !== role) {
+          return unauthorizedResponse({ request, response })
+        }
+      }
+
+      const estAccordé = typeof submitAccept === 'string'
+
+      if (statusUpdateOnly) {
+        const newStatus = estAccordé ? 'acceptée' : 'rejetée'
+        return await updateModificationRequestStatus({
+          modificationRequestId: new UniqueEntityID(modificationRequestId),
+          versionDate: new Date(Number(versionDate)),
+          newStatus,
+          submittedBy: request.user,
+        }).match(
+          _handleSuccess(response, modificationRequestId),
+          _handleErrors(request, response, modificationRequestId)
+        )
+      }
+
+      if (!isStrictlyPositiveNumber(Number(puissance))) {
+        return response.redirect(
+          addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
+            error:
+              "La réponse n'a pas pu être envoyée: la puissance doit être un nombre supérieur à 0.",
           })
         )
       }
 
-      const newStatus = estAccordé ? 'acceptée' : 'rejetée'
+      const fichierRéponse = file && {
+        contents: createReadStream(file.path),
+        filename: file.originalname,
+      }
 
-      return await updateModificationRequestStatus({
-        modificationRequestId,
-        versionDate: new Date(Number(versionDate)),
-        newStatus,
-        submittedBy: request.user,
-      }).match(
-        _handleSuccess(response, modificationRequestId),
-        _handleErrors(request, response, modificationRequestId)
-      )
-    }
+      const courrierReponseIsOk = fichierRéponse || (estAccordé && isDecisionJustice)
+      if (!courrierReponseIsOk) {
+        return response.redirect(
+          addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
+            error:
+              "La réponse n'a pas pu être envoyée car il manque le courrier de réponse (obligatoire pour cette réponse).",
+          })
+        )
+      }
 
-    if (!isStrictlyPositiveNumber(puissance)) {
-      return response.redirect(
-        addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-          error:
-            "La réponse n'a pas pu être envoyée: la puissance doit être un nombre supérieur à 0.",
-        })
-      )
-    }
+      const acceptanceParams = { type, newPuissance: Number(puissance), isDecisionJustice }
+      if (!acceptanceParams) {
+        return errorResponse({ request, response })
+      }
 
-    const fichierRéponse = request.file && {
-      contents: fs.createReadStream(request.file.path),
-      filename: request.file.originalname,
-    }
+      if (estAccordé) {
+        return await acceptModificationRequest({
+          responseFile: fichierRéponse,
+          modificationRequestId: new UniqueEntityID(modificationRequestId),
+          versionDate: new Date(Number(versionDate)),
+          acceptanceParams,
+          submittedBy: request.user,
+        }).match(
+          _handleSuccess(response, modificationRequestId),
+          _handleErrors(request, response, modificationRequestId)
+        )
+      }
 
-    const courrierReponseIsOk = fichierRéponse || (estAccordé && isDecisionJustice)
-
-    if (!courrierReponseIsOk) {
-      return response.redirect(
-        addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-          error:
-            "La réponse n'a pas pu être envoyée car il manque le courrier de réponse (obligatoire pour cette réponse).",
-        })
-      )
-    }
-    const acceptanceParams = { type, newPuissance: puissance, isDecisionJustice }
-
-    if (!acceptanceParams) {
-      return errorResponse({ request, response })
-    }
-
-    if (estAccordé) {
-      return await acceptModificationRequest({
+      return rejectModificationRequest({
         responseFile: fichierRéponse,
-        modificationRequestId,
+        modificationRequestId: new UniqueEntityID(modificationRequestId),
         versionDate: new Date(Number(versionDate)),
-        acceptanceParams,
-        submittedBy: request.user,
+        rejectedBy: request.user,
       }).match(
         _handleSuccess(response, modificationRequestId),
         _handleErrors(request, response, modificationRequestId)
       )
     }
-
-    if (demanderConfirmation) {
-      return await requestConfirmation({
-        modificationRequestId,
-        versionDate: new Date(Number(versionDate)),
-        responseFile: fichierRéponse!,
-        confirmationRequestedBy: request.user,
-      }).match(
-        _handleSuccess(response, modificationRequestId),
-        _handleErrors(request, response, modificationRequestId)
-      )
-    }
-
-    return rejectModificationRequest({
-      responseFile: fichierRéponse,
-      modificationRequestId,
-      versionDate: new Date(Number(versionDate)),
-      rejectedBy: request.user,
-    }).match(
-      _handleSuccess(response, modificationRequestId),
-      _handleErrors(request, response, modificationRequestId)
-    )
-  })
+  )
 )
-
-function _handleSuccess(response, modificationRequestId) {
-  return () => {
-    response.redirect(
-      routes.SUCCESS_OR_ERROR_PAGE({
-        success: 'Votre réponse a bien été enregistrée.',
-        redirectUrl: routes.DEMANDE_PAGE_DETAILS(modificationRequestId),
-        redirectTitle: 'Retourner à la demande',
-      })
-    )
-  }
-}
-
-function _handleErrors(request, response, modificationRequestId) {
-  return (e) => {
-    if (e instanceof AggregateHasBeenUpdatedSinceError) {
-      return response.redirect(
-        addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-          error: `Votre réponse n'a pas pu être prise en compte parce que la demande a été mise à jour entre temps. Merci de réessayer.`,
-        })
-      )
-    }
-
-    if (e instanceof PuissanceVariationWithDecisionJusticeError) {
-      return response.redirect(
-        addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-          error: e.message,
-        })
-      )
-    }
-
-    if (e instanceof ProjetDéjàClasséError) {
-      return response.redirect(
-        addQueryParams(routes.DEMANDE_PAGE_DETAILS(modificationRequestId), {
-          error: `Vous ne pouvez pas accepter cette demande de recours car le projet est déjà "classé". Le porteur a la possibilité d'annuler sa demande, ou bien vous pouvez la rejeter.`,
-        })
-      )
-    }
-
-    if (e instanceof EntityNotFoundError) {
-      return notFoundResponse({ request, response })
-    }
-
-    if (e instanceof UnauthorizedError) {
-      return unauthorizedResponse({ request, response })
-    }
-
-    logger.error(e)
-
-    return errorResponse({ request, response })
-  }
-}
