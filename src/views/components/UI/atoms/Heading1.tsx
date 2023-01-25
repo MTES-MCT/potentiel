7755b39293f7586a4b8fb1e961a320b3c884b5cb
HanaeY <hanae.yamashita@lilo.org>
2023-01-25 15:46:35 +0100

    💄 (UI): Atoms headings

---
+++ src/views/components/UI/atoms/Heading1.tsx
@@ -0,0 +1,7 @@
+import React, { ComponentProps } from 'react'
+
+export const Heading1 = ({ children, className = '', ...props }: ComponentProps<'h1'>) => (
+  <h1 {...props} className={`text-3xl font-bold text-gray-900 ${className}`}>
+    {children}
+  </h1>
+)
