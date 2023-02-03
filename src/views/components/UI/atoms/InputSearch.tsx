e5623b44e0cc938a71ebc2922f3c4e58dbf0577c
HubM <hubmoncenis@gmail.com>
2023-02-03 17:49:07 +0100

    🎨 (storybook): finalisation du composant + export

--- src/views/components/UI/atoms/InputSearch.tsx
+++ src/views/components/UI/atoms/InputSearch.tsx
@@ -1,13 +1,11 @@
 import React, { ComponentProps, FC } from 'react'
 import { SearchIcon } from './icons'
 
-type InputSearchProps = ComponentProps<'input'> & {
-  placeholderTitle?: string
-}
-
+type InputSearchProps = ComponentProps<'input'>
 export const InputSearch: FC<InputSearchProps> = ({
   className = '',
-  placeholderTitle = 'Rechercher',
+  placeholder = 'Rechercher',
+  name = 'champ-recherche',
   ...props
 }) => {
   return (
@@ -16,18 +14,19 @@ export const InputSearch: FC<InputSearchProps> = ({
       style={{ fontFamily: 'Marianne, arial, sans-serif' }}
       {...props}
     >
-      <label className="hidden" htmlFor="champ-recherche">
+      <label className="hidden" htmlFor={name}>
         Recherche
       </label>
       <input
-        placeholder={placeholderTitle}
+        placeholder={placeholder}
         type="search"
-        id="champ-recherche"
-        name="champ-recherche"
+        id={name}
+        name={name}
         className="leading-none px-4 pt-2 pb-[5px] rounded-t-[4px] rounded-tr-[4px] rounded-b-none rounded-bl-none border-0 border-b-[3px] border-b-blue-france-sun-base focus:border-b-blue-france-sun-base bg-grey-950-base placeholder:italic"
       />
       <button
-        title={placeholderTitle}
+        type="submit"
+        title={placeholder}
         className="flex items-center py-2 px-2 lg:px-6 border-0 bg-blue-france-sun-base hover:bg-blue-france-sun-hover text-white"
       >
         <SearchIcon className="w-6 h-6 lg:mr-2" />
