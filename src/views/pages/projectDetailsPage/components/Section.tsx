2e11a967e65f89524e0a5b05608a12c7142ea5d6
HubM <hubmoncenis@gmail.com>
2023-02-08 14:59:30 +0100

    🎨 (composant): retravailler le composant + ajout d'une props Icon

--- src/views/pages/projectDetailsPage/components/Section.tsx
+++ src/views/pages/projectDetailsPage/components/Section.tsx
@@ -1,30 +1,25 @@
+import { IconType } from '@react-icons/all-files/lib'
 import { Heading2 } from '@views/components'
 import React from 'react'
-import { dataId } from '../../../../helpers/testId'
 
-interface SectionProps {
+type SectionProps = {
   title: string
-  defaultOpen?: boolean
   icon?: string
+  Icon?: IconType
   children: React.ReactNode
 }
 
-export const Section = ({ title, defaultOpen, children, icon }: SectionProps) => {
-  return (
-    <div className="panel p-4 flex-1 xs:w-full min-w-fit" {...dataId('projectDetails-section')}>
-      <Heading2 className={'section--title text-2xl'} {...dataId('visibility-toggle')}>
-        {icon ? (
-          <svg className="icon section-icon">
-            <use xlinkHref={'#' + icon}></use>
-          </svg>
-        ) : (
-          ''
-        )}
-        {title}
-      </Heading2>
-      <div className="" {...dataId('projectDetails-section-content')}>
-        {children}
-      </div>
-    </div>
-  )
-}
+export const Section = ({ title, children, icon, Icon }: SectionProps) => (
+  <div className="panel p-4 flex-1 xs:w-full min-w-fit">
+    <Heading2 className="section--title text-2xl">
+      {Icon && <Icon />}
+      {icon && (
+        <svg className="icon section-icon">
+          <use xlinkHref={'#' + icon}></use>
+        </svg>
+      )}
+      {title}
+    </Heading2>
+    {children}
+  </div>
+)
