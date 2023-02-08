bfc23dc97ea7fa0039ba22f4b90d877eba643324
HubM <hubmoncenis@gmail.com>
2023-02-08 15:04:56 +0100

    🎨 (composant): espace entre icone et titre

--- src/views/pages/projectDetailsPage/components/Section.tsx
+++ src/views/pages/projectDetailsPage/components/Section.tsx
@@ -11,8 +11,8 @@ type SectionProps = {
 
 export const Section = ({ title, children, icon, Icon }: SectionProps) => (
   <div className="panel p-4 flex-1 xs:w-full min-w-fit">
-    <Heading2 className="section--title text-2xl">
-      {Icon && <Icon />}
+    <Heading2 className="flex items-center section--title text-2xl">
+      {Icon && <Icon className="mr-[10px]" />}
       {icon && (
         <svg className="icon section-icon">
           <use xlinkHref={'#' + icon}></use>
