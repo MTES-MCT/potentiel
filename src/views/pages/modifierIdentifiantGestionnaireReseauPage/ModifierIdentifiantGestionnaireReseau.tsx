fce7e52b42a6a200790450b752a3727acb3cd20f
HubM <hubmoncenis@gmail.com>
2023-02-13 16:16:26 +0100

    🎨 (formulaire): ajout de label + information ancien identifiant

--- src/views/pages/modifierIdentifiantGestionnaireReseauPage/ModifierIdentifiantGestionnaireReseau.tsx
+++ src/views/pages/modifierIdentifiantGestionnaireReseauPage/ModifierIdentifiantGestionnaireReseau.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Request } from 'express'
 
-import { Button, Heading1, Input, PageTemplate, SecondaryLinkButton } from '@components'
+import { Button, Heading1, Input, Label, PageTemplate, SecondaryLinkButton } from '@components'
 import { hydrateOnClient } from '../../helpers'
 import routes from '@routes'
 
@@ -26,22 +26,27 @@ export const ModifierIdentifiantGestionnaireReseau = ({
             : "J'ajoute un numéro de gestionnaire réseau"}
         </Heading1>
       </div>
+
       <form
         action={routes.POST_MODIFIER_IDENTIFIANT_GESTIONNAIRE_RESEAU}
         method="post"
         className="flex flex-col gap-5"
       >
         <input type="hidden" name="projetId" value={id} />
-
-        <Input
-          type="text"
-          name="identifiantGestionnaireRéseau"
-          placeholder={
-            numeroGestionnaire
-              ? `Remplacer l'identifiant : ${numeroGestionnaire}`
-              : "Ajouter l'identifiant"
-          }
-        />
+        {numeroGestionnaire && (
+          <p className="m-0">Identifiant du gestionnaire réseau actuel : {numeroGestionnaire}</p>
+        )}
+        <div>
+          <Label required htmlFor="identifiantGestionnaireRéseau">
+            {numeroGestionnaire ? "Remplacer l'identifiant" : "Ajouter l'identifiant"}
+          </Label>
+          <Input
+            type="text"
+            id="identifiantGestionnaireRéseau"
+            name="identifiantGestionnaireRéseau"
+            placeholder="Saisir un nouvel identifiant"
+          />
+        </div>
 
         <div className="m-auto flex">
           <Button className="mr-1" type="submit">
