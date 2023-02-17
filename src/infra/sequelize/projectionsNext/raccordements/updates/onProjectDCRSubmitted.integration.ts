136e13efb981249e437f3bd77d391a53cd2a2789
HanaeY <hanae.yamashita@lilo.org>
2023-02-17 10:34:24 +0100

    ♻️ (raccordements): handler onProjectDCRSubmitted pour ajouter identifiant gestionnaire

---
+++ src/infra/sequelize/projectionsNext/raccordements/updates/onProjectDCRSubmitted.integration.ts
@@ -0,0 +1,36 @@
+import { UniqueEntityID } from '@core/domain'
+import { ProjectDCRSubmitted } from '@modules/project'
+import { resetDatabase } from '../../../helpers'
+import { Raccordements } from '../raccordements.model'
+import onProjectDCRSubmitted from './onProjectDCRSubmitted'
+
+describe('Raccordements.onProjectDCRSubmitted', () => {
+  const projetId = new UniqueEntityID().toString()
+  const identifiantGestionnaire = 'identifiant'
+
+  beforeEach(async () => await resetDatabase())
+
+  it(`Lorsque l'event ProjectDCRSubmitted survient avec un numéro de dossier renseigné, 
+      Alors l'identifiant de gestionnaire devrait être mise à jour`, async () => {
+    await Raccordements.create({
+      id: new UniqueEntityID().toString(),
+      projetId,
+    })
+
+    await onProjectDCRSubmitted(
+      new ProjectDCRSubmitted({
+        payload: {
+          projectId: projetId,
+          numeroDossier: identifiantGestionnaire,
+          submittedBy: 'id',
+          dcrDate: new Date('2022-01-01'),
+          fileId: '123',
+        },
+      })
+    )
+
+    expect(await Raccordements.findOne({ where: { projetId } })).toMatchObject({
+      identifiantGestionnaire,
+    })
+  })
+})
