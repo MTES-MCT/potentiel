b5d6cfe3e4341b9c91062062024900d5ff02c30c
HanaeY <hanae.yamashita@lilo.org>
2023-02-20 15:06:08 +0100

    ♻️ Renommage variables

--- src/infra/sequelize/projectionsNext/userDreal/updates/onDrealUserInvited.ts
+++ src/infra/sequelize/projectionsNext/userDreal/updates/onDrealUserInvited.ts
@@ -3,19 +3,19 @@ import { DrealUserInvited } from '@modules/authZ'
 import { ProjectionEnEchec } from '@modules/shared'
 import { UserDreal, UserDrealProjector } from '../userDreal.model'
 
-export default UserDrealProjector.on(DrealUserInvited, async (event, transaction) => {
+export default UserDrealProjector.on(DrealUserInvited, async (évènement, transaction) => {
   const {
-    payload: { region, userId },
-  } = event
+    payload: { region: dreal, userId },
+  } = évènement
 
   try {
-    await UserDreal.create({ dreal: region, userId }, { transaction })
+    await UserDreal.create({ dreal, userId }, { transaction })
   } catch (error) {
     logger.error(
       new ProjectionEnEchec(
         `Erreur lors du traitement de l'évènement DrealUserInvited`,
         {
-          évènement: event,
+          évènement,
           nomProjection: 'UserDreal.DrealUserInvited',
         },
         error
