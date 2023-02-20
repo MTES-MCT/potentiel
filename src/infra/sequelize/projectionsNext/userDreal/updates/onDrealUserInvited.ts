9510d0de21ec8b0301466c89ee2f3cf6a94c2008
HanaeY <hanae.yamashita@lilo.org>
2023-02-20 15:01:46 +0100

    🚧 Projecteur onDrealUserInvited implémentation cas nominal

--- src/infra/sequelize/projectionsNext/userDreal/updates/onDrealUserInvited.ts
+++ src/infra/sequelize/projectionsNext/userDreal/updates/onDrealUserInvited.ts
@@ -1,10 +1,10 @@
 import { DrealUserInvited } from '@modules/authZ'
-import { UserDrealProjector } from '../userDreal.model'
+import { UserDreal, UserDrealProjector } from '../userDreal.model'
 
-export default UserDrealProjector.on(DrealUserInvited, async (event) => {
-  return Promise.reject()
+export default UserDrealProjector.on(DrealUserInvited, async (event, transaction) => {
+  const {
+    payload: { region, userId },
+  } = event
+
+  await UserDreal.create({ dreal: region, userId })
 })
-// .create(({ payload: { region, userId } }) => ({
-//   userId,
-//   dreal: region,
-// }))
