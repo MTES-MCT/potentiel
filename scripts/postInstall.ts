98e2d9b75bca1dd9927f81563d2085d0d81ab34d
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2023-02-21 15:18:45 +0100

    🔀 ♻️ Amélioration autour du package.json (#949)
    * 🔧 Update type check script
    
    * 🔥 Remove lint:write script
    
    * 🚚 Rename prettier:check to format:check
    
    * ➕ Add rimraf
    
    * ♻️ Reorganisation des scripts dédiés aux migrations/seeds
    
    * 🔧 Suppression de la commande NPX dans le script bpm build:css
    
    * 🔥 Suppression d'un script non utilisé
    
    * ♻️ Change post install scripts
    
    * 🔧 Réorganisation des dépendances
    
    * ➕ Ajout glob en tant que dépendance de dev
    
    * 🔧 Recréation du package-lock suite à la réorganisation des dépendances
    
    * 🔧 Mise à jour script de lancement de db
    
    * 💚 Suppression du NODE_ENV production sur les build PR
    
    * 🔧 Suppression du copy dans le script npm prepare

--- scripts/postInstall.ts
+++ scripts/postInstall.ts
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import dotenv from 'dotenv'
 import { spawnSync } from 'child_process'
 
@@ -10,15 +9,6 @@ const executeMigrateScript = process.env.DO_NOT_EXECUTE_MIGRATE ? false : true
 postInstall()
 
 async function postInstall() {
-  if (['', 'local'].includes(NODE_ENV)) {
-    spawnSync('npm', ['run', 'configure-dev-env-variables'], { stdio: 'inherit' })
-
-    fs.rmSync('.husky/pre-commit', { force: true })
-    spawnSync('npm', ['run', 'configure-pre-commit'], { stdio: 'inherit' })
-
-    spawnSync('npm', ['run', 'configure-prepare-commit-msg'], { stdio: 'inherit' })
-  }
-
   if (!['', 'local', 'test'].includes(NODE_ENV)) {
     const build = spawnSync('npm', ['run', 'build'], { stdio: 'inherit' })
     if (build.status && build.status > 0) process.exit(build.status)
