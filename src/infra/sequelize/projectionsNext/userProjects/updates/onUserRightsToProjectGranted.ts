c1710deced4152397da9ea068f52bd734b31b0b8
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-02-22 16:20:28 +0100

    ♻️ Refacto UserRightsToProject event handler

--- src/infra/sequelize/projectionsNext/userProjects/updates/onUserRightsToProjectGranted.ts
+++ src/infra/sequelize/projectionsNext/userProjects/updates/onUserRightsToProjectGranted.ts
@@ -1,16 +1,33 @@
 import { logger } from '@core/utils';
 import { UserRightsToProjectGranted } from '@modules/authZ';
+import { ProjectionEnEchec } from '@modules/shared';
+import { UserProjects, UserProjectsProjector } from '../userProjects.model';
 
-export const onUserRightsToProjectGranted =
-  (models) => async (event: UserRightsToProjectGranted) => {
-    const { UserProjects } = models;
-    const { userId, projectId } = event.payload;
+export default UserProjectsProjector.on(
+  UserRightsToProjectGranted,
+  async (évènement, transaction) => {
+    const {
+      payload: { userId, projectId },
+    } = évènement;
     try {
-      await UserProjects.create({
-        userId,
-        projectId,
-      });
-    } catch (e) {
-      logger.error(e);
+      await UserProjects.create(
+        {
+          userId,
+          projectId,
+        },
+        { transaction },
+      );
+    } catch (error) {
+      logger.error(
+        new ProjectionEnEchec(
+          `Erreur lors du traitement de l'évènement UserRightsToProjectGranted`,
+          {
+            évènement,
+            nomProjection: 'UserProjects.UserRightsToProjectGranted',
+          },
+          error,
+        ),
+      );
     }
-  };
+  },
+);
