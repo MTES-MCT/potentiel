a11e774d83c96dc70131d926af5dd9b48822ddeb
HubM <hubmoncenis@gmail.com>
2023-02-22 16:24:41 +0100

    ♻️ export des eventHandlers

--- src/infra/sequelize/projectionsNext/userProjects/updates/index.ts
+++ src/infra/sequelize/projectionsNext/userProjects/updates/index.ts
@@ -1,3 +1,7 @@
 export * from './onProjectClaimed';
 export * from './onProjectClaimedByOwner';
 export * from './onToutAccèsAuProjetRevoqué';
+export * from './onUserInvitedToProject';
+export * from './onUserProjectsLinkedByContactEmail';
+export * from './onUserRightsToProjectGranted';
+export * from './onUserRightsToProjectRevoked';
