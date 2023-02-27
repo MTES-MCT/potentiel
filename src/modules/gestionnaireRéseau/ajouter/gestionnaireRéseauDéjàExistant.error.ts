93b80bee27f72ab3f2e9b94c42aad434fdc22f9d
HanaeY <hanae.yamashita@lilo.org>
2023-02-27 18:28:24 +0100

    💄 Wording erreur

--- src/modules/gestionnaireRéseau/ajouter/gestionnaireRéseauDéjàExistant.error.ts
+++ src/modules/gestionnaireRéseau/ajouter/gestionnaireRéseauDéjàExistant.error.ts
@@ -2,6 +2,6 @@ import { DomainError } from '@core/domain';
 
 export class GestionnaireRéseauDéjàExistantError extends DomainError {
   constructor() {
-    super(`Le gestionnaire de réseau existe déjà.`);
+    super(`Un gestionnaire de réseau est déjà enregistré avec ce code EIC.`);
   }
 }
