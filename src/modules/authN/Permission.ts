092c9f5e8ad834f89caa2c71cb5824c0620cb781
HanaeY <hanae.yamashita@lilo.org>
2023-03-03 11:34:09 +0100

    🎨 (TEMPORAIRE): Réactiver fonctionnalité ajout gestionnaire pour les tests

--- src/modules/authN/Permission.ts
+++ src/modules/authN/Permission.ts
@@ -17,7 +17,7 @@ import { PermissionListerProjetsÀNotifier } from '@modules/notificationCandidat
 import {
   PermissionListerGestionnairesRéseau,
   PermissionConsulterGestionnaireRéseau,
-  //PermissionAjouterGestionnaireRéseau,
+  PermissionAjouterGestionnaireRéseau,
 } from '@modules/gestionnaireRéseau';
 
 export type Permission = {
@@ -74,7 +74,7 @@ export const getPermissions = ({ role }: { role: UserRole }): Array<Permission> 
         PermissionModifierIdentifiantGestionnaireReseau,
         PermissionListerGestionnairesRéseau,
         PermissionConsulterGestionnaireRéseau,
-        //PermissionAjouterGestionnaireRéseau,
+        PermissionAjouterGestionnaireRéseau,
       ];
     case 'dgec-validateur':
       return [
@@ -89,7 +89,7 @@ export const getPermissions = ({ role }: { role: UserRole }): Array<Permission> 
         PermissionModifierIdentifiantGestionnaireReseau,
         PermissionListerGestionnairesRéseau,
         PermissionConsulterGestionnaireRéseau,
-        //PermissionAjouterGestionnaireRéseau,
+        PermissionAjouterGestionnaireRéseau,
       ];
     case 'acheteur-obligé':
     case 'ademe':
