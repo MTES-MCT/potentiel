b06ba72af9f14ebfead714ff75c11124b89795cc
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-03-06 10:27:14 +0100

    🐛 Clean index

--- src/infra/sequelize/projectionsNext/project/updates/index.ts
+++ src/infra/sequelize/projectionsNext/project/updates/index.ts
@@ -1,122 +1,32 @@
-import { EventBus } from '@core/domain';
-import { logger } from '@core/utils';
-import {
-  AppelOffreProjetModifié,
-  CovidDelayGranted,
-  ProjectAbandoned,
-  ProjectActionnaireUpdated,
-  ProjectCertificateGenerated,
-  ProjectCertificateObsolete,
-  ProjectCertificateRegenerated,
-  ProjectCertificateUpdated,
-  ProjectClasseGranted,
-  ProjectCompletionDueDateCancelled,
-  ProjectCompletionDueDateSet,
-  ProjectDataCorrected,
-  ProjectDCRDueDateCancelled,
-  ProjectDCRDueDateSet,
-  ProjectFournisseursUpdated,
-  ProjectImported,
-  CahierDesChargesChoisi,
-  ProjectNotificationDateSet,
-  ProjectNotified,
-  ProjectProducteurUpdated,
-  ProjectPuissanceUpdated,
-  ProjectReimported,
-  IdentifiantPotentielPPE2Batiment2Corrigé,
-  DonnéesDeRaccordementRenseignées,
-  LegacyAbandonSupprimé,
-  AbandonProjetAnnulé,
-} from '@modules/project';
-import { ContratEDFRapprochéAutomatiquement, ContratEDFMisAJour } from '@modules/edf';
-import { ProjectClaimed, ProjectClaimedByOwner } from '@modules/projectClaim';
-import { onAppelOffreProjetModifié } from './onAppelOffreProjetModifié';
-import { onCovidDelayGranted } from './onCovidDelayGranted';
-import { onProjectAbandoned } from './onProjectAbandoned';
-import { onProjectActionnaireUpdated } from './onProjectActionnaireUpdated';
-import { onProjectCertificateGenerated } from './onProjectCertificateGenerated';
-import { onProjectCertificateRegenerated } from './onProjectCertificateRegenerated';
-import { onProjectCertificateUpdated } from './onProjectCertificateUpdated';
-import { onProjectCertificateObsolete } from './onProjectCertificateObsolete';
-import { onProjectClaimed } from './onProjectClaimed';
-import { onProjectClasseGranted } from './onProjectClasseGranted';
-import { onProjectCompletionDueDateCancelled } from './onProjectCompletionDueDateCancelled';
-import { onProjectCompletionDueDateSet } from './onProjectCompletionDueDateSet';
-import { onProjectDataCorrected } from './onProjectDataCorrected';
-import { onProjectDCRDueDateCancelled } from './onProjectDCRDueDateCancelled';
-import { onProjectDCRDueDateSet } from './onProjectDCRDueDateSet';
-import { onProjectFournisseursUpdated } from './onProjectFournisseursUpdated';
-import { onProjectImported } from './onProjectImported';
-import { onCahierDesChargesChoisi } from './onCahierDesChargesChoisi';
-import { onProjectNotificationDateSet } from './onProjectNotificationDateSet';
-import { onProjectProducteurUpdated } from './onProjectProducteurUpdated';
-import { onProjectPuissanceUpdated } from './onProjectPuissanceUpdated';
-import { onProjectReimported } from './onProjectReimported';
-import { onContratEDFRapprochéAutomatiquement } from './onContratEDFRapprochéAutomatiquement';
-import { onContratEDFMisAJour } from './onContratEDFMisAJour';
-import {
-  ContratEnedisRapprochéAutomatiquement,
-  ContratEnedisMisAJour,
-} from '../../../../../modules/enedis';
-import { onContratEnedisMisAJour } from './onContratEnedisMisAJour';
-import { onContratEnedisRapprochéAutomatiquement } from './onContratEnedisRapprochéAutomatiquement';
-import { onIdentifiantPotentielPPE2Batiment2Corrigé } from './onIdentifiantPotentielPPE2Batiment2Corrigé';
-import { onDonnéesDeRaccordementRenseignées } from './onDonnéesDeRaccordementRenseignées';
-
-import { onLegacyAbandonSupprimé } from './onLegacyAbandonSupprimé';
-import { onAbandonProjetAnnulé } from './onAbandonProjetAnnulé';
-import { onProjectClaimedByOwner } from './onProjectClaimedByOwner';
-import { onProjectNotified } from './onProjectNotified';
-
-export const initProjectProjections = (eventBus: EventBus) => {
-  eventBus.subscribe(ProjectImported.type, onProjectImported);
-  eventBus.subscribe(ProjectReimported.type, onProjectReimported);
-  eventBus.subscribe(ProjectDataCorrected.type, onProjectDataCorrected);
-  eventBus.subscribe(ProjectDCRDueDateSet.type, onProjectDCRDueDateSet);
-  eventBus.subscribe(ProjectDCRDueDateCancelled.type, onProjectDCRDueDateCancelled);
-  eventBus.subscribe(ProjectCompletionDueDateSet.type, onProjectCompletionDueDateSet);
-  eventBus.subscribe(ProjectCompletionDueDateCancelled.type, onProjectCompletionDueDateCancelled);
-  eventBus.subscribe(ProjectNotified.type, onProjectNotified);
-  eventBus.subscribe(ProjectNotificationDateSet.type, onProjectNotificationDateSet);
-  eventBus.subscribe(ProjectCertificateGenerated.type, onProjectCertificateGenerated);
-  eventBus.subscribe(ProjectCertificateRegenerated.type, onProjectCertificateRegenerated);
-  eventBus.subscribe(ProjectCertificateUpdated.type, onProjectCertificateUpdated);
-
-  eventBus.subscribe(ProjectClasseGranted.type, onProjectClasseGranted);
-
-  eventBus.subscribe(ProjectAbandoned.type, onProjectAbandoned);
-  eventBus.subscribe(ProjectPuissanceUpdated.type, onProjectPuissanceUpdated);
-  eventBus.subscribe(ProjectActionnaireUpdated.type, onProjectActionnaireUpdated);
-  eventBus.subscribe(ProjectProducteurUpdated.type, onProjectProducteurUpdated);
-  eventBus.subscribe(ProjectFournisseursUpdated.type, onProjectFournisseursUpdated);
-  eventBus.subscribe(CahierDesChargesChoisi.type, onCahierDesChargesChoisi);
-  eventBus.subscribe(ProjectClaimed.type, onProjectClaimed);
-  eventBus.subscribe(ProjectClaimedByOwner.type, onProjectClaimedByOwner);
-
-  eventBus.subscribe(ProjectCertificateObsolete.type, onProjectCertificateObsolete);
-
-  eventBus.subscribe(CovidDelayGranted.type, onCovidDelayGranted);
-  eventBus.subscribe(AppelOffreProjetModifié.type, onAppelOffreProjetModifié);
-
-  eventBus.subscribe(ContratEDFRapprochéAutomatiquement.type, onContratEDFRapprochéAutomatiquement);
-  eventBus.subscribe(ContratEDFMisAJour.type, onContratEDFMisAJour);
-
-  eventBus.subscribe(
-    ContratEnedisRapprochéAutomatiquement.type,
-    onContratEnedisRapprochéAutomatiquement,
-  );
-  eventBus.subscribe(ContratEnedisMisAJour.type, onContratEnedisMisAJour);
-
-  eventBus.subscribe(
-    IdentifiantPotentielPPE2Batiment2Corrigé.type,
-    onIdentifiantPotentielPPE2Batiment2Corrigé,
-  );
-
-  eventBus.subscribe(DonnéesDeRaccordementRenseignées.type, onDonnéesDeRaccordementRenseignées);
-
-  eventBus.subscribe(LegacyAbandonSupprimé.type, onLegacyAbandonSupprimé);
-
-  eventBus.subscribe(AbandonProjetAnnulé.type, onAbandonProjetAnnulé);
-
-  logger.info('Initialized Project projections');
-};
+export * from './onAbandonProjetAnnulé';
+export * from './onAppelOffreProjetModifié';
+export * from './onCahierDesChargesChoisi';
+export * from './onContratEDFMisAJour';
+export * from './onContratEDFRapprochéAutomatiquement';
+export * from './onContratEnedisMisAJour';
+export * from './onContratEnedisRapprochéAutomatiquement';
+export * from './onCovidDelayGranted';
+export * from './onDonnéesDeRaccordementRenseignées';
+export * from './onIdentifiantPotentielPPE2Batiment2Corrigé';
+export * from './onLegacyAbandonSupprimé';
+export * from './onProjectAbandoned';
+export * from './onProjectActionnaireUpdated';
+export * from './onProjectCertificateGenerated';
+export * from './onProjectCertificateObsolete';
+export * from './onProjectCertificateRegenerated';
+export * from './onProjectCertificateUpdated';
+export * from './onProjectClaimed';
+export * from './onProjectClaimedByOwner';
+export * from './onProjectClasseGranted';
+export * from './onProjectCompletionDueDateCancelled';
+export * from './onProjectCompletionDueDateSet';
+export * from './onProjectDCRDueDateCancelled';
+export * from './onProjectDCRDueDateSet';
+export * from './onProjectDataCorrected';
+export * from './onProjectFournisseursUpdated';
+export * from './onProjectImported';
+export * from './onProjectNotificationDateSet';
+export * from './onProjectNotified';
+export * from './onProjectProducteurUpdated';
+export * from './onProjectPuissanceUpdated';
+export * from './onProjectReimported';
