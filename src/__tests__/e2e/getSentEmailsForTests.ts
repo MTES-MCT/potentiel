aaede65d7f29209d49ed87bb4e01d36cebb36569
Julien Pavon <julien.pavon@gmail.com>
2023-03-08 16:22:24 +0100

    🔥 (Tests): Suppression de code inutilise

--- src/__tests__/e2e/getSentEmailsForTests.ts
+++
@@ -1,6 +0,0 @@
-import { getSentEmails } from '@infra/mail';
-import { testRouter } from './testRouter';
-
-testRouter.get('/test/getSentEmails', async (request, response) => {
-  return response.send({ emails: getSentEmails() });
-});
