89a0088b0d258f5d77751348005de02f643a262d
Hubert Moncenis <hubmoncenis@gmail.com>
2023-03-08 17:30:51 +0100

    ♻️ passage du controller postInviteDrealUser en safeAsyncHandler (#995)
    ♻️ passage du postInviteDrealUser en safeAsyncHandler

--- src/controllers/admin/drealUsers/postInviteDrealUser.ts
+++ src/controllers/admin/drealUsers/postInviteDrealUser.ts
@@ -1,42 +1,57 @@
 import { createUser, eventStore, ensureRole } from '@config';
-import asyncHandler from '../../helpers/asyncHandler';
 import { addQueryParams } from '../../../helpers/addQueryParams';
 import { DrealUserInvited } from '@modules/authZ';
 import routes from '@routes';
 import { v1Router } from '../../v1Router';
 import * as yup from 'yup';
-import { errorResponse, RequestValidationError, validateRequestBody } from '../../helpers';
-import { logger } from '../../../core/utils';
-import { EmailAlreadyUsedError } from '../../../modules/shared/errors';
+import safeAsyncHandler from '../../helpers/safeAsyncHandler';
+import { errorResponse, RequestValidationError } from '../../helpers';
+import { EmailAlreadyUsedError } from '@modules/shared/errors';
+import { logger } from '@core/utils';
 import { REGIONS } from '@modules/dreal/région';
 
-const requestBodySchema = yup.object({
-  role: yup
-    .mixed()
-    .oneOf(['dreal'])
-    .required('Ce champ est obligatoire')
-    .typeError(`Le rôle n'est pas valide`),
-  email: yup.string().email("L'email saisi est invalide").required('Ce champ est obligatoire'),
-  region: yup
-    .mixed()
-    .oneOf([...REGIONS], 'Vous devez sélectionner une région dans le menu déroulant ci-dessus')
-    .required('Ce champ est obligatoire')
-    .typeError("La région saisie n'est pas valide"),
+const schema = yup.object({
+  body: yup.object({
+    role: yup
+      .mixed()
+      .oneOf(['dreal'])
+      .required('Ce champ est obligatoire')
+      .typeError(`Le rôle n'est pas valide`),
+    email: yup
+      .string()
+      .email("L'email saisi est invalide")
+      .required("L'email est un champ obligatoire"),
+    region: yup
+      .mixed()
+      .oneOf([...REGIONS], 'Vous devez sélectionner une région dans le menu déroulant ci-dessous')
+      .required('La région est un champ obligatoire')
+      .typeError("La région saisie n'est pas valide"),
+  }),
 });
 
 v1Router.post(
   routes.ADMIN_INVITE_DREAL_USER_ACTION,
   ensureRole(['admin', 'dgec-validateur']),
-  asyncHandler(async (request, response) => {
-    validateRequestBody(request.body, requestBodySchema)
-      .asyncAndThen((body) => {
-        const { email, role, region } = body;
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({ response, request, error }) =>
+        response.redirect(
+          addQueryParams(routes.ADMIN_DREAL_LIST, {
+            ...request.params,
+            error: `${error.errors.join(' ')}`,
+          }),
+        ),
+    },
+    async (request, response) => {
+      const { email, role, region } = request.body;
 
-        return createUser({
-          email: email.toLowerCase(),
-          role,
-          createdBy: request.user,
-        }).andThen(({ id: userId }) => {
+      return createUser({
+        email: email.toLowerCase(),
+        role,
+        createdBy: request.user,
+      })
+        .andThen(({ id: userId }) => {
           return eventStore
             .publish(
               new DrealUserInvited({
@@ -48,43 +63,42 @@ v1Router.post(
               }),
             )
             .map(() => ({ email }));
-        });
-      })
-      .match(
-        ({ email }) =>
-          response.redirect(
-            routes.SUCCESS_OR_ERROR_PAGE({
-              success: `Une invitation a bien été envoyée à ${email}.`,
-              redirectUrl: routes.ADMIN_DREAL_LIST,
-              redirectTitle: 'Retourner à la liste des DREALs',
-            }),
-          ),
-        (error: Error) => {
-          if (error instanceof RequestValidationError) {
-            return response.redirect(
+        })
+        .match(
+          ({ email }) =>
+            response.redirect(
               addQueryParams(routes.ADMIN_DREAL_LIST, {
-                ...request.body,
-                ...error.errors,
+                ...request.params,
+                success: `Une invitation a bien été envoyée à ${email}.`,
               }),
-            );
-          }
-          if (error instanceof EmailAlreadyUsedError) {
-            return response.redirect(
-              addQueryParams(routes.ADMIN_DREAL_LIST, {
-                ...request.body,
-                error:
-                  "L'invitation n'a pas pu être envoyée car l'adresse email est déjà associée à un compte Potentiel.",
-              }),
-            );
-          }
-          logger.error(error);
-          return errorResponse({
-            request,
-            response,
-            customMessage:
-              'Il y a eu une erreur lors de la soumission de votre demande. Merci de recommencer.',
-          });
-        },
-      );
-  }),
+            ),
+          (error: Error) => {
+            if (error instanceof RequestValidationError) {
+              return response.redirect(
+                addQueryParams(routes.ADMIN_DREAL_LIST, {
+                  ...request.body,
+                  ...error.errors,
+                }),
+              );
+            }
+            if (error instanceof EmailAlreadyUsedError) {
+              return response.redirect(
+                addQueryParams(routes.ADMIN_DREAL_LIST, {
+                  ...request.body,
+                  error:
+                    "L'invitation n'a pas pu être envoyée car l'adresse email est déjà associée à un compte Potentiel.",
+                }),
+              );
+            }
+            logger.error(error);
+            return errorResponse({
+              request,
+              response,
+              customMessage:
+                'Il y a eu une erreur lors de la soumission de votre demande. Merci de recommencer.',
+            });
+          },
+        );
+    },
+  ),
 );
