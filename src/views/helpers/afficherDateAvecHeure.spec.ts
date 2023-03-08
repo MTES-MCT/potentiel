d06f7ef4da1760b1603662fa2b81b5f72b75a52e
HubM <hubmoncenis@gmail.com>
2023-03-08 16:27:07 +0100

    ✅ revoir le wording des scénarios de test

--- src/views/helpers/afficherDateAvecHeure.spec.ts
+++ src/views/helpers/afficherDateAvecHeure.spec.ts
@@ -1,10 +1,14 @@
 import { afficherDateAvecHeure } from './afficherDateAvecHeure';
 
-describe('méthode afficherDateAvecHeure', () => {
-  it(`Lorsque la méthode est appelée pour une date ou un timestamp 
-    Alors la date retournée doit respecté le format de date par défaut (dd/MM/yyyy HH:mm)`, () => {
-    const date = new Date('2023-01-01 16:30');
+describe("Afficher une date avec l'heure", () => {
+  const date = new Date('2023-01-01 16:30');
+  it(`Lorsqu'on affiche une date au format JJ/MM/AAAA HH:MM à partir d'une date
+    Alors la chaîne de caractère retournée doit respecter le format de date par défaut (dd/MM/yyyy HH:mm)`, () => {
     expect(afficherDateAvecHeure(date)).toStrictEqual('01/01/2023 16:30');
+  });
+
+  it(`Lorsqu'on affiche une date au format JJ/MM/AAAA HH:MM à partir d'une date
+    Alors la chaîne de caractère retournée doit respecter le format de date par défaut (dd/MM/yyyy HH:mm)`, () => {
     expect(afficherDateAvecHeure(date.getTime())).toStrictEqual('01/01/2023 16:30');
   });
 });
