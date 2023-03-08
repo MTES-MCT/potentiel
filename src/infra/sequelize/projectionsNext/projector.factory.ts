330b3ce520293a89a9a50bb3fc93514a4c8b9de2
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-03-08 10:31:06 +0100

    ♻️ Replace console with logger

--- src/infra/sequelize/projectionsNext/projector.factory.ts
+++ src/infra/sequelize/projectionsNext/projector.factory.ts
@@ -76,7 +76,7 @@ export const createProjectorFactory =
             eventToHandle && (await handleEvent(eventToHandle, transaction));
           }
         } catch (e) {
-          console.log(e);
+          logger.error(e);
         }
       },
     };
