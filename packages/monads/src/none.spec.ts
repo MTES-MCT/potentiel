3cae1bb76ed9dce60af5e222daabb9f2468a739a
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-03-15 05:56:17 +0100

    ✅ Ajout test sur isSome et isNone

---
+++ packages/monads/src/none.spec.ts
@@ -0,0 +1,29 @@
+import { isNone, none } from './none';
+
+describe('isNone', () => {
+  it(`Lorsque qu'une valeur est 'none'
+      Alors la valeur est considérée comme du type None
+      Et la fonction isNone retourne true`, () => {
+    // Arrange
+    const value = none;
+
+    // Act
+    const actual = isNone(value);
+
+    // Assert
+    expect(actual).toBeTruthy();
+  });
+
+  it(`Lorsque qu'une valeur n'est pas 'none'
+      Alors la valeur n'est pas considérée comme du type None
+      Et la fonction isNone retourne false`, () => {
+    // Arrange
+    const value = {};
+
+    // Act
+    const actual = isNone(value);
+
+    // Assert
+    expect(actual).toBeFalsy();
+  });
+});
