4c5580470575a09ebb2f625f38a1d94c6d1acb72
HubM <hubmoncenis@gmail.com>
2023-03-17 15:46:55 +0100

    🎨 retravailler la page fake login

--- src/views/pages/FakeLoginPage.tsx
+++ src/views/pages/FakeLoginPage.tsx
@@ -2,7 +2,7 @@ import { Request } from 'express';
 import React from 'react';
 import { dataId } from '../../helpers/testId';
 import routes from '@routes';
-import { Button, ErrorBox, PageTemplate } from '@components';
+import { Button, ErrorBox, Input, Label, PageTemplate } from '@components';
 import { hydrateOnClient } from '../helpers';
 
 interface FakeLoginProps {
@@ -13,29 +13,29 @@ export const FakeLogin = ({ request }: FakeLoginProps) => {
   const { error } = (request.query as any) || {};
   return (
     <PageTemplate user={request.user}>
-      <main role="main">
-        <section className="section section-grey" style={{ minHeight: 'calc(100vh - 420px)' }}>
-          <div className="container">
-            <form action={routes.LOGIN_ACTION} method="post" name="form">
-              <h3 id="login">Je m‘identifie</h3>
-              {!!error && <ErrorBox title={error} />}
-              <div className="form__group">
-                <label htmlFor="email">Courrier électronique</label>
-                <input type="email" name="email" id="email" {...dataId('email-field')} />
-                <Button
-                  type="submit"
-                  name="submit"
-                  id="submit"
-                  {...dataId('submit-button')}
-                  className="mt-2"
-                >
-                  Je m'identifie
-                </Button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
+      <section className="flex justify-center items-center h-[calc(100vh-420px)]">
+        <form action={routes.LOGIN_ACTION} method="post" name="form">
+          <h3 id="login">Je m‘identifie</h3>
+          {!!error && <ErrorBox title={error} />}
+          <Label htmlFor="email">Courrier électronique</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            {...dataId('email-field')}
+            defaultValue="admin@test.test"
+          />
+          <Button
+            type="submit"
+            name="submit"
+            id="submit"
+            {...dataId('submit-button')}
+            className="mt-2"
+          >
+            Je m'identifie
+          </Button>
+        </form>
+      </section>
     </PageTemplate>
   );
 };
