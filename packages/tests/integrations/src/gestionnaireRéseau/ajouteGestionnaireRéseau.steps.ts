c68e8412a86dd33fdf58a1302b197da941879147
HanaeY <hanae.yamashita@lilo.org>
2023-03-30 14:22:54 +0200

    ✅ (Ajouter gestionnaire): ajout steps

--- packages/tests/integrations/src/gestionnaireRéseau/ajouteGestionnaireRéseau.steps.ts
+++ packages/tests/integrations/src/gestionnaireRéseau/ajouteGestionnaireRéseau.steps.ts
@@ -1,7 +1,84 @@
-import { When as Quand, Then as Alors } from '@cucumber/cucumber';
+import { should } from 'chai';
+import { When as Quand, Then as Alors, BeforeAll, Before, After } from '@cucumber/cucumber';
+import { Unsubscribe } from '@potentiel/core-domain';
+import {
+  ajouterGestionnaireRéseauCommandHandlerFactory,
+  consulterGestionnaireRéseauQueryHandlerFactory,
+  GestionnaireRéseauAjoutéEvent,
+  gestionnaireRéseauAjoutéHandlerFactory,
+} from '@potentiel/domain';
+import { publish, loadAggregate, subscribe } from '@potentiel/pg-event-sourcing';
+import { createProjection, findProjection } from '@potentiel/pg-projections';
+import waitForExpect from 'wait-for-expect';
+import { executeQuery } from '@potentiel/pg-helpers';
 
-Quand('un administrateur ajoute un gestionnaire de réseau', () => {
-  return 'pending';
+should();
+
+let unsubscribe: Unsubscribe | undefined;
+
+const codeEIC = '17X100A100A0001A';
+const raisonSociale = 'Enedis';
+const format = 'XX-YY-ZZ';
+const légende = 'la légende';
+
+BeforeAll(() => {
+  process.env.EVENT_STORE_CONNECTION_STRING = 'postgres://testuser@localhost:5433/potentiel_test';
 });
 
-Alors('le gestionnaire devrait être ajouté', () => {});
+Before(async () => {
+  await executeQuery(`DELETE FROM "EVENT_STREAM"`);
+  await executeQuery(`DELETE FROM "PROJECTION"`);
+});
+
+After(async () => {
+  if (unsubscribe) {
+    await unsubscribe();
+    unsubscribe = undefined;
+  }
+});
+
+Quand('un administrateur ajoute un gestionnaire de réseau', async () => {
+  const ajouterGestionnaireRéseau = ajouterGestionnaireRéseauCommandHandlerFactory({
+    publish,
+    loadAggregate,
+  });
+
+  const gestionnaireRéseauAjoutéHandler = gestionnaireRéseauAjoutéHandlerFactory({
+    create: createProjection,
+  });
+
+  unsubscribe = await subscribe<GestionnaireRéseauAjoutéEvent>(
+    'GestionnaireRéseauAjouté',
+    gestionnaireRéseauAjoutéHandler,
+  );
+
+  await ajouterGestionnaireRéseau({
+    codeEIC,
+    raisonSociale,
+    aideSaisieRéférenceDossierRaccordement: {
+      format,
+      légende,
+    },
+  });
+});
+
+Alors('le gestionnaire devrait être ajouté', async () => {
+  const consulterGestionnaireRéseau = consulterGestionnaireRéseauQueryHandlerFactory({
+    findGestionnaireRéseau: findProjection,
+  });
+
+  await waitForExpect(async () => {
+    const actual = await consulterGestionnaireRéseau({ codeEIC });
+
+    const expected: typeof actual = {
+      type: 'gestionnaire-réseau',
+      codeEIC,
+      raisonSociale,
+      aideSaisieRéférenceDossierRaccordement: {
+        format,
+        légende,
+      },
+    };
+    actual.should.be.deep.equal(expected);
+  });
+});
