254a6b14e9e5d412970158ac501226233372ad08
Julien Pavon <julien.pavon@gmail.com>
2023-04-05 14:24:01 +0200

    💩 Gestion des doublons pour ProjectClaimedByOwner

--- src/infra/sequelize/projectionsNext/userProjects/updates/onProjectClaimedByOwner.ts
+++ src/infra/sequelize/projectionsNext/userProjects/updates/onProjectClaimedByOwner.ts
@@ -3,13 +3,19 @@ import { ProjectClaimedByOwner } from '@modules/projectClaim/events';
 import { ProjectionEnEchec } from '@modules/shared';
 import { UserProjects } from '../userProjects.model';
 import { UserProjectsProjector } from '../userProjects.projector';
+import { User } from '../../users/users.model';
 
 export default UserProjectsProjector.on(ProjectClaimedByOwner, async (évènement, transaction) => {
   const {
-    payload: { projectId, claimedBy },
+    payload: { projectId, claimedBy: userId },
   } = évènement;
   try {
-    await UserProjects.create({ userId: claimedBy, projectId }, { transaction });
+    const foundUser = await User.findOne({ where: { id: userId } });
+    const allUsers = await User.findAll({ where: { email: foundUser?.email } });
+
+    for (const user of allUsers) {
+      await UserProjects.findOrCreate({ where: { userId: user.id, projectId }, transaction });
+    }
   } catch (error) {
     logger.error(
       new ProjectionEnEchec(
