fb3eed2942db3ab9b7a31088f1bef9df729eba45
Julien Pavon <julien.pavon@gmail.com>
2023-04-06 15:05:30 +0200

    ♻️ Debut implementation

--- packages/specifications/src/raccordement/déposerDemandeRaccordement.steps.ts
+++ packages/specifications/src/raccordement/déposerDemandeRaccordement.steps.ts
@@ -1,6 +1,14 @@
 import { Given as EtantDonné, When as Quand, Then as Alors } from '@cucumber/cucumber';
+import { RaccordementWorld } from './raccordement.world';
 
-EtantDonné('un projet', function () {});
+EtantDonné('un projet', function (this: RaccordementWorld) {
+  const identifiantProjet: IdentifiantProjet = {
+    appelOffre: 'PPE2 - Eolien',
+    période: '1',
+    famille: '',
+    numéroCRE: '23',
+  };
+});
 
 Quand(
   'le porteur du projet dépose une demande de raccordement auprès du gestionnaire de réseau {string}',
@@ -10,4 +18,9 @@ Quand(
 Alors(
   'le projet devrait avoir une demande de raccordement pour le gestionnaire de réseau {string}',
   function (nomDuGestionnaire: string) {},
+);
+
+Alors(
+  'la demande est consultable dans la liste des demandes de raccordement du projet',
+  function () {},
 );
