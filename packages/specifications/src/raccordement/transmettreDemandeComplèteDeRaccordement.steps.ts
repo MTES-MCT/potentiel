aa51ed46b59b4d9d4da1e6562ae4706a16b96d08
Julien Pavon <julien.pavon@gmail.com>
2023-04-11 10:20:27 +0200

    🧪 Implementation pour ajouter 2 DCR (WIP)

--- packages/specifications/src/raccordement/transmettreDemandeComplèteDeRaccordement.steps.ts
+++ packages/specifications/src/raccordement/transmettreDemandeComplèteDeRaccordement.steps.ts
@@ -17,6 +17,58 @@ EtantDonné('un projet', function (this: PotentielWorld) {
   };
 });
 
+EtantDonné(
+  "un projet avec une demande complète de raccordement transmise auprès d'un gestionnaire de réseau avec :",
+  async function (this: PotentielWorld, table: DataTable) {
+    await this.gestionnaireRéseauWorld.createGestionnaireRéseau(
+      this.raccordementWorld.enedis.codeEIC,
+      this.raccordementWorld.enedis.raisonSociale,
+    );
+    const exemple = table.rowsHash();
+    this.raccordementWorld.dateQualification = new Date(exemple['La date de qualification']);
+    this.raccordementWorld.référenceDemandeRaccordement =
+      exemple['La référence de la demande de raccordement'];
+
+    const transmettreDemandeComplèteRaccordement =
+      transmettreDemandeComplèteRaccordementCommandHandlerFactory({
+        publish,
+      });
+
+    await transmettreDemandeComplèteRaccordement({
+      identifiantProjet: this.raccordementWorld.identifiantProjet,
+      identifiantGestionnaireRéseau: {
+        codeEIC: this.raccordementWorld.enedis.codeEIC,
+      },
+      dateQualification: this.raccordementWorld.dateQualification,
+      référenceDemandeRaccordement: this.raccordementWorld.référenceDemandeRaccordement,
+    });
+  },
+);
+
+Quand(
+  'le porteur du projet transmet une autre demande complète de raccordement auprès du même gestionnaire de réseau avec :',
+  async function (this: PotentielWorld, table: DataTable) {
+    const exemple = table.rowsHash();
+    this.raccordementWorld.dateQualification = new Date(exemple['La date de qualification']);
+    this.raccordementWorld.référenceDemandeRaccordement =
+      exemple['La référence de la demande de raccordement'];
+
+    const transmettreDemandeComplèteRaccordement =
+      transmettreDemandeComplèteRaccordementCommandHandlerFactory({
+        publish,
+      });
+
+    await transmettreDemandeComplèteRaccordement({
+      identifiantProjet: this.raccordementWorld.identifiantProjet,
+      identifiantGestionnaireRéseau: {
+        codeEIC: this.raccordementWorld.enedis.codeEIC,
+      },
+      dateQualification: this.raccordementWorld.dateQualification,
+      référenceDemandeRaccordement: this.raccordementWorld.référenceDemandeRaccordement,
+    });
+  },
+);
+
 Quand(
   `le porteur du projet transmet une demande complète de raccordement auprès d'un gestionnaire de réseau avec :`,
   async function (this: PotentielWorld, table: DataTable) {
@@ -41,6 +93,23 @@ Quand(
       },
       dateQualification: this.raccordementWorld.dateQualification,
       référenceDemandeRaccordement: this.raccordementWorld.référenceDemandeRaccordement,
+    });
+  },
+);
+
+Alors(
+  'le projet devrait avoir {int} demandes complètes de raccordement pour ce gestionnaire de réseau',
+  async function (this: PotentielWorld, nombreDeDemandes: number) {
+    const listerDemandeComplèteRaccordement = listerDemandeComplèteRaccordementQueryHandlerFactory({
+      find: findProjection,
+    });
+
+    await waitForExpect(async () => {
+      const actual = await listerDemandeComplèteRaccordement({
+        identifiantProjet: this.raccordementWorld.identifiantProjet,
+      });
+      actual.gestionnaireRéseau.should.be.deep.equal(this.raccordementWorld.enedis);
+      actual.référencesDemandeRaccordement.should.length(nombreDeDemandes);
     });
   },
 );
