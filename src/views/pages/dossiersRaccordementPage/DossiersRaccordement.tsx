3f6dd5d29fbaad76d8c65fb61fcad30d8ed90753
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-04-13 16:41:50 +0200

    ✨ (Formulaire envoi DCR): WIP

--- src/views/pages/dossiersRaccordementPage/DossiersRaccordement.tsx
+++ src/views/pages/dossiersRaccordementPage/DossiersRaccordement.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { UtilisateurReadModel } from '@modules/utilisateur/récupérer/UtilisateurReadModel';
-import { Heading1, PageTemplate, Tile } from '@components';
+import { Button, Heading1, Input, Label, PageTemplate, Tile } from '@components';
 import { hydrateOnClient } from '../../helpers';
 import { ListeDossiersRaccordementReadModel } from '@potentiel/domain';
+import routes from '@routes';
 
 type DossiersRaccordementProps = {
   user: UtilisateurReadModel;
@@ -16,13 +17,38 @@ export const DossiersRaccordement = ({ user, dossiersRaccordement }: DossiersRac
       <div className="panel">
         <div className="panel__header">
           <Heading1>Raccordement</Heading1>
-          <ul>
-            {dossiersRaccordement.références.map((dossierRaccordement) => (
-              <li>
-                <Tile>{dossierRaccordement}</Tile>
-              </li>
-            ))}
-          </ul>
+          {dossiersRaccordement.références.length > 0 ? (
+            <ul>
+              {dossiersRaccordement.références.map((dossierRaccordement) => (
+                <li>
+                  <Tile>{dossierRaccordement}</Tile>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <form
+              className="flex gap-3 flex-col"
+              method="POST"
+              action={routes.POST_TRANSMETTRE_DEMANDE_COMPLETE_RACCORDEMENT}
+            >
+              {/* Ajouter : date qualification, identifiant gestionnaire réseau (+ aide à la saisie) */}
+              <div>
+                <Label htmlFor="référence">Référence du dossier de raccordement</Label>
+                <Input type="text" id="référence" name="référence" />
+              </div>
+              <div>
+                <Label htmlFor="demandeComplèteRaccordement">
+                  Demande complète de raccordement
+                </Label>
+                <Input
+                  type="file"
+                  id="demandeComplèteRaccordement"
+                  name="demandeComplèteRaccordement"
+                />
+              </div>
+              <Button type="submit">Envoyer</Button>
+            </form>
+          )}
         </div>
       </div>
     </PageTemplate>
