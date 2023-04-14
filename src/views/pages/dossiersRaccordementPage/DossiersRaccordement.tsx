ebbd78ee9b438ff70fc2b9e45d3a0a0c899c9acb
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-04-14 09:57:17 +0200

    🎨 (Formulaire DCR): Ajout inputs front

--- src/views/pages/dossiersRaccordementPage/DossiersRaccordement.tsx
+++ src/views/pages/dossiersRaccordementPage/DossiersRaccordement.tsx
@@ -22,51 +22,67 @@ export const DossiersRaccordement = ({
       <div className="panel">
         <div className="panel__header">
           <Heading1>Raccordement</Heading1>
-          {dossiersRaccordement.références.length > 0 ? (
-            <ul>
-              {dossiersRaccordement.références.map((dossierRaccordement) => (
-                <li>
-                  <Tile>{dossierRaccordement}</Tile>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <form
-              className="flex gap-3 flex-col"
-              method="POST"
-              action={routes.POST_TRANSMETTRE_DEMANDE_COMPLETE_RACCORDEMENT}
-            >
-              {/* Ajouter : date qualification, identifiant gestionnaire réseau (+ aide à la saisie) */}
-              <div>
-                <Label htmlFor="codeEIC">Gestionnaire réseau</Label>
-                <Select name="codeEIC" id="codeEIC">
-                  {gestionnairesRéseau.map((gestionnaireRéseau) => {
-                    return (
-                      <option value={gestionnaireRéseau.codeEIC}>
-                        {gestionnaireRéseau.raisonSociale}
-                      </option>
-                    );
-                  })}
-                </Select>
-              </div>
-              <div>
-                <Label htmlFor="référence">Référence du dossier de raccordement</Label>
-                <Input type="text" id="référence" name="référence" />
-              </div>
-              <div>
-                <Label htmlFor="demandeComplèteRaccordement">
-                  Demande complète de raccordement
-                </Label>
-                <Input
-                  type="file"
-                  id="demandeComplèteRaccordement"
-                  name="demandeComplèteRaccordement"
-                />
-              </div>
-              <Button type="submit">Envoyer</Button>
-            </form>
-          )}
         </div>
+        {dossiersRaccordement.références.length > 0 ? (
+          <ul>
+            {dossiersRaccordement.références.map((dossierRaccordement) => (
+              <li>
+                <Tile>{dossierRaccordement}</Tile>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <form
+            className="flex gap-3 flex-col"
+            method="POST"
+            action={routes.POST_TRANSMETTRE_DEMANDE_COMPLETE_RACCORDEMENT}
+          >
+            {/* Ajouter : date qualification, identifiant gestionnaire réseau (+ aide à la saisie) */}
+            <div>
+              <Label htmlFor="codeEIC" required>
+                Gestionnaire réseau
+              </Label>
+              <Select name="codeEIC" id="codeEIC" defaultValue="none" required>
+                <option value="none" disabled hidden>
+                  Sélectionnez un gestionnaire
+                </option>
+                {gestionnairesRéseau.map((gestionnaireRéseau) => {
+                  return (
+                    <option value={gestionnaireRéseau.codeEIC}>
+                      {gestionnaireRéseau.raisonSociale}
+                    </option>
+                  );
+                })}
+              </Select>
+            </div>
+            <div>
+              <Label htmlFor="référence" required>
+                Référence du dossier de raccordement
+              </Label>
+              <Input type="text" id="référence" name="référence" required />
+            </div>
+            <div>
+              <Label htmlFor="demandeComplèteRaccordement" required>
+                Demande complète de raccordement
+              </Label>
+              <Input
+                type="file"
+                id="demandeComplèteRaccordement"
+                name="demandeComplèteRaccordement"
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="dateQualification" required>
+                Date de qualification
+              </Label>
+              <Input type="text" id="dateQualification" name="dateQualification" required />
+            </div>
+            <Button type="submit" className="m-auto">
+              Envoyer
+            </Button>
+          </form>
+        )}
       </div>
     </PageTemplate>
   );
