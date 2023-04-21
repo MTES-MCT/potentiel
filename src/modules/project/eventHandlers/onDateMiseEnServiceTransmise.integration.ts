4ba34461487f19fa7c2ef900bc5f9bbc85ead2b9
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-04-21 17:34:17 +0200

    ✅ (Ajout délai CDC 2022): Cas d'erreurs

--- src/modules/project/eventHandlers/onDateMiseEnServiceTransmise.integration.ts
+++ src/modules/project/eventHandlers/onDateMiseEnServiceTransmise.integration.ts
@@ -18,9 +18,44 @@ describe(`Handler onDateMiseEnServiceTransmise`, () => {
     okAsync<null, InfraNotAvailableError>(null),
   );
 
+  const getProjectAppelOffre = jest.fn(
+    () =>
+      ({
+        type: 'eolien',
+        cahiersDesChargesModifiésDisponibles: [
+          {
+            type: 'modifié',
+            paruLe: '30/08/2022',
+            délaiApplicable: {
+              délaiEnMois: 18,
+              intervaleDateMiseEnService: {
+                min: new Date('2022-06-01'),
+                max: new Date('2024-09-30'),
+              },
+            },
+          } as CahierDesChargesModifié,
+        ] as ReadonlyArray<CahierDesChargesModifié>,
+      } as ProjectAppelOffre),
+  );
+
+  const projetId = new UniqueEntityID();
+  const appelOffreId = 'Eolien';
+  const periodeId = '1';
+  const numeroCRE = '123';
+  const familleId = '';
+
+  const projet = makeFakeProject({
+    id: projetId.toString(),
+    appelOffreId,
+    periodeId,
+    numeroCRE,
+    familleId,
+  });
+
   beforeEach(async () => {
     await publishToEventStore.mockClear();
     await resetDatabase();
+    await ProjectModel.create(projet);
   });
 
   const dateAchèvementInitiale = new Date('2024-01-01').getTime();
@@ -35,50 +70,11 @@ describe(`Handler onDateMiseEnServiceTransmise`, () => {
       ayant souscrit au CDC 2022,
       n'ayant pas déjà bénéficié du délai,
       alors le délai de 18 mois en lien avec le CDC 2022 devrait être appliqué`, async () => {
-      const projetId = new UniqueEntityID();
-      const appelOffreId = 'Eolien';
-      const periodeId = '1';
-      const numeroCRE = '123';
-      const familleId = '';
-
-      const projet = makeFakeProject({
-        id: projetId.toString(),
-        appelOffreId,
-        periodeId,
-        numeroCRE,
-        familleId,
-      });
-
-      try {
-        await ProjectModel.create(projet);
-      } catch (error) {
-        console.log('error', error);
-      }
-
-      const getProjectAppelOffre = jest.fn(
-        () =>
-          ({
-            type: 'eolien',
-            cahiersDesChargesModifiésDisponibles: [
-              {
-                type: 'modifié',
-                paruLe: '30/08/2022',
-                délaiApplicable: {
-                  délaiEnMois: 18,
-                  intervaleDateMiseEnService: {
-                    min: new Date('2022-06-01'),
-                    max: new Date('2024-09-30'),
-                  },
-                },
-              } as CahierDesChargesModifié,
-            ] as ReadonlyArray<CahierDesChargesModifié>,
-          } as ProjectAppelOffre),
-      );
       const fakeProject = {
         ...makeFakeProjectAggregate(),
         cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
         completionDueOn: dateAchèvementInitiale,
-        délaiCDC2022Appliqué: false,
+        délaiCDC2022appliqué: false,
         numeroCRE,
         appelOffreId,
         periodeId,
@@ -97,7 +93,7 @@ describe(`Handler onDateMiseEnServiceTransmise`, () => {
         type: 'DateMiseEnServiceTransmise',
         payload: {
           dateMiseEnService: new Date('01/01/2023').toISOString(),
-          référenceDossierRaccordement: '',
+          référenceDossierRaccordement: 'ref-du-dossier',
           identifiantProjet: `${appelOffreId}#${periodeId}#${familleId}#${numeroCRE}`,
         },
       };
@@ -117,218 +113,124 @@ describe(`Handler onDateMiseEnServiceTransmise`, () => {
     });
   });
 
-  // describe(`Projets ne pouvant pas bénéficier du délai de 18 mois`, () => {
-  //   describe(`Dates hors limites`, () => {
-  //     describe(`Projets PV`, () => {
-  //       for (const type of ['batiment', 'innovation', 'sol', 'zni', 'autre']) {
-  //         const getProjectAppelOffre = jest.fn(
-  //           () =>
-  //             ({
-  //               type,
-  //               cahiersDesChargesModifiésDisponibles: [
-  //                 {
-  //                   type: 'modifié',
-  //                   paruLe: '30/08/2022',
-  //                   délaiApplicable: {
-  //                     délaiEnMois: 18,
-  //                     intervaleDateMiseEnService: {
-  //                       min: new Date('2022-09-01'),
-  //                       max: new Date('2024-12-31'),
-  //                     },
-  //                   },
-  //                 } as CahierDesChargesModifié,
-  //               ] as ReadonlyArray<CahierDesChargesModifié>,
-  //             } as ProjectAppelOffre),
-  //         );
-  //         it(`Etant donné un projet PV ${type} dont la date de mise en service est supérieure au 31 décembre 2024
-  //     alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //           const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //             projectRepo,
-  //             publishToEventStore,
-  //             getProjectAppelOffre,
-  //           });
+  describe(`Projets ne pouvant pas bénéficier du délai de 18 mois`, () => {
+    describe(`Date mise en service hors limites du CDC`, () => {
+      it(`Etant donné un projet éolien,
+          dont la date de mise en service n'est pas comprise entre le 1er juin 2022 et le 30 septembre 2024,
+          ayant souscrit au CDC 2022,
+          n'ayant pas déjà bénéficié du délai,
+          alors le délai de 18 mois en lien avec le CDC 2022 ne devrait pas être appliqué`, async () => {
+        const fakeProject = {
+          ...makeFakeProjectAggregate(),
+          cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
+          completionDueOn: dateAchèvementInitiale,
+          délaiCDC2022appliqué: false,
+          numeroCRE,
+          appelOffreId,
+          periodeId,
+          id: projetId,
+          familleId,
+        };
+        const projectRepo = fakeTransactionalRepo(fakeProject as Project);
 
-  //           const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //             payload: {
-  //               projetId: fakeProject.id.toString(),
-  //               dateMiseEnService: new Date('31/12/2025'),
-  //             } as DonnéesDeRaccordementRenseignéesdPayload,
-  //           });
+        const onDateMiseEnServiceTransmise = makeOnDateMiseEnServiceTransmise({
+          projectRepo,
+          publishToEventStore,
+          getProjectAppelOffre,
+        });
 
-  //           await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //           expect(publishToEventStore).not.toHaveBeenCalled();
-  //         });
+        const événementDateMiseEnServiceTransmise: DateMiseEnServiceTransmiseEvent = {
+          type: 'DateMiseEnServiceTransmise',
+          payload: {
+            dateMiseEnService: new Date('01/01/2021').toISOString(),
+            référenceDossierRaccordement: 'ref-du-dossier',
+            identifiantProjet: `${appelOffreId}#${periodeId}#${familleId}#${numeroCRE}`,
+          },
+        };
 
-  //         it(`Etant donné un projet PV ${type} dont la date de mise en service est inférieure au 1er septembre 2022,
-  //     alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //           const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //             projectRepo,
-  //             publishToEventStore,
-  //             getProjectAppelOffre,
-  //           });
+        await onDateMiseEnServiceTransmise(événementDateMiseEnServiceTransmise);
 
-  //           const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //             payload: {
-  //               projetId: fakeProject.id.toString(),
-  //               dateMiseEnService: new Date('01/08/2022'),
-  //             } as DonnéesDeRaccordementRenseignéesdPayload,
-  //           });
+        expect(publishToEventStore).not.toHaveBeenCalled();
+      });
+    });
 
-  //           await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //           expect(publishToEventStore).not.toHaveBeenCalled();
-  //         });
-  //       }
-  //     });
-  //     describe(`Projets éolien`, () => {
-  //       const getProjectAppelOffre = jest.fn(
-  //         () =>
-  //           ({
-  //             type: 'eolien',
-  //             cahiersDesChargesModifiésDisponibles: [
-  //               {
-  //                 type: 'modifié',
-  //                 paruLe: '30/08/2022',
-  //                 délaiApplicable: {
-  //                   délaiEnMois: 18,
-  //                   intervaleDateMiseEnService: {
-  //                     min: new Date('2022-06-01'),
-  //                     max: new Date('2024-09-30'),
-  //                   },
-  //                 },
-  //               } as CahierDesChargesModifié,
-  //             ] as ReadonlyArray<CahierDesChargesModifié>,
-  //           } as ProjectAppelOffre),
-  //       );
-  //       it(`Etant donné un projet éolien dont la date de mise en service est antérieure au 1er juin 2022
-  //     alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //         const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //           projectRepo,
-  //           publishToEventStore,
-  //           getProjectAppelOffre,
-  //         });
+    describe(`Cahier des charges 2022 non souscrit`, () => {
+      it(`Etant donné un projet éolien,
+          dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
+          n'ayant pas souscrit au CDC 2022,
+          alors le délai de 18 mois en lien avec le CDC 2022 ne devrait pas être appliqué`, async () => {
+        const fakeProject = {
+          ...makeFakeProjectAggregate(),
+          cahierDesCharges: { type: 'initial' },
+          completionDueOn: dateAchèvementInitiale,
+          délaiCDC2022appliqué: false,
+          numeroCRE,
+          appelOffreId,
+          periodeId,
+          id: projetId,
+          familleId,
+        };
+        const projectRepo = fakeTransactionalRepo(fakeProject as Project);
 
-  //         const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //           payload: {
-  //             projetId: fakeProject.id.toString(),
-  //             dateMiseEnService: new Date('01/05/2022'),
-  //           } as DonnéesDeRaccordementRenseignéesdPayload,
-  //         });
+        const onDateMiseEnServiceTransmise = makeOnDateMiseEnServiceTransmise({
+          projectRepo,
+          publishToEventStore,
+          getProjectAppelOffre,
+        });
 
-  //         await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //         expect(publishToEventStore).not.toHaveBeenCalled();
-  //       });
+        const événementDateMiseEnServiceTransmise: DateMiseEnServiceTransmiseEvent = {
+          type: 'DateMiseEnServiceTransmise',
+          payload: {
+            dateMiseEnService: new Date('01/01/2023').toISOString(),
+            référenceDossierRaccordement: 'ref-du-dossier',
+            identifiantProjet: `${appelOffreId}#${periodeId}#${familleId}#${numeroCRE}`,
+          },
+        };
 
-  //       it(`Etant donné un projet éolien dont la date de mise en service est postérieure au 30 septembre 2024
-  //     alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //         const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //           projectRepo,
-  //           publishToEventStore,
-  //           getProjectAppelOffre,
-  //         });
+        await onDateMiseEnServiceTransmise(événementDateMiseEnServiceTransmise);
 
-  //         const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //           payload: {
-  //             projetId: fakeProject.id.toString(),
-  //             dateMiseEnService: new Date('30/10/2024'),
-  //           } as DonnéesDeRaccordementRenseignéesdPayload,
-  //         });
+        expect(publishToEventStore).not.toHaveBeenCalled();
+      });
+    });
 
-  //         await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //         expect(publishToEventStore).not.toHaveBeenCalled();
-  //       });
-  //     });
-  //   });
+    describe(`Délai CDC 2022 déjà appliqué`, () => {
+      it(`Etant donné un projet éolien,
+          dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
+          ayant souscrit au CDC 2022,
+          ayant déjà bénéficié du délai,
+          alors le délai de 18 mois en lien avec le CDC 2022 ne devrait pas être appliqué`, async () => {
+        const fakeProject = {
+          ...makeFakeProjectAggregate(),
+          cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
+          completionDueOn: dateAchèvementInitiale,
+          délaiCDC2022appliqué: true,
+          numeroCRE,
+          appelOffreId,
+          periodeId,
+          id: projetId,
+          familleId,
+        };
+        const projectRepo = fakeTransactionalRepo(fakeProject as Project);
 
-  //   describe(`Cahier des charges 2022 non souscrit`, () => {
-  //     it(`Etant donné un projet éolien
-  //        dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
-  //        MAIS qui n'a pas souscrit au CDC 2022,
-  //        alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //       const getProjectAppelOffre = jest.fn(
-  //         () =>
-  //           ({
-  //             type: 'eolien',
-  //             cahiersDesChargesModifiésDisponibles: [
-  //               {
-  //                 type: 'modifié',
-  //                 paruLe: '30/08/2022',
-  //                 délaiApplicable: {
-  //                   délaiEnMois: 18,
-  //                   intervaleDateMiseEnService: {
-  //                     min: new Date('2022-06-01'),
-  //                     max: new Date('2024-09-30'),
-  //                   },
-  //                 },
-  //               } as CahierDesChargesModifié,
-  //             ] as ReadonlyArray<CahierDesChargesModifié>,
-  //           } as ProjectAppelOffre),
-  //       );
-  //       const fakeProject = {
-  //         ...makeFakeProject(),
-  //         cahierDesCharges: { type: 'modifié', paruLe: '30/07/2022' },
-  //       };
-  //       const projectRepo = fakeTransactionalRepo(fakeProject as Project);
-  //       const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //         projectRepo,
-  //         publishToEventStore,
-  //         getProjectAppelOffre,
-  //       });
-  //       const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //         payload: {
-  //           projetId: fakeProject.id.toString(),
-  //           dateMiseEnService: new Date('01/01/2023'),
-  //         } as DonnéesDeRaccordementRenseignéesdPayload,
-  //       });
-  //       await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //       expect(publishToEventStore).not.toHaveBeenCalled();
-  //     });
-  //   });
+        const onDateMiseEnServiceTransmise = makeOnDateMiseEnServiceTransmise({
+          projectRepo,
+          publishToEventStore,
+          getProjectAppelOffre,
+        });
 
-  //   describe(`Délai déjà appliqué`, () => {
-  //     it(`Etant donné un projet éolien
-  //        dont la date de mise en service est comprise entre le 1er juin 2022 et le 30 septembre 2024,
-  //        qui a souscrit au CDC 2022,
-  //        MAIS qui a déjà bénéficié du délai de 18 mois,
-  //        alors le projet ne doit pas être modifié et aucun événement n'est émis`, async () => {
-  //       const getProjectAppelOffre = jest.fn(
-  //         () =>
-  //           ({
-  //             type: 'eolien',
-  //             cahiersDesChargesModifiésDisponibles: [
-  //               {
-  //                 type: 'modifié',
-  //                 paruLe: '30/08/2022',
-  //                 délaiApplicable: {
-  //                   délaiEnMois: 18,
-  //                   intervaleDateMiseEnService: {
-  //                     min: new Date('2022-06-01'),
-  //                     max: new Date('2024-09-30'),
-  //                   },
-  //                 },
-  //               } as CahierDesChargesModifié,
-  //             ] as ReadonlyArray<CahierDesChargesModifié>,
-  //           } as ProjectAppelOffre),
-  //       );
-  //       const fakeProject = {
-  //         ...makeFakeProject(),
-  //         cahierDesCharges: { type: 'modifié', paruLe: '30/08/2022' },
-  //         délaiCDC2022appliqué: true,
-  //       };
-  //       const projectRepo = fakeTransactionalRepo(fakeProject as Project);
-  //       const onDonnéesDeRaccordementRenseignées = makeOnDateMiseEnServiceTransmise({
-  //         projectRepo,
-  //         publishToEventStore,
-  //         getProjectAppelOffre,
-  //       });
-  //       const événementMeSRenseignée = new DonnéesDeRaccordementRenseignées({
-  //         payload: {
-  //           projetId: fakeProject.id.toString(),
-  //           dateMiseEnService: new Date('01/01/2023'),
-  //         } as DonnéesDeRaccordementRenseignéesdPayload,
-  //       });
-  //       await onDonnéesDeRaccordementRenseignées(événementMeSRenseignée);
-  //       expect(publishToEventStore).not.toHaveBeenCalled();
-  //     });
-  //   });
-  // });
+        const événementDateMiseEnServiceTransmise: DateMiseEnServiceTransmiseEvent = {
+          type: 'DateMiseEnServiceTransmise',
+          payload: {
+            dateMiseEnService: new Date('01/01/2023').toISOString(),
+            référenceDossierRaccordement: 'ref-du-dossier',
+            identifiantProjet: `${appelOffreId}#${periodeId}#${familleId}#${numeroCRE}`,
+          },
+        };
+
+        await onDateMiseEnServiceTransmise(événementDateMiseEnServiceTransmise);
+
+        expect(publishToEventStore).not.toHaveBeenCalled();
+      });
+    });
+  });
 });
