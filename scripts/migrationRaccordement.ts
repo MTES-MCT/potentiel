dfa0d2434517f39dc08c0eb22fd9f16e12fe62bf
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-04-25 14:45:29 +0200

    🔨 Add script de migration raccordement

---
+++ scripts/migrationRaccordement.ts
@@ -0,0 +1,126 @@
+import { loadAggregate, publish } from '@potentiel/pg-event-sourcing';
+import { findProjection } from '@potentiel/pg-projections';
+import { executeSelect } from '@potentiel/pg-helpers';
+import {
+  consulterGestionnaireRéseauQueryHandlerFactory,
+  transmettreDateMiseEnServiceCommandHandlerFactory,
+  transmettrePropositionTechniqueEtFinancièreCommandHandlerFactory,
+  transmettreDemandeComplèteRaccordementCommandHandlerFactory,
+  transmettreDemandeComplèteRaccordementUseCaseFactory,
+} from '@potentiel/domain';
+
+const consulterGestionnaireRéseauQuery = consulterGestionnaireRéseauQueryHandlerFactory({
+  find: findProjection,
+});
+
+const transmettreDemandeComplèteRaccordementUseCase =
+  transmettreDemandeComplèteRaccordementUseCaseFactory({
+    transmettreDemandeComplèteRaccordementCommand:
+      transmettreDemandeComplèteRaccordementCommandHandlerFactory({
+        loadAggregate,
+        publish,
+      }),
+    consulterGestionnaireRéseauQuery,
+  });
+
+const transmettreDateMiseEnServiceCommand = transmettreDateMiseEnServiceCommandHandlerFactory({
+  loadAggregate,
+  publish,
+});
+
+const transmettrePropositionTechniqueEtFinancièreCommand =
+  transmettrePropositionTechniqueEtFinancièreCommandHandlerFactory({
+    loadAggregate,
+    publish,
+  });
+
+(async () => {
+  const result = await executeSelect<{
+    id: string;
+    appelOffreId: string;
+    periodeId: string;
+    familleId: string;
+    numeroCRE: string;
+    identifiantGestionnaire: string | undefined;
+    dateMiseEnService: Date | undefined;
+    dcrDate: Date | undefined;
+    dateFileAttente: Date;
+    dcrFilePath: string;
+    ptfFilePath: string;
+    ptfDateDeSignature: Date | undefined;
+  }>(`
+    select
+      p."id",
+      p."appelOffreId", 
+      p."periodeId", 
+      p."familleId", 
+      p."numeroCRE",
+      r."identifiantGestionnaire", 
+      p."dateMiseEnService", 
+      p."dateFileAttente",
+      "dcrFile"."storedAt" as "dcrFilePath", 
+      dcr."valueDate" as "dcrDate",
+      "ptfFile"."storedAt" as "ptfFilePath", 
+      r."ptfDateDeSignature"
+    from projects p
+    left join raccordements r on r."projetId" = p.id
+    left join project_events dcr on dcr."projectId" = p.id and dcr.type = 'ProjectDCRSubmitted'
+    left join files "dcrFile" on "dcrFile".id::text = dcr.payload->'file'->>'id' and "dcrFile".designation = 'dcr'
+    left join files "ptfFile" on "ptfFile".id = r."ptfFichierId" and "ptfFile".designation = 'ptf'`);
+
+  // TODO archiver event du projet
+  const projetMigré: string[] = [];
+
+  for (const projet of result) {
+    try {
+      if (projet.identifiantGestionnaire && projet.dcrDate) {
+        const identifiantProjet = {
+          appelOffre: projet.appelOffreId,
+          numéroCRE: projet.numeroCRE,
+          période: projet.periodeId,
+          famille: projet.familleId,
+        };
+        const référenceDossierRaccordement = projet.identifiantGestionnaire;
+
+        await transmettreDemandeComplèteRaccordementUseCase({
+          identifiantGestionnaireRéseau: {
+            codeEIC: '17X100A100A0001A',
+          },
+          référenceDossierRaccordement,
+          dateQualification: projet.dcrDate,
+          identifiantProjet,
+        });
+
+        if (projet.ptfDateDeSignature) {
+          await transmettrePropositionTechniqueEtFinancièreCommand({
+            dateSignature: projet.ptfDateDeSignature,
+            identifiantProjet,
+            référenceDossierRaccordement,
+          });
+        }
+
+        if (projet.dateMiseEnService) {
+          await transmettreDateMiseEnServiceCommand({
+            dateMiseEnService: projet.dateMiseEnService,
+            identifiantProjet,
+            référenceDossierRaccordement,
+          });
+        }
+        projetMigré.push(projet.id);
+      } else if (projet.identifiantGestionnaire && !projet.dcrDate) {
+        // Step 2
+        // Les projets qui ont un identifiant gestionnaire réseaux et pas de DCR
+      } else if (projet.ptfDateDeSignature && !projet.dcrDate) {
+        // Step 3
+        // Les projets qui ont une PTF sans DCR avec ou sans identifiant
+      } else {
+        console.log('Projet ne rentrant pas dans les conditions');
+        console.table(projet);
+      }
+    } catch (e) {
+      console.log('Erreur lors de la migration du projet');
+      console.error(e);
+    }
+    console.log(`${result.indexOf(projet) + 1}/${result.length}`);
+  }
+})();
