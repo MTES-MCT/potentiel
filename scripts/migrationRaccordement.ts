ca53092bdabe9f99544967858fa2cc0da0854582
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-04-26 10:26:54 +0200

    ♻️ Remove promise.all

--- scripts/migrationRaccordement.ts
+++ scripts/migrationRaccordement.ts
@@ -81,72 +81,70 @@ export const sleep = async (ms: number) => {
   // TODO archiver event du projet
   const projetMigré: string[] = [];
 
-  await Promise.all(
-    projets.map(async (projet) => {
-      try {
-        const identifiantProjet = {
-          appelOffre: projet.appelOffreId,
-          numéroCRE: projet.numeroCRE,
-          période: projet.periodeId,
-          famille: projet.familleId,
-        };
-        console.log(`Projet: ${formatIdentifiantProjet(identifiantProjet)}`);
-        if (projet.identifiantGestionnaire && projet.dcrDate) {
-          console.log('Projet avec identifiant Gestionnaire et avec DCR');
-        } else if (!projet.identifiantGestionnaire && projet.dcrDate) {
-          console.log('Projet sans identifiant Gestionnaire et avec DCR');
-        } else if (projet.identifiantGestionnaire && !projet.dcrDate) {
-          console.log('Projet avec identifiant Gestionnaire et sans DCR');
-        } else {
-          console.log('Projet sans identifiant Gestionnaire et sans DCR');
-        }
+  for (const projet of projets) {
+    try {
+      const identifiantProjet = {
+        appelOffre: projet.appelOffreId,
+        numéroCRE: projet.numeroCRE,
+        période: projet.periodeId,
+        famille: projet.familleId,
+      };
+      console.log(`Projet: ${formatIdentifiantProjet(identifiantProjet)}`);
+      if (projet.identifiantGestionnaire && projet.dcrDate) {
+        console.log('Projet avec identifiant Gestionnaire et avec DCR');
+      } else if (!projet.identifiantGestionnaire && projet.dcrDate) {
+        console.log('Projet sans identifiant Gestionnaire et avec DCR');
+      } else if (projet.identifiantGestionnaire && !projet.dcrDate) {
+        console.log('Projet avec identifiant Gestionnaire et sans DCR');
+      } else {
+        console.log('Projet sans identifiant Gestionnaire et sans DCR');
+      }
 
-        const référenceDossierRaccordement =
-          projet.identifiantGestionnaire || 'Référence non transmise';
-        const dateQualification = projet.dcrDate ?? projet.dateFileAttente ?? undefined;
+      const référenceDossierRaccordement =
+        projet.identifiantGestionnaire || 'Référence non transmise';
+      const dateQualification = projet.dcrDate ?? projet.dateFileAttente ?? undefined;
 
-        await transmettreDemandeComplèteRaccordementUseCase({
-          identifiantGestionnaireRéseau: {
-            codeEIC: '17X100A100A0001A',
-          },
-          référenceDossierRaccordement,
-          dateQualification,
+      await transmettreDemandeComplèteRaccordementUseCase({
+        identifiantGestionnaireRéseau: {
+          codeEIC: '17X100A100A0001A',
+        },
+        référenceDossierRaccordement,
+        dateQualification,
+        identifiantProjet,
+      });
+
+      await sleep(50);
+
+      if (projet.ptfDateDeSignature) {
+        await transmettrePropositionTechniqueEtFinancièreCommand({
+          dateSignature: projet.ptfDateDeSignature,
           identifiantProjet,
+          référenceDossierRaccordement,
+        });
+      }
+      await sleep(50);
+
+      if (projet.dateMiseEnService) {
+        await transmettreDateMiseEnServiceCommand({
+          dateMiseEnService: projet.dateMiseEnService,
+          identifiantProjet,
+          référenceDossierRaccordement,
         });
 
         await sleep(50);
-
-        if (projet.ptfDateDeSignature) {
-          await transmettrePropositionTechniqueEtFinancièreCommand({
-            dateSignature: projet.ptfDateDeSignature,
-            identifiantProjet,
-            référenceDossierRaccordement,
-          });
-        }
-        await sleep(50);
-
-        if (projet.dateMiseEnService) {
-          await transmettreDateMiseEnServiceCommand({
-            dateMiseEnService: projet.dateMiseEnService,
-            identifiantProjet,
-            référenceDossierRaccordement,
-          });
-
-          await sleep(50);
-        }
-
-        // TODO: gérer les fichiers
-
-        // TODO: archiver les events
-
-        projetMigré.push(projet.id);
-      } catch (e) {
-        console.log('Erreur lors de la migration du projet');
-        console.error(e);
       }
-      console.log(`${projets.indexOf(projet) + 1}/${projets.length}`);
-    }),
-  );
+
+      // TODO: gérer les fichiers
+
+      // TODO: archiver les events
+
+      projetMigré.push(projet.id);
+    } catch (e) {
+      console.log('Erreur lors de la migration du projet');
+      console.error(e);
+    }
+    console.log(`${projets.indexOf(projet) + 1}/${projets.length}`);
+  }
 
   console.log(`nombre de projets migrés : ${projetMigré.length}/${projets.length}`);
 })();
