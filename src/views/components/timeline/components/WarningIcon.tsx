9c4f9acfe66523edb7c91c4a5001a1981bee81f6
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-05-17 12:49:33 +0200

    🚸 Warning item

--- src/views/components/timeline/components/WarningIcon.tsx
+++ src/views/components/timeline/components/WarningIcon.tsx
@@ -6,7 +6,7 @@ export const WarningIcon = () => (
   <div className="h-9 flex items-center">
     <span
       className={
-        'relative z-2 w-8 h-8 flex items-center justify-center bg-yellow-400 rounded-full group-hover:bg-yellow-200'
+        'relative z-2 w-8 h-8 flex items-center justify-center bg-warning-425-base rounded-full group-hover:bg-warning-425-hover'
       }
     >
       <ExclamationIcon className="h-6 w-6 text-white" title="alerte" />
