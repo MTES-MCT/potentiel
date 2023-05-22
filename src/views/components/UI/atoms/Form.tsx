7e42b951ca21ebf7098ad671216d849e3b70b285
HubM <hubmoncenis@gmail.com>
2023-05-22 16:14:13 +0200

    🎨 remove useless type

--- src/views/components/UI/atoms/Form.tsx
+++ src/views/components/UI/atoms/Form.tsx
@@ -1,6 +1,6 @@
 import React, { ComponentProps } from 'react';
 
-type FormProps = ComponentProps<'form'> & {};
+type FormProps = ComponentProps<'form'>;
 
 export const Form = ({ children, className = '', ...props }: FormProps) => (
   <form {...props} className={`max-w-xl flex flex-col gap-4 ${className}`}>
