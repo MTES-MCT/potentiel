60e60e2cbb28b6c401096965783ebac62c54d009
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-05-22 14:25:05 +0200

    ♻️ Ajout usecase lister dossier

---
+++ packages/domain/src/raccordement/dossierRaccordement/listerDossierRaccordement.usecase.ts
@@ -0,0 +1,30 @@
+import { Message, MessageHandler, mediator, getMessageBuilder } from 'mediateur';
+import { IdentifiantProjet } from '../../projet/identifiantProjet';
+import { ListeDossiersRaccordementReadModel } from './lister/listeDossierRaccordement.readModel';
+
+const LISTER_DOSSIER_RACCORDEMENT_USECASE = Symbol('LISTER_DOSSIER_RACCORDEMENT_USECASE');
+
+export type ListerDossiersRaccordementUseCase = Message<
+  typeof LISTER_DOSSIER_RACCORDEMENT_USECASE,
+  {
+    identifiantProjet: IdentifiantProjet;
+  },
+  ListeDossiersRaccordementReadModel
+>;
+
+export const registerListerDossiersRaccordementQuery = () => {
+  const runner: MessageHandler<ListerDossiersRaccordementUseCase> = async ({
+    identifiantProjet,
+  }) => {
+    return await mediator.send(
+      buildListerDossiersRaccordementUseCase({
+        identifiantProjet,
+      }),
+    );
+  };
+
+  mediator.register(LISTER_DOSSIER_RACCORDEMENT_USECASE, runner);
+};
+
+export const buildListerDossiersRaccordementUseCase =
+  getMessageBuilder<ListerDossiersRaccordementUseCase>(LISTER_DOSSIER_RACCORDEMENT_USECASE);
