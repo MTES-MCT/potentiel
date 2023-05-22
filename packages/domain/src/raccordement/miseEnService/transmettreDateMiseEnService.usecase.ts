4c2f784f065bfb4a1cfa1f5240ba961ef9260b38
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-05-22 14:28:54 +0200

    ♻️ Ajout usecase mise en service

---
+++ packages/domain/src/raccordement/miseEnService/transmettreDateMiseEnService.usecase.ts
@@ -0,0 +1,34 @@
+import { Message, MessageHandler, mediator, getMessageBuilder } from 'mediateur';
+import { IdentifiantProjet } from '../../projet/identifiantProjet';
+
+const TRANSMETTRE_DATE_MISE_EN_SERVICE_USECASE = Symbol('TRANSMETTRE_DATE_MISE_EN_SERVICE_USECASE');
+
+type TransmettreDateMiseEnServiceUseCase = Message<
+  typeof TRANSMETTRE_DATE_MISE_EN_SERVICE_USECASE,
+  {
+    dateMiseEnService: Date;
+    référenceDossierRaccordement: string;
+    identifiantProjet: IdentifiantProjet;
+  }
+>;
+
+export const registerTransmettreDateMiseEnServiceUseCase = () => {
+  const runner: MessageHandler<TransmettreDateMiseEnServiceUseCase> = async ({
+    dateMiseEnService,
+    référenceDossierRaccordement,
+    identifiantProjet,
+  }) => {
+    await mediator.send(
+      buildTransmettreDateMiseEnServiceCommand({
+        dateMiseEnService,
+        identifiantProjet,
+        référenceDossierRaccordement,
+      }),
+    );
+  };
+
+  mediator.register(TRANSMETTRE_DATE_MISE_EN_SERVICE_USECASE, runner);
+};
+
+export const buildTransmettreDateMiseEnServiceCommand =
+  getMessageBuilder<TransmettreDateMiseEnServiceUseCase>(TRANSMETTRE_DATE_MISE_EN_SERVICE_USECASE);
