cfe115af267544a42664e1f4608863248c7661fe
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-06-08 14:58:35 +0200

    ♻️ Refacto DomainUseCase

--- packages/domain/src/domain.usecase.ts
+++ packages/domain/src/domain.usecase.ts
@@ -1,3 +1,5 @@
-export * from './gestionnaireRéseau/gestionnaireRéseau.usecase';
-export * from './projet/projet.usecase';
-export * from './raccordement/raccordement.usecase';
+import { GestionnaireRéseauUseCase } from './gestionnaireRéseau/gestionnaireRéseau.usecase';
+import { ProjetUseCase } from './projet/projet.usecase';
+import { RaccordementUsecase } from './raccordement/raccordement.usecase';
+
+export type DomainUseCase = GestionnaireRéseauUseCase | ProjetUseCase | RaccordementUsecase;
