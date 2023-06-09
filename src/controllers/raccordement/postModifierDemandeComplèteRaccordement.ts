afc32dab11329fe535fd774216ceaa1616f4ff94
Julien Pavon <julien.pavon@gmail.com>
2023-06-09 09:36:51 +0200

    ♻️ Modifier DCR

--- src/controllers/raccordement/postModifierDemandeComplèteRaccordement.ts
+++ src/controllers/raccordement/postModifierDemandeComplèteRaccordement.ts
@@ -2,7 +2,10 @@ import { createReadStream } from 'fs';
 import { mediator } from 'mediateur';
 import {
   PermissionTransmettreDemandeComplèteRaccordement,
-  buildModifierDemandeComplèteRaccordementUseCase,
+  convertirEnIdentifiantProjet,
+  convertirEnRéférenceDossierRaccordement,
+  estUnRawIdentifiantProjet,
+  DomainUseCase,
 } from '@potentiel/domain';
 import routes from '@routes';
 import { v1Router } from '../v1Router';
@@ -20,14 +23,15 @@ import { addQueryParams } from '../../helpers/addQueryParams';
 import { logger } from '@core/utils';
 import { upload as uploadMiddleware } from '../upload';
 import { DomainError } from '@potentiel/core-domain';
+import { isSome } from '@potentiel/monads';
 
 const schema = yup.object({
   params: yup.object({
-    projetId: yup.string().uuid().required(),
+    identifiantProjet: yup.string().required(),
     reference: yup.string().required(),
   }),
   body: yup.object({
-    referenceDossierRaccordement: yup.string().required(),
+    nouvelleReferenceDossierRaccordement: yup.string().required(),
     dateQualification: yup
       .date()
       .required(`La date de qualification est obligatoire`)
@@ -60,15 +64,15 @@ v1Router.post(
     async (request, response) => {
       const {
         user,
-        params: { projetId, reference },
-        body: { dateQualification, referenceDossierRaccordement },
+        params: { identifiantProjet, reference },
+        body: { dateQualification, nouvelleReferenceDossierRaccordement },
         file,
       } = request;
 
       if (!file) {
         return response.redirect(
           addQueryParams(
-            routes.GET_MODIFIER_DEMANDE_COMPLETE_RACCORDEMENT_PAGE(projetId, reference),
+            routes.GET_MODIFIER_DEMANDE_COMPLETE_RACCORDEMENT_PAGE(identifiantProjet, reference),
             {
               error: `Vous devez joindre l'accusé de réception de la demande complète de raccordement`,
             },
@@ -76,8 +80,20 @@ v1Router.post(
         );
       }
 
-      const projet = await Project.findByPk(projetId, {
-        attributes: ['appelOffreId', 'periodeId', 'familleId', 'numeroCRE'],
+      if (!estUnRawIdentifiantProjet(identifiantProjet)) {
+        return notFoundResponse({ request, response, ressourceTitle: 'Projet' });
+      }
+
+      const identifiantProjetValueType = convertirEnIdentifiantProjet(identifiantProjet);
+
+      const projet = await Project.findOne({
+        where: {
+          appelOffreId: identifiantProjetValueType.appelOffre,
+          periodeId: identifiantProjetValueType.période,
+          familleId: isSome(identifiantProjetValueType.famille) ?? undefined,
+          numeroCRE: identifiantProjetValueType.numéroCRE,
+        },
+        attributes: ['id'],
       });
 
       if (!projet) {
@@ -90,7 +106,7 @@ v1Router.post(
 
       if (user.role === 'porteur-projet') {
         const porteurAAccèsAuProjet = !!(await UserProjects.findOne({
-          where: { projectId: projetId, userId: user.id },
+          where: { projectId: projet.id, userId: user.id },
         }));
 
         if (!porteurAAccèsAuProjet) {
@@ -102,31 +118,45 @@ v1Router.post(
         }
       }
 
-      const identifiantProjet = {
-        appelOffre: projet.appelOffreId,
-        période: projet.periodeId,
-        famille: projet.familleId,
-        numéroCRE: projet.numeroCRE,
-      };
-
+      // TODO: Avoir 2 use case exécutés dans un controller n'est pas normal il faut avoir un controller dédié pour cette action
       try {
-        await mediator.send(
-          buildModifierDemandeComplèteRaccordementUseCase({
-            identifiantProjet,
+        const nouvelleRéférenceDossierRaccordementValueType =
+          convertirEnRéférenceDossierRaccordement(nouvelleReferenceDossierRaccordement);
+
+        const référenceDossierRaccordementActuelle =
+          convertirEnRéférenceDossierRaccordement(reference);
+        if (
+          !nouvelleRéférenceDossierRaccordementValueType.estÉgaleÀ(
+            référenceDossierRaccordementActuelle,
+          )
+        ) {
+          await mediator.send<DomainUseCase>({
+            type: 'MODIFIER_RÉFÉRENCE_DOSSIER_RACCORDEMENT_USE_CASE',
+            data: {
+              identifiantProjet: convertirEnIdentifiantProjet(identifiantProjet),
+              nouvelleRéférenceDossierRaccordement: nouvelleRéférenceDossierRaccordementValueType,
+              référenceDossierRaccordementActuelle: référenceDossierRaccordementActuelle,
+            },
+          });
+        }
+
+        await mediator.send<DomainUseCase>({
+          type: 'MODIFIER_DEMANDE_COMPLÈTE_RACCORDEMENT_USE_CASE',
+          data: {
+            identifiantProjet: convertirEnIdentifiantProjet(identifiantProjet),
+            référenceDossierRaccordement: nouvelleRéférenceDossierRaccordementValueType,
             dateQualification,
-            nouvelleRéférenceDossierRaccordement: referenceDossierRaccordement,
-            ancienneRéférenceDossierRaccordement: reference,
-            nouvelAccuséRéception: {
+            accuséRéception: {
               format: file.mimetype,
               content: createReadStream(file.path),
             },
-          }),
-        );
+          },
+        });
 
         return response.redirect(
           routes.SUCCESS_OR_ERROR_PAGE({
             success: 'La demande complète de raccordement est bien mise à jour',
-            redirectUrl: routes.GET_LISTE_DOSSIERS_RACCORDEMENT(projetId),
+            redirectUrl: routes.GET_LISTE_DOSSIERS_RACCORDEMENT(identifiantProjet),
             redirectTitle: 'Retourner sur la page raccordement',
           }),
         );
@@ -134,7 +164,7 @@ v1Router.post(
         if (error instanceof DomainError) {
           return response.redirect(
             addQueryParams(
-              routes.GET_MODIFIER_DEMANDE_COMPLETE_RACCORDEMENT_PAGE(projetId, reference),
+              routes.GET_MODIFIER_DEMANDE_COMPLETE_RACCORDEMENT_PAGE(identifiantProjet, reference),
               {
                 error: error.message,
               },
