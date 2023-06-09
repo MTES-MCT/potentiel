ed69eda6c7eb181b09a22ed07fcc9a9552d72804
Julien Pavon <julien.pavon@gmail.com>
2023-06-09 10:17:41 +0200

    ♻️ Modifier PTF controller

--- src/controllers/raccordement/postModifierPropositionTechniqueEtFinancière.ts
+++ src/controllers/raccordement/postModifierPropositionTechniqueEtFinancière.ts
@@ -1,7 +1,9 @@
 import {
-  DossierRaccordementNonRéférencéError,
+  DomainUseCase,
   PermissionTransmettrePropositionTechniqueEtFinancière,
-  buildModifierPropositiontechniqueEtFinancièreUseCase,
+  convertirEnIdentifiantProjet,
+  convertirEnRéférenceDossierRaccordement,
+  estUnRawIdentifiantProjet,
 } from '@potentiel/domain';
 import routes from '@routes';
 import { v1Router } from '../v1Router';
@@ -21,10 +23,12 @@ import { upload as uploadMiddleware } from '../upload';
 import { createReadStream } from 'fs';
 
 import { mediator } from 'mediateur';
+import { isSome } from '@potentiel/monads';
+import { DomainError } from '@potentiel/core-domain';
 
 const schema = yup.object({
   params: yup.object({
-    projetId: yup.string().uuid().required(),
+    identifiantProjet: yup.string().required(),
     reference: yup.string().required(),
   }),
   body: yup.object({
@@ -53,7 +57,7 @@ v1Router.post(
     },
     async (request, response) => {
       const {
-        params: { projetId, reference },
+        params: { identifiantProjet, reference },
         body: { dateSignature },
         file,
         user,
@@ -61,14 +65,29 @@ v1Router.post(
 
       if (!file) {
         return response.redirect(
-          addQueryParams(routes.GET_MODIFIER_PROPOSITION_TECHNIQUE_ET_FINANCIERE_PAGE(projetId), {
-            error: `Vous devez joindre la proposition technique et financière`,
-          }),
+          addQueryParams(
+            routes.GET_MODIFIER_PROPOSITION_TECHNIQUE_ET_FINANCIERE_PAGE(identifiantProjet),
+            {
+              error: `Vous devez joindre la proposition technique et financière`,
+            },
+          ),
         );
       }
 
-      const projet = await Project.findByPk(projetId, {
-        attributes: ['appelOffreId', 'periodeId', 'familleId', 'numeroCRE'],
+      if (!estUnRawIdentifiantProjet(identifiantProjet)) {
+        return notFoundResponse({ request, response, ressourceTitle: 'Projet' });
+      }
+
+      const identifiantProjetValueType = convertirEnIdentifiantProjet(identifiantProjet);
+
+      const projet = await Project.findOne({
+        where: {
+          appelOffreId: identifiantProjetValueType.appelOffre,
+          periodeId: identifiantProjetValueType.période,
+          familleId: isSome(identifiantProjetValueType.famille) ?? undefined,
+          numeroCRE: identifiantProjetValueType.numéroCRE,
+        },
+        attributes: ['id'],
       });
 
       if (!projet) {
@@ -81,7 +100,7 @@ v1Router.post(
 
       if (user.role === 'porteur-projet') {
         const porteurAAccèsAuProjet = !!(await UserProjects.findOne({
-          where: { projectId: projetId, userId: user.id },
+          where: { projectId: projet.id, userId: user.id },
         }));
 
         if (!porteurAAccèsAuProjet) {
@@ -93,38 +112,35 @@ v1Router.post(
         }
       }
 
-      const identifiantProjet = {
-        appelOffre: projet.appelOffreId,
-        période: projet.periodeId,
-        famille: projet.familleId,
-        numéroCRE: projet.numeroCRE,
-      };
-
       try {
-        await mediator.send(
-          buildModifierPropositiontechniqueEtFinancièreUseCase({
-            identifiantProjet,
-            référenceDossierRaccordement: reference,
+        await mediator.send<DomainUseCase>({
+          type: 'MODIFIER_PROPOSITION_TECHNIQUE_ET_FINANCIÈRE_USECASE',
+          data: {
+            identifiantProjet: identifiantProjetValueType,
+            référenceDossierRaccordement: convertirEnRéférenceDossierRaccordement(reference),
             dateSignature,
-            nouvellePropositionTechniqueEtFinancière: {
+            propositionTechniqueEtFinancièreSignée: {
               format: file.mimetype,
               content: createReadStream(file.path),
             },
-          }),
-        );
+          },
+        });
 
         return response.redirect(
           routes.SUCCESS_OR_ERROR_PAGE({
             success: 'La proposition technique et financière a bien été mise à jour',
-            redirectUrl: routes.GET_LISTE_DOSSIERS_RACCORDEMENT(projetId),
+            redirectUrl: routes.GET_LISTE_DOSSIERS_RACCORDEMENT(identifiantProjet),
             redirectTitle: 'Retourner sur la page raccordement',
           }),
         );
       } catch (error) {
-        if (error instanceof DossierRaccordementNonRéférencéError) {
+        if (error instanceof DomainError) {
           return response.redirect(
             addQueryParams(
-              routes.GET_MODIFIER_PROPOSITION_TECHNIQUE_ET_FINANCIERE_PAGE(projetId, reference),
+              routes.GET_MODIFIER_PROPOSITION_TECHNIQUE_ET_FINANCIERE_PAGE(
+                identifiantProjet,
+                reference,
+              ),
               {
                 error: error.message,
               },
