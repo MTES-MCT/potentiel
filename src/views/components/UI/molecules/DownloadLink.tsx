b329f35b99d0cd1f57102b10e2f3e431517addad
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-06-19 16:54:55 +0200

    🚸 Liste projets GF

--- src/views/components/UI/molecules/DownloadLink.tsx
+++ src/views/components/UI/molecules/DownloadLink.tsx
@@ -4,10 +4,16 @@ import { Link, FileDownloadIcon } from '../atoms';
 type DownloadLinkProps = {
   className?: string;
   fileUrl: string;
+  intituléLien?: string;
 };
 
-export const DownloadLink: FC<DownloadLinkProps> = ({ children, className, fileUrl }) => (
-  <Link className={className} href={fileUrl} download>
+export const DownloadLink: FC<DownloadLinkProps> = ({
+  children,
+  className,
+  fileUrl,
+  intituléLien = '',
+}) => (
+  <Link className={className} href={fileUrl} download aria-label={intituléLien}>
     <FileDownloadIcon className="text-lg mr-1 -mb-1 shrink-0" aria-hidden />
     {children}
   </Link>
