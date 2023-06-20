cb09a09221dd1e97b184a3eb4006ad47521a0144
Julien Pavon <julien.pavon@gmail.com>
2023-06-20 18:08:38 +0200

    🔥 Suppression du local file storage inutilisé

--- src/config/fileStorage.config.ts
+++ src/config/fileStorage.config.ts
@@ -1,42 +1,34 @@
-import { makeLocalFileStorageService, makeS3FileStorageService } from '@infra/file';
-import { FileStorageService } from '@modules/file';
-import { isProdEnv, isStagingEnv } from './env.config';
+import { makeS3FileStorageService } from '@infra/file';
 
-let fileStorageService: FileStorageService;
-if (isStagingEnv || isProdEnv) {
-  const {
-    LEGACY_S3_ACCESS_KEY_ID,
-    LEGACY_S3_SECRET_ACCESS_KEY,
-    LEGACY_S3_ENDPOINT,
-    LEGACY_S3_BUCKET,
-  } = process.env;
+const {
+  LEGACY_S3_ACCESS_KEY_ID,
+  LEGACY_S3_SECRET_ACCESS_KEY,
+  LEGACY_S3_ENDPOINT,
+  LEGACY_S3_BUCKET,
+} = process.env;
 
-  const missingVars = [
-    'LEGACY_S3_BUCKET',
-    'LEGACY_S3_ENDPOINT',
-    'LEGACY_S3_ACCESS_KEY_ID',
-    'LEGACY_S3_SECRET_ACCESS_KEY',
-  ].filter((key) => !process.env[key]);
+const missingVars = [
+  'LEGACY_S3_BUCKET',
+  'LEGACY_S3_ENDPOINT',
+  'LEGACY_S3_ACCESS_KEY_ID',
+  'LEGACY_S3_SECRET_ACCESS_KEY',
+].filter((key) => !process.env[key]);
 
-  if (missingVars.length) {
-    const errorMsg = `Cannot start S3FileStorageService because of missing environment variables: ${missingVars.join(
-      ', ',
-    )}`;
-    console.error(errorMsg);
-    process.exit(1);
-  }
-
-  fileStorageService = makeS3FileStorageService({
-    accessKeyId: LEGACY_S3_ACCESS_KEY_ID!,
-    secretAccessKey: LEGACY_S3_SECRET_ACCESS_KEY!,
-    endpoint: LEGACY_S3_ENDPOINT!,
-    bucket: LEGACY_S3_BUCKET!,
-  });
-
-  console.log(`FileService will be using S3 on bucket ${LEGACY_S3_BUCKET}`);
-} else {
-  console.log('FileService will be using LocalFileStorage is userData/');
-  fileStorageService = makeLocalFileStorageService('userData');
+if (missingVars.length) {
+  const errorMsg = `Cannot start S3FileStorageService because of missing environment variables: ${missingVars.join(
+    ', ',
+  )}`;
+  console.error(errorMsg);
+  process.exit(1);
 }
+
+const fileStorageService = makeS3FileStorageService({
+  accessKeyId: LEGACY_S3_ACCESS_KEY_ID!,
+  secretAccessKey: LEGACY_S3_SECRET_ACCESS_KEY!,
+  endpoint: LEGACY_S3_ENDPOINT!,
+  bucket: LEGACY_S3_BUCKET!,
+});
+
+console.log(`FileService will be using S3 on bucket ${LEGACY_S3_BUCKET}`);
 
 export { fileStorageService };
