a1ac976beca6f381d3a95d7fd51e11d984ef9bca
Julien Pavon <julien.pavon@gmail.com>
2023-06-21 09:06:24 +0200

    🔧 Ajout des variables d'environnements pour les tests d'integration et s3

---
+++ jest.integration.env.ts
@@ -0,0 +1,4 @@
+process.env.LEGACY_S3_BUCKET = 'potentiel';
+process.env.LEGACY_S3_ENDPOINT = 'http://localhost:9001';
+process.env.LEGACY_S3_ACCESS_KEY_ID = 'minioadmin';
+process.env.LEGACY_S3_SECRET_ACCESS_KEY = 'minioadmin';
