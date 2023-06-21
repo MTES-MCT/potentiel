cd9b28042c71e0859114ea65a0e5d8de28f8c0ab
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-06-21 12:20:31 +0200

    🎨 ne pas utiliser la balise legend hors contexte de formulaire

--- src/views/components/UI/organisms/ProjectList.tsx
+++ src/views/components/UI/organisms/ProjectList.tsx
@@ -94,7 +94,7 @@ export const ProjectList = ({
 
   return (
     <>
-      <legend className="flex flex-col md:flex-row gap-2 mb-2 text-sm italic mt-4" aria-hidden>
+      <div className="flex flex-col md:flex-row gap-2 mb-2 text-sm italic mt-4" aria-hidden>
         Légende :
         <div className="flex items-center">
           <PowerIcon
@@ -132,7 +132,7 @@ export const ProjectList = ({
             Évaluation carbone
           </div>
         )}
-      </legend>
+      </div>
 
       <div className="p-5 flex items-center">
         {displaySelection && (
