4b9dc932d678c521017142337ad07b3eca3e94da
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2023-06-26 10:51:05 +0200

    ♻️ Publication des messages pour executer les projectors et saga (#1135)

--- packages/applications/web/src/subscribe.factory.ts
+++ packages/applications/web/src/subscribe.factory.ts
@@ -19,7 +19,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   const consumerGestionnaireRéseau = await consumerFactory('gestionnaireRéseauProjector');
 
   consumerGestionnaireRéseau.consume('GestionnaireRéseauAjouté', (event: GestionnaireRéseauEvent) =>
-    mediator.send<ExecuteGestionnaireRéseauProjector>({
+    mediator.publish<ExecuteGestionnaireRéseauProjector>({
       type: 'EXECUTE_GESTIONNAIRE_RÉSEAU_PROJECTOR',
       data: event,
     }),
@@ -27,7 +27,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerGestionnaireRéseau.consume(
     'GestionnaireRéseauModifié',
     (event: GestionnaireRéseauEvent) =>
-      mediator.send<ExecuteGestionnaireRéseauProjector>({
+      mediator.publish<ExecuteGestionnaireRéseauProjector>({
         type: 'EXECUTE_GESTIONNAIRE_RÉSEAU_PROJECTOR',
         data: event,
       }),
@@ -36,14 +36,14 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   const consumerProjet = await consumerFactory('projetProjector');
 
   consumerProjet.consume('GestionnaireRéseauProjetDéclaré', (event: ProjetEvent) =>
-    mediator.send<ExecuteProjetProjector>({
+    mediator.publish<ExecuteProjetProjector>({
       type: 'EXECUTE_PROJET_PROJECTOR',
       data: event,
     }),
   );
 
   consumerProjet.consume('GestionnaireRéseauProjetModifié', (event: ProjetEvent) =>
-    mediator.send<ExecuteProjetProjector>({
+    mediator.publish<ExecuteProjetProjector>({
       type: 'EXECUTE_PROJET_PROJECTOR',
       data: event,
     }),
@@ -52,7 +52,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerProjet.consume(
     'DemandeComplèteDeRaccordementTransmise',
     (event: DemandeComplèteRaccordementTransmiseEvent) =>
-      mediator.send<ExecuterAjouterGestionnaireRéseauProjetSaga>({
+      mediator.publish<ExecuterAjouterGestionnaireRéseauProjetSaga>({
         type: 'EXECUTER_DÉCLARER_GESTIONNAIRE_RÉSEAU_PROJET_SAGA',
         data: event,
       }),
@@ -63,13 +63,13 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'AccuséRéceptionDemandeComplèteRaccordementTransmis',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
   );
   consumerRaccordement.consume('DateMiseEnServiceTransmise', (event: RaccordementEvent) =>
-    mediator.send<ExecuteRaccordementProjector>({
+    mediator.publish<ExecuteRaccordementProjector>({
       type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
       data: event,
     }),
@@ -78,14 +78,14 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'DemandeComplèteDeRaccordementTransmise',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
   );
 
   consumerRaccordement.consume('DemandeComplèteRaccordementModifiée', (event: RaccordementEvent) =>
-    mediator.send<ExecuteRaccordementProjector>({
+    mediator.publish<ExecuteRaccordementProjector>({
       type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
       data: event,
     }),
@@ -94,7 +94,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'DemandeComplèteRaccordementModifiée-V1',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
@@ -103,7 +103,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'PropositionTechniqueEtFinancièreModifiée',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
@@ -112,7 +112,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'PropositionTechniqueEtFinancièreSignéeTransmise',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
@@ -121,7 +121,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'PropositionTechniqueEtFinancièreTransmise',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
@@ -130,7 +130,7 @@ export const subscribeFactory = async (): Promise<Subscribe> => {
   consumerRaccordement.consume(
     'RéférenceDossierRacordementModifiée-V1',
     (event: RaccordementEvent) =>
-      mediator.send<ExecuteRaccordementProjector>({
+      mediator.publish<ExecuteRaccordementProjector>({
         type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
         data: event,
       }),
