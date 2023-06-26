3a2474b69530488c1f03aff021ac446559b61ca0
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-06-26 15:57:30 +0200

    🚸 (Formulaire DCR): Texte infoBox et lien exemple AR

--- src/views/pages/raccordement/components/InfoBoxFormulaireDCR.tsx
+++ src/views/pages/raccordement/components/InfoBoxFormulaireDCR.tsx
@@ -11,17 +11,7 @@ export const InfoBoxFormulaireDCR = ({
 }: InfoBoxFormulaireDCRProps) => (
   <InfoBox className="flex md:w-1/3 md:mx-auto" {...props}>
     <p>
-      <span className="font-bold">* Où trouver la référence de mon dossier ?</span>
-      <br />
-      Vous pouvez retrouver cette donnée sur le courriel d'accusé de réception de votre demande
-      complète de raccordement (
-      <ExternalLink href="https://docs.potentiel.beta.gouv.fr/gerer-mes-projets-et-documents/comment-transmettre-ma-demande-complete-de-raccordement-dcr">
-        Voir un exemple
-      </ExternalLink>
-      )
-    </p>
-    <p>
-      <span className="font-bold">** Quel document transmettre ?</span>
+      <span className="font-bold">** Quel document transmettre dans Potentiel ?</span>
       <br />
       Vous devez déposer une demande de raccordement dans les{' '}
       {delaiDemandeDeRaccordementEnMois.texte} ({delaiDemandeDeRaccordementEnMois.valeur}) mois
@@ -30,6 +20,21 @@ export const InfoBoxFormulaireDCR = ({
       Votre gestionnaire de réseau vous retourne un{' '}
       <span className="italic">accusé de réception</span> lorsque votre demande de raccordement est
       jugée complète.
+      <br />
+      Cet accusé de réception transmis sur Potentiel facilitera vos démarches administratives avec
+      les différents acteurs connectés à Potentiel (DGEC, DREAL, Cocontractant, etc.), il est
+      nécessaire dans le cadre de l’instruction selon les cahiers des charges modificatifs et publié
+      le 30/08/2022.
+    </p>
+    <p>
+      <span className="font-bold">* Où trouver la référence de mon dossier ?</span>
+      <br />
+      Vous pouvez retrouver cette donnée sur le courriel d'accusé de réception de votre demande
+      complète de raccordement (
+      <ExternalLink href="https://docs.potentiel.beta.gouv.fr/faq/ou-trouver-la-reference-du-dossier-de-raccordement-de-mon-projet">
+        voir un exemple d'accusé de réception
+      </ExternalLink>
+      )
     </p>
   </InfoBox>
 );
