b757c2f60cfed3df4ad8aa936804a36c75493a0f
HubM <hubmoncenis@gmail.com>
2023-06-27 16:32:12 +0200

    🎨 update lien page d'aide depuis la page choix cdc

--- src/views/components/InfoLienGuideUtilisationCDC.tsx
+++ src/views/components/InfoLienGuideUtilisationCDC.tsx
@@ -4,7 +4,7 @@ import { ExternalLink } from './UI';
 export const InfoLienGuideUtilisationCDC = () => (
   <span>
     Pour plus d'informations sur les modalités d'instruction veuillez consulter cette&nbsp;
-    <ExternalLink href="https://docs.potentiel.beta.gouv.fr/faire-une-demande-de-modification">
+    <ExternalLink href="https://docs.potentiel.beta.gouv.fr/guide-dutilisation/gestion-de-mon-projet-sur-potentiel/cahiers-des-charges-modificatifs">
       page d'aide
     </ExternalLink>
     .
