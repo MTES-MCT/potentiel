2209bde6f68884efe03a6447b29807078343466c
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-07-12 14:50:26 +0200

    🎨 (Demande envoyée): Mise à jour scenario test

--- src/modules/notification/eventHandlers/demandeAbandon/onAbandonDemandé.spec.ts
+++ src/modules/notification/eventHandlers/demandeAbandon/onAbandonDemandé.spec.ts
@@ -3,76 +3,75 @@ import { AbandonDemandé } from '@modules/demandeModification';
 import { makeOnAbandonDemandé } from '.';
 
 describe(`Notifier lorsqu'un abandon est demandé`, () => {
-  describe(`Etant donné un projet accessible pour deux porteurs`, () => {
-    it(`  Quand un abandon est demandé
-          Alors les deux porteurs ayant accès au projet devrait être notifié`, async () => {
-      const notifierPorteurChangementStatutDemande = jest.fn();
-      const getModificationRequestInfoForStatusNotification = () =>
-        okAsync({
-          porteursProjet: [
-            {
-              role: 'porteur-projet',
-              id: 'porteur-1',
-              email: 'porteur1@test.test',
-              fullName: 'Porteur de projet 1',
-            },
-            {
-              role: 'porteur-projet',
-              id: 'porteur-2',
-              email: 'porteur2@test.test',
-              fullName: 'Porteur de projet 2',
-            },
-          ],
-          nomProjet: 'nom-du-projet',
-          regionProjet: 'region',
-          departementProjet: 'departement',
-          type: 'abandon',
-        });
-
-      const onAbandonDemandé = makeOnAbandonDemandé({
-        notifierPorteurChangementStatutDemande,
-        getModificationRequestInfoForStatusNotification,
+  it(`Etant donné un projet accessible pour deux porteurs
+      Quand un abandon est demandé
+      Alors les deux porteurs ayant accès au projet devrait être notifié`, async () => {
+    const notifierPorteurChangementStatutDemande = jest.fn();
+    const getModificationRequestInfoForStatusNotification = () =>
+      okAsync({
+        porteursProjet: [
+          {
+            role: 'porteur-projet',
+            id: 'porteur-1',
+            email: 'porteur1@test.test',
+            fullName: 'Porteur de projet 1',
+          },
+          {
+            role: 'porteur-projet',
+            id: 'porteur-2',
+            email: 'porteur2@test.test',
+            fullName: 'Porteur de projet 2',
+          },
+        ],
+        nomProjet: 'nom-du-projet',
+        regionProjet: 'region',
+        departementProjet: 'departement',
+        type: 'abandon',
       });
 
-      await onAbandonDemandé(
-        new AbandonDemandé({
-          payload: {
-            demandeAbandonId: 'la-demande',
-            projetId: 'le-projet',
-            porteurId: 'le-porteur',
-            autorité: 'dgec',
-          },
-        }),
-      );
-
-      expect(notifierPorteurChangementStatutDemande).toHaveBeenCalledTimes(2);
-      expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
-        1,
-        expect.objectContaining({
-          email: 'porteur1@test.test',
-          status: 'envoyée',
-          fullName: 'Porteur de projet 1',
-          porteurId: 'porteur-1',
-          typeDemande: 'abandon',
-          nomProjet: 'nom-du-projet',
-          modificationRequestId: 'la-demande',
-          hasDocument: true,
-        }),
-      );
-
-      expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
-        2,
-        expect.objectContaining({
-          email: 'porteur2@test.test',
-          status: 'envoyée',
-          fullName: 'Porteur de projet 2',
-          porteurId: 'porteur-2',
-          typeDemande: 'abandon',
-          nomProjet: 'nom-du-projet',
-          modificationRequestId: 'la-demande',
-          hasDocument: true,
-        }),
-      );
+    const onAbandonDemandé = makeOnAbandonDemandé({
+      notifierPorteurChangementStatutDemande,
+      getModificationRequestInfoForStatusNotification,
     });
+
+    await onAbandonDemandé(
+      new AbandonDemandé({
+        payload: {
+          demandeAbandonId: 'la-demande',
+          projetId: 'le-projet',
+          porteurId: 'le-porteur',
+          autorité: 'dgec',
+        },
+      }),
+    );
+
+    expect(notifierPorteurChangementStatutDemande).toHaveBeenCalledTimes(2);
+    expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
+      1,
+      expect.objectContaining({
+        email: 'porteur1@test.test',
+        status: 'envoyée',
+        fullName: 'Porteur de projet 1',
+        porteurId: 'porteur-1',
+        typeDemande: 'abandon',
+        nomProjet: 'nom-du-projet',
+        modificationRequestId: 'la-demande',
+        hasDocument: true,
+      }),
+    );
+
+    expect(notifierPorteurChangementStatutDemande).toHaveBeenNthCalledWith(
+      2,
+      expect.objectContaining({
+        email: 'porteur2@test.test',
+        status: 'envoyée',
+        fullName: 'Porteur de projet 2',
+        porteurId: 'porteur-2',
+        typeDemande: 'abandon',
+        nomProjet: 'nom-du-projet',
+        modificationRequestId: 'la-demande',
+        hasDocument: true,
+      }),
+    );
   });
 });
--- src/modules/notification/eventHandlers/modificationRequests/handleModificationRequested.spec.ts
+++ src/modules/notification/eventHandlers/modificationRequests/handleModificationRequested.spec.ts
@@ -22,127 +22,127 @@ describe('notification.handleModificationRequested', () => {
   ) as GetInfoForModificationRequested;
 
   const sendNotification = jest.fn(async (args: NotificationArgs) => null);
+  const findProjectById = jest.fn();
+  const findUsersForDreal = jest.fn();
 
-  describe('in general', () => {
-    const findProjectById = jest.fn();
-    const findUsersForDreal = jest.fn();
+  it(`Etant donné un projet ayant plusieurs porteurs rattachés
+      Lorsque l'un des porteurs dépose une demande
+      Alors tous les porteurs ayant accès au projet devraient être notifiés`, async () => {
+    sendNotification.mockClear();
 
-    it('should set send a status-update email to the PP that submitted the request', async () => {
-      sendNotification.mockClear();
+    await handleModificationRequested({
+      sendNotification,
+      getInfoForModificationRequested,
+      findUsersForDreal,
+      findProjectById,
+    })(
+      new ModificationRequested({
+        payload: {
+          type: 'recours',
+          modificationRequestId,
+          projectId,
+          requestedBy: userId,
+          authority: 'dgec',
+        },
+      }),
+    );
 
-      await handleModificationRequested({
-        sendNotification,
-        getInfoForModificationRequested,
-        findUsersForDreal,
-        findProjectById,
-      })(
-        new ModificationRequested({
-          payload: {
-            type: 'recours',
-            modificationRequestId,
-            projectId,
-            requestedBy: userId,
-            authority: 'dgec',
-          },
-        }),
-      );
+    expect(getInfoForModificationRequested).toHaveBeenCalledWith({ projectId, userId });
 
-      expect(getInfoForModificationRequested).toHaveBeenCalledWith({ projectId, userId });
-
-      expect(sendNotification).toHaveBeenCalledTimes(1);
-      const notifications = sendNotification.mock.calls.map((call) => call[0]);
-      expect(
-        notifications.every(
-          (notification) =>
-            notification.type === 'modification-request-status-update' &&
-            notification.message.email === 'email@test.test' &&
-            notification.message.name === 'john doe' &&
-            notification.variables.status === 'envoyée' &&
-            notification.variables.nom_projet === 'nomProjet' &&
-            notification.variables.type_demande === 'recours' &&
-            notification.variables.document_absent === '',
-        ),
-      ).toBe(true);
-    });
+    expect(sendNotification).toHaveBeenCalledTimes(1);
+    const notifications = sendNotification.mock.calls.map((call) => call[0]);
+    expect(
+      notifications.every(
+        (notification) =>
+          notification.type === 'modification-request-status-update' &&
+          notification.message.email === 'email@test.test' &&
+          notification.message.name === 'john doe' &&
+          notification.variables.status === 'envoyée' &&
+          notification.variables.nom_projet === 'nomProjet' &&
+          notification.variables.type_demande === 'recours' &&
+          notification.variables.document_absent === '',
+      ),
+    ).toBe(true);
   });
 
-  describe('when modification request type is destined to DREAL admins', () => {
-    it('should send an email to the DREAL users for the project region(s)', async () => {
-      const sendNotification = jest.fn(async (args: NotificationArgs) => null);
-      const findProjectById = jest.fn(async (region: string) =>
-        makeProject(
-          makeFakeProject({
-            id: projectId,
-            nomProjet: 'nomProjet',
-            regionProjet: 'regionA / regionB',
-            departementProjet: 'departement',
-          }),
-        ).unwrap(),
-      );
-      const findUsersForDreal = jest.fn(async (region: string) =>
-        region === 'regionA'
-          ? [makeFakeUser({ email: 'drealA@test.test', fullName: 'drealA' })]
-          : [makeFakeUser({ email: 'drealB@test.test', fullName: 'drealB' })],
-      );
-
-      await handleModificationRequested({
-        sendNotification,
-        findProjectById,
-        getInfoForModificationRequested,
-        findUsersForDreal,
-      })(
-        new ModificationRequested({
-          payload: {
-            type: 'puissance',
-            modificationRequestId,
-            projectId,
-            requestedBy: userId,
-            puissance: 18,
-            justification: 'justification',
-            authority: 'dreal',
-          },
+  it(`Etant donné un projet sous l'autorité de deux régions 
+      Et plusieurs agent rattachés à la DREAL chaque région 
+      Quand une demande est envoyée
+      Alors tous les agents des deux régions du projet devraient être notifiés`, async () => {
+    const sendNotification = jest.fn(async (args: NotificationArgs) => null);
+    const findProjectById = jest.fn(async (region: string) =>
+      makeProject(
+        makeFakeProject({
+          id: projectId,
+          nomProjet: 'nomProjet',
+          regionProjet: 'regionA / regionB',
+          departementProjet: 'departement',
         }),
-      );
+      ).unwrap(),
+    );
+    const findUsersForDreal = jest.fn(async (region: string) =>
+      region === 'regionA'
+        ? [makeFakeUser({ email: 'drealA@test.test', fullName: 'drealA' })]
+        : [makeFakeUser({ email: 'drealB@test.test', fullName: 'drealB' })],
+    );
 
-      expect(findUsersForDreal).toHaveBeenCalledTimes(2);
-      expect(findUsersForDreal).toHaveBeenCalledWith('regionA');
-      expect(findUsersForDreal).toHaveBeenCalledWith('regionB');
+    await handleModificationRequested({
+      sendNotification,
+      findProjectById,
+      getInfoForModificationRequested,
+      findUsersForDreal,
+    })(
+      new ModificationRequested({
+        payload: {
+          type: 'puissance',
+          modificationRequestId,
+          projectId,
+          requestedBy: userId,
+          puissance: 18,
+          justification: 'justification',
+          authority: 'dreal',
+        },
+      }),
+    );
 
-      expect(sendNotification).toHaveBeenCalledTimes(3);
-      const notifications = sendNotification.mock.calls.map((call) => call[0]);
+    expect(findUsersForDreal).toHaveBeenCalledTimes(2);
+    expect(findUsersForDreal).toHaveBeenCalledWith('regionA');
+    expect(findUsersForDreal).toHaveBeenCalledWith('regionB');
 
-      expect(
-        notifications.some(
-          (notification) =>
-            notification.type === 'admin-modification-requested' &&
-            notification.message.email === 'drealA@test.test' &&
-            notification.message.name === 'drealA' &&
-            notification.variables.nom_projet === 'nomProjet' &&
-            notification.variables.type_demande === 'puissance' &&
-            notification.variables.modification_request_url ===
-              routes.DEMANDE_PAGE_DETAILS(modificationRequestId) &&
-            notification.variables.departement_projet === 'departement' &&
-            notification.context.modificationRequestId === modificationRequestId &&
-            notification.context.dreal === 'regionA' &&
-            notification.context.projectId === projectId,
-        ),
-      ).toBe(true);
-      expect(
-        notifications.some(
-          (notification) =>
-            notification.type === 'admin-modification-requested' &&
-            notification.message.email === 'drealB@test.test' &&
-            notification.message.name === 'drealB' &&
-            notification.variables.nom_projet === 'nomProjet' &&
-            notification.variables.type_demande === 'puissance' &&
-            notification.variables.modification_request_url ===
-              routes.DEMANDE_PAGE_DETAILS(modificationRequestId) &&
-            notification.variables.departement_projet === 'departement' &&
-            notification.context.modificationRequestId === modificationRequestId &&
-            notification.context.dreal === 'regionB' &&
-            notification.context.projectId === projectId,
-        ),
-      ).toBe(true);
-    });
+    expect(sendNotification).toHaveBeenCalledTimes(3);
+    const notifications = sendNotification.mock.calls.map((call) => call[0]);
+
+    expect(
+      notifications.some(
+        (notification) =>
+          notification.type === 'admin-modification-requested' &&
+          notification.message.email === 'drealA@test.test' &&
+          notification.message.name === 'drealA' &&
+          notification.variables.nom_projet === 'nomProjet' &&
+          notification.variables.type_demande === 'puissance' &&
+          notification.variables.modification_request_url ===
+            routes.DEMANDE_PAGE_DETAILS(modificationRequestId) &&
+          notification.variables.departement_projet === 'departement' &&
+          notification.context.modificationRequestId === modificationRequestId &&
+          notification.context.dreal === 'regionA' &&
+          notification.context.projectId === projectId,
+      ),
+    ).toBe(true);
+    expect(
+      notifications.some(
+        (notification) =>
+          notification.type === 'admin-modification-requested' &&
+          notification.message.email === 'drealB@test.test' &&
+          notification.message.name === 'drealB' &&
+          notification.variables.nom_projet === 'nomProjet' &&
+          notification.variables.type_demande === 'puissance' &&
+          notification.variables.modification_request_url ===
+            routes.DEMANDE_PAGE_DETAILS(modificationRequestId) &&
+          notification.variables.departement_projet === 'departement' &&
+          notification.context.modificationRequestId === modificationRequestId &&
+          notification.context.dreal === 'regionB' &&
+          notification.context.projectId === projectId,
+      ),
+    ).toBe(true);
   });
 });
