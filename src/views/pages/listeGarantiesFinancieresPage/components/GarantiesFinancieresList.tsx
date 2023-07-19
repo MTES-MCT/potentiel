82108c3eb15e57b38e659649dc605fc1ba18a2e4
HubM <hubmoncenis@gmail.com>
2023-07-19 17:35:36 +0200

    🚧 WIP affichage des infos sur la liste des gfs

--- src/views/pages/listeGarantiesFinancieresPage/components/GarantiesFinancieresList.tsx
+++ src/views/pages/listeGarantiesFinancieresPage/components/GarantiesFinancieresList.tsx
@@ -7,29 +7,21 @@ import { PaginatedList } from '../../../../types';
 import { Badge, DownloadLink, Link, Tile, PaginationPanel } from '@components';
 import { afficherDate } from '@views/helpers';
 
-const StatutBadge = ({ project }: { project: ProjectListItem; role: UserRole }) => {
-  if (project.abandonedOn) {
-    return <Badge type="warning">Abandonné</Badge>;
-  }
-
-  if (project.classe === 'Eliminé') {
-    return <Badge type="error">Eliminé</Badge>;
-  }
-
-  const getFinancementType = (project: ProjectListItem) => {
-    if (project.isFinancementParticipatif) return 'FP';
-    if (project.isInvestissementParticipatif) return 'IP';
-    if (project.actionnariat === 'financement-collectif') return 'FC';
-    if (project.actionnariat === 'gouvernance-partagee') return 'GP';
-
+const getStatutBadge = (statut?: 'en attente' | 'à traiter' | 'validé') => {
+  if (!statut) {
     return null;
-  };
+  }
 
-  return (
-    <Badge type="success">
-      Classé {getFinancementType(project) && `(${getFinancementType(project)})`}
-    </Badge>
-  );
+  switch (statut) {
+    case 'en attente':
+      return <Badge type="warning">En attente</Badge>;
+    case 'à traiter':
+      return <Badge type="error">À traiter</Badge>;
+    case 'validé':
+      return <Badge type="success">Validé</Badge>;
+    default:
+      return null;
+  }
 };
 
 type Props = {
@@ -46,39 +38,54 @@ export const GarantiesFinancieresList = ({ projects, role, GFPastDue }: Props) =
         {projects.items.map(({ id, potentielIdentifier, nomProjet, garantiesFinancières }) => (
           <li className="list-none p-0 m-0" key={id}>
             <Tile className="mb-4 flex md:relative flex-col" key={'project_' + id}>
-              <div className="flex flex-col gap-2 mb-4">
-                <div className="flex flex-col md:flex-row gap-2">
-                  <Link href={routes.PROJECT_DETAILS(id)}>{nomProjet}</Link>
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-col gap-2 mb-4">
+                  <div className="flex flex-col md:flex-row gap-2">
+                    <Link href={routes.PROJECT_DETAILS(id)}>{nomProjet}</Link>
+                  </div>
+                  <div className="italic text-xs text-grey-425-base">{potentielIdentifier}</div>
                 </div>
-                <div className="italic text-xs text-grey-425-base">{potentielIdentifier}</div>
+                {garantiesFinancières?.statut && (
+                  <div>{getStatutBadge(garantiesFinancières?.statut)}</div>
+                )}
               </div>
-              <div className="flex flex-row">
-                {garantiesFinancières?.type && <div>type : {garantiesFinancières?.type}</div>}
-                {garantiesFinancières?.dateConstitution && (
-                  <div>
-                    datede constitution :{' '}
-                    {afficherDate(new Date(garantiesFinancières?.dateConstitution))}
-                  </div>
-                )}
-                {garantiesFinancières?.dateEchéance && (
-                  <div>
-                    date d'écheance : {afficherDate(new Date(garantiesFinancières?.dateEchéance))}
-                  </div>
-                )}
-                {garantiesFinancières?.fichier && garantiesFinancières.dateEnvoi && (
-                  <DownloadLink
-                    className="flex text-sm items-center"
-                    fileUrl={routes.DOWNLOAD_PROJECT_FILE(
-                      garantiesFinancières.fichier.id,
-                      garantiesFinancières.fichier.filename,
+              <div className="flex flex-row justify-between">
+                {(garantiesFinancières?.type || garantiesFinancières?.dateEchéance) && (
+                  <div className="flex flex-col">
+                    {garantiesFinancières?.type && <div>type : {garantiesFinancières?.type}</div>}
+                    {garantiesFinancières?.dateEchéance && (
+                      <div>
+                        date d'écheance :{' '}
+                        {afficherDate(new Date(garantiesFinancières?.dateEchéance))}
+                      </div>
                     )}
-                    aria-label={`Télécharger les garanties financières du projet ${nomProjet} déposées le ${afficherDate(
-                      new Date(garantiesFinancières.dateEnvoi),
-                    )}`}
-                  >
-                    Déposées le <br />
-                    {afficherDate(new Date(garantiesFinancières.dateEnvoi))}
-                  </DownloadLink>
+                  </div>
+                )}
+                {(garantiesFinancières?.dateConstitution ||
+                  (garantiesFinancières?.fichier && garantiesFinancières.dateEnvoi)) && (
+                  <div className="flex flex-col">
+                    {garantiesFinancières?.dateConstitution && (
+                      <div>
+                        date de constitution :{' '}
+                        {afficherDate(new Date(garantiesFinancières?.dateConstitution))}
+                      </div>
+                    )}
+                    {garantiesFinancières?.fichier && garantiesFinancières.dateEnvoi && (
+                      <DownloadLink
+                        className="flex text-sm items-center"
+                        fileUrl={routes.DOWNLOAD_PROJECT_FILE(
+                          garantiesFinancières.fichier.id,
+                          garantiesFinancières.fichier.filename,
+                        )}
+                        aria-label={`Télécharger les garanties financières du projet ${nomProjet} déposées le ${afficherDate(
+                          new Date(garantiesFinancières.dateEnvoi),
+                        )}`}
+                      >
+                        Déposées le <br />
+                        {afficherDate(new Date(garantiesFinancières.dateEnvoi))}
+                      </DownloadLink>
+                    )}
+                  </div>
                 )}
               </div>
             </Tile>
