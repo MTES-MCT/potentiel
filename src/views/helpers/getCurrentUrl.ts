0e7e5329383bfde6404b09aeef33f2d12fd7aa2d
Julien Pavon <julien.pavon@gmail.com>
2023-07-19 10:16:59 +0200

    🔥 Supression duplication fonction

--- src/views/helpers/getCurrentUrl.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from 'express';
-
-export const getCurrentUrl = (request: Request) => {
-  return new URL(`${request.protocol}://${request.get('host')}${request.originalUrl}`);
-};
