0079ac901dce2e2e6262d14668543a34709f37c3
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-07-19 17:22:22 +0200

    ♻️ Convertion des delimiteurs # en | pour la gestion de la category

--- packages/libraries/pg-event-sourcing/sql/event-store.sql
+++ packages/libraries/pg-event-sourcing/sql/event-store.sql
@@ -60,7 +60,7 @@ create function insert_into_event_store()
     returns trigger as
 $trigger$
     begin
-        insert into event_store.event_stream (stream_id, created_at, type, version, payload) values (new."streamId", new."createdAt", new."type", new."version", new."payload");
+        insert into event_store.event_stream (stream_id, created_at, type, version, payload) values (split_part(new."streamId", '#', 1) || '|' || split_part(new."streamId", split_part(new."streamId", '#', 1) || '#', 2), new."createdAt", new."type", new."version", new."payload");
         return new;
     end;
 $trigger$
@@ -77,48 +77,51 @@ create schema system_projections;
 
 create view system_projections.stream_info as
     select
-        split_part(event_stream.stream_id, '#', 1) category,
-        split_part(event_stream.stream_id, split_part(event_stream.stream_id, '#', 1) || '#', 2) id,
+        split_part(event_stream.stream_id, '|', 1) category,
+        split_part(event_stream.stream_id, '|', 2) id,
         min(event_stream.created_at) created_at,
         max(event_stream.created_at) updated_at,
         count(event_stream.stream_id) event_count
     from event_store.event_stream
     group by
         category,
-        event_stream.stream_id
+        id
     order by updated_at desc;
 
 create view system_projections.stream_category as
-    select category, count(category) from system_projections.stream_info group by category order by category;
+    select category, count(id) from system_projections.stream_info group by category order by category;
 
 -- Rebuild
 create procedure event_store.rebuild(
   p_category varchar,
-  p_stream_id varchar default null
+  p_id varchar default null
 )
 as
 $$
   begin
-    if p_stream_id is not null then
+    if p_id is not null then
         insert into event_store.event_stream
         values (
-            p_category || '#' || p_stream_id,
+            p_category || '|' || p_id,
             left(trim(both '"' from to_json(now())::text), 23) || 'Z',
             'RebuildTriggered',
             (
-                select coalesce(max(version), 0) + 1
+                select coalesce(version, 0) + 1
                 from event_store.event_stream
-                where stream_id = p_category || '#' || p_stream_id limit 1
+                where stream_id = p_category || '|' || p_id limit 1
             ),
             '{}'
         );
     else
       insert into event_store.event_stream
-      select p_category || '#' || si.id, left(trim(both '"' from to_json(now())::text), 23) || 'Z', 'RebuildTriggered', coalesce(max(es.version), 0) + 1, '{}'
+      select p_category || '|' || si.id, left(trim(both '"' from to_json(now())::text), 23) || 'Z', 'RebuildTriggered', coalesce(max(es.version), 0) + 1, '{}'
       from system_projections.stream_info si
-      left join event_store.event_stream es on (p_category || '#' || si.id) = es.stream_id
+      left join event_store.event_stream es on (p_category || '|' || si.id) = es.stream_id
       where si.category = p_category
       group by si.id;
     end if;
   end
 $$ language plpgsql;
+
+-- insert into event_store.event_stream(stream_id, created_at, type, version, payload)
+-- select split_part("streamId", '#', 1) || '|' || split_part("streamId", split_part("streamId", '#', 1) || '#', 2), "createdAt", "type", "version", "payload" from "EVENT_STREAM";
