699a419606082f59e985e5ba58ca54f40364f69f
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-07-20 17:43:50 +0200

    ✨ Acknowledgement

--- packages/libraries/pg-event-sourcing/sql/event-store.sql
+++ packages/libraries/pg-event-sourcing/sql/event-store.sql
@@ -6,65 +6,98 @@ create table event_store.event_stream (
   created_at varchar not null,
   type varchar not null,
   version integer not null,
-  payload jsonb,
+  payload jsonb not null,
   primary key (stream_id, created_at, version)
 );
 
 create index on event_store.event_stream (stream_id);
 
-create function event_store.notify_new_event() returns trigger as
-$trigger$
-  begin
-    perform pg_notify('new_event', row_to_json(new)::text);
-    return new;
-  end;
-$trigger$
-  language plpgsql volatile
-  cost 100;
+create table event_store.subscriber (
+  subscriber_id varchar not null primary key,
+  filter jsonb default null
+);
 
-create trigger notify_new_event
-after insert
-on event_store.event_stream
+create table event_store.pending_acknowledgement (
+  subscriber_id varchar not null,
+  stream_id varchar not null,
+  created_at varchar not null,
+  version integer not null,
+  primary key (subscriber_id, stream_id, created_at, version)
+);
+
+create or replace function event_store.notify_subscribers()
+returns trigger as
+$$
+declare
+  v_type varchar;
+  v_subscriber event_store.subscriber;
+  v_error_msg jsonb;
+begin
+  v_type := new.type;
+
+  for v_subscriber in
+    select subscriber_id
+    from event_store.subscriber
+    where filter is null or v_type in (select jsonb_array_elements_text(filter))
+  loop
+    begin
+      insert into event_store.pending_acknowledgement
+      values (v_subscriber.subscriber_id, new.stream_id, new.created_at, new.version);
+
+      perform pg_notify(v_subscriber.subscriber_id, row_to_json(new)::text);
+    exception
+      when others then
+        v_error_msg := json_build_object('error_message', sqlerrm);
+
+        perform pg_notify('error_notifications', v_error_msg::text);
+    end;
+  end loop;
+
+  return new;
+end;
+$$ language plpgsql;
+
+-- créer le déclencheur sur la table event_store.event_stream
+create trigger notify_subscribers_trigger
+after insert on event_store.event_stream
 for each row
-execute procedure event_store.notify_new_event();
+execute function event_store.notify_subscribers();
+
 
 create function event_store.throw_when_trying_to_update_event() returns void as
 $$
   begin
     raise exception 'Event are immutable and can not be updated';
   end;
-$$ language plpgsql volatile;
+$$ language plpgsql;
 
 create function event_store.throw_when_trying_to_delete_event() returns void as
 $$
   begin
     raise exception 'Event are immutable and can not be deleted';
   end;
-$$ language plpgsql volatile;
+$$ language plpgsql;
 
 create rule prevent_delete_on_event_stream as on delete to event_store.event_stream do instead select event_store.throw_when_trying_to_delete_event();
 create rule prevent_update_on_event_stream as on update to event_store.event_stream do instead select event_store.throw_when_trying_to_update_event();
 
 -- Replication
-CREATE OR REPLACE FUNCTION notify_new_event() RETURNS TRIGGER AS
-$trigger$
-  BEGIN
-      PERFORM pg_notify('new_event', row_to_json(NEW)::text);
-      RETURN NEW; -- modification du trigger pour retourner new
-  END;
-$trigger$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+create or replace function notify_new_event() returns trigger as
+$$
+  begin
+      perform pg_notify('new_event', row_to_json(NEW)::text);
+      return new;
+  end;
+$$ language plpgsql;
 
 create function insert_into_event_store()
     returns trigger as
-$trigger$
+$$
     begin
         insert into event_store.event_stream (stream_id, created_at, type, version, payload) values (split_part(new."streamId", '#', 1) || '|' || split_part(new."streamId", split_part(new."streamId", '#', 1) || '#', 2), new."createdAt", new."type", new."version", new."payload");
         return new;
     end;
-$trigger$
-language plpgsql volatile;
+$$ language plpgsql;
 
 create trigger insert_into_event_store_trigger
 after insert
@@ -108,7 +141,9 @@ $$
             (
                 select coalesce(version, 0) + 1
                 from event_store.event_stream
-                where stream_id = p_category || '|' || p_id limit 1
+                where stream_id = p_category || '|' || p_id
+                order by version desc
+                limit 1
             ),
             '{}'
         );
