02b560251363abbae21bda6f458bf64c7a2ca13b
Hubert Moncenis <hubmoncenis@gmail.com>
2023-07-21 17:26:22 +0200

    🐛 Fixer le layout de la page projet (#1211)
    🐛 remove min-w-fit class

--- src/views/components/UI/molecules/Section.tsx
+++ src/views/components/UI/molecules/Section.tsx
@@ -9,7 +9,7 @@ type SectionProps = ComponentProps<'div'> & {
 
 export const Section = ({ title, children, icon, className = '', ...props }: SectionProps) => (
   <section
-    className={`m-0 p-4 flex-1 xs:w-full min-w-fit border border-solid border-grey-900-base rounded-[3px] ${className}`}
+    className={`m-0 p-4 flex-1 border border-solid border-grey-900-base rounded-[3px] ${className}`}
     {...props}
   >
     <Heading2 className="flex items-center text-2xl border-solid border-x-0 border-t-0 border-b-[1px] border-b-grey-900-base">
