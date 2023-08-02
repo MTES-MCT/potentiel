57e00fc7d2858e873fd31e4143e1a94be325ea2f
Hanaë Yamashita <hanae.yamashita@lilo.org>
2023-08-02 10:50:44 +0200

    ✅ Scenario test migration des GF legacy

--- packages/domain/src/domain.events.ts
+++ packages/domain/src/domain.events.ts
@@ -1,7 +1,18 @@
 import { GestionnaireRéseauEvent } from './gestionnaireRéseau/gestionnaireRéseau.event';
+import {
+  TypeGarantiesFinancièresEnregistréEventV0,
+  AttestationGarantiesFinancièresEnregistréeEvent,
+} from './projet/garantiesFinancières/garantiesFinancières.event';
 import { ProjetEvent, GarantiesFinancièresEvent } from './projet/projet.event';
 import { RaccordementEvent } from './raccordement/raccordement.event';
 
 export type DomainEvents = GestionnaireRéseauEvent | RaccordementEvent | ProjetEvent;
 
-export { GestionnaireRéseauEvent, ProjetEvent, RaccordementEvent, GarantiesFinancièresEvent };
+export {
+  GestionnaireRéseauEvent,
+  ProjetEvent,
+  RaccordementEvent,
+  GarantiesFinancièresEvent,
+  TypeGarantiesFinancièresEnregistréEventV0,
+  AttestationGarantiesFinancièresEnregistréeEvent,
+};
--- packages/specifications/src/projet/garantiesFinancières/stepDefinitions/projet.garantiesFinancières.when.ts
+++ packages/specifications/src/projet/garantiesFinancières/stepDefinitions/projet.garantiesFinancières.when.ts
@@ -1,15 +1,22 @@
 import { When as Quand, DataTable } from '@cucumber/cucumber';
 import { PotentielWorld } from '../../../potentiel.world';
 import {
+  AttestationGarantiesFinancièresEnregistréeEvent,
   DomainUseCase,
   TypeGarantiesFinancières,
+  TypeGarantiesFinancièresEnregistréEventV0,
   Utilisateur,
   convertirEnDateTime,
   convertirEnIdentifiantProjet,
+  createProjetAggregateId,
 } from '@potentiel/domain';
 import { mediator } from 'mediateur';
 import { sleep } from '../../../helpers/sleep';
 import { convertStringToReadable } from '../../../helpers/convertStringToReadable';
+import { upload } from '@potentiel/file-storage';
+import { publish } from '@potentiel/pg-event-sourcing';
+import { extension } from 'mime-types';
+import { join } from 'path';
 
 Quand(
   `un utilisateur avec le rôle {string} transmet des garanties financières pour le projet {string}`,
@@ -44,6 +51,65 @@ Quand(
           identifiantProjet: convertirEnIdentifiantProjet(identifiantProjet),
         },
       });
+      await sleep(500);
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `un utilisateur avec le rôle {string} migre des garanties financières pour le projet {string}`,
+  async function (this: PotentielWorld, rôle: string, nomProjet: string, table: DataTable) {
+    const exemple = table.rowsHash();
+
+    try {
+      const typeGarantiesFinancières = exemple['type'] as TypeGarantiesFinancières;
+      const dateÉchéance = exemple[`date d'échéance`];
+      const format = exemple['format'];
+      const dateConstutition = exemple[`date de constitution`];
+      const contenuFichier = convertStringToReadable(exemple['contenu fichier']);
+
+      const { identifiantProjet } = this.projetWorld.rechercherProjetFixture(nomProjet);
+
+      // TYPE
+      const legacyTypeEvent: TypeGarantiesFinancièresEnregistréEventV0 = {
+        type: 'TypeGarantiesFinancièresEnregistré-v0',
+        payload: {
+          identifiantProjet: convertirEnIdentifiantProjet(identifiantProjet).formatter(),
+          dateÉchéance: convertirEnDateTime(dateÉchéance).formatter(),
+          typeGarantiesFinancières,
+        },
+      };
+      await publish(
+        createProjetAggregateId(convertirEnIdentifiantProjet(identifiantProjet)),
+        legacyTypeEvent,
+      );
+      await sleep(100);
+
+      // ATTESTATION
+      const attestationEvent: AttestationGarantiesFinancièresEnregistréeEvent = {
+        type: 'AttestationGarantiesFinancièresEnregistrée',
+        payload: {
+          identifiantProjet: convertirEnIdentifiantProjet(identifiantProjet).formatter(),
+          format: format,
+          date: convertirEnDateTime(dateConstutition).formatter(),
+        },
+      };
+
+      // FICHIER
+      const path = join(
+        convertirEnIdentifiantProjet(identifiantProjet).formatter(),
+        `attestation-constitution-garanties-Financieres.${extension(format)}`,
+      );
+
+      await upload(path, contenuFichier);
+
+      await publish(
+        createProjetAggregateId(convertirEnIdentifiantProjet(identifiantProjet)),
+        attestationEvent,
+      );
+
       await sleep(500);
     } catch (error) {
       this.error = error as Error;
