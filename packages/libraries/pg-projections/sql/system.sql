de7fc06fc479e7a5133238b8afa0a4a8071a2a00
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-08-03 16:42:07 +0200

    ✨ Ajout des informations de base dans la view stream_info

--- packages/libraries/pg-projections/sql/system.sql
+++ packages/libraries/pg-projections/sql/system.sql
@@ -3,12 +3,16 @@ create schema system_views;
 
 create view system_views.stream_info as
     select
-        split_part(event_stream.stream_id, '|', 1) category,
-        split_part(event_stream.stream_id, '|', 2) id,
-        min(event_stream.created_at) created_at,
-        max(event_stream.created_at) updated_at,
-        count(event_stream.stream_id) event_count
-    from event_store.event_stream
+        split_part(es.stream_id, '|', 1) category,
+        split_part(es.stream_id, '|', 2) id,
+        min(es.created_at) created_at,
+        max(es.created_at) updated_at,
+        count(es.stream_id) event_count,
+        count(pa.*) pending_acknowledgement_count,
+        case
+            when coalesce(max(pa.version), max(es.version)) < max(es.version) then 'inconsistent_after_next_retry' else 'ok' end as status
+    from event_store.event_stream as es
+    left outer join event_store.pending_acknowledgement as pa on pa.stream_id = es.stream_id and pa.created_at = es.created_at and pa.version = es.version
     group by
         category,
         id
