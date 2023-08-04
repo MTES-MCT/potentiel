1d11ca41dee90dba976e62267d52c597938b6f49
Julien Pavon <julien.pavon@gmail.com>
2023-08-04 10:37:26 +0200

    ✨ Interface utilisateur pour enregistrer une garantie financière d'un projet (#1235)
    * 💄 Ajout de la page pour enregistrer une GF
    
    * 💄 Ajustement de l'état disabled
    
    * 🚧 Branchement page - use case
    
    * 🚧 Ne pas pouvoir enregistrer fichier sans date
    
    * 🚨 Correction type après rebase
    
    * ✨ Ajout de la route pour télécharger l'attestation
    
    * 🚨 Correction typage après rebase
    
    * ✨ Ne pas enregistrer des GF si AO non soumis
    
    * ✨ Gestion des champs obligatoires/optionnels
    
    * 💄 Ajustement des informations annexes du formulaire
    
    * 💄 Changement typos + vérification si soumis avec l'AO du projet
    
    * 🐛 Utilisation de isSoumisAuxGF

---
+++ src/controllers/garantiesFinancieres/getEnregistrerGarantiesFinancièresPage.ts
@@ -0,0 +1,128 @@
+import routes from '@routes';
+import { v1Router } from '../v1Router';
+import * as yup from 'yup';
+import safeAsyncHandler from '../helpers/safeAsyncHandler';
+import { notFoundResponse, unauthorizedResponse, vérifierPermissionUtilisateur } from '../helpers';
+import { ConsulterGarantiesFinancièresQuery, ConsulterProjetQuery } from '@potentiel/domain-views';
+import { EnregistrerGarantiesFinancièresPage } from '@views';
+import { mediator } from 'mediateur';
+import {
+  convertirEnIdentifiantProjet,
+  estUnRawIdentifiantProjet,
+  PermissionConsulterGarantiesFinancières,
+} from '@potentiel/domain';
+import { isNone, isSome } from '@potentiel/monads';
+import { Project, UserProjects } from '@infra/sequelize/projectionsNext';
+import { addQueryParams } from '../../helpers/addQueryParams';
+import { getProjectAppelOffre } from '@config';
+
+const schema = yup.object({
+  params: yup.object({
+    identifiantProjet: yup.string().required(),
+  }),
+});
+
+v1Router.get(
+  routes.GET_ENREGISTRER_GARANTIES_FINANCIERES_PAGE(),
+  vérifierPermissionUtilisateur(PermissionConsulterGarantiesFinancières),
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({ request, response }) =>
+        notFoundResponse({ request, response, ressourceTitle: 'Projet' }),
+    },
+    async (request, response) => {
+      const {
+        user,
+        params: { identifiantProjet },
+        query: { error },
+      } = request;
+
+      if (!estUnRawIdentifiantProjet(identifiantProjet)) {
+        return notFoundResponse({ request, response, ressourceTitle: 'Projet' });
+      }
+
+      const identifiantProjetValueType = convertirEnIdentifiantProjet(identifiantProjet);
+
+      const projet = await mediator.send<ConsulterProjetQuery>({
+        type: 'CONSULTER_PROJET',
+        data: {
+          identifiantProjet: identifiantProjetValueType,
+        },
+      });
+
+      if (isNone(projet)) {
+        return notFoundResponse({
+          request,
+          response,
+          ressourceTitle: 'Projet',
+        });
+      }
+
+      const projetWithId = await Project.findOne({
+        where: {
+          appelOffreId: identifiantProjetValueType.appelOffre,
+          periodeId: identifiantProjetValueType.période,
+          familleId: isSome(identifiantProjetValueType.famille)
+            ? identifiantProjetValueType.famille
+            : '',
+          numeroCRE: identifiantProjetValueType.numéroCRE,
+        },
+        attributes: ['id', 'appelOffreId', 'periodeId', 'familleId'],
+      });
+
+      if (!projetWithId) {
+        return notFoundResponse({
+          request,
+          response,
+          ressourceTitle: 'Projet',
+        });
+      }
+
+      const appelOffre = getProjectAppelOffre({
+        appelOffreId: projetWithId.appelOffreId,
+        periodeId: projetWithId.periodeId,
+        familleId: projetWithId.familleId,
+      });
+      if (appelOffre && !appelOffre.isSoumisAuxGF) {
+        response.redirect(
+          addQueryParams(routes.PROJECT_DETAILS(identifiantProjet), {
+            error: `L'appel d'offres de votre projet n'est pas soumis aux garanties financières.`,
+          }),
+        );
+      }
+
+      if (user.role === 'porteur-projet') {
+        const porteurAAccèsAuProjet = !!(await UserProjects.findOne({
+          where: { projectId: projetWithId.id, userId: user.id },
+        }));
+
+        if (!porteurAAccèsAuProjet) {
+          return unauthorizedResponse({
+            request,
+            response,
+            customMessage: `Vous n'avez pas accès à ce projet.`,
+          });
+        }
+      }
+
+      const garantiesFinancières = await mediator.send<ConsulterGarantiesFinancièresQuery>({
+        type: 'CONSULTER_GARANTIES_FINANCIÈRES',
+        data: {
+          identifiantProjet: identifiantProjetValueType,
+        },
+      });
+
+      console.log(garantiesFinancières);
+
+      return response.send(
+        EnregistrerGarantiesFinancièresPage({
+          user,
+          projet,
+          garantiesFinancières: isNone(garantiesFinancières) ? undefined : garantiesFinancières,
+          error: error as string,
+        }),
+      );
+    },
+  ),
+);
