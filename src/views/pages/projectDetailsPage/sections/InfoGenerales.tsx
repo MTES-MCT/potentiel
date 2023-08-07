a349822e3b5280d56e4fcf171333e53a634c919f
HanaeY <72154904+HanaeY@users.noreply.github.com>
2023-08-07 18:41:53 +0200

    ✨ Afficher les GF sur la page d'un projet (#1237)
    * 🚧 Récupérer les GF pour la page projet
    
    * ✨ Composant AlertMessage
    
    * ✨ Afficher les GF dans les info générales du projet
    
    * 🔥 Suppression des GF legacy de la frise
    
    * 💄 Disposition des sections sur la page projet
    
    * 🐛 Correction timeline stories
    
    * 💄 Affichage info générales projet

--- src/views/pages/projectDetailsPage/sections/InfoGenerales.tsx
+++ src/views/pages/projectDetailsPage/sections/InfoGenerales.tsx
@@ -1,41 +1,66 @@
 import React from 'react';
 import { ProjectDataForProjectPage } from '../../../../modules/project';
-import { BuildingIcon, Heading3, Link, Section } from '../../../components';
+import {
+  AlertMessage,
+  BuildingIcon,
+  DownloadLink,
+  Heading3,
+  Link,
+  Section,
+} from '../../../components';
 import routes from '../../../../routes';
 import { UserRole } from '../../../../modules/users';
 import { convertirEnIdentifiantProjet } from '@potentiel/domain';
+import { afficherDate } from '../../../helpers';
+
+export type GarantiesFinancièresDataForProjetPage = {
+  actionRequise?: 'compléter enregistrement' | 'enregistrer' | 'déposer';
+  typeGarantiesFinancières?: "avec date d'échéance" | 'consignation' | '6 mois après achèvement';
+  dateÉchéance?: string;
+  attestationConstitution?: { format: string; date: string };
+};
 
 type InfoGeneralesProps = {
   project: ProjectDataForProjectPage;
   role: UserRole;
+  garantiesFinancières?: GarantiesFinancièresDataForProjetPage;
 };
 
-export const InfoGenerales = ({ project, role }: InfoGeneralesProps) => (
-  <Section title="Informations générales" icon={<BuildingIcon />} className="flex gap-4 flex-col">
-    <div>
-      <Heading3 className="m-0">Performances</Heading3>
-      <p className="m-0">
-        Puissance installée: {project.puissance} {project.appelOffre?.unitePuissance}
+export const InfoGenerales = ({ project, role, garantiesFinancières }: InfoGeneralesProps) => (
+  <Section title="Informations générales" icon={<BuildingIcon />} className="flex flex-col">
+    <div className="mb-6">
+      <Heading3 className="m-0 flex text-sm font-semibold tracking-wide uppercase">
+        puissance
+      </Heading3>
+      <p className="my-0">
+        Puissance installée : {project.puissance} {project.appelOffre?.unitePuissance}
       </p>
     </div>
-    <div>
-      <Heading3 className="mb-0">Site de production</Heading3>
-      <p className="m-0">{project.adresseProjet}</p>
-      <p className="m-0">
-        {project.codePostalProjet} {project.communeProjet}
-      </p>
-      <p className="m-0">
-        {project.departementProjet}, {project.regionProjet}
-      </p>
+    <div className="mb-6">
+      <Heading3 className="m-0 flex text-sm font-semibold tracking-wide uppercase">
+        site de production{' '}
+      </Heading3>
+      <div>
+        <span className="m-0">{project.adresseProjet}</span>
+        <br />
+        <span className="m-0">
+          {project.codePostalProjet} {project.communeProjet}
+        </span>
+        <br />
+        <span className="m-0">
+          {project.departementProjet}, {project.regionProjet}
+        </span>
+      </div>
     </div>
-
     {project.isClasse &&
     !project.isAbandoned &&
     ['admin', 'dgec-validateur', 'porteur-projet', 'dreal', 'acheteur-obligé', 'cre'].includes(
       role,
     ) ? (
-      <div className="mb-3">
-        <Heading3 className="mb-0">Raccordement au réseau</Heading3>
+      <div className="mb-6">
+        <Heading3 className="m-0 flex text-sm font-semibold tracking-wide uppercase">
+          raccordement au réseau
+        </Heading3>
         <Link
           href={routes.GET_LISTE_DOSSIERS_RACCORDEMENT(
             convertirEnIdentifiantProjet({
@@ -51,22 +76,72 @@ export const InfoGenerales = ({ project, role }: InfoGeneralesProps) => (
       </div>
     ) : null}
 
-    {project.appelOffre.isSoumisAuxGF && (
-      <div className="mb-3">
-        <Heading3 className="mb-0">Garanties financières</Heading3>
-        <Link
-          href={routes.GET_ENREGISTRER_GARANTIES_FINANCIERES_PAGE(
-            convertirEnIdentifiantProjet({
-              appelOffre: project.appelOffreId,
-              période: project.periodeId,
-              famille: project.familleId,
-              numéroCRE: project.numeroCRE,
-            }).formatter(),
-          )}
-        >
-          Mettre à jour ou consulter les garanties financières
-        </Link>
-      </div>
-    )}
+    {project.appelOffre.isSoumisAuxGF &&
+      ['porteur-projet', 'admin', 'dgec-validateur', 'dreal', 'caisse-des-dépôts', 'cre'].includes(
+        role,
+      ) && (
+        <div className="mb-6">
+          <Heading3 className="m-0 flex text-sm font-semibold tracking-wide uppercase">
+            garanties financières
+          </Heading3>
+          <div>
+            {garantiesFinancières?.actionRequise && (
+              <AlertMessage
+                message={`garanties financières à ${
+                  garantiesFinancières?.actionRequise === 'compléter enregistrement'
+                    ? 'compléter'
+                    : garantiesFinancières?.actionRequise.valueOf()
+                }`}
+              />
+            )}
+            <div className="flex flex-col gap-2">
+              {garantiesFinancières?.typeGarantiesFinancières === "avec date d'échéance" &&
+                garantiesFinancières?.dateÉchéance && (
+                  <div>
+                    Garanties Financières avec date d'échéance au{' '}
+                    {afficherDate(new Date(garantiesFinancières.dateÉchéance))}
+                  </div>
+                )}
+              {garantiesFinancières?.typeGarantiesFinancières === '6 mois après achèvement' && (
+                <div>Garanties Financières valides jusqu'à six mois après l'achèvement</div>
+              )}
+              {garantiesFinancières?.typeGarantiesFinancières === 'consignation' && (
+                <div>Garanties financières de type consignation</div>
+              )}
+
+              {garantiesFinancières?.attestationConstitution && (
+                <DownloadLink
+                  fileUrl={routes.GET_ATTESTATION_CONSTITUTION_GARANTIES_FINANCIERES(
+                    convertirEnIdentifiantProjet({
+                      appelOffre: project.appelOffreId,
+                      période: project.periodeId,
+                      famille: project.familleId,
+                      numéroCRE: project.numeroCRE,
+                    }).formatter(),
+                  )}
+                >
+                  Télécharger l'attestation (constituée le{' '}
+                  {afficherDate(new Date(garantiesFinancières.attestationConstitution.date))})
+                </DownloadLink>
+              )}
+
+              {garantiesFinancières?.actionRequise !== 'déposer' && (
+                <Link
+                  href={routes.GET_ENREGISTRER_GARANTIES_FINANCIERES_PAGE(
+                    convertirEnIdentifiantProjet({
+                      appelOffre: project.appelOffreId,
+                      période: project.periodeId,
+                      famille: project.familleId,
+                      numéroCRE: project.numeroCRE,
+                    }).formatter(),
+                  )}
+                >
+                  Mettre à jour les garanties financières
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
   </Section>
 );
