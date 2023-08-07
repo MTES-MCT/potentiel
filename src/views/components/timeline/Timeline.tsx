5a05a440b7b16fa1fd5553137d7d2b88e10e6937
HanaeY <72154904+HanaeY@users.noreply.github.com>
2023-08-07 18:41:53 +0200

    ✨ Afficher les GF sur la page d'un projet (#1237)
    * 🚧 Récupérer les GF pour la page projet
    
    * ✨ Composant AlertMessage
    
    * ✨ Afficher les GF dans les info générales du projet
    
    * 🔥 Suppression des GF legacy de la frise
    
    * 💄 Disposition des sections sur la page projet
    
    * 🐛 Correction timeline stories
    
    * 💄 Affichage info générales projet

--- src/views/components/timeline/Timeline.tsx
+++ src/views/components/timeline/Timeline.tsx
@@ -8,13 +8,11 @@ import {
   DemandeDélaiDTO,
   DemandeAbandonDTO,
   CahierDesChargesChoisiDTO,
-  GarantiesFinancièresDTO,
   DemandeAnnulationAbandonDTO,
 } from '../../../modules/frise';
 import {
   TimelineItem,
   DesignationItem,
-  GFItem,
   ImportItem,
   ACItem,
   CAItem,
@@ -68,21 +66,17 @@ type ItemProps =
   | DemandeDélaiDTO
   | DemandeAbandonDTO
   | DemandeAnnulationAbandonDTO
-  | CahierDesChargesChoisiDTO
-  | GarantiesFinancièresDTO;
+  | CahierDesChargesChoisiDTO;
 
 export const Timeline = ({
   projectEventList: {
     events,
-    project: { id: projectId, status, garantieFinanciereEnMois, nomProjet },
+    project: { id: projectId, status, nomProjet },
   },
 }: TimelineProps) => {
-  const garantiesFinancières = events.find(is('garanties-financières'));
-
   const itemProps: ItemProps[] = [
     extractDesignationItemProps(events, projectId, status),
     extractImportItemProps(events),
-    garantiesFinancières?.date !== 0 ? garantiesFinancières : undefined,
     extractACItemProps(events, { status }),
     ...extractModificationRequestsItemProps(events),
     ...events.filter(is('DemandeDelaiSignaled')),
@@ -100,7 +94,6 @@ export const Timeline = ({
     .sort((a, b) => a.date - b.date);
 
   insertAfter(itemProps, 'attestation-de-conformite', extractCAItemProps(events, { status }));
-  garantiesFinancières?.date === 0 && insertAfter(itemProps, 'designation', garantiesFinancières);
 
   const timelineItems = itemProps.map((props) => {
     const { type } = props;
@@ -111,16 +104,6 @@ export const Timeline = ({
 
       case 'import':
         return <ImportItem {...props} />;
-
-      case 'garanties-financières':
-        return (
-          <GFItem
-            {...{
-              project: { id: projectId, status, garantieFinanciereEnMois, nomProjet },
-              ...props,
-            }}
-          />
-        );
 
       case 'attestation-de-conformite':
         return <ACItem {...props} />;
