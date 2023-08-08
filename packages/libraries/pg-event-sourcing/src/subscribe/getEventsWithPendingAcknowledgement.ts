678bfb7147b0f651da0be802ec7c178fedb119ee
Julien Pavon <julien.pavon@gmail.com>
2023-08-08 07:54:47 +0200

    ♻️ Indentation de la requête pour récupérer les pending acknowledgement

--- packages/libraries/pg-event-sourcing/src/subscribe/getEventsWithPendingAcknowledgement.ts
+++ packages/libraries/pg-event-sourcing/src/subscribe/getEventsWithPendingAcknowledgement.ts
@@ -1,8 +1,13 @@
 import { executeSelect } from '@potentiel/pg-helpers';
 import { Event } from '../event';
 
-const selectEventsWithPendingAcknowledgement =
-  'select es.* from event_store.event_stream es inner join event_store.pending_acknowledgement pa on pa.stream_id = es.stream_id and pa.created_at = es.created_at and pa.version = es.version where pa.subscriber_id = $1';
+const selectEventsWithPendingAcknowledgement = `
+   select es.*   
+   from event_store.event_stream es 
+   inner join event_store.pending_acknowledgement pa on pa.stream_id = es.stream_id 
+                                                    and pa.created_at = es.created_at 
+                                                    and pa.version = es.version 
+   where pa.subscriber_id = $1`;
 
 export const getEventsWithPendingAcknowledgement = async (subscribeName: string) =>
   executeSelect<Event>(selectEventsWithPendingAcknowledgement, subscribeName);
