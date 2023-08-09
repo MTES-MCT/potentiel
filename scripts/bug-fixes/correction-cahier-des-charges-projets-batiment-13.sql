b66d83f70077e880739d28dab1fa63dd498531a3
Julien Pavon <julien.pavon@gmail.com>
2023-08-09 07:26:56 +0200

    🐛 Suppression des éléments dans la frise

--- scripts/bug-fixes/correction-cahier-des-charges-projets-batiment-13.sql
+++ scripts/bug-fixes/correction-cahier-des-charges-projets-batiment-13.sql
@@ -2,7 +2,7 @@
 INSERT INTO
       "eventStores" (
             "id",
-            "aggregateId"
+            "aggregateId",
             "type",
             "version",
             "payload",
@@ -45,51 +45,28 @@ WHERE
       );
 
 -- Correction de la frise 
-INSERT INTO
-      "project_events" (
-            "id",
-            "projectId",
-            "type",
-            "payload",
-            "valueDate",
-            "createdAt",
-            "updatedAt",
-            "eventPublishedAt"      
-      )
-VALUES
-SELECT
-      gen_random_uuid (),
-      "projects".id,
-      'CahierDesChargesChoisi',
-      jsonb_build_object(
-            'paruLe',
-            '30/07/2021',
-            'choisiPar',
-            'potentiel',
-            'type',
-            'initial'
-      ),
-      extract(epoch FROM current_timestamp) * 1000,
-      now(),
-      now(),
-      extract(epoch FROM current_timestamp) * 1000
-FROM
-      "projects"
-WHERE
-      "cahierDesChargesActuel" = '30/07/2021'
-      AND (
-            "appelOffreId" in (
-                  'PPE2 - Autoconsommation métropole',
-                  'PPE2 - Sol',
-                  'PPE2 - Bâtiment',
-                  'Eolien'
+DELETE FROM "project_events"
+WHERE "project_events".type = 'CahierDesChargesChoisi'
+AND   "project_events".payload->>'paruLe' = '30/07/2021'
+AND   "project_events"."projectId" IN (
+      SELECT "projects".id
+      FROM
+            "projects"
+      WHERE
+            "cahierDesChargesActuel" = '30/07/2021'
+            AND (
+                  "appelOffreId" in (
+                        'PPE2 - Autoconsommation métropole',
+                        'PPE2 - Sol',
+                        'PPE2 - Bâtiment',
+                        'Eolien'
+                  )
+                  OR (
+                        "appelOffreId" = 'CRE4 - Bâtiment'
+                        and "periodeId" = '13'
+                  )
             )
-            OR (
-                  "appelOffreId" = 'CRE4 - Bâtiment'
-                  and "periodeId" = '13'
-            )
-      );
-
+)
 
 -- Correction dans la projection "projects"
 UPDATE
