2216abdd1388096e1f12f47f69cf986d037e2596
Hubert Moncenis <hubmoncenis@gmail.com>
2023-08-22 10:04:23 +0200

    Update src/modules/modificationRequest/errors/PuissanceVariationError.ts
    Co-authored-by: HanaeY <72154904+HanaeY@users.noreply.github.com>

--- src/modules/modificationRequest/errors/PuissanceVariationError.ts
+++ src/modules/modificationRequest/errors/PuissanceVariationError.ts
@@ -3,7 +3,7 @@ import { DomainError } from '../../../core/domain';
 export class PuissanceVariationWithDecisionJusticeError extends DomainError {
   constructor() {
     super(
-      "Vous ne pouvez pas accepter une augmentation de puissance avec une variation de plus de 10% alors que vous avez indiqué que la demande fait suite à une décision de l'état.",
+      "Vous ne pouvez pas accepter une augmentation de puissance avec une variation de plus de 10% alors que vous avez indiqué que la demande fait suite à une décision de l'État.",
     );
   }
 }
