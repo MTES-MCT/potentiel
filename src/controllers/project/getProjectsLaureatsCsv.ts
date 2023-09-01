0a6ab6bec8015306c192ff0452546547fc5e3267
HubM <hubmoncenis@gmail.com>
2023-09-01 12:03:22 +0200

    🐛 fix de l'export de la liste des candidats à notifier + ajout dans le CSV du statut du projet (classé / éliminé)

--- src/controllers/project/getProjectsLaureatsCsv.ts
+++ src/controllers/project/getProjectsLaureatsCsv.ts
@@ -6,11 +6,12 @@ import { ensureRole, getDonnéesPourPageNotificationCandidats } from '../../conf
 import asyncHandler from '../helpers/asyncHandler';
 import { formatField, writeCsvOnDisk } from '../../helpers/csv';
 import { promises as fsPromises } from 'fs';
-import { Project } from '../../entities';
 import { getPagination } from '../helpers';
+import { ProjectListItem } from '../../modules/project';
 
 const getProjectsLaureatsCsv = asyncHandler(async (request, response) => {
-  const { appelOffreId, periodeId, recherche, beforeNotification } = request.query as any;
+  const { appelOffreId, periodeId, recherche, beforeNotification, classement } =
+    request.query as any;
 
   if (!appelOffreId || !periodeId) {
     return response
@@ -25,6 +26,7 @@ const getProjectsLaureatsCsv = asyncHandler(async (request, response) => {
   const projetsCandidats = [
     { dataField: 'nomCandidat' },
     { dataField: 'nomProjet' },
+    { dataField: 'classe' },
     { dataField: 'puissance' },
     { dataField: 'familleId' },
     { dataField: 'departementProjet' },
@@ -32,26 +34,39 @@ const getProjectsLaureatsCsv = asyncHandler(async (request, response) => {
   ];
 
   try {
+    const résultat =
+      beforeNotification === 'true'
+        ? await getDonnéesPourPageNotificationCandidats({
+            appelOffreId,
+            periodeId,
+            pagination,
+            recherche,
+            classement,
+          })
+        : null;
+
+    if (!résultat) {
+      return response.send('Aucun projet lauréat sur cette période');
+    }
+
     const {
       projetsPériodeSélectionnée: { items: projects },
-    }: any =
-      beforeNotification === 'true' &&
-      (await getDonnéesPourPageNotificationCandidats({
-        appelOffreId,
-        periodeId,
-        pagination,
-        recherche,
-        classement: 'classés',
-      }));
+    } = résultat;
 
-    if (!projects?.length) return response.send('Aucun projet lauréat sur cette période');
+    if (projects.length === 0) {
+      return response.send('Aucun projet lauréat sur cette période');
+    }
 
     const sortedProjects = _sortProjectsByRegionsAndDepartements(projects);
 
     const fields = projetsCandidats.map((field) => formatField(field));
-    const title = `Liste des lauréats de la ${projects[0].appelOffre.periode.title} période d'appel d'offres portant sur ${projects[0].appelOffre.title}`;
+    const title = `Liste des lauréats de la ${
+      projects[0].appelOffre && projects[0].appelOffre.periode.title
+    } période d'appel d'offres portant sur ${
+      projects[0].appelOffre && projects[0].appelOffre.title
+    }`;
     fields.unshift({ label: 'Titre', value: () => title });
-
+    console.log(sortedProjects, fields);
     const csv = await parseAsync(sortedProjects, { fields, delimiter: ';' });
     const csvFilePath = await writeCsvOnDisk(csv, '/tmp');
 
@@ -70,7 +85,7 @@ const getProjectsLaureatsCsv = asyncHandler(async (request, response) => {
   }
 });
 
-function _sortProjectsByRegionsAndDepartements(projects: Project[]) {
+function _sortProjectsByRegionsAndDepartements(projects: ProjectListItem[]) {
   return projects.sort((p1, p2): number => {
     if (p1.regionProjet > p2.regionProjet) return 1;
     if (p1.regionProjet < p2.regionProjet) return -1;
