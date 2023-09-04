e1a7ef725457d4f26df9e3ac33b658398cbf3a89
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-09-04 08:48:45 +0200

    ✨ Ajout DLQ

--- packages/libraries/pg-event-sourcing/sql/event-store.sql
+++ packages/libraries/pg-event-sourcing/sql/event-store.sql
@@ -28,6 +28,7 @@ create table event_store.pending_acknowledgement (
   primary key (subscriber_id, stream_id, created_at, version)
 );
 
+
 create or replace function event_store.notify_subscribers()
 returns trigger as
 $$
@@ -38,23 +39,34 @@ declare
 begin
   v_type := new.type;
 
-  for v_subscriber in
-    select subscriber_id
-    from event_store.subscriber
-    where filter is null or v_type in (select jsonb_array_elements_text(filter))
-  loop
-    begin
-      insert into event_store.pending_acknowledgement
-      values (v_subscriber.subscriber_id, new.stream_id, new.created_at, new.version);
+  select into v_subscriber
+    "subscriber_id"
+  from event_store."subscriber"
+  where "filter" is not null and v_type in (select jsonb_array_elements_text(filter))
+  limit 1;
 
-      perform pg_notify(v_subscriber.subscriber_id, row_to_json(new)::text);
-    exception
-      when others then
-        v_error_msg := json_build_object('error_message', sqlerrm);
+  if v_subscriber is null then
+    insert into event_store.pending_acknowledgement
+    values ('dead-letter-queue', new.stream_id, new.created_at, new.version);
+  else
+    for v_subscriber in
+      select subscriber_id
+      from event_store.subscriber
+      where filter is null or v_type in (select jsonb_array_elements_text(filter))
+    loop
+      begin
+        insert into event_store.pending_acknowledgement
+        values (v_subscriber.subscriber_id, new.stream_id, new.created_at, new.version);
 
-        perform pg_notify('error_notifications', v_error_msg::text);
-    end;
-  end loop;
+        perform pg_notify(v_subscriber.subscriber_id, row_to_json(new)::text);
+      exception
+        when others then
+          v_error_msg := json_build_object('error_message', sqlerrm);
+
+          perform pg_notify('error_notifications', v_error_msg::text);
+      end;
+    end loop;
+  end if;
   return new;
 end;
 $$ language plpgsql;
--- src/infra/sequelize/migrations/20230904084200-update-event-store.ts
+++ src/infra/sequelize/migrations/20230904084200-update-event-store.ts
@@ -2,7 +2,7 @@ import { QueryInterface } from 'sequelize';
 
 export default {
   up: async (queryInterface: QueryInterface) => {
-    await queryInterface.sequelize.query(`
+    await queryInterface.sequelize.query(`      
       create or replace function event_store.notify_subscribers()
       returns trigger as
       $$
@@ -13,23 +13,34 @@ export default {
       begin
         v_type := new.type;
 
-        for v_subscriber in
-          select subscriber_id
-          from event_store.subscriber
-          where filter is null or v_type in (select jsonb_array_elements_text(filter))
-        loop
-          begin
-            insert into event_store.pending_acknowledgement
-            values (v_subscriber.subscriber_id, new.stream_id, new.created_at, new.version);
+        select into v_subscriber
+          "subscriber_id"
+        from event_store."subscriber"
+        where "filter" is not null and v_type in (select jsonb_array_elements_text(filter))
+        limit 1;
 
-            perform pg_notify(v_subscriber.subscriber_id, row_to_json(new)::text);
-          exception
-            when others then
-              v_error_msg := json_build_object('error_message', sqlerrm);
+        if v_subscriber is null then
+          insert into event_store.pending_acknowledgement
+          values ('dead-letter-queue', new.stream_id, new.created_at, new.version);
+        else
+          for v_subscriber in
+            select subscriber_id
+            from event_store.subscriber
+            where filter is null or v_type in (select jsonb_array_elements_text(filter))
+          loop
+            begin
+              insert into event_store.pending_acknowledgement
+              values (v_subscriber.subscriber_id, new.stream_id, new.created_at, new.version);
 
-              perform pg_notify('error_notifications', v_error_msg::text);
-          end;
-        end loop;
+              perform pg_notify(v_subscriber.subscriber_id, row_to_json(new)::text);
+            exception
+              when others then
+                v_error_msg := json_build_object('error_message', sqlerrm);
+
+                perform pg_notify('error_notifications', v_error_msg::text);
+            end;
+          end loop;
+        end if;
         return new;
       end;
       $$ language plpgsql;
