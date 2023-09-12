5528b1813fd076ed8cb537742a19fb3d82f7f009
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-09-12 14:47:17 +0200

    ✅ Update test

--- packages/libraries/pg-event-sourcing/src/load/loadAggregate.integration.ts
+++ packages/libraries/pg-event-sourcing/src/load/loadAggregate.integration.ts
@@ -1,23 +1,21 @@
 import { beforeAll, beforeEach, describe, expect, it } from '@jest/globals';
-import { DomainEvent, AggregateFactory } from '@potentiel/core-domain';
+import { DomainEvent, AggregateFactory, Aggregate } from '@potentiel/core-domain';
 import { none } from '@potentiel/monads';
 import { executeQuery } from '@potentiel/pg-helpers';
 import { loadAggregate } from './loadAggregate';
+import { publish } from '../publish/publish';
 
 describe(`loadAggregate`, () => {
-  const aggregateId = 'aggregateCategory|aggregateId';
-
   beforeAll(() => {
     process.env.EVENT_STORE_CONNECTION_STRING = 'postgres://testuser@localhost:5433/potentiel_test';
   });
 
-  beforeEach(() =>
-    executeQuery(`delete from event_store.event_stream where stream_id = $1`, aggregateId),
-  );
+  beforeEach(() => executeQuery(`delete from event_store.event_stream`));
 
   it(`Lorsqu'on charge un agrégat sans évènement
       Alors aucun agrégat ne devrait pas être chargé`, async () => {
     // Arrange
+    const aggregateId = 'aggregateCategory|aggregateId';
 
     // Act
     const result = await loadAggregate(aggregateId, () => ({}));
@@ -29,59 +27,37 @@ describe(`loadAggregate`, () => {
   it(`Lorsqu'on charge un agrégat avec des évènements
       Alors l'agrégat devrait être chargé`, async () => {
     // Arrange
-    const createdAt = new Date().toISOString();
+    const aggregateId = 'aggregateCategory|aggregateId';
 
-    await executeQuery(
-      `insert 
-       into event_store.event_stream
-       values (
-          $1, 
-          $2, 
-          $3, 
-          $4,
-          $5
-          )`,
-      aggregateId,
-      createdAt,
-      'event-1',
-      1,
-      { propriété: 'première-propriété' },
-    );
+    type Event1 = DomainEvent<'event-1', { propriété: string }>;
+    type Event2 = DomainEvent<'event-2', { secondePropriété: string }>;
 
-    await executeQuery(
-      `insert 
-       into event_store.event_stream
-       values (
-          $1, 
-          $2, 
-          $3, 
-          $4,
-          $5
-          )`,
-      aggregateId,
-      createdAt,
-      'event-2',
-      15,
-      { secondePropriété: 'seconde-propriété' },
-    );
+    const event1: Event1 = {
+      type: 'event-1',
+      payload: {
+        propriété: 'première-propriété',
+      },
+    };
 
-    // Act
-    type FakeAggregateState = {
+    const event2: Event2 = {
+      type: 'event-2',
+      payload: {
+        secondePropriété: 'seconde-propriété',
+      },
+    };
+
+    await publish(aggregateId, event1, event2);
+
+    type AggregateState = {
       propriété?: string;
       secondePropriété?: string;
     };
 
-    type Event1 = DomainEvent<'event-1', { propriété: string }>;
-    type Event2 = DomainEvent<'event-2', { secondePropriété: string }>;
-    type FakeAggregateEvent = Event1 | Event2;
-
-    const aggregateStateFactory: AggregateFactory<FakeAggregateState, FakeAggregateEvent> = (
-      events,
-    ) =>
+    const aggregateStateFactory: AggregateFactory<AggregateState, Event1 | Event2> = (events) =>
       events.reduce((state, event) => {
         switch (event.type) {
           case 'event-1':
-            return { ...state, proriété: event.payload.propriété };
+            return { ...state, propriété: event.payload.propriété };
 
           case 'event-2':
             return { ...state, secondePropriété: event.payload.secondePropriété };
@@ -91,14 +67,17 @@ describe(`loadAggregate`, () => {
         }
       }, {});
 
+    // Act
     const actual = await loadAggregate(aggregateId, aggregateStateFactory);
 
     // Assert
-    expect(actual).toEqual({
+    const expected: Aggregate & AggregateState = {
       aggregateId,
-      version: 15,
-      proriété: 'première-propriété',
+      version: 2,
+      propriété: 'première-propriété',
       secondePropriété: 'seconde-propriété',
-    });
+    };
+
+    expect(actual).toEqual(expected);
   });
 });
