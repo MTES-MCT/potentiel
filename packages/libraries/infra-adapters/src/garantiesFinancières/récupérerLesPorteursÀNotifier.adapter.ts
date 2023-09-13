10d44fb3d9db8b11c83629c75284c817c5785566
HubM <hubmoncenis@gmail.com>
2023-09-13 12:15:29 +0200

    ♻️ faire une seule requete pour récupérer les infos des PP à notifier (adapter)

--- packages/libraries/infra-adapters/src/garantiesFinancières/récupérerLesPorteursÀNotifier.adapter.ts
+++ packages/libraries/infra-adapters/src/garantiesFinancières/récupérerLesPorteursÀNotifier.adapter.ts
@@ -1,31 +1,22 @@
 import { RécupérerLesPorteursÀNotifierPort } from '@potentiel/domain';
-import { ProjetReadModel } from '@potentiel/domain-views';
 import { isSome, none } from '@potentiel/monads';
 import { executeSelect } from '@potentiel/pg-helpers';
 
-const selectProjectQuery = `
+const selectEmailEtNomPorteursÀNotifierPourUnProjetQuery = `
   select json_build_object(
-    'legacyId', "id"
+    'name', u."fullName",
+    'email', u."email"
   ) as value
-  from "projects"
-  where "appelOffreId" = $1 and "periodeId" = $2 and "numeroCRE" = $3 and "familleId" = $4
-`;
-
-const selectUserIdsQuery = `
-  select json_build_object(
-    'userId', "userId"
-  ) as value
-  from "UserProjects"
-  where "projectId" = $1
-`;
-
-const selectUserEmailAndNameQuery = `
-  select json_build_object(
-    'name', "fullName",
-    'email', "email"
-  ) as value
-  from "users"
-  where "id" = $1
+  from "users" u
+  where u."id" in (
+    select up."userId"
+    from "UserProjects" up
+    where up."projectId" = (
+      select "id"
+      from "projects"
+      where "appelOffreId" = $1 and "periodeId" = $2 and "numeroCRE" = $3 and "familleId" = $4
+    )
+  )
 `;
 
 export const récupérerLesPorteursÀNotifierAdapter: RécupérerLesPorteursÀNotifierPort = async ({
@@ -33,43 +24,23 @@ export const récupérerLesPorteursÀNotifierAdapter: RécupérerLesPorteursÀNo
 }) => {
   const { appelOffre, période, famille, numéroCRE } = identifiantProjet;
 
-  const projet = await executeSelect<{
-    value: Pick<ProjetReadModel, 'legacyId' | 'nom'>;
-  }>(selectProjectQuery, appelOffre, période, numéroCRE, isSome(famille) ? famille : '');
-
-  if (!projet.length) {
-    return none;
-  }
-
-  const { legacyId } = projet[0].value;
-
-  const userIds = await executeSelect<{
+  const porteurs = await executeSelect<{
     value: {
-      userId: string;
+      name: string;
+      email: string;
     };
-  }>(selectUserIdsQuery, legacyId);
+  }>(
+    selectEmailEtNomPorteursÀNotifierPourUnProjetQuery,
+    appelOffre,
+    période,
+    numéroCRE,
+    isSome(famille) ? famille : '',
+  );
 
-  if (!userIds.length) {
-    return none;
-  }
-
-  const porteurs: { name: string; email: string }[] = [];
-
-  for (const {
-    value: { userId },
-  } of userIds) {
-    const userEmailAndName = await executeSelect<{
-      value: {
-        name: string;
-        email: string;
-      };
-    }>(selectUserEmailAndNameQuery, userId);
-    if (!userEmailAndName.length) {
-      // TODO : Quid du porteur non trouvé ? On arrete l'éxécution ou on continue ?
-      continue;
-    }
-    porteurs.push(userEmailAndName[0].value);
-  }
-
-  return porteurs;
+  return !porteurs.length
+    ? none
+    : porteurs.map(({ value: { name, email } }) => ({
+        name,
+        email,
+      }));
 };
