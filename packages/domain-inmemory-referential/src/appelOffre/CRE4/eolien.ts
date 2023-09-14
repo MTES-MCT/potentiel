4132590aa510609cbf05803073c5194d59c1ec0d
HubM <hubmoncenis@gmail.com>
2023-09-14 16:39:45 +0200

    🎨 CRE4 Eolien : adapter les règles

--- packages/domain-inmemory-referential/src/appelOffre/CRE4/eolien.ts
+++ packages/domain-inmemory-referential/src/appelOffre/CRE4/eolien.ts
@@ -1,4 +1,52 @@
-import { AppelOffre } from '@potentiel/domain-views';
+import { AppelOffre, CahierDesChargesModifié } from '@potentiel/domain-views';
+
+const CDCModifié30082022: CahierDesChargesModifié = {
+  type: 'modifié',
+  paruLe: '30/08/2022',
+  numéroGestionnaireRequis: true,
+  url: 'https://www.cre.fr/media/Fichiers/publications/appelsoffres/cre-4-eolien-telecharger-l-avis-modificatif-publie-le-30-aout-2022',
+  donnéesCourriersRéponse: {
+    texteChangementDePuissance: {
+      référenceParagraphe: `5.4.5`,
+      dispositions: `
+          Les modifications de la Puissance installée avant l’Achèvement sont autorisées, sous réserve que la Puissance de l’Installation modifiée soit comprise entre quatre-vingt-dix pourcents (90 %) et cent-dix pourcents (110 %) de la Puissance indiquée dans l’offre. Elles doivent faire l’objet d’une information au Préfet.
+    Pour  les  projets  dont  soit  l'achèvement,  soit  la  mise  en  service  est  antérieur  au  31  décembre  2024,  cette  augmentation  de  puissance  peut  être  portée  à  140%  de  la  Puissance  formulée  dans  l’offre,  à condition qu’elle soit permise par l’autorisation environnementale de l’Installation, y compris si celle-ci a été modifiée.
+    Les modifications de la Puissance installée hors de cette fourchette ou les modifications à la hausse de la Puissance installée après l’Achèvement ne sont pas autorisées.
+    Par dérogation, les modifications à la baisse de la Puissance installée qui seraient imposées soit par une décision de l’Etat dans le cadre de la procédure d’autorisation mentionnée au 3.3.3 pour la première période de candidature, ou par une décision de justice concernant l’autorisation mentionnée au 3.3.3 pour l’ensemble des périodes de candidature, sont acceptées. Elles doivent faire l’objet d’une information au Préfet.`,
+    },
+    texteDélaisDAchèvement: {
+      référenceParagraphe: '6.4',
+      dispositions: `
+          Le  Candidat  dont  l’offre  a  été  retenue  s’engage  à  transmettre  à  EDF  l’attestation  de  conformité mentionnée au 6.5 dans un délai de trente-six (36) mois à compter de la Date de désignation. En cas de dépassement de ce délai, la durée de contrat mentionnée au 7.1 est réduite de la durée de dépassement.
+Sous  réserve  que  la  demande  complète  de  raccordement  de  l’Installation  ait  été  déposée  dans  les conditions  prévues  par  le  6.1  et  sous  réserve  que  le  Producteur  ait  mis  en  œuvre  toutes  les démarches  dans  le  respect  des  exigences  du  gestionnaire  de  réseau  pour  que  les  travaux  de raccordement soient réalisés dans les délais, les délais de transmission de l’attestation mentionnés ci-dessus sont prolongés lorsque la  mise  en service de l’Installation est  retardée du fait des délais nécessaires  à  la  réalisation  des  travaux  de  raccordement.  Dans  ce  cas,  le  Producteur  transmet l’attestation de conformité dans un délai de 2 mois à compter de la fin des travaux de raccordement notifiée   par   tout   document   transmis   par   le   gestionnaire   du   réseau   compétent.   
+En   cas   de dépassement de ce délai, la durée de contrat mentionnée au 7.1  est amputée d’un raccourcissement égal à la durée de dépassement.
+
+          !!!!!OPTION : PERIODES 1 A 2 !!!!! Pour la première période de candidature uniquement, les délais de transmission de l’attestation de conformité sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
+Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus,  la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.
+Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
+Dans tous les cas, l’attribution des délais est soumis à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais, aucun délai ne pourra être accordé sans preuve de la prolongation de la garantie. Il appartient au Producteur d’informer EDF dans le cas d’obtention de délais, et ce dans les deux (2) semaines suivant la réception de la décision du ministre.
+
+          !!!!!OPTION : PERIODES 3 A 4 !!!!! Pour les première, troisième et quatrième périodes de candidature uniquement, les délais de transmission de l’attestation de conformité sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
+Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus, la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
+Dans tous les cas, l’attribution des délais supplémentaires est conditionnée à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais. Le producteur transmet à la DREAL de la région d’implantation (cf. coordonnées en Annexe 5) un document conforme au modèle de l’Annexe 2 attestant de la prolongation de la garantie, la charge de la preuve de l’envoi reposant sur le lauréat en cas de litige. Pour attribution des délais, le Producteur fait parvenir à EDF, ou au ministre chargé de l’énergie le cas échéant, la preuve de cette transmission.
+
+          !!!!!OPTION : PERIODES 5 A 8 !!!!! Excepté pour la seconde période, les délais de transmission de l’attestation mentionnés ci-dessus sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
+Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus, la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
+Dans tous les cas, l’attribution des délais supplémentaires est conditionnée à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais. Le producteur transmet à la DREAL de la région d’implantation (cf. coordonnées en Annexe 5) un document conforme au modèle de l’Annexe 2 attestant de la prolongation de la garantie, la charge de la preuve de l’envoi reposant sur le lauréat en cas de litige. Pour attribution des délais, le Producteur fait parvenir à EDF, ou au ministre chargé de l’énergie le cas échéant, la preuve de cette transmission.
+          `,
+    },
+  },
+  délaiApplicable: {
+    délaiEnMois: 18,
+    intervaleDateMiseEnService: { min: new Date('2022-06-01'), max: new Date('2024-09-30') },
+  },
+  seuilSupplémentaireChangementPuissance: {
+    ratios: {
+      min: 0.9,
+      max: 1.4,
+    },
+  },
+};
 
 const eolien: AppelOffre = {
   autoritéCompétenteDemandesDélai: 'dgec',
@@ -97,6 +145,7 @@ Dans tous les cas, l’attribution des délais est soumis à la prolongation de 
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '2',
@@ -120,6 +169,7 @@ Dans tous les cas, l’attribution des délais est soumis à la prolongation de 
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '3',
@@ -144,6 +194,7 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '4',
@@ -167,6 +218,7 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '5',
@@ -190,6 +242,7 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '6',
@@ -214,6 +267,7 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '7',
@@ -238,6 +292,7 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
 `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
     {
       id: '8',
@@ -262,52 +317,11 @@ Dans tous les cas, l’attribution des délais supplémentaires est conditionné
         `,
         },
       },
+      cahiersDesChargesModifiésDisponibles: [CDCModifié30082022],
     },
   ],
   familles: [],
-  cahiersDesChargesModifiésDisponibles: [
-    {
-      type: 'modifié',
-      paruLe: '30/08/2022',
-      numéroGestionnaireRequis: true,
-      url: 'https://www.cre.fr/media/Fichiers/publications/appelsoffres/cre-4-eolien-telecharger-l-avis-modificatif-publie-le-30-aout-2022',
-      donnéesCourriersRéponse: {
-        texteChangementDePuissance: {
-          référenceParagraphe: `5.4.5`,
-          dispositions: `
-          Les modifications de la Puissance installée avant l’Achèvement sont autorisées, sous réserve que la Puissance de l’Installation modifiée soit comprise entre quatre-vingt-dix pourcents (90 %) et cent-dix pourcents (110 %) de la Puissance indiquée dans l’offre. Elles doivent faire l’objet d’une information au Préfet.
-    Pour  les  projets  dont  soit  l'achèvement,  soit  la  mise  en  service  est  antérieur  au  31  décembre  2024,  cette  augmentation  de  puissance  peut  être  portée  à  140%  de  la  Puissance  formulée  dans  l’offre,  à condition qu’elle soit permise par l’autorisation environnementale de l’Installation, y compris si celle-ci a été modifiée.
-    Les modifications de la Puissance installée hors de cette fourchette ou les modifications à la hausse de la Puissance installée après l’Achèvement ne sont pas autorisées.
-    Par dérogation, les modifications à la baisse de la Puissance installée qui seraient imposées soit par une décision de l’Etat dans le cadre de la procédure d’autorisation mentionnée au 3.3.3 pour la première période de candidature, ou par une décision de justice concernant l’autorisation mentionnée au 3.3.3 pour l’ensemble des périodes de candidature, sont acceptées. Elles doivent faire l’objet d’une information au Préfet.`,
-        },
-        texteDélaisDAchèvement: {
-          référenceParagraphe: '6.4',
-          dispositions: `
-          Le  Candidat  dont  l’offre  a  été  retenue  s’engage  à  transmettre  à  EDF  l’attestation  de  conformité mentionnée au 6.5 dans un délai de trente-six (36) mois à compter de la Date de désignation. En cas de dépassement de ce délai, la durée de contrat mentionnée au 7.1 est réduite de la durée de dépassement.
-Sous  réserve  que  la  demande  complète  de  raccordement  de  l’Installation  ait  été  déposée  dans  les conditions  prévues  par  le  6.1  et  sous  réserve  que  le  Producteur  ait  mis  en  œuvre  toutes  les démarches  dans  le  respect  des  exigences  du  gestionnaire  de  réseau  pour  que  les  travaux  de raccordement soient réalisés dans les délais, les délais de transmission de l’attestation mentionnés ci-dessus sont prolongés lorsque la  mise  en service de l’Installation est  retardée du fait des délais nécessaires  à  la  réalisation  des  travaux  de  raccordement.  Dans  ce  cas,  le  Producteur  transmet l’attestation de conformité dans un délai de 2 mois à compter de la fin des travaux de raccordement notifiée   par   tout   document   transmis   par   le   gestionnaire   du   réseau   compétent.   
-En   cas   de dépassement de ce délai, la durée de contrat mentionnée au 7.1  est amputée d’un raccourcissement égal à la durée de dépassement.
-
-          !!!!!OPTION : PERIODES 1 A 2 !!!!! Pour la première période de candidature uniquement, les délais de transmission de l’attestation de conformité sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
-Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus,  la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.
-Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
-Dans tous les cas, l’attribution des délais est soumis à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais, aucun délai ne pourra être accordé sans preuve de la prolongation de la garantie. Il appartient au Producteur d’informer EDF dans le cas d’obtention de délais, et ce dans les deux (2) semaines suivant la réception de la décision du ministre.
-
-          !!!!!OPTION : PERIODES 3 A 4 !!!!! Pour les première, troisième et quatrième périodes de candidature uniquement, les délais de transmission de l’attestation de conformité sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
-Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus, la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
-Dans tous les cas, l’attribution des délais supplémentaires est conditionnée à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais. Le producteur transmet à la DREAL de la région d’implantation (cf. coordonnées en Annexe 5) un document conforme au modèle de l’Annexe 2 attestant de la prolongation de la garantie, la charge de la preuve de l’envoi reposant sur le lauréat en cas de litige. Pour attribution des délais, le Producteur fait parvenir à EDF, ou au ministre chargé de l’énergie le cas échéant, la preuve de cette transmission.
-
-          !!!!!OPTION : PERIODES 5 A 8 !!!!! Excepté pour la seconde période, les délais de transmission de l’attestation mentionnés ci-dessus sont également prolongés lorsque des recours contentieux dirigés contre les autorisations administratives nécessaires à la réalisation de l'Installation ont eu pour effet de retarder son achèvement. La durée prise en compte pour calculer la prorogation des délais débute à la date d'enregistrement de chaque première requête de première instance et s'achève à la date à laquelle la dernière décision juridictionnelle relative à cette requête est devenue définitive.
-Pour les installations dont la mise en service a lieu entre le 1er septembre 2022 et le 31 décembre 2024 inclus, la limite d’Achèvement est repoussée de dix-huit (18) mois supplémentaires.Des délais supplémentaires pour l’Achèvement ou, pour ce qui concerne l’échéance du 31 décembre 2024 mentionnée au présent 6.4 et au 7.1, pour la mise en service, laissés à l’appréciation du ministre chargé de l’énergie, peuvent être accordés en cas d’événement imprévisible à la Date de désignation et extérieur au Producteur, dûment justifié.
-Dans tous les cas, l’attribution des délais supplémentaires est conditionnée à la prolongation de la garantie financière mentionnée au 6.2 d’une durée équivalente à celle desdits délais. Le producteur transmet à la DREAL de la région d’implantation (cf. coordonnées en Annexe 5) un document conforme au modèle de l’Annexe 2 attestant de la prolongation de la garantie, la charge de la preuve de l’envoi reposant sur le lauréat en cas de litige. Pour attribution des délais, le Producteur fait parvenir à EDF, ou au ministre chargé de l’énergie le cas échéant, la preuve de cette transmission.
-          `,
-        },
-      },
-      délaiApplicable: {
-        délaiEnMois: 18,
-        intervaleDateMiseEnService: { min: new Date('2022-06-01'), max: new Date('2024-09-30') },
-      },
-    },
-  ],
+  cahiersDesChargesModifiésDisponibles: [],
 };
 
 export { eolien };
