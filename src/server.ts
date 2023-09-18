d846206e4bb8a5dee13f40ad1dc8167caca55719
Julien Pavon <julien.pavon@gmail.com>
2023-09-18 11:19:35 +0200

    🐛 Correction du bug ne permettant pas de telecharger les export metabase

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 import path from 'path';
 import morgan from 'morgan';
 import * as Sentry from '@sentry/node';
-import { isDevEnv, registerAuth } from './config';
+import { registerAuth } from './config';
 import { v1Router } from './controllers';
 import { logger } from './core/utils';
 import { bootstrap } from '@potentiel/web';
@@ -54,45 +54,43 @@ export async function makeServer(port: number, sessionSecret: string) {
     // Always first middleware
     app.use(Sentry.Handlers.requestHandler());
 
-    if (!isDevEnv) {
-      app.use(
-        helmet({
-          // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-          // Only send refererer for same origin and transport (HTTPS->HTTPS)
-          referrerPolicy: { policy: 'strict-origin' },
-          hsts: {
-            maxAge: 63072000,
-            includeSubDomains: false,
-            preload: true,
+    app.use(
+      helmet({
+        // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+        // Only send refererer for same origin and transport (HTTPS->HTTPS)
+        referrerPolicy: { policy: 'strict-origin' },
+        hsts: {
+          maxAge: 63072000,
+          includeSubDomains: false,
+          preload: true,
+        },
+        crossOriginEmbedderPolicy: false,
+        contentSecurityPolicy: {
+          useDefaults: false,
+          directives: {
+            'default-src': ["'self'", 'blob:', 'metabase.potentiel.beta.gouv.fr'],
+            'connect-src': [
+              "'self'",
+              "'unsafe-inline'",
+              'analytics.potentiel.beta.gouv.fr',
+              'client.crisp.chat',
+              'wss://client.relay.crisp.chat',
+            ],
+            'img-src': ["'self'", 'data:', 'client.crisp.chat', 'image.crisp.chat'],
+            'font-src': ["'self'", 'data:', 'client.crisp.chat'],
+            'style-src': ["'self'", 'data:', "'unsafe-inline'", 'client.crisp.chat'],
+            'script-src': [
+              "'unsafe-inline'",
+              "'self'",
+              'metabase.potentiel.beta.gouv.fr',
+              'analytics.potentiel.beta.gouv.fr',
+              'client.crisp.chat',
+            ],
+            'object-src': ["'none'"],
           },
-          crossOriginEmbedderPolicy: false,
-          contentSecurityPolicy: {
-            useDefaults: false,
-            directives: {
-              'default-src': ["'self'", 'metabase.potentiel.beta.gouv.fr'],
-              'connect-src': [
-                "'self'",
-                "'unsafe-inline'",
-                'analytics.potentiel.beta.gouv.fr',
-                'client.crisp.chat',
-                'wss://client.relay.crisp.chat',
-              ],
-              'img-src': ["'self'", 'data:', 'client.crisp.chat', 'image.crisp.chat'],
-              'font-src': ["'self'", 'data:', 'client.crisp.chat'],
-              'style-src': ["'self'", 'data:', "'unsafe-inline'", 'client.crisp.chat'],
-              'script-src': [
-                "'unsafe-inline'",
-                "'self'",
-                'metabase.potentiel.beta.gouv.fr',
-                'analytics.potentiel.beta.gouv.fr',
-                'client.crisp.chat',
-              ],
-              'object-src': ["'none'"],
-            },
-          },
-        }),
-      );
-    }
+        },
+      }),
+    );
 
     app.use(
       morgan('tiny', {
