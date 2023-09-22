646f490bfbbdf3c8b32835d8f55b3c2807539e6a
Hubert Moncenis <hubmoncenis@gmail.com>
2023-09-22 10:52:23 +0200

    Update src/modules/project/errors/PasDeChangementDeCDCPourLaPériodeDeCetAOError.ts
    Co-authored-by: HanaeY <72154904+HanaeY@users.noreply.github.com>

--- src/modules/project/errors/PasDeChangementDeCDCPourLaPériodeDeCetAOError.ts
+++ src/modules/project/errors/PasDeChangementDeCDCPourLaPériodeDeCetAOError.ts
@@ -1,6 +1,6 @@
 import { DomainError } from '../../../core/domain';
 
-export class PasDeChangementDeCDCPourLaPériodeDeCetAOError extends DomainError {
+export class PasDeCDCModifiéDisponiblePourLaPériodeError extends DomainError {
   constructor() {
     super(
       `Vous ne pouvez pas changer de cahier des charges pour cette période d'appel d'offres.`,
