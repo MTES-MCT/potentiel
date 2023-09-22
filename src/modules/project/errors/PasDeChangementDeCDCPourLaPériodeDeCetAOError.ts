c66ec939af28cd5e99c49987ab7a5a12ea0c3e7e
Julien Pavon <julien.pavon@gmail.com>
2023-09-22 11:27:19 +0200

    🚨 Renommage de l'erreur PasDeChangementDeCDCPourLaPériodeDeCetAOError

--- src/modules/project/errors/PasDeChangementDeCDCPourLaPériodeDeCetAOError.ts
+++ src/modules/project/errors/PasDeChangementDeCDCPourLaPériodeDeCetAOError.ts
@@ -1,9 +1,7 @@
 import { DomainError } from '../../../core/domain';
 
-export class PasDeCDCModifiéDisponiblePourLaPériodeError extends DomainError {
+export class PasDeChangementDeCDCPourLaPériodeDeCetAOError extends DomainError {
   constructor() {
-    super(
-      `Vous ne pouvez pas changer de cahier des charges pour cette période d'appel d'offres.`,
-    );
+    super(`Vous ne pouvez pas changer de cahier des charges pour cette période d'appel d'offres.`);
   }
 }
