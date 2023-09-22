b113b1975070e05176755c9cc8c5d9a3de965aa0
Hanaë <hanae.yamashita@lilo.org>
2023-09-22 15:22:08 +0200

    🔥 GF dues en cas d'annulation d'abandon accordée

--- src/modules/project/eventHandlers/onAnnulationAbandonAccordée.spec.ts
+++ src/modules/project/eventHandlers/onAnnulationAbandonAccordée.spec.ts
@@ -1,6 +1,5 @@
 import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import { UniqueEntityID } from '../../../core/domain';
-import { ProjectAppelOffre } from '../../../entities';
 import { AnnulationAbandonAccordée } from '../../demandeModification';
 import { InfraNotAvailableError } from '../../shared';
 import { okAsync } from 'neverthrow';
@@ -28,68 +27,15 @@ describe(`Handler de projet onAnnulationAbandonAccordée`, () => {
       fichierRéponseId: new UniqueEntityID().toString(),
     },
   });
-  it(`Etant donné un projet soumis à garanties financières, 
-        lorsqu'un événement AnnulationAbandonAccordée est émis pour ce projet, 
-        alors l'abandon du projet devrait être annulé, 
-        les anciennes garanties financières devraient être retirées du projet, 
-        et le projet devrait être en attente de nouvelles garanties financières`, async () => {
-    const getProjectAppelOffre = jest.fn(() => ({ isSoumisAuxGF: true } as ProjectAppelOffre));
-    const projectRepo = fakeTransactionalRepo(
-      makeFakeProject({ id: projetId, completionDueOn, dcrDueOn, notifiedOn, hasCurrentGf: true }),
-    );
-    const onAnnulationAbandonAccordée = makeOnAnnulationAbandonAccordée({
-      projectRepo,
-      publishToEventStore,
-      getProjectAppelOffre,
-    });
-
-    await onAnnulationAbandonAccordée(événemement);
-
-    expect(publishToEventStore).toHaveBeenCalledTimes(3);
-
-    expect(publishToEventStore).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: 'AbandonProjetAnnulé',
-        payload: {
-          projetId,
-          dateAchèvement: new Date(completionDueOn),
-          dateLimiteEnvoiDcr: dcrDueOn,
-        },
-      }),
-    );
-
-    expect(publishToEventStore).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: 'ProjectGFRemoved',
-        payload: {
-          projectId: projetId,
-        },
-      }),
-    );
-
-    expect(publishToEventStore).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: 'ProjectGFDueDateSet',
-        payload: {
-          projectId: projetId,
-          garantiesFinancieresDueOn: new Date('2020-03-01').getTime(),
-        },
-      }),
-    );
-  });
-
-  it(`Etant donné un projet non soumis à garanties financières,
-        lorsqu'un événement AnnulationAbandonAccordée est émis pour ce projet, 
-        alors l'abandon du projet devrait être annulé, 
-        et le projet ne devrait pas être en attente de nouvelles garanties financières`, async () => {
-    const getProjectAppelOffre = jest.fn(() => ({ isSoumisAuxGF: false } as ProjectAppelOffre));
+  it(`Etant donné un projet
+      Lorsqu'une demande d'annulation d'abandon est accordée
+      Alors l'abandon du projet devrait être annulé`, async () => {
     const projectRepo = fakeTransactionalRepo(
       makeFakeProject({ id: projetId, completionDueOn, dcrDueOn, notifiedOn }),
     );
     const onAnnulationAbandonAccordée = makeOnAnnulationAbandonAccordée({
       projectRepo,
       publishToEventStore,
-      getProjectAppelOffre,
     });
 
     await onAnnulationAbandonAccordée(événemement);
