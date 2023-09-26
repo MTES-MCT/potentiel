a538dce3e86136314f985835afca3b1b145e5744
HubM <hubmoncenis@gmail.com>
2023-09-26 11:51:19 +0200

    🎨 ajout de la pagination pour la liste des projets abandonnés avec recandidature

--- src/controllers/project/getListeProjetsAbandonnésAvecRecandidature.ts
+++ src/controllers/project/getListeProjetsAbandonnésAvecRecandidature.ts
@@ -2,7 +2,7 @@ import asyncHandler from '../helpers/asyncHandler';
 import routes from '../../routes';
 import { v1Router } from '../v1Router';
 import { ListeProjetsAbandonnésAvecRecandidaturePage } from '../../views';
-import { vérifierPermissionUtilisateur } from '../helpers';
+import { getCurrentUrl, getPagination, vérifierPermissionUtilisateur } from '../helpers';
 
 import {
   ListerProjetEnAttenteRecandidatureQuery,
@@ -14,14 +14,22 @@ v1Router.get(
   routes.LISTE_PROJETS_ABANDONNÉS_AVEC_RECANDIDATURE,
   vérifierPermissionUtilisateur(PermissionListerProjetsAbandonnésAvecRecandidature),
   asyncHandler(async (request, response) => {
+    const { page, pageSize: itemsPerPage } = getPagination(request);
+
     const projets = await mediator.send<ListerProjetEnAttenteRecandidatureQuery>({
       type: 'LISTER_PROJET_EN_ATTENTE_RECANDIDATURE_QUERY',
-      data: {},
+      data: {
+        pagination: { page, itemsPerPage },
+      },
     });
+
     response.send(
       ListeProjetsAbandonnésAvecRecandidaturePage({
         request,
-        projets,
+        projets: {
+          ...projets,
+          currentUrl: getCurrentUrl(request),
+        },
       }),
     );
   }),
