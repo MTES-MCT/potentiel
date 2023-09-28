db328ef3b48d64ed47f9e2480caa44c26fc00193
Julien Pavon <julien.pavon@gmail.com>
2023-09-28 17:17:57 +0200

    ✨ Ajout du tableau et ajustement de la liste

--- src/views/certificates/abandon/PassageConcernantAbandonDuCahierDesCharges.tsx
+++ src/views/certificates/abandon/PassageConcernantAbandonDuCahierDesCharges.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@react-pdf/renderer';
+import { Text, View } from '@react-pdf/renderer';
 import React from 'react';
 
 export const PassageConcernantAbandonDuCahierDesCharges = () => {
@@ -32,15 +32,41 @@ export const PassageConcernantAbandonDuCahierDesCharges = () => {
         n’entrainera pas un prélèvement de vos garanties financières ni une sanction pécuniaire au
         titre de l’article L. 311-15 du code de l’énergie, dès lors que vous respecterez votre
         engagement à candidater à une prochaine période d’un appel d’offres avant le 31/12/2024 et
-        que cette recandidature respectera les conditions suivantes : - Le dossier doit être complet
-        et respecter les conditions d’éligibilité du cahier des charges de la période concernée par
-        la recandidature ; - Le projet doit avoir le même lieu d’implantation que le projet
-        abandonné ; - Le projet doit être d’une puissance équivalente que le projet abandonné, à
-        plus ou moins 20% ; - Le tarif proposé ne doit pas être supérieur au tarif du dernier
-        lauréat retenu pour la période dont le projet était initialement lauréat (et le cas échéant
-        de la famille concernée) indexé jusqu'à septembre 2023. Une fois cette recandidature
-        respectant les conditions précédentes indiquée sur Potentiel, vos garanties financières
-        pourront alors être levées.
+        que cette recandidature respectera les conditions suivantes :
+      </Text>
+
+      <View style={{ flexDirection: 'column', width: 450, marginTop: 10 }}>
+        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+          <Text style={{ marginHorizontal: 16 }}>-</Text>
+          <Text>
+            Le dossier doit être complet et respecter les conditions d’éligibilité du cahier des
+            charges de la période concernée par la recandidature ;
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+          <Text style={{ marginHorizontal: 16 }}>-</Text>
+          <Text>Le projet doit avoir le même lieu d’implantation que le projet abandonné ;</Text>
+        </View>
+        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+          <Text style={{ marginHorizontal: 16 }}>-</Text>
+          <Text>
+            Le projet doit être d’une puissance équivalente que le projet abandonné, à plus ou moins
+            20% ;
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+          <Text style={{ marginHorizontal: 16 }}>-</Text>
+          <Text>
+            Le tarif proposé ne doit pas être supérieur au tarif du dernier lauréat retenu pour la
+            période dont le projet était initialement lauréat (et le cas échéant de la famille
+            concernée) indexé jusqu'à septembre 2023.
+          </Text>
+        </View>
+      </View>
+
+      <Text style={{ fontSize: 10, textAlign: 'justify', marginTop: 10 }}>
+        Une fois cette recandidature respectant les conditions précédentes indiquée sur Potentiel,
+        vos garanties financières pourront alors être levées.
       </Text>
     </>
   );
