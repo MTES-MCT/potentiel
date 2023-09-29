65dbad8b6880858bf12a665fa239298581c62965
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-09-29 16:40:25 +0200

    ✨ Confirmer abandon dans le nouveau socle

--- src/controllers/modificationRequest/abandon/postConfirmerAbandon.ts
+++ src/controllers/modificationRequest/abandon/postConfirmerAbandon.ts
@@ -1,64 +1,111 @@
 import * as yup from 'yup';
 
-import { confirmerDemandeAbandon, ensureRole } from '../../../config';
+import {
+  confirmerDemandeAbandon,
+  ensureRole,
+  getIdentifiantProjetByLegacyId,
+} from '../../../config';
 import { logger } from '../../../core/utils';
-import asyncHandler from '../../helpers/asyncHandler';
 import { addQueryParams } from '../../../helpers/addQueryParams';
-import { EntityNotFoundError, UnauthorizedError } from '../../../modules/shared';
+import { UnauthorizedError } from '../../../modules/shared';
 import routes from '../../../routes';
 import { v1Router } from '../../v1Router';
 import {
   errorResponse,
-  notFoundResponse,
   unauthorizedResponse,
-  validateRequestBody,
 } from '../../helpers';
-import { ConfirmerDemandeAbandonError } from '../../../modules/demandeModification/demandeAbandon/confirmerAbandon/ConfirmerDemandeAbandonError';
+import safeAsyncHandler from '../../helpers/safeAsyncHandler';
+import { mediator } from 'mediateur';
+import {
+  DomainUseCase,
+  convertirEnDateTime,
+  convertirEnIdentifiantProjet,
+} from '@potentiel/domain';
+import { none } from '@potentiel/monads';
+import { InvalidOperationError } from '@potentiel/core-domain';
 
-const requestBodySchema = yup.object({
-  demandeAbandonId: yup.string().uuid().required(),
+const schema = yup.object({
+  body: yup.object({
+    demandeAbandonId: yup.string().uuid().required(),
+    projectId: yup.string().uuid().required(),
+  }),
 });
 
 v1Router.post(
   routes.CONFIRMER_DEMANDE_ABANDON,
   ensureRole(['porteur-projet']),
-  asyncHandler(async (request, response) => {
-    validateRequestBody(request.body, requestBodySchema)
-      .asyncAndThen((body) => {
-        const { user } = request;
-        const { demandeAbandonId } = body;
+  safeAsyncHandler(
+    {
+      schema,
+      onError: ({ request, response, error }) =>
+        response.redirect(
+          addQueryParams(routes.GET_DEMANDER_ABANDON(request.body.projectId), {
+            ...error.errors,
+          }),
+        ),
+    },
+    async (request, response) => {
+      const { projectId, demandeAbandonId } = request.body;
 
-        return confirmerDemandeAbandon({
-          demandeAbandonId,
-          confirméPar: user,
-        }).map(() => demandeAbandonId);
-      })
-      .match(
-        (demandeAbandonId) => {
-          response.redirect(
+      const identifiantProjet = await getIdentifiantProjetByLegacyId(projectId);
+
+      try {
+        await mediator.send<DomainUseCase>({
+          type: 'CONFIRMER_ABANDON_USECASE',
+          data: {
+            identifiantProjet: convertirEnIdentifiantProjet({
+              appelOffre: identifiantProjet?.appelOffre || '',
+              famille: identifiantProjet?.famille || none,
+              numéroCRE: identifiantProjet?.numéroCRE || '',
+              période: identifiantProjet?.période || '',
+            }),
+            dateConfirmationAbandon: convertirEnDateTime(new Date()),
+          },
+        });
+      } catch (error) {
+        if (error instanceof InvalidOperationError) {
+          return response.redirect(
+            addQueryParams(routes.GET_DEMANDER_ABANDON(projectId), {
+              error: error.message,
+            }),
+          );
+        }
+
+        logger.error(error);
+        return errorResponse({
+          request,
+          response,
+          customMessage: `Il y a eu une erreur lors de la soumission de votre demande. Veuillez répéter l'opération ou contacter un administrateur.`,
+        });
+      }
+
+      return confirmerDemandeAbandon({
+        confirméPar: user,
+        demandeAbandonId,
+      }).match(
+        () => {
+          return response.redirect(
             routes.SUCCESS_OR_ERROR_PAGE({
-              success: 'Votre confirmation a bien été enregistrée.',
-              redirectUrl: routes.DEMANDE_PAGE_DETAILS(demandeAbandonId),
-              redirectTitle: 'Retourner à la demande',
+              success: `Votre demande d'abandon a bien été envoyée.`,
+              redirectUrl: routes.PROJECT_DETAILS(projectId),
+              redirectTitle: 'Retourner à la page projet',
             }),
           );
         },
-        (e) => {
-          if (e instanceof ConfirmerDemandeAbandonError) {
-            return response.redirect(
-              addQueryParams(routes.DEMANDE_PAGE_DETAILS(request.body.demandeAbandonId), {
-                error: e.message,
-              }),
-            );
-          } else if (e instanceof UnauthorizedError) {
+        (error) => {
+          if (error instanceof UnauthorizedError) {
             return unauthorizedResponse({ request, response });
-          } else if (e instanceof EntityNotFoundError) {
-            return notFoundResponse({ request, response, ressourceTitle: 'Demande' });
           }
 
-          logger.error(e);
-          return errorResponse({ request, response });
+          logger.error(error);
+          return errorResponse({
+            request,
+            response,
+            customMessage:
+              'Il y a eu une erreur lors de la soumission de votre demande. Merci de recommencer.',
+          });
         },
       );
-  }),
+    },
+  ),
 );
