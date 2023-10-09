2e5dabe760f8e035e65f4d019a8b4841b0f4fe2e
Hanaë <hanae.yamashita@lilo.org>
2023-10-09 14:09:13 +0200

    🚸 Afficher intervalle de dates pour maintenir le délai CDC 2022

--- src/views/pages/raccordement/miseEnService/TransmettreDateMiseEnServicePage.tsx
+++ src/views/pages/raccordement/miseEnService/TransmettreDateMiseEnServicePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { UtilisateurReadModel } from '../../../../modules/utilisateur/récupérer/UtilisateurReadModel';
 import {
@@ -11,6 +11,7 @@ import {
   PageProjetTemplate,
   Form,
   InfoBox,
+  AlertBox,
 } from '../../../components';
 import { afficherDate, formatDateForInput, hydrateOnClient } from '../../../helpers';
 import { CandidatureLegacyReadModel, DossierRaccordementReadModel } from '@potentiel/domain-views';
@@ -23,6 +24,7 @@ type TransmettreDateMiseEnServiceProps = {
   dossierRaccordement: DossierRaccordementReadModel;
   error?: string;
   délaiCDC2022Appliqué?: true;
+  intervalleDatesMeSDélaiCDC2022?: { min: Date; max: Date };
 };
 
 export const TransmettreDateMiseEnService = ({
@@ -31,8 +33,23 @@ export const TransmettreDateMiseEnService = ({
   error,
   projet,
   délaiCDC2022Appliqué,
+  intervalleDatesMeSDélaiCDC2022,
 }: TransmettreDateMiseEnServiceProps) => {
   const { identifiantProjet } = projet;
+
+  const [afficherAlerteAnnulationDélaiCDC2022, setAfficherAlerteAnnulationDélaiCDC2022] =
+    useState(false);
+
+  const handleInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!délaiCDC2022Appliqué || !intervalleDatesMeSDélaiCDC2022) {
+      return;
+    }
+    const dateMiseEnService = new Date(e.target.value).getTime();
+    setAfficherAlerteAnnulationDélaiCDC2022(
+      dateMiseEnService < new Date(intervalleDatesMeSDélaiCDC2022.min).getTime() ||
+        dateMiseEnService > new Date(intervalleDatesMeSDélaiCDC2022.max).getTime(),
+    );
+  };
 
   return (
     <PageProjetTemplate titre={<TitrePageRaccordement />} user={user} résuméProjet={projet}>
@@ -57,8 +74,21 @@ export const TransmettreDateMiseEnService = ({
               max={new Date().toISOString().split('T').shift()}
               min={formatDateForInput(projet.dateDésignation)}
               required
+              onChange={(e) => handleInputChanged(e)}
             />
           </div>
+
+          {afficherAlerteAnnulationDélaiCDC2022 && (
+            <AlertBox>
+              La date que vous renseignez ne permet plus au projet de bénéficier du délai relatif au
+              cahier des charges du 30/08/2022.{' '}
+              <span className="font-bold">
+                Si vous transmettez cette date, la date limite d'achèvement du projet sera impactée
+                et le porteur en sera informé par mail.
+              </span>
+            </AlertBox>
+          )}
+
           <div className="flex flex-col md:flex-row gap-4 md:mt-4">
             <PrimaryButton type="submit">Transmettre</PrimaryButton>
             <Link
@@ -71,18 +101,28 @@ export const TransmettreDateMiseEnService = ({
         </Form>
         <InfoBox className='flex md:w-1/3 md:mx-auto"'>
           <ul>
-            <li>
-              La date de mise en service est comprise dans l'intervalle entre la date de désignation
-              du projet ({afficherDate(new Date(projet.dateDésignation))}) et ce jour.
-            </li>
             {délaiCDC2022Appliqué && (
-              <li className="mt-4">
+              <li className="mb-4">
                 Ce projet a déjà bénéficié du délai supplémentaire relatif du cahier des charges
                 modification du 30/08/2022. Une modification de la date de mise en service peut
                 remettre en cause l'application de ce délai et entraîner une modification de la date
                 d'achèvement du projet.
+                {intervalleDatesMeSDélaiCDC2022 && (
+                  <>
+                    <br />
+                    <span>
+                      Pour conserver le délai appliqué, la date de mise en service doit être entre
+                      le {afficherDate(new Date(intervalleDatesMeSDélaiCDC2022.min))} et le{' '}
+                      {afficherDate(new Date(intervalleDatesMeSDélaiCDC2022.max))}.
+                    </span>
+                  </>
+                )}
               </li>
             )}
+            <li>
+              La date de mise en service est comprise dans l'intervalle entre la date de désignation
+              du projet ({afficherDate(new Date(projet.dateDésignation))}) et ce jour.
+            </li>
           </ul>
         </InfoBox>
       </div>
