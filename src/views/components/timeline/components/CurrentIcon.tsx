4ac69e84dbc778c84786cc7f0491dd41fbc47258
HubM <hubmoncenis@gmail.com>
2023-10-26 15:45:22 +0200

    🎨 derniers ajustements

--- src/views/components/timeline/components/CurrentIcon.tsx
+++ src/views/components/timeline/components/CurrentIcon.tsx
@@ -5,7 +5,7 @@ export const CurrentIcon = () => (
     <div className="hidden print:block text-xs mb-2 whitespace-nowrap">étape à valider</div>
     <span
       className={
-        'relative z-2 w-8 h-8 flex items-center justify-center bg-white print:bg-none border-2 border-solid rounded-full border-blue-700 group-hover:border-blue-900'
+        'relative z-2 w-8 h-8 flex items-center justify-center bg-white print:bg-transparent border-2 border-solid rounded-full border-blue-700 group-hover:border-blue-900'
       }
     >
       <span
