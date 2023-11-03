039fbe89781ce723930133724bf988fafc2e382f
Julien Pavon <julien.pavon@gmail.com>
2023-11-03 08:37:56 +0100

    ♻️ Remove useless type extensions

--- packages/applications/ssr/src/types/next-auth.d.ts
+++ packages/applications/ssr/src/types/next-auth.d.ts
@@ -1,17 +1,3 @@
-import { DefaultSession } from 'next-auth';
-
-declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session extends DefaultSession {
-    user?: {
-      /** The user's role. */
-      role?: string;
-    } & DefaultSession['user'];
-  }
-}
-
 declare module 'next-auth/jwt' {
   interface JWT extends JWT {
     accessToken?: string;
