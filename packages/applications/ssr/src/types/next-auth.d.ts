9de2caa056247ce973f9f8c5e08bf1d467981f3f
Julien Pavon <julien.pavon@gmail.com>
2023-11-03 09:14:39 +0100

    🚨 Fix type error with the JWT decode function

--- packages/applications/ssr/src/types/next-auth.d.ts
+++ packages/applications/ssr/src/types/next-auth.d.ts
@@ -1,5 +1,7 @@
+import NextAuthJwt from 'next-auth/jwt';
+
 declare module 'next-auth/jwt' {
-  interface JWT extends JWT {
+  interface JWT extends NextAuthJwt.JWT {
     accessToken?: string;
   }
 }
