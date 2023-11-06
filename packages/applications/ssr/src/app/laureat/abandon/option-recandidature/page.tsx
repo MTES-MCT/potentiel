f452515447819ddeb54ce134103deefaba04418d
Hanaë <hanae.yamashita@lilo.org>
2023-11-06 11:31:51 +0100

    ✨ (page): liste abandons pour recandidature

--- packages/applications/ssr/src/app/laureat/abandon/option-recandidature/page.tsx
+++ packages/applications/ssr/src/app/laureat/abandon/option-recandidature/page.tsx
@@ -1,61 +1,55 @@
 import { Heading1 } from '@/components/atoms/headings';
 import { Tile } from '@/components/organisms/Tile';
-import { Abandon } from '@potentiel-domain/laureat';
 import Badge from '@codegouvfr/react-dsfr/Badge';
 import { displayDate } from '@/utils/displayDate';
 import { KeyIcon } from '@/components/atoms/icons';
 
-export default async function ListeAbandonsPage() {
-  const liste: Abandon.ListerAbandonReadModel = {
-    currentPage: 1,
-    itemsPerPage: 10,
-    totalItems: 3,
-    items: [
-      {
-        nomProjet: 'Centrale solaire 1',
-        identifiantProjet: 'PPE2 - Sol#1#1#test1',
-        statut: 'accordé',
-        demandeDemandéLe: new Date('2023-09-01').toISOString(),
-        confirmationDemandéeLe: new Date('2023-10-01').toISOString(),
-        confirmationConfirméLe: new Date('2023-10-05').toISOString(),
-        accordAccordéLe: new Date('2023-09-10').toISOString(),
-      },
-      {
-        nomProjet: 'Champ éolien 123',
-        identifiantProjet: 'PPE2 - Eolien#2#1#test2',
-        statut: 'demandé',
-        demandeDemandéLe: new Date('2023-10-02').toISOString(),
-      },
-      {
-        nomProjet: 'Centrale PV ombrière',
-        identifiantProjet: 'PPE2 - Bâtiment#2#1#test3',
-        statut: 'rejeté',
-        demandeDemandéLe: new Date('2023-10-01').toISOString(),
-        rejetRejetéLe: new Date('2023-10-03').toISOString(),
-      },
-      {
-        nomProjet: 'Centrale PV Mairie',
-        identifiantProjet: 'PPE2 - Bâtiment#3#1#test4',
-        statut: 'confirmation demandée',
-        demandeDemandéLe: new Date('2023-10-01').toISOString(),
-        confirmationDemandéeLe: new Date('2023-10-08').toISOString(),
-      },
-      {
-        nomProjet: 'Centrale solaire 12',
-        identifiantProjet: 'PPE2 - Neutre#3#1#test5',
-        statut: 'demande confirmée',
-        demandeDemandéLe: new Date('2023-10-01').toISOString(),
-        confirmationDemandéeLe: new Date('2023-10-08').toISOString(),
-        confirmationConfirméLe: new Date('2023-10-15').toISOString(),
-      },
-    ],
-  };
+import { Abandon } from '@potentiel-domain/laureat';
+import { ConsulterCandidatureLegacyQuery } from '@potentiel/domain-views';
+import { isSome } from '@potentiel/monads';
+import { mediator } from 'mediateur';
+
+export const dynamic = 'force-dynamic';
+
+export default async function ListeAbandonsOptionRecandidaturePage() {
+  const abandons = await mediator.send<Abandon.ListerAbandonsQuery>({
+    type: 'LISTER_ABANDONS_QUERY',
+    data: {
+      recandidature: true,
+      pagination: { page: 1, itemsPerPage: 10 },
+    },
+  });
+
+  if (!abandons.items.length) {
+    return (
+      <>
+        <Heading1>Demandes d'abandon avec option recandidature</Heading1>
+        <div>Aucune demande à afficher</div>
+      </>
+    );
+  }
+
+  const liste = await Promise.all(
+    abandons.items.map(async (a) => {
+      const projet = await mediator.send<ConsulterCandidatureLegacyQuery>({
+        type: 'CONSULTER_CANDIDATURE_LEGACY_QUERY',
+        data: {
+          identifiantProjet: a.identifiantProjet,
+        },
+      });
+
+      return {
+        ...a,
+        projet: isSome(projet) ? projet : undefined,
+      };
+    }),
+  );
 
   return (
     <>
-      <Heading1>Demandes d'abandon</Heading1>
-      <ul>
-        {liste.items.map(
+      <Heading1>Demandes d'abandon avec option recandidature</Heading1>
+      <ul className="mt-10">
+        {liste.map(
           ({
             identifiantProjet,
             statut,
@@ -64,13 +58,13 @@ export default async function ListeAbandonsPage() {
             rejetRejetéLe,
             confirmationDemandéeLe,
             confirmationConfirméLe,
-            nomProjet,
+            projet,
           }) => (
             <li className="mb-6" key={`abandon-projet-${identifiantProjet}`}>
               <Tile className="flex flex-col md:flex-row md:justify-between">
                 <div>
                   <h2>
-                    Abandon du projet <span className="font-bold">{nomProjet}</span>
+                    Abandon du projet <span className="font-bold">{projet.nomProjet}</span>
                     <Badge
                       noIcon
                       severity={
@@ -111,7 +105,7 @@ export default async function ListeAbandonsPage() {
                   <a
                     href="http://"
                     className="self-end"
-                    aria-label={`voir le détail de la demande d'abandon en statut ${statut} pour le projet ${nomProjet}`}
+                    aria-label={`voir le détail de la demande d'abandon en statut ${statut} pour le projet ${projet.nomProjet}`}
                   >
                     voir le détail
                   </a>
--- src/views/components/UI/organisms/UserNavigation.tsx
+++ src/views/components/UI/organisms/UserNavigation.tsx
@@ -73,7 +73,7 @@ const MenuAdmin = (currentPage?: string) => (
         Toutes les demandes
       </DropdownMenu.DropdownItem>
       <DropdownMenu.DropdownItem
-        href="/laureat/abandon"
+        href="/laureat/abandon/option-recandidature"
         {...(currentPage === 'liste-abandons' && { isCurrent: true })}
       >
         Abandons avec recandidature
