cf9de21e70827bd7a99d42de3103d31fbe91e50e
Julien Pavon <julien.pavon@gmail.com>
2023-11-07 16:07:19 +0100

    ♻️ Fix type error introducing AbandonListItem type

--- packages/domain/lauréat/src/abandon/lister/listerAbandon.query.ts
+++ packages/domain/lauréat/src/abandon/lister/listerAbandon.query.ts
@@ -1,9 +1,32 @@
 import { Message, MessageHandler, mediator } from 'mediateur';
 import { AbandonProjection } from '../abandon.projection';
 import { List } from '@potentiel-libraries/projection';
+import { IdentifiantProjet } from '@potentiel-domain/common';
+
+type AbandonListItem = {
+  identifiantProjet: IdentifiantProjet.RawType;
+  nomProjet: string;
+  statut: string;
+  demandeRaison: string;
+  demandePièceJustificativeFormat?: string;
+  demandeRecandidature: boolean;
+  demandeDemandéPar: string;
+  demandeDemandéLe: string;
+  accordRéponseSignéeFormat?: string;
+  accordAccordéPar?: string;
+  accordAccordéLe?: string;
+  rejetRéponseSignéeFormat?: string;
+  rejetRejetéPar?: string;
+  rejetRejetéLe?: string;
+  confirmationDemandéePar?: string;
+  confirmationDemandéeLe?: string;
+  confirmationDemandéeRéponseSignéeFormat?: string;
+  confirmationConfirméLe?: string;
+  confirmationConfirméPar?: string;
+};
 
 export type ListerAbandonReadModel = {
-  items: ReadonlyArray<AbandonProjection>;
+  items: ReadonlyArray<AbandonListItem>;
   currentPage: number;
   itemsPerPage: number;
   totalItems: number;
@@ -27,7 +50,7 @@ export const registerListerAbandonQuery = ({ list }: ListerAbandonDependencies) 
     recandidature,
     pagination: { page, itemsPerPage },
   }) => {
-    return await list<AbandonProjection>({
+    const result = await list<AbandonProjection>({
       type: 'abandon',
       pagination: { page, itemsPerPage },
       where:
@@ -41,7 +64,21 @@ export const registerListerAbandonQuery = ({ list }: ListerAbandonDependencies) 
         ascending: false,
       },
     });
+
+    return {
+      ...result,
+      items: result.items.map((item) => mapToReadModel(item)),
+    };
   };
 
   mediator.register('LISTER_ABANDONS_QUERY', handler);
+};
+
+const mapToReadModel = (projection: AbandonProjection): AbandonListItem => {
+  return {
+    ...projection,
+    identifiantProjet: IdentifiantProjet.convertirEnValueType(
+      projection.identifiantProjet,
+    ).formatter(),
+  };
 };
