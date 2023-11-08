3376dc3f08017938976133e145cda009aa087178
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2023-11-08 11:32:05 +0100

    ♻️ Déclenchement du rebuild sans persistance de l'évènement (#1387)

--- packages/infrastructure/pg-event-sourcing/sql/event-store/functions/rebuild.sql
+++ packages/infrastructure/pg-event-sourcing/sql/event-store/functions/rebuild.sql
@@ -4,29 +4,55 @@ create or replace procedure event_store.rebuild(
 )
 as
 $$
-  begin
+declare
+  v_row event_store.event_stream%ROWTYPE;
+  v_chanel varchar;
+  v_streamId varchar;
+begin
+  for v_chanel in
+    select stream_category || '|' || subscriber_name
+    from event_store.subscriber
+    where stream_category = p_category and (filter is null or 'RebuildTriggered' in (select jsonb_array_elements_text(filter)))
+  loop
     if p_id is not null then
-        insert into event_store.event_stream
-        values (
-            p_category || '|' || p_id,
-            left(trim(both '"' from to_json(now())::text), 23) || 'Z',
-            'RebuildTriggered',
-            (
-                select coalesce(version, 0) + 1
-                from event_store.event_stream
-                where stream_id = p_category || '|' || p_id
-                order by version desc
-                limit 1
-            ),
-            json_build_object('category', p_category, 'id', p_id)
-        );
+      select into v_row
+        stream_id
+      from event_store.event_stream
+      where stream_id = p_category || '|' || p_id;
+
+      if found then
+        perform pg_notify(v_chanel, json_build_object(
+          'version', 1,
+          'created_at', now()::text,
+          'stream_id', v_row.stream_id,
+          'type', 'RebuildTriggered',
+          'payload', json_build_object(
+            'category', p_category,
+            'id', p_id
+          )
+        )::text);
+      end if;
     else
-      insert into event_store.event_stream
-      select p_category || '|' || si.id, left(trim(both '"' from to_json(now())::text), 23) || 'Z', 'RebuildTriggered', coalesce(max(es.version), 0) + 1, json_build_object('category', p_category, 'id', si.id)
-      from system_views.stream_info si
-      left join event_store.event_stream es on (p_category || '|' || si.id) = es.stream_id
-      where si.category = p_category
-      group by si.id;
+      for v_streamId in
+        select stream_id
+        from event_store.event_stream
+        where stream_id like p_category || '|%'
+      loop
+        perform pg_notify(v_chanel, json_build_object(
+          'version', 1,
+          'created_at', now()::text,
+          'stream_id', v_streamId,
+          'type', 'RebuildTriggered',
+          'payload', json_build_object(
+            'category', p_category,
+            'id', split_part(v_streamId, '|', 2)
+          )
+        )::text);
+      end loop;
     end if;
-  end
+  end loop;
+  exception
+  when others then
+    perform pg_notify('error_notifications', json_build_object('error_message', sqlerrm)::text);
+end;
 $$ language plpgsql;
