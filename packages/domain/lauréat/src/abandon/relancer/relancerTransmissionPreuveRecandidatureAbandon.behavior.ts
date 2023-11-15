bb71d2e9944647c42cb671d721ea70fc5ceae300
HubM <hubmoncenis@gmail.com>
2023-11-15 15:40:58 +0100

    ♻️ ajout d'une classe pour l'erreur

--- packages/domain/lauréat/src/abandon/relancer/relancerTransmissionPreuveRecandidatureAbandon.behavior.ts
+++ packages/domain/lauréat/src/abandon/relancer/relancerTransmissionPreuveRecandidatureAbandon.behavior.ts
@@ -1,7 +1,14 @@
 import { DateTime, IdentifiantProjet } from '@potentiel-domain/common';
 import { AbandonAggregate } from '../abandon.aggregate';
+import { InvalidOperationError } from '@potentiel-domain/core';
 
 const dateLégaleRelance = DateTime.convertirEnValueType(new Date('2025-03-31'));
+
+class RelanceTransmissionPreuveRecandidatureImpossibleError extends InvalidOperationError {
+  constructor() {
+    super('Impossible de relancer le porteur après la date légale du 31/03/2025');
+  }
+}
 
 export type RelancerTransmissionPreuveRecandidatureOptions = {
   identifiantProjet: IdentifiantProjet.ValueType;
@@ -13,7 +20,7 @@ export async function relancerTransmissionPreuveRecandidature(
   { identifiantProjet, dateRelance }: RelancerTransmissionPreuveRecandidatureOptions,
 ) {
   if (dateRelance.estUltérieureÀ(dateLégaleRelance)) {
-    throw new Error('Impossible de relancer le porteur après la date légale du 31/03/2025');
+    throw new RelanceTransmissionPreuveRecandidatureImpossibleError();
   }
   return Promise.resolve();
 }
