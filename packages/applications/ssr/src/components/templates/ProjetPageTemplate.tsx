4ee566e621088a45e8493411a349437a43f34651
Julien Pavon <julien.pavon@gmail.com>
2023-11-21 12:07:56 +0100

    🚚 Rename ProjetPageTemplateProps

--- packages/applications/ssr/src/components/templates/ProjetPageTemplate.tsx
+++ packages/applications/ssr/src/components/templates/ProjetPageTemplate.tsx
@@ -2,13 +2,13 @@ import { ProjetBanner } from '@/components/molecules/projet/ProjetBanner';
 import { PageTemplate } from '@/components/templates/PageTemplate';
 import { Heading1 } from '../atoms/headings';
 
-type ProjetPageTemplate = {
+type ProjetPageTemplateProps = {
   heading: React.ReactNode;
   projet: Parameters<typeof ProjetBanner>[0];
   children: React.ReactNode;
 };
 
-export const ProjetPageTemplate = ({ heading, projet, children }: ProjetPageTemplate) => {
+export const ProjetPageTemplate = ({ heading, projet, children }: ProjetPageTemplateProps) => {
   return (
     <PageTemplate banner={<ProjetBanner {...projet} />}>
       <Heading1 className="mt-0 mb-8">{heading}</Heading1>
