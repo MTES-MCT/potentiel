48fa3a19460dd096c0ba515fe3b9bc7c231d37be
Hanaë <hanae.yamashita@lilo.org>
2023-11-22 12:22:25 +0100

    🎨 extract demander confirmation form component

---
+++ packages/applications/ssr/src/components/molecules/abandon/DemanderConfirmationAbandonForm.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
+import Alert from '@codegouvfr/react-dsfr/Alert';
+import { Upload } from '@codegouvfr/react-dsfr/Upload';
+import ButtonsGroup from '@codegouvfr/react-dsfr/ButtonsGroup';
+import {
+  DemanderConfirmationAbandonState,
+  demanderConfirmationAbandonAction,
+} from '@/app/laureat/[identifiant]/abandon/instruire/demanderConfirmation.action';
+import { Utilisateur } from '@/utils/utilisateur';
+
+const initialState: DemanderConfirmationAbandonState = {
+  error: undefined,
+  validationErrors: [],
+};
+
+type DemanderConfirmationAbandonFormProps = {
+  identifiantProjet: string;
+  utilisateur: Utilisateur;
+};
+
+export const DemanderConfirmationAbandonForm = ({
+  identifiantProjet,
+  utilisateur,
+}: DemanderConfirmationAbandonFormProps) => {
+  const { pending } = useFormStatus();
+  const [state, formAction] = useFormState(demanderConfirmationAbandonAction, initialState);
+
+  return (
+    <form action={formAction} method="post" encType="multipart/form-data">
+      {state.error && <Alert severity="error" title={state.error} className="mb-4" />}
+      <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+      <input type={'hidden'} value={utilisateur.email} name="utilisateurValue" />
+      <Upload
+        label="Téléverser une réponse signée"
+        hint="au format pdf"
+        state={state.validationErrors.includes('reponseSignee') ? 'error' : 'default'}
+        stateRelatedMessage="Réponse signée obligatoire"
+        nativeInputProps={{
+          name: 'reponseSignee',
+        }}
+        className="mb-4"
+      />
+      <ButtonsGroup
+        inlineLayoutWhen="always"
+        alignment="left"
+        buttons={[
+          {
+            children: 'Demander confirmation',
+            type: 'submit',
+            priority: 'primary',
+            nativeButtonProps: {
+              'aria-disabled': pending,
+            },
+            className: 'bg-blue-france-sun-base text-white',
+          },
+          {
+            children: 'Retour à la liste',
+            linkProps: {
+              href: '/laureat/abandon',
+            },
+            priority: 'secondary',
+          },
+        ]}
+      />
+    </form>
+  );
+};
--- packages/applications/ssr/src/components/molecules/abandon/InstructionAbandonForm.tsx
+++ packages/applications/ssr/src/components/molecules/abandon/InstructionAbandonForm.tsx
@@ -1,23 +1,11 @@
 'use client';
 
 import { useState } from 'react';
-import { useFormState, useFormStatus } from 'react-dom';
-import Alert from '@codegouvfr/react-dsfr/Alert';
-import { Upload } from '@codegouvfr/react-dsfr/Upload';
 import RadioButtons from '@codegouvfr/react-dsfr/RadioButtons';
-import ButtonsGroup from '@codegouvfr/react-dsfr/ButtonsGroup';
 import { Heading2 } from '../../atoms/headings';
 import { StatutAbandonBadge } from './StatutAbandonBadge';
-import {
-  DemanderConfirmationAbandonState,
-  demanderConfirmationAbandonAction,
-} from '@/app/laureat/[identifiant]/abandon/instruire/demanderConfirmation.action';
 import { Utilisateur } from '@/utils/utilisateur';
-
-const initialState: DemanderConfirmationAbandonState = {
-  error: undefined,
-  validationErrors: [],
-};
+import { DemanderConfirmationAbandonForm } from './DemanderConfirmationAbandonForm';
 
 type InstructionAbandonFormProps = {
   statut: Parameters<typeof StatutAbandonBadge>[0]['statut'];
@@ -38,25 +26,16 @@ export const InstructionAbandonForm = ({
   }
   const réponsePermise =
     utilisateur.rôle === 'dgec-validateur' || (utilisateur.rôle === 'admin' && !recandidature);
-
   const demandeConfirmationPossible = statut === 'demandé' && !recandidature;
 
-  const [needToUploadFile, setNeedToUploadFile] = useState(true);
   const [instruction, setInstruction] = useState('demander-confirmation');
-  const { pending } = useFormStatus();
-  const [state, formAction] = useFormState(demanderConfirmationAbandonAction, initialState);
 
   return (
     <>
       {réponseAttendue && réponsePermise ? (
         <>
           <Heading2>Instruire la demande</Heading2>
-          <form action={formAction} method="post" encType="multipart/form-data">
-            {state.error && <Alert severity="error" title={state.error} className="mb-4" />}
-
-            <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
-            <input type={'hidden'} value={utilisateur.email} name="utilisateurValue" />
-
+          <form>
             <RadioButtons
               legend="Instruire l'abandon"
               orientation="horizontal"
@@ -69,9 +48,9 @@ export const InstructionAbandonForm = ({
                         nativeInputProps: {
                           value: 'demander-confirmation',
                           onClick: () => {
-                            setNeedToUploadFile(true);
                             setInstruction('demander-confirmation');
                           },
+                          checked: true,
                         },
                       },
                     ]
@@ -81,7 +60,6 @@ export const InstructionAbandonForm = ({
                   nativeInputProps: {
                     value: 'accorder',
                     onClick: () => {
-                      setNeedToUploadFile(!recandidature);
                       setInstruction('accorder');
                     },
                     disabled: true,
@@ -92,7 +70,6 @@ export const InstructionAbandonForm = ({
                   nativeInputProps: {
                     value: 'rejeter',
                     onClick: () => {
-                      setNeedToUploadFile(true);
                       setInstruction('rejeter');
                     },
                     disabled: true,
@@ -100,43 +77,13 @@ export const InstructionAbandonForm = ({
                 },
               ]}
             />
-
-            {needToUploadFile && (
-              <Upload
-                label="Téléverser une réponse signée"
-                hint="au format pdf"
-                state={state.validationErrors.includes('reponseSignee') ? 'error' : 'default'}
-                stateRelatedMessage="Réponse signée obligatoire"
-                nativeInputProps={{
-                  name: 'reponseSignee',
-                }}
-                className="mb-4"
-              />
-            )}
-
-            <ButtonsGroup
-              inlineLayoutWhen="always"
-              alignment="left"
-              buttons={[
-                {
-                  children: 'Envoyer',
-                  type: 'submit',
-                  priority: 'primary',
-                  nativeButtonProps: {
-                    'aria-disabled': pending,
-                  },
-                  className: 'bg-blue-france-sun-base text-white',
-                },
-                {
-                  children: 'Retour à la liste',
-                  linkProps: {
-                    href: '/laureat/abandon',
-                  },
-                  priority: 'secondary',
-                },
-              ]}
-            />
           </form>
+          {instruction === 'demander-confirmation' && (
+            <DemanderConfirmationAbandonForm
+              identifiantProjet={identifiantProjet}
+              utilisateur={utilisateur}
+            />
+          )}
         </>
       ) : (
         ''
