0b00dc0abbe7d9dd92a7a9fe096203bdd955d563
Hanaë <hanae.yamashita@lilo.org>
2023-11-22 16:24:24 +0100

    ✨ rejeter délai form

---
+++ packages/applications/ssr/src/app/laureat/[identifiant]/abandon/instruire/rejeter.action.ts
@@ -0,0 +1,38 @@
+'use server';
+
+import { mediator } from 'mediateur';
+import * as zod from 'zod';
+import { Abandon } from '@potentiel-domain/laureat';
+import { FormAction, FormState, formAction } from '@/utils/formAction';
+
+export type RejeterAbandonState = FormState;
+
+const schema = zod.object({
+  identifiantProjet: zod.string(),
+  utilisateur: zod.string().email(),
+  reponseSignee: zod.instanceof(Blob).refine((data) => data.size > 0, {
+    message: 'Vous devez joindre une réponse signée.',
+  }),
+});
+
+const action: FormAction<FormState, typeof schema> = async (
+  previousState,
+  { identifiantProjet, reponseSignee, utilisateur },
+) => {
+  await mediator.send<Abandon.AbandonUseCase>({
+    type: 'REJETER_ABANDON_USECASE',
+    data: {
+      identifiantProjetValue: identifiantProjet,
+      utilisateurValue: utilisateur,
+      dateRejetValue: new Date().toISOString(),
+      réponseSignéeValue: {
+        content: reponseSignee.stream(),
+        format: reponseSignee.type,
+      },
+    },
+  });
+
+  return previousState;
+};
+
+export const rejeterAbandonAction = formAction(action, schema);
