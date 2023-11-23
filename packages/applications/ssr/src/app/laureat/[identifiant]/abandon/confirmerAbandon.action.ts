791012469e8a2df96fa67becb3f6798623f93b18
Hanaë <hanae.yamashita@lilo.org>
2023-11-23 16:49:18 +0100

    🎨 confirmer form and action

---
+++ packages/applications/ssr/src/app/laureat/[identifiant]/abandon/confirmerAbandon.action.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { mediator } from 'mediateur';
+import * as zod from 'zod';
+import { Abandon } from '@potentiel-domain/laureat';
+import { FormAction, FormState, formAction } from '@/utils/formAction';
+
+export type ConfirmerAbandonState = FormState;
+
+const schema = zod.object({
+  identifiantProjet: zod.string(),
+  utilisateur: zod.string().email(),
+});
+
+const action: FormAction<FormState, typeof schema> = async (
+  previousState,
+  { identifiantProjet, utilisateur },
+) => {
+  await mediator.send<Abandon.AbandonUseCase>({
+    type: 'CONFIRMER_ABANDON_USECASE',
+    data: {
+      identifiantProjetValue: identifiantProjet,
+      utilisateurValue: utilisateur,
+      dateConfirmationValue: new Date().toISOString(),
+    },
+  });
+
+  return previousState;
+};
+
+export const confirmerAbandonAction = formAction(action, schema);
---
+++ packages/applications/ssr/src/components/molecules/abandon/ConfirmerAbandonForm.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
+import Alert from '@codegouvfr/react-dsfr/Alert';
+import {
+  ConfirmerAbandonState,
+  confirmerAbandonAction,
+} from '@/app/laureat/[identifiant]/abandon/confirmerAbandon.action';
+import { Utilisateur } from '@/utils/getUtilisateur';
+import { useRouter } from 'next/navigation';
+import Button from '@codegouvfr/react-dsfr/Button';
+
+const initialState: ConfirmerAbandonState = {
+  error: undefined,
+  validationErrors: [],
+};
+
+type RejeterAbandonFormProps = {
+  identifiantProjet: string;
+  utilisateur: Utilisateur;
+};
+
+export const ConfirmerAbandonForm = ({
+  identifiantProjet,
+  utilisateur,
+}: RejeterAbandonFormProps) => {
+  const router = useRouter();
+  const { pending } = useFormStatus();
+  const [state, formAction] = useFormState(confirmerAbandonAction, initialState);
+
+  if (state.success) {
+    router.push(`/laureat/${encodeURIComponent(identifiantProjet)}/abandon`);
+  }
+
+  return (
+    <form action={formAction} method="post">
+      {state.error && <Alert severity="error" title={state.error} className="mb-4" />}
+      <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+      <input type={'hidden'} value={utilisateur.email} name="utilisateur" />
+      <Button
+        type="submit"
+        priority="primary"
+        nativeButtonProps={{
+          'aria-disabled': pending,
+          disabled: pending,
+          onClick: () => {
+            confirm("Êtes-vous sûr de vouloir confirmer l'abandon ?");
+          },
+        }}
+        className="bg-blue-france-sun-base text-white"
+      >
+        Confirmer l'abandon
+      </Button>
+    </form>
+  );
+};
