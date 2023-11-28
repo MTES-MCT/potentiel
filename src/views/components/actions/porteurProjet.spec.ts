880a44d86f24218b28f283bb7b03f9125ea072e4
Hanaë <hanae.yamashita@lilo.org>
2023-11-28 17:18:54 +0100

    ✅ fix tests

--- src/views/components/actions/porteurProjet.spec.ts
+++ src/views/components/actions/porteurProjet.spec.ts
@@ -16,6 +16,7 @@ describe('porteurProjetActions', () => {
       expect(result).toEqual([]);
     });
   });
+
   describe('when project is not "classé"', () => {
     describe('when project has a certificate file', () => {
       it('should return certificate link and "recours" link', () => {
@@ -45,22 +46,75 @@ describe('porteurProjetActions', () => {
         });
       });
     });
-    describe('when project is "classé"', () => {
-      it('should return an action array with the following actions: "Demander un délai", "Changer de producteur", "Changer de fournisseur", "Changer d\'actionnaire", "Changer de puissance", "Demander un abandon"', () => {
+  });
+
+  describe('when project is "classé"', () => {
+    it('should return an action array with the following actions: "Demander un délai", "Changer de producteur", "Changer de fournisseur", "Changer d\'actionnaire", "Changer de puissance"', () => {
+      const fakeProject = makeFakeProject({
+        isClasse: true,
+        appelOffre: { changementProducteurPossibleAvantAchèvement: true },
+      });
+      const result = porteurProjetActions(fakeProject);
+      expect(result).toHaveLength(5);
+      expect(result).toEqual([
+        {
+          title: 'Demander un délai',
+          link: ROUTES.DEMANDER_DELAI(fakeProject.id),
+        },
+        {
+          title: 'Changer de producteur',
+          link: ROUTES.GET_CHANGER_PRODUCTEUR(fakeProject.id),
+        },
+        {
+          title: 'Changer de fournisseur',
+          link: ROUTES.CHANGER_FOURNISSEUR(fakeProject.id),
+        },
+        {
+          title: "Changer d'actionnaire",
+          link: ROUTES.CHANGER_ACTIONNAIRE(fakeProject.id),
+        },
+        {
+          title: 'Changer de puissance',
+          link: ROUTES.DEMANDER_CHANGEMENT_PUISSANCE(fakeProject.id),
+        },
+      ]);
+    });
+    describe('when project has a certificate file', () => {
+      it('should return also a link to get this file', () => {
         const fakeProject = makeFakeProject({
           isClasse: true,
           appelOffre: { changementProducteurPossibleAvantAchèvement: true },
+          certificateFile: {
+            id: '1',
+            filename: 'file-name',
+          },
         });
         const result = porteurProjetActions(fakeProject);
         expect(result).toHaveLength(6);
+        expect(result[0]).toMatchObject({
+          title: 'Télécharger mon attestation',
+          link: ROUTES.CANDIDATE_CERTIFICATE_FOR_CANDIDATES({
+            id: fakeProject.id,
+            certificateFileId: fakeProject.certificateFile.id,
+            nomProjet: fakeProject.nomProjet,
+            potentielIdentifier: fakeProject.potentielIdentifier,
+          }),
+          isDownload: true,
+        });
+      });
+    });
+    describe('when the AO cannot change producteur before achevement', () => {
+      it('should not return "changement de producteur" action', () => {
+        const fakeProject = makeFakeProject({
+          isClasse: true,
+          appelOffre: { changementProducteurPossibleAvantAchèvement: false },
+        });
+        const result = porteurProjetActions(fakeProject);
+        expect(result).toHaveLength(4);
         expect(result).toEqual([
           {
             title: 'Demander un délai',
             link: ROUTES.DEMANDER_DELAI(fakeProject.id),
-          },
-          {
-            title: 'Changer de producteur',
-            link: ROUTES.GET_CHANGER_PRODUCTEUR(fakeProject.id),
           },
           {
             title: 'Changer de fournisseur',
@@ -74,67 +128,7 @@ describe('porteurProjetActions', () => {
             title: 'Changer de puissance',
             link: ROUTES.DEMANDER_CHANGEMENT_PUISSANCE(fakeProject.id),
           },
-          {
-            title: 'Demander un abandon',
-            link: ROUTES.GET_DEMANDER_ABANDON(fakeProject.id),
-          },
         ]);
-      });
-      describe('when project has a certificate file', () => {
-        it('should return also a link to get this file', () => {
-          const fakeProject = makeFakeProject({
-            isClasse: true,
-            appelOffre: { changementProducteurPossibleAvantAchèvement: true },
-            certificateFile: {
-              id: '1',
-              filename: 'file-name',
-            },
-          });
-          const result = porteurProjetActions(fakeProject);
-          expect(result).toHaveLength(7);
-          expect(result[0]).toMatchObject({
-            title: 'Télécharger mon attestation',
-            link: ROUTES.CANDIDATE_CERTIFICATE_FOR_CANDIDATES({
-              id: fakeProject.id,
-              certificateFileId: fakeProject.certificateFile.id,
-              nomProjet: fakeProject.nomProjet,
-              potentielIdentifier: fakeProject.potentielIdentifier,
-            }),
-            isDownload: true,
-          });
-        });
-      });
-      describe('when the AO cannot change producteur before achevement', () => {
-        it('should not return "changement de producteur" action', () => {
-          const fakeProject = makeFakeProject({
-            isClasse: true,
-            appelOffre: { changementProducteurPossibleAvantAchèvement: false },
-          });
-          const result = porteurProjetActions(fakeProject);
-          expect(result).toHaveLength(5);
-          expect(result).toEqual([
-            {
-              title: 'Demander un délai',
-              link: ROUTES.DEMANDER_DELAI(fakeProject.id),
-            },
-            {
-              title: 'Changer de fournisseur',
-              link: ROUTES.CHANGER_FOURNISSEUR(fakeProject.id),
-            },
-            {
-              title: "Changer d'actionnaire",
-              link: ROUTES.CHANGER_ACTIONNAIRE(fakeProject.id),
-            },
-            {
-              title: 'Changer de puissance',
-              link: ROUTES.DEMANDER_CHANGEMENT_PUISSANCE(fakeProject.id),
-            },
-            {
-              title: 'Demander un abandon',
-              link: ROUTES.GET_DEMANDER_ABANDON(fakeProject.id),
-            },
-          ]);
-        });
       });
     });
   });
