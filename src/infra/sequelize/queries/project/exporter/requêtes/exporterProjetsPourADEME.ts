8d45660dbffdb8d0035165bf3fdac8357b393a11
Hanaë <hanae.yamashita@lilo.org>
2023-11-30 15:37:19 +0100

    ✨ mise à jour colonnes export ademe

--- src/infra/sequelize/queries/project/exporter/requêtes/exporterProjetsPourADEME.ts
+++ src/infra/sequelize/queries/project/exporter/requêtes/exporterProjetsPourADEME.ts
@@ -1,35 +1,53 @@
 import { FiltreListeProjets } from '../../../../../../modules/project/queries/listerProjets';
 import {
   contenuLocal,
+  coordonnéesCandidat,
+  coordonnéesGéodésiques,
   coûtInvestissement,
   donnéesAutoconsommation,
+  donnéesDeRaccordement,
   donnéesFournisseurs,
   financementCitoyen,
+  garantiesFinancières,
   identificationProjet,
   implantation,
+  instruction,
   localisationProjet,
   modificationsAvantImport,
   noteInnovation,
   noteInnovationSensible,
+  notes,
   potentielSolaire,
+  prix,
+  référencesCandidature,
+  résultatInstructionSensible,
   évaluationCarbone,
 } from '../colonnesParCatégorie';
 import { récupérerExportProjets } from './récupérerExportProjets';
 
 const colonnesÀExporter = [
   ...identificationProjet,
+  ...coordonnéesCandidat,
   ...financementCitoyen,
   ...contenuLocal,
   ...localisationProjet,
+  ...coordonnéesGéodésiques,
   ...coûtInvestissement,
   ...donnéesAutoconsommation,
+  ...donnéesDeRaccordement,
   ...donnéesFournisseurs,
   ...évaluationCarbone,
   ...potentielSolaire,
   ...implantation,
+  ...prix,
+  ...référencesCandidature,
+  ...instruction,
+  ...résultatInstructionSensible,
   ...noteInnovation,
   ...noteInnovationSensible,
+  ...notes,
   ...modificationsAvantImport,
+  ...garantiesFinancières,
 ];
 
 export const exporterProjetsPourADEME = ({ filtres }: { filtres?: FiltreListeProjets }) =>
