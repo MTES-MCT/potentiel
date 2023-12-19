2a3bdea14ba8fd92b03b53810b6883e7fd4987a6
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-12-19 16:50:19 +0100

    ♻️ Link ajouter gestionnaire

--- packages/applications/ssr/src/app/reseau/gestionnaires/ajouter/page.tsx
+++ packages/applications/ssr/src/app/reseau/gestionnaires/ajouter/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 import { AjouterGestionnaireRéseauPage } from '@/components/pages/réseau/gestionnaire/AjouterGestionnaireRéseauPage';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   return PageWithErrorHandling(async () => {
     const utilisateur = await getUser();
--- packages/applications/ssr/src/components/pages/réseau/gestionnaire/GestionnaireRéseauListPage.tsx
+++ packages/applications/ssr/src/components/pages/réseau/gestionnaire/GestionnaireRéseauListPage.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import { FC } from 'react';
-import { ListPageTemplate } from '@/components/templates/ListPageTemplate';
 import { GestionnaireRéseauListItem } from '@/components/molecules/réseau/gestionnaireRéseau/GestionnaireRéseauListItem';
+import { PageTemplate } from '@/components/templates/PageTemplate';
+import { Heading1 } from '@/components/atoms/headings';
+import { List } from '@/components/organisms/List';
+import { ListHeader } from '@/components/organisms/ListHeader';
+import Link from 'next/link';
 
 type GestionnaireRéseauListPageProps = {
   list: {
@@ -17,18 +21,38 @@ export const GestionnaireRéseauListPage: FC<GestionnaireRéseauListPageProps> =
   list: { items: gestionnaireRéseaux, currentPage, totalItems, itemsPerPage },
 }) => {
   return (
-    <ListPageTemplate
-      heading="Gestionnaires réseaux"
-      items={gestionnaireRéseaux.map((gestionnaireRéseaux) => ({
-        ...gestionnaireRéseaux,
-        key: gestionnaireRéseaux.identifiantGestionnaireRéseau,
-      }))}
-      currentPage={currentPage}
-      totalItems={totalItems}
-      itemsPerPage={itemsPerPage}
-      ItemComponent={GestionnaireRéseauListItem}
-      filters={[]}
-      tagFilters={[]}
-    />
+    <PageTemplate banner={<Heading1 className="text-white">Gestionnaires réseaux</Heading1>}>
+      <Link href="/reseau/gestionnaires/ajouter">Ajouter un gestionnaire</Link>
+      <div className="flex flex-col md:flex-row md:items-center gap-3">
+        <ListHeader tagFilters={[]} totalCount={totalItems} />
+      </div>
+      {gestionnaireRéseaux.length ? (
+        <List
+          items={gestionnaireRéseaux.map((gestionnaireRéseaux) => ({
+            ...gestionnaireRéseaux,
+            key: gestionnaireRéseaux.identifiantGestionnaireRéseau,
+          }))}
+          currentPage={currentPage}
+          totalItems={totalItems}
+          itemsPerPage={itemsPerPage}
+          ItemComponent={GestionnaireRéseauListItem}
+        />
+      ) : (
+        <div className="flex flex-grow">Aucun résultat à afficher</div>
+      )}
+    </PageTemplate>
+    // <ListPageTemplate
+    //   heading="Gestionnaires réseaux"
+    //   items={gestionnaireRéseaux.map((gestionnaireRéseaux) => ({
+    //     ...gestionnaireRéseaux,
+    //     key: gestionnaireRéseaux.identifiantGestionnaireRéseau,
+    //   }))}
+    //   currentPage={currentPage}
+    //   totalItems={totalItems}
+    //   itemsPerPage={itemsPerPage}
+    //   ItemComponent={GestionnaireRéseauListItem}
+    //   filters={[]}
+    //   tagFilters={[]}
+    // />
   );
 };
