fe7b6bf60f2c653b468c29605b21e01171ffee29
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2023-12-21 15:15:57 +0100

    💄 Adjust display of DetailsAboutProjetPageTemplate

--- packages/applications/ssr/src/components/templates/DetailsAboutProjetPageTemplate.tsx
+++ packages/applications/ssr/src/components/templates/DetailsAboutProjetPageTemplate.tsx
@@ -16,8 +16,8 @@ export const DetailsAboutProjetPageTemplate: FC<DetailsAboutProjetPageTemplatePr
 }) => (
   <ProjetPageTemplate projet={projet} heading={heading}>
     <div className="flex flex-col justify-center items-center md:items-start md:flex-row md:gap-6">
-      <div className={`w-full ${actions ? 'md:w-3/4' : ''} flex flex-col gap-6`}>{details}</div>
-      {actions && <div className="w-full md:w-1/4 flex flex-col gap-4">{actions}</div>}
+      <div className={`flex-1 flex-col gap-6`}>{details}</div>
+      {actions && <div className="flex flex-col w-full md:w-1/4 gap-4">{actions}</div>}
     </div>
   </ProjetPageTemplate>
 );
