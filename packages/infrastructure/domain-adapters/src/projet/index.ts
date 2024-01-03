dfad2d3fbf97457cabb06ae20e656ebf52b8a763
Hanaë <hanae.yamashita@lilo.org>
2024-01-03 17:38:58 +0100

    🔥 (adapters): suppression adapter non utilisé

--- packages/infrastructure/domain-adapters/src/projet/index.ts
+++ packages/infrastructure/domain-adapters/src/projet/index.ts
@@ -2,4 +2,3 @@ export * from './récupérerCandidatureLegacy.adapter';
 export * from './récupérerPorteursProjet.adapter';
 export * from './consulterCahierDesChargesChoisi.adapter';
 export * from './listerIdentifiantsProjetsParPorteur.adapter';
-export * from './listerIdentifiantsProjetsParDreal.adapter';
