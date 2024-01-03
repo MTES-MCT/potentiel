98e3203e699ff090c4b3143b3b307cf605a4c207
HubM <hubmoncenis@gmail.com>
2024-01-03 16:37:34 +0100

    ✨ Add @potentiel/legacy-routes package

---
+++ packages/libraries/legacy-routes/src/routes/single-page.ts
@@ -0,0 +1,9 @@
+/**
+ * @deprecated à supprimer quand la page sera migrée
+ */
+export const PAGE_DECLARATION_ACCESSIBILITE = '/accessibilite.html';
+
+/**
+ * @deprecated à supprimer quand la page sera migrée
+ */
+export const PAGE_STATISTIQUES = '/stats.html';
