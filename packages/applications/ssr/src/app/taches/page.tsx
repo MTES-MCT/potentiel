f5ea2b39dc1f768c790dd712bf9bc88cc3c5bf56
HanaeY <72154904+HanaeY@users.noreply.github.com>
2024-01-05 15:21:07 +0100

    🐛 (revert): fix build nextjs (#1517)

--- packages/applications/ssr/src/app/taches/page.tsx
+++ packages/applications/ssr/src/app/taches/page.tsx
@@ -10,8 +10,6 @@ import { OperationRejectedError } from '@potentiel-domain/core';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 import type { Metadata } from 'next';
 
-export const dynamic = 'force-dynamic';
-
 type PageProps = {
   searchParams?: Record<string, string>;
 };
