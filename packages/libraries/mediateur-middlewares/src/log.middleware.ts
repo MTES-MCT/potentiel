ce3f731de3bb11d895f775fab50303d8b8e59a89
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2024-01-08 10:33:28 +0100

    ♻️ Log DomainError as Warning (#1521)

--- packages/libraries/mediateur-middlewares/src/log.middleware.ts
+++ packages/libraries/mediateur-middlewares/src/log.middleware.ts
@@ -1,6 +1,7 @@
 import { getLogger } from '@potentiel/monitoring';
 import { randomUUID } from 'crypto';
 import { Middleware } from 'mediateur';
+import { DomainError } from '@potentiel-domain/core';
 
 export const logMiddleware: Middleware = async (message, next) => {
   const correlationId = randomUUID();
@@ -10,7 +11,12 @@ export const logMiddleware: Middleware = async (message, next) => {
     getLogger().info('Message executed', { result: JSON.stringify(result), correlationId });
     return result;
   } catch (e) {
-    getLogger().error(e as Error, { result: JSON.stringify(e), correlationId });
+    if (e instanceof DomainError) {
+      getLogger().warn(e.message, { meta: e.meta });
+    } else {
+      getLogger().error(e as Error, { result: JSON.stringify(e), correlationId });
+    }
+
     throw e;
   }
 };
