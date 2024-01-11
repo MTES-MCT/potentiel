77dbe9db29fa1c533d5289ef172c8eaaf051b70e
HubM <hubmoncenis@gmail.com>
2024-01-11 14:26:37 +0100

    🎨 remove useless check in taches route

--- packages/applications/ssr/src/app/api/v1/taches/route.ts
+++ packages/applications/ssr/src/app/api/v1/taches/route.ts
@@ -1,7 +1,6 @@
 import { apiAction } from '@/utils/apiAction';
 import { mediator } from 'mediateur';
 import { ConsulterNombreTâchesQuery } from '@potentiel-domain/tache';
-import { OperationRejectedError } from '@potentiel-domain/core';
 import { withUtilisateur } from '@/utils/withUtilisateur';
 
 export const dynamic = 'force-dynamic';
@@ -9,16 +8,12 @@ export const dynamic = 'force-dynamic';
 export const GET = () =>
   apiAction(() =>
     withUtilisateur(async (utilisateur) => {
-      if (utilisateur) {
-        const result = await mediator.send<ConsulterNombreTâchesQuery>({
-          type: 'CONSULTER_NOMBRE_TÂCHES_QUERY',
-          data: {
-            email: utilisateur.identifiantUtilisateur.email,
-          },
-        });
-        return Response.json(result);
-      }
-
-      throw new OperationRejectedError('Utilisateur non connecté');
+      const result = await mediator.send<ConsulterNombreTâchesQuery>({
+        type: 'CONSULTER_NOMBRE_TÂCHES_QUERY',
+        data: {
+          email: utilisateur.identifiantUtilisateur.email,
+        },
+      });
+      return Response.json(result);
     }),
   );
