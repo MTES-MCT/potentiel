70937f0e586d536f209cc2f3d0eab95bdd3a619a
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-01-11 15:37:57 +0100

    ♻️ Modifier Gestionnaire Réseau Raccordement

---
+++ packages/domain/réseau/src/raccordement/modifier/modifierGestionnaireRéseauRaccordement.behavior.ts
@@ -0,0 +1,71 @@
+import { DomainEvent, NotFoundError } from '@potentiel-domain/core';
+import { IdentifiantGestionnaireRéseau } from '../../gestionnaire';
+import { IdentifiantProjet } from '@potentiel-domain/common';
+import { RaccordementAggregate } from '../raccordement.aggregate';
+
+/**
+ * @deprecated Utilisez GestionnaireRéseauRaccordementModifiéEvent et RéférenceDossierRacordementModifiéeEvent à la place. Cet event a été conserver pour la compatibilité avec le chargement des aggrégats et la fonctionnalité de rebuild des projections
+ */
+export type GestionnaireRéseauProjetModifiéEvent = DomainEvent<
+  'GestionnaireRéseauProjetModifié-V1',
+  {
+    identifiantProjet: string;
+    identifiantGestionnaireRéseau: string;
+  }
+>;
+
+export type GestionnaireRéseauRaccordementModifiéEvent = DomainEvent<
+  'GestionnaireRéseauRaccordementModifié-V1',
+  {
+    identifiantProjet: string;
+    identifiantGestionnaireRéseau: string;
+  }
+>;
+
+type ModifierGestionnaireRéseauOptions = {
+  identifiantProjet: IdentifiantProjet.ValueType;
+  identifiantGestionnaireRéseau: IdentifiantGestionnaireRéseau.ValueType;
+};
+
+export async function modifierGestionnaireRéseau(
+  this: RaccordementAggregate,
+  { identifiantGestionnaireRéseau, identifiantProjet }: ModifierGestionnaireRéseauOptions,
+) {
+  if (this.identifiantGestionnaireRéseau.estÉgaleÀ(identifiantGestionnaireRéseau)) {
+    throw new MêmeGestionnaireRéseauUtiliséPourModifierLeRaccordementError(
+      identifiantProjet,
+      identifiantGestionnaireRéseau,
+    );
+  }
+
+  const event: GestionnaireRéseauRaccordementModifiéEvent = {
+    type: 'GestionnaireRéseauRaccordementModifié-V1',
+    payload: {
+      identifiantProjet: identifiantProjet.formatter(),
+      identifiantGestionnaireRéseau: identifiantGestionnaireRéseau.formatter(),
+    },
+  };
+
+  await this.publish(event);
+}
+
+export function applyGestionnaireRéseauRaccordementModifiéEventV1(
+  this: RaccordementAggregate,
+  { payload: { identifiantGestionnaireRéseau } }: GestionnaireRéseauRaccordementModifiéEvent,
+) {
+  this.identifiantGestionnaireRéseau = IdentifiantGestionnaireRéseau.convertirEnValueType(
+    identifiantGestionnaireRéseau,
+  );
+}
+
+export class MêmeGestionnaireRéseauUtiliséPourModifierLeRaccordementError extends NotFoundError {
+  constructor(
+    identifiantProjet: IdentifiantProjet.ValueType,
+    identifiantGestionnaireRéseau: IdentifiantGestionnaireRéseau.ValueType,
+  ) {
+    super(`Ce gestionnaire de réseau est déjà déclarer pour le dossier de raccordement`, {
+      identifiantProjet: identifiantProjet.formatter(),
+      identifiantGestionnaireRéseau: identifiantGestionnaireRéseau.formatter(),
+    });
+  }
+}
