ac88b78f993b40d7c205435ed7b4ea85ae2be043
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-01-11 11:21:43 +0100

    🔥 Remove raccordement usecases

--- packages/domain-usecases/src/projet/lauréat/gestionnaireRéseau/modifier/modifierGestionnaireRéseauProjet.usecase.ts
+++ packages/domain-usecases/src/projet/lauréat/gestionnaireRéseau/modifier/modifierGestionnaireRéseauProjet.usecase.ts
@@ -1,26 +1,26 @@
-import { Message, MessageHandler, mediator } from 'mediateur';
-import { ModifierGestionnaireRéseauProjetCommand } from './modifierGestionnaireRéseauProjet.command';
-import { ProjetCommand } from '../gestionnaireRéseauProjet.command';
+// import { Message, MessageHandler, mediator } from 'mediateur';
+// import { ModifierGestionnaireRéseauProjetCommand } from './modifierGestionnaireRéseauProjet.command';
+// import { ProjetCommand } from '../gestionnaireRéseauProjet.command';
 
-type ModifierGestionnaireRéseauProjetUseCaseData = ModifierGestionnaireRéseauProjetCommand['data'];
+// type ModifierGestionnaireRéseauProjetUseCaseData = ModifierGestionnaireRéseauProjetCommand['data'];
 
-export type ModifierGestionnaireRéseauProjetUseCase = Message<
-  'MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET_USE_CASE',
-  ModifierGestionnaireRéseauProjetUseCaseData
->;
+// export type ModifierGestionnaireRéseauProjetUseCase = Message<
+//   'MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET_USE_CASE',
+//   ModifierGestionnaireRéseauProjetUseCaseData
+// >;
 
-export const registerModifierGestionnaireRéseauProjetUseCase = () => {
-  const runner: MessageHandler<ModifierGestionnaireRéseauProjetUseCase> = async ({
-    identifiantGestionnaireRéseau,
-    identifiantProjet,
-  }) => {
-    await mediator.send<ProjetCommand>({
-      type: 'MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET',
-      data: {
-        identifiantProjet,
-        identifiantGestionnaireRéseau,
-      },
-    });
-  };
-  mediator.register('MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET_USE_CASE', runner);
-};
+// export const registerModifierGestionnaireRéseauProjetUseCase = () => {
+//   const runner: MessageHandler<ModifierGestionnaireRéseauProjetUseCase> = async ({
+//     identifiantGestionnaireRéseau,
+//     identifiantProjet,
+//   }) => {
+//     await mediator.send<ProjetCommand>({
+//       type: 'MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET',
+//       data: {
+//         identifiantProjet,
+//         identifiantGestionnaireRéseau,
+//       },
+//     });
+//   };
+//   mediator.register('MODIFIER_GESTIONNAIRE_RÉSEAU_PROJET_USE_CASE', runner);
+// };
