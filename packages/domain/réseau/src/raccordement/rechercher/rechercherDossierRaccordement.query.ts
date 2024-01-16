69bcc2b83b3e32024a792bb850832a6ef9edf042
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-01-16 15:28:17 +0100

    ♻️ Rechercher dossier raccordement

---
+++ packages/domain/réseau/src/raccordement/rechercher/rechercherDossierRaccordement.query.ts
@@ -0,0 +1,53 @@
+import { Message, MessageHandler, mediator } from 'mediateur';
+import * as RéférenceDossierRaccordement from '../référenceDossierRaccordement.valueType';
+import { IdentifiantProjet } from '@potentiel-domain/common';
+import { Find } from '@potentiel-libraries/projection';
+import { RéférenceRaccordementIdentifiantProjetEntity } from '../raccordement.entity';
+import { isNone } from '@potentiel/monads';
+import { DossierRaccordementNonRéférencéError } from '../dossierRaccordementNonRéférencé.error';
+
+export type RechercherDossierRaccordementReadModel = {
+  référenceDossierRaccordement: RéférenceDossierRaccordement.ValueType;
+  identifiantProjet: IdentifiantProjet.ValueType;
+};
+
+export type RechercherDossierRaccordementQuery = Message<
+  'RECHERCHER_DOSSIER_RACCORDEMENT_QUERY',
+  {
+    référenceDossierRaccordement: string;
+  },
+  RechercherDossierRaccordementReadModel
+>;
+
+export type RechercherDossierRaccordementDependencies = {
+  find: Find;
+};
+
+export const registerRechercherDossierRaccordementQuery = ({
+  find,
+}: RechercherDossierRaccordementDependencies) => {
+  const handler: MessageHandler<RechercherDossierRaccordementQuery> = async ({
+    référenceDossierRaccordement,
+  }) => {
+    const result = await find<RéférenceRaccordementIdentifiantProjetEntity>(
+      `référence-raccordement-identifiant-projet|${référenceDossierRaccordement}`,
+    );
+
+    if (isNone(result)) {
+      throw new DossierRaccordementNonRéférencéError();
+    }
+
+    return mapToReadModel(result);
+  };
+
+  mediator.register('RECHERCHER_DOSSIER_RACCORDEMENT_QUERY', handler);
+};
+
+const mapToReadModel = (result: RéférenceRaccordementIdentifiantProjetEntity) => {
+  return {
+    identifiantProjet: IdentifiantProjet.convertirEnValueType(result.identifiantProjet),
+    référenceDossierRaccordement: RéférenceDossierRaccordement.convertirEnValueType(
+      result.référence,
+    ),
+  };
+};
