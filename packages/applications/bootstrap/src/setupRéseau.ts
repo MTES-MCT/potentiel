b6287e1853977177bfdae28565fec144288910a2
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-01-17 14:44:27 +0100

    ♻️ Projector (WIP)

--- packages/applications/bootstrap/src/setupRéseau.ts
+++ packages/applications/bootstrap/src/setupRéseau.ts
@@ -1,7 +1,10 @@
 import { registerRéseauQueries, registerRéseauUseCases } from '@potentiel-domain/reseau';
 import { loadAggregate, subscribe } from '@potentiel-infrastructure/pg-event-sourcing';
 import { findProjection, listProjection } from '@potentiel-infrastructure/pg-projections';
-import { GestionnaireRéseauProjector } from '@potentiel-infrastructure/projectors';
+import {
+  GestionnaireRéseauProjector,
+  RaccordementProjector,
+} from '@potentiel-infrastructure/projectors';
 import { mediator } from 'mediateur';
 
 export const setupRéseau = async () => {
@@ -15,6 +18,7 @@ export const setupRéseau = async () => {
   });
 
   GestionnaireRéseauProjector.register();
+  RaccordementProjector.register();
 
   const unsubscribeGestionnaireRéseauProjector =
     await subscribe<GestionnaireRéseauProjector.SubscriptionEvent>({
@@ -33,7 +37,38 @@ export const setupRéseau = async () => {
       streamCategory: 'gestionnaire-réseau',
     });
 
+  const unsubscribeRaccordementProjector = await subscribe<RaccordementProjector.SubscriptionEvent>(
+    {
+      name: 'projector',
+      eventType: [
+        'RebuildTriggered',
+        'AccuséRéceptionDemandeComplèteRaccordementTransmis-V1',
+        'DateMiseEnServiceTransmise-V1',
+        'DemandeComplèteDeRaccordementTransmise-V1',
+        'DemandeComplèteDeRaccordementTransmise-V2',
+        'DemandeComplèteRaccordementModifiée-V1',
+        'DemandeComplèteRaccordementModifiée-V2',
+        'DemandeComplèteRaccordementModifiée-V3',
+        'GestionnaireRéseauRaccordementModifié-V1',
+        'PropositionTechniqueEtFinancièreModifiée-V1',
+        'PropositionTechniqueEtFinancièreModifiée-V2',
+        'PropositionTechniqueEtFinancièreSignéeTransmise-V1',
+        'PropositionTechniqueEtFinancièreTransmise-V1',
+        'PropositionTechniqueEtFinancièreTransmise-V2',
+        'RéférenceDossierRacordementModifiée-V1',
+      ],
+      eventHandler: async (event) => {
+        await mediator.send<RaccordementProjector.Execute>({
+          type: 'EXECUTE_RACCORDEMENT_PROJECTOR',
+          data: event,
+        });
+      },
+      streamCategory: 'raccordement',
+    },
+  );
+
   return async () => {
     await unsubscribeGestionnaireRéseauProjector();
+    await unsubscribeRaccordementProjector();
   };
 };
