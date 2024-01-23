1b57222eb556090e55fb5167414d4006e7997738
Hubert MONCENIS <hub@macbook-14-hub.home>
2024-01-23 14:55:05 +0100

    :art: Add switch for theme on storybook

--- packages/applications/ssr/.storybook/main.ts
+++ packages/applications/ssr/.storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-themes'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
--- packages/applications/ssr/tailwind.config.ts
+++ packages/applications/ssr/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: ['class'],
   theme: {
     extend: {
       backgroundImage: {
