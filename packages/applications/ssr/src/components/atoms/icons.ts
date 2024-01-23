d805b98c54f565d32d30b2d5007ddb13781eca56
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-01-23 14:58:03 +0100

    💄 Add the page for Dossiers de raccordement

--- packages/applications/ssr/src/components/atoms/icons.ts
+++ packages/applications/ssr/src/components/atoms/icons.ts
@@ -3,4 +3,12 @@ export {
   RiArrowRightSLine as NextPageIcon,
   RiSkipLeftLine as FirstPageIcon,
   RiSkipRightLine as LastPageIcon,
+  RiPencilFill as EditIcon,
+  RiPriceTag3Line as TagIcon,
+  RiCalendarLine as CalendarIcon,
+  RiCheckboxCircleFill as SuccessIcon,
+  RiTimeLine as ClockIcon,
+  RiArrowRightCircleLine as ArrowRightWithCircle,
+  RiArrowDownCircleLine as ArrowDownWithCircle,
 } from 'react-icons/ri';
+export { IoIosWarning as WarningIcon } from 'react-icons/io';
---
+++ packages/libraries/routes/src/lauréat/raccordement.routes.ts
@@ -0,0 +1,46 @@
+import { encodeParameter } from '../encodeParameter';
+
+export const détail = (identifiantProjet: string, référenceDossierRaccordement: string) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/${encodeParameter(
+    référenceDossierRaccordement,
+  )}`;
+
+export const modifierGestionnaireDeRéseau = (identifiantProjet: string) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/modifier-gestionnaire-de-reseau`;
+
+export const transmettreDemandeComplèteDeRaccordement = (identifiantProjet: string) =>
+  `/laureats/${encodeParameter(
+    identifiantProjet,
+  )}/raccordements/transmettre-demande-complete-de-raccordement`;
+
+export const modifierDemandeComplèteDeRaccordement = (
+  identifiantProjet: string,
+  référenceDossierRaccordement: string,
+) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/${encodeParameter(
+    référenceDossierRaccordement,
+  )}/modifier-demande-complete-de-raccordement`;
+
+export const transmettrePropositionTechniqueEtFinancière = (
+  identifiantProjet: string,
+  référenceDossierRaccordement: string,
+) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/${encodeParameter(
+    référenceDossierRaccordement,
+  )}/transmettre-technique-et-financiere`;
+
+export const modifierPropositionTechniqueEtFinancière = (
+  identifiantProjet: string,
+  référenceDossierRaccordement: string,
+) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/${encodeParameter(
+    référenceDossierRaccordement,
+  )}/modifier-technique-et-financiere`;
+
+export const transmettreDateMiseEnService = (
+  identifiantProjet: string,
+  référenceDossierRaccordement: string,
+) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/raccordements/${encodeParameter(
+    référenceDossierRaccordement,
+  )}/transmettre-date-mise-en-service`;
