2dc99b14f688365f8460a62022ab8a09be34f804
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-26 10:39:24 +0100

    :truck: Use correct file name convention

--- packages/applications/ssr/src/components/pages/réseau/raccordement/transmettreDemandeComplète/TransmettreDemandeComplèteRaccordement.stories.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/transmettreDemandeComplète/TransmettreDemandeComplèteRaccordement.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { TransmettreDemandeComplèteRaccordementPage } from './transmettreDemandeComplèteRaccordementPage';
+import { TransmettreDemandeComplèteRaccordementPage } from './TransmettreDemandeComplèteRaccordementPage';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
