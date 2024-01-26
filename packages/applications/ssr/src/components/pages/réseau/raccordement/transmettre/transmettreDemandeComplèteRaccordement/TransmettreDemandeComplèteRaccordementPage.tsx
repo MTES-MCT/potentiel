87389d3a215ee19f09fb0ac00b005c9964a9da90
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-01-26 16:59:56 +0100

    ✨ Review information about demande complete de raccordement

--- packages/applications/ssr/src/components/pages/réseau/raccordement/transmettre/transmettreDemandeComplèteRaccordement/TransmettreDemandeComplèteRaccordementPage.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/transmettre/transmettreDemandeComplèteRaccordement/TransmettreDemandeComplèteRaccordementPage.tsx
@@ -17,12 +17,16 @@ import { SubmitButton } from '@/components/atoms/form/SubmitButton';
 import Input from '@codegouvfr/react-dsfr/Input';
 import { Upload } from '@codegouvfr/react-dsfr/Upload';
 import Link from 'next/link';
+import {
+  InformationDemandeComplèteRaccordement,
+  InformationDemandeComplèteRaccordementProps,
+} from '../../InformationDemandeComplèteRaccordement';
 
 type TransmettreDemandeComplèteRaccordementProps = {
   listeGestionnairesRéseau: GestionnaireRéseauSelectProps['gestionnairesRéseau'];
   identifiantGestionnaireRéseauActuel: string;
   projet: FormForProjetPageTemplateProps['projet'];
-  delaiDemandeDeRaccordementEnMois: { texte: string; valeur: number };
+  delaiDemandeDeRaccordementEnMois: InformationDemandeComplèteRaccordementProps['delaiDemandeDeRaccordementEnMois'];
 };
 
 export const TransmettreDemandeComplèteRaccordementPage: FC<
@@ -134,34 +138,9 @@ export const TransmettreDemandeComplèteRaccordementPage: FC<
       }
       information={{
         description: (
-          <>
-            <p>
-              <span className="font-bold">* Où trouver la référence de mon dossier ?</span>
-              <br />
-              Vous pouvez retrouver cette donnée sur le courriel d'accusé de réception de votre
-              demande complète de raccordement (
-              <a href="https://docs.potentiel.beta.gouv.fr/faq/ou-trouver-la-reference-du-dossier-de-raccordement-de-mon-projet">
-                voir un exemple d'accusé de réception
-              </a>
-              )
-            </p>
-            <p>
-              <span className="font-bold">** Quel document transmettre dans Potentiel ?</span>
-              <br />
-              Vous devez déposer une demande de raccordement dans les{' '}
-              {delaiDemandeDeRaccordementEnMois.texte} ({delaiDemandeDeRaccordementEnMois.valeur})
-              mois suivant la date de désignation du projet auprès de votre gestionnaire de réseau.
-              <br />
-              Votre gestionnaire de réseau vous retourne un{' '}
-              <span className="italic">accusé de réception</span> lorsque votre demande de
-              raccordement est jugée complète.
-              <br />
-              Cet accusé de réception transmis sur Potentiel facilitera vos démarches
-              administratives avec les différents acteurs connectés à Potentiel (DGEC, DREAL,
-              Cocontractant, etc.), il est nécessaire dans le cadre de l’instruction selon les
-              cahiers des charges modificatifs et publié le 30/08/2022.
-            </p>
-          </>
+          <InformationDemandeComplèteRaccordement
+            delaiDemandeDeRaccordementEnMois={delaiDemandeDeRaccordementEnMois}
+          />
         ),
       }}
     />
