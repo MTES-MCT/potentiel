5ef86f5e4d2c337d2b5af4d0d35ed0162c99813b
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-26 10:39:24 +0100

    :truck: Use correct file name convention

--- packages/applications/ssr/src/components/pages/réseau/raccordement/transmettrePropositionTechniqueEtFinancière/TransmettrePropositionTechniqueEtFinancière.stories.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/transmettrePropositionTechniqueEtFinancière/TransmettrePropositionTechniqueEtFinancière.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { TransmettrePropositionTechniqueEtFinancièrePage } from './transmettrePropositionTechniqueEtFinancièrePage';
+import { TransmettrePropositionTechniqueEtFinancièrePage } from './TransmettrePropositionTechniqueEtFinancièrePage';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
