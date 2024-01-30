51109875719a2b24e7a2decd4c6794ac34808573
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-30 11:11:40 +0100

    :recycle: Create ColumnPageTemplate and use it instead of FormForProjetPageTemplate

--- packages/applications/ssr/src/components/pages/réseau/raccordement/modifier/modifierPropositionTechniqueEtFinancière/ModifierPropositionTechniqueEtFinancière.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/modifier/modifierPropositionTechniqueEtFinancière/ModifierPropositionTechniqueEtFinancière.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 import Input from '@codegouvfr/react-dsfr/Input';
+import { Alert } from '@codegouvfr/react-dsfr/Alert';
 
 import { Routes } from '@potentiel-libraries/routes';
 
@@ -8,10 +9,9 @@ import { Form } from '@/components/atoms/form/Form';
 import { SubmitButton } from '@/components/atoms/form/SubmitButton';
 import { InputFile } from '@/components/atoms/form/InputFile';
 import { formatDateForInput } from '@/utils/formatDateForInput';
-import {
-  FormForProjetPageTemplate,
-  FormForProjetPageTemplateProps,
-} from '@/components/templates/FormForProjetPageTemplate';
+import { FormForProjetPageTemplateProps } from '@/components/templates/FormForProjetPageTemplate';
+import { ColumnPageTemplate } from '@/components/templates/ColumnPageTemplate';
+import { ProjetBanner } from '@/components/molecules/projet/ProjetBanner';
 
 import { TitrePageRaccordement } from '../../TitrePageRaccordement';
 
@@ -40,49 +40,60 @@ export const ModifierPropositionTechniqueEtFinancièrePage: FC<
   const { identifiantProjet } = projet;
 
   return (
-    <FormForProjetPageTemplate
+    <ColumnPageTemplate
+      banner={<ProjetBanner {...projet} />}
       heading={<TitrePageRaccordement />}
-      projet={projet}
-      form={
-        <Form
-          method="POST"
-          encType="multipart/form-data"
-          action={modifierPropositionTechniqueEtFinancièreAction}
-          heading="Modifier la proposition technique et financière"
-        >
-          <p className="my-2 p-0">Référence du dossier de raccordement : {référence}</p>
+      leftColumn={{
+        children: (
+          <Form
+            method="POST"
+            encType="multipart/form-data"
+            action={modifierPropositionTechniqueEtFinancièreAction}
+            heading="Modifier la proposition technique et financière"
+          >
+            <p className="my-2 p-0">Référence du dossier de raccordement : {référence}</p>
 
-          <InputFile
-            id="file"
-            label="Proposition technique et financière signée"
-            name="file"
-            fileUrl={Routes.Document.télécharger(propositionTechniqueEtFinancièreSignée)}
+            <InputFile
+              id="file"
+              label="Proposition technique et financière signée"
+              name="file"
+              fileUrl={Routes.Document.télécharger(propositionTechniqueEtFinancièreSignée)}
+            />
+
+            <Input
+              id="dateSignature"
+              label="Date de signature"
+              nativeInputProps={{
+                type: 'date',
+                name: 'dateSignature',
+                defaultValue: formatDateForInput(dateSignature),
+                max: new Date().toISOString().split('T').shift(),
+                required: true,
+              }}
+            />
+
+            <div className="flex flex-col md:flex-row gap-4 m-auto">
+              <SubmitButton>Modifier</SubmitButton>
+              <Link href={Routes.Raccordement.détail(identifiantProjet)} className="m-auto">
+                Retour vers le dossier de raccordement
+              </Link>
+            </div>
+          </Form>
+        ),
+      }}
+      rightColumn={{
+        children: (
+          <Alert
+            severity="info"
+            small
+            title="Concernant le dépôt"
+            description={
+              <div className="py-4 text-justify">
+                Le dépôt est informatif, il ne remplace pas la transmission à votre gestionnaire.
+              </div>
+            }
           />
-
-          <Input
-            id="dateSignature"
-            label="Date de signature"
-            nativeInputProps={{
-              type: 'date',
-              name: 'dateSignature',
-              defaultValue: formatDateForInput(dateSignature),
-              max: new Date().toISOString().split('T').shift(),
-              required: true,
-            }}
-          />
-
-          <div className="flex flex-col md:flex-row gap-4 m-auto">
-            <SubmitButton>Modifier</SubmitButton>
-            <Link href={Routes.Raccordement.détail(identifiantProjet)} className="m-auto">
-              Retour vers le dossier de raccordement
-            </Link>
-          </div>
-        </Form>
-      }
-      information={{
-        description:
-          'Le dépôt est informatif, il ne remplace pas la transmission à votre gestionnaire.',
-        title: 'Concernant le dépôt',
+        ),
       }}
     />
   );
