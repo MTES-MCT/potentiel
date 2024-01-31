6ea4713f9ee1538390e224295f2e82f43ff3536d
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-01-31 08:58:18 +0100

    ♻️ Extract FormError component

--- packages/applications/ssr/src/components/atoms/form/Form.tsx
+++ packages/applications/ssr/src/components/atoms/form/Form.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import Alert, { AlertProps } from '@codegouvfr/react-dsfr/Alert';
 import { FC, FormHTMLAttributes } from 'react';
 import { useFormState } from 'react-dom';
 
-import { FormState, formAction } from '@/utils/formAction';
+import { formAction } from '@/utils/formAction';
 
 import { Heading2 } from '../headings';
+
+import { FormError } from './FormError';
 
 export type FormProps = Omit<FormHTMLAttributes<HTMLFormElement>, 'action'> & {
   action: ReturnType<typeof formAction>;
@@ -54,45 +55,3 @@ export const Form: FC<FormProps> = ({
     </form>
   );
 };
-
-type FormErrorProps = {
-  formState: FormState;
-};
-
-const FormError: FC<FormErrorProps> = ({ formState }) => {
-  switch (formState.status) {
-    case 'domain-error':
-      return <AlertError description={formState.message} />;
-
-    case 'form-error':
-      return <AlertError description="Erreur lors de la validation des données du formulaire" />;
-
-    case 'csv-error':
-      return (
-        <AlertError
-          title={`Le fichier contient les erreurs suivantes :`}
-          description={
-            <ul className="list-disc pl-3 mt-2">
-              {formState.errors.map((error) => (
-                <li key={`${error.line}-${error.field}`}>
-                  Ligne {error.line} (champ {error.field}) : {error.message}
-                </li>
-              ))}
-            </ul>
-          }
-        />
-      );
-
-    case 'unknown-error':
-      return <AlertError description="Une erreur est survenue" />;
-
-    default:
-      return null;
-  }
-};
-
-type AlertErrorProps = Omit<AlertProps.Small, 'small'>;
-
-const AlertError: FC<AlertErrorProps> = ({ title, description }) => (
-  <Alert small severity="error" title={title} description={description} className="mb-4" />
-);
