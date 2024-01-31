b1284bbecca300399196954dd31f310ac5146343
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-31 11:22:58 +0100

    🚸 Add gestionnaires stories

---
+++ packages/applications/ssr/src/components/pages/réseau/gestionnaire/lister/GestionnaireRéseauList.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { GestionnaireRéseauListPage } from './GestionnaireRéseauListPage';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Pages/Réseau/Gestionnaire/Lister/GestionnaireRéseauListPage',
+  component: GestionnaireRéseauListPage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof GestionnaireRéseauListPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const itemsLength = 10;
+export const Default: Story = {
+  args: {
+    list: {
+      items: Array.from({ length: itemsLength }, (_, i) => ({
+        identifiantGestionnaireRéseau: `identifiantGestionnaireRéseau${i}`,
+        raisonSociale: `Gestionnaire#${(i += 1)}`,
+      })),
+      currentPage: 1,
+      itemsPerPage: 10,
+      totalItems: itemsLength,
+    },
+  },
+};
