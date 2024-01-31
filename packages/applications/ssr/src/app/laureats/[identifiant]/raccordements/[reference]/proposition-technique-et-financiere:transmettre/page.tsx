8ce543b0fb9c69428449d037c97414eae7749367
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-31 16:15:39 +0100

    🚚 Next route raccordement to raccordements

---
+++ packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/[reference]/proposition-technique-et-financiere:transmettre/page.tsx
@@ -0,0 +1,60 @@
+import { mediator } from 'mediateur';
+import type { Metadata } from 'next';
+
+import { ConsulterCandidatureQuery } from '@potentiel-domain/candidature';
+import { Raccordement } from '@potentiel-domain/reseau';
+
+import { IdentifiantParameter } from '@/utils/identifiantParameter';
+import { decodeParameter } from '@/utils/decodeParameter';
+import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
+import { withUtilisateur } from '@/utils/withUtilisateur';
+import {
+  TransmettrePropositionTechniqueEtFinancièrePage,
+  TransmettrePropositionTechniqueEtFinancièreProps,
+} from '@/components/pages/réseau/raccordement/transmettre/transmettrePropositionTechniqueEtFinancière/TransmettrePropositionTechniqueEtFinancière.page';
+
+type PageProps = {
+  params: {
+    identifiant: IdentifiantParameter['params']['identifiant'];
+    reference: string;
+  };
+};
+
+export const metadata: Metadata = {
+  title: 'Transmettre la proposition technique et financière - Potentiel',
+  description: 'Transmettre la proposition technique et financière',
+};
+
+export default async function Page({ params: { identifiant, reference } }: PageProps) {
+  return PageWithErrorHandling(async () =>
+    withUtilisateur(async () => {
+      const identifiantProjet = decodeParameter(identifiant);
+      const referenceDossierRaccordement = decodeParameter(reference);
+
+      const candidature = await mediator.send<ConsulterCandidatureQuery>({
+        type: 'CONSULTER_CANDIDATURE_QUERY',
+        data: {
+          identifiantProjet,
+        },
+      });
+
+      await mediator.send<Raccordement.ConsulterDossierRaccordementQuery>({
+        type: 'CONSULTER_DOSSIER_RACCORDEMENT_QUERY',
+        data: {
+          identifiantProjetValue: identifiantProjet,
+          référenceDossierRaccordementValue: referenceDossierRaccordement,
+        },
+      });
+
+      const props: TransmettrePropositionTechniqueEtFinancièreProps = {
+        projet: {
+          ...candidature,
+          identifiantProjet,
+        },
+        referenceDossierRaccordement,
+      };
+
+      return <TransmettrePropositionTechniqueEtFinancièrePage {...props} />;
+    }),
+  );
+}
