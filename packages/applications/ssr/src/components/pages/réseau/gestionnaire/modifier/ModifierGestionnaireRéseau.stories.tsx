a069e62aaece604e458f710e8c53f05954f1e1ae
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-01-31 11:22:58 +0100

    🚸 Add gestionnaires stories

---
+++ packages/applications/ssr/src/components/pages/réseau/gestionnaire/modifier/ModifierGestionnaireRéseau.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ModifierGestionnaireRéseauPage } from './ModifierGestionnaireRéseauPage';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Pages/Réseau/Gestionnaire/Modifier/ModifierGestionnaireRéseauPage',
+  component: ModifierGestionnaireRéseauPage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof ModifierGestionnaireRéseauPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    identifiantGestionnaireRéseau: 'identifiantGestionnaireRéseau',
+    raisonSociale: 'raisonSociale',
+    format: 'format',
+    légende: 'légende',
+    expressionReguliere: 'expressionReguliere',
+  },
+};
