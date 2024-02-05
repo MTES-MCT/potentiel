9bd28ada0bb9c93b8f6e612930dba084b0d46ee7
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-02-05 11:38:22 +0100

    ♻️ Policies.raccordement.dcr.modifier

--- packages/libraries/legacy-permissions/src/raccordement.permissions.ts
+++ packages/libraries/legacy-permissions/src/raccordement.permissions.ts
@@ -1,14 +1,6 @@
 /**
  * @deprecated un nouveau systéme de permission va être implémenter dans la nouvelle stack technique
  */
-export const PermissionTransmettreDemandeComplèteRaccordement = {
-  nom: 'transmettre-demande-complète-raccordement',
-  description: 'Transmettre une demande complète de raccordement',
-};
-
-/**
- * @deprecated un nouveau systéme de permission va être implémenter dans la nouvelle stack technique
- */
 export const PermissionTransmettreDateMiseEnService = {
   nom: 'transmettre-date-mise-en-service',
   description: 'Transmettre une date de mise en service',
