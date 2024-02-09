080a8e3a2e8cef972070782230464dec61859915
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-02-09 10:28:10 +0100

    💡 @todo for file path constitution

--- packages/libraries/file-storage/src/upload.ts
+++ packages/libraries/file-storage/src/upload.ts
@@ -2,6 +2,11 @@ import { getBucketName } from './getBucketName';
 import { getClient } from './getClient';
 import { Upload } from '@aws-sdk/lib-storage';
 
+/**
+ *
+ * @todo ici la key devrait être un IdentifiantDocumentProjet et la
+ * fonction upload a la responsabilité de créer le chemin de fichier et les caractères spéciaux interdit
+ */
 export const upload = async (filePath: string, content: ReadableStream) => {
   await new Upload({
     client: getClient(),
