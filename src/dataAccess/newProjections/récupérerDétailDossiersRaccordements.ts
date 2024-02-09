68b55f194158e4bbac585516ef3c784dfdd6ac38
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-02-09 10:27:56 +0100

    🐛 Bad query name

--- src/dataAccess/newProjections/récupérerDétailDossiersRaccordements.ts
+++ src/dataAccess/newProjections/récupérerDétailDossiersRaccordements.ts
@@ -6,8 +6,8 @@ import { IdentifiantProjet } from '@potentiel-domain/common';
 export const récupérerDétailDossiersRaccordements: RécupérerDétailDossiersRaccordements = async (
   identifiantProjet: IdentifiantProjet.ValueType,
 ) => {
-  const { dossiers } = await mediator.send<Raccordement.ListerDossierRaccordementQuery>({
-    type: 'LISTER_DOSSIER_RACCORDEMENT_QUERY',
+  const { dossiers } = await mediator.send<Raccordement.ConsulterRaccordementQuery>({
+    type: 'CONSULTER_RACCORDEMENT_QUERY',
     data: { identifiantProjetValue: identifiantProjet.formatter() },
   });
   return dossiers;
