4c22e2b98d2470c37044f792eff3ca8c4bf4e6df
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-02-23 11:26:42 +0100

    ✨ Add use case to demander recours

---
+++ packages/domain/éliminé/src/recours/demander/demanderRecours.usecase.ts
@@ -0,0 +1,71 @@
+import { Message, MessageHandler, mediator } from 'mediateur';
+import { DemanderRecoursCommand } from './demanderRecours.command';
+import { DocumentProjet, EnregistrerDocumentProjetCommand } from '@potentiel-domain/document';
+import { DateTime, IdentifiantProjet } from '@potentiel-domain/common';
+import { IdentifiantUtilisateur } from '@potentiel-domain/utilisateur';
+import * as TypeDocumentRecours from '../typeDocumentRecours.valueType';
+
+// TODO :
+// Pour tout type de demande, il faut vérifier que le CDC permet une demande via Potentiel
+// nous avons besoin du CDC actuel et de l'AO pour vérifier cela
+
+export type DemanderRecoursUseCase = Message<
+  'DEMANDER_RECOURS_USECASE',
+  {
+    dateDemandeValue: string;
+    identifiantUtilisateurValue: string;
+    identifiantProjetValue: string;
+    pièceJustificativeValue?: {
+      content: ReadableStream;
+      format: string;
+    };
+    raisonValue: string;
+  }
+>;
+
+export const registerDemanderRecoursUseCase = () => {
+  const runner: MessageHandler<DemanderRecoursUseCase> = async ({
+    dateDemandeValue,
+    identifiantProjetValue,
+    pièceJustificativeValue,
+    identifiantUtilisateurValue,
+    raisonValue,
+  }) => {
+    const identifiantProjet = IdentifiantProjet.convertirEnValueType(identifiantProjetValue);
+    const dateDemande = DateTime.convertirEnValueType(dateDemandeValue);
+    const identifiantUtilisateur = IdentifiantUtilisateur.convertirEnValueType(
+      identifiantUtilisateurValue,
+    );
+
+    const pièceJustificative = pièceJustificativeValue
+      ? DocumentProjet.convertirEnValueType(
+          identifiantProjetValue,
+          TypeDocumentRecours.pièceJustificative.formatter(),
+          dateDemandeValue,
+          pièceJustificativeValue.format,
+        )
+      : undefined;
+
+    if (pièceJustificative) {
+      await mediator.send<EnregistrerDocumentProjetCommand>({
+        type: 'ENREGISTRER_DOCUMENT_PROJET_COMMAND',
+        data: {
+          content: pièceJustificativeValue!.content,
+          documentProjet: pièceJustificative,
+        },
+      });
+    }
+
+    await mediator.send<DemanderRecoursCommand>({
+      type: 'DEMANDER_RECOURS_COMMAND',
+      data: {
+        dateDemande,
+        raison: raisonValue,
+        identifiantProjet,
+        identifiantUtilisateur,
+        pièceJustificative,
+      },
+    });
+  };
+  mediator.register('DEMANDER_RECOURS_USECASE', runner);
+};
