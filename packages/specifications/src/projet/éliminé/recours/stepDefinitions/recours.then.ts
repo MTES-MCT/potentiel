8cd4523a905eb04c1802ad8ad2c02be96e09b3b9
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-02-23 15:36:22 +0100

    🚧 Start specs implementation for recours

---
+++ packages/specifications/src/projet/éliminé/recours/stepDefinitions/recours.then.ts
@@ -0,0 +1,200 @@
+import { Then as Alors } from '@cucumber/cucumber';
+import { mediator } from 'mediateur';
+import waitForExpect from 'wait-for-expect';
+
+import { Recours } from '@potentiel-domain/elimine';
+import { ConsulterDocumentProjetQuery } from '@potentiel-domain/document';
+
+import { PotentielWorld } from '../../../../potentiel.world';
+import { convertReadableStreamToString } from '../../../../helpers/convertReadableToString';
+import { NotFoundError } from '@potentiel-domain/core';
+import { expect } from 'chai';
+
+Alors(
+  `le recours du projet éliminé {string} devrait être consultable dans la liste des recours`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+    await waitForExpect(async () => {
+      const {
+        statut: actualStatut,
+        identifiantProjet: actualIdentifiantProjet,
+        demande: {
+          demandéLe: actualDateDemande,
+          demandéPar: actualUtilisateur,
+          raison: actualRaison,
+          piéceJustificative: actualPiéceJustificative,
+        },
+      } = await mediator.send<Recours.ConsulterRecoursQuery>({
+        type: 'CONSULTER_RECOURS_QUERY',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+        },
+      });
+
+      const {
+        dateDemande,
+        utilisateur,
+        raison,
+        pièceJustificative: { content },
+      } = this.eliminéWorld.recoursWorld;
+
+      actualStatut.estÉgaleÀ(Recours.StatutRecours.demandé).should.be.true;
+      actualIdentifiantProjet.estÉgaleÀ(identifiantProjet).should.be.true;
+      actualDateDemande.estÉgaleÀ(dateDemande).should.be.true;
+      actualUtilisateur.estÉgaleÀ(utilisateur).should.be.true;
+      actualRaison.should.be.equal(raison);
+
+      if (actualPiéceJustificative) {
+        const result = await mediator.send<ConsulterDocumentProjetQuery>({
+          type: 'CONSULTER_DOCUMENT_PROJET',
+          data: {
+            documentKey: actualPiéceJustificative.formatter(),
+          },
+        });
+
+        const actualContent = await convertReadableStreamToString(result.content);
+        actualContent.should.be.equal(content);
+      }
+    });
+  },
+);
+
+Alors(
+  `le recours du projet éliminé {string} ne devrait plus exister`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+    await waitForExpect(async () => {
+      try {
+        const result = await mediator.send<Recours.ConsulterRecoursQuery>({
+          type: 'CONSULTER_RECOURS_QUERY',
+          data: {
+            identifiantProjetValue: identifiantProjet.formatter(),
+          },
+        });
+        result.should.be.undefined;
+      } catch (e) {
+        (e as Error).should.be.instanceOf(NotFoundError);
+      }
+    });
+  },
+);
+
+Alors(
+  `le recours du projet éliminé {string} devrait être rejeté`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+    await waitForExpect(async () => {
+      const {
+        statut: actualStatut,
+        identifiantProjet: actualIdentifiantProjet,
+        rejet,
+      } = await mediator.send<Recours.ConsulterRecoursQuery>({
+        type: 'CONSULTER_RECOURS_QUERY',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+        },
+      });
+
+      const {
+        dateRejet,
+        utilisateur,
+        réponseSignée: { content },
+      } = this.eliminéWorld.recoursWorld;
+
+      actualStatut.estÉgaleÀ(Recours.StatutRecours.rejeté).should.be.true;
+      actualIdentifiantProjet.estÉgaleÀ(identifiantProjet).should.be.true;
+      expect(rejet).to.be.not.undefined;
+
+      const actualDateRejet = rejet!.rejetéLe;
+      const actualUtilisateur = rejet!.rejetéPar;
+      const actualRéponseSignée = rejet!.réponseSignée;
+
+      actualDateRejet.estÉgaleÀ(dateRejet).should.be.true;
+      actualUtilisateur.estÉgaleÀ(utilisateur).should.be.true;
+
+      const result = await mediator.send<ConsulterDocumentProjetQuery>({
+        type: 'CONSULTER_DOCUMENT_PROJET',
+        data: {
+          documentKey: actualRéponseSignée.formatter(),
+        },
+      });
+
+      const actualContent = await convertReadableStreamToString(result.content);
+      actualContent.should.be.equal(content);
+    });
+  },
+);
+
+Alors(
+  `le recours du projet éliminé {string} devrait être accordé`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+    await waitForExpect(async () => {
+      const {
+        statut: actualStatut,
+        identifiantProjet: actualIdentifiantProjet,
+        accord,
+      } = await mediator.send<Recours.ConsulterRecoursQuery>({
+        type: 'CONSULTER_RECOURS_QUERY',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+        },
+      });
+
+      const {
+        dateAccord,
+        utilisateur,
+        réponseSignée: { content },
+      } = this.eliminéWorld.recoursWorld;
+
+      actualStatut.estÉgaleÀ(Recours.StatutRecours.accordé).should.be.true;
+      actualIdentifiantProjet.estÉgaleÀ(identifiantProjet).should.be.true;
+      expect(accord).to.be.not.undefined;
+
+      const actualDateRejet = accord!.accordéLe;
+      const actualUtilisateur = accord!.accordéPar;
+      const actualRéponseSignée = accord!.réponseSignée;
+
+      actualDateRejet.estÉgaleÀ(dateAccord).should.be.true;
+      actualUtilisateur.estÉgaleÀ(utilisateur).should.be.true;
+
+      const result = await mediator.send<ConsulterDocumentProjetQuery>({
+        type: 'CONSULTER_DOCUMENT_PROJET',
+        data: {
+          documentKey: actualRéponseSignée.formatter(),
+        },
+      });
+
+      const actualContent = await convertReadableStreamToString(result.content);
+      actualContent.should.be.equal(content);
+    });
+  },
+);
+
+Alors(
+  `le recours du projet éliminé {string} devrait être de nouveau demandé`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+    await waitForExpect(async () => {
+      const {
+        statut: actualStatut,
+        identifiantProjet: actualIdentifiantProjet,
+        rejet,
+      } = await mediator.send<Recours.ConsulterRecoursQuery>({
+        type: 'CONSULTER_RECOURS_QUERY',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+        },
+      });
+
+      actualStatut.estÉgaleÀ(Recours.StatutRecours.demandé).should.be.true;
+      actualIdentifiantProjet.estÉgaleÀ(identifiantProjet).should.be.true;
+      expect(rejet).to.be.undefined;
+    });
+  },
+);
---
+++ packages/specifications/src/projet/éliminé/recours/stepDefinitions/recours.when.ts
@@ -0,0 +1,238 @@
+import { DataTable, When as Quand } from '@cucumber/cucumber';
+import { mediator } from 'mediateur';
+import { Recours } from '@potentiel-domain/elimine';
+import { PotentielWorld } from '../../../../potentiel.world';
+import { convertStringToReadableStream } from '../../../../helpers/convertStringToReadable';
+import { DateTime } from '@potentiel-domain/common';
+import { IdentifiantUtilisateur } from '@potentiel-domain/utilisateur';
+
+Quand(
+  `le porteur demande le recours pour le projet éliminé {string} avec :`,
+  async function (this: PotentielWorld, nomProjet: string, table: DataTable) {
+    try {
+      const exemple = table.rowsHash();
+      const raison = exemple[`La raison du recours`] ?? `La raison du recours`;
+      const format = exemple[`Le format de la pièce justificative`] ?? undefined;
+      const content = exemple[`Le contenu de la pièce justificative`] ?? undefined;
+      const dateDemande = new Date();
+      const email = 'porteur@test.test';
+
+      this.eliminéWorld.recoursWorld.raison = raison;
+      this.eliminéWorld.recoursWorld.dateDemande = DateTime.convertirEnValueType(dateDemande);
+      this.eliminéWorld.recoursWorld.pièceJustificative = {
+        format,
+        content,
+      };
+      this.eliminéWorld.recoursWorld.utilisateur =
+        IdentifiantUtilisateur.convertirEnValueType(email);
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'DEMANDER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          raisonValue: raison,
+          pièceJustificativeValue: format
+            ? {
+                content: convertStringToReadableStream(content),
+                format,
+              }
+            : undefined,
+          dateDemandeValue: dateDemande.toISOString(),
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le porteur demande le recours pour le projet éliminé {string}`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    try {
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'DEMANDER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          raisonValue: `La raison du recours`,
+          dateDemandeValue: new Date().toISOString(),
+          identifiantUtilisateurValue: 'porteur@test.test',
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le porteur annule le recours pour le projet éliminé {string}`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    try {
+      const dateAnnulation = new Date();
+      const email = 'porteur@test.test';
+
+      this.eliminéWorld.recoursWorld.dateAnnulation = DateTime.convertirEnValueType(dateAnnulation);
+      this.eliminéWorld.recoursWorld.utilisateur =
+        IdentifiantUtilisateur.convertirEnValueType(email);
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'ANNULER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          dateAnnulationValue: dateAnnulation.toISOString(),
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le DGEC validateur rejette le recours pour le projet éliminé {string}`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    try {
+      const dateRejet = new Date();
+      const email = 'validateur@test.test';
+
+      this.eliminéWorld.recoursWorld.dateRejet = DateTime.convertirEnValueType(dateRejet);
+      this.eliminéWorld.recoursWorld.réponseSignée = {
+        format: 'text/plain',
+        content: `Le contenu de la réponse signée`,
+      };
+      this.eliminéWorld.recoursWorld.utilisateur =
+        IdentifiantUtilisateur.convertirEnValueType(email);
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'REJETER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          dateRejetValue: dateRejet.toISOString(),
+          réponseSignéeValue: {
+            content: convertStringToReadableStream(`Le contenu de la réponse signée`),
+            format: `text/plain`,
+          },
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le DGEC validateur rejette le recours pour le projet éliminé {string} avec :`,
+  async function (this: PotentielWorld, nomProjet: string, table: DataTable) {
+    try {
+      const exemple = table.rowsHash();
+      const format = exemple[`Le format de la réponse signée`] ?? `Le format de la réponse signée`;
+      const content =
+        exemple[`Le contenu de la réponse signée`] ?? `Le contenu de la réponse signée`;
+      const dateRejet = new Date();
+      const email = 'validateur@test.test';
+
+      this.eliminéWorld.recoursWorld.dateRejet = DateTime.convertirEnValueType(dateRejet);
+      this.eliminéWorld.recoursWorld.réponseSignée = {
+        format,
+        content,
+      };
+      this.eliminéWorld.recoursWorld.utilisateur =
+        IdentifiantUtilisateur.convertirEnValueType(email);
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'REJETER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          dateRejetValue: dateRejet.toISOString(),
+          réponseSignéeValue: {
+            content: convertStringToReadableStream(content),
+            format,
+          },
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le DGEC validateur accorde le recours pour le projet éliminé {string} avec :`,
+  async function (this: PotentielWorld, nomProjet: string, table: DataTable) {
+    try {
+      const exemple = table.rowsHash();
+      const format = exemple[`Le format de la réponse signée`] ?? `Le format de la réponse signée`;
+      const content =
+        exemple[`Le contenu de la réponse signée`] ?? `Le contenu de la réponse signée`;
+      const dateAccord = DateTime.now();
+      const email = 'validateur@test.test';
+
+      this.eliminéWorld.recoursWorld.dateAccord = dateAccord;
+      this.eliminéWorld.recoursWorld.réponseSignée = {
+        format,
+        content,
+      };
+      this.eliminéWorld.recoursWorld.utilisateur =
+        IdentifiantUtilisateur.convertirEnValueType(email);
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'ACCORDER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          dateAccordValue: dateAccord.formatter(),
+          réponseSignéeValue: {
+            content: convertStringToReadableStream(content),
+            format,
+          },
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
+
+Quand(
+  `le DGEC validateur accorde le recours pour le projet éliminé {string}`,
+  async function (this: PotentielWorld, nomProjet: string) {
+    try {
+      const dateAccord = new Date();
+      const email = 'validateur@test.test';
+
+      const { identifiantProjet } = this.eliminéWorld.rechercherEliminéFixture(nomProjet);
+
+      await mediator.send<Recours.RecoursUseCase>({
+        type: 'ACCORDER_RECOURS_USECASE',
+        data: {
+          identifiantProjetValue: identifiantProjet.formatter(),
+          dateAccordValue: dateAccord.toISOString(),
+          réponseSignéeValue: {
+            content: convertStringToReadableStream(`Le contenu de la réponse signée`),
+            format: 'text/plain',
+          },
+          identifiantUtilisateurValue: email,
+        },
+      });
+    } catch (error) {
+      this.error = error as Error;
+    }
+  },
+);
