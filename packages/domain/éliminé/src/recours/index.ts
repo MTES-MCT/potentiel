e5c8d92c73a93628b62fb25e9c6adacac528ef50
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-02-23 11:26:42 +0100

    ✨ Add use case to demander recours

---
+++ packages/domain/éliminé/src/recours/index.ts
@@ -0,0 +1,97 @@
+import { AccorderAbandonUseCase } from './accorder/accorderRecours.usecase';
+import { AnnulerAbandonUseCase } from './annuler/annulerRecours.usecase';
+import { AnnulerRejetAbandonUseCase } from './annulerRejet/annulerRejetAbandon.usecase';
+import { ConfirmerAbandonUseCase } from './confirmer/confirmerAbandon.usecase';
+import {
+  ConsulterAbandonQuery,
+  ConsulterAbandonReadModel,
+} from './consulter/consulterAbandon.query';
+import { DemanderAbandonUseCase } from './demander/demanderRecours.usecase';
+import { DemanderConfirmationAbandonUseCase } from './demanderConfirmation/demanderConfirmationAbandon.usecase';
+import {
+  ListerAbandonsQuery,
+  ListerAbandonReadModel,
+  ListerAbandonsPourPorteurPort,
+  ListerAbandonsPort,
+  RécupérerRégionDrealPort,
+} from './lister/listerAbandon.query';
+import { RejeterAbandonUseCase } from './rejeter/rejeterAbandon.usecase';
+import { DemanderPreuveRecandidatureAbandonUseCase } from './demanderPreuveRecandidature/demanderPreuveRecandidatureAbandon.usecase';
+import { TransmettrePreuveRecandidatureAbandonUseCase } from './transmettre/transmettrePreuveRecandidatureAbandon.usecase';
+import { ListerAbandonsAvecRecandidatureÀRelancerQuery } from './lister/listerAbandonAvecRecandidatureÀRelancer.query';
+import { DétecterAbandonQuery } from './détecter/détecterAbandon.query';
+import {
+  BuildModèleRéponseAbandonPort,
+  GénérerModèleRéponseAbandonQuery,
+  GénérerModèleRéponseAbandonReadModel,
+} from './générerModèleRéponse/générerModèleRéponseAbandon.query';
+
+// Query
+export type AbandonQuery =
+  | DétecterAbandonQuery
+  | ConsulterAbandonQuery
+  | ListerAbandonsQuery
+  | ListerAbandonsAvecRecandidatureÀRelancerQuery
+  | GénérerModèleRéponseAbandonQuery;
+
+export {
+  DétecterAbandonQuery,
+  ConsulterAbandonQuery,
+  ListerAbandonsQuery,
+  ListerAbandonsAvecRecandidatureÀRelancerQuery,
+  GénérerModèleRéponseAbandonQuery,
+};
+
+// ReadModel
+export { ConsulterAbandonReadModel, ListerAbandonReadModel, GénérerModèleRéponseAbandonReadModel };
+
+// UseCases
+export type AbandonUseCase =
+  | AccorderAbandonUseCase
+  | AnnulerAbandonUseCase
+  | AnnulerRejetAbandonUseCase
+  | ConfirmerAbandonUseCase
+  | DemanderAbandonUseCase
+  | DemanderConfirmationAbandonUseCase
+  | RejeterAbandonUseCase
+  | TransmettrePreuveRecandidatureAbandonUseCase
+  | DemanderPreuveRecandidatureAbandonUseCase;
+
+export {
+  AccorderAbandonUseCase,
+  AnnulerAbandonUseCase,
+  AnnulerRejetAbandonUseCase,
+  ConfirmerAbandonUseCase,
+  DemanderAbandonUseCase,
+  DemanderConfirmationAbandonUseCase,
+  RejeterAbandonUseCase,
+};
+
+// Event
+export { AbandonEvent } from './recours.aggregate';
+export { AbandonAnnuléEvent } from './annuler/annulerRecours.behavior';
+export { ConfirmationAbandonDemandéeEvent } from './demanderConfirmation/demanderConfirmationAbandon.behavior';
+export { AbandonConfirméEvent } from './confirmer/confirmerAbandon.behavior';
+export { AbandonRejetéEvent } from './rejeter/rejeterAbandon.behavior';
+export { PreuveRecandidatureDemandéeEvent } from './demanderPreuveRecandidature/demanderPreuveRecandidatureAbandon.behavior';
+export { PreuveRecandidatureTransmiseEvent } from './transmettre/transmettrePreuveRecandidatureAbandon.behavior';
+
+// Register
+export { registerAbandonQueries, registerAbandonUseCases } from './abandon.register';
+
+// ValueTypes
+export * as StatutAbandon from './statutRecours.valueType';
+export * as StatutPreuveRecandidature from './statutPreuveRecandidature.valueType';
+export * as Abandon from './abandon.valueType';
+export * as TypeDocumentRecours from './typeDocumentRecours.valueType';
+
+// Entities
+export * from './abandon.entity';
+
+// Ports
+export {
+  ListerAbandonsPourPorteurPort,
+  BuildModèleRéponseAbandonPort,
+  ListerAbandonsPort,
+  RécupérerRégionDrealPort,
+};
