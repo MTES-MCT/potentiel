2203213033da54e522c7bdc38e716dc38f7ca3d8
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-03-27 10:03:14 +0100

    🚚 Move projectors

---
+++ packages/applications/projectors/src/appel-offre/seed.ts
@@ -0,0 +1,20 @@
+import { appelsOffreData } from '@potentiel-domain/inmemory-referential';
+import { getLogger } from '@potentiel-librairies/monitoring';
+import { removeProjectionByCategory } from '../utils/removeProjectionByCategory';
+import { createProjection } from '../utils/createProjection';
+
+export const seedAppelOffre = async () => {
+  getLogger().info('Starting to seed referential data...');
+
+  // Delete all appel offre projections
+  getLogger().info('Removing all appel offre projections...');
+  await removeProjectionByCategory('appel-offre');
+
+  // Seed all appels offres from inmemory data
+  getLogger().info('Add all appel offre in memory data as projections...');
+
+  for (const appelOffre of appelsOffreData) {
+    const appelOffreReadModelKey: `${string}|${string}` = `appel-offre|${appelOffre.id}`;
+    await createProjection(appelOffreReadModelKey, appelOffre);
+  }
+};
---
+++ packages/applications/projectors/src/index.ts
@@ -0,0 +1,6 @@
+export { seedAppelOffre } from './appel-offre/seed';
+export * as AbandonProjector from './lauréat/abandon.projector';
+export * as GestionnaireRéseauProjector from './réseau/gestionnaireRéseau.projector';
+export * as RaccordementProjector from './réseau/raccordement.projector';
+export * as TâcheProjector from './tâche/tâche.projector';
+export * as GarantiesFinancièreProjector from './lauréat/garantiesFinancières.projector';
