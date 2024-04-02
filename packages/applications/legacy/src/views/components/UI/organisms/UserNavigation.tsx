ed4610ae552cb5ddc2451ef0e655af7e5d0623c6
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-04-02 10:26:59 +0200

    🎨 Conditionnal link on legacy app

--- packages/applications/legacy/src/views/components/UI/organisms/UserNavigation.tsx
+++ packages/applications/legacy/src/views/components/UI/organisms/UserNavigation.tsx
@@ -5,6 +5,10 @@ import { DropdownMenu } from '../molecules/dropdowns/DropdownMenu';
 import { UtilisateurReadModel } from '../../../../modules/utilisateur/récupérer/UtilisateurReadModel';
 import { Routes } from '@potentiel-applications/routes';
 
+const routeListeGarantiesFinancières = process.env.FEATURE_FLAG_SHOW_GARANTIES_FINANCIERES
+  ? Routes.GarantiesFinancières.dépôt.lister
+  : routes.ADMIN_GARANTIES_FINANCIERES;
+
 export const UserNavigation = ({
   user,
   currentPage,
@@ -86,6 +90,12 @@ const MenuAdmin = (currentPage?: string) => (
         Abandons
       </DropdownMenu.DropdownItem>
     </DropdownMenu>
+    <Header.MenuItem
+      href={routeListeGarantiesFinancières}
+      {...(currentPage === 'list-garanties-financieres' && { isCurrent: true })}
+    >
+      Garanties Financières
+    </Header.MenuItem>
     <DropdownMenu buttonChildren={'Imports'}>
       <DropdownMenu.DropdownItem
         href={routes.IMPORT_PROJECTS}
@@ -265,7 +275,7 @@ const MenuDreal = (currentPage?: string) => (
       </DropdownMenu.DropdownItem>
     </DropdownMenu>
     <Header.MenuItem
-      href={routes.ADMIN_GARANTIES_FINANCIERES}
+      href={routeListeGarantiesFinancières}
       {...(currentPage === 'list-garanties-financieres' && { isCurrent: true })}
     >
       Garanties Financières
