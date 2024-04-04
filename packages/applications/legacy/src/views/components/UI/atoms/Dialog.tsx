be175e7e85ca6014e5d2358ca1259a9c10977316
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-04-04 16:38:16 +0200

    ✨ Demande de confirmation de notification d'une période d'un appel d'offres (#1709)
    * ✨ Demande de confirmation de notification d'une période d'un appel d'offres
    
    * ✨ Add checkbox to enable the notify button

--- packages/applications/legacy/src/views/components/UI/atoms/Dialog.tsx
+++ packages/applications/legacy/src/views/components/UI/atoms/Dialog.tsx
@@ -8,7 +8,7 @@ export const Dialog = ({ open, children }: ComponentProps<'dialog'>) => (
     } z-50 fixed left-0 top-0 w-screen h-screen m-0 p-0 border-none bg-grey-425-base bg-opacity-70`}
   >
     <div className="fixed bottom-0 md:relative md:m-auto p-0 w-screen md:w-2/5 h-fit max-h-screen bg-white">
-      <div className="px-4 md:px-8 py-14 md:py-16">{children}</div>
+      <div className="px-4 md:px-8 py-4 md:py-6">{children}</div>
     </div>
   </dialog>
 );
