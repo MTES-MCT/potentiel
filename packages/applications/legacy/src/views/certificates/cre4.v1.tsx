e794309bbb377bc30af8356c6751c3b038b90987
Hanaë <hanae.yamashita@lilo.org>
2024-04-09 18:00:05 +0200

    ✨ (Motif élimination projet): utiliser motif du fichier de candidats

--- packages/applications/legacy/src/views/certificates/cre4.v1.tsx
+++ packages/applications/legacy/src/views/certificates/cre4.v1.tsx
@@ -7,7 +7,7 @@ import { formatDate } from '../../helpers/formatDate';
 import { ProjectDataForCertificate } from '../../modules/project/dtos';
 import { IllegalProjectStateError } from '../../modules/project/errors';
 import { OtherError } from '../../modules/shared';
-import { formatNumber, getNoteThreshold } from './helpers';
+import { formatNumber } from './helpers';
 import { Validateur } from '@potentiel-domain/appel-offre';
 
 dotenv.config();
@@ -300,38 +300,9 @@ const Elimine = (project: ProjectDataForCertificate) => {
           marginTop: 10,
         }}
       >
-        {project.motifsElimination === 'Au-dessus de Pcible'
-          ? "À la suite de l'instruction par les services de la Commission de régulation de l’énergie, je suis au regret de vous informer que votre offre a été classée au-delà de la puissance offerte pour cette période de candidature" +
-            (appelOffre.familles.length ? ' dans la famille concernée' : '') +
-            '. Votre offre a en effet obtenu une note de ' +
-            formatNumber(project.note) +
-            " points alors qu'à la suite du classement des dossiers, il apparaît que le volume appelé" +
-            (appelOffre.familles.length ? ' pour cette famille' : '') +
-            (appelOffre.familles.length && appelOffre.afficherPhraseRegionImplantation
-              ? ', et'
-              : '') +
-            (appelOffre.afficherPhraseRegionImplantation
-              ? ' pour la région d’implantation du projet définis au 1.2.2 du cahier des charges'
-              : '') +
-            ' a été atteint avec les offres ayant des notes supérieures à ' +
-            formatNumber(getNoteThreshold(project)) +
-            ' points. Par conséquent, cette offre n’a pas été retenue.'
-          : project.motifsElimination === 'Déjà lauréat - Non instruit'
-          ? "À la suite de l'instruction par les services de la Commission de régulation de l’énergie, je suis au regret de vous informer que votre offre n'a pas été retenue, elle avait déjà été désignée lauréate au cours d'un précédent appel d'offres."
-          : project.motifsElimination.includes('20%') &&
-            project.motifsElimination.includes('compétitivité')
-          ? "À la suite de l'instruction par les services de la Commission de régulation de l’énergie, je suis au regret de vous informer que votre offre a été classée au-delà de la puissance maximale que la Ministre a décidé de retenir afin de préserver la compétitivité de l’appel d’offres en application des dispositions du(des) paragraphe(s) " +
-            appelOffre.paragrapheClauseCompetitivite +
-            ' du cahier des charges. Ainsi, ' +
-            (appelOffre.familles.length ? 'pour chaque famille,' : '') +
-            ' seuls 80 % des projets les mieux notés ont été retenus. Votre offre a en effet obtenu une note de ' +
-            formatNumber(project.note) +
-            ' points alors que la sélection des offres s’est faite jusqu’à la note de ' +
-            formatNumber(getNoteThreshold(project)) +
-            ' points. Par conséquent, cette offre n’a pas été retenue.'
-          : "À la suite de l'instruction par les services de la Commission de régulation de l’énergie, je suis au regret de vous informer que votre offre a été éliminée pour le motif suivant : « " +
-            project.motifsElimination +
-            ' ». Par conséquent, cette offre n’a pas été retenue.'}
+        À la suite de l'instruction par les services de la Commission de régulation de l’énergie, je
+        suis au regret de vous informer que votre offre a été éliminée pour le motif suivant : «
+        {project.motifsElimination}». Par conséquent, cette offre n’a pas été retenue.
       </Text>
       <Text style={{ fontSize: 10, textAlign: 'justify', marginTop: 10 }}>
         Vous avez la possibilité de contester la présente décision auprès du tribunal administratif
