505d30e2c827e965eb9a7fc7720c2fa2b6eadab6
VioMrqs <violette.marquis@gmail.com>
2024-04-17 15:11:47 +0200

    ✅ Test v2

--- packages/applications/ssr/src/utils/displayDate.test.ts
+++ packages/applications/ssr/src/utils/displayDate.test.ts
@@ -1,45 +1,49 @@
 import { describe, it } from 'node:test';
 
-import { expect } from 'chai';
+import { should } from 'chai';
 
-import { FormattedForPageDate, displayDate } from './displayDate';
+import { displayDate } from './displayDate';
 
 const expected = '17/04/2023';
+
+should();
 
 describe('displayDate', () => {
   it('should format a valid date string', () => {
     const validDateString = '2023-04-17T12:00:00Z';
 
-    const formattedDate: FormattedForPageDate = displayDate(validDateString);
+    const actual = displayDate(validDateString);
 
-    expect(formattedDate).to.equal(expected);
+    actual.should.equal(expected);
   });
 
   it('should return the same formatted date if it is already in the correct format', () => {
-    const formattedDate: FormattedForPageDate = displayDate('17/04/2023');
+    const actual = displayDate('17/04/2023');
 
-    expect(formattedDate).to.equal(expected);
+    actual.should.equal(expected);
   });
 
   it('should throw an error for an invalid date string', () => {
     const invalidDateString = 'not a valid date';
 
-    expect(() => displayDate(invalidDateString)).to.throw();
+    const actual = displayDate(invalidDateString);
+
+    actual.should.Throw();
   });
 
   it('should format a Date object', () => {
     const date = new Date(2023, 3, 17);
 
-    const formattedDate: FormattedForPageDate = displayDate(date);
+    const actual = displayDate(date);
 
-    expect(formattedDate).to.equal(expected);
+    actual.should.equal(expected);
   });
 
   it('should format a number representing milliseconds since Unix epoch', () => {
     const timestamp = 1678545600000;
 
-    const formattedDate: FormattedForPageDate = displayDate(timestamp);
+    const actual = displayDate(timestamp);
 
-    expect(formattedDate).to.equal(expected);
+    actual.should.equal(expected);
   });
 });
