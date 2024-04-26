64982e6f672f4a61e0faf0d59f8fd3ba93a4e300
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-04-26 16:20:21 +0200

    ♻️ Supprimer dépot gf w/ new system for modal

--- packages/applications/ssr/src/components/pages/garanties-financières/dépôt/modifier/supprimer/SupprimerDépôtEnCoursGarantiesFinancières.tsx
+++ packages/applications/ssr/src/components/pages/garanties-financières/dépôt/modifier/supprimer/SupprimerDépôtEnCoursGarantiesFinancières.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import Button from '@codegouvfr/react-dsfr/Button';
 
 import { Routes } from '@potentiel-applications/routes';
 
-import { ButtonWithFormInModal } from '@/components/molecules/ButtonWithFormInModal';
+import { ModalWithForm } from '@/components/molecules/ModalWithForm';
 
 import { supprimerDépôtEnCoursGarantiesFinancièresAction } from './supprimerDépôtEnCoursGarantiesFinancières.action';
 
@@ -16,27 +18,37 @@ export const SupprimerDépôtEnCoursGarantiesFinancières = ({
   identifiantProjet,
 }: SupprimerDépôtEnCoursGarantiesFinancièresProps) => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <ButtonWithFormInModal
-      name="Supprimer"
-      // priority="primary"
-      description="Supprimer les garanties financières en attente de validation"
-      yesNo
-      form={{
-        id: 'supprimer-garanties-financieres-a-traiter-form',
-        method: 'post',
-        encType: 'multipart/form-data',
-        omitMandatoryFieldsLegend: true,
-        onSuccess: () => router.push(Routes.GarantiesFinancières.détail(identifiantProjet)),
-        action: supprimerDépôtEnCoursGarantiesFinancièresAction,
-        children: (
-          <>
-            <p className="mt-3">Êtes-vous sûr de vouloir supprimer ces garanties financières ? </p>
-            <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
-          </>
-        ),
-      }}
-    ></ButtonWithFormInModal>
+    <>
+      <Button priority="secondary" onClick={() => setIsOpen(true)}>
+        Supprimer
+      </Button>
+
+      <ModalWithForm
+        title="Supprimer les garanties financières en attente de validation"
+        acceptButtonLabel="Oui"
+        rejectButtonLabel="Non"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        form={{
+          id: 'supprimer-garanties-financieres-a-traiter-form',
+          method: 'post',
+          encType: 'multipart/form-data',
+          omitMandatoryFieldsLegend: true,
+          onSuccess: () => router.push(Routes.GarantiesFinancières.détail(identifiantProjet)),
+          action: supprimerDépôtEnCoursGarantiesFinancièresAction,
+          children: (
+            <>
+              <p className="mt-3">
+                Êtes-vous sûr de vouloir supprimer ces garanties financières ?{' '}
+              </p>
+              <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+            </>
+          ),
+        }}
+      />
+    </>
   );
 };
