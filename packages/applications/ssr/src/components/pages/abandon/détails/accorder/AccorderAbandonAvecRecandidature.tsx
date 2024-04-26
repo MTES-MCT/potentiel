bd0cdd1eab9ce4b07d9f85bbfdaa0ea89f2ebd47
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-04-26 15:43:31 +0200

    ♻️ Accorder abandon avec recandidature w/ new system for modal

--- packages/applications/ssr/src/components/pages/abandon/détails/accorder/AccorderAbandonAvecRecandidature.tsx
+++ packages/applications/ssr/src/components/pages/abandon/détails/accorder/AccorderAbandonAvecRecandidature.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import Button from '@codegouvfr/react-dsfr/Button';
+import { useState } from 'react';
 
 import { Routes } from '@potentiel-applications/routes';
 
-import { ButtonWithFormInModal } from '@/components/molecules/ButtonWithFormInModal';
+import { ModalWithForm } from '@/components/molecules/ModalWithForm';
 
 import { accorderAbandonAvecRecandidatureAction } from './accorderAbandonAvecRecandidature.action';
 
@@ -16,27 +18,35 @@ export const AccorderAbandonAvecRecandidature = ({
   identifiantProjet,
 }: AccorderAbandonAvecRecandidatureFormProps) => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <ButtonWithFormInModal
-      // className="w-full"
-      name="Accorder"
-      yesNo
-      description="Accorder l'abandon"
-      form={{
-        id: 'accorder-abandon-avec-recandidature-form',
-        action: accorderAbandonAvecRecandidatureAction,
-        method: 'post',
-        encType: 'multipart/form-data',
-        omitMandatoryFieldsLegend: true,
-        onSuccess: () => router.push(Routes.Abandon.détail(identifiantProjet)),
-        children: (
-          <>
-            <p className="mt-3">Êtes-vous sûr de vouloir accorder cet abandon ?</p>
-            <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
-          </>
-        ),
-      }}
-    ></ButtonWithFormInModal>
+    <>
+      <Button priority="secondary" onClick={() => setIsOpen(true)} className="w-full text-center">
+        Accorder
+      </Button>
+
+      <ModalWithForm
+        title="Accorder l'abandon"
+        acceptButtonLabel="Oui"
+        rejectButtonLabel="Non"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        form={{
+          id: 'accorder-abandon-avec-recandidature-form',
+          action: accorderAbandonAvecRecandidatureAction,
+          method: 'post',
+          encType: 'multipart/form-data',
+          omitMandatoryFieldsLegend: true,
+          onSuccess: () => router.push(Routes.Abandon.détail(identifiantProjet)),
+          children: (
+            <>
+              <p className="mt-3">Êtes-vous sûr de vouloir accorder cet abandon ?</p>
+              <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+            </>
+          ),
+        }}
+      />
+    </>
   );
 };
