39899d584356749ad1e864b9e18d27a763043f57
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-04-26 15:59:06 +0200

    ♻️ Confirmer abandon w/ new system for modal

--- packages/applications/ssr/src/components/pages/abandon/détails/confirmer/ConfirmerAbandon.tsx
+++ packages/applications/ssr/src/components/pages/abandon/détails/confirmer/ConfirmerAbandon.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import Button from '@codegouvfr/react-dsfr/Button';
+import { useState } from 'react';
 
-import { ButtonWithFormInModal } from '@/components/molecules/ButtonWithFormInModal';
+import { ModalWithForm } from '@/components/molecules/ModalWithForm';
 
 import { confirmerAbandonAction } from './confirmerAbandon.action';
 
@@ -12,24 +14,38 @@ type ConfirmerAbandonFormProps = {
 
 export const ConfirmerAbandon = ({ identifiantProjet }: ConfirmerAbandonFormProps) => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <ButtonWithFormInModal
-      name="Confirmer"
-      description="Confirmer l'abandon"
-      form={{
-        action: confirmerAbandonAction,
-        method: 'post',
-        id: 'confirmer-abandon-form',
-        omitMandatoryFieldsLegend: true,
-        onSuccess: () => router.refresh(),
-        children: (
-          <>
-            <p className="mt-3">Êtes-vous sûr de vouloir confirmer cet abandon ?</p>
-            <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
-          </>
-        ),
-      }}
-    ></ButtonWithFormInModal>
+    <>
+      <Button
+        priority="secondary"
+        onClick={() => setIsOpen(true)}
+        className="block w-full text-center"
+      >
+        Confirmer
+      </Button>
+
+      <ModalWithForm
+        title="Confirmer l'abandon"
+        acceptButtonLabel="Oui"
+        rejectButtonLabel="Non"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        form={{
+          action: confirmerAbandonAction,
+          method: 'post',
+          id: 'confirmer-abandon-form',
+          omitMandatoryFieldsLegend: true,
+          onSuccess: () => router.refresh(),
+          children: (
+            <>
+              <p className="mt-3">Êtes-vous sûr de vouloir confirmer cet abandon ?</p>
+              <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+            </>
+          ),
+        }}
+      />
+    </>
   );
 };
