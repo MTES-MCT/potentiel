0782d2278aed16df1b7274ba9ebe833e667bf5bd
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-04-26 09:13:02 +0200

    🔥 Remove the notification part

--- packages/applications/bootstrap/src/setupEliminé.ts
+++ packages/applications/bootstrap/src/setupEliminé.ts
@@ -7,7 +7,6 @@ import {
   récupérerRégionDrealAdapter,
 } from '@potentiel-infrastructure/domain-adapters';
 import { RecoursProjector } from '@potentiel-applications/projectors';
-import { RecoursNotification } from '@potentiel-applications/notifications';
 
 export const setupEliminé = async () => {
   registerEliminéUseCases({
@@ -22,19 +21,6 @@ export const setupEliminé = async () => {
   });
 
   RecoursProjector.register();
-  RecoursNotification.register();
-
-  const unsubscribeRecoursNotification = await subscribe<RecoursNotification.SubscriptionEvent>({
-    name: 'notifications',
-    streamCategory: 'recours',
-    eventType: ['RecoursDemandé-V1', 'RecoursAccordé-V1', 'RecoursAnnulé-V1', 'RecoursRejeté-V1'],
-    eventHandler: async (event) => {
-      await mediator.publish<RecoursNotification.Execute>({
-        type: 'System.Notification.Eliminé.Recours',
-        data: event,
-      });
-    },
-  });
 
   const unsubscribeRecoursProjector = await subscribe<RecoursProjector.SubscriptionEvent>({
     name: 'projector',
@@ -55,7 +41,6 @@ export const setupEliminé = async () => {
   });
 
   return async () => {
-    await unsubscribeRecoursNotification();
     await unsubscribeRecoursProjector();
   };
 };
--- packages/applications/notifications/src/index.ts
+++ packages/applications/notifications/src/index.ts
@@ -1,3 +1,2 @@
 export * as AbandonNotification from './subscribers/lauréat/abandon.notification';
 export * as GarantiesFinancièresNotification from './subscribers/lauréat/garantiesFinancières.notification';
-export * as RecoursNotification from './subscribers/éliminé/recours.notification';
