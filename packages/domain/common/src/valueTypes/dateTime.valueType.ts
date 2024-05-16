fc2aa4fd3b77690e9e822462b1115e1e17b96551
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-05-16 15:29:20 +0200

    ✨ DateTime.ajouterNombreDeMois() value type helper

--- packages/domain/common/src/valueTypes/dateTime.valueType.ts
+++ packages/domain/common/src/valueTypes/dateTime.valueType.ts
@@ -10,6 +10,7 @@ export type ValueType = ReadonlyValueType<{
   estAntérieurÀ(dateTime: ValueType): boolean;
   estUltérieureÀ(dateTime: ValueType): boolean;
   nombreJoursÉcartAvec(dateTime: ValueType): number;
+  ajouterNombreDeMois(nombreDeMois: number): Date;
   formatter(): RawType;
 }>;
 
@@ -43,6 +44,9 @@ export const convertirEnValueType = (value: Date | string): ValueType => {
     nombreJoursÉcartAvec(dateTime) {
       const écart = differenceInDays(this.date, dateTime.date);
       return Math.abs(écart); // Peu importe si la date est avant ou aprés, on veut l'écart positif.
+    },
+    ajouterNombreDeMois(nombreDeMois) {
+      return new Date(this.date.setMonth(this.date.getMonth() + nombreDeMois));
     },
   };
 };
