89c88b164dec9f28874f71348d92f6c0c7377c90
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-05-17 17:03:47 +0200

    ♻️ Improve isNone typing

--- packages/libraries/monads/src/option/none.ts
+++ packages/libraries/monads/src/option/none.ts
@@ -1,3 +1,4 @@
+import { Option } from './option';
 // Initiallement none était un Symbol, cependant le symbol ne pas être unique côté client et server dans un contexte SSR,
 // car l'hydratation côté Front nécessite la sérialization de la variable. Le symbol est donc différent côté client et server.
 // Cela pose probléme pour le control flow une fois les données récupérer dans une page. Donc le type valeur de none a été
@@ -6,6 +7,6 @@ export const none = `__none__`;
 
 export type None = typeof none;
 
-export const isNone = (value: any): value is None => {
+export const isNone = <TType>(value: Option<TType>): value is None => {
   return value === none;
 };
