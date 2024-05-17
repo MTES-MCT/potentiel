34c9ded3204f8ecbb927ae95655608e8c295f0f6
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-05-17 16:34:02 +0200

    ♻️ Make none serializable

--- packages/libraries/monads/src/option/none.ts
+++ packages/libraries/monads/src/option/none.ts
@@ -1,4 +1,8 @@
-export const none = Symbol('none');
+// Initiallement none était un Symbol, cependant le symbol ne pas être unique côté client et server dans un contexte SSR,
+// car l'hydratation côté Front nécessite la sérialization de la variable. Le symbol est donc différent côté client et server.
+// Cela pose probléme pour le control flow une fois les données récupérer dans une page. Donc le type valeur de none a été
+// fixé à cette valeur pour garantir l'isomorphisme de la libraries et débloquer le control flow partout.
+export const none = `__none__`;
 
 export type None = typeof none;
 
