83ad68b5b2a67aba5afeb39d20ae023b03af797b
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-05-20 10:39:07 +0200

    🏷️ Update PlainType to handle ReadonlyArray correctly

--- packages/domain/core/src/plainType.ts
+++ packages/domain/core/src/plainType.ts
@@ -1,10 +1,10 @@
 import { Option } from '@potentiel-libraries/monads';
 
 export type PlainType<T> = {
-  [K in keyof T as T[K] extends Function ? never : K]: T[K] extends
-    | (infer U)[]
-    | ReadonlyArray<infer U>
+  [K in keyof T as T[K] extends Function ? never : K]: T[K] extends Array<infer U>
     ? Array<PlainType<U>>
+    : T[K] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<PlainType<U>>
     : T[K] extends Record<string, unknown>
     ? PlainType<T[K]>
     : T[K] extends Option.Type<infer U>
