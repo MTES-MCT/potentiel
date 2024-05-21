40766647709f5d2110b5826074e2e403f0cf253c
Hubert Moncenis <hubmoncenis@gmail.com>
2024-05-21 15:54:09 +0200

    🔥 Remove useless legacy GF code (#1808)

--- packages/applications/legacy/src/modules/project/errors/PasDePermissionPourChangerTypeGFError.ts
+++
@@ -1,7 +0,0 @@
-import { DomainError } from '../../../core/domain';
-
-export class PasDePermissionPourChangerTypeGFError extends DomainError {
-  constructor() {
-    super("Vous n'avez pas l'autorisation pour ajouter le type de garanties financières.");
-  }
-}
