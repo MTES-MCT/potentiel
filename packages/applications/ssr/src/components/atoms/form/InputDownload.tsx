cb21067a1b04355d47d7dddcc4f9adca78f2e1b5
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-06-18 09:16:12 +0200

    🚧 Start renaming component...

--- packages/applications/ssr/src/components/atoms/form/InputDownload.tsx
+++ packages/applications/ssr/src/components/atoms/form/InputDownload.tsx
@@ -1,23 +1,19 @@
 import { FC } from 'react';
 import { Download, DownloadProps } from '@codegouvfr/react-dsfr/Download';
 
-type InputDownloadProps = DownloadProps & {
-  ariaLabel: string;
-};
+export type DownloadDocumentProps = DownloadProps;
 
 /**
  * @description Ce composant est une surcouche du composant Download de react-dsfr pour ajouter l'attribut target blan et que le document se télécharge directement dans un nouvel onglet, sans preview
  * @param props cf https://components.react-dsfr.codegouv.studio/?path=/docs/components-download--default
  */
-export const InputDownload: FC<InputDownloadProps> = (props) => (
+export const DownloadDocument: FC<DownloadDocumentProps> = ({ linkProps, ...props }) => (
   <Download
-    {...{
-      ...props,
-      linkProps: {
-        href: props.linkProps.href,
-        'aria-label': `${props.ariaLabel} (dans un nouvel onglet)`,
-        target: '_blank',
-      },
+    {...props}
+    linkProps={{
+      ...linkProps,
+      'aria-label': `${linkProps['aria-label'] ?? props.label} (dans un nouvel onglet)`,
+      target: '_blank',
     }}
   />
 );
