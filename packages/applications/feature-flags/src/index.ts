bad1335c5efe4bf7d892cf58a29114af7887ef88
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-06-19 12:16:27 +0200

    🎨 Comments

--- packages/applications/feature-flags/src/index.ts
+++ packages/applications/feature-flags/src/index.ts
@@ -2,6 +2,7 @@ type FeatureFlags = {
   SHOW_MAINLEVEE_GARANTIES_FINANCIERES: boolean;
 };
 
+// env variables should be duplicated in both .env.template (and .env obviously) using the prefix NEXT_PUBLIC for the ssr folder
 export const featureFlags: FeatureFlags = {
   SHOW_MAINLEVEE_GARANTIES_FINANCIERES:
     process.env.SHOW_MAINLEVEE_GARANTIES_FINANCIERES === 'true' ||
