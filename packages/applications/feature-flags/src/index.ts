70251fa0720df692c68d8761671d23130dc82571
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-06-19 12:41:48 +0200

    🎨 Ben review

--- packages/applications/feature-flags/src/index.ts
+++ packages/applications/feature-flags/src/index.ts
@@ -2,10 +2,13 @@ type FeatureFlags = {
   SHOW_MAINLEVEE_GARANTIES_FINANCIERES: boolean;
 };
 
-// env variables should be duplicated in both .env.template (and .env obviously) using the prefix NEXT_PUBLIC for the ssr folder
+/*
+env variables should be prefixed by NEXT_PUBLIC and duplicated in both .env.template (and .env obviously)
+this enable the var to be used in a browser environment for Next app
+doc: nextjs.org/docs/pages/building-your-application/configuring/environment-variables
+*/
 export const featureFlags: FeatureFlags = {
   SHOW_MAINLEVEE_GARANTIES_FINANCIERES:
-    process.env.SHOW_MAINLEVEE_GARANTIES_FINANCIERES === 'true' ||
     process.env.NEXT_PUBLIC_SHOW_MAINLEVEE_GARANTIES_FINANCIERES === 'true',
 };
 
