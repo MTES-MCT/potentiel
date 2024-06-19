d9771ef8efc976316de91cbd7bae236f1c9fc73c
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-06-19 14:19:06 +0200

    🎨 Test

--- packages/applications/feature-flags/src/index.ts
+++ packages/applications/feature-flags/src/index.ts
@@ -9,7 +9,8 @@ doc: nextjs.org/docs/pages/building-your-application/configuring/environment-var
 */
 export const featureFlags: FeatureFlags = {
   SHOW_MAINLEVEE_GARANTIES_FINANCIERES:
-    process.env.NEXT_PUBLIC_SHOW_MAINLEVEE_GARANTIES_FINANCIERES === 'true',
+    process.env.NEXT_PUBLIC_SHOW_MAINLEVEE_GARANTIES_FINANCIERES === 'true' ||
+    process.env.APPLICATION_STAGE === 'test',
 };
 
 export const showMainlevéeGarantiesFinancières = featureFlags.SHOW_MAINLEVEE_GARANTIES_FINANCIERES;
