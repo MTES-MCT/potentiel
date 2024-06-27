2cc6a46f07c5842e8e2fef6fbb588db27558b63b
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-06-27 18:12:48 +0200

    🚑️ Désactiver les vérifications de chemin S3 dans le legacy (#1907)
    🚑️ Désactiver les check de chemin S3 dans le legacy

--- packages/applications/legacy/src/infra/file/S3FileStorageService.ts
+++ packages/applications/legacy/src/infra/file/S3FileStorageService.ts
@@ -1,16 +1,10 @@
 import AWS from 'aws-sdk';
-import { wrapInfra, err, ok, okAsync, Result } from '../../core/utils';
+import { wrapInfra, ok, okAsync, Result } from '../../core/utils';
 import { FileStorageService } from '../../modules/file';
 
 class WrongIdentifierFormat extends Error {
   constructor() {
     super('Identifier is not recognized as S3.');
-  }
-}
-
-class WrongBucket extends Error {
-  constructor() {
-    super('The S3 bucket does not match the current bucket.');
   }
 }
 
@@ -21,16 +15,6 @@ function makeIdentifier(filePath: string, bucket: string): string {
 }
 
 function parseIdentifier(fileId: string, _bucket: string): Result<string, WrongIdentifierFormat> {
-  if (!fileId || fileId.indexOf(IDENTIFIER_PREFIX) !== 0) {
-    return err(new WrongIdentifierFormat());
-  }
-
-  const bucket = fileId.substring(IDENTIFIER_PREFIX.length + 1, fileId.lastIndexOf(':'));
-
-  if (bucket !== _bucket) {
-    return err(new WrongBucket());
-  }
-
   return ok(fileId.substring(fileId.lastIndexOf(':') + 1));
 }
 
