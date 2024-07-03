8b8dd48d99632fee14d5424e240d477ef248bbe0
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-07-03 15:04:53 +0200

    🚧 Start changing entity for mainlevée

--- packages/domain/lauréat/src/garantiesFinancières/mainlevée/mainlevéeGarantiesFinancières.entity.ts
+++ packages/domain/lauréat/src/garantiesFinancières/mainlevée/mainlevéeGarantiesFinancières.entity.ts
@@ -1,24 +1,50 @@
 import { Entity } from '@potentiel-domain/core';
 
 export type MainlevéeGarantiesFinancièresEntity = Entity<
-  'mainlevee-garanties-financieres',
+  'demande-mainlevee',
   {
     identifiantProjet: string;
-    nomProjet: string;
-    régionProjet: string;
+
     appelOffre: string;
     période: string;
-    famille?: string;
+    famille: string;
 
-    statut: string;
+    nomProjet: string;
+    régionProjet: string;
+
+    statut: Demandé | EnCoursDInstruction | Accordé | Rejeté;
+
     motif: string;
-    demande: { demandéeLe: string; demandéePar: string };
-    instruction?: { démarréeLe: string; démarréePar: string };
-    accord?: { accordéeLe: string; accordéePar: string; courrierAccord: { format: string } };
 
-    dernièreMiseÀJour: {
-      date: string;
-      par: string;
-    };
+    demandéLe: string;
+    demandéPar: string;
   }
 >;
+
+type Demandé = {
+  type: 'demandé';
+};
+
+type EnCoursDInstruction = {
+  type: 'en-cours-d-instruction';
+  instructionDémarréPar: string;
+  instructionDémarréLe: string;
+};
+
+type Accordé = {
+  type: 'accordé';
+  accordéLe: string;
+  accordéPar: string;
+  courrierRéponse: {
+    format: string;
+  };
+};
+
+type Rejeté = {
+  type: 'rejeté';
+  rejetéLe: string;
+  rejetéPar: string;
+  courrierRéponse: {
+    format: string;
+  };
+};
