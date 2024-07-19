f504b9174f755d4b5e152a5d2d2a2eec6102ad6c
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-07-19 17:08:38 +0200

    🎨 Retour review changer extends de l'erreur AucunesGarantiesFinancièresValidéesError

--- packages/domain/lauréat/src/garantiesFinancières/garantiesFinancièresActuelles/aucunesGarantiesFinancièresValidéesError.ts
+++ packages/domain/lauréat/src/garantiesFinancières/garantiesFinancièresActuelles/aucunesGarantiesFinancièresValidéesError.ts
@@ -1,6 +1,6 @@
-import { AggregateNotFoundError } from '@potentiel-domain/core';
+import { InvalidOperationError } from '@potentiel-domain/core';
 
-export class AucunesGarantiesFinancièresValidéesError extends AggregateNotFoundError {
+export class AucunesGarantiesFinancièresValidéesError extends InvalidOperationError {
   constructor() {
     super(`Il n'y a aucunes garanties financières validées pour ce projet`);
   }
