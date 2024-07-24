d44118244a66b2f4fd284066e26e8ebcf4cdf3ae
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-07-24 11:38:35 +0200

    🚧 Test front again

---
+++ packages/applications/ssr/src/app/global-error.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Alert from '@codegouvfr/react-dsfr/Alert';
+import * as Sentry from '@sentry/nextjs';
+import { useEffect } from 'react';
+
+import { DefaultError } from '../components/pages/custom-error/DefaultError';
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    console.log(error);
+    console.error(error);
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <Alert
+          severity="warning"
+          title="Erreur dans l'application"
+          className="mb-6"
+          description={<DefaultError />}
+        />
+      </body>
+    </html>
+  );
+}
