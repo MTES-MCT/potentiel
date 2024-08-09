e4f59f6f1d6c9e84326f14c6f501c6d679ead885
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-08-09 10:05:19 +0200

    🚚 Move schema next to the action and fix parseCsv type

--- packages/applications/ssr/src/components/pages/candidature/importer/importerCandidatures.action.ts
+++ packages/applications/ssr/src/components/pages/candidature/importer/importerCandidatures.action.ts
@@ -1,14 +1,14 @@
 'use server';
 
 import * as zod from 'zod';
-// import { mediator } from 'mediateur';
+import { mediator } from 'mediateur';
 
-// import { DomainError } from '@potentiel-domain/core';
-// import { parseCsv } from '@potentiel-libraries/csv';
+import { DomainError } from '@potentiel-domain/core';
+import { parseCsv } from '@potentiel-libraries/csv';
 
-import { FormAction, formAction, FormState } from '@/utils/formAction';
+import { ActionResult, FormAction, formAction, FormState } from '@/utils/formAction';
 
-// import { candidatureSchema } from '../../candidatures/importer/candidature.schema';
+import { candidatureSchema } from './candidature.schema';
 
 export type ImporterCandidaturesState = FormState;
 
@@ -18,63 +18,55 @@ const schema = zod.object({
   fichierImport: zod.instanceof(Blob).refine((data) => data.size > 0),
 });
 
-const action: FormAction<FormState, typeof schema> = async () =>
-  // _,
-  // { fichierImport, appelOffre, periode: période },
-  {
-    // const lines = await parseCsv(fichierImport.stream(), candidatureSchema);
+const action: FormAction<FormState, typeof schema> = async (
+  _,
+  { fichierImport, appelOffre, periode: période },
+) => {
+  const lines = await parseCsv(fichierImport.stream(), candidatureSchema);
 
-    // if (lines.length === 0) {
-    //   return {
-    //     status: 'form-error',
-    //     errors: ['fichierImport'],
-    //   };
-    // }
-
-    // let success: number = 0;
-    // const errors: ActionResult['errors'] = [];
-
-    // for (const line of lines) {
-    //   try {
-    //     await mediator.send<Candidature.InstruireCandidature>({
-    //       type: 'Candidature.UseCase.InstruireCandidature',
-    //       data: {
-    //         appelOffre,
-    //         période,
-    //       },
-    //     });
-
-    //     success++;
-    //   } catch (error) {
-    //     if (error instanceof DomainError) {
-    //       errors.push({
-    //         key: line.nom_projet,
-    //         reason: error.message,
-    //       });
-    //       continue;
-    //     }
-    //     errors.push({
-    //       key: line.nom_projet,
-    //       reason: `Une erreur inconnue empêche l'import des candidatures`,
-    //     });
-    //   }
-    // }
-
-    // return {
-    //   status: 'success',
-    //   result: {
-    //     successCount: success,
-    //     errors,
-    //   },
-    // };
-
+  if (lines.length === 0) {
     return {
-      status: 'success',
-      result: {
-        successCount: 0,
-        errors: [],
-      },
+      status: 'form-error',
+      errors: ['fichierImport'],
     };
+  }
+
+  let success: number = 0;
+  const errors: ActionResult['errors'] = [];
+
+  for (const line of lines) {
+    try {
+      await mediator.send<Candidature.InstruireCandidature>({
+        type: 'Candidature.UseCase.InstruireCandidature',
+        data: {
+          appelOffre,
+          période,
+        },
+      });
+
+      success++;
+    } catch (error) {
+      if (error instanceof DomainError) {
+        errors.push({
+          key: line.nom_projet,
+          reason: error.message,
+        });
+        continue;
+      }
+      errors.push({
+        key: line.nom_projet,
+        reason: `Une erreur inconnue empêche l'import des candidatures`,
+      });
+    }
+  }
+
+  return {
+    status: 'success',
+    result: {
+      successCount: success,
+      errors,
+    },
   };
+};
 
 export const importerCandidaturesAction = formAction(action, schema);
