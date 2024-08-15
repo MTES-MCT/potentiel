e3466f673a0eecd2ec6239b333954c3ae9c64531
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-08-15 15:23:01 +0200

    🎨 La projection candidature retourne plus d'informations

--- packages/applications/projectors/src/subscribers/candidature/candidature.projector.ts
+++ packages/applications/projectors/src/subscribers/candidature/candidature.projector.ts
@@ -1,8 +1,9 @@
 import { Message, MessageHandler, mediator } from 'mediateur';
 
-import { Candidature } from '@potentiel-domain/candidature';
+import { Candidature, HistoriqueAbandon } from '@potentiel-domain/candidature';
 import { RebuildTriggered, Event } from '@potentiel-infrastructure/pg-event-sourcing';
-import { StatutProjet } from '@potentiel-domain/common';
+import { DateTime, StatutProjet } from '@potentiel-domain/common';
+import { GarantiesFinancières } from '@potentiel-domain/laureat';
 
 import { removeProjection } from '../../infrastructure/removeProjection';
 import { upsertProjection } from '../../infrastructure/upsertProjection';
@@ -18,12 +19,30 @@ export const register = () => {
     if (type === 'RebuildTriggered') {
       await removeProjection<Candidature.CandidatureEntity>(`candidature|${payload.id}`);
     } else {
-      const { statut, identifiantProjet, nomProjet } = payload;
+      const {
+        identifiantProjet,
+        statut,
+        typeGarantiesFinancières,
+        historiqueAbandon,
+        dateÉchéanceGf,
+        technologie,
+        ...restPayload
+      } = payload;
 
       const candidatureDefaultValue: Omit<Candidature.CandidatureEntity, 'type'> = {
+        ...restPayload,
         identifiantProjet,
         statut: StatutProjet.convertirEnValueType(statut).statut,
-        nom: nomProjet,
+        typeGarantiesFinancières: typeGarantiesFinancières
+          ? GarantiesFinancières.TypeGarantiesFinancières.convertirEnValueType(
+              typeGarantiesFinancières,
+            ).type
+          : undefined,
+        historiqueAbandon: HistoriqueAbandon.convertirEnValueType(historiqueAbandon).type,
+        dateÉchéanceGf: dateÉchéanceGf
+          ? DateTime.convertirEnValueType(dateÉchéanceGf).formatter()
+          : undefined,
+        technologie: Candidature.Technologie.convertirEnValueType(technologie).type,
       };
 
       switch (type) {
