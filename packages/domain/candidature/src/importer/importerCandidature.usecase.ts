217cd2e852d5df43990fb402f815f8537aaf9922
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-08-16 11:51:50 +0200

    ♻️ Partage type + méthode pour mapper data des usecases

--- packages/domain/candidature/src/importer/importerCandidature.usecase.ts
+++ packages/domain/candidature/src/importer/importerCandidature.usecase.ts
@@ -9,97 +9,92 @@ import { HistoriqueAbandon } from '../candidature';
 
 import { ImporterCandidatureCommand } from './importerCandidature.command';
 
+export type ImporterCandidatureUseCasePayload = {
+  typeGarantiesFinancièresValue?: string;
+  historiqueAbandonValue: string;
+  appelOffreValue: string;
+  périodeValue: string;
+  familleValue: string;
+  numéroCREValue: string;
+  nomProjetValue: string;
+  sociétéMèreValue: string;
+  nomCandidatValue: string;
+  puissanceProductionAnnuelleValue: number;
+  prixReferenceValue: number;
+  noteTotaleValue: number;
+  nomReprésentantLégalValue: string;
+  emailContactValue: string;
+  adresse1Value: string;
+  adresse2Value: string;
+  codePostalValue: string;
+  communeValue: string;
+  statutValue: string;
+  motifÉliminationValue: string;
+  puissanceALaPointeValue: boolean;
+  evaluationCarboneSimplifiéeValue: number;
+  valeurÉvaluationCarboneValue?: number;
+  technologieValue: string;
+  financementCollectifValue: boolean;
+  financementParticipatifValue: boolean;
+  gouvernancePartagéeValue: boolean;
+  dateÉchéanceGfValue?: string;
+  territoireProjetValue: string;
+  détailsValue: Record<string, string>;
+};
+
 export type ImporterCandidatureUseCase = Message<
   'Candidature.UseCase.ImporterCandidature',
-  {
-    typeGarantiesFinancièresValue?: string;
-    historiqueAbandonValue: string;
-    appelOffreValue: string;
-    périodeValue: string;
-    familleValue: string;
-    numéroCREValue: string;
-    nomProjetValue: string;
-    sociétéMèreValue: string;
-    nomCandidatValue: string;
-    puissanceProductionAnnuelleValue: number;
-    prixReferenceValue: number;
-    noteTotaleValue: number;
-    nomReprésentantLégalValue: string;
-    emailContactValue: string;
-    adresse1Value: string;
-    adresse2Value: string;
-    codePostalValue: string;
-    communeValue: string;
-    statutValue: string;
-    motifÉliminationValue: string;
-    puissanceALaPointeValue: boolean;
-    evaluationCarboneSimplifiéeValue: number;
-    valeurÉvaluationCarboneValue?: number;
-    technologieValue: string;
-    financementCollectifValue: boolean;
-    financementParticipatifValue: boolean;
-    gouvernancePartagéeValue: boolean;
-    dateÉchéanceGfValue?: string;
-    territoireProjetValue: string;
-    détailsValue: Record<string, string>;
-  }
+  ImporterCandidatureUseCasePayload
 >;
 
 export const registerImporterCandidatureUseCase = () => {
-  const handler: MessageHandler<ImporterCandidatureUseCase> = async (payload) => {
-    const { appelOffreValue, périodeValue, familleValue, numéroCREValue } = payload;
-    const {
-      statutValue,
-      dateÉchéanceGfValue,
-      technologieValue,
-      historiqueAbandonValue,
-      typeGarantiesFinancièresValue,
-    } = payload;
-    await mediator.send<ImporterCandidatureCommand>({
+  const handler: MessageHandler<ImporterCandidatureUseCase> = async (payload) =>
+    mediator.send<ImporterCandidatureCommand>({
       type: 'Candidature.Command.ImporterCandidature',
-      data: {
-        identifiantProjet: IdentifiantProjet.convertirEnValueType(
-          `${appelOffreValue}#${périodeValue}#${familleValue}#${numéroCREValue}`,
-        ),
-        appelOffre: appelOffreValue,
-        période: périodeValue,
-        famille: familleValue,
-        numéroCRE: numéroCREValue,
-        statut: StatutCandidature.convertirEnValueType(statutValue),
-        dateÉchéanceGf: dateÉchéanceGfValue
-          ? DateTime.convertirEnValueType(dateÉchéanceGfValue)
-          : undefined,
-        technologie: Technologie.convertirEnValueType(technologieValue),
-        typeGarantiesFinancières: typeGarantiesFinancièresValue
-          ? GarantiesFinancières.TypeGarantiesFinancières.convertirEnValueType(
-              typeGarantiesFinancièresValue,
-            )
-          : undefined,
-        historiqueAbandon: HistoriqueAbandon.convertirEnValueType(historiqueAbandonValue),
-        nomProjet: payload.nomProjetValue,
-        adresse1: payload.adresse1Value,
-        adresse2: payload.adresse2Value,
-        codePostal: payload.codePostalValue,
-        commune: payload.communeValue,
-        emailContact: payload.emailContactValue,
-        evaluationCarboneSimplifiée: payload.evaluationCarboneSimplifiéeValue,
-        financementCollectif: payload.financementCollectifValue,
-        financementParticipatif: payload.financementParticipatifValue,
-        gouvernancePartagée: payload.financementCollectifValue,
-        nomCandidat: payload.nomCandidatValue,
-        nomReprésentantLégal: payload.nomReprésentantLégalValue,
-        noteTotale: payload.noteTotaleValue,
-        prixReference: payload.prixReferenceValue,
-        puissanceProductionAnnuelle: payload.puissanceProductionAnnuelleValue,
-        motifÉlimination: payload.motifÉliminationValue,
-        puissanceALaPointe: payload.puissanceALaPointeValue,
-        sociétéMère: payload.sociétéMèreValue,
-        valeurÉvaluationCarbone: payload.valeurÉvaluationCarboneValue,
-        territoireProjet: payload.territoireProjetValue,
-        détails: payload.détailsValue,
-      },
+      data: mapPayloadForCommand(payload),
     });
-  };
 
   mediator.register('Candidature.UseCase.ImporterCandidature', handler);
 };
+
+export const mapPayloadForCommand = (payload: ImporterCandidatureUseCase['data']) => ({
+  identifiantProjet: IdentifiantProjet.convertirEnValueType(
+    `${payload.appelOffreValue}#${payload.périodeValue}#${payload.familleValue}#${payload.numéroCREValue}`,
+  ),
+  appelOffre: payload.appelOffreValue,
+  période: payload.périodeValue,
+  famille: payload.familleValue,
+  numéroCRE: payload.numéroCREValue,
+  statut: StatutCandidature.convertirEnValueType(payload.statutValue),
+  dateÉchéanceGf: payload.dateÉchéanceGfValue
+    ? DateTime.convertirEnValueType(payload.dateÉchéanceGfValue)
+    : undefined,
+  technologie: Technologie.convertirEnValueType(payload.technologieValue),
+  typeGarantiesFinancières: payload.typeGarantiesFinancièresValue
+    ? GarantiesFinancières.TypeGarantiesFinancières.convertirEnValueType(
+        payload.typeGarantiesFinancièresValue,
+      )
+    : undefined,
+  historiqueAbandon: HistoriqueAbandon.convertirEnValueType(payload.historiqueAbandonValue),
+  nomProjet: payload.nomProjetValue,
+  adresse1: payload.adresse1Value,
+  adresse2: payload.adresse2Value,
+  codePostal: payload.codePostalValue,
+  commune: payload.communeValue,
+  emailContact: payload.emailContactValue,
+  evaluationCarboneSimplifiée: payload.evaluationCarboneSimplifiéeValue,
+  financementCollectif: payload.financementCollectifValue,
+  financementParticipatif: payload.financementParticipatifValue,
+  gouvernancePartagée: payload.financementCollectifValue,
+  nomCandidat: payload.nomCandidatValue,
+  nomReprésentantLégal: payload.nomReprésentantLégalValue,
+  noteTotale: payload.noteTotaleValue,
+  prixReference: payload.prixReferenceValue,
+  puissanceProductionAnnuelle: payload.puissanceProductionAnnuelleValue,
+  motifÉlimination: payload.motifÉliminationValue,
+  puissanceALaPointe: payload.puissanceALaPointeValue,
+  sociétéMère: payload.sociétéMèreValue,
+  valeurÉvaluationCarbone: payload.valeurÉvaluationCarboneValue,
+  territoireProjet: payload.territoireProjetValue,
+  détails: payload.détailsValue,
+});
