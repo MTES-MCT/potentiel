98fbf6869aa1ab4ceb6b05611a961025e5e8f908
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-08-16 10:31:45 +0200

    ✨ Saga avec CandidatureCorrigée

--- packages/applications/legacy/src/sagas/candidature.saga.ts
+++ packages/applications/legacy/src/sagas/candidature.saga.ts
@@ -2,7 +2,11 @@ import { Message, MessageHandler, mediator } from 'mediateur';
 import { Event } from '@potentiel-infrastructure/pg-event-sourcing';
 import { Candidature } from '@potentiel-domain/candidature';
 import { publishToEventBus } from '../config/eventBus.config';
-import { DésignationCatégorie, ProjectRawDataImported } from '../modules/project';
+import {
+  DésignationCatégorie,
+  ProjectRawDataImported,
+  ProjectReimported,
+} from '../modules/project';
 import { logger } from '../core/utils';
 import { v4 } from 'uuid';
 import { AppelOffre } from '@potentiel-domain/appel-offre';
@@ -13,87 +17,90 @@ import getDepartementRegionFromCodePostal, {
 } from '../helpers/getDepartementRegionFromCodePostal';
 import { GarantiesFinancières } from '@potentiel-domain/laureat';
 
-export type SubscriptionEvent = Candidature.CandidatureImportéeEvent & Event;
+export type SubscriptionEvent = (
+  | Candidature.CandidatureImportéeEvent
+  | Candidature.CandidatureCorrigéeEvent
+) &
+  Event;
 
 export type Execute = Message<'System.Saga.Candidature', SubscriptionEvent>;
 
 export const register = () => {
   const handler: MessageHandler<Execute> = async (event) => {
+    const { payload } = event;
+    const appelOffre = await mediator.send<AppelOffre.ConsulterAppelOffreQuery>({
+      type: 'AppelOffre.Query.ConsulterAppelOffre',
+      data: {
+        identifiantAppelOffre: payload.appelOffre,
+      },
+    });
     switch (event.type) {
       case 'CandidatureImportée-V1':
-        const { payload } = event;
-
-        const appelOffre = await mediator.send<AppelOffre.ConsulterAppelOffreQuery>({
-          type: 'AppelOffre.Query.ConsulterAppelOffre',
-          data: {
-            identifiantAppelOffre: payload.appelOffre,
-          },
-        });
-
-        if (Option.isNone(appelOffre)) {
-          throw new Error(`Appel offre ${payload.appelOffre} non trouvée`);
-        }
-        const période = appelOffre.periodes.find((x) => x.id === payload.période);
-        if (!période) {
-          throw new Error(`Période ${payload.période} non trouvée pour l'AO ${payload.appelOffre}`);
-        }
-
+      case 'CandidatureCorrigée-V1':
         await publishToEventBus(
           new ProjectRawDataImported({
             payload: {
               importId: v4(),
-              data: {
-                appelOffreId: payload.appelOffre,
-                periodeId: payload.période,
-                familleId: payload.famille,
-                numeroCRE: payload.numéroCRE,
-                classe: payload.statut === 'classé' ? 'Classé' : 'Eliminé', // TODO check
-                nomProjet: payload.nomProjet,
-                nomCandidat: payload.nomCandidat,
-                nomRepresentantLegal: payload.nomReprésentantLégal,
-                email: payload.emailContact,
-                motifsElimination: payload.motifÉlimination,
-                garantiesFinancièresDateEchéance: payload.dateÉchéanceGf,
-                technologie: payload.technologie,
-                historiqueAbandon: payload.historiqueAbandon,
-                puissance: payload.puissanceProductionAnnuelle,
-                garantiesFinancièresType: getTypeGarantiesFinancieresLabel(
-                  payload.typeGarantiesFinancières,
-                ),
-                details: payload.détails,
-                engagementFournitureDePuissanceAlaPointe: payload.puissanceALaPointe,
-                actionnaire: payload.sociétéMère,
-                prixReference: payload.prixReference,
-                note: payload.noteTotale,
-                evaluationCarbone: payload.evaluationCarboneSimplifiée,
-                actionnariat: payload.financementCollectif
-                  ? 'financement-collectif'
-                  : payload.gouvernancePartagée
-                    ? 'gouvernance-partagee'
-                    : undefined,
-                désignationCatégorie: getDésignationCatégorie({
-                  puissance: payload.puissanceProductionAnnuelle,
-                  note: payload.noteTotale,
-                  periodeDetails: période,
-                }),
-                isFinancementParticipatif: payload.financementCollectif,
-                isInvestissementParticipatif: payload.financementParticipatif,
-                notifiedOn: 0,
-                territoireProjet: payload.teritoireProjet,
-                ...getLocalilitéInfo(payload),
-              },
+              data: mapToLegacyEventPayload(payload, appelOffre),
             },
           }),
         );
-        logger.warning('Identifiant projet inconnu', {
-          saga: 'System.Saga.Candidature',
-          event,
-        });
         return;
     }
   };
 
   mediator.register('System.Saga.Candidature', handler);
+};
+
+const mapToLegacyEventPayload = (
+  payload: SubscriptionEvent['payload'],
+  appelOffre: Option.Type<AppelOffre.AppelOffreReadModel>,
+) => {
+  if (Option.isNone(appelOffre)) {
+    throw new Error(`Appel offre ${payload.appelOffre} non trouvée`);
+  }
+  const période = appelOffre.periodes.find((x) => x.id === payload.période);
+  if (!période) {
+    throw new Error(`Période ${payload.période} non trouvée pour l'AO ${payload.appelOffre}`);
+  }
+  return {
+    appelOffreId: payload.appelOffre,
+    periodeId: payload.période,
+    familleId: payload.famille,
+    numeroCRE: payload.numéroCRE,
+    classe: payload.statut === 'classé' ? 'Classé' : 'Eliminé',
+    nomProjet: payload.nomProjet,
+    nomCandidat: payload.nomCandidat,
+    nomRepresentantLegal: payload.nomReprésentantLégal,
+    email: payload.emailContact,
+    motifsElimination: payload.motifÉlimination,
+    garantiesFinancièresDateEchéance: payload.dateÉchéanceGf,
+    technologie: payload.technologie,
+    historiqueAbandon: payload.historiqueAbandon,
+    puissance: payload.puissanceProductionAnnuelle,
+    garantiesFinancièresType: getTypeGarantiesFinancieresLabel(payload.typeGarantiesFinancières),
+    details: payload.détails,
+    engagementFournitureDePuissanceAlaPointe: payload.puissanceALaPointe,
+    actionnaire: payload.sociétéMère,
+    prixReference: payload.prixReference,
+    note: payload.noteTotale,
+    evaluationCarbone: payload.evaluationCarboneSimplifiée,
+    actionnariat: payload.financementCollectif
+      ? 'financement-collectif'
+      : payload.gouvernancePartagée
+        ? 'gouvernance-partagee'
+        : undefined,
+    désignationCatégorie: getDésignationCatégorie({
+      puissance: payload.puissanceProductionAnnuelle,
+      note: payload.noteTotale,
+      periodeDetails: période,
+    }),
+    isFinancementParticipatif: payload.financementCollectif,
+    isInvestissementParticipatif: payload.financementParticipatif,
+    notifiedOn: 0,
+    territoireProjet: payload.teritoireProjet,
+    ...getLocalilitéInfo(payload),
+  };
 };
 
 const getLocalilitéInfo = ({
