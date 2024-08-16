fad2d569d89e852fef0b0fefeacd463c3398f9df
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-08-16 17:07:01 +0200

    🚧 Start getting all needed events and reducing them

--- packages/applications/scheduled-tasks/src/candidature/migrate-project-import-events.ts
+++ packages/applications/scheduled-tasks/src/candidature/migrate-project-import-events.ts
@@ -1,11 +1,16 @@
 import * as readline from 'readline';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Candidature } from '@potentiel-domain/candidature';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { DateTime, IdentifiantProjet } from '@potentiel-domain/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { GarantiesFinancières } from '@potentiel-domain/laureat';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { publish } from '@potentiel-infrastructure/pg-event-sourcing';
 import { executeSelect } from '@potentiel-libraries/pg-helpers';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ProjectRawDataImportedPayload = {
   periodeId: string;
   appelOffreId: string;
@@ -48,115 +53,185 @@ type ProjectRawDataImportedPayload = {
 (async () => {
   console.info(`ℹ️ Lancement du script [migrate-project-import-events] ...`);
 
-  const query = `
-    select payload->'data'->>'nomProjet' as "nom_projet", 
-        payload->'data'->>'appelOffreId' as "appel_offre", 
-        payload->'data'->>'periodeId' as "periode", 
-        payload->'data'->>'familleId' as "famille", 
-        payload->'data'->>'numeroCRE' as "numero_cre", 
-        count(id) as "total_import",
-        array_agg(id) as "event_ids" 
+  const getProjectRawDataImportedEventsQuery = `
+    select payload->'data'->>'appelOffreId' as "appel_offre", 
+           payload->'data'->>'periodeId' as "periode", 
+           payload->'data'->>'familleId' as "famille", 
+           payload->'data'->>'numeroCRE' as "numero_cre", 
+           count(id) as "total_import",
+           array_agg(id) as "event_ids" 
     from "eventStores" es 
     where type = 'ProjectRawDataImported'
-    group by payload->'data'->>'nomProjet', 
-            payload->'data'->>'appelOffreId', 
-            payload->'data'->>'periodeId', 
-            payload->'data'->>'familleId', 
-            payload->'data'->>'numeroCRE';
+    group by payload->'data'->>'appelOffreId', 
+             payload->'data'->>'periodeId', 
+             payload->'data'->>'familleId', 
+             payload->'data'->>'numeroCRE';
+  `;
+
+  const getLegacyProjectSourcedEventsQuery = `
+    select payload->>'appelOffreId' as "appel_offre", 
+           payload->>'periodeId' as "periode", 
+           payload->>'familleId' as "famille",
+           payload->>'numeroCRE' as "numero_cre", 
+           count(id) as "total_import",
+           array_agg(id) as "event_ids" 
+    from "eventStores" es 
+    where type = 'LegacyProjectSourced'
+    group by payload->>'appelOffreId', 
+             payload->>'periodeId', 
+             payload->>'familleId', 
+             payload->>'numeroCRE';
+  `;
+
+  const getProjectImportedEventsQuery = `
+    select payload->>'appelOffreId' as "appel_offre", 
+           payload->>'periodeId' as "periode", 
+           payload->>'familleId' as "famille",
+           payload->>'numeroCRE' as "numero_cre", 
+           count(id) as "total_import",
+           array_agg(id) as "event_ids" 
+    from "eventStores" es 
+    where type = 'ProjectImported'
+    group by payload->>'appelOffreId', 
+             payload->>'periodeId', 
+             payload->>'familleId', 
+             payload->>'numeroCRE';
+  `;
+
+  const getProjectReimportedEventsQuery = `
+    select payload->'data'->>'appelOffreId' as "appel_offre", 
+           payload->'data'->>'periodeId' as "periode", 
+           payload->'data'->>'familleId' as "famille", 
+           payload->'data'->>'numeroCRE' as "numero_cre", 
+           count(id) as "total_import",
+           array_agg(id) as "event_ids" 
+    from "eventStores" es 
+    where type = 'ProjectReimported'
+    group by payload->'data'->>'appelOffreId', 
+             payload->'data'->>'periodeId', 
+             payload->'data'->>'familleId', 
+             payload->'data'->>'numeroCRE';
   `;
 
   try {
-    const result = await executeSelect<{
-      nom_projet: string;
+    type EventIdsPerProject = {
       appel_offre: string;
       periode: string;
       famille: string;
       numero_cre: string;
       total_import: number;
       event_ids: string[];
-    }>(query);
+    };
 
-    console.info(`🧐 ${result.length} projects found to migrate`);
+    const projectRawDataImportedEventsPerProjects = await executeSelect<EventIdsPerProject>(
+      getProjectRawDataImportedEventsQuery,
+    );
+    const legacyProjectSourcedEventsPerProjects = await executeSelect<EventIdsPerProject>(
+      getLegacyProjectSourcedEventsQuery,
+    );
+    const projectImportedEventsPerProjects = await executeSelect<EventIdsPerProject>(
+      getProjectImportedEventsQuery,
+    );
+    const projectReimportedEventsPerProjects = await executeSelect<EventIdsPerProject>(
+      getProjectReimportedEventsQuery,
+    );
 
-    let current = 0;
+    const allEventsPerProject = projectRawDataImportedEventsPerProjects
+      .concat(legacyProjectSourcedEventsPerProjects)
+      .concat(projectImportedEventsPerProjects)
+      .concat(projectReimportedEventsPerProjects)
+      .reduce((acc, { appel_offre, periode, famille, numero_cre, event_ids }) => {
+        const identifiantProjet: IdentifiantProjet.RawType = `${appel_offre}#${periode}#${famille}#${numero_cre}`;
 
-    for (let i = 0; i < result.length; i += 100) {
-      const chunk = result.slice(i, i + 100);
+        const alreadyAddedEventIds = acc.get(identifiantProjet);
+        acc.set(identifiantProjet, [...(alreadyAddedEventIds ?? []), ...event_ids]);
 
-      await Promise.all(
-        chunk.map(async ({ appel_offre, periode, famille, numero_cre, event_ids }) => {
-          current++;
-          const identifiantProjet = `${appel_offre}#${periode}#${famille}#${numero_cre}`;
+        return acc;
+      }, new Map<IdentifiantProjet.RawType, Array<string>>());
 
-          const query = `
-            select payload->'data' as data
-            from "eventStores" es
-            where id = any($1)
-            order by "createdAt" asc;
-          `;
+    console.info(`🧐 ${allEventsPerProject.size} projects found to migrate`);
 
-          const events = await executeSelect<{
-            data: ProjectRawDataImportedPayload;
-          }>(query, event_ids);
+    // let current = 0;
+    // allEventsPerProject.forEach(async (eventIds, identifiantProjet) => {
+    //   current++;
 
-          const payload = events.reduce(
-            (acc, { data }) => {
-              const result: Candidature.CandidatureImportéeEvent['payload'] = {
-                ...acc,
-                identifiantProjet:
-                  IdentifiantProjet.convertirEnValueType(identifiantProjet).formatter(),
-                statut: data.classe === 'Classé' ? 'classé' : 'éliminé',
-                typeGarantiesFinancières: (data.garantiesFinancièresType ??
-                  'type-inconnu') as GarantiesFinancières.TypeGarantiesFinancières.RawType,
-                historiqueAbandon: data.historiqueAbandon,
-                appelOffre: data.appelOffreId,
-                période: data.periodeId,
-                famille: data.familleId,
-                numéroCRE: data.numeroCRE,
-                nomProjet: data.nomProjet,
-                sociétéMère: data.actionnaire,
-                nomCandidat: data.nomCandidat,
-                puissanceProductionAnnuelle: data.puissance,
-                prixReference: data.prixReference,
-                noteTotale: data.note,
-                nomReprésentantLégal: data.nomRepresentantLegal,
-                emailContact: data.email,
-                adresse1: data.adresseProjet,
-                adresse2: '',
-                codePostal: data.codePostalProjet,
-                commune: data.communeProjet,
-                motifÉlimination: data.motifsElimination,
-                puissanceALaPointe: data.engagementFournitureDePuissanceAlaPointe,
-                evaluationCarboneSimplifiée: data.evaluationCarbone,
-                valeurÉvaluationCarbone: data.evaluationCarbone,
-                technologie:
-                  data.technologie as Candidature.CandidatureImportéeEvent['payload']['technologie'],
-                financementCollectif: data.actionnariat === 'financement-collectif',
-                financementParticipatif: data.isInvestissementParticipatif,
-                gouvernancePartagée: data.actionnariat === 'gouvernance-partagée',
-                dateÉchéanceGf: data.garantiesFinancièresDateEchéance
-                  ? DateTime.convertirEnValueType(
-                      new Date(data.garantiesFinancièresDateEchéance),
-                    ).formatter()
-                  : undefined,
-                teritoireProjet: data.territoireProjet,
-                détails: data.details,
-              };
+    //   const query = `
+    //         select payload->'data' as data
+    //         from "eventStores" es
+    //         where id = any($1)
+    //         order by "createdAt" asc;
+    //       `;
 
-              return result;
-            },
-            {} as Candidature.CandidatureImportéeEvent['payload'],
-          );
+    //   const events = await executeSelect<{
+    //     data: ProjectRawDataImportedPayload;
+    //   }>(query, eventIds);
 
-          await publish(`candidature|${identifiantProjet}`, {
-            type: 'CandidatureImportée-V1',
-            payload,
-          });
+    //   const payload = events.reduce(
+    //     (acc, { data }) => {
+    //       const result: Candidature.CandidatureImportéeEvent['payload'] = {
+    //         ...acc,
+    //         identifiantProjet:
+    //           IdentifiantProjet.convertirEnValueType(identifiantProjet).formatter(),
+    //         statut: data.classe === 'Classé' ? 'classé' : 'éliminé',
+    //         typeGarantiesFinancières: (data.garantiesFinancièresType ??
+    //           'type-inconnu') as GarantiesFinancières.TypeGarantiesFinancières.RawType,
+    //         historiqueAbandon: data.historiqueAbandon,
+    //         appelOffre: data.appelOffreId,
+    //         période: data.periodeId,
+    //         famille: data.familleId,
+    //         numéroCRE: data.numeroCRE,
+    //         nomProjet: data.nomProjet,
+    //         sociétéMère: data.actionnaire,
+    //         nomCandidat: data.nomCandidat,
+    //         puissanceProductionAnnuelle: data.puissance,
+    //         prixReference: data.prixReference,
+    //         noteTotale: data.note,
+    //         nomReprésentantLégal: data.nomRepresentantLegal,
+    //         emailContact: data.email,
+    //         adresse1: data.adresseProjet,
+    //         adresse2: '',
+    //         codePostal: data.codePostalProjet,
+    //         commune: data.communeProjet,
+    //         motifÉlimination: data.motifsElimination,
+    //         puissanceALaPointe: data.engagementFournitureDePuissanceAlaPointe,
+    //         evaluationCarboneSimplifiée: data.evaluationCarbone,
+    //         valeurÉvaluationCarbone: data.evaluationCarbone,
+    //         technologie:
+    //           data.technologie as Candidature.CandidatureImportéeEvent['payload']['technologie'],
+    //         financementCollectif: data.actionnariat === 'financement-collectif',
+    //         financementParticipatif: data.isInvestissementParticipatif,
+    //         gouvernancePartagée: data.actionnariat === 'gouvernance-partagée',
+    //         dateÉchéanceGf: data.garantiesFinancièresDateEchéance
+    //           ? DateTime.convertirEnValueType(
+    //               new Date(data.garantiesFinancièresDateEchéance),
+    //             ).formatter()
+    //           : undefined,
+    //         teritoireProjet: data.territoireProjet,
+    //         détails: data.details,
+    //       };
 
-          printProgress(`${current}/${result.length}`);
-        }),
-      );
-    }
+    //       return result;
+    //     },
+    //     {} as Candidature.CandidatureImportéeEvent['payload'],
+    //   );
+
+    //   await publish(`candidature|${identifiantProjet}`, {
+    //     type: 'CandidatureImportée-V1',
+    //     payload,
+    //   });
+
+    //   printProgress(`${current}/${projectRawDataImportedEventsPerProjects.length}`);
+    // });
+
+    const getAllProjects = `
+      select count(id) as "total_projects"
+      from projects;
+    `;
+
+    const [{ total_projects }] = await executeSelect<{ total_projects: number }>(getAllProjects);
+    console.info(
+      `${allEventsPerProject.size} events were created for ${total_projects} existant projects`,
+    );
 
     console.info('\nFin du script ✨');
   } catch (error) {
@@ -166,6 +241,7 @@ type ProjectRawDataImportedPayload = {
   process.exit(0);
 })();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function printProgress(progress: string) {
   readline.cursorTo(process.stdout, 0);
   process.stdout.write(progress);
