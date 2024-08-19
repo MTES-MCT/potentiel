eb96e1add225ef23ec68005d7052e746c03158a5
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-08-19 14:02:35 +0200

    🎨 Remove useless informations from stories

--- packages/applications/ssr/src/components/pages/candidature/lister/CandidatureList.stories.tsx
+++ packages/applications/ssr/src/components/pages/candidature/lister/CandidatureList.stories.tsx
@@ -1,8 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Candidature } from '@potentiel-domain/candidature';
 import { IdentifiantProjet, StatutProjet } from '@potentiel-domain/common';
-import { GarantiesFinancières } from '@potentiel-domain/laureat';
 import { AppelOffre } from '@potentiel-domain/appel-offre';
 
 import { CandidatureListPage, CandidatureListPageProps } from './CandidatureList.page';
@@ -30,31 +28,16 @@ type CommonItem = CandidatureListPageProps['list']['items'][number] &
 const commonItem: CommonItem = {
   identifiantProjet: IdentifiantProjet.convertirEnValueType('PPE2 - Eolien#1##23'),
   statut: StatutProjet.classé,
-  motifÉlimination: '',
-  typeGarantiesFinancières: GarantiesFinancières.TypeGarantiesFinancières.consignation,
   nomProjet: 'Nom projet',
   nomCandidat: 'Candidat',
-  technologie: Candidature.Technologie.nonApplicable,
   emailContact: 'porteur@test.test',
   codePostal: '13000',
   commune: 'MARSEILLE',
-  adresse1: '5 avenue laeticia',
-  adresse2: '',
-  puissanceALaPointe: true,
-  sociétéMère: '',
-  territoireProjet: '',
-  dateÉchéanceGf: undefined,
-  historiqueAbandon: Candidature.HistoriqueAbandon.premièreCandidature,
   puissanceProductionAnnuelle: 1,
   unitePuissance: 'MWc',
   prixReference: 1,
-  noteTotale: 1,
   nomReprésentantLégal: 'Frodon Sacquet',
   evaluationCarboneSimplifiée: 1,
-  valeurÉvaluationCarbone: 1,
-  financementCollectif: true,
-  gouvernancePartagée: true,
-  financementParticipatif: true,
 };
 
 export const Default: Story = {
