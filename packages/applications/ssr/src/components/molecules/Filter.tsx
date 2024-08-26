23eee43006b9b44eea15cb48fe57677df1373190
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-08-26 14:19:47 +0200

    🎨 Suppression du filtre actif dans ListFilters quand on supprime un tag appliqué

--- packages/applications/ssr/src/components/molecules/Filter.tsx
+++ packages/applications/ssr/src/components/molecules/Filter.tsx
@@ -6,14 +6,22 @@ type FilterProps = {
   label: string;
   options: Array<{ label: string; value: string }>;
   defaultValue: string;
+  value: string;
   onValueSelected?: (value: string | undefined) => void;
 };
-export const Filter: FC<FilterProps> = ({ label, options, defaultValue, onValueSelected }) => (
+export const Filter: FC<FilterProps> = ({
+  label,
+  options,
+  defaultValue,
+  value,
+  onValueSelected,
+}) => (
   <SelectNext
     label={label}
     placeholder={`Filtrer par ${label.toLocaleLowerCase()}`}
     nativeSelectProps={{
       defaultValue,
+      value,
       onChange: (e) => {
         const value = e.currentTarget.value;
         if (onValueSelected) {
