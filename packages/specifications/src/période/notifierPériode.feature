b7b619492bfc224bd2789c4f66fc14904ee3e4ac
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-08-30 10:51:46 +0200

    🧪 Lauréat and Éliminé should exist when notifying a periode

--- packages/specifications/src/période/notifierPériode.feature
+++ packages/specifications/src/période/notifierPériode.feature
@@ -6,3 +6,4 @@ Fonctionnalité: Notifier une période d'un appel d'offres
         Étant donné des candidats d'une période d'un appel d'offres
         Quand un DGEC validateur notifie la période d'un appel d'offres
         Alors la période devrait être notifiée avec les lauréats et les éliminés
+        Et les lauréats et éliminés devraient être consultables
--- packages/specifications/src/période/stepDefinitions/période.then.ts
+++ packages/specifications/src/période/stepDefinitions/période.then.ts
@@ -2,8 +2,10 @@ import { Then as Alors } from '@cucumber/cucumber';
 import waitForExpect from 'wait-for-expect';
 import { mediator } from 'mediateur';
 
+import { Option } from '@potentiel-libraries/monads';
 import { mapToPlainObject } from '@potentiel-domain/core';
 import { Période } from '@potentiel-domain/periode';
+import { Lauréat } from '@potentiel-domain/laureat';
 
 import { PotentielWorld } from '../../potentiel.world';
 
@@ -13,6 +15,16 @@ Alors(
     await waitForExpect(async () =>
       vérifierPériode.call(this, this.périodeWorld.identifiantPériode),
     );
+  },
+);
+
+Alors(
+  `les lauréats et éliminés devraient être consultables`,
+  async function (this: PotentielWorld) {
+    await waitForExpect(async () => {
+      vérifierLauréats.call(this, this.périodeWorld.identifiantPériode);
+      vérifierÉliminés.call(this, this.périodeWorld.identifiantPériode);
+    });
   },
 );
 
@@ -31,4 +43,78 @@ async function vérifierPériode(
   const expected = mapToPlainObject(this.périodeWorld.mapToExpected(identifiantPériode));
 
   actual.should.be.deep.equal(expected);
+}
+
+async function vérifierLauréats(
+  this: PotentielWorld,
+  identifiantPériode: Période.IdentifiantPériode.ValueType,
+) {
+  const période = await mediator.send<Période.ConsulterPériodeQuery>({
+    type: 'Période.Query.ConsulterPériode',
+    data: {
+      identifiantPériodeValue: identifiantPériode.formatter(),
+    },
+  });
+
+  période.should.not.be.equal(
+    Option.none,
+    `La période ${identifiantPériode.période} notifiée pour l'appel d'offre ${identifiantPériode.appelOffre}`,
+  );
+
+  if (Option.isSome(période)) {
+    période.estNotifiée.should.be.true;
+
+    if (période.estNotifiée) {
+      for (const identifiantLauréat of période.lauréats) {
+        const lauréat = await mediator.send<Lauréat.ConsulterLauréatQuery>({
+          type: 'Lauréat.Query.ConsulterLauréat',
+          data: {
+            identifiantProjet: identifiantLauréat.formatter(),
+          },
+        });
+
+        lauréat.should.not.be.equal(
+          Option.none,
+          `Aucun lauréat consultable pour ${identifiantLauréat.formatter()}`,
+        );
+      }
+    }
+  }
+}
+
+async function vérifierÉliminés(
+  this: PotentielWorld,
+  identifiantPériode: Période.IdentifiantPériode.ValueType,
+) {
+  const période = await mediator.send<Période.ConsulterPériodeQuery>({
+    type: 'Période.Query.ConsulterPériode',
+    data: {
+      identifiantPériodeValue: identifiantPériode.formatter(),
+    },
+  });
+
+  période.should.not.be.equal(
+    Option.none,
+    `La période ${identifiantPériode.période} notifiée pour l'appel d'offre ${identifiantPériode.appelOffre}`,
+  );
+
+  if (Option.isSome(période)) {
+    période.estNotifiée.should.be.true;
+
+    if (période.estNotifiée) {
+      for (const identifiantÉliminé of période.éliminés) {
+        const éliminé = await mediator.send<Lauréat.ConsulterLauréatQuery>({
+          type: 'Lauréat.Query.ConsulterLauréat',
+          data: {
+            identifiantProjet: identifiantÉliminé.formatter(),
+          },
+        });
+
+        éliminé.should.not.be.equal(
+          Option.none,
+          `Aucun éliminé consultable pour ${identifiantÉliminé.formatter()}`,
+        );
+      }
+    }
+  }
 }
