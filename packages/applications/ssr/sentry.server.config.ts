6ffd06b0a1857240f4893387831d0cc806f05db5
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2024-09-03 17:12:41 +0200

    🐛 Essai fix taille image

--- packages/applications/ssr/sentry.server.config.ts
+++ packages/applications/ssr/sentry.server.config.ts
@@ -2,15 +2,15 @@
 // // The config you add here will be used whenever the server handles a request.
 // // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs';
 
-// Sentry.init({
-//   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-//   environment: process.env.APPLICATION_STAGE,
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.APPLICATION_STAGE,
 
-//   // Adjust this value in production, or use tracesSampler for greater control
-//   tracesSampleRate: 1,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
 
-//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-//   debug: false,
-// });
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
