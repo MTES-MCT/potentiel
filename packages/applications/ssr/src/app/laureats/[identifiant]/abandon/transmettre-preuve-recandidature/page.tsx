e61929d03e551736b29d9c52c1741719d32fe08b
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-09-03 16:12:09 +0200

    🔥 Remove route transmettrePreuveRecandidature (useless)

--- packages/applications/ssr/src/app/laureats/[identifiant]/abandon/transmettre-preuve-recandidature/page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/abandon/transmettre-preuve-recandidature/page.tsx
@@ -7,6 +7,11 @@ import { decodeParameter } from '@/utils/decodeParameter';
 
 type PageProps = IdentifiantParameter;
 
+/**
+ * @deprecated
+ * Cette Page est conservée pour la retrocompatibilité avec les mails préalablement envoyés,
+ * et redirige vers le détail de l'abandon où il y a un bouton d'action avec une modale pour transmettre
+ */
 export default async function Page({ params: { identifiant } }: PageProps) {
   const identifiantProjet = decodeParameter(identifiant);
   return redirect(Routes.Abandon.détail(identifiantProjet));
