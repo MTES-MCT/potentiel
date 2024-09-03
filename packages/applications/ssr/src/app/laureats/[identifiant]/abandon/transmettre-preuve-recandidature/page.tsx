186fb9e2548d66a0d6981a538345a332710f09c6
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-09-03 16:08:49 +0200

    🐛 Fix bug redirect

--- packages/applications/ssr/src/app/laureats/[identifiant]/abandon/transmettre-preuve-recandidature/page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/abandon/transmettre-preuve-recandidature/page.tsx
@@ -3,9 +3,11 @@ import { redirect } from 'next/navigation';
 import { Routes } from '@potentiel-applications/routes';
 
 import { IdentifiantParameter } from '@/utils/identifiantParameter';
+import { decodeParameter } from '@/utils/decodeParameter';
 
 type PageProps = IdentifiantParameter;
 
 export default async function Page({ params: { identifiant } }: PageProps) {
-  return redirect(Routes.Abandon.détail(identifiant));
+  const identifiantProjet = decodeParameter(identifiant);
+  return redirect(Routes.Abandon.détail(identifiantProjet));
 }
