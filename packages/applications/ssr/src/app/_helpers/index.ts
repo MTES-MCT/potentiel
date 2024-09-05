59f9425cb31856b82b3136557df6b79b934cadcf
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-09-05 10:33:57 +0200

    🔥 Yagni

--- packages/applications/ssr/src/app/_helpers/index.ts
+++ packages/applications/ssr/src/app/_helpers/index.ts
@@ -1,4 +1,2 @@
 export { récupérerProjet } from './récupérerProjet';
-export { vérifierQueLeProjetNestPasAbandonné } from './vérifierQueLeProjetNestPasAbandonné';
-export { vérifierQueLeProjetNestPasÉliminé } from './vérifierQueLeProjetNestPasÉliminé';
 export { vérifierQueLeProjetNestPasAbandonnéOuÉliminé } from './vérifierQueLeProjetNestPasAbandonnéOuÉliminé';
