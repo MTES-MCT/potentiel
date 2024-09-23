95f4199e8382e1fdb8a2a0eb5b04b42c868b178f
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-09-23 16:36:24 +0200

    ✨ Prise en compte des multiples notifications de période

--- packages/applications/scheduled-tasks/src/période/migrate-events-for-periode-notifiee-event.ts
+++ packages/applications/scheduled-tasks/src/période/migrate-events-for-periode-notifiee-event.ts
@@ -1,105 +1,69 @@
-import { mediator } from 'mediateur';
-
 import { executeSelect } from '@potentiel-libraries/pg-helpers';
 import { publish } from '@potentiel-infrastructure/pg-event-sourcing';
-import { findProjection, listProjection } from '@potentiel-infrastructure/pg-projections';
+import { listProjection } from '@potentiel-infrastructure/pg-projections';
 import { Candidature } from '@potentiel-domain/candidature';
 import { Période } from '@potentiel-domain/periode';
-import { DateTime } from '@potentiel-domain/common';
-import { CandidatureAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { DateTime, IdentifiantProjet } from '@potentiel-domain/common';
 import { Lauréat } from '@potentiel-domain/laureat';
 import { Éliminé } from '@potentiel-domain/elimine';
-
-Candidature.registerCandidatureQueries({
-  find: findProjection,
-  récupérerProjet: CandidatureAdapter.récupérerProjetAdapter,
-  récupérerProjetsEligiblesPreuveRecanditure:
-    CandidatureAdapter.récupérerProjetsEligiblesPreuveRecanditureAdapter,
-  récupérerProjets: CandidatureAdapter.récupérerProjetsAdapter,
-  list: listProjection,
-});
+import { Where } from '@potentiel-domain/entity';
 
 (async () => {
   try {
-    // TODO
-    const périodes = await executeSelect<{
+    const allPériodes = await executeSelect<{
       appelOffreId: string;
       periodeId: string;
       notifiedOn: number;
-      requestedBy: string;
+      notifiedBy: string | null;
+      identifiants: IdentifiantProjet.RawType[];
     }>(`
-        select es.payload->>'appelOffreId' as "appelOffreId",
-              es.payload->>'periodeId' as "periodeId",
-              es.payload->'notifiedOn' as "notifiedOn",
-              es.payload->>'requestedBy' as "requestedBy"
-        from "eventStores" es 
-        where es."type" = 'PeriodeNotified'
-        order by es.payload->>'notifiedOn' asc;
+        select
+          case
+            when p."appelOffreId" = 'PPE2 - Bâtiment 2' then 'PPE2 - Bâtiment'
+            else p."appelOffreId"
+          end as "appelOffreId",
+          p."periodeId",
+          p."notifiedOn",
+          u."email" as "notifiedBy",
+          array_agg(format('%s#%s#%s#%s',	REPLACE(p."appelOffreId",'PPE2 - Bâtiment 2', 'PPE2 - Bâtiment'),p."periodeId",p."familleId",p."numeroCRE")) as identifiants
+        from
+          projects p
+        left join "eventStores" es on
+          es."type" = 'PeriodeNotified'
+          and p."appelOffreId" = es.payload->>'appelOffreId'
+          and p."periodeId" = es.payload->>'periodeId'
+        left join users u on
+          u.id::text = es.payload->>'requestedBy'
+        group by
+          p."appelOffreId",
+          p."periodeId",
+          p."notifiedOn",
+          u."email" ;
     `);
-
-    const legacyPeriodes = await executeSelect<{
-      appelOffreId: string;
-      periodeId: string;
-      notifiedOn: number;
-      requestedBy: string;
-    }>(`
-        select distinct on (p."appelOffreId", p."periodeId") 
-            case 
-              when p."appelOffreId" = 'PPE2 - Bâtiment 2' then 'PPE2 - Bâtiment'
-              else p."appelOffreId"
-            end as "appelOffreId",
-            p."periodeId", 
-            p."notifiedOn", 
-            null as "requestedBy"
-        from projects p 
-        left join "eventStores" es on es."type" = 'PeriodeNotified'
-                      and p."appelOffreId" = es.payload->>'appelOffreId'  
-                      and p."periodeId" = es.payload->>'periodeId'
-        where es.id is null
-        group by p."appelOffreId", 
-            p."periodeId", 
-            p."notifiedOn" 
-        order by p."appelOffreId", 
-            p."periodeId", 
-            p."notifiedOn";
-    `);
-
-    const allPériodes = périodes.concat(legacyPeriodes);
 
     console.info(`Migrating ${allPériodes.length} periodes`);
 
-    for (const { appelOffreId, periodeId, notifiedOn, requestedBy } of allPériodes) {
-      const users = await executeSelect<{
-        email: string;
-      }>(
-        `
-        select email
-        from "users"
-        where id = $1;
-        `,
-        requestedBy,
-      );
-
+    for (const { appelOffreId, periodeId, notifiedOn, notifiedBy, identifiants } of allPériodes) {
       const identifiantPériode = `${appelOffreId}#${periodeId}` as const;
 
-      const candidatures = await mediator.send<Candidature.ListerCandidaturesQuery>({
-        type: 'Candidature.Query.ListerCandidatures',
-        data: {
-          appelOffre: appelOffreId,
-          période: periodeId,
+      const candidatures = await listProjection<Candidature.CandidatureEntity>('candidature', {
+        where: {
+          identifiantProjet: Where.include(identifiants),
+          appelOffre: Where.equal(appelOffreId),
+          période: Where.equal(periodeId),
         },
       });
 
-      const notifiéePar = users[0]?.email.trim() ?? 'aopv.dgec@developpement-durable.gouv.fr';
+      const notifiéePar = notifiedBy ?? 'aopv.dgec@developpement-durable.gouv.fr';
       const notifiéeLe = DateTime.convertirEnValueType(new Date(notifiedOn)).formatter();
 
       const identifiantLauréats = candidatures.items
-        .filter((item) => item.statut.estClassé())
-        .map(({ identifiantProjet }) => identifiantProjet.formatter());
+        .filter((item) => item.statut === 'classé')
+        .map(({ identifiantProjet }) => identifiantProjet);
 
       const identifiantÉliminés = candidatures.items
-        .filter((item) => item.statut.estÉliminé())
-        .map(({ identifiantProjet }) => identifiantProjet.formatter());
+        .filter((item) => item.statut === 'éliminé')
+        .map(({ identifiantProjet }) => identifiantProjet);
 
       const event: Période.PériodeNotifiéeEvent = {
         type: 'PériodeNotifiée-V1',
