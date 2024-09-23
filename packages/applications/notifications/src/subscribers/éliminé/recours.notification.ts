23dd893b83a2517b6a60b731c59f498c3603ba6d
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-09-23 15:54:48 +0200

    🎨 Envoi de l'email à destination des utilisateurs CRE quand le recours est accordé

--- packages/applications/notifications/src/subscribers/éliminé/recours.notification.ts
+++ packages/applications/notifications/src/subscribers/éliminé/recours.notification.ts
@@ -1,4 +1,5 @@
 import { Message, MessageHandler, mediator } from 'mediateur';
+import { match } from 'ts-pattern';
 
 import { Option } from '@potentiel-libraries/monads';
 import { Event } from '@potentiel-infrastructure/pg-event-sourcing';
@@ -7,8 +8,10 @@ import { IdentifiantProjet } from '@potentiel-domain/common';
 import { Routes } from '@potentiel-applications/routes';
 import {
   CandidatureAdapter,
+  listerUtilisateursAdapter,
   récupérerPorteursParIdentifiantProjetAdapter,
 } from '@potentiel-infrastructure/domain-adapters';
+import { Role } from '@potentiel-domain/utilisateur';
 
 import { SendEmail } from '../../sendEmail';
 
@@ -16,19 +19,9 @@ export type SubscriptionEvent = Recours.RecoursEvent & Event;
 
 export type Execute = Message<'System.Notification.Éliminé.Recours', SubscriptionEvent>;
 
-const templateId = 6310637;
-
-const getStatutByEventType = (type: SubscriptionEvent['type']) => {
-  switch (type) {
-    case 'RecoursDemandé-V1':
-      return 'demandée';
-    case 'RecoursAnnulé-V1':
-      return 'annulée';
-    case 'RecoursAccordé-V1':
-      return 'accordée';
-    case 'RecoursRejeté-V1':
-      return 'rejetée';
-  }
+const templateId = {
+  changementStatutRecours: 6310637,
+  recoursAccordéCRE: 6189222,
 };
 
 export type RegisterRecoursNotificationDependencies = {
@@ -59,10 +52,15 @@ export const register = ({ sendEmail }: RegisterRecoursNotificationDependencies)
     const départementProjet = projet.localité.département;
     const appelOffre = projet.appelOffre;
     const période = projet.période;
-    const statut = getStatutByEventType(event.type);
+    const statut = match(event.type)
+      .with('RecoursDemandé-V1', () => 'demandée')
+      .with('RecoursAnnulé-V1', () => 'annulée')
+      .with('RecoursAccordé-V1', () => 'accordée')
+      .with('RecoursRejeté-V1', () => 'rejetée')
+      .exhaustive();
 
     await sendEmail({
-      templateId,
+      templateId: templateId.changementStatutRecours,
       messageSubject: `Potentiel - Demande de recours ${statut} pour le projet ${nomProjet} (${appelOffre} période ${période})`,
       recipients: [...porteurs, ...admins],
       variables: {
@@ -75,6 +73,26 @@ export const register = ({ sendEmail }: RegisterRecoursNotificationDependencies)
             : `${BASE_URL}${Routes.Recours.détail(identifiantProjet.formatter())}`,
       },
     });
+
+    if (event.type === 'RecoursAccordé-V1') {
+      const utilisateursCre = await listerUtilisateursAdapter([Role.cre.nom]);
+      const recipients = utilisateursCre.map(({ email, nomComplet }) => ({
+        email,
+        fullName: nomComplet,
+      }));
+
+      if (recipients.length > 0) {
+        await sendEmail({
+          templateId: templateId.recoursAccordéCRE,
+          messageSubject: `Potentiel - Un recours a été accepté pour le projet ${nomProjet} (${appelOffre} période ${période})`,
+          recipients,
+          variables: {
+            nom_projet: nomProjet,
+            modification_request_url: `${BASE_URL}${Routes.Recours.détail(identifiantProjet.formatter())}`,
+          },
+        });
+      }
+    }
   };
 
   mediator.register('System.Notification.Éliminé.Recours', handler);
