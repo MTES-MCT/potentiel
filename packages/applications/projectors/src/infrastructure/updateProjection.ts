ea6e574f73143543290e4f03ae6517fb35947a89
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-09-26 17:03:36 +0200

    ⚡️ Optimisation de `updateProjection`

--- packages/applications/projectors/src/infrastructure/updateProjection.ts
+++ packages/applications/projectors/src/infrastructure/updateProjection.ts
@@ -2,20 +2,35 @@ import { Entity } from '@potentiel-domain/entity';
 import { flatten } from '@potentiel-libraries/flat';
 import { executeQuery } from '@potentiel-libraries/pg-helpers';
 
-const updateQuery =
-  'update domain_views.projection set value=jsonb_set(value,$2,$3) where key = $1';
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
+/** */
 export const updateProjection = async <TProjection extends Entity>(
   id: `${TProjection['type']}|${string}`,
-  readModel: Partial<Omit<TProjection, 'type'>>,
+  readModel: AtLeastOne<Omit<TProjection, 'type'>>,
 ): Promise<void> => {
-  const flatReadModel = Object.entries(flatten(readModel) as Record<string, unknown>);
-  for (const [key, value] of flatReadModel) {
-    await executeQuery(
-      updateQuery,
-      id,
-      `{"${key}"}`,
-      typeof value === 'string' ? `"${value}"` : value,
-    );
-  }
+  const [updateQuery, values] = prepareUpdateProjectionQuery(readModel);
+  console.log(updateQuery, values);
+  await executeQuery(updateQuery, id, ...values);
+};
+
+export const prepareUpdateProjectionQuery = <TProjection extends Entity>(
+  readModel: AtLeastOne<Omit<TProjection, 'type'>>,
+): [string, Array<unknown>] => {
+  const flatReadModel = flatten(readModel) as Record<string, unknown>;
+  /*
+    The following generates a recursive update similar to this:
+    select jsonb_set(jsonb_set(data,'{field1}','"new_value1"'), '{field2}', '"new_value2"')
+    from (select '{"field1": "a", "field2":"b"}'::jsonb as data ) a
+
+    the `i+2` operation is due to `i` starting at 0, and $1 being the key
+   */
+  const jsonb_set = Object.keys(flatReadModel).reduce(
+    (acc, curr, i) => `jsonb_set(${acc},'{${curr}}',$${i + 2})`,
+    'value',
+  );
+  const values = Object.values(flatReadModel).map((value) =>
+    typeof value === 'string' ? `"${value}"` : value,
+  );
+  return [`update domain_views.projection set value=${jsonb_set} where key = $1`, values];
 };
