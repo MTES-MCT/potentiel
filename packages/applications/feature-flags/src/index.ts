67165428a4d079f96a1e346b55668bd8780eda24
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-09-27 16:00:19 +0200

    ⚰️ Remove feature flags package

--- packages/applications/feature-flags/src/index.ts
+++
@@ -1,7 +0,0 @@
-type FeatureFlags = {
-  isRecoursEnabled: boolean;
-};
-
-export const featureFlags: FeatureFlags = {
-  isRecoursEnabled: process.env.NEXT_PUBLIC_IS_RECOURS_ENABLED === 'true',
-};
