8d33795ce7dc90d20d4940d966ce8bf43c80e562
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-09-27 15:24:36 +0200

    🎨 Updated text to version1

--- packages/applications/ssr/src/components/pages/réseau/raccordement/détails/AucunDossierDeRaccordementAlert.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/détails/AucunDossierDeRaccordementAlert.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 import { Routes } from '@potentiel-applications/routes';
 
-type Props = { identifiantProjet: string };
+type Props = { identifiantProjet: string; showLink?: boolean };
 
-export const AucunDossierDeRaccordementAlert = ({ identifiantProjet }: Props) => (
+export const AucunDossierDeRaccordementAlert = ({ identifiantProjet, showLink = true }: Props) => (
   <Alert
     severity="warning"
     title="Données de raccordement à compléter"
@@ -14,19 +14,18 @@ export const AucunDossierDeRaccordementAlert = ({ identifiantProjet }: Props) =>
         <p>Vous n'avez pas encore transmis votre demande complète de raccordement sur Potentiel.</p>
         <p>
           L'accusé de réception de cette demande ainsi que les documents complémentaires
-          (proposition technique et financière ou convention de raccordement directe, convention de
-          raccordement…) transmis sur Potentiel faciliteront vos démarches administratives avec les
-          différents acteurs connectés à Potentiel (DGEC, DREAL, Cocontractant, etc.).
+          (proposition technique et financière…) transmis sur Potentiel faciliteront vos démarches
+          administratives avec les différents acteurs connectés à Potentiel (DGEC, services de
+          l'Etat en région, Cocontractant, etc.).
         </p>
-        <span>
-          Mettre à jour votre{' '}
+        {showLink && (
           <Link
             href={Routes.Raccordement.transmettreDemandeComplèteRaccordement(identifiantProjet)}
             className="font-semibold"
           >
-            dossier de raccordement
+            Mettre à jour votre dossier de raccordement
           </Link>
-        </span>
+        )}
       </div>
     }
   />
