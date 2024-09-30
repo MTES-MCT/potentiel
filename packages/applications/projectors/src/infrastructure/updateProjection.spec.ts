9853f418d09b08c2e071ba268a62b74e41b81075
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-09-30 09:37:33 +0200

    ⚡️ Optimisation de `updateProjection` (#2259)

---
+++ packages/applications/projectors/src/infrastructure/updateProjection.spec.ts
@@ -0,0 +1,54 @@
+import { describe, test } from 'node:test';
+
+import { expect } from 'chai';
+
+import { Entity } from '@potentiel-domain/entity';
+
+import { prepareUpdateProjectionQuery } from './updateProjection';
+
+type TestEntity = Entity<
+  'test',
+  {
+    foo: string;
+    bar: number;
+    baz: boolean;
+  }
+>;
+
+describe('updateProjection', () => {
+  test(`single string key`, () => {
+    const [query, values] = prepareUpdateProjectionQuery<TestEntity>({ foo: 'hello' });
+    expect(query).to.eq(
+      "update domain_views.projection set value=jsonb_set(value,'{foo}',$2) where key = $1",
+    );
+    expect(values).to.deep.eq(['"hello"']);
+  });
+
+  test(`single number key`, () => {
+    const [query, values] = prepareUpdateProjectionQuery<TestEntity>({ bar: 1 });
+    expect(query).to.eq(
+      "update domain_views.projection set value=jsonb_set(value,'{bar}',$2) where key = $1",
+    );
+    expect(values).to.deep.eq([1]);
+  });
+
+  test(`single boolean key`, () => {
+    const [query, values] = prepareUpdateProjectionQuery<TestEntity>({ baz: true });
+    expect(query).to.eq(
+      "update domain_views.projection set value=jsonb_set(value,'{baz}',$2) where key = $1",
+    );
+    expect(values).to.deep.eq([true]);
+  });
+
+  test(`multiple keys`, () => {
+    const [query, values] = prepareUpdateProjectionQuery<TestEntity>({
+      foo: '',
+      bar: -1,
+      baz: false,
+    });
+    expect(query).to.eq(
+      "update domain_views.projection set value=jsonb_set(jsonb_set(jsonb_set(value,'{foo}',$2),'{bar}',$3),'{baz}',$4) where key = $1",
+    );
+    expect(values).to.deep.eq(['""', -1, false]);
+  });
+});
