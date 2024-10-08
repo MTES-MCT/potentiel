516fc04508d0ba563a4cab75312c9c6783a87b05
Violette <27735540+VioMrqs@users.noreply.github.com>
2024-10-08 14:27:38 +0200

    🎨 Déclencher NotifierCandidature depuis NotifierLauréat ou Eliminé (#2288)

--- packages/domain/lauréat/src/notifier/notifierLauréat.usecase.ts
+++ packages/domain/lauréat/src/notifier/notifierLauréat.usecase.ts
@@ -1,6 +1,7 @@
 import { Message, MessageHandler, mediator } from 'mediateur';
 
 import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { Candidature } from '@potentiel-domain/candidature';
 
 import { NotifierLauréatCommand } from './notifierLauréat.command';
 
@@ -23,6 +24,17 @@ export const registerNotifierLauréatUseCase = () => {
     notifiéLeValue,
     attestationValue: { format },
   }) => {
+    await mediator.send<Candidature.NotifierCandidatureUseCase>({
+      type: 'Candidature.UseCase.NotifierCandidature',
+      data: {
+        identifiantProjetValue,
+        notifiéeLeValue: notifiéLeValue,
+        notifiéeParValue: notifiéParValue,
+        attestationValue: {
+          format: 'application/pdf',
+        },
+      },
+    });
     await mediator.send<NotifierLauréatCommand>({
       type: 'Lauréat.Command.NotifierLauréat',
       data: {
