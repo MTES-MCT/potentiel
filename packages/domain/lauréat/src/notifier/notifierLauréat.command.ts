66f8c0791851f470e7ff9c7d8077dd5948cf9cf7
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-10-08 12:13:57 +0200

    🎨 Changement reaction

--- packages/domain/lauréat/src/notifier/notifierLauréat.command.ts
+++ packages/domain/lauréat/src/notifier/notifierLauréat.command.ts
@@ -2,6 +2,7 @@ import { Message, MessageHandler, mediator } from 'mediateur';
 
 import { LoadAggregate } from '@potentiel-domain/core';
 import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { Candidature } from '@potentiel-domain/candidature';
 
 import { loadLauréatFactory } from '../lauréat.aggregate';
 
@@ -20,6 +21,17 @@ export const registerNotifierLauréatCommand = (loadAggregate: LoadAggregate) =>
   const handler: MessageHandler<NotifierLauréatCommand> = async (payload) => {
     const Lauréat = await loadLauréatAggregate(payload.identifiantProjet, false);
     await Lauréat.notifier(payload);
+    await mediator.send<Candidature.NotifierCandidatureUseCase>({
+      type: 'Candidature.UseCase.NotifierCandidature',
+      data: {
+        identifiantProjetValue: payload.identifiantProjet.formatter(),
+        notifiéeLeValue: payload.notifiéLe.formatter(),
+        notifiéeParValue: payload.notifiéPar.formatter(),
+        attestationValue: {
+          format: 'application/pdf',
+        },
+      },
+    });
   };
 
   mediator.register('Lauréat.Command.NotifierLauréat', handler);
--- packages/domain/éliminé/src/notifier/notifierÉliminé.command.ts
+++ packages/domain/éliminé/src/notifier/notifierÉliminé.command.ts
@@ -2,6 +2,7 @@ import { Message, MessageHandler, mediator } from 'mediateur';
 
 import { LoadAggregate } from '@potentiel-domain/core';
 import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { Candidature } from '@potentiel-domain/candidature';
 
 import { loadÉliminéFactory } from '../éliminé.aggregate';
 
@@ -20,6 +21,17 @@ export const registerNotifierÉliminéCommand = (loadAggregate: LoadAggregate) =
   const handler: MessageHandler<NotifierÉliminéCommand> = async (payload) => {
     const éliminé = await loadÉliminéAggregate(payload.identifiantProjet, false);
     await éliminé.notifier(payload);
+    await mediator.send<Candidature.NotifierCandidatureUseCase>({
+      type: 'Candidature.UseCase.NotifierCandidature',
+      data: {
+        identifiantProjetValue: payload.identifiantProjet.formatter(),
+        notifiéeLeValue: payload.notifiéLe.formatter(),
+        notifiéeParValue: payload.notifiéPar.formatter(),
+        attestationValue: {
+          format: 'application/pdf',
+        },
+      },
+    });
   };
 
   mediator.register('Éliminé.Command.NotifierÉliminé', handler);
