a76907ad2408dea74c3af0d92b0aa92bccc5cf7b
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-10-11 09:59:44 +0200

    🔧 Ajout de vm-browserify en fallback dans la config webpack

--- packages/applications/legacy/webpack.config.js
+++ packages/applications/legacy/webpack.config.js
@@ -43,6 +43,7 @@ module.exports = {
       path: require.resolve('path-browserify'),
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
+      vm: require.resolve('vm-browserify'),
     },
   },
   plugins: [new Dotenv()],
