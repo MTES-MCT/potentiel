df92ef4428251a77da48b3dc3db9c6dd5b6855d2
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-14 15:16:25 +0200

    ✏️ Fix peutEtreNotifiée

--- packages/applications/ssr/src/app/periodes/page.tsx
+++ packages/applications/ssr/src/app/periodes/page.tsx
@@ -120,9 +120,9 @@ const mapToProps = async ({
         appelOffre: période.identifiantPériode.appelOffre,
         période: période.identifiantPériode.période,
         identifiantPériode: période.identifiantPériode.formatter(),
-        peutÊtreNotifiée: période.estNotifiée
-          ? !!stats.restants?.total
-          : utilisateur.role.aLaPermission('période.notifier'),
+        peutÊtreNotifiée:
+          utilisateur.role.aLaPermission('période.notifier') &&
+          (période.estNotifiée ? !!stats.restants?.total : !!stats.tous.total),
         notifiéLe: période.estNotifiée ? période.notifiéeLe?.formatter() : undefined,
         notifiéPar: période.estNotifiée ? période.notifiéePar?.formatter() : undefined,
         stats,
