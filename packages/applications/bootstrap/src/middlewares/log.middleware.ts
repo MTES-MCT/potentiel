0bbe81e11d60008966444aa7a53ee5836e853205
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-15 11:08:56 +0200

    🔇 Réduire la verbosité du middleware des logs mediator

--- packages/applications/bootstrap/src/middlewares/log.middleware.ts
+++ packages/applications/bootstrap/src/middlewares/log.middleware.ts
@@ -7,18 +7,33 @@ import { DomainError } from '@potentiel-domain/core';
 
 export const logMiddleware: Middleware = async (message, next) => {
   const correlationId = randomUUID();
-  getLogger().info('Executing message', { message: JSON.stringify(message), correlationId });
+  getLogger().debug('Executing message', { message: JSON.stringify(message), correlationId });
   try {
     const result = await next();
-    getLogger().info('Message executed', { result: JSON.stringify(result), correlationId });
+    const resultJson = getResultJsonBody(message.type, result);
+    getLogger().debug('Message executed', {
+      messageType: message.type,
+      result: JSON.stringify(resultJson),
+      correlationId,
+    });
     return result;
   } catch (e) {
     if (e instanceof DomainError) {
-      getLogger().warn(e.message, { meta: e.meta });
+      getLogger().warn(e.message, { meta: e.meta, messageType: message.type, correlationId });
     } else {
-      getLogger().error(e as Error, { correlationId });
+      getLogger().error(e as Error, { correlationId, messageType: message.type });
     }
 
     throw e;
   }
+};
+
+const messagesToTruncate = [
+  'AppelOffre.Query.ListerAppelOffre',
+  'AppelOffre.Query.ConsulterAppelOffre',
+];
+// This is to avoid extremely long results from ConsulterAppelOffre, which makes the logs unreadable.
+const getResultJsonBody = (messageType: string, result: unknown) => {
+  if (messagesToTruncate.includes(messageType)) return {};
+  return result;
 };
