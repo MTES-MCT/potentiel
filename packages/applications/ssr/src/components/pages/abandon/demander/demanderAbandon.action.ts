d55e1111cde4fcde17c36648b639fb0b0cfcb971
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-10-16 15:28:07 +0200

    🐛 Fix: document optional when abandon with recandidature

--- packages/applications/ssr/src/components/pages/abandon/demander/demanderAbandon.action.ts
+++ packages/applications/ssr/src/components/pages/abandon/demander/demanderAbandon.action.ts
@@ -8,20 +8,29 @@ import { Routes } from '@potentiel-applications/routes';
 
 import { FormAction, formAction, FormState } from '@/utils/formAction';
 import { withUtilisateur } from '@/utils/withUtilisateur';
-import { document } from '@/utils/zod/documentTypes';
+import { document, optionalDocument } from '@/utils/zod/documentTypes';
 
-const schema = zod.object({
+const abandonWithRecandidatureSchema = zod.object({
   identifiantProjet: zod.string().min(1),
-  recandidature: zod.string().optional(),
+  recandidature: zod.literal('true'),
+  raison: zod.string().min(1, { message: 'Champ obligatoire' }),
+  pieceJustificative: optionalDocument,
+});
+
+const abandonWithoutRecandidatureSchema = zod.object({
+  identifiantProjet: zod.string().min(1),
+  recandidature: zod.literal('false'),
   raison: zod.string().min(1, { message: 'Champ obligatoire' }),
   pieceJustificative: document,
 });
+
+const schema = zod.union([abandonWithRecandidatureSchema, abandonWithoutRecandidatureSchema]);
 
 export type DemanderAbandonFormKeys = keyof zod.infer<typeof schema>;
 
 const action: FormAction<FormState, typeof schema> = async (
   previousState,
-  { identifiantProjet, pieceJustificative, recandidature, raison },
+  { identifiantProjet, raison, recandidature, pieceJustificative },
 ) => {
   return withUtilisateur(async (utilisateur) => {
     await mediator.send<Abandon.AbandonUseCase>({
