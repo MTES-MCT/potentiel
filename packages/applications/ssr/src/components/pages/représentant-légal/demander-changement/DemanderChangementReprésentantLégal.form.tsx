078bcc130a5a47e4fab4dca754bb9618f19449eb
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-10-17 15:47:28 +0200

    🎨 Add alert information

--- packages/applications/ssr/src/components/pages/représentant-légal/demander-changement/DemanderChangementReprésentantLégal.form.tsx
+++ packages/applications/ssr/src/components/pages/représentant-légal/demander-changement/DemanderChangementReprésentantLégal.form.tsx
@@ -21,7 +21,36 @@ export type DemanderChangementReprésentantLégalFormProps = {
 export const DemanderChangementReprésentantLégalForm: FC<
   DemanderChangementReprésentantLégalFormProps
 > = ({ identifiantProjet }) => {
-  const [, selectTypeDePersonne] = useState('');
+  type TypeDePersonne = 'Personne physique' | 'Personne morale' | 'Collectivité' | 'Autre';
+
+  const [typeDePersonne, selectTypeDePersonne] = useState<TypeDePersonne>();
+
+  const getNomReprésentantLégalHintText = () => {
+    switch (typeDePersonne) {
+      case 'Personne physique':
+        return 'les nom et prénom';
+      case 'Personne morale':
+        return 'le nom de la société';
+      case 'Collectivité':
+        return 'le nom de la collectivité';
+      case 'Autre':
+        return `le nom de l'organisme`;
+      default:
+        return 'le nom';
+    }
+  };
+  const getPièceJustificativeHintText = () => {
+    switch (typeDePersonne) {
+      case 'Personne physique':
+        return `Une copie de titre d'identité (carte d'identité ou passeport) en cours de validité`;
+      case 'Personne morale':
+        return 'Un extrait Kbis, pour les sociétés en cours de constitutionv une copie des statuts de la société en cours de constitution, une attestation de récépissé de dépôt de fonds pour constitution de capital social et une copie de l’acte désignant le représentant légal de la société';
+      case 'Collectivité':
+        return `Un extrait de délibération portant sur le projet objet de l'offre`;
+      default:
+        return `Tout document officiel permettant d'attester de l'existence juridique de la personne`;
+    }
+  };
 
   const [validationErrors, setValidationErrors] = useState<
     ValidationErrors<DemanderChangementReprésentantLégalFormKeys>
@@ -33,18 +62,18 @@ export const DemanderChangementReprésentantLégalForm: FC<
       method="POST"
       encType="multipart/form-data"
       onValidationError={(validationErrors) => setValidationErrors(validationErrors)}
-      actions={<SubmitButton>Demander le recours</SubmitButton>}
+      actions={<SubmitButton>Demander</SubmitButton>}
     >
       <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
 
       <SelectNext
-        label="Choisir le type de personne du représentant légal"
-        placeholder={`Sélectionner un projet`}
+        label="Choisir le type de personne pour le représentant légal"
+        placeholder={`Sélectionner le type de personne pour le représentant légal`}
         state={validationErrors['typeDePersonne'] ? 'error' : 'default'}
-        stateRelatedMessage="La sélection du projet est obligatoire"
+        stateRelatedMessage="Le type de personne pour le représentant légal est obligatoire"
         nativeSelectProps={{
           onChange: ({ currentTarget: { value } }) => {
-            selectTypeDePersonne(value);
+            selectTypeDePersonne(value as TypeDePersonne);
           },
         }}
         options={['Personne physique', 'Personne morale', 'Collectivité', 'Autre'].map((type) => ({
@@ -54,11 +83,10 @@ export const DemanderChangementReprésentantLégalForm: FC<
       />
 
       <Input
-        textArea
         label="Nom du représentant légal"
         id="nomRepresentantLegal"
-        hintText="Veuillez préciser le nom du nouveau représentant légal de votre projet"
-        nativeTextAreaProps={{
+        hintText={`Veuillez préciser ${getNomReprésentantLégalHintText()} pour le nouveau représentant légal de votre projet`}
+        nativeInputProps={{
           name: 'nomRepresentantLegal',
           required: true,
           'aria-required': true,
@@ -71,6 +99,7 @@ export const DemanderChangementReprésentantLégalForm: FC<
         label={'Pièce justificative'}
         id="pieceJustificative"
         name="pieceJustificative"
+        hintText={getPièceJustificativeHintText()}
         required
         state={validationErrors['pieceJustificative'] ? 'error' : 'default'}
         stateRelatedMessage={validationErrors['pieceJustificative']}
