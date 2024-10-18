cadef34e7f87638a7a60ae86006ba3fc43e53d80
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-10-18 12:01:52 +0200

    🚧 POC utilisation api.filigrane.beta.gouv.fr

---
+++ packages/applications/ssr/src/app/poc/filigrane/page.tsx
@@ -0,0 +1,9 @@
+import { AjouterFiligraneForm } from './AjouterFiligrane.form';
+
+export default async function Page() {
+  return (
+    <div className="mt-10 flex items-center justify-center">
+      <AjouterFiligraneForm />;
+    </div>
+  );
+}
