ebf34a35c5a9f7a1c2582e36e3ebe0f78bee4864
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-10-18 10:30:54 +0200

    💄 Page pour demander un changement de représentant légal (#2343)
    * ✨ Add demander changement représentant légal form page
    
    * 🎨 Add alert information
    
    * ♻️  Use ts-pattern match function

---
+++ packages/applications/ssr/src/components/pages/représentant-légal/demander-changement/DemanderChangementReprésentantLégal.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import {
+  DemanderChangementReprésentantLégalPage,
+  DemanderChangementReprésentantLégalPageProps,
+} from './DemanderChangementReprésentantLégal.page';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Pages/Représentant légal/Demander changement/DemanderChangementReprésentantLégalPage',
+  component: DemanderChangementReprésentantLégalPage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<DemanderChangementReprésentantLégalPageProps>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    identifiantProjet: 'PPE2 - Bâtiment#4#1#id-cre-738',
+  },
+};
