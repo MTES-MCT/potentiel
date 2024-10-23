f423f0be66344b546e993624a14b671113f49243
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-10-23 17:18:49 +0200

    💄 Espacement Form

--- packages/applications/ssr/src/components/pages/réseau/raccordement/importerDatesMiseEnService/importerDatesMiseEnService.form.tsx
+++ packages/applications/ssr/src/components/pages/réseau/raccordement/importerDatesMiseEnService/importerDatesMiseEnService.form.tsx
@@ -51,29 +51,23 @@ export const ImporterDatesMiseEnServiceForm: FC<
       successMessage={'date(s) de mise en service transmise(s)'}
       actions={<SubmitButton>Importer</SubmitButton>}
     >
-      <div className="flex flex-col gap-5">
-        <div>
-          <GestionnaireRéseauSelect
-            id="identifiantGestionnaireReseau"
-            name="identifiantGestionnaireReseau"
-            onGestionnaireRéseauSelected={({ identifiantGestionnaireRéseau }) => {
-              setIdentifiantGestionnaireReseau(identifiantGestionnaireRéseau);
-            }}
-            listeGestionnairesRéseau={listeGestionnairesRéseau}
-            identifiantGestionnaireRéseauActuel={identifiantGestionnaireRéseauActuel}
-          />
-        </div>
-        <div>
-          <DownloadDocument
-            className="mb-4"
-            url={Routes.Gestionnaire.téléchargerRaccordementEnAttenteMiseEnService(
-              identifiantGestionnaireReseau,
-            )}
-            format="csv"
-            label="Télécharger la liste des raccordements en attente de mise en service"
-          />
-        </div>
-      </div>
+      <GestionnaireRéseauSelect
+        id="identifiantGestionnaireReseau"
+        name="identifiantGestionnaireReseau"
+        onGestionnaireRéseauSelected={({ identifiantGestionnaireRéseau }) => {
+          setIdentifiantGestionnaireReseau(identifiantGestionnaireRéseau);
+        }}
+        listeGestionnairesRéseau={listeGestionnairesRéseau}
+        identifiantGestionnaireRéseauActuel={identifiantGestionnaireRéseauActuel}
+      />
+      <DownloadDocument
+        className="mb-4"
+        url={Routes.Gestionnaire.téléchargerRaccordementEnAttenteMiseEnService(
+          identifiantGestionnaireReseau,
+        )}
+        format="csv"
+        label="Télécharger la liste des raccordements en attente de mise en service"
+      />
       <UploadDocument
         label="Fichier des dates de mise en service"
         format="csv"
