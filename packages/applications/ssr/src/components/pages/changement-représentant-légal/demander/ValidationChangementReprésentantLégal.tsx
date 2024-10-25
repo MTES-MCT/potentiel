96de82215669a20ae884cec40c93cdb657a734d3
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-10-25 16:23:57 +0200

    🚨 Fix key

--- packages/applications/ssr/src/components/pages/changement-représentant-légal/demander/ValidationChangementReprésentantLégal.tsx
+++ packages/applications/ssr/src/components/pages/changement-représentant-légal/demander/ValidationChangementReprésentantLégal.tsx
@@ -33,11 +33,11 @@ export const ValidationChangementReprésentantLégal: FC<
         <div className="flex flex-row gap-2">
           <div>Pièces justificatives :</div>
           <div className="pl-2">
-            {piècesJustificatives.map((pièceJustificative, index) => (
-              <ul className="list-disc">
+            <ul className="list-disc">
+              {piècesJustificatives.map((pièceJustificative, index) => (
                 <li key={`pièce-justificative-${index}`}>{pièceJustificative}</li>
-              </ul>
-            ))}
+              ))}
+            </ul>
           </div>
         </div>
       </div>
