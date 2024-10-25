b0f0b38c15e0eedce76b02b40f5e39ef0b58d4ac
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-25 14:13:32 +0200

    ✏️ Renommer Groupe

--- packages/domain/utilisateur/src/groupe.valueType.ts
+++ packages/domain/utilisateur/src/groupe.valueType.ts
@@ -2,23 +2,23 @@ import { PlainType, ReadonlyValueType } from '@potentiel-domain/core';
 
 import { GroupeRefuséError } from './errors';
 
-export type GroupType = 'GestionnairesRéseau';
-export type RawType = `/${GroupType}/${string}`;
+export type GroupeType = 'GestionnairesRéseau';
+export type RawType = `/${GroupeType}/${string}`;
 
 export type ValueType = ReadonlyValueType<{
-  type: GroupType;
+  type: GroupeType;
   nom: string;
   formatter(): RawType;
 }>;
 
 // the regex accepts any letter character (not only A-Z) and whitespaces due to names like "EDF Réunion"
-const groupRegex = /\/(?<type>GestionnairesRéseau)\/(?<nom>[\p{L}0-9\-\s]+$)$/u;
+const groupeRegex = /\/(?<type>GestionnairesRéseau)\/(?<nom>[\p{L}0-9\-\s]+$)$/u;
 
 export const convertirEnValueType = (value: string): ValueType => {
   estValide(value);
-  const { nom, type } = value.match(groupRegex)!.groups!;
+  const { nom, type } = value.match(groupeRegex)!.groups!;
   return {
-    type: type as GroupType,
+    type: type as GroupeType,
     nom,
     estÉgaleÀ(valueType) {
       return valueType.type === this.type && valueType.nom === this.nom;
@@ -29,12 +29,12 @@ export const convertirEnValueType = (value: string): ValueType => {
   };
 };
 
-export const bind = ({ nom }: PlainType<ValueType>) => {
-  return convertirEnValueType(nom);
+export const bind = ({ nom, type }: PlainType<ValueType>) => {
+  return convertirEnValueType(`${type}/${nom}`);
 };
 
 export const estUnGroupeValide = (value: string) => {
-  return groupRegex.test(value);
+  return groupeRegex.test(value);
 };
 
 function estValide(value: string): asserts value is RawType {
