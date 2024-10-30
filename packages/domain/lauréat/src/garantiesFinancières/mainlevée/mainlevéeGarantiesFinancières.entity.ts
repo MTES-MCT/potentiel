ff07b6dddb936578d18f011308b0630a76101db3
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-10-30 12:12:24 +0100

    🚧 First commit poc projection

--- packages/domain/lauréat/src/garantiesFinancières/mainlevée/mainlevéeGarantiesFinancières.entity.ts
+++ packages/domain/lauréat/src/garantiesFinancières/mainlevée/mainlevéeGarantiesFinancières.entity.ts
@@ -10,15 +10,36 @@ export type MainlevéeGarantiesFinancièresEntity = Entity<
     période: string;
     famille?: string;
 
-    statut: string;
-    motif: string;
-    demande: { demandéeLe: string; demandéePar: string };
-    instruction?: { démarréeLe: string; démarréePar: string };
-    accord?: { accordéeLe: string; accordéePar: string; courrierAccord: { format: string } };
+    mainlevée: Array<{
+      statut: string;
+      motif: string;
+      demande: { demandéeLe: string; demandéePar: string };
+      instruction?: { démarréeLe: string; démarréePar: string };
+      accord?: { accordéeLe: string; accordéePar: string; courrierAccord: { format: string } };
+      rejet?: { rejetéLe: string; rejetéPar: string; courrierRejet: { format: string } };
 
-    dernièreMiseÀJour: {
-      date: string;
-      par: string;
-    };
+      dernièreMiseÀJour: {
+        date: string;
+        par: string;
+      };
+    }>;
+  }
+>;
+
+export type HistoriqueMainlevéeRejetéeGarantiesFinancièresEntity = Entity<
+  'historique-mainlevee-rejetee-garanties-financieres',
+  {
+    identifiantProjet: string;
+    nomProjet: string;
+    régionProjet: string;
+    appelOffre: string;
+    période: string;
+    famille?: string;
+
+    historique: Array<{
+      motif: string;
+      demande: { demandéeLe: string; demandéePar: string };
+      rejet: { rejetéLe: string; rejetéPar: string; courrierRejet: { format: string } };
+    }>;
   }
 >;
