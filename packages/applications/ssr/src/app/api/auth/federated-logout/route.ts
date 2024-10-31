72055c1a1b70d0b18bc3d5cbe26b42f4ad0d7fde
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-10-31 09:19:03 +0100

    🎨 Compat with legacy auth logout

--- packages/applications/ssr/src/app/api/auth/federated-logout/route.ts
+++ packages/applications/ssr/src/app/api/auth/federated-logout/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
+import { cookies } from 'next/headers';
 
 import { authOptions, issuerUrl } from '@/auth';
 
@@ -9,7 +10,8 @@ import { authOptions, issuerUrl } from '@/auth';
  * @see https://github.com/nextauthjs/next-auth/discussions/3938
  */
 export async function GET() {
-  const { NEXTAUTH_URL = '' } = process.env;
+  const { NEXTAUTH_URL = '', NEXT_AUTH_SESSION_TOKEN_COOKIE_NAME = 'next-auth.session-token' } =
+    process.env;
 
   // Gets the session, with idToken
   const session = await getServerSession({
@@ -29,10 +31,12 @@ export async function GET() {
   // after keycloak logout, redirect the user to this route to remove the session
   const redirectUrl = new URL('/auth/signOut', NEXTAUTH_URL);
   const ssoLogoutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`);
-  ssoLogoutUrl.searchParams.set('post_logout_redirect_uri', redirectUrl.toString());
+
+  cookies().delete(NEXT_AUTH_SESSION_TOKEN_COOKIE_NAME);
 
   if (session.idToken) {
     // without this, Keycloak prompts the user for confirmation
+    ssoLogoutUrl.searchParams.set('post_logout_redirect_uri', redirectUrl.toString());
     ssoLogoutUrl.searchParams.set('id_token_hint', session.idToken);
   }
 
