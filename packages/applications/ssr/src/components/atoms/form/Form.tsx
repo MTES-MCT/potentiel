50059b32407053983122af31f25f57441b77e554
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-10-31 15:43:10 +0100

    🐛 Pending modal pass children

--- packages/applications/ssr/src/components/atoms/form/Form.tsx
+++ packages/applications/ssr/src/components/atoms/form/Form.tsx
@@ -50,26 +50,22 @@ export const Form: FC<FormProps> = ({
     // eslint-disable-next-line react/no-unknown-property
     <form id={id} action={formAction} onInvalid={onInvalid} onError={onError}>
       {heading && <Heading2 className="mb-4">{heading}</Heading2>}
-
       <FormFeedback formState={state} successMessage={successMessage} />
 
       {pendingModal && (
         <FormPendingModal id={pendingModal.id} title={pendingModal.title}>
-          {children ?? null}
+          {pendingModal.children}
         </FormPendingModal>
       )}
-
       {!omitMandatoryFieldsLegend && (
         <div className="text-sm italic my-4">
           Sauf mention contraire "(optionnel)" dans le label, tous les champs sont obligatoires
         </div>
       )}
-
       <div className="flex flex-col gap-5">
         {children}
         <div className="flex flex-col md:flex-row gap-2">{actions}</div>
       </div>
-
       <FormFeedbackCsvErrors formState={state} />
     </form>
   );
