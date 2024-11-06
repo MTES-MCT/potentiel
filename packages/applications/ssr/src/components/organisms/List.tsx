794134e97113375555122e2a34742af4e00a421a
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-11-06 15:01:58 +0100

    🐛 Fix: list for période

--- packages/applications/ssr/src/components/organisms/List.tsx
+++ packages/applications/ssr/src/components/organisms/List.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { FC } from 'react';
 
 import { Tile } from './Tile';
@@ -23,7 +24,7 @@ export const List = <TItem,>({
       {items.map(({ key, ...item }) => (
         <li className="mb-6" key={key}>
           <Tile className="flex flex-col md:flex-row md:justify-between">
-            {ItemComponent(item)}
+            <ItemComponent {...item} />
           </Tile>
         </li>
       ))}
