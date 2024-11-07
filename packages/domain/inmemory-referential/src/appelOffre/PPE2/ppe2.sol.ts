3db6a611d1c31b31b174edf2ed44b2935e33b7f2
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-11-07 17:09:44 +0100

    ✨ Ajout de la période 6 de l'AO PPE2 Sol (#2422)

--- packages/domain/inmemory-referential/src/appelOffre/PPE2/ppe2.sol.ts
+++ packages/domain/inmemory-referential/src/appelOffre/PPE2/ppe2.sol.ts
@@ -271,5 +271,39 @@ Le Candidat peut également être délié de cette obligation selon l’appréci
       },
       cahiersDesChargesModifiésDisponibles: [],
     },
+    {
+      id: '6',
+      title: 'sixième',
+      certificateTemplate: 'ppe2.v2',
+      ministère: 'MCE',
+      validateurParDéfaut: validateurParDéfaut.hermine,
+      cahierDesCharges: {
+        référence: '2024/S 422369-2024',
+      },
+      delaiDcrEnMois: { valeur: 3, texte: 'trois' },
+      familles: [],
+      noteThresholdBy: 'category',
+      noteThreshold: {
+        volumeReserve: {
+          noteThreshold: 18.97,
+          puissanceMax: 5,
+        },
+        autres: {
+          noteThreshold: 26.98,
+        },
+      },
+      donnéesCourriersRéponse: {
+        texteEngagementRéalisationEtModalitésAbandon: {
+          référenceParagraphe: '6.2',
+          dispositions: `Le Candidat dont l’offre a été retenue met en service l’Installation dans les conditions du présent cahier des charges, et réalise l’Installation conformément aux éléments du dossier de candidature (les possibilités et modalités de modification sont indiquées au 5.2).
+Par exception, le Candidat est délié de cette obligation :
+˗ en cas de retrait de l’autorisation par l’autorité compétente ou d’annulation de cette autorisation à la suite d’un contentieux. Les retraits gracieux sur demande du candidat ne sont pas concernés.
+˗ en cas de non obtention ou de retrait de toute autre autorisation administrative ou dérogation nécessaire à la réalisation du projet.
+Il en informe dans ce cas le Préfet en joignant les pièces justificatives. La garantie financière est alors levée.
+Le Candidat peut également être délié de cette obligation selon l’appréciation du ministre chargé de l’énergie à la suite d’une demande dûment justifiée. L’État peut toutefois prélever la totalité ou une partie de la garantie financière dans les conditions du paragraphe 5.1. L’accord du Ministre, les conditions imposées et le prélèvement de la garantie financière ne limitent pas la possibilité de recours de l’État aux sanctions du 8.2.`,
+        },
+      },
+      cahiersDesChargesModifiésDisponibles: [],
+    },
   ],
 };
