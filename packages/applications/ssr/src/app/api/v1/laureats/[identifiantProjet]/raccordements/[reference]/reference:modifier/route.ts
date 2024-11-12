c3aa820c7b5475afb47bdb375876b414706d89e3
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2024-11-12 11:34:21 +0100

    ✨ API Web Raccordement (#2417)
    * ✨ [API] Lister dossiers de raccordement
    
    * 🚧 WIP
    
    * ✨ Map fields
    
    * ✨ [API] Modifier une référence de dossier de raccordement
    
    * ♻️ Retour d'erreur formatté
    
    * 🔇 Remove log
    
    ---------
    
    Co-authored-by: Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>

---
+++ packages/applications/ssr/src/app/api/v1/laureats/[identifiantProjet]/raccordements/[reference]/reference:modifier/route.ts
@@ -0,0 +1,55 @@
+import { mediator } from 'mediateur';
+import { NextRequest, NextResponse } from 'next/server';
+import * as zod from 'zod';
+
+import { Raccordement } from '@potentiel-domain/reseau';
+
+import { apiAction } from '@/utils/apiAction';
+import { withUtilisateur } from '@/utils/withUtilisateur';
+
+type RouteParams = {
+  params: {
+    identifiantProjet: string;
+    reference: string;
+  };
+};
+
+const schema = zod.object({
+  identifiantProjet: zod.string().min(1),
+  reference: zod.string().min(1),
+  nouvelleReference: zod.string().min(1, { message: 'Champ obligatoire' }),
+});
+
+export const POST = (request: NextRequest, routeParams: RouteParams) =>
+  apiAction(async () => {
+    return withUtilisateur(async (utilisateur) => {
+      const result = schema.safeParse({
+        ...(await request.json()),
+        ...routeParams.params,
+      });
+
+      if (result.success) {
+        const { nouvelleReference, identifiantProjet, reference } = result.data;
+        await mediator.send<Raccordement.RaccordementUseCase>({
+          type: 'Réseau.Raccordement.UseCase.ModifierRéférenceDossierRaccordement',
+          data: {
+            identifiantProjetValue: identifiantProjet,
+            référenceDossierRaccordementActuelleValue: reference,
+            nouvelleRéférenceDossierRaccordementValue: nouvelleReference,
+            rôleValue: utilisateur.role.nom,
+          },
+        });
+
+        return new NextResponse(null, { status: 200 });
+      }
+
+      return NextResponse.json(
+        {
+          error: result.error.format(),
+        },
+        {
+          status: 400,
+        },
+      );
+    });
+  });
---
+++ packages/applications/ssr/src/app/api/v1/reseaux/raccordements/route.ts
@@ -0,0 +1,99 @@
+import { mediator } from 'mediateur';
+import { z } from 'zod';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { Option } from '@potentiel-libraries/monads';
+import { Raccordement } from '@potentiel-domain/reseau';
+import { Role, Utilisateur } from '@potentiel-domain/utilisateur';
+import { RangeOptions } from '@potentiel-domain/entity';
+
+import { withUtilisateur } from '@/utils/withUtilisateur';
+import { apiAction } from '@/utils/apiAction';
+import { mapToRangeOptions } from '@/utils/pagination';
+
+export const dynamic = 'force-dynamic';
+
+const routeParamsSchema = z.object({
+  page: z.coerce.number().int().optional(),
+  avecDateMiseEnService: z
+    .string()
+    .optional()
+    .refine((value) => value === 'true' || value === 'false' || value === undefined, {
+      message: "Le paramètre avecDateMiseEnService doit être 'true' ou 'false'",
+    })
+    .transform((value) => (value === 'true' ? true : value === 'false' ? false : undefined)),
+});
+
+type DossierRaccordementApiResponse = {
+  nomProjet: string;
+  identifiantProjet: string;
+  appelOffre: string;
+  periode: string;
+  famille: string;
+  numeroCRE: string;
+  commune: string;
+  codePostal: string;
+  referenceDossier: string;
+  statutDGEC: string;
+};
+
+type ApiResponse = {
+  items: DossierRaccordementApiResponse[];
+  range: RangeOptions;
+  total: number;
+};
+
+export const GET = (request: NextRequest) =>
+  apiAction(() =>
+    withUtilisateur(async (utilisateur) => {
+      const { searchParams } = new URL(request.url);
+      const { avecDateMiseEnService, page } = routeParamsSchema.parse(
+        Object.fromEntries(searchParams.entries()),
+      );
+      const result = await mediator.send<Raccordement.ListerDossierRaccordementQuery>({
+        type: 'Réseau.Raccordement.Query.ListerDossierRaccordementQuery',
+        data: {
+          identifiantGestionnaireRéseau: récupérerIdentifiantGestionnaireUtilisateur(utilisateur),
+          avecDateMiseEnService,
+          range: page
+            ? mapToRangeOptions({
+                currentPage: page,
+              })
+            : undefined,
+        },
+      });
+      return NextResponse.json(mapToApiResponse(result));
+    }),
+  );
+
+const récupérerIdentifiantGestionnaireUtilisateur = (utilisateur: Utilisateur.ValueType) => {
+  if (!utilisateur.role.estÉgaleÀ(Role.grd)) {
+    return;
+  }
+  if (Option.isNone(utilisateur.groupe)) {
+    return 'inconnu';
+  }
+  if (utilisateur.groupe.type !== 'GestionnairesRéseau') {
+    return 'inconnu';
+  }
+  return utilisateur.groupe.nom;
+};
+
+type MapToApiResponse = (dossiers: Raccordement.ListerDossierRaccordementReadModel) => ApiResponse;
+
+const mapToApiResponse: MapToApiResponse = ({ items, range, total }) => ({
+  total,
+  range,
+  items: items.map((dossier) => ({
+    identifiantProjet: dossier.identifiantProjet.formatter(),
+    nomProjet: dossier.nomProjet,
+    appelOffre: dossier.appelOffre,
+    periode: dossier.période,
+    famille: dossier.famille,
+    numeroCRE: dossier.numéroCRE,
+    commune: dossier.commune,
+    codePostal: dossier.codePostal,
+    statutDGEC: dossier.statutDGEC,
+    referenceDossier: dossier.référenceDossier.formatter(),
+  })),
+});
