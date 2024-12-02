7578595768b57609b9e717c15d259116151a6127
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-12-02 10:31:04 +0100

    ✅ Impossible de demander le changement de représentant légal d'un projet lauréat si le changement est déjà en cours

--- packages/specifications/src/projet/lauréat/représentant-légal/demandeChangement/stepDefinitions/demandeChangementReprésentantLégal.given.ts
+++ packages/specifications/src/projet/lauréat/représentant-légal/demandeChangement/stepDefinitions/demandeChangementReprésentantLégal.given.ts
@@ -1,52 +1,39 @@
-// import { Given as EtantDonné } from '@cucumber/cucumber';
-// import { mediator } from 'mediateur';
+import { Given as EtantDonné } from '@cucumber/cucumber';
+import { mediator } from 'mediateur';
 
-// import { ReprésentantLégal } from '@potentiel-domain/laureat';
-// import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { ReprésentantLégal } from '@potentiel-domain/laureat';
 
-// import { PotentielWorld } from '../../../../potentiel.world';
+import { PotentielWorld } from '../../../../../potentiel.world';
 
-// EtantDonné(
-//   /une demande de changement de représentant légal en cours pour le projet lauréat/,
-//   async function (this: PotentielWorld) {
-//     await créerDemandeChangementReprésentantLégal.call(this);
-//   },
-// );
+EtantDonné(
+  /une demande de changement de représentant légal en cours pour le projet lauréat/,
+  async function (this: PotentielWorld) {
+    await créerDemandeChangementReprésentantLégal.call(this);
+  },
+);
 
-// export async function créerDemandeChangementReprésentantLégal(this: PotentielWorld) {
-//   const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
+export async function créerDemandeChangementReprésentantLégal(this: PotentielWorld) {
+  const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
 
-//   const { raison, demandéLe, demandéPar, pièceJustificative, recandidature } =
-//     this.lauréatWorld.représentantLégalWorld.demanderChangementReprésentantLégal.créer({
-//       identifiantProjet,
-//     });
+  const {
+    nomReprésentantLégal,
+    typeReprésentantLégal,
+    piècesJustificative,
+    demandéLe,
+    demandéPar,
+  } = this.lauréatWorld.représentantLégalWorld.demanderChangementReprésentantLégalFixture.créer({
+    identifiantProjet,
+  });
 
-//   /*
-//     identifiantProjetValue: string;
-//     nomReprésentantLégalValue: string;
-//     typeReprésentantLégalValue: string;
-//     piècesJustificativeValue: Array<{
-//       content: ReadableStream;
-//       format: string;
-//     }>;
-//     identifiantUtilisateurValue: string;
-//     dateDemandeValue: string;
-
-// */
-
-//   await mediator.send<ReprésentantLégal.ReprésentantLégalUseCase>({
-//     type: 'Lauréat.ReprésentantLégal.UseCase.DemanderChangementReprésentantLégal',
-//     data: {
-//       identifiantProjetValue: identifiantProjet,
-//     },
-//   });
-
-//   await mediator.send<ReprésentantLégal.ReprésentantLégalCommand>({
-//     type: 'Lauréat.ReprésentantLégal.Command.ImporterReprésentantLégal',
-//     data: {
-//       identifiantProjet: IdentifiantProjet.convertirEnValueType(identifiantProjet),
-//       importéLe: DateTime.convertirEnValueType(importéLe),
-//       importéPar: Email.system(),
-//     },
-//   });
-// }
+  await mediator.send<ReprésentantLégal.ReprésentantLégalUseCase>({
+    type: 'Lauréat.ReprésentantLégal.UseCase.DemanderChangementReprésentantLégal',
+    data: {
+      identifiantProjetValue: identifiantProjet,
+      nomReprésentantLégalValue: nomReprésentantLégal,
+      typeReprésentantLégalValue: typeReprésentantLégal.formatter(),
+      piècesJustificativeValue: piècesJustificative,
+      identifiantUtilisateurValue: demandéPar,
+      dateDemandeValue: demandéLe,
+    },
+  });
+}
