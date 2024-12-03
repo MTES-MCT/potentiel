35b463a4b4ea711aac8306ba3ac7f4c6f62ae352
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-12-03 17:31:13 +0100

    🐛 Fix

--- packages/applications/request-context/src/openid.ts
+++ packages/applications/request-context/src/openid.ts
@@ -6,19 +6,21 @@ import { clientId, clientSecret, issuerUrl } from './constants';
 let openIdIssuerPromise: Promise<Issuer<BaseClient>>;
 let jwksPromise: Promise<KeyLike>;
 
-export const getOpenIdClient = async () => {
+const getOpenIdIssuer = () => {
   if (!openIdIssuerPromise) {
     openIdIssuerPromise = Issuer.discover(issuerUrl);
   }
-  const { Client } = await openIdIssuerPromise;
+  return openIdIssuerPromise;
+};
+export const getOpenIdClient = async () => {
+  const { Client } = await getOpenIdIssuer();
   return new Client({ client_id: clientId, client_secret: clientSecret });
 };
 
 export const getJwks = async () => {
   if (!jwksPromise) {
-    jwksPromise = openIdIssuerPromise.then((issuer) =>
-      createRemoteJWKSet(new URL(issuer.metadata.jwks_uri!))({ alg: 'RS256' }),
-    );
+    const issuer = await getOpenIdIssuer();
+    jwksPromise = createRemoteJWKSet(new URL(issuer.metadata.jwks_uri!))({ alg: 'RS256' });
   }
   return jwksPromise;
 };
