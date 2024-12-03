9c3ea94f9a2f666c869cbaee546093e7440c0416
Hubert MONCENIS <hubmoncenis@gmail.com>
2024-12-03 10:07:03 +0100

    🚧 WIP

--- packages/applications/routes/src/lauréat/représentantLégal.routes.ts
+++ packages/applications/routes/src/lauréat/représentantLégal.routes.ts
@@ -1,5 +1,8 @@
 import { encodeParameter } from '../encodeParameter';
 
+export const détail = (identifiantProjet: string) =>
+  `/laureats/${encodeParameter(identifiantProjet)}/representant-legal`;
+
 export const modifier = (identifiantProjet: string) =>
   `/laureats/${encodeParameter(identifiantProjet)}/representant-legal/modifier`;
 
