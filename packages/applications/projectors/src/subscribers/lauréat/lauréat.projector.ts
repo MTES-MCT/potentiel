504680184a1589e440574561a2b8edab4aa9d733
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-12-05 14:13:17 +0100

    🎨 Remove from laureat projector

--- packages/applications/projectors/src/subscribers/lauréat/lauréat.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/lauréat.projector.ts
@@ -2,9 +2,6 @@ import { Message, MessageHandler, mediator } from 'mediateur';
 
 import { Lauréat } from '@potentiel-domain/laureat';
 import { RebuildTriggered, Event } from '@potentiel-infrastructure/pg-event-sourcing';
-import { findProjection } from '@potentiel-infrastructure/pg-projections';
-import { DateTime } from '@potentiel-domain/common';
-import { Option } from '@potentiel-libraries/monads';
 
 import { upsertProjection } from '../../infrastructure/upsertProjection';
 import { removeProjection } from '../../infrastructure';
@@ -20,31 +17,16 @@ export const register = () => {
     if (type === 'RebuildTriggered') {
       await removeProjection<Lauréat.LauréatEntity>(`lauréat|${payload.id}`);
     } else {
-      const { identifiantProjet } = payload;
-
-      const lauréat = await findProjection<Lauréat.LauréatEntity>(`lauréat|${identifiantProjet}`);
-
-      const lauréatDefaultValue: Omit<Lauréat.LauréatEntity, 'type'> = {
-        identifiantProjet,
-        notifiéLe: DateTime.now().formatter(),
-        notifiéPar: '',
-        actionnaire: { nom: '', dernièreMiseÀJourLe: DateTime.now().formatter() },
-      };
-
-      const lauréatToUpsert: Omit<Lauréat.LauréatEntity, 'type'> = Option.isSome(lauréat)
-        ? lauréat
-        : lauréatDefaultValue;
-
       switch (type) {
         case 'LauréatNotifié-V1':
-          const { notifiéLe, notifiéPar } = payload;
+          const { identifiantProjet, notifiéLe, notifiéPar } = payload;
 
           await upsertProjection<Lauréat.LauréatEntity>(`lauréat|${identifiantProjet}`, {
-            ...lauréatToUpsert,
             identifiantProjet,
             notifiéLe,
             notifiéPar,
             représentantLégal: undefined,
+            actionnaire: undefined,
           });
           break;
       }
