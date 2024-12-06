0f0be8c52af54b17560a994b98ad87e1e373eb89
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-12-06 16:03:24 +0100

    🐛 Exécuter les retry après les register (#2500)

--- packages/infrastructure/pg-event-sourcing/src/index.ts
+++ packages/infrastructure/pg-event-sourcing/src/index.ts
@@ -2,5 +2,5 @@ export { Event } from './event';
 export { loadAggregate } from './load/loadAggregate';
 export { loadFromStream } from './load/loadFromStream';
 export { publish } from './publish/publish';
-export { subscribe } from './subscribe/subscribe';
+export { subscribe, executeSubscribersRetry } from './subscribe/subscribe';
 export { RebuildTriggered } from './subscribe/rebuild/rebuildTriggered.event';
