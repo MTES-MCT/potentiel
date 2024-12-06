782e691d8d98c719e395e4ab8c456c53229e4848
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-12-06 10:10:09 +0100

    🐛 Fix select

--- packages/domain/entity/src/find.ts
+++ packages/domain/entity/src/find.ts
@@ -3,10 +3,11 @@ import { Option } from '@potentiel-libraries/monads';
 import { Entity } from './entity';
 import { SelectOptions } from './selectOptions';
 
-export type Find = <TEntity extends Entity>(
-  id: `${TEntity['type']}|${string}`,
-) => Promise<Option.Type<TEntity>>;
-
 export type FindOptions<TEntity extends Entity> = {
   select?: SelectOptions<Omit<TEntity, 'type'>>;
 };
+
+export type Find = <TEntity extends Entity>(
+  id: `${TEntity['type']}|${string}`,
+  options?: FindOptions<TEntity>,
+) => Promise<Option.Type<TEntity>>;
