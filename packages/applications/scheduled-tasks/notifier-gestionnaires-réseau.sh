735fbb31205bbb6f6becbf7a608b6a6343cc74d6
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2024-12-10 11:42:53 +0100

    🎨 Move back to scheduled-tasks

---
+++ packages/applications/scheduled-tasks/notifier-gestionnaires-réseau.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -l
+
+if [ -z $EVENT_STORE_CONNECTION_STRING ]
+then
+    echo "Connection is missing!"
+    exit 1
+fi
+
+echo "Launching job [NOTIFIER GESTIONNAIRES RESEAU]"
+
+npm run start:notifier-gestionnaires-réseau -w @potentiel-applications/scheduled-tasks
+
+exit 0
