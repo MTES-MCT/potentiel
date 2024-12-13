d66b3e270627c1d9ee35c159011675037318651d
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2024-12-13 10:50:49 +0100

    🚚 Rename function

--- packages/applications/ssr/src/utils/zod/blob/cannotExceedSize.ts
+++ packages/applications/ssr/src/utils/zod/blob/cannotExceedSize.ts
@@ -1,7 +1,7 @@
 export const fileSizeLimitInMegaBytes = 5;
-const toBytes = (sizeInMegaBytes: number): number => sizeInMegaBytes * 1024 * 1024;
+const fromMegaBytesToBytes = (sizeInMegaBytes: number): number => sizeInMegaBytes * 1024 * 1024;
 
-const refine = ({ size }: Blob) => size <= toBytes(fileSizeLimitInMegaBytes);
+const refine = ({ size }: Blob) => size <= fromMegaBytesToBytes(fileSizeLimitInMegaBytes);
 const message = `Le fichier dépasse la taille maximale autorisée (${fileSizeLimitInMegaBytes}Mo)`;
 
 export const cannotExceedSize = { refine, message };
