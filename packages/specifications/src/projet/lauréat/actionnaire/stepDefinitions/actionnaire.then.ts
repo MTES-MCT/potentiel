185551de722f6f8e925ca3c3d58d3e9314b259fa
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2024-12-17 10:21:17 +0100

    🎨 Details

--- packages/specifications/src/projet/lauréat/actionnaire/stepDefinitions/actionnaire.then.ts
+++ packages/specifications/src/projet/lauréat/actionnaire/stepDefinitions/actionnaire.then.ts
@@ -1,6 +1,7 @@
 import { Then as Alors } from '@cucumber/cucumber';
 import { mediator } from 'mediateur';
 import waitForExpect from 'wait-for-expect';
+import { expect } from 'chai';
 
 import { mapToPlainObject } from '@potentiel-domain/core';
 import { Actionnaire } from '@potentiel-domain/laureat';
@@ -75,7 +76,7 @@ Alors(
         },
       });
 
-      actual.should.be.deep.equal(Option.none);
+      expect(Option.isNone(actual));
     });
   },
 );
