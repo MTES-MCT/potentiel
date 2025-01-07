cce5da791e2c0a504aff1b2bbcb7a3c4c9461dc8
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-01-07 10:35:09 +0100

    🐛 Correction de l'upload optionel

--- packages/applications/ssr/src/utils/zod/blob/acceptOnlyFileTypes.ts
+++ packages/applications/ssr/src/utils/zod/blob/acceptOnlyFileTypes.ts
@@ -2,8 +2,8 @@ export type FileTypes = 'application/pdf' | 'text/csv';
 
 const refine =
   (fileTypes: Array<FileTypes>) =>
-  ({ type }: Blob) =>
-    fileTypes.includes(type as FileTypes);
+  ({ type, size }: Blob) =>
+    size === 0 || fileTypes.includes(type as FileTypes);
 
 const message = (fileTypes: Array<FileTypes>) =>
   `Seuls les fichiers ${fileTypes.map((fileType) => fileType.replace('application/', '')).join(',')} sont autorisés`;
