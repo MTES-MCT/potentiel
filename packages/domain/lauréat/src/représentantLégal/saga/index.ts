e0bbdcdf6fe6ab93c17c273972c13d213abeed8b
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-01-13 11:32:01 +0100

    ✨ Supprimer la demande et la tâche planifiée changement de de représentant légal quand attestation conformité transmise (#2601)
    * 🧪 Add failing scenario
    
    * ✅ Pass the scenario
    
    * ♻️  Refactoring about saga

--- packages/domain/lauréat/src/représentantLégal/saga/index.ts
+++ packages/domain/lauréat/src/représentantLégal/saga/index.ts
@@ -4,17 +4,19 @@ import { match } from 'ts-pattern';
 import { TâchePlanifiéeExecutéeEvent } from '@potentiel-domain/tache-planifiee';
 
 import { LauréatNotifiéEvent } from '../../lauréat';
-import { TypeTâchePlanifiéeChangementReprésentantLégal } from '..';
 import { AbandonAccordéEvent } from '../../abandon';
+import { AttestationConformitéTransmiseEvent } from '../../achèvement';
 
 import { handleLauréatNotifié } from './handleLauréatNotifié';
 import { handleTâchePlanifiéeGestionAutomatiqueDemandeChangementExecutée } from './handleTâchePlanifiéeGestionAutomatiqueDemandeChangementExecutée';
 import { handleAbandonAccordé } from './handleAbandonAccordé';
+import { handleAttestationConformitéTransmise } from './handleAttestationConformitéTransmise';
 
 export type SubscriptionEvent =
   | LauréatNotifiéEvent
   | TâchePlanifiéeExecutéeEvent
-  | AbandonAccordéEvent;
+  | AbandonAccordéEvent
+  | AttestationConformitéTransmiseEvent;
 
 export type Execute = Message<'System.Lauréat.ReprésentantLégal.Saga.Execute', SubscriptionEvent>;
 
@@ -25,16 +27,12 @@ export const register = () => {
       .with(
         {
           type: 'TâchePlanifiéeExecutée-V1',
-          payload: {
-            typeTâchePlanifiée:
-              TypeTâchePlanifiéeChangementReprésentantLégal.gestionAutomatiqueDemandeChangement
-                .type,
-          },
         },
         handleTâchePlanifiéeGestionAutomatiqueDemandeChangementExecutée,
       )
       .with({ type: 'AbandonAccordé-V1' }, handleAbandonAccordé)
-      .otherwise(() => {});
+      .with({ type: 'AttestationConformitéTransmise-V1' }, handleAttestationConformitéTransmise)
+      .exhaustive();
 
   mediator.register('System.Lauréat.ReprésentantLégal.Saga.Execute', handler);
 };
