1e2a43f24006417f2bd5bda063f615368dcb5159
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-01-16 16:35:51 +0100

    🎨 Handle rebuild

--- packages/applications/projectors/src/subscribers/lauréat/actionnaire/handleRebuildTriggered.ts
+++ packages/applications/projectors/src/subscribers/lauréat/actionnaire/handleRebuildTriggered.ts
@@ -1,8 +1,21 @@
 import { RebuildTriggered } from '@potentiel-infrastructure/pg-event-sourcing';
 import { Actionnaire } from '@potentiel-domain/laureat';
+import { Where } from '@potentiel-domain/entity';
+import { listProjection } from '@potentiel-infrastructure/pg-projections';
 
 import { removeProjection } from '../../../infrastructure';
 
 export const handleRebuilTriggered = async ({ payload: { id } }: RebuildTriggered) => {
   await removeProjection<Actionnaire.ActionnaireEntity>(`actionnaire|${id}`);
+
+  const demandeChangementActionnaire =
+    await listProjection<Actionnaire.ChangementActionnaireEntity>(`changement-actionnaire`, {
+      where: { identifiantProjet: Where.equal(id) },
+    });
+
+  for (const changement of demandeChangementActionnaire.items) {
+    await removeProjection<Actionnaire.ChangementActionnaireEntity>(
+      `changement-actionnaire|${id}#${changement.demande.demandéeLe}`,
+    );
+  }
 };
