d44316f7555e711975ef2f0e0280ea4b862e8ee8
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-01-20 12:00:58 +0100

    🎨 Ne plus supprimer demande si achevement

--- packages/domain/lauréat/src/actionnaire/saga/index.ts
+++ packages/domain/lauréat/src/actionnaire/saga/index.ts
@@ -3,16 +3,11 @@ import { match } from 'ts-pattern';
 
 import { LauréatNotifiéEvent } from '../../lauréat';
 import { AbandonAccordéEvent } from '../../abandon';
-import { AttestationConformitéTransmiseEvent } from '../../achèvement';
 
 import { handleLauréatNotifié } from './handleLauréatNotifié';
 import { handleAbandonAccordé } from './handleAbandonAccordé';
-import { handleAttestationConformitéTransmise } from './handleAttestationConformitéTransmise';
 
-export type SubscriptionEvent =
-  | LauréatNotifiéEvent
-  | AbandonAccordéEvent
-  | AttestationConformitéTransmiseEvent;
+export type SubscriptionEvent = LauréatNotifiéEvent | AbandonAccordéEvent;
 
 export type Execute = Message<'System.Lauréat.Actionnaire.Saga.Execute', SubscriptionEvent>;
 
@@ -21,7 +16,6 @@ export const register = () => {
     match(event)
       .with({ type: 'LauréatNotifié-V1' }, handleLauréatNotifié)
       .with({ type: 'AbandonAccordé-V1' }, handleAbandonAccordé)
-      .with({ type: 'AttestationConformitéTransmise-V1' }, handleAttestationConformitéTransmise)
       .exhaustive();
 
   mediator.register('System.Lauréat.Actionnaire.Saga.Execute', handler);
