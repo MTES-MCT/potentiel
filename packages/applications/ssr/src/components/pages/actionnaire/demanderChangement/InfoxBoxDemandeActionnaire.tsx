dc024cb25a836c3dde5c21feb022e35f03db0931
Violette <27735540+VioMrqs@users.noreply.github.com>
2025-01-21 14:56:29 +0100

    🔀 Intégration des modifications de la release 3.38 (#2637)

---
+++ packages/applications/ssr/src/components/pages/actionnaire/demanderChangement/InfoxBoxDemandeActionnaire.tsx
@@ -0,0 +1,22 @@
+import Alert from '@codegouvfr/react-dsfr/Alert';
+import { FC } from 'react';
+
+export const InfoBoxDemandeActionnaire: FC = () => (
+  <Alert
+    severity="info"
+    small
+    description={
+      <div className="p-3">
+        Votre demande de modification de l'actionnariat nécessite une instruction si votre projet
+        remplit <span className="font-semibold">une</span> des conditions suivantes :
+        <ul className="list-disc list-inside mb-2">
+          <li>l'actionnariat est de type financement ou investissement participatif</li>
+          <li>
+            il n'y a pas de garanties financières validées sur le projet ou il y a une demande de
+            renouvellement ou de modification des garanties financières en cours de traitement
+          </li>
+        </ul>
+      </div>
+    }
+  />
+);
