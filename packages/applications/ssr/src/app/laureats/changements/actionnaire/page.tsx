e7ff62fe3c1c29706f187c08eef327271927c0c0
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-01-22 16:28:01 +0100

    🎨 Stupid hobbit

--- packages/applications/ssr/src/app/laureats/changements/actionnaire/page.tsx
+++ packages/applications/ssr/src/app/laureats/changements/actionnaire/page.tsx
@@ -85,7 +85,6 @@ export default async function Page({ searchParams }: PageProps) {
   );
 }
 
-// viovio
 const mapToListProps = (
   readModel: Actionnaire.ListerChangementActionnaireReadModel,
 ): ChangementActionnaireListPageProps['list'] => {
