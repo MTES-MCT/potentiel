0de21320164bd6d59691ebb7550aec78d3816bc6
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-01-22 15:33:08 +0100

    🎨 Col flex

--- packages/applications/ssr/src/components/molecules/historique/timeline/actionnaire/mapToActionnaireModifiéTimelineItemsProps.tsx
+++ packages/applications/ssr/src/components/molecules/historique/timeline/actionnaire/mapToActionnaireModifiéTimelineItemsProps.tsx
@@ -15,7 +15,7 @@ export const mapToActionnaireModifiéTimelineItemProps = (
     date: modifiéLe,
     title: <div>Actionnaire modifié par {<span className="font-semibold">{modifiéPar}</span>}</div>,
     content: (
-      <div className="flex gap-2">
+      <div className="flex flex-col gap-2">
         <div>
           Nouvel actionnaire : <span className="font-semibold">{actionnaire}</span>
         </div>
