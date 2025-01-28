50fb894b1dce5f86460278ca97f6f213f7372e5c
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-01-28 17:26:06 +0100

    🚧 WIP specs

---
+++ packages/domain/lauréat/src/actionnaire/changement/enregistrerChangement/enregistrerChangement.behavior.ts
@@ -0,0 +1,117 @@
+import { DomainEvent } from '@potentiel-domain/core';
+import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { DocumentProjet } from '@potentiel-domain/document';
+
+import { ActionnaireAggregate } from '../actionnaire.aggregate';
+import {
+  ActionnaireNePeutPasÊtreModifiéDirectement,
+  ActionnaireIdentiqueError,
+  DemandeDeChangementEnCoursError,
+  ProjetAbandonnéError,
+  ProjetAvecDemandeAbandonEnCoursError,
+  ProjetAchevéError,
+} from '../errors';
+
+export type ActionnaireModifiéEvent = DomainEvent<
+  'ActionnaireModifié-V1',
+  {
+    identifiantProjet: IdentifiantProjet.RawType;
+    actionnaire: string;
+    modifiéLe: DateTime.RawType;
+    modifiéPar: Email.RawType;
+    raison: string;
+    pièceJustificative: {
+      format: string;
+    };
+  }
+>;
+
+export type ModifierOptions = {
+  identifiantProjet: IdentifiantProjet.ValueType;
+  identifiantUtilisateur: Email.ValueType;
+  actionnaire: string;
+  dateModification: DateTime.ValueType;
+  pièceJustificative?: DocumentProjet.ValueType;
+  raison: string;
+  estAbandonné: boolean;
+  estAchevé: boolean;
+  demandeAbandonEnCours: boolean;
+  utilisateurEstPorteur: boolean;
+  estParticipatif: boolean;
+  aDesGarantiesFinancièresConstituées: boolean;
+  aUnDépotEnCours: boolean;
+};
+
+export async function modifier(
+  this: ActionnaireAggregate,
+  {
+    identifiantProjet,
+    actionnaire,
+    dateModification,
+    identifiantUtilisateur,
+    pièceJustificative,
+    raison,
+    estAbandonné,
+    estAchevé,
+    demandeAbandonEnCours,
+    utilisateurEstPorteur,
+    estParticipatif,
+    aUnDépotEnCours,
+    aDesGarantiesFinancièresConstituées,
+  }: ModifierOptions,
+) {
+  // Règle métier, spécifique à l'AO Eolien (pour lequel le type de GF est `après candidature`) pour les porteurs
+  // La demande doit être en "instruction" si il n'y a pas de GF validées sur le projet ou si il y a une demande de renouvellement ou de modifications des garanties financières en cours
+  // La demande doit être en "instruction" si le candidat a joint à son offre la lettre d’engagement (l'investissement participatif ou financement participatif)
+  const devraitPasserParUneDemande =
+    utilisateurEstPorteur &&
+    identifiantProjet.appelOffre === 'Eolien' &&
+    (!aDesGarantiesFinancièresConstituées || aUnDépotEnCours || estParticipatif);
+
+  if (devraitPasserParUneDemande) {
+    throw new ActionnaireNePeutPasÊtreModifiéDirectement();
+  }
+
+  if (this.actionnaire === actionnaire) {
+    throw new ActionnaireIdentiqueError();
+  }
+
+  if (this.demande?.statut.estDemandé()) {
+    throw new DemandeDeChangementEnCoursError();
+  }
+
+  if (utilisateurEstPorteur && estAbandonné) {
+    throw new ProjetAbandonnéError();
+  }
+
+  if (utilisateurEstPorteur && demandeAbandonEnCours) {
+    throw new ProjetAvecDemandeAbandonEnCoursError();
+  }
+
+  if (utilisateurEstPorteur && estAchevé) {
+    throw new ProjetAchevéError();
+  }
+
+  const event: ActionnaireModifiéEvent = {
+    type: 'ActionnaireModifié-V1',
+    payload: {
+      identifiantProjet: identifiantProjet.formatter(),
+      actionnaire,
+      modifiéLe: dateModification.formatter(),
+      modifiéPar: identifiantUtilisateur.formatter(),
+      raison,
+      pièceJustificative: pièceJustificative && {
+        format: pièceJustificative.format,
+      },
+    },
+  };
+
+  await this.publish(event);
+}
+
+export function applyActionnaireModifié(
+  this: ActionnaireAggregate,
+  { payload: { actionnaire } }: ActionnaireModifiéEvent,
+) {
+  this.actionnaire = actionnaire;
+}
---
+++ packages/domain/lauréat/src/actionnaire/changement/enregistrerChangement/enregistrerChangement.usecase.ts
@@ -0,0 +1,72 @@
+import { Message, MessageHandler, mediator } from 'mediateur';
+
+import { DateTime, Email, IdentifiantProjet } from '@potentiel-domain/common';
+import { DocumentProjet, EnregistrerDocumentProjetCommand } from '@potentiel-domain/document';
+import { Role } from '@potentiel-domain/utilisateur';
+
+import { TypeDocumentActionnaire } from '..';
+
+import { ModifierActionnaireCommand } from './enregistrerChangement.command';
+
+export type ModifierActionnaireUseCase = Message<
+  'Lauréat.Actionnaire.UseCase.ModifierActionnaire',
+  {
+    identifiantProjetValue: string;
+    identifiantUtilisateurValue: string;
+    actionnaireValue: string;
+    pièceJustificativeValue: {
+      content: ReadableStream;
+      format: string;
+    };
+    dateModificationValue: string;
+    rôleValue: string;
+    raisonValue: string;
+  }
+>;
+
+export const registerModifierActionnaireUseCase = () => {
+  const runner: MessageHandler<ModifierActionnaireUseCase> = async ({
+    identifiantProjetValue,
+    identifiantUtilisateurValue,
+    actionnaireValue,
+    dateModificationValue,
+    pièceJustificativeValue,
+    rôleValue,
+    raisonValue,
+  }) => {
+    const pièceJustificative = pièceJustificativeValue
+      ? DocumentProjet.convertirEnValueType(
+          identifiantProjetValue,
+          TypeDocumentActionnaire.pièceJustificative.formatter(),
+          dateModificationValue,
+          pièceJustificativeValue.format,
+        )
+      : undefined;
+
+    if (pièceJustificative) {
+      await mediator.send<EnregistrerDocumentProjetCommand>({
+        type: 'Document.Command.EnregistrerDocumentProjet',
+        data: {
+          content: pièceJustificativeValue!.content,
+          documentProjet: pièceJustificative,
+        },
+      });
+    }
+
+    const rôle = Role.convertirEnValueType(rôleValue);
+
+    await mediator.send<ModifierActionnaireCommand>({
+      type: 'Lauréat.Actionnaire.Command.ModifierActionnaire',
+      data: {
+        identifiantProjet: IdentifiantProjet.convertirEnValueType(identifiantProjetValue),
+        identifiantUtilisateur: Email.convertirEnValueType(identifiantUtilisateurValue),
+        actionnaire: actionnaireValue,
+        dateModification: DateTime.convertirEnValueType(dateModificationValue),
+        pièceJustificative,
+        rôle,
+        raison: raisonValue,
+      },
+    });
+  };
+  mediator.register('Lauréat.Actionnaire.UseCase.ModifierActionnaire', runner);
+};
