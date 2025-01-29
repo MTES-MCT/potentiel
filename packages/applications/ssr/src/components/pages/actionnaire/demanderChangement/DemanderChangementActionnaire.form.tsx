ee1ab8c58b07ed204db07e43813f7bccf0e23734
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-01-29 16:09:19 +0100

    💬 Wording

--- packages/applications/ssr/src/components/pages/actionnaire/demanderChangement/DemanderChangementActionnaire.form.tsx
+++ packages/applications/ssr/src/components/pages/actionnaire/demanderChangement/DemanderChangementActionnaire.form.tsx
@@ -50,7 +50,7 @@ export const DemanderChangementActionnaireForm: FC<DemanderChangementActionnaire
               !piècesJustificatives.length || Object.keys(validationErrors).length > 0
             }
           >
-            Demander la modification
+            Confirmer la demande
           </SubmitButton>
         </>
       }
@@ -78,7 +78,7 @@ export const DemanderChangementActionnaireForm: FC<DemanderChangementActionnaire
           textArea
           label="Raison"
           id="raison"
-          hintText="Pour faciliter le traitement de votre demande, veuillez détailler les raisons ayant conduit au changement d'actionnaires."
+          hintText="Pour faciliter le traitement de votre demande, veuillez détailler les raisons ayant conduit au changement d'actionnaire(s)."
           nativeTextAreaProps={{ name: 'raison', required: true, 'aria-required': true }}
           state={validationErrors['raison'] ? 'error' : 'default'}
           stateRelatedMessage={validationErrors['raison']}
