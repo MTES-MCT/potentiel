2b902752330a435b1ff60c97af1a47fa5b0c3206
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-01-29 11:42:04 +0100

    🐛 Wait step vérif email envoyé

--- packages/specifications/src/notification/stepDefinitions/notification.then.ts
+++ packages/specifications/src/notification/stepDefinitions/notification.then.ts
@@ -1,17 +1,24 @@
 import { Then as Alors, DataTable } from '@cucumber/cucumber';
 import { expect } from 'chai';
+import waitForExpect from 'wait-for-expect';
 
 import { PotentielWorld } from '../../potentiel.world';
 
-function vérifierEmailEnvoyé(this: PotentielWorld, email: string, data: DataTable) {
-  const exemple = data.rowsHash();
-  const notif = this.notificationWorld.récupérerNotification(email, exemple.sujet);
+async function vérifierEmailEnvoyé(this: PotentielWorld, email: string, data: DataTable) {
+  await waitForExpect(async () => {
+    const exemple = data.rowsHash();
+    const notification = this.notificationWorld.récupérerNotification(email, exemple.sujet);
 
-  for (const [key, value] of Object.entries(exemple)) {
-    expect(key === 'sujet' ? notif.messageSubject : notif.variables[key]).to.match(
-      new RegExp(value),
-    );
-  }
+    for (const [key, value] of Object.entries(exemple)) {
+      if (key === 'sujet') {
+        expect(notification.messageSubject).to.match(new RegExp(value));
+        continue;
+      }
+
+      const variable = notification.variables[key];
+      expect(variable).to.match(new RegExp(value));
+    }
+  });
 }
 
 Alors('un email a été envoyé au porteur avec :', function (this: PotentielWorld, data: DataTable) {
