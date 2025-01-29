47e203ab12188665b4bcdc646fb6e0802fd0b4a4
Hubert Moncenis <hubmoncenis@gmail.com>
2025-01-29 11:36:35 +0100

    🐛 AbandonDemandé-V2 doit être représenté dans l'historique abandon (#2650)
    🐛 AbandonDemandé-V2 doit être représenté dans la timeline abandon

--- packages/applications/ssr/src/components/molecules/historique/timeline/abandon/mapToAbandonDemandéTimelineItemProps.tsx
+++ packages/applications/ssr/src/components/molecules/historique/timeline/abandon/mapToAbandonDemandéTimelineItemProps.tsx
@@ -1,22 +1,39 @@
+import { match } from 'ts-pattern';
+
 import { Routes } from '@potentiel-applications/routes';
 import { DocumentProjet } from '@potentiel-domain/document';
 import { Historique } from '@potentiel-domain/historique';
 import { Abandon } from '@potentiel-domain/laureat';
+import { DateTime } from '@potentiel-domain/common';
 
 import { DownloadDocument } from '@/components/atoms/form/document/DownloadDocument';
 
 export const mapToAbandonDemandéTimelineItemProps = (
   abandonDemandé: Historique.ListerHistoriqueProjetReadModel['items'][number],
 ) => {
-  const { demandéLe, demandéPar, identifiantProjet, recandidature, pièceJustificative } =
-    abandonDemandé.payload as Abandon.AbandonDemandéEventV1['payload'];
+  const event = match(abandonDemandé)
+    .with(
+      { type: 'AbandonDemandé-V1' },
+      (event) => event as unknown as Abandon.AbandonDemandéEventV1,
+    )
+    .with({ type: 'AbandonDemandé-V2' }, (event) => event as unknown as Abandon.AbandonDemandéEvent)
+    .otherwise(() => undefined);
+
+  if (!event) {
+    return {
+      date: abandonDemandé.createdAt as DateTime.RawType,
+      title: 'Étape abandon demandé inconnue',
+    };
+  }
+
+  const { demandéLe, demandéPar, identifiantProjet, pièceJustificative } = event.payload;
 
   return {
     date: demandéLe,
     title: <div>Demande déposée par {<span className="font-semibold">{demandéPar}</span>}</div>,
     content: (
       <>
-        {recandidature && (
+        {event.type === 'AbandonDemandé-V1' && event.payload.recandidature && (
           <div className="mb-4">
             Le projet s'inscrit dans un{' '}
             <span className="font-semibold">contexte de recandidature</span>
