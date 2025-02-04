e0a2058ccc0309f1e6c3d2de41e69a8687e35c9f
Hubert Moncenis <hubmoncenis@gmail.com>
2025-02-04 15:59:35 +0100

    ⚰️ Remove SSR storybook (#2698)

--- packages/applications/ssr/.storybook/mockedWithUtilisateur.js
+++
@@ -1,6 +0,0 @@
-import { Role } from '@potentiel-domain/utilisateur';
-
-// L'import de withUtilisateur pose des problèmes à l'execution de storybook
-export async function withUtilisateur(action) {
-  return action({ role: Role.admin });
-}
