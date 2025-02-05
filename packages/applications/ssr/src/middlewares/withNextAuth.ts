65cf3c8b1d3a8d87c8d78b2eac705403f6b8cefc
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-02-05 15:39:27 +0100

    🐛 Corriger la redirection du flow d'authentification

--- packages/applications/ssr/src/middlewares/withNextAuth.ts
+++ packages/applications/ssr/src/middlewares/withNextAuth.ts
@@ -7,7 +7,9 @@ export function withNextAuth(middleware: CustomMiddleware) {
   return async (request: NextRequest, event: NextFetchEvent) => {
     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
     if (!token) {
-      return NextResponse.redirect(new URL('/auth/signIn', request.url));
+      const redirectUrl = new URL('/auth/signIn', request.url);
+      redirectUrl.searchParams.set('callbackUrl', request.nextUrl.pathname);
+      return NextResponse.redirect(redirectUrl);
     }
 
     return middleware(request, event, NextResponse.next());
