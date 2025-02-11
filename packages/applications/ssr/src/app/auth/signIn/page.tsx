af62815c5cccd22fd81f75de1006591642bd4232
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-02-11 14:10:25 +0100

    💄 Update signin page to choose between ProConnect and Keycloak

--- packages/applications/ssr/src/app/auth/signIn/page.tsx
+++ packages/applications/ssr/src/app/auth/signIn/page.tsx
@@ -5,10 +5,12 @@ import { signIn, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import Button from '@codegouvfr/react-dsfr/Button';
 import ProConnectButton from '@codegouvfr/react-dsfr/ProConnectButton';
+import Tile from '@codegouvfr/react-dsfr/Tile';
 
 import { Routes } from '@potentiel-applications/routes';
 
 import { PageTemplate } from '@/components/templates/Page.template';
+import { Heading1 } from '@/components/atoms/headings';
 
 export default function SignIn() {
   const params = useSearchParams();
@@ -16,29 +18,43 @@ export default function SignIn() {
   const callbackUrl = params.get('callbackUrl') ?? Routes.Auth.redirectToDashboard();
 
   useEffect(() => {
-    switch (status) {
-      case 'authenticated':
-        // This checks that the session is up to date with the necessary requirements
-        // it's useful when changing what's inside the cookie for instance
-        if (!data.utilisateur) {
-          redirect(Routes.Auth.signOut(callbackUrl));
-          break;
-        }
-        redirect(callbackUrl);
-      //   break;
-      // case 'unauthenticated':
-      //   signIn('keycloak', { callbackUrl });
+    if (status === 'authenticated') {
+      // This checks that the session is up to date with the necessary requirements
+      // it's useful when changing what's inside the cookie for instance
+      if (!data.utilisateur) {
+        redirect(Routes.Auth.signOut(callbackUrl));
+      }
+
+      redirect(callbackUrl);
     }
   }, [status, callbackUrl, data]);
 
   return (
     <PageTemplate>
-      <div className="flex flex-col m-auto">
-        <Button onClick={() => signIn('keycloak', { callbackUrl })}>
-          Connexion email/mot de passe
-        </Button>
-
-        <ProConnectButton onClick={() => signIn('proconnect', { callbackUrl })} />
+      <Heading1>Identifiez-vous</Heading1>
+      <div className="flex flex-col items-center gap-6 mt-12 md:mt-20">
+        <Tile
+          className="md:w-2/3"
+          title="ProConnect"
+          detail={
+            <div className="flex flex-col gap-4">
+              <p>Connectez-vous facilement à l'aide de votre adresse professionnelle</p>
+              <ProConnectButton onClick={() => signIn('proconnect', { callbackUrl })} />
+            </div>
+          }
+        />
+        <Tile
+          className="md:w-2/3"
+          title="Mot de passe"
+          detail={
+            <div className="flex flex-col gap-4">
+              <p>Vous pouvez toujours vous connecter à l'aide de vos identifiants classiques</p>
+              <Button className="mx-auto" onClick={() => signIn('keycloak', { callbackUrl })}>
+                Connexion avec mot de passe
+              </Button>
+            </div>
+          }
+        />
       </div>
     </PageTemplate>
   );
