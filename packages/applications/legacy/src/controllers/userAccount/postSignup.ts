094c46c0152bdab80dd47cd8422f6002880608fe
Hubert Moncenis <hubmoncenis@gmail.com>
2025-02-12 16:31:55 +0100

    🐛 Empecher de créer un utilisateur de type porteur avec un email déjà utilisé par un compte utilisateur Potentiel (désactivé ou non) (#2721)
    * 🐛 Empecher de créer un utilisateur de type porteur avec un email déjà associé à un compte utilisateur
    
    * 💬 Update wording (pas de contexte)

--- packages/applications/legacy/src/controllers/userAccount/postSignup.ts
+++ packages/applications/legacy/src/controllers/userAccount/postSignup.ts
@@ -1,5 +1,8 @@
 import { sanitize } from 'isomorphic-dompurify';
 
+import { récupérerUtilisateurAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { Option } from '@potentiel-libraries/monads';
+
 import routes from '../../routes';
 import { v1Router } from '../v1Router';
 import { createUser, créerProfilUtilisateur } from '../../config';
@@ -45,54 +48,66 @@ v1Router.post(
     async (request, response) => {
       const { firstname, lastname, email, utilisateurInvité } = request.body;
 
-      if (utilisateurInvité) {
-        return créerProfilUtilisateur({ email, nom: lastname, prénom: firstname }).match(
-          () =>
-            response.redirect(
-              addQueryParams(routes.SIGNUP, {
-                success: true,
-              }),
-            ),
-          (e) =>
-            response.redirect(
+      const existingUser = await récupérerUtilisateurAdapter(email);
+
+      return Option.match(existingUser)
+        .some(() =>
+          response.redirect(
+            addQueryParams(routes.SIGNUP, {
+              error: `Une erreur est survenue lors de la création du compte. N'hésitez pas à nous contacter si le problème persiste.`,
+            }),
+          ),
+        )
+        .none(async () => {
+          if (utilisateurInvité) {
+            return créerProfilUtilisateur({ email, nom: lastname, prénom: firstname }).match(
+              () =>
+                response.redirect(
+                  addQueryParams(routes.SIGNUP, {
+                    success: true,
+                  }),
+                ),
+              (e) =>
+                response.redirect(
+                  addQueryParams(routes.SIGNUP, {
+                    error:
+                      e.message ||
+                      `Une erreur est survenue lors de la création du compte. N'hésitez pas à nous contacter si le problème persiste.`,
+                    ...request.body,
+                  }),
+                ),
+            );
+          }
+
+          const res = await createUser({
+            email,
+            fullName: `${firstname} ${lastname}`,
+            role: 'porteur-projet',
+          });
+
+          if (res.isErr()) {
+            if (!(res.error instanceof EmailAlreadyUsedError)) {
+              logger.error(res.error);
+            } else {
+              logger.info(res.error.message, email, res.error.userId);
+            }
+
+            return response.redirect(
               addQueryParams(routes.SIGNUP, {
                 error:
-                  e.message ||
+                  res.error.message ||
                   `Une erreur est survenue lors de la création du compte. N'hésitez pas à nous contacter si le problème persiste.`,
                 ...request.body,
               }),
-            ),
-        );
-      } else {
-        const res = await createUser({
-          email,
-          fullName: `${firstname} ${lastname}`,
-          role: 'porteur-projet',
-        });
-
-        if (res.isErr()) {
-          if (!(res.error instanceof EmailAlreadyUsedError)) {
-            logger.error(res.error);
-          } else {
-            logger.info(res.error.message, email, res.error.userId);
+            );
           }
 
           return response.redirect(
             addQueryParams(routes.SIGNUP, {
-              error:
-                res.error.message ||
-                `Une erreur est survenue lors de la création du compte. N'hésitez pas à nous contacter si le problème persiste.`,
-              ...request.body,
+              success: true,
             }),
           );
-        }
-
-        return response.redirect(
-          addQueryParams(routes.SIGNUP, {
-            success: true,
-          }),
-        );
-      }
+        });
     },
   ),
 );
