e5f73a7b075820ddefd7aa56ce1f247ef7e4cfe3
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-02-12 17:58:06 +0100

    ♻️ Switch ÉliminéAggregate en V2

---
+++ packages/domain/projet/src/éliminé/notifier/notifierÉliminé.command.ts
@@ -0,0 +1,35 @@
+import { Message, MessageHandler, mediator } from 'mediateur';
+
+import { DateTime, Email } from '@potentiel-domain/common';
+
+import { GetProjetAggregateRoot } from '../../getProjetAggregateRoot.port';
+import { IdentifiantProjet } from '../..';
+
+export type NotifierÉliminéCommand = Message<
+  'Éliminé.Command.NotifierÉliminé',
+  {
+    identifiantProjet: IdentifiantProjet.ValueType;
+    notifiéLe: DateTime.ValueType;
+    notifiéPar: Email.ValueType;
+    attestation: { format: string };
+  }
+>;
+
+export const registerNotifierÉliminéCommand = (getProjetAggregateRoot: GetProjetAggregateRoot) => {
+  const handler: MessageHandler<NotifierÉliminéCommand> = async ({
+    attestation,
+    identifiantProjet,
+    notifiéLe,
+    notifiéPar,
+  }) => {
+    const projet = await getProjetAggregateRoot(identifiantProjet);
+
+    await projet.éliminé.notifier({
+      attestation,
+      notifiéLe,
+      notifiéPar,
+    });
+  };
+
+  mediator.register('Éliminé.Command.NotifierÉliminé', handler);
+};
