f7f978c00f90ac05978711f0f17e8eb8b9af30a1
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-02-18 16:55:46 +0100

    ♻️ Refactor config

--- packages/applications/request-context/src/getProviderConfiguration.ts
+++ packages/applications/request-context/src/getProviderConfiguration.ts
@@ -1,23 +1,32 @@
+import { z } from 'zod';
+
+const keycloakConfigSchema = z.object({
+  KEYCLOAK_SERVER: z.string().url(),
+  KEYCLOAK_REALM: z.string(),
+  KEYCLOAK_USER_CLIENT_ID: z.string(),
+  KEYCLOAK_USER_CLIENT_SECRET: z.string(),
+});
+
+const proconnectConfigSchema = z.object({
+  PROCONNECT_ISSUER: z.string().url(),
+  PROCONNECT_CLIENT_ID: z.string(),
+  PROCONNECT_CLIENT_SECRET: z.string(),
+});
+
 export function getProviderConfiguration(provider: string) {
   if (provider === 'keycloak') {
-    const keycloakIssuerUrl = `${process.env.KEYCLOAK_SERVER}/realms/${process.env.KEYCLOAK_REALM}`;
-    const keycloakClientId = process.env.KEYCLOAK_USER_CLIENT_ID ?? '';
-    const keycloakClientSecret = process.env.KEYCLOAK_USER_CLIENT_SECRET ?? '';
-
+    const authConfig = keycloakConfigSchema.parse(process.env);
     return {
-      issuer: keycloakIssuerUrl,
-      clientId: keycloakClientId,
-      clientSecret: keycloakClientSecret,
+      issuer: `${authConfig.KEYCLOAK_SERVER}/realms/${authConfig.KEYCLOAK_REALM}`,
+      clientId: authConfig.KEYCLOAK_USER_CLIENT_ID,
+      clientSecret: authConfig.KEYCLOAK_USER_CLIENT_SECRET,
     };
   } else if (provider === 'proconnect') {
-    const proConnectIssuerUrl = `${process.env.PROCONNECT_ENDPOINT}/api/v2`;
-    const proConnectClientId = process.env.PROCONNECT_CLIENT_ID ?? '';
-    const proConnectClientSecret = process.env.PROCONNECT_CLIENT_SECRET ?? '';
-
+    const authConfig = proconnectConfigSchema.parse(process.env);
     return {
-      issuer: proConnectIssuerUrl,
-      clientId: proConnectClientId,
-      clientSecret: proConnectClientSecret,
+      issuer: authConfig.PROCONNECT_ISSUER,
+      clientId: authConfig.PROCONNECT_CLIENT_ID,
+      clientSecret: authConfig.PROCONNECT_CLIENT_SECRET,
     };
   } else {
     throw new Error(`Provider ${provider} not supported`);
