851bd5b59a310e8edd106104b42f5aa6b586a6c5
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-02-18 14:44:28 +0100

    ✨ Add proconnect search param to show the proconnect choice

--- packages/applications/ssr/src/app/auth/signIn/page.tsx
+++ packages/applications/ssr/src/app/auth/signIn/page.tsx
@@ -16,46 +16,61 @@ export default function SignIn() {
   const params = useSearchParams();
   const { status, data } = useSession();
   const callbackUrl = params.get('callbackUrl') ?? Routes.Auth.redirectToDashboard();
+  const proConnectActivated = params.get('proconnect') ?? false;
 
   useEffect(() => {
-    if (status === 'authenticated') {
-      // This checks that the session is up to date with the necessary requirements
-      // it's useful when changing what's inside the cookie for instance
-      if (!data.utilisateur) {
-        redirect(Routes.Auth.signOut(callbackUrl));
-      }
+    switch (status) {
+      case 'authenticated':
+        // This checks that the session is up to date with the necessary requirements
+        // it's useful when changing what's inside the cookie for instance
+        if (!data.utilisateur) {
+          redirect(Routes.Auth.signOut(callbackUrl));
+          break;
+        }
+        redirect(callbackUrl);
+        break;
 
-      redirect(callbackUrl);
+      case 'unauthenticated':
+        if (!proConnectActivated) {
+          signIn('keycloak', { callbackUrl });
+        }
+        break;
     }
   }, [status, callbackUrl, data]);
 
   return (
     <PageTemplate>
-      <Heading1>Identifiez-vous</Heading1>
-      <div className="flex flex-col items-center gap-6 mt-12 md:mt-20">
-        <Tile
-          className="md:w-2/3"
-          title="ProConnect"
-          detail={
-            <div className="flex flex-col gap-4">
-              <p>Connectez-vous facilement à l'aide de votre adresse professionnelle</p>
-              <ProConnectButton onClick={() => signIn('proconnect', { callbackUrl })} />
-            </div>
-          }
-        />
-        <Tile
-          className="md:w-2/3"
-          title="Mot de passe"
-          detail={
-            <div className="flex flex-col gap-4">
-              <p>Vous pouvez toujours vous connecter à l'aide de vos identifiants classiques</p>
-              <Button className="mx-auto" onClick={() => signIn('keycloak', { callbackUrl })}>
-                Connexion avec mot de passe
-              </Button>
-            </div>
-          }
-        />
-      </div>
+      {proConnectActivated ? (
+        <>
+          <Heading1>Identifiez-vous</Heading1>
+          <div className="flex flex-col items-center gap-6 mt-12 md:mt-20">
+            <Tile
+              className="md:w-2/3"
+              title="ProConnect"
+              detail={
+                <div className="flex flex-col gap-4">
+                  <p>Connectez-vous facilement à l'aide de votre adresse professionnelle</p>
+                  <ProConnectButton onClick={() => signIn('proconnect', { callbackUrl })} />
+                </div>
+              }
+            />
+            <Tile
+              className="md:w-2/3"
+              title="Mot de passe"
+              detail={
+                <div className="flex flex-col gap-4">
+                  <p>Vous pouvez toujours vous connecter à l'aide de vos identifiants classiques</p>
+                  <Button className="mx-auto" onClick={() => signIn('keycloak', { callbackUrl })}>
+                    Connexion avec mot de passe
+                  </Button>
+                </div>
+              }
+            />
+          </div>
+        </>
+      ) : (
+        'Authentification en cours ...'
+      )}
     </PageTemplate>
   );
 }
