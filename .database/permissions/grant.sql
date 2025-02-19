03b543476643ab3907cefddafbca7205a2926472
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-02-19 17:28:45 +0100

    🗃️ Redefined grants

---
+++ .database/permissions/grant.sql
@@ -0,0 +1,29 @@
+-- Niveau de droit à mettre en place une fois Metabase isolé via un schéma (doit être exe avec un compte admin postgres)
+
+-- Grant Application Potentiel (App Node + CRON)
+-- Legacy
+GRANT USAGE ON SCHEMA public TO potentiel;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO potentiel;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO potentiel;
+
+-- Event Store
+GRANT USAGE ON SCHEMA event_store TO potentiel;
+GRANT SELECT, INSERT ON event_store.event_stream TO potentiel;
+GRANT SELECT, INSERT, UPDATE, DELETE ON event_store.subscriber TO potentiel;
+GRANT SELECT, INSERT, UPDATE, DELETE ON event_store.pending_acknowledgement TO potentiel;
+
+-- Domain Views
+GRANT USAGE ON SCHEMA domain_views TO potentiel;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA domain_views TO potentiel;
+ALTER DEFAULT PRIVILEGES IN SCHEMA domain_views GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO potentiel;
+
+-- Grant Application Potentiel (CRON extraire stats)
+-- Stats
+GRANT USAGE ON SCHEMA domain_public_statistic TO potentiel;
+GRANT SELECT, INSERT, DELETE ON ALL TABLES IN SCHEMA domain_public_statistic TO potentiel;
+ALTER DEFAULT PRIVILEGES IN SCHEMA domain_public_statistic GRANT SELECT, INSERT, DELETE ON TABLES TO potentiel;
+
+-- Grant Metabase Potentiel
+GRANT USAGE ON SCHEMA domain_public_statistic TO metabase;
+GRANT SELECT ON ALL TABLES IN SCHEMA domain_public_statistic TO metabase;
+ALTER DEFAULT PRIVILEGES IN SCHEMA domain_public_statistic GRANT SELECT ON TABLES TO metabase;
