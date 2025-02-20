ed379f63e084cb83d4c6cb05ed5a4925b6092fed
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-02-20 11:56:35 +0100

    ♻️ Specific script for up

---
+++ scripts/up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker compose --profile app up -d
+
+echo "ℹ️  Waiting for database to be ready..."
+RETRIES=10
+COUNT=0
+
+until docker exec potentiel_db pg_isready -U postgres_admin -d potentiel 2>/dev/null || [ $COUNT -eq $RETRIES ]; do
+    echo "⚠️ Try $COUNT : PostgreSQL isn't ready ..."
+    sleep 1
+    ((COUNT++))
+done
+
+if [ $COUNT -eq $RETRIES ]; then
+    echo "❌ Error : Timeout - PostgreSQL isn't ready after $RETRIES retries."
+    exit 1
+fi
+
+echo "✅ PostgreSQL is ready !"
+exit 0
