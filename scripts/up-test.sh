0b5283f5a0352cc3ff09511f417abf953568629b
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-02-20 11:52:20 +0100

    🎨 Script npm up:test defined in specific bash script (w/ retry policies and better logging)

---
+++ scripts/up-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker compose --profile test up -d
+
+echo "ℹ️  Waiting for database to be ready..."
+RETRIES=10
+COUNT=0
+
+until docker exec potentiel_db_tests_integration pg_isready -U testuser -d potentiel_test 2>/dev/null || [ $COUNT -eq $RETRIES ]; do
+    echo "⚠️ Try $COUNT : PostgreSQL isn't ready ..."
+    sleep 1
+    ((COUNT++))
+done
+
+if [ $COUNT -eq $RETRIES ]; then
+    echo "❌ Error : Timeout - PostgreSQL isn't ready after $RETRIES retries."
+    exit 1
+fi
+
+echo "✅ PostgreSQL is ready !"
+exit 0
