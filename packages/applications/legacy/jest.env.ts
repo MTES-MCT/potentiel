035624d7050ebc8b700d21bada20921c08b93d28
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-02-20 14:24:30 +0100

    ✅ Update test env variables

--- packages/applications/legacy/jest.env.ts
+++ packages/applications/legacy/jest.env.ts
@@ -4,3 +4,4 @@ process.env.AWS_ACCESS_KEY_ID = 'minioadmin';
 process.env.AWS_SECRET_ACCESS_KEY = 'minioadmin';
 process.env.POTENTIEL_IDENTIFIER_SECRET = 'none';
 process.env.DATABASE_CONNECTION_STRING = 'postgres://potentiel:password@localhost:5433/potentiel';
+process.env.POSTGRESQL_POOL_MAX = '2';
