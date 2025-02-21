0cf7f25a59386f9cc45f1a79b9eacd584c85d44e
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-02-21 17:20:50 +0100

    🧑‍💻 Ajout d'un garde fou pour garantir l'éxécution des scripts bash via le script npm + DX copy de la commande

--- .database/scripts/update-metabase-dump.sh
+++ .database/scripts/update-metabase-dump.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Vérifier si le script est exécuté via npm
+if [ "$npm_lifecycle_event" != "update:dump-metabase" ]; then
+  echo "❌ Ce script ne peut être exécuté que via 'npm run update:dump-metabase'"
+  
+  echo "npm run update:dump-metabase" | pbcopy
+  echo "📋 La commande a été copiée dans le presse-papier"
+  
+  exit 1
+fi
+
+
+
 docker exec -it potentiel_db pg_dump -U metabase -Fc -b -v -f metabase-dev.dump metabase && docker cp potentiel_db:/metabase-dev.dump ./.database/metabase-dev.dump
--- .database/scripts/update-potentiel-dump.sh
+++ .database/scripts/update-potentiel-dump.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Vérifier si le script est exécuté via npm
+if [ "$npm_lifecycle_event" != "update:dump" ]; then
+  echo "❌ Ce script ne peut être exécuté que via 'npm run update:dump'"
+ 
+  echo "npm run update:dump" | pbcopy
+  echo "📋 La commande a été copiée dans le presse-papier"
+ 
+ 
+  exit 1
+fi
+
+
 docker exec -it potentiel_db pg_dump -U potentiel -Fc -b -v -f potentiel-dev.dump potentiel && docker cp potentiel_db:/potentiel-dev.dump ./.database/potentiel-dev.dump
