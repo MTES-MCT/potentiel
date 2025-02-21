290be3cf044300f7304beb46f73c8fb6a296bf81
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-02-21 18:02:01 +0100

    🔨 Fix script shebang (#2770)

--- .database/scripts/update-potentiel-dump.sh
+++ .database/scripts/update-potentiel-dump.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 docker exec -it potentiel_db pg_dump -U potentiel -Fc -b -v -f \"potentiel-dev.dump\" potentiel && docker cp potentiel_db:/potentiel-dev.dump ./potentiel-dev.dump
