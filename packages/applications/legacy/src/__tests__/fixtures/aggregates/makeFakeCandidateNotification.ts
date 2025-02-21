eb3383d0070b93e822709bb1a4a7ddf79d735aa7
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-02-21 14:42:57 +0100

    🔥 Remove Jest

--- packages/applications/legacy/src/__tests__/fixtures/aggregates/makeFakeCandidateNotification.ts
+++
@@ -1,9 +0,0 @@
-import { DomainEvent, UniqueEntityID } from '../../../core/domain';
-import { jest } from '@jest/globals';
-
-export const makeFakeCandidateNotification = () => ({
-  notifyCandidateIfReady: jest.fn(),
-  lastUpdatedOn: new Date(0),
-  pendingEvents: [] as DomainEvent[],
-  id: new UniqueEntityID(),
-});
