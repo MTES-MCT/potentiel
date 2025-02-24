3222d21cfadb68845956b679e441c6cade49dc22
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-02-24 17:36:43 +0100

    🎨 Review ben : supprimer jwt, remplacé par jose

--- packages/applications/ssr/src/app/statistiques-publiques/page.tsx
+++ packages/applications/ssr/src/app/statistiques-publiques/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next';
-import jwt from 'jsonwebtoken';
-
-import { getLogger } from '@potentiel-libraries/monitoring';
+import { SignJWT } from 'jose';
+import { z } from 'zod';
 
 import { StatistiquesPubliquesPage } from '@/components/pages/statistiques-publiques/StatistiquesPubliques.page';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
@@ -10,36 +9,39 @@ export const metadata: Metadata = {
   title: 'Statistiques - Potentiel',
 };
 
+const schema = z.object({
+  METABASE_PUBLIC_SITE_URL: z.string(),
+  METABASE_PUBLIC_SECRET_KEY: z.string(),
+  METABASE_PUBLIC_DASHBOARD_ID: z.coerce.number(),
+  METABASE_PUBLIC_DASHBOARD_MAP_ID: z.coerce.number(),
+});
+
 export default async function Page() {
   return PageWithErrorHandling(async () => {
-    const { METABASE_PUBLIC_SITE_URL, METABASE_PUBLIC_SECRET_KEY } = process.env;
-
-    if (!METABASE_PUBLIC_SITE_URL || !METABASE_PUBLIC_SECRET_KEY) {
-      getLogger().error(
-        'Missing METABASE_PUBLIC_SITE_URL and/or METABASE_PUBLIC_SECRET_KEY environment variables',
-      );
-      throw new Error(
-        'Missing METABASE_PUBLIC_SITE_URL and/or METABASE_PUBLIC_SECRET_KEY environment variables',
-      );
-    }
-
-    const tokenForMainDashboard = jwt.sign(
-      {
-        resource: { dashboard: 2 },
-        params: {},
-        exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
-      },
+    const {
+      METABASE_PUBLIC_SITE_URL,
       METABASE_PUBLIC_SECRET_KEY,
-    );
+      METABASE_PUBLIC_DASHBOARD_ID,
+      METABASE_PUBLIC_DASHBOARD_MAP_ID,
+    } = schema.parse(process.env);
 
-    const tokenForMapDashboard = jwt.sign(
-      {
-        resource: { dashboard: 3 },
-        params: {},
-        exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
-      },
-      METABASE_PUBLIC_SECRET_KEY,
-    );
+    const tokenForMainDashboard = await new SignJWT({
+      resource: { dashboard: METABASE_PUBLIC_DASHBOARD_ID },
+      params: {},
+    })
+      .setIssuedAt()
+      .setExpirationTime('10m')
+      .setProtectedHeader({ alg: 'HS256' })
+      .sign(new TextEncoder().encode(METABASE_PUBLIC_SECRET_KEY));
+
+    const tokenForMapDashboard = await new SignJWT({
+      resource: { dashboard: METABASE_PUBLIC_DASHBOARD_MAP_ID },
+      params: {},
+    })
+      .setIssuedAt()
+      .setExpirationTime('10m')
+      .setProtectedHeader({ alg: 'HS256' })
+      .sign(new TextEncoder().encode(METABASE_PUBLIC_SECRET_KEY));
 
     const mainDashboardIframeUrl = new URL(
       `${METABASE_PUBLIC_SITE_URL}/embed/dashboard/${tokenForMainDashboard}#bordered=false&titled=false`,
