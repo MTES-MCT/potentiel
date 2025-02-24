970a0a037999de6e2da7fca999642f3cd1165a6e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-02-24 11:27:15 +0100

    🔨 Fix dump script

--- .database/scripts/update-metabase-dump.sh
+++ .database/scripts/update-metabase-dump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it potentiel_db pg_dump -U metabase -Fc -b -v -f \"metabase-dev.dump\" metabase && docker cp potentiel_db:/metabase-dev.dump ./metabase-dev.dump
+docker exec -it potentiel_db pg_dump -U metabase -Fc -b -v -f metabase-dev.dump metabase && docker cp potentiel_db:/metabase-dev.dump ./metabase-dev.dump
--- .database/scripts/update-potentiel-dump.sh
+++ .database/scripts/update-potentiel-dump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it potentiel_db pg_dump -U potentiel -Fc -b -v -f \"potentiel-dev.dump\" potentiel && docker cp potentiel_db:/potentiel-dev.dump ./potentiel-dev.dump
+docker exec -it potentiel_db pg_dump -U potentiel -Fc -b -v -f potentiel-dev.dump potentiel && docker cp potentiel_db:/potentiel-dev.dump ./potentiel-dev.dump
