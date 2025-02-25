f5adf4a55a5be9df0e7926d8e49e1c3d693436f2
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-02-25 11:47:37 +0100

    🐛 Permettre la déconnexion de proconnect si l'utilisateur n'est pas autorisé

---
+++ packages/applications/ssr/src/components/molecules/SignOutProConnect.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { fr } from '@codegouvfr/react-dsfr';
+import Button from '@codegouvfr/react-dsfr/Button';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import { Heading1 } from '../atoms/headings';
+
+type SignOutProConnectProps = {
+  callbackUrl: string | undefined;
+};
+
+export const SignOutProConnect = ({ callbackUrl }: SignOutProConnectProps) => {
+  const router = useRouter();
+  return (
+    <div className={fr.cx('fr-container')}>
+      <Heading1>Vous n'êtes pas autorisé à utiliser ProConnect.</Heading1>
+      <p className={fr.cx('fr-text--sm', 'fr-mb-3w')}>
+        ProConnect est en phase d'expérimentation actuellement. <br />
+        Vous allez être déconnecté, veuillez choisir l'authentification par mot de passe.
+      </p>
+      <ul className={fr.cx('fr-btns-group', 'fr-btns-group--inline-md')}>
+        <li>
+          <Button onClick={() => signOut().then(() => router.push(callbackUrl ?? '/'))}>
+            Me déconnecter
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
+};
