84eb324aac8e63da09c7e8f3ed021a6184630fab
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-02-25 15:27:01 +0100

    🎨 Suppression formulaire legacy (sauf puissance)

--- packages/applications/legacy/src/views/pages/projectDetailsPage/sections/EditProjectData.tsx
+++ packages/applications/legacy/src/views/pages/projectDetailsPage/sections/EditProjectData.tsx
@@ -8,9 +8,7 @@ import {
   Input,
   Label,
   Section,
-  Select,
   InfoBox,
-  Checkbox,
   Form,
 } from '../../../components';
 import { Routes } from '@potentiel-applications/routes';
@@ -32,19 +30,21 @@ export const EditProjectData = ({ project, request }: EditProjectDataProps) => {
   return (
     <Section title="Modifier le projet" icon={<BuildingIcon />} className="print:hidden">
       <InfoBox className="mb-5">
-        Ce formulaire permet de modifier des informations qui ont changé.
+        Ce formulaire est accessible temporairement pour modifier la puissance du projet.
         <br />
-        Pour corriger des données à la candidature et régénérer l'attestation, utiliser le{' '}
-        <a href={Routes.Candidature.corriger(identifiantProjet)}>formulaire de modification</a> de
-        la candidature
-        <br />
-        {project.isClasse && (
+        {project.notifiedOn && project.isClasse && (
           <>
             Pour modifier la candidature et le projet après notification, utiliser le{' '}
             <a href={Routes.Lauréat.modifier(identifiantProjet)}>formulaire de modification</a> du
             projet.
           </>
         )}
+        <br />
+        Pour corriger des données à la candidature et régénérer l'attestation, utiliser le{' '}
+        <a href={Routes.Candidature.corriger(identifiantProjet)}>formulaire de modification</a> de
+        la candidature.
+        <br />
+        <br />
       </InfoBox>
       <Form
         action={ROUTES.ADMIN_CORRECT_PROJECT_DATA_ACTION}
@@ -58,17 +58,48 @@ export const EditProjectData = ({ project, request }: EditProjectDataProps) => {
           name="projectVersionDate"
           value={new Date(project.updatedAt || 0).getTime()}
         />
-
-        <div>
-          <Label htmlFor="territoireProjet">Territoire</Label>
-          <Input
-            type="text"
-            id="territoireProjet"
-            name="territoireProjet"
-            placeholder="Renseigner un territoire"
-            defaultValue={query.territoireProjet || project.territoireProjet || ''}
-          />
-        </div>
+        <input
+          type="hidden"
+          name="territoireProjet"
+          value={query.territoireProjet || project.territoireProjet || ''}
+        />
+        <input
+          type="hidden"
+          name="prixReference"
+          value={query.prixReference || project.prixReference}
+        />
+        <input
+          type="hidden"
+          name="evaluationCarbone"
+          value={query.evaluationCarbone || project.evaluationCarbone}
+        />
+        <input type="hidden" name="nomCandidat" value={query.nomCandidat || project.nomCandidat} />
+        <input type="hidden" name="email" value={query.email || project.email} />
+        <input
+          type="hidden"
+          name="engagementFournitureDePuissanceAlaPointe"
+          value={
+            query.engagementFournitureDePuissanceAlaPointe ||
+            project.engagementFournitureDePuissanceAlaPointe
+          }
+        />
+        <input
+          type="hidden"
+          name="participatif"
+          value={
+            query.participatif ||
+            (project.isFinancementParticipatif
+              ? 'financement'
+              : project.isInvestissementParticipatif
+                ? 'investissement'
+                : '')
+          }
+        />
+        <input
+          type="hidden"
+          name="actionnariat"
+          value={query.actionnariat || project.actionnariat}
+        />
 
         <div>
           <Label htmlFor="puissance">Puissance (en {project.appelOffre?.unitePuissance})</Label>
@@ -81,95 +112,6 @@ export const EditProjectData = ({ project, request }: EditProjectDataProps) => {
             placeholder="Renseigner une puissance"
             defaultValue={query.puissance || project.puissance}
           />
-        </div>
-        <div>
-          <Label htmlFor="prixReference">Prix de référence</Label>
-          <Input
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]+([\.,][0-9]+)?"
-            name="prixReference"
-            id="prixReference"
-            placeholder="Renseigner un prix de référence"
-            defaultValue={query.prixReference || project.prixReference}
-          />
-        </div>
-        <div className="form__group">
-          <Label htmlFor="evaluationCarbone">Evaluation carbone</Label>
-          <Input
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]+([\.,][0-9]+)?"
-            name="evaluationCarbone"
-            id="evaluationCarbone"
-            placeholder="Renseigner une évaluation carbone"
-            defaultValue={query.evaluationCarbone || project.evaluationCarbone}
-          />
-        </div>
-
-        <div>
-          <Label htmlFor="nomCandidat">Nom candidat</Label>
-          <Input
-            type="text"
-            id="nomCandidat"
-            name="nomCandidat"
-            placeholder="Renseigner un nom candidat"
-            defaultValue={query.nomCandidat || project.nomCandidat}
-          />
-        </div>
-        <div>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            id="email"
-            name="email"
-            defaultValue={query.email || project.email}
-            placeholder="Renseigner un email"
-          />
-        </div>
-
-        <div className="form__group flex">
-          <Checkbox
-            id="engagementFournitureDePuissanceAlaPointe"
-            name="engagementFournitureDePuissanceAlaPointe"
-            defaultChecked={
-              query.engagementFournitureDePuissanceAlaPointe ||
-              project.engagementFournitureDePuissanceAlaPointe
-            }
-          >
-            Engagement de fourniture de puissance à la pointe
-          </Checkbox>
-        </div>
-        <div>
-          <Label htmlFor="participatif">Financement ou Investissement participatif</Label>
-          <Select
-            id="participatif"
-            name="participatif"
-            defaultValue={
-              query.participatif ||
-              (project.isFinancementParticipatif
-                ? 'financement'
-                : project.isInvestissementParticipatif
-                  ? 'investissement'
-                  : '')
-            }
-          >
-            <option value={''}>Non</option>
-            <option value={'financement'}>Financement participatif</option>
-            <option value={'investissement'}>Investissement participatif</option>
-          </Select>
-        </div>
-        <div>
-          <Label htmlFor="actionnariat">Financement collectif ou Gouvernance partagée</Label>
-          <Select
-            id="actionnariat"
-            name="actionnariat"
-            defaultValue={query.actionnariat || project.actionnariat}
-          >
-            <option value={''}>Non</option>
-            <option value={'financement-collectif'}>Financement collectif</option>
-            <option value={'gouvernance-partagee'}>Gouvernance partagée</option>
-          </Select>
         </div>
 
         <PrimaryButton className="mt-2 mx-auto" type="submit" name="submit">
