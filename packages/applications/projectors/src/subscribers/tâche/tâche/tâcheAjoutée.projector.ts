744025f7d47730958103b1a25cd68019b47562c3
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-02-26 16:18:15 +0100

    🎨 Useless utils

--- packages/applications/projectors/src/subscribers/tâche/tâche/tâcheAjoutée.projector.ts
+++ packages/applications/projectors/src/subscribers/tâche/tâche/tâcheAjoutée.projector.ts
@@ -1,9 +1,29 @@
+import { match } from 'ts-pattern';
+
 import { TâcheAjoutéeEvent, TâcheEntity } from '@potentiel-domain/tache';
 import { getLogger } from '@potentiel-libraries/monitoring';
+import { CandidatureAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { Option } from '@potentiel-libraries/monads';
 
 import { upsertProjection } from '../../../infrastructure';
 
-import { récupérerProjet } from './utils/récupérerProjet';
+const récupérerProjet = async (identifiantProjet: string) => {
+  const projetEntity = await CandidatureAdapter.récupérerProjetAdapter(identifiantProjet);
+
+  const projet = Option.match(projetEntity)
+    .some<TâcheEntity['projet']>(({ nom, appelOffre, période, numéroCRE, famille }) => ({
+      appelOffre,
+      nom,
+      numéroCRE,
+      période,
+      famille: match(famille)
+        .with('', () => undefined)
+        .otherwise((value) => value),
+    }))
+    .none(() => undefined);
+
+  return projet;
+};
 
 export const tâcheAjoutéeProjector = async ({
   payload: { identifiantProjet, typeTâche, ajoutéeLe },
--- packages/applications/projectors/src/subscribers/tâche/tâche/tâcheRenouvellée.projector.ts
+++ packages/applications/projectors/src/subscribers/tâche/tâche/tâcheRenouvellée.projector.ts
@@ -1,8 +1,9 @@
-import { TâcheEntity, TâcheRenouvelléeEvent } from '@potentiel-domain/tache';
+import { TypeTâche, TâcheEntity, TâcheRenouvelléeEvent } from '@potentiel-domain/tache';
+import { DateTime } from '@potentiel-domain/common';
+import { findProjection } from '@potentiel-infrastructure/pg-projections';
+import { Option } from '@potentiel-libraries/monads';
 
 import { upsertProjection } from '../../../infrastructure';
-
-import { récupérerTâche } from './utils/récupérerTâche';
 
 export const tâcheRenouvelléeProjector = async ({
   payload: { identifiantProjet, typeTâche, ajoutéeLe },
@@ -14,4 +15,19 @@ export const tâcheRenouvelléeProjector = async ({
     typeTâche: typeTâche,
     misÀJourLe: ajoutéeLe,
   });
+};
+
+const récupérerTâche = async (typeTâche: string, identifiantProjet: string) => {
+  const tâcheEntity = await findProjection<TâcheEntity>(`tâche|${typeTâche}#${identifiantProjet}`);
+
+  const tâcheDefaultEntity: TâcheEntity = {
+    identifiantProjet,
+    typeTâche: TypeTâche.inconnue.type,
+    misÀJourLe: DateTime.now().formatter(),
+    type: 'tâche',
+  };
+
+  return Option.match(tâcheEntity)
+    .some((value) => value)
+    .none(() => tâcheDefaultEntity);
 };
