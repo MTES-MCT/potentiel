3b4e1783cb6e4551ba6a30d434041de41aecdadb
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2025-02-26 09:40:30 +0100

    ✨ Ajout Dashboard carto (#2781)
    * 🚧 WIP
    
    * ✨ Changement structure table carto_projet_statistic
    
    * 🚧 WIP
    
    * ✨ Ajout Dashboard carto

--- packages/statistiques/statistiques-publiques/sql/cartoProjetStatistic.sql
+++ packages/statistiques/statistiques-publiques/sql/cartoProjetStatistic.sql
@@ -1,4 +1,9 @@
 create table if not exists domain_public_statistic.carto_projet_statistic (
   identifiant varchar not null primary key,
-  value jsonb not null
+  appel_offre varchar not null,
+  date_notification timestamp,
+  departement_projet varchar not null,
+  is_financement_participatif boolean,
+  is_investissement_participatif boolean,
+  puissance decimal
 );
