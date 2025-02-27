5eb5c81e247d97ebb8ebd595d23a7d3d197f3b7c
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-02-27 11:00:01 +0100

    🐛 Ajout condition createdAt pouvant etre null

--- packages/statistiques/statistiques-publiques/src/projet/utilisateurCréation.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/projet/utilisateurCréation.statistic.ts
@@ -11,31 +11,30 @@ export const cleanUtilisateurCréation = () =>
 export const computeUtilisateurCréation = async () => {
   await executeQuery(
     `
-    insert
+    INSERT
     into 
       domain_public_statistic.utilisateur_creation_statistic
     SELECT
-  "source"."createdAt",
-  COUNT(*),
-  SUM(COUNT(*)) OVER (
-
-ORDER BY
-      "source"."createdAt" ASC ROWS UNBOUNDED PRECEDING
-  ) AS "count_2"
-FROM
-  (
-    SELECT
-      DATE_TRUNC('month', "public"."users"."createdAt") AS "createdAt"
+      "source"."createdAt",
+      COUNT(*),
+      SUM(COUNT(*)) OVER (
+        ORDER BY "source"."createdAt" ASC ROWS UNBOUNDED PRECEDING
+      ) AS "count_2"
     FROM
-      "public"."users"
-   
-WHERE
-      "public"."users"."role" = 'porteur-projet'
-  ) AS "source"
-GROUP BY
-  "source"."createdAt"
-ORDER BY
-  "source"."createdAt" ASC
+      (
+        SELECT
+          DATE_TRUNC('month', "public"."users"."createdAt") AS "createdAt"
+        FROM
+          "public"."users"
+        WHERE
+          "public"."users"."role" = 'porteur-projet'
+      ) AS "source"
+    WHERE
+      "source"."createdAt" IS NOT NULL
+    GROUP BY
+      "source"."createdAt"
+    ORDER BY
+      "source"."createdAt" ASC
     `,
   );
 };
