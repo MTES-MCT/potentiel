dd38f4d8c2e5e3e0c7c44eda175a7746c47409b5
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-03-03 14:07:31 +0100

    ♻️ Refacto geo api

--- packages/applications/ssr/src/app/utilisateurs/inviter/page.tsx
+++ packages/applications/ssr/src/app/utilisateurs/inviter/page.tsx
@@ -3,6 +3,7 @@ import { mediator } from 'mediateur';
 
 import { Role } from '@potentiel-domain/utilisateur';
 import { GestionnaireRéseau } from '@potentiel-domain/reseau';
+import { GeoApiClient } from '@potentiel-infrastructure/geo-api-client';
 
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 import { InviterUtilisateurPage } from '@/components/pages/utilisateur/inviter/InviterUtilisateur.page';
@@ -15,26 +16,26 @@ export const metadata: Metadata = {
 export default async function Page({ searchParams }: { searchParams: { role?: string } }) {
   return PageWithErrorHandling(async () => {
     const role = searchParams?.role ? Role.convertirEnValueType(searchParams.role) : undefined;
-    const régions = (
-      await fetch(`${process.env.NEXT_PUBLIC_GEO_API_URL}/regions`).then(
-        (r) => r.json() as Promise<{ nom: string }[]>,
-      )
-    ).map(({ nom }) => ({ label: nom, value: nom }));
-    const gestionnairesRéseau = (
+    const geoApiClient = GeoApiClient(process.env.NEXT_PUBLIC_GEO_API_URL || '');
+    const régions = await geoApiClient.fetchRegions();
+    const gestionnairesRéseau =
       await mediator.send<GestionnaireRéseau.ListerGestionnaireRéseauQuery>({
         type: 'Réseau.Gestionnaire.Query.ListerGestionnaireRéseau',
         data: {},
-      })
-    ).items.map(({ raisonSociale, identifiantGestionnaireRéseau: { codeEIC } }) => ({
-      label: raisonSociale,
-      value: codeEIC,
-    }));
+      });
 
     return (
       <InviterUtilisateurPage
         role={role?.nom}
-        régions={régions}
-        gestionnairesRéseau={gestionnairesRéseau}
+        régions={régions
+          .map(({ nom }) => ({ label: nom, value: nom }))
+          .sort((a, b) => a.label.localeCompare(b.label))}
+        gestionnairesRéseau={gestionnairesRéseau.items.map(
+          ({ raisonSociale, identifiantGestionnaireRéseau: { codeEIC } }) => ({
+            label: raisonSociale,
+            value: codeEIC,
+          }),
+        )}
       />
     );
   });
--- packages/infrastructure/geo-api-client/src/index.ts
+++ packages/infrastructure/geo-api-client/src/index.ts
@@ -1,7 +1,9 @@
 import { fetchCommunes } from './fetchCommunes';
 import { fetchDepartements } from './fetchDepartements';
+import { fetchRegions } from './fetchRegions';
 
 export const GeoApiClient = (baseUrl: string) => ({
   fetchCommunes: fetchCommunes(baseUrl),
   fetchDepartements: fetchDepartements(baseUrl),
+  fetchRegions: fetchRegions(baseUrl),
 });
