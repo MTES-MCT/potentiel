0ca658e98762ae402ffcdedf297e68a76ec23280
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-03-05 11:52:01 +0100

    ♻️ Mise à jour view abandon

--- packages/statistiques/statistiques-privées/sql/views/abandon.view.sql
+++ packages/statistiques/statistiques-privées/sql/views/abandon.view.sql
@@ -10,8 +10,8 @@ SELECT
     MAX(CASE WHEN j.key = 'identifiantProjet' THEN split_part(j.value, '#', 4) END) AS numero_cre,
     
     -- demande
-    MAX(CASE WHEN j.key = 'demande.raison' THEN j.value END) AS raison,
     MAX(CASE WHEN j.key = 'statut' THEN j.value END) AS statut,
+    MAX(CASE WHEN j.key = 'demande.raison' THEN j.value END) AS demande_raison,
     MAX(CASE WHEN j.key = 'demande.demandéLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS demande_demandé_le,
     MAX(CASE WHEN j.key = 'demande.demandéPar' THEN j.value END) AS demande_demandé_par,
     MAX(CASE
@@ -29,11 +29,11 @@ SELECT
                 ELSE 0
             END
     )::BOOLEAN AS demande_est_une_recandidature,
-    MAX(CASE WHEN j.key = 'demande.recandidature.statut' THEN j.value END) AS recandidature_statut,
-    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.identifiantProjet' THEN j.value END) AS recandidature_preuve_identifiant_projet,
-    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.demandéeLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS recandidature_preuve_demandé_le,
-    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.transmiseLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS recandidature_preuve_transmise_le,
-    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.transmisePar' THEN j.value END) AS recandidature_preuve_transmise_par,
+    MAX(CASE WHEN j.key = 'demande.recandidature.statut' THEN j.value END) AS demande_recandidature_statut,
+    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.identifiantProjet' THEN j.value END) AS demande_recandidature_preuve_identifiant_projet,
+    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.demandéeLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS demande_recandidature_preuve_demandé_le,
+    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.transmiseLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS demande_recandidature_preuve_transmise_le,
+    MAX(CASE WHEN j.key = 'demande.recandidature.preuve.transmisePar' THEN j.value END) AS demande_recandidature_preuve_transmise_par,
 
     -- confirmation
     MAX(CASE
@@ -41,7 +41,7 @@ SELECT
         THEN 1
         ELSE 0
       END
-    )::BOOLEAN AS a_une_confirmation,
+    )::BOOLEAN AS demande_a_une_confirmation,
     MAX(CASE WHEN j.key = 'confirmation.demandéeLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS confirmation_demandée_le,
     MAX(CASE WHEN j.key = 'confirmation.demandéePar' THEN j.value END) AS confirmation_demandée_par,
     MAX(CASE WHEN j.key = 'confirmation.confirméLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS confirmation_confirmé_le,
@@ -54,7 +54,7 @@ SELECT
                 THEN 1
                 ELSE 0
             END
-    )::BOOLEAN AS est_en_instruction,
+    )::BOOLEAN AS demande_est_en_instruction,
     MAX(CASE WHEN j.key = 'instruction.passéEnInstructionLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS instruction_passé_en_instruction_le,
     MAX(CASE WHEN j.key = 'instruction.passéEnInstructionPar' THEN j.value END) AS instruction_passé_en_instruction_par,
 
@@ -64,7 +64,7 @@ SELECT
                 THEN 1
                 ELSE 0
             END
-    )::BOOLEAN AS a_un_accord,
+    )::BOOLEAN AS demande_a_un_accord,
     MAX(CASE WHEN j.key = 'accord.accordéLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS accord_accordé_le,
     MAX(CASE WHEN j.key = 'accord.accordéPar' THEN j.value END) AS accord_accordé_par,
 
@@ -74,7 +74,7 @@ SELECT
                 THEN 1
                 ELSE 0
             END
-    )::BOOLEAN AS a_un_rejet,
+    )::BOOLEAN AS demande_a_un_rejet,
     MAX(CASE WHEN j.key = 'rejet.rejetéLe' THEN TO_TIMESTAMP(j.value, 'YYYY-MM-DD"T"HH24:MI:SS') END) AS rejet_rejeté_le,
     MAX(CASE WHEN j.key = 'rejet.rejetéPar' THEN j.value END) AS rejet_rejeté_par,
 FROM domain_views.projection t,
