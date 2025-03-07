438f5b9c1ed43c6e0ec9d5450dccbcb3e845ae10
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-03-07 16:43:50 +0100

    🐛 Fix probléme type colonne

--- packages/statistiques/statistiques-publiques/sql/utilisateurCreationStatistic.sql
+++ packages/statistiques/statistiques-publiques/sql/utilisateurCreationStatistic.sql
@@ -1,5 +1,5 @@
 create table if not exists domain_public_statistic.utilisateur_creation_statistic (
   date timestamp primary key,
-  sum varchar not null,
+  sum decimal not null,
   count decimal not null
 );
