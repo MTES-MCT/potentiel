683270159e0ee925d1068ae0f1cdf23c4bf34a95
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-03-07 09:55:47 +0100

    💄 Simplify the verifyRequest page design

--- packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
+++ packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
@@ -1,4 +1,4 @@
-import { fr } from '@codegouvfr/react-dsfr';
+import Button from '@codegouvfr/react-dsfr/Button';
 
 import { Routes } from '@potentiel-applications/routes';
 
@@ -8,40 +8,21 @@ import { PageTemplate } from '@/components/templates/Page.template';
 export default function VerifyRequest() {
   return (
     <PageTemplate>
-      <div className={fr.cx('fr-container')}>
-        <div
-          className={fr.cx(
-            'fr-my-7w',
-            'fr-mt-md-12w',
-            'fr-mb-md-10w',
-            'fr-grid-row',
-            'fr-grid-row--gutters',
-            'fr-grid-row--middle',
-            'fr-grid-row--center',
-          )}
-        >
-          <div className={fr.cx('fr-py-0', 'fr-col-12', 'fr-col-md-6')}>
-            <Heading1>Vérifiez votre boîte de réception</Heading1>
-            <p className={fr.cx('fr-text--sm', 'fr-mb-3w')}>
-              Nous vous avons envoyé un courriel avec un lien unique de connexion valable 15
-              minutes. Celui-ci vous permettra d'accéder à Potentiel sans saisir de mot de passe.
-            </p>
+      <div className="flex flex-col gap-4 md:mt-5 md:mx-auto md:max-w-lg">
+        <Heading1>Vérifiez votre boîte de réception</Heading1>
+        <p>
+          Nous vous avons envoyé un courriel avec un lien unique de connexion valable 15 minutes.
+          Celui-ci vous permettra d'accéder à Potentiel sans saisir de mot de passe.
+        </p>
 
-            <p className={fr.cx('fr-text--sm', 'fr-mb-3w')}>
-              Si après plusieurs minutes vous n'avez pas reçu de courriel, vous pouvez toujours
-              réessayer retrounant sur la page de connexion grâce au bouton ci-dessous. Si
-              malheureusement le problème persiste, n'hésitez pas à nous contacter ou bien à choisir
-              un autre mode de connexion.
-            </p>
-            <ul className={fr.cx('fr-btns-group', 'fr-btns-group--inline-md')}>
-              <li>
-                <a className={fr.cx('fr-btn')} href={Routes.Auth.signIn()}>
-                  Retour au choix de connexion
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <p>
+          Si après plusieurs minutes vous n'avez pas reçu de courriel, nous vous invitons à
+          retourner sur la page de connexion en cliquant sur le bouton ci dessous.
+        </p>
+
+        <p>Si malheureusement le problème persiste, n'hésitez pas à nous contacter.</p>
+
+        <Button linkProps={{ href: Routes.Auth.signIn() }}>Retour au choix de connexion</Button>
       </div>
     </PageTemplate>
   );
