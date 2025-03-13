37263032df094c2982543dcdbcbc7bee833f5f33
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-03-13 12:17:19 +0100

    🎨 Form réclamer projet

--- packages/applications/ssr/src/components/pages/utilisateur/inviter/InviterUtilisateur.form.tsx
+++ packages/applications/ssr/src/components/pages/utilisateur/inviter/InviterUtilisateur.form.tsx
@@ -68,30 +68,30 @@ export const InviterUtilisateurForm: FC<InviterUtilisateurFormProps> = ({
           />
 
           {match(role)
-            .with('dgec-validateur', () => (
-              <>
-                <Input
-                  label="Nom Complet"
-                  state={validationErrors['nomComplet'] ? 'error' : 'default'}
-                  stateRelatedMessage={validationErrors['nomComplet']}
-                  nativeInputProps={{
-                    name: 'nomComplet',
-                    required: true,
-                    'aria-required': true,
-                  }}
-                />
-                <Input
-                  label="Fonction"
-                  state={validationErrors['fonction'] ? 'error' : 'default'}
-                  stateRelatedMessage={validationErrors['fonction']}
-                  nativeInputProps={{
-                    name: 'fonction',
-                    required: true,
-                    'aria-required': true,
-                  }}
-                />
-              </>
-            ))
+            // .with('dgec-validateur', () => (
+            //   <>
+            //     <Input
+            //       label="Nom Complet"
+            //       state={validationErrors['nomComplet'] ? 'error' : 'default'}
+            //       stateRelatedMessage={validationErrors['nomComplet']}
+            //       nativeInputProps={{
+            //         name: 'nomComplet',
+            //         required: true,
+            //         'aria-required': true,
+            //       }}
+            //     />
+            //     <Input
+            //       label="Fonction"
+            //       state={validationErrors['fonction'] ? 'error' : 'default'}
+            //       stateRelatedMessage={validationErrors['fonction']}
+            //       nativeInputProps={{
+            //         name: 'fonction',
+            //         required: true,
+            //         'aria-required': true,
+            //       }}
+            //     />
+            //   </>
+            // ))
             .with('dreal', () => (
               <Select
                 label="Région"
