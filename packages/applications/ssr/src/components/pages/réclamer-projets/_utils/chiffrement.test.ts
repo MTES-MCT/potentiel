494088a6effcab52fed9394fb4769510f31d5c30
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-03-14 08:13:57 +0100

    fixup! 🔒️ Chiffrer l'identifiantProjet dans réclamer

--- packages/applications/ssr/src/components/pages/réclamer-projets/_utils/chiffrement.test.ts
+++ packages/applications/ssr/src/components/pages/réclamer-projets/_utils/chiffrement.test.ts
@@ -1,8 +1,12 @@
-import { test } from 'node:test';
+import { test, before } from 'node:test';
 
 import { expect } from 'chai';
 
 import { chiffrerIdentifiantProjet, déchiffrerIdentifiantProjet, generateIV } from './chiffrement';
+
+before(() => {
+  process.env.POTENTIEL_IDENTIFIER_SECRET = 'abcd';
+});
 
 test('chiffrer et déchiffrer identifiantProjet', () => {
   const identifiantProjet = '1234567890';
@@ -10,4 +14,5 @@ test('chiffrer et déchiffrer identifiantProjet', () => {
   const identifiantProjetChiffré = chiffrerIdentifiantProjet(identifiantProjet, iv);
   const identifiantProjetDéchiffré = déchiffrerIdentifiantProjet(identifiantProjetChiffré, iv);
   expect(identifiantProjetDéchiffré).to.eq(identifiantProjet);
+  expect(identifiantProjetDéchiffré).not.to.eq(identifiantProjetChiffré);
 });
