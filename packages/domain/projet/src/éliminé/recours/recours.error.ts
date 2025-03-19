7078016a0c0a2e39ea3b32ac4e22280b5b274bd5
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-03-19 17:35:53 +0100

    🐛 Fix missing exists rules

--- packages/domain/projet/src/éliminé/recours/recours.error.ts
+++ packages/domain/projet/src/éliminé/recours/recours.error.ts
@@ -1,7 +1,13 @@
-import { InvalidOperationError } from '@potentiel-domain/core';
+import { AggregateNotFoundError, InvalidOperationError } from '@potentiel-domain/core';
 
 export class RecoursDéjàEnInstructionAvecLeMêmeAdministrateurError extends InvalidOperationError {
   constructor() {
     super('Le recours est déjà en instruction avec le même administrateur');
+  }
+}
+
+export class AucunRecoursEnCours extends AggregateNotFoundError {
+  constructor() {
+    super(`Aucun recours n'est en cours`);
   }
 }
