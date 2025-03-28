fae3042851defb7f3ac3d3e1d69087103fbbdc66
Violette <27735540+VioMrqs@users.noreply.github.com>
2025-03-28 11:00:23 +0100

    ✨ V1 puissance formulaire de modification (#2886)

---
+++ packages/applications/ssr/src/components/pages/puissance/modifier/modifierPuissance.action.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { mediator } from 'mediateur';
+import * as zod from 'zod';
+
+import { Puissance } from '@potentiel-domain/laureat';
+import { Routes } from '@potentiel-applications/routes';
+
+import { FormAction, formAction, FormState } from '@/utils/formAction';
+import { withUtilisateur } from '@/utils/withUtilisateur';
+import { puissanceProductionAnnuelleSchema } from '@/utils/zod/candidature/candidatureFields.schema';
+
+const schema = zod.object({
+  identifiantProjet: zod.string().min(1),
+  puissance: puissanceProductionAnnuelleSchema,
+  raison: zod.string().optional(),
+});
+
+export type ModifierPuissanceFormKeys = keyof zod.infer<typeof schema>;
+
+const action: FormAction<FormState, typeof schema> = async (
+  _,
+  { identifiantProjet, puissance, raison },
+) =>
+  withUtilisateur(async (utilisateur) => {
+    await mediator.send<Puissance.PuissanceUseCase>({
+      type: 'Lauréat.Puissance.UseCase.ModifierPuissance',
+      data: {
+        identifiantProjetValue: identifiantProjet,
+        identifiantUtilisateurValue: utilisateur.identifiantUtilisateur.formatter(),
+        dateModificationValue: new Date().toISOString(),
+        raisonValue: raison ?? '',
+        puissanceValue: puissance,
+      },
+    });
+
+    return {
+      status: 'success',
+      redirection: {
+        url: Routes.Projet.details(identifiantProjet),
+        message: 'Le changement de puissance a été pris en compte',
+      },
+    };
+  });
+
+export const modifierPuissanceAction = formAction(action, schema);
