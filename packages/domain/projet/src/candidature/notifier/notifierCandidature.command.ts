584bdb152739a2b1d0f2d5f39f9d2a53cf5dfeb6
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-03-28 09:54:50 +0100

    ♻️ Move notifier dans package projet

---
+++ packages/domain/projet/src/candidature/notifier/notifierCandidature.command.ts
@@ -0,0 +1,31 @@
+import { Message, MessageHandler, mediator } from 'mediateur';
+
+import { DateTime, Email } from '@potentiel-domain/common';
+import { AppelOffre } from '@potentiel-domain/appel-offre';
+
+import { GetProjetAggregateRoot, IdentifiantProjet } from '../..';
+
+export type NotifierCandidatureCommand = Message<
+  'Candidature.Command.NotifierCandidature',
+  {
+    identifiantProjet: IdentifiantProjet.ValueType;
+    notifiéeLe: DateTime.ValueType;
+    notifiéePar: Email.ValueType;
+    validateur: AppelOffre.Validateur;
+    attestation: { format: string };
+  }
+>;
+
+export const registerNotifierCandidatureCommand = (
+  getProjetAggregateRoot: GetProjetAggregateRoot,
+) => {
+  const handler: MessageHandler<NotifierCandidatureCommand> = async ({
+    identifiantProjet,
+    ...options
+  }) => {
+    const projet = await getProjetAggregateRoot(identifiantProjet);
+    await projet.candidature.notifier(options);
+  };
+
+  mediator.register('Candidature.Command.NotifierCandidature', handler);
+};
