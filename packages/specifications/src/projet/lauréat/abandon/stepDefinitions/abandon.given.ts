f8ebf301ea1bd8471f134baab5f742961d59f7c8
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-03-31 16:24:09 +0200

    ✅ Adapter les tests suite au dépassement de la date limite de recandidature (#2895)

--- packages/specifications/src/projet/lauréat/abandon/stepDefinitions/abandon.given.ts
+++ packages/specifications/src/projet/lauréat/abandon/stepDefinitions/abandon.given.ts
@@ -11,19 +11,30 @@ import { importerCandidature } from '../../../../candidature/stepDefinitions/can
 import { notifierLauréat } from '../../stepDefinitions/lauréat.given';
 
 EtantDonné(
-  /une demande d'abandon en cours(.*)pour le projet lauréat/,
-  async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
+  /une demande d'abandon en cours pour le projet lauréat/,
+  async function (this: PotentielWorld) {
+    await créerDemandeAbandon.call(this);
+  },
+);
+
+EtantDonné(
+  /une demande d'abandon en cours avec recandidature pour le projet lauréat/,
+  async function (this: PotentielWorld) {
+    await créerDemandeAbandonAvecRecandidature.call(this);
   },
 );
 
 EtantDonné(
   /un abandon accordé(.*)pour le projet lauréat/,
   async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
-    await créerAccordAbandon.call(this);
-
     const recandidature = etat.includes('avec recandidature');
+
+    if (recandidature) {
+      await créerDemandeAbandonAvecRecandidature.call(this);
+    } else {
+      await créerDemandeAbandon.call(this);
+    }
+    await créerAccordAbandon.call(this);
 
     if (recandidature) {
       await créerDemandePreuveRecandidature.call(this);
@@ -40,7 +51,11 @@ EtantDonné(
 EtantDonné(
   /un abandon rejeté(.*)pour le projet lauréat/,
   async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
+    if (etat.includes('avec recandidature')) {
+      await créerDemandeAbandonAvecRecandidature.call(this);
+    } else {
+      await créerDemandeAbandon.call(this);
+    }
     await créerRejetAbandon.call(this);
   },
 );
@@ -48,7 +63,11 @@ EtantDonné(
 EtantDonné(
   /une confirmation d'abandon demandée(.*)pour le projet lauréat/,
   async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
+    if (etat.includes('avec recandidature')) {
+      await créerDemandeAbandonAvecRecandidature.call(this);
+    } else {
+      await créerDemandeAbandon.call(this);
+    }
     await créerDemandeConfirmationAbandon.call(this);
   },
 );
@@ -56,7 +75,11 @@ EtantDonné(
 EtantDonné(
   /un abandon confirmé(.*)pour le projet lauréat/,
   async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
+    if (etat.includes('avec recandidature')) {
+      await créerDemandeAbandonAvecRecandidature.call(this);
+    } else {
+      await créerDemandeAbandon.call(this);
+    }
     await créerDemandeConfirmationAbandon.call(this);
     await créerConfirmationAbandon.call(this);
   },
@@ -65,39 +88,57 @@ EtantDonné(
 EtantDonné(
   /une demande d'abandon en instruction(.*)pour le projet lauréat/,
   async function (this: PotentielWorld, etat: string) {
-    await créerDemandeAbandon.call(this, etat);
+    if (etat.includes('avec recandidature')) {
+      await créerDemandeAbandonAvecRecandidature.call(this);
+    } else {
+      await créerDemandeAbandon.call(this);
+    }
     await passerDemandeAbandonEnInstruction.call(this);
   },
 );
 
-async function créerDemandeAbandon(this: PotentielWorld, etat: string) {
+async function créerDemandeAbandonAvecRecandidature(this: PotentielWorld) {
   const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
 
-  const { raison, demandéLe, demandéPar, pièceJustificative, recandidature } =
+  const { raison, demandéLe, demandéPar, recandidature } =
     this.lauréatWorld.abandonWorld.demanderAbandonFixture.créer({
       identifiantProjet,
-      recandidature: etat.includes('avec recandidature'),
+      recandidature: true,
+      // La date de demande d'abandon avec recandidature est forcément antérieure au 31/03/2025
+      demandéLe: faker.date
+        .between({
+          from: '2025-01-01',
+          to: '2025-03-31',
+        })
+        .toISOString(),
       demandéPar: this.utilisateurWorld.porteurFixture.email,
     });
 
   // Comme il est désormais impossible de demander un abandon avec recandidature,
   // il faut publier manuellement l'event V1 afin de recréer le cas pour les use case accorder, rejeter et preuve de recandidature
-  if (recandidature) {
-    const event: Abandon.AbandonDemandéEventV1 = {
-      type: 'AbandonDemandé-V1',
-      payload: {
-        identifiantProjet,
-        demandéLe: DateTime.convertirEnValueType(demandéLe).formatter(),
-        demandéPar,
-        raison,
-        pièceJustificative: pièceJustificative && {
-          format: pièceJustificative.format,
-        },
-        recandidature,
-      },
-    };
-    await publish(`abandon|${identifiantProjet}`, event);
-  } else if (pièceJustificative) {
+  const event: Abandon.AbandonDemandéEventV1 = {
+    type: 'AbandonDemandé-V1',
+    payload: {
+      identifiantProjet,
+      demandéLe: DateTime.convertirEnValueType(demandéLe).formatter(),
+      demandéPar,
+      raison,
+      recandidature,
+    },
+  };
+  await publish(`abandon|${identifiantProjet}`, event);
+}
+
+async function créerDemandeAbandon(this: PotentielWorld) {
+  const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
+
+  const { raison, demandéLe, demandéPar, pièceJustificative } =
+    this.lauréatWorld.abandonWorld.demanderAbandonFixture.créer({
+      identifiantProjet,
+      demandéPar: this.utilisateurWorld.porteurFixture.email,
+    });
+
+  if (pièceJustificative) {
     await mediator.send<Abandon.DemanderAbandonUseCase>({
       type: 'Lauréat.Abandon.UseCase.DemanderAbandon',
       data: {
@@ -115,10 +156,15 @@ async function créerDemandeAbandon(this: PotentielWorld, etat: string) {
 
 async function créerAccordAbandon(this: PotentielWorld) {
   const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
+  const { demandéLe, recandidature } = this.lauréatWorld.abandonWorld.demanderAbandonFixture;
 
   const { accordéeLe, accordéePar, réponseSignée } =
     this.lauréatWorld.abandonWorld.accorderAbandonFixture.créer({
       accordéePar: this.utilisateurWorld.validateurFixture.email,
+      // dans le contexte d'une recandidature, l'accord a forcément été fait avant le 31/03/2025
+      ...(recandidature
+        ? { accordéeLe: faker.date.between({ from: demandéLe, to: '2025-03-31' }).toISOString() }
+        : {}),
     });
 
   await mediator.send<Abandon.AbandonUseCase>({
@@ -222,8 +268,12 @@ async function créerDemandePreuveRecandidature(this: PotentielWorld) {
 async function créerPreuveRecandidatureTransmise(this: PotentielWorld) {
   const identifiantProjet = this.lauréatWorld.identifiantProjet.formatter();
 
-  await importerCandidature.call(this, faker.company.name(), 'classé');
-  await notifierLauréat.call(this, faker.date.recent().toISOString());
+  const dateDésignation = new Date('2024-01-01').toISOString();
+
+  await importerCandidature.call(this, faker.company.name(), 'classé', {
+    importéLe: dateDésignation,
+  });
+  await notifierLauréat.call(this, dateDésignation);
 
   const { transmiseLe: dateTransmissionPreuveRecandidature, preuveRecandidature } =
     this.lauréatWorld.abandonWorld.transmettrePreuveRecandidatureAbandonFixture.créer({
