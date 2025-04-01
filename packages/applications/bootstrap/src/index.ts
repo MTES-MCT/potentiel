171cacbe35bfd592a03c31f899c9672fd561356b
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-04-01 16:32:11 +0200

    🥅 Gestion de l'erreur d'authentification levée dans le middleware de permission (#2907)

--- packages/applications/bootstrap/src/index.ts
+++ packages/applications/bootstrap/src/index.ts
@@ -1,2 +1,3 @@
 export { bootstrap } from './bootstrap';
+export * from './errors';
 export { permissionMiddleware } from './middlewares/permission.middleware';
--- packages/applications/ssr/src/utils/withErrorHandling.ts
+++ packages/applications/ssr/src/utils/withErrorHandling.ts
@@ -4,6 +4,7 @@ import { isRedirectError } from 'next/dist/client/components/redirect';
 import { getLogger } from '@potentiel-libraries/monitoring';
 import { DomainError } from '@potentiel-domain/core';
 import { NoAuthenticatedUserError } from '@potentiel-applications/request-context';
+import { AuthenticationError } from '@potentiel-applications/bootstrap';
 
 export async function withErrorHandling<TResult>(
   action: () => Promise<TResult>,
@@ -18,7 +19,7 @@ export async function withErrorHandling<TResult>(
       throw e;
     }
 
-    if (e instanceof NoAuthenticatedUserError) {
+    if (e instanceof NoAuthenticatedUserError || e instanceof AuthenticationError) {
       return onAuthenticationError();
     }
 
