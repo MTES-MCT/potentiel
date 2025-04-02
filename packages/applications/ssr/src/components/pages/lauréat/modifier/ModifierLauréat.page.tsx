3bd97a90daad4953557526ad15b3d18dbae48d46
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-04-02 11:16:05 +0200

    ✨ Modifier lauréat

--- packages/applications/ssr/src/components/pages/lauréat/modifier/ModifierLauréat.page.tsx
+++ packages/applications/ssr/src/components/pages/lauréat/modifier/ModifierLauréat.page.tsx
@@ -5,38 +5,18 @@ import { Option } from '@potentiel-libraries/monads';
 import { NotificationBadge } from '@/components/molecules/candidature/NotificationBadge';
 import { ProjetBannerTemplate } from '@/components/molecules/projet/ProjetBanner.template';
 import { StatutProjetBadge } from '@/components/molecules/projet/StatutProjetBadge';
-import {
-  ModifierCandidatureNotifiéeFormEntries,
-  ModifierLauréatKeys,
-  ModifierLauréatValueFormEntries,
-} from '@/utils/zod/candidature';
 
 import { PageTemplate } from '../../../templates/Page.template';
 
-import { ModifierLauréatForm } from './ModifierLauréat.form';
+import { ModifierLauréatForm, ModifierLauréatFormProps } from './ModifierLauréat.form';
 
-type ModifierLauréatFormEntries = {
-  [K in ModifierLauréatKeys]: {
-    currentValue: ModifierLauréatValueFormEntries[K];
-    estEnCoursDeModification: boolean;
-  };
-};
-
-export type ModifierLauréatPageProps = {
-  candidature: ModifierCandidatureNotifiéeFormEntries;
-  lauréat: ModifierLauréatFormEntries;
-  projet: {
-    identifiantProjet: string;
-    nomProjet: string;
-    isCRE4ZNI: boolean;
-    isPPE2: boolean;
-  };
-};
+export type ModifierLauréatPageProps = ModifierLauréatFormProps;
 
 export const ModifierLauréatPage: React.FC<ModifierLauréatPageProps> = ({
   candidature,
   lauréat,
   projet,
+  champsSpéciaux,
 }) => {
   const identifiantProjet = IdentifiantProjet.convertirEnValueType(projet.identifiantProjet);
 
@@ -57,7 +37,12 @@ export const ModifierLauréatPage: React.FC<ModifierLauréatPageProps> = ({
         />
       }
     >
-      <ModifierLauréatForm candidature={candidature} lauréat={lauréat} projet={projet} />
+      <ModifierLauréatForm
+        candidature={candidature}
+        lauréat={lauréat}
+        projet={projet}
+        champsSpéciaux={champsSpéciaux}
+      />
     </PageTemplate>
   );
 };
