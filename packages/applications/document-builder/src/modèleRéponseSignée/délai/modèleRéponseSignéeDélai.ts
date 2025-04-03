14b0b91e4ae45e76141f2be2162b5a83f452252b
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-04-03 14:43:48 +0200

    ♻️ Split types

--- packages/applications/document-builder/src/modèleRéponseSignée/délai/modèleRéponseSignéeDélai.ts
+++ packages/applications/document-builder/src/modèleRéponseSignée/délai/modèleRéponseSignéeDélai.ts
@@ -2,38 +2,40 @@ import { ModèleRéponse } from '../modèleRéponseSignée';
 
 export const modèleRéponseDélaiFileName = 'délai-modèle-réponse.docx';
 
-// Ce type est basé sur la possibilité qu'une demande de délai
-// soit exprimée en mois (jusqu'à 2022) ou en date.
-// Dans le cas où le délai est en date, la réponse est également en date.
-// Dans le cas où le délai est en nombre de mois, la réponse peut être exprimée en mois ou en date
+type DemandePrécédenteEnDate = {
+  demandeEnDate: 'yes';
+  demandeEnMois?: undefined;
+  dateDemandePrecedenteDemandée: string;
+  dateDemandePrecedenteAccordée: string;
+};
+
+type DemandePrécédenteEnMois = {
+  demandeEnDate?: undefined;
+  demandeEnMois: 'yes';
+  dureeDelaiDemandePrecedenteEnMois: string;
+} & (DemandePrécédenteEnMoisAccordéeAvecDate | DemandePrécédenteEnMoisAccordéeAvecMois);
+
+type DemandePrécédenteEnMoisAccordéeAvecDate = {
+  demandeEnMoisAccordéeEnDate: 'yes';
+  dateDemandePrecedenteAccordée: string;
+};
+
+type DemandePrécédenteEnMoisAccordéeAvecMois = {
+  demandeEnMoisAccordéeEnDate?: undefined;
+  delaiDemandePrecedenteAccordeEnMois: string;
+};
+
 export type DemandePrécédente = {
   demandePrecedente: 'yes';
   dateDepotDemandePrecedente: string;
   dateReponseDemandePrecedente: string;
   autreDelaiDemandePrecedenteAccorde: 'yes' | '';
-} & (
-  | {
-      demandeEnDate: 'yes';
-      demandeEnMois?: undefined;
-      dateDemandePrecedenteDemandée: string;
-      dateDemandePrecedenteAccordée: string;
-    }
-  | ({
-      demandeEnDate?: undefined;
-      demandeEnMois: 'yes';
-      dureeDelaiDemandePrecedenteEnMois: string;
-    } & (
-      | {
-          demandeEnMoisAccordéeEnDate: 'yes';
-          dateDemandePrecedenteAccordée: string;
-        }
-      | {
-          demandeEnMoisAccordéeEnDate?: undefined;
-          delaiDemandePrecedenteAccordeEnMois: string;
-        }
-    ))
-);
+} & (DemandePrécédenteEnDate | DemandePrécédenteEnMois);
 
+// Ce type est basé sur la possibilité qu'une demande de délai
+// soit exprimée en mois (jusqu'à 2022) ou en date.
+// Dans le cas où le délai est en date, la réponse est également en date.
+// Dans le cas où le délai est en nombre de mois, la réponse peut être exprimée en mois ou en date
 export type ModèleRéponseDélai = ModèleRéponse & {
   type: 'délai';
   data: {
@@ -44,5 +46,5 @@ export type ModèleRéponseDélai = ModèleRéponse & {
     dateAchèvementDemandée: string;
 
     enCopies: Array<string>;
-  } & ({ demandePrecedente: '' } | ({ demandePrecedente: 'yes' } & DemandePrécédente));
+  } & ({ demandePrecedente: '' } | DemandePrécédente);
 };
