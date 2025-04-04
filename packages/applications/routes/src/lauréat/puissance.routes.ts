ed7337f9c224927a4c8a981b68e60e011cf18248
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-04-04 15:32:55 +0200

    🎨 Droits + route

--- packages/applications/routes/src/lauréat/puissance.routes.ts
+++ packages/applications/routes/src/lauréat/puissance.routes.ts
@@ -2,3 +2,10 @@ import { encodeParameter } from '../encodeParameter';
 
 export const modifier = (identifiantProjet: string) =>
   `/laureats/${encodeParameter(identifiantProjet)}/puissance/modifier`;
+
+export const changement = {
+  demander: (identifiantProjet: string) =>
+    `/laureats/${encodeParameter(identifiantProjet)}/actionnaire/changement/demander`,
+  enregistrer: (identifiantProjet: string) =>
+    `/laureats/${encodeParameter(identifiantProjet)}/actionnaire/changement/enregistrer`,
+};
