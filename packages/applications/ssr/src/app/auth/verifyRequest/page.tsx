fc5a96ac9558710485bdeff6498ef1a0c846cbdd
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-04-10 10:49:19 +0200

    ♻️  Ajout des PageWithErrorHandling manquants (#2947)

--- packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
+++ packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
@@ -9,37 +9,40 @@ import { Routes } from '@potentiel-applications/routes';
 
 import { Heading1 } from '@/components/atoms/headings';
 import { PageTemplate } from '@/components/templates/Page.template';
+import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 
 export default function VerifyRequest() {
-  const { status, data } = useSession();
+  return PageWithErrorHandling(async () => {
+    const { status, data } = useSession();
 
-  useEffect(() => {
-    if (status === 'authenticated' && data.utilisateur) {
-      redirect(Routes.Auth.redirectToDashboard());
-    }
-  }, [status, data]);
+    useEffect(() => {
+      if (status === 'authenticated' && data.utilisateur) {
+        redirect(Routes.Auth.redirectToDashboard());
+      }
+    }, [status, data]);
 
-  return (
-    <PageTemplate>
-      <div className="flex flex-col gap-4 md:mt-5 md:mx-auto md:max-w-lg">
-        <Heading1>Vérifiez votre boîte de réception</Heading1>
-        <p>
-          Nous vous avons envoyé un courriel avec un lien unique de connexion valable 15 minutes.
-          Celui-ci vous permettra d'accéder à Potentiel sans saisir de mot de passe.
-        </p>
+    return (
+      <PageTemplate>
+        <div className="flex flex-col gap-4 md:mt-5 md:mx-auto md:max-w-lg">
+          <Heading1>Vérifiez votre boîte de réception</Heading1>
+          <p>
+            Nous vous avons envoyé un courriel avec un lien unique de connexion valable 15 minutes.
+            Celui-ci vous permettra d'accéder à Potentiel sans saisir de mot de passe.
+          </p>
 
-        <p>
-          Si après plusieurs minutes vous n'avez pas reçu de courriel, nous vous invitons à
-          retourner sur la page de connexion en cliquant sur le bouton ci dessous.
-        </p>
+          <p>
+            Si après plusieurs minutes vous n'avez pas reçu de courriel, nous vous invitons à
+            retourner sur la page de connexion en cliquant sur le bouton ci dessous.
+          </p>
 
-        <p>
-          Si malheureusement le problème persiste, n'hésitez pas à nous contacter à l'adresse
-          contact@potentiel.beta.gouv.fr.
-        </p>
+          <p>
+            Si malheureusement le problème persiste, n'hésitez pas à nous contacter à l'adresse
+            contact@potentiel.beta.gouv.fr.
+          </p>
 
-        <Button linkProps={{ href: Routes.Auth.signIn() }}>Retour au choix de connexion</Button>
-      </div>
-    </PageTemplate>
-  );
+          <Button linkProps={{ href: Routes.Auth.signIn() }}>Retour au choix de connexion</Button>
+        </div>
+      </PageTemplate>
+    );
+  });
 }
--- packages/applications/ssr/src/app/reseaux/raccordements/references:corriger/page.tsx
+++ packages/applications/ssr/src/app/reseaux/raccordements/references:corriger/page.tsx
@@ -1,5 +1,6 @@
 import { CorrigerRéférencesDossierPage } from '@/components/pages/réseau/raccordement/corriger-par-lot/CorrigerRéférencesDossier.page';
+import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 
 export default async function Page() {
-  return <CorrigerRéférencesDossierPage />;
+  return PageWithErrorHandling(async () => <CorrigerRéférencesDossierPage />);
 }
