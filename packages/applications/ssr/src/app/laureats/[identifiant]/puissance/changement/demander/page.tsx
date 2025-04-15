bdb3300cc6c8c2094de5d46477ee26727907cadd
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-04-15 10:05:16 +0200

    🎨 Utilitaire

--- packages/applications/ssr/src/app/laureats/[identifiant]/puissance/changement/demander/page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/puissance/changement/demander/page.tsx
@@ -6,13 +6,13 @@ import { Option } from '@potentiel-libraries/monads';
 import { CahierDesCharges, Puissance } from '@potentiel-domain/laureat';
 import { IdentifiantProjet } from '@potentiel-domain/common';
 import { mapToPlainObject } from '@potentiel-domain/core';
-import { AppelOffre } from '@potentiel-domain/appel-offre';
 import { Candidature } from '@potentiel-domain/candidature';
 
 import { decodeParameter } from '@/utils/decodeParameter';
 import { IdentifiantParameter } from '@/utils/identifiantParameter';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 import { DemanderChangementPuissancePage } from '@/components/pages/puissance/changement/demander/DemanderChangementPuissance.page';
+import { getPériodeAppelOffres } from '@/app/_helpers/getPériodeAppelOffres';
 
 export const metadata: Metadata = {
   title: 'Demander le changement de puissance du projet - Potentiel',
@@ -45,25 +45,8 @@ export default async function Page({ params: { identifiant } }: IdentifiantParam
       return notFound();
     }
 
-    // Après migration aggregate root, à remplacer
-    const appelOffre = await mediator.send<AppelOffre.ConsulterAppelOffreQuery>({
-      type: 'AppelOffre.Query.ConsulterAppelOffre',
-      data: {
-        identifiantAppelOffre: identifiantProjet.appelOffre,
-      },
-    });
+    const { appelOffres, période } = await getPériodeAppelOffres(candidature.identifiantProjet);
 
-    if (Option.isNone(appelOffre)) {
-      return notFound();
-    }
-
-    const période = appelOffre.periodes.find((p) => p.id === identifiantProjet.période);
-
-    if (!période) {
-      return notFound();
-    }
-
-    // Après migration cahier des charges, à remplacer
     const cahierDesChargesChoisi =
       await mediator.send<CahierDesCharges.ConsulterCahierDesChargesChoisiQuery>({
         type: 'Lauréat.CahierDesCharges.Query.ConsulterCahierDesChargesChoisi',
@@ -80,7 +63,7 @@ export default async function Page({ params: { identifiant } }: IdentifiantParam
       <DemanderChangementPuissancePage
         identifiantProjet={mapToPlainObject(puissanceActuel.identifiantProjet)}
         puissance={puissanceActuel.puissance}
-        appelOffre={mapToPlainObject(appelOffre)}
+        appelOffre={mapToPlainObject(appelOffres)}
         période={mapToPlainObject(période)}
         technologie={candidature.technologie.type}
         famille={période.familles.find((f) => f.id === identifiantProjet.famille)}
