53d0c6278906df25b881c77bbf9a29df2ba868be
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2025-04-16 16:50:48 +0200

    ♻️ Mise à jour stats publiques suite migration puissance

--- packages/applications/projectors/src/subscribers/lauréat/puissance/puissanceImportée.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/puissance/puissanceImportée.projector.ts
@@ -1,11 +1,15 @@
+import { IdentifiantProjet } from '@potentiel-domain/common';
 import { Puissance } from '@potentiel-domain/laureat';
 import { upsertProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const puissanceImportéeProjector = async ({
   payload: { identifiantProjet, puissance, importéeLe },
-}: Puissance.PuissanceImportéeEvent) =>
+}: Puissance.PuissanceImportéeEvent) => {
+  const { appelOffre } = IdentifiantProjet.convertirEnValueType(identifiantProjet);
   await upsertProjection<Puissance.PuissanceEntity>(`puissance|${identifiantProjet}`, {
+    appelOffre,
     identifiantProjet,
     puissance,
     miseÀJourLe: importéeLe,
   });
+};
--- packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreTotalDemande.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreTotalDemande.statistic.ts
@@ -12,10 +12,10 @@ export const computeNombreTotalDemande = async () => {
       $1, 
       (
         select 
-          (select count(*) from "modificationRequests" where "type" in ('delai', 'puissance', 'producteur', 'fournisseur'))
+          (select count(*) from "modificationRequests" where "type" in ('delai', 'producteur', 'fournisseur'))
           + 
             (select count(*) from "event_store"."event_stream" 
-            where type like any (array['AbandonDemandé-V%', 'ChangementActionnaireDemandé-V%', 'ChangementReprésentantLégalDemandé-V%', 'RecoursDemandé-V%']))
+            where type like any (array['AbandonDemandé-V%', 'ChangementActionnaireDemandé-V%', 'ChangementReprésentantLégalDemandé-V%', 'RecoursDemandé-V%', 'ChangementPuissanceDemandé-V%']))
       )
     )
     `,
