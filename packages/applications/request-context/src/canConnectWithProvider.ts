6e680e250d115a00e780706b9acfa95be4ee65a4
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-04-23 11:18:19 +0200

    ♻️ .env, and include proconnect

--- packages/applications/request-context/src/canConnectWithProvider.ts
+++ packages/applications/request-context/src/canConnectWithProvider.ts
@@ -6,11 +6,10 @@ export const canConnectWithProvider = (provider: string, role: Role.RawType) => 
   const tousSaufDgecDreal = P.not(P.union(Role.admin.nom, Role.dreal.nom, Role.dgecValidateur.nom));
   return match({ provider, role })
     .with({ role: tousSaufDgecDreal }, () => true)
-    .with({ provider: 'proconnect' }, () => true)
-    .otherwise(
-      ({ provider }) =>
-        process.env.NEXTAUTH_PROVIDERS_DREAL_DGEC?.split(',')
-          .map((str) => str.trim().toLowerCase())
-          ?.includes(provider) ?? false,
-    );
+    .otherwise(({ provider }) => {
+      const providers = process.env.NEXTAUTH_PROVIDERS_DREAL_DGEC?.split(',').map((str) =>
+        str.trim().toLowerCase(),
+      ) ?? ['proconnect'];
+      return providers.includes(provider);
+    });
 };
