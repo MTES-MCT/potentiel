f202e8ffd4eb30b61798e6ec88b6b58a7481d306
Hubert Moncenis <hubmoncenis@gmail.com>
2025-04-29 09:51:10 +0200

    🎨 Ajout statistique "total de ptf déposées" (#3003)

---
+++ packages/statistiques/statistiques-publiques/src/04-mise-en-service/nombreTotalPTFDéposées.statistic.ts
@@ -0,0 +1,26 @@
+import { executeQuery } from '@potentiel-libraries/pg-helpers';
+
+const statisticType = 'nombreTotalPTFDéposées';
+
+export const computeNombreTotalPTFDéposées = async () => {
+  await executeQuery(
+    `
+    insert
+    into 
+      domain_public_statistic.scalar_statistic
+    values(
+      $1, 
+      (
+        select 
+            count(p.key)
+        from
+            domain_views.projection p
+        where 
+            p.key like 'dossier-raccordement|%'
+            and p.value->>'propositionTechniqueEtFinancière.propositionTechniqueEtFinancièreSignée.format' is not null
+      )
+    )
+    `,
+    statisticType,
+  );
+};
