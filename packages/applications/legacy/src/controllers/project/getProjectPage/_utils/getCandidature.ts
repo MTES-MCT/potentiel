e0a7edf75fe3fc949029dfedf1bdd3af0f345e57
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-05-05 17:51:06 +0200

    🐛 Correction bug affichage data projet au lieu de candidature

---
+++ packages/applications/legacy/src/controllers/project/getProjectPage/_utils/getCandidature.ts
@@ -0,0 +1,35 @@
+import { Option } from '@potentiel-libraries/monads';
+import { getLogger } from '@potentiel-libraries/monitoring';
+import { IdentifiantProjet } from '@potentiel-domain/common';
+import { mediator } from 'mediateur';
+import { Candidature } from '@potentiel-domain/projet';
+
+export type GetCandidatureForProjectPage = {
+  emailContact: string;
+};
+
+type Props = {
+  identifiantProjet: IdentifiantProjet.ValueType;
+};
+
+const defaultValue = 'Non renseigné à la candidature';
+
+export const getCandidature = async ({
+  identifiantProjet,
+}: Props): Promise<GetCandidatureForProjectPage> => {
+  try {
+    const candidature = await mediator.send<Candidature.ConsulterCandidatureQuery>({
+      type: 'Candidature.Query.ConsulterCandidature',
+      data: { identifiantProjet: identifiantProjet.formatter() },
+    });
+
+    return {
+      emailContact: Option.isSome(candidature)
+        ? candidature.emailContact.formatter()
+        : defaultValue,
+    };
+  } catch (error) {
+    getLogger().error(error);
+    return { emailContact: defaultValue };
+  }
+};
