36e0addb26bdc781b0863233a83640b2a146386e
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-05-12 14:57:26 +0200

    🚧 Migrate first events

--- packages/applications/cli/src/commands/producteur/migrer-partiellement.ts
+++ packages/applications/cli/src/commands/producteur/migrer-partiellement.ts
@@ -1,8 +1,7 @@
 import { Command, Flags } from '@oclif/core';
 
-import { Candidature, Lauréat } from '@potentiel-domain/projet';
+import { Lauréat } from '@potentiel-domain/projet';
 import { executeSelect } from '@potentiel-libraries/pg-helpers';
-import { listProjection } from '@potentiel-infrastructure/pg-projection-read';
 import { publish } from '@potentiel-infrastructure/pg-event-sourcing';
 
 export class Migrer extends Command {
@@ -12,9 +11,6 @@ export class Migrer extends Command {
 
   async run(): Promise<void> {
     const { flags } = await this.parse(Migrer);
-    const { items: lauréats } = await listProjection<Lauréat.LauréatEntity>('lauréat');
-    const { items: candidatures } =
-      await listProjection<Candidature.CandidatureEntity>('candidature');
 
     const subscriberCount = await executeSelect<{ count: number }>(
       "select count(*) as count from event_store.subscriber where stream_category='producteur'",
@@ -25,33 +21,42 @@ export class Migrer extends Command {
       process.exit(1);
     }
 
-    console.log(`${candidatures.length} candidatures à importer`);
-    console.log(`${lauréats.length} lauréats à importer`);
-
-    const eventsPerProjet: Record<string, Lauréat.Producteur.ProducteurEvent[]> = {};
-
-    for (const lauréat of lauréats) {
-      const candidature = candidatures.find(
-        (candidature) => candidature.identifiantProjet === lauréat.identifiantProjet,
+    const queryPayloads = `
+      select concat(p."appelOffreId", '#', p."periodeId", '#', p."familleId", '#', p."numeroCRE") as "identifiantProjet",
+             es.payload->>'producteur' as "producteur",
+             (SELECT to_char (es."occurredAt" at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"')) as "enregistréLe",
+             u."email" as "enregistréPar",
+             es.payload->>'justification' as "raison",
+             json_build_object('format', 'pdf') as "pièceJustificative",
+             es.payload->>'fileId' as "fileId"
+      from "eventStores" es
+      inner join projects p on es.payload->>'projectId' = p.id::text
+      inner join users u on es.payload->>'requestedBy' = u.id::text
+      where es.type like 'Modification%'
+      and   payload->>'type' = 'producteur'
+      order by es."occurredAt" asc
+    `;
+    const payloads =
+      await executeSelect<Lauréat.Producteur.ChangementProducteurEnregistréEvent['payload']>(
+        queryPayloads,
       );
-      if (!candidature) {
-        console.warn('candidature non trouvée', lauréat.identifiantProjet);
-        continue;
-      }
-      // const producteurImporté: Lauréat.Producteur.ProducteurImportéEvent = {
-      //   type: 'ProducteurImporté-V1',
-      //   payload: {
-      //     producteur: candidature.nomCandidat,
-      //     identifiantProjet: candidature.identifiantProjet,
-      //     importéLe: candidature.notification!.notifiéeLe,
-      //   },
-      // };
-      // eventsPerProjet[candidature.identifiantProjet] = [producteurImporté];
-    }
+
+    const eventsPerProjet: Map<string, Lauréat.Producteur.ChangementProducteurEnregistréEvent[]> =
+      payloads.reduce((eventsPerProjet, payload) => {
+        eventsPerProjet.set(payload.identifiantProjet, [
+          ...(eventsPerProjet.get(payload.identifiantProjet) ?? []),
+          {
+            type: 'ChangementProducteurEnregistré-V1',
+            payload,
+          },
+        ]);
+
+        return eventsPerProjet;
+      }, new Map());
 
     const eventsStats: Record<string, number> = {};
 
-    for (const [identifiantProjet, events] of Object.entries(eventsPerProjet)) {
+    for (const [identifiantProjet, events] of eventsPerProjet) {
       for (const event of events) {
         if (!flags.dryRun) {
           await publish(`producteur|${identifiantProjet}`, event);
