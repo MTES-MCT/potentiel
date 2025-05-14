e14b9bd7198f1924d060a5b1c05dfdabeb53ae6c
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-05-14 16:29:17 +0200

    🎨 Filter producteur

--- packages/applications/legacy/src/modules/modificationRequest/dtos/ModificationRequestListItemDTO.ts
+++ packages/applications/legacy/src/modules/modificationRequest/dtos/ModificationRequestListItemDTO.ts
@@ -7,13 +7,8 @@ export const MODIFICATION_REQUEST_TYPES = [
   'autre',
 ] as const;
 // Utilisé pour filtrer facilement la liste demande legacy lors de la migration
-// Domaines migrés : actionnaire, puissance
-export const MODIFICATION_REQUEST_TYPES_V2 = [
-  'fournisseur',
-  'producteur',
-  'delai',
-  'autre',
-] as const;
+// Domaines migrés : actionnaire, puissance, producteur
+export const MODIFICATION_REQUEST_TYPES_V2 = ['fournisseur', 'delai', 'autre'] as const;
 export type ModificationRequestTypes = (typeof MODIFICATION_REQUEST_TYPES)[number];
 export type ModificationRequestTypesV2 = (typeof MODIFICATION_REQUEST_TYPES_V2)[number];
 
