1df63f153cfa5c59e43966d17ebb816e699357e1
Violette <27735540+VioMrqs@users.noreply.github.com>
2025-05-19 16:54:49 +0200

    🔀 Intégration des modifications de la release 3.50 (#3056)

--- packages/applications/projectors/src/subscribers/lauréat/producteur/producteurRebuildTrigerred.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/producteur/producteurRebuildTrigerred.projector.ts
@@ -1,7 +1,20 @@
 import { RebuildTriggered } from '@potentiel-infrastructure/pg-event-sourcing';
 import { removeProjection } from '@potentiel-infrastructure/pg-projection-write';
 import { Lauréat } from '@potentiel-domain/projet';
+import { Where } from '@potentiel-domain/entity';
+import { listProjection } from '@potentiel-infrastructure/pg-projection-read';
 
 export const producteurRebuilTriggeredProjector = async ({ payload: { id } }: RebuildTriggered) => {
   await removeProjection<Lauréat.Producteur.ProducteurEntity>(`producteur|${id}`);
+
+  const demandesChangementProducteur =
+    await listProjection<Lauréat.Producteur.ChangementProducteurEntity>(`changement-producteur`, {
+      where: { identifiantProjet: Where.equal(id) },
+    });
+
+  for (const changement of demandesChangementProducteur.items) {
+    await removeProjection<Lauréat.Producteur.ChangementProducteurEntity>(
+      `changement-producteur|${id}#${changement.changement.enregistréLe}`,
+    );
+  }
 };
