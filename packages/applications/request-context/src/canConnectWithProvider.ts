d1baf1106fb7182c1f31baefc3ba9db030c89120
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-05-20 16:56:50 +0200

    🛂 Configuration des providers d'authentification pour DREAL et DGEC
    🔧 .env

--- packages/applications/request-context/src/canConnectWithProvider.ts
+++ packages/applications/request-context/src/canConnectWithProvider.ts
@@ -5,8 +5,13 @@ import { Role } from '@potentiel-domain/utilisateur';
 export const canConnectWithProvider = (provider: string, role: Role.RawType) => {
   const tousSaufDgecDreal = P.not(P.union(Role.admin.nom, Role.dreal.nom, Role.dgecValidateur.nom));
   return match({ provider, role })
-    .with({ provider: 'email', role: tousSaufDgecDreal }, () => true)
-    .with({ provider: 'keycloak' }, () => true)
-    .with({ provider: 'proconnect' }, () => true)
-    .otherwise(() => false);
+    .with({ role: tousSaufDgecDreal }, () => true)
+    .otherwise(({ provider }) => {
+      const providersParDéfautAgents = ['proconnect'];
+      const providers =
+        process.env.NEXTAUTH_PROVIDERS_DREAL_DGEC?.split(',').map((str) =>
+          str.trim().toLowerCase(),
+        ) ?? providersParDéfautAgents;
+      return providers.includes(provider);
+    });
 };
