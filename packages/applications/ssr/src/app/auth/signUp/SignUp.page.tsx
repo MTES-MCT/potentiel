3b46e24ec431da68f9ab84392e710728f559a4b3
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-05-21 14:55:27 +0200

    🛂 Configuration des providers d'authentification pour DREAL et DGEC (#3062)

--- packages/applications/ssr/src/app/auth/signUp/SignUp.page.tsx
+++ packages/applications/ssr/src/app/auth/signUp/SignUp.page.tsx
@@ -13,6 +13,7 @@ import { Routes } from '@potentiel-applications/routes';
 import { PageTemplate } from '@/components/templates/Page.template';
 import { Heading1 } from '@/components/atoms/headings';
 import { MagicLinkForm } from '@/components/organisms/auth/MagicLinkForm';
+import { ProfilesBadge } from '@/components/organisms/auth/ProfilesBadge';
 
 type SignUpPageProps = {
   providers: Array<string>;
@@ -64,7 +65,20 @@ export default function SignUpPage({ providers }: SignUpPageProps) {
           {providers.includes('proconnect') && (
             <Tile
               title="ProConnect"
-              desc="Inscrivez-vous facilement à l'aide de votre adresse professionnelle"
+              desc={
+                <div className="flex flex-col flex-wrap">
+                  <ProfilesBadge
+                    profiles={{
+                      'Porteurs de Projet': true,
+                      DREAL: false,
+                      DGEC: false,
+                      'Autres Partenaires*': false,
+                    }}
+                    title="Profils pouvant s'inscrire avec ProConnect, la solution d'identité de l'État pour les professionnels"
+                  />
+                  Inscrivez-vous facilement à l'aide de votre adresse professionnelle
+                </div>
+              }
               detail={<ProConnectButton onClick={() => signIn('proconnect', { callbackUrl })} />}
               className="flex-1"
             />
@@ -73,12 +87,29 @@ export default function SignUpPage({ providers }: SignUpPageProps) {
           {providers.includes('email') && (
             <Tile
               title="Lien magique"
-              desc="Inscrivez-vous facilement sans mot de passe à l'aide d'un lien magique qui sera envoyé sur votre adresse de courriel"
+              desc={
+                <div className="flex flex-col ">
+                  <ProfilesBadge
+                    profiles={{
+                      'Porteurs de Projet': true,
+                      DREAL: false,
+                      DGEC: false,
+                      'Autres Partenaires*': false,
+                    }}
+                    title="Profils pouvant s'inscrire avec un lien de connexion envoyé par email"
+                  />
+                  Inscrivez-vous facilement sans mot de passe à l'aide d'un lien magique qui sera
+                  envoyé sur votre adresse de courriel
+                </div>
+              }
               detail={<MagicLinkForm callbackUrl={callbackUrl} />}
               className="flex-1"
             />
           )}
         </div>
+      </div>
+      <div className="text-right italic mt-2">
+        * Autres Partenaires : CRE, Ademe, Caisse des dépôts, Gestionnaire de Réseau, Co-contractant
       </div>
     </PageTemplate>
   );
