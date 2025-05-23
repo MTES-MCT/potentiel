0335a32bb73ebd1e29c4e8ae973b7dbdd42c2a42
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-05-23 17:45:14 +0200

    ♻️ Déplacer la commande d'extraction des stats dans la CLI

---
+++ packages/applications/cli/src/helpers/healthcheck.ts
@@ -0,0 +1,42 @@
+import { getLogger } from '@potentiel-libraries/monitoring';
+
+type GetHealthcheckClientProps = {
+  healthcheckUrl: string | undefined;
+  environment: string;
+  slug: string;
+};
+export function getHealthcheckClient({
+  environment,
+  healthcheckUrl,
+  slug,
+}: GetHealthcheckClientProps) {
+  if (!healthcheckUrl) {
+    return {
+      async start() {},
+      async success() {},
+      async error() {},
+    };
+  }
+
+  const uuid = crypto.randomUUID();
+  const notify = async (status: string) => {
+    const url = new URL(healthcheckUrl.replace('<monitor_slug>', slug));
+    url.searchParams.set('check_in_id', uuid);
+    url.searchParams.set('status', status);
+    url.searchParams.set('environment', environment);
+
+    try {
+      await fetch(url);
+    } catch (e) {
+      getLogger().error(`Healtcheck failed: ${e}`);
+    }
+  };
+
+  return {
+    start: () => notify('in_progress'),
+    success: () => notify('ok'),
+    error: () => notify('error'),
+  };
+}
+
+export type HealthcheckClient = ReturnType<typeof getHealthcheckClient>;
