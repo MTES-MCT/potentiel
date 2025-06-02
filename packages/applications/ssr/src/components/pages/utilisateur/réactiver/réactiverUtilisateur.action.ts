0be660931104352f26390d70dab5dfa13d6623e3
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-06-02 12:26:56 +0200

    🐛 Redirection après dés-/ré-activation d'un utilisateur (#3100)

--- packages/applications/ssr/src/components/pages/utilisateur/réactiver/réactiverUtilisateur.action.ts
+++ packages/applications/ssr/src/components/pages/utilisateur/réactiver/réactiverUtilisateur.action.ts
@@ -2,7 +2,6 @@
 
 import { mediator } from 'mediateur';
 import * as zod from 'zod';
-import { headers } from 'next/headers';
 
 import { RéactiverUtilisateurUseCase } from '@potentiel-domain/utilisateur';
 import { DateTime } from '@potentiel-domain/common';
@@ -13,13 +12,17 @@ import { withUtilisateur } from '@/utils/withUtilisateur';
 
 const schema = zod.object({
   identifiantUtilisateurReactive: zod.string().min(1, { message: 'Champ obligatoire' }),
+  actif: zod
+    .enum(['', 'true', 'false'])
+    .optional()
+    .transform((actif) => (actif === 'true' ? true : actif === 'false' ? false : undefined)),
 });
 
 export type RéactiverUtilisateurFormKeys = keyof zod.infer<typeof schema>;
 
 const action: FormAction<FormState, typeof schema> = async (
   _,
-  { identifiantUtilisateurReactive },
+  { identifiantUtilisateurReactive, actif },
 ) =>
   withUtilisateur(async (utilisateur) => {
     await mediator.send<RéactiverUtilisateurUseCase>({
@@ -34,7 +37,7 @@ const action: FormAction<FormState, typeof schema> = async (
     return {
       status: 'success',
       redirection: {
-        url: headers().get('referer') ?? Routes.Utilisateur.lister({ actif: true }),
+        url: Routes.Utilisateur.lister({ actif }),
         message: "L'utilisateur a été réactivé",
       },
     };
