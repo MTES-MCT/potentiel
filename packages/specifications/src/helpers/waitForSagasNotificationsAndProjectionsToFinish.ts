066de5443c6a3385c04a07a3f1f693624807afdc
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-06-10 18:22:27 +0200

    ♻️ Improve waitForSagasNotificationsAndProjectionsToFinish

--- packages/specifications/src/helpers/waitForSagasNotificationsAndProjectionsToFinish.ts
+++ packages/specifications/src/helpers/waitForSagasNotificationsAndProjectionsToFinish.ts
@@ -3,13 +3,25 @@ import { expect } from 'chai';
 
 import { executeSelect } from '@potentiel-libraries/pg-helpers';
 
+const queryPendingAcknowledgements = `
+select 
+  pa.subscriber_name,
+  es.stream_id,
+  es.created_at,
+  es.version,
+  es.type,
+  es.payload
+from event_store.event_stream es 
+inner join event_store.pending_acknowledgement pa on 
+  pa.stream_id = es.stream_id and 
+  pa.created_at = es.created_at and 
+  pa.version = es.version`;
+
 export async function waitForSagasNotificationsAndProjectionsToFinish() {
   await new Promise((r) => setTimeout(r, 50));
   // wait for sagas, notifications and projections to finish
   await waitForExpect(async () => {
-    const [{ count }] = await executeSelect<{ count: number }>(
-      `select count(*) as count from event_store.pending_acknowledgement where error is null`,
-    );
-    expect(count).to.eq(0, "pending_acknowledgement n'est pas vide");
+    const pending = await executeSelect(queryPendingAcknowledgements);
+    expect(pending).to.deep.eq([], "pending_acknowledgement n'est pas vide");
   });
 }
