65757fac599a399827868ceadca68c2a7595a80b
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-06-10 15:13:05 +0200

    🔥 Simplification dgec

--- packages/specifications/src/utilisateur/fixtures/validateur.fixture.ts
+++ packages/specifications/src/utilisateur/fixtures/validateur.fixture.ts
@@ -2,31 +2,13 @@ import { Fixture } from '../../fixture';
 
 import { Utilisateur, AbstractUtilisateur } from './utilisateur';
 
-const validateurFonction: string = 'fonction du DGEC validateur';
-
-interface Validateur extends Utilisateur<'dgec-validateur'> {
-  readonly fonction: typeof validateurFonction;
-}
+interface Validateur extends Utilisateur<'dgec-validateur'> {}
 
 export class ValidateurFixture
   extends AbstractUtilisateur<'dgec-validateur'>
   implements Validateur, Fixture<Validateur>
 {
-  #fonction!: string;
   get fonction(): string {
-    return this.#fonction;
-  }
-
-  créer(partialFixture?: Partial<Readonly<Omit<Validateur, 'role>'>>>): Readonly<Validateur> {
-    const utilisateur = super.créer(partialFixture);
-
-    const porteur: Validateur = {
-      fonction: validateurFonction,
-      ...partialFixture,
-      ...utilisateur,
-    };
-    this.#fonction = porteur.fonction;
-
-    return porteur;
+    return 'fonction du DGEC validateur';
   }
 }
