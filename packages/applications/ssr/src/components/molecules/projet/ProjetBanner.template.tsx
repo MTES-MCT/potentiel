237eb1edb3724be7b6116c29dc7ff049eb98874b
Hubert Moncenis <hubmoncenis@gmail.com>
2025-06-10 13:39:00 +0200

    💄 Fix problème CSS  (#3134)
    * 💄 Improve projetBanner css
    
    * 💄 Fix scroll horizontal sur input file avec long nom de fichier

--- packages/applications/ssr/src/components/molecules/projet/ProjetBanner.template.tsx
+++ packages/applications/ssr/src/components/molecules/projet/ProjetBanner.template.tsx
@@ -30,8 +30,8 @@ export const ProjetBannerTemplate: FC<ProjetBannerProps> = ({
   <aside className="mb-3">
     <div className="md:flex items-start justify-between">
       <div>
-        <div className="flex justify-start items-center">
-          <div>
+        <div className="flex justify-start w-fit gap-4 flex-col md:flex-row md:gap-0 md:items-center">
+          <div className="flex">
             {href ? (
               <a href={href} className="text-xl font-bold !text-theme-white mr-2">
                 {nom}
@@ -39,16 +39,16 @@ export const ProjetBannerTemplate: FC<ProjetBannerProps> = ({
             ) : (
               <span className="text-xl font-bold !text-theme-white mr-2">{nom}</span>
             )}
-            {badge}
+            {process.env.APPLICATION_STAGE !== 'production' && (
+              <CopyButton
+                className="mx-2"
+                textToCopy={identifiantProjet.formatter()}
+                prority="primary"
+                noChildren
+              />
+            )}
           </div>
-          {process.env.APPLICATION_STAGE !== 'production' && (
-            <CopyButton
-              className="ml-4"
-              textToCopy={identifiantProjet.formatter()}
-              prority="primary"
-              noChildren
-            />
-          )}
+          {badge}
         </div>
         {localité && (
           <p className="text-sm font-medium p-0 m-0 mt-2">
