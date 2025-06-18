2755781749cd51321e2a330f6336b9a105e7a796
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-06-18 11:35:14 +0200

    🎨 Fix: rebuild

--- packages/applications/projectors/src/subscribers/lauréat/fournisseur/fournisseurRebuildTrigerred.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/fournisseur/fournisseurRebuildTrigerred.projector.ts
@@ -1,9 +1,25 @@
 import { RebuildTriggered } from '@potentiel-infrastructure/pg-event-sourcing';
 import { removeProjection } from '@potentiel-infrastructure/pg-projection-write';
 import { Lauréat } from '@potentiel-domain/projet';
+import { Where } from '@potentiel-domain/entity';
+import { listProjection } from '@potentiel-infrastructure/pg-projection-read';
 
 export const fournisseurRebuilTriggeredProjector = async ({
   payload: { id },
 }: RebuildTriggered) => {
   await removeProjection<Lauréat.Fournisseur.FournisseurEntity>(`fournisseur|${id}`);
+
+  const demandeChangementFournisseur =
+    await listProjection<Lauréat.Fournisseur.ChangementFournisseurEntity>(
+      `changement-fournisseur`,
+      {
+        where: { identifiantProjet: Where.equal(id) },
+      },
+    );
+
+  for (const changement of demandeChangementFournisseur.items) {
+    await removeProjection<Lauréat.Fournisseur.ChangementFournisseurEntity>(
+      `changement-fournisseur|${id}#${changement.changement.enregistréLe}`,
+    );
+  }
 };
