22d85d155a643268989291c781158ce41f09203d
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-06-25 10:34:24 +0200

    ✨ Envoyer un email aux admins dgec pour qu'il se connecte avec ProConnect plutôt que leur email

--- packages/applications/request-context/src/sendVerificationRequest.ts
+++ packages/applications/request-context/src/sendVerificationRequest.ts
@@ -1,20 +1,42 @@
 import { EmailUserConfig } from 'next-auth/providers/email';
 
 import { SendEmail } from '@potentiel-applications/notifications';
+import { Option } from '@potentiel-libraries/monads';
+
+import { GetUtilisateurFromEmail } from './getUtilisateur';
 
 type BuildSendVerificationRequest = (
   sendEmail: SendEmail,
+  getUtilisateurFromEmail: GetUtilisateurFromEmail,
 ) => NonNullable<EmailUserConfig['sendVerificationRequest']>;
 
 export const buildSendVerificationRequest: BuildSendVerificationRequest =
-  (sendEmail) =>
+  (sendEmail, getUtilisateurFromEmail) =>
   async ({ identifier, url }) => {
-    await sendEmail({
-      templateId: 6785365,
-      messageSubject: 'Connexion à Potentiel',
-      recipients: [{ email: identifier, fullName: '' }],
-      variables: {
-        url,
-      },
-    });
+    const utilisateur = await getUtilisateurFromEmail(identifier);
+
+    await Option.match(utilisateur)
+      .some(async (utilisateur) => {
+        if (utilisateur.role.nom === 'porteur-projet') {
+          await sendEmail({
+            templateId: 6785365,
+            messageSubject: 'Connexion à Potentiel',
+            recipients: [{ email: identifier, fullName: '' }],
+            variables: {
+              url,
+            },
+          });
+        }
+        if (utilisateur.role.nom === 'admin') {
+          await sendEmail({
+            templateId: 999999,
+            messageSubject: 'Potentiel - Connexion avec ProConnect obligatoire',
+            recipients: [{ email: identifier, fullName: '' }],
+            variables: {
+              url,
+            },
+          });
+        }
+      })
+      .none();
   };
