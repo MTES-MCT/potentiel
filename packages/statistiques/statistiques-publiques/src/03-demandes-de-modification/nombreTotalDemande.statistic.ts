9c0f5ee8d29c83d74a2202438a205bac199d1acf
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-07-01 12:02:26 +0200

    ✨ Statistiques publiques Fournisseur (#3208)

--- packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreTotalDemande.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreTotalDemande.statistic.ts
@@ -12,10 +12,10 @@ export const computeNombreTotalDemande = async () => {
       $1, 
       (
         select 
-          (select count(*) from "modificationRequests" where "type" in ('delai', 'fournisseur'))
+          (select count(*) from "modificationRequests" where "type"= 'delai')
           + 
             (select count(*) from "event_store"."event_stream" 
-            where type like any (array['AbandonDemandé-V%', 'ChangementActionnaireDemandé-V%', 'ChangementReprésentantLégalDemandé-V%', 'RecoursDemandé-V%', 'ChangementPuissanceDemandé-V%', 'ChangementActionnaireEnregistré-V%', 'ChangementPuissanceEnregistré-V%', 'ChangementProducteurEnregistré-V%']))
+            where type like any (array['AbandonDemandé-V%', 'ChangementActionnaireDemandé-V%', 'ChangementReprésentantLégalDemandé-V%', 'RecoursDemandé-V%', 'ChangementPuissanceDemandé-V%', 'ChangementActionnaireEnregistré-V%', 'ChangementPuissanceEnregistré-V%', 'ChangementProducteurEnregistré-V%','ChangementFournisseurEnregistré-V%']))
       )
     )
     `,
