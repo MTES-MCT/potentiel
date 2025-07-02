5f5dc5fc8c644eeff7f2b50c8caaae6c2162bdd5
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-07-02 10:38:14 +0200

    🎨 Amélioration des item de l'historique

--- packages/applications/ssr/src/utils/historique/mapToProps/délai/events/mapToDélaiAccordéTimelineItemProps.tsx
+++ packages/applications/ssr/src/utils/historique/mapToProps/délai/events/mapToDélaiAccordéTimelineItemProps.tsx
@@ -11,17 +11,27 @@ export const mapToDélaiAccordéTimelineItemProps = (
   return {
     date: accordéLe,
     title: <div>{getTitleFromRaison(raison)}</div>,
-    content: <>Durée : {durée} mois</>,
+    content: (
+      <>
+        Durée : <span className="font-semibold">{durée} mois</span>
+      </>
+    ),
   };
 };
 
 const getTitleFromRaison = (raison: Lauréat.Délai.DélaiAccordéEvent['payload']['raison']) =>
   match(raison)
     .with('demande', () => `Demande de délai exceptionnel accordé par l'administration`)
-    .with(
-      'cdc-18-mois',
-      () =>
-        `Attribution d'un délai supplémentaire prévu dans le cahier des charges rétroactif du 30/08/2022`,
-    )
-    .with('covid', () => `Attribution d'un délai supplémentaire dû à la crise du COVID`)
+    .with('cdc-18-mois', () => (
+      <>
+        Attribution d'un délai supplémentaire prévu dans le{' '}
+        <span className="font-semibold">cahier des charges rétroactif du 30/08/2022</span>
+      </>
+    ))
+    .with('covid', () => (
+      <>
+        Attribution d'un délai supplémentaire dû à la{' '}
+        <span className="font-semibold">crise du COVID</span>
+      </>
+    ))
     .exhaustive();
