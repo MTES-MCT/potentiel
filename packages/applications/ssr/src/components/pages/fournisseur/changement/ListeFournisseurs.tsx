ccc0ac5dc6585e9aec632b1837e9a4176eb829b9
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-07-03 11:41:20 +0200

    🎨 Affichage des details de fo

--- packages/applications/ssr/src/components/pages/fournisseur/changement/ListeFournisseurs.tsx
+++ packages/applications/ssr/src/components/pages/fournisseur/changement/ListeFournisseurs.tsx
@@ -9,25 +9,28 @@ export type ListeFournisseursProps = {
   fournisseurs: PlainType<Array<Lauréat.Fournisseur.Fournisseur.ValueType>>;
 };
 
-export const ListeFournisseurs: FC<ListeFournisseursProps> = ({ fournisseurs }) => (
-  <ul className="list-disc pl-4">
-    {/* TODO We can use Object.groupBy starting from Node 21+  */}
-    {Object.entries(
-      fournisseurs.reduce(
-        (prev, { nomDuFabricant, lieuDeFabrication, typeFournisseur: { typeFournisseur } }) => ({
-          ...prev,
-          [typeFournisseur]: [
-            ...(prev[typeFournisseur] ?? []),
-            lieuDeFabrication ? `${nomDuFabricant} (${lieuDeFabrication})` : nomDuFabricant,
-          ],
-        }),
-        {} as Record<Lauréat.Fournisseur.TypeFournisseur.RawType, string[]>,
-      ),
-    ).map(([typeFournisseur, nomsDesFabricants]) => (
-      <li key={typeFournisseur}>
-        {typeFournisseurLabel[typeFournisseur as Lauréat.Fournisseur.TypeFournisseur.RawType]} :{' '}
-        {nomsDesFabricants.join(', ')}
-      </li>
-    ))}
-  </ul>
-);
+export const ListeFournisseurs: FC<ListeFournisseursProps> = ({ fournisseurs }) =>
+  fournisseurs.length === 0 ? (
+    <div>Aucun fournisseur renseigné</div>
+  ) : (
+    <ul className="list-disc pl-4">
+      {/* TODO We can use Object.groupBy starting from Node 21+  */}
+      {Object.entries(
+        fournisseurs.reduce(
+          (prev, { nomDuFabricant, lieuDeFabrication, typeFournisseur: { typeFournisseur } }) => ({
+            ...prev,
+            [typeFournisseur]: [
+              ...(prev[typeFournisseur] ?? []),
+              lieuDeFabrication ? `${nomDuFabricant} (${lieuDeFabrication})` : nomDuFabricant,
+            ],
+          }),
+          {} as Record<Lauréat.Fournisseur.TypeFournisseur.RawType, string[]>,
+        ),
+      ).map(([typeFournisseur, nomsDesFabricants]) => (
+        <li key={typeFournisseur}>
+          {typeFournisseurLabel[typeFournisseur as Lauréat.Fournisseur.TypeFournisseur.RawType]} :{' '}
+          {nomsDesFabricants.join(', ')}
+        </li>
+      ))}
+    </ul>
+  );
