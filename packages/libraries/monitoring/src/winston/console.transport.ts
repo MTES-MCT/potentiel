d742b95e5d9d02a93028386f749f77bb72b8ed48
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-07-04 11:42:02 +0200

    ✨ Gestion des métadonnées par défaut dans le logger, et ajout de métadonnées à la CLI

--- packages/libraries/monitoring/src/winston/console.transport.ts
+++ packages/libraries/monitoring/src/winston/console.transport.ts
@@ -3,21 +3,29 @@ import winston from 'winston';
 declare module 'logform' {
   interface TransformableInfo {
     meta: Record<string, unknown>;
+    defaultMeta?: Record<string, unknown>;
   }
 }
+
+/** Merge and format Meta  */
+const getMeta = (info: winston.Logform.TransformableInfo) => {
+  if ('error' in info && info.error instanceof Error && info.error.cause) {
+    info.meta.cause = String(info.error.cause);
+  }
+
+  return {
+    ...info.defaultMeta,
+    ...info.meta,
+  };
+};
 
 /**
  * We are currently using a custom formatter because we don't really have monitoring capabilities.
  * In the futur, we can switch to logstash if beta.gouv.fr setup something like ELK
  */
 const customFormat = winston.format((info) => {
-  if ('error' in info && info.error instanceof Error && info.error.cause) {
-    info.meta.cause = String(info.error.cause);
-  }
-  const meta = Object.keys(info.meta)
-    .map((k) => `{${k}=${JSON.stringify(info.meta[k])}}`)
-    .join(' ');
-  info.message = `${info.message} | Service(${info.service}) | Metadata(${meta})`;
+  const meta = getMeta(info);
+  info.message = `${info.message} | Service(${info.service}) | Metadata(${JSON.stringify(meta)})`;
   return info;
 });
 
