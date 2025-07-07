6cf29ef04ddccc662d15e7c20d9d6ecda8a761bc
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-07-07 11:25:32 +0200

    🎨 Autoriser l'envoi du formulaire si la date est positive

--- packages/applications/ssr/src/components/pages/délai/demander/DemanderDélai.form.tsx
+++ packages/applications/ssr/src/components/pages/délai/demander/DemanderDélai.form.tsx
@@ -3,7 +3,6 @@
 import Input from '@codegouvfr/react-dsfr/Input';
 import { FC, useState } from 'react';
 import Button from '@codegouvfr/react-dsfr/Button';
-import Alert from '@codegouvfr/react-dsfr/Alert';
 
 import { DateTime } from '@potentiel-domain/common';
 import { Routes } from '@potentiel-applications/routes';
@@ -25,57 +24,57 @@ export const DemanderDélaiForm: FC<DemanderDélaiFormProps> = ({
   identifiantProjet,
   dateAchèvementPrévisionnelleActuelle,
 }) => {
-  const [nouvelleDate, setNouvelleDate] = useState<DateTime.RawType | undefined>(undefined);
+  const dateActuelle = DateTime.convertirEnValueType(dateAchèvementPrévisionnelleActuelle);
+
+  const [nouvelleDate, setNouvelleDate] = useState<DateTime.ValueType>(dateActuelle);
   const [validationErrors, setValidationErrors] = useState<ValidationErrors<DemanderDélaiFormKeys>>(
     {},
   );
-  return (
-    <>
-      <Alert
-        description={
-          <div>
-            Date d'achèvement prévisionnelle actuelle :{' '}
-            <FormattedDate date={dateAchèvementPrévisionnelleActuelle} className="font-semibold" />
-            {nouvelleDate && (
-              <div>
-                Nouvelle date d'achèvement prévisionnelle :{' '}
-                <FormattedDate date={nouvelleDate} className="font-semibold" />
-              </div>
-            )}
-          </div>
-        }
-        severity="info"
-        small
-      />
-      <Form
-        action={demanderDélaiAction}
-        onValidationError={(validationErrors) => setValidationErrors(validationErrors)}
-        actions={
-          <>
-            <Button
-              priority="secondary"
-              linkProps={{
-                href: Routes.Projet.details(identifiantProjet),
-                prefetch: false,
-              }}
-              iconId="fr-icon-arrow-left-line"
-            >
-              Retour à la page projet
-            </Button>
-            <SubmitButton>Demander un délai</SubmitButton>
-          </>
-        }
-      >
-        <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
-        <input
-          type={'hidden'}
-          value={dateAchèvementPrévisionnelleActuelle}
-          name="dateAchevementPrevisionnelleActuelle"
-        />
 
+  const peutDemanderUnDélai = () => nouvelleDate.estUltérieureÀ(dateActuelle);
+
+  const ajouterDélaiÀLaDateActuelle = (nombreDeMois: number) => {
+    const nouvelleDate = DateTime.convertirEnValueType(
+      dateAchèvementPrévisionnelleActuelle,
+    ).ajouterNombreDeMois(nombreDeMois);
+
+    setNouvelleDate(nouvelleDate);
+  };
+
+  return (
+    <Form
+      action={demanderDélaiAction}
+      onValidationError={(validationErrors) => setValidationErrors(validationErrors)}
+      actions={
+        <>
+          <Button
+            priority="secondary"
+            linkProps={{
+              href: Routes.Projet.details(identifiantProjet),
+              prefetch: false,
+            }}
+            iconId="fr-icon-arrow-left-line"
+          >
+            Retour à la page projet
+          </Button>
+          <SubmitButton disabledCondition={() => !peutDemanderUnDélai()}>
+            Demander un délai
+          </SubmitButton>
+        </>
+      }
+    >
+      <input type={'hidden'} value={identifiantProjet} name="identifiantProjet" />
+      <input
+        type={'hidden'}
+        value={dateAchèvementPrévisionnelleActuelle}
+        name="dateAchevementPrevisionnelleActuelle"
+      />
+
+      <div>
         <Input
           label="Durée du délai"
-          hintText="Veuillez ajouter un nombre de mois"
+          hintText="Veuillez saisir une durée de délai en mois, avec au minimum 1 mois"
+          className="mb-0"
           nativeInputProps={{
             min: '1',
             name: 'nombreDeMois',
@@ -83,39 +82,41 @@ export const DemanderDélaiForm: FC<DemanderDélaiFormProps> = ({
             'aria-required': true,
             type: 'number',
             inputMode: 'numeric',
-            pattern: '[0-9]+([.][0-9]+)?',
+            pattern: '[1-9]+',
             step: '1',
-            onChange: (e) => {
-              const date = DateTime.convertirEnValueType(
-                dateAchèvementPrévisionnelleActuelle,
-              ).ajouterNombreDeMois(Number(e.target.value));
-              setNouvelleDate(date.formatter());
-            },
+            onChange: (e) => ajouterDélaiÀLaDateActuelle(Number(e.target.value)),
           }}
           state={validationErrors['nombreDeMois'] ? 'error' : 'default'}
           stateRelatedMessage={validationErrors['nombreDeMois']}
         />
 
-        <Input
-          textArea
-          label="Raison"
-          id="raison"
-          hintText="Pour faciliter le traitement de votre demande, veuillez détailler les raisons ayant
-                conduit à cette demande de délai."
-          nativeTextAreaProps={{ name: 'raison', required: true, 'aria-required': true }}
-          state={validationErrors['raison'] ? 'error' : 'default'}
-          stateRelatedMessage={validationErrors['raison']}
-        />
+        {peutDemanderUnDélai() && (
+          <div>
+            Après accord de votre demande de délai, la date d'achèvement prévisionnelle serait le{' '}
+            <FormattedDate date={nouvelleDate.formatter()} className="font-semibold" />
+          </div>
+        )}
+      </div>
 
-        <UploadNewOrModifyExistingDocument
-          label="Pièce justificative"
-          name="pieceJustificative"
-          formats={['pdf']}
-          required
-          state={validationErrors['pieceJustificative'] ? 'error' : 'default'}
-          stateRelatedMessage={validationErrors['pieceJustificative']}
-        />
-      </Form>
-    </>
+      <Input
+        textArea
+        label="Raison"
+        id="raison"
+        hintText="Pour faciliter le traitement de votre demande, veuillez détailler les raisons ayant
+                conduit à cette demande de délai."
+        nativeTextAreaProps={{ name: 'raison', required: true, 'aria-required': true }}
+        state={validationErrors['raison'] ? 'error' : 'default'}
+        stateRelatedMessage={validationErrors['raison']}
+      />
+
+      <UploadNewOrModifyExistingDocument
+        label="Pièce justificative"
+        name="pieceJustificative"
+        formats={['pdf']}
+        required
+        state={validationErrors['pieceJustificative'] ? 'error' : 'default'}
+        stateRelatedMessage={validationErrors['pieceJustificative']}
+      />
+    </Form>
   );
 };
--- packages/applications/ssr/src/components/pages/délai/demander/DemanderDélai.page.tsx
+++ packages/applications/ssr/src/components/pages/délai/demander/DemanderDélai.page.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
+import Alert from '@codegouvfr/react-dsfr/Alert';
 
 import { ProjetBanner } from '@/components/molecules/projet/ProjetBanner';
 import { Heading1 } from '@/components/atoms/headings';
 import { PageTemplate } from '@/components/templates/Page.template';
+import { FormattedDate } from '@/components/atoms/FormattedDate';
 
 import { DemanderDélaiForm, DemanderDélaiFormProps } from './DemanderDélai.form';
 
@@ -13,7 +15,17 @@ export const DemanderDélaiPage: FC<DemanderDélaiPageProps> = ({
   dateAchèvementPrévisionnelleActuelle,
 }) => (
   <PageTemplate banner={<ProjetBanner identifiantProjet={identifiantProjet} />} feature="délai">
-    <Heading1>Demander un délai pour le projet</Heading1>
+    <Heading1>Demander un délai</Heading1>
+    <Alert
+      description={
+        <div>
+          La date d'achèvement prévisionnelle actuelle est{' '}
+          <FormattedDate date={dateAchèvementPrévisionnelleActuelle} className="font-semibold" />
+        </div>
+      }
+      severity="info"
+      small
+    />
     <DemanderDélaiForm
       identifiantProjet={identifiantProjet}
       dateAchèvementPrévisionnelleActuelle={dateAchèvementPrévisionnelleActuelle}
