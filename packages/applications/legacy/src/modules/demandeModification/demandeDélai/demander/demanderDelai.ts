47133e57f018158a0d5ab64a3e623c708df84d04
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-07-07 21:46:40 +0200

    ✏️ Legacy

--- packages/applications/legacy/src/modules/demandeModification/demandeDélai/demander/demanderDelai.ts
+++ packages/applications/legacy/src/modules/demandeModification/demandeDélai/demander/demanderDelai.ts
@@ -114,7 +114,7 @@ export const makeDemanderDélai: MakeDemanderDélai =
               ...(fileId && { fichierId: fileId }),
               justification,
               dateAchèvementDemandée,
-              autorité: appelOffre?.autoritéCompétenteDemandesDélai === 'dgec' ? 'dgec' : 'dreal',
+              autorité: appelOffre?.délai.autoritéCompétente ?? 'dreal',
               porteurId: user.id,
               cahierDesCharges: formatCahierDesChargesRéférence(project.cahierDesCharges),
             },
