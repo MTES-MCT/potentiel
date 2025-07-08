df9160e004ca28a8cefd4c14b8eb35c1a420893d
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-07-08 14:53:30 +0200

    🎨 Oupsy

--- packages/applications/ssr/src/utils/historique/mapToProps/raccordement/events/gestionnaire-réseau/mapToGestionnaireRéseauAttribuéTimelineItemProps.tsx
+++ packages/applications/ssr/src/utils/historique/mapToProps/raccordement/events/gestionnaire-réseau/mapToGestionnaireRéseauAttribuéTimelineItemProps.tsx
@@ -4,19 +4,10 @@ import { Lauréat } from '@potentiel-domain/projet';
 export const mapToGestionnaireRéseauAttribuéTimelineItemProps = (
   attribution: Lauréat.ListerHistoriqueProjetReadModel['items'][number],
 ) => {
-  const { identifiantGestionnaireRéseau } =
-    attribution.payload as Lauréat.Raccordement.GestionnaireRéseauAttribuéEvent['payload'];
-
   return {
     date: attribution.createdAt as DateTime.RawType,
     title: (
       <div>Un gestionnaire de réseau de raccordement a été attribué au raccordement du projet</div>
-    ),
-    content: (
-      <div>
-        Gestionnaire attribué :{' '}
-        <span className="font-semibold">{identifiantGestionnaireRéseau}</span>
-      </div>
     ),
   };
 };
--- packages/applications/ssr/src/utils/historique/mapToProps/raccordement/events/gestionnaire-réseau/mapToGestionnaireRéseauRaccordementModifiéTimelineItemProps.tsx
+++ packages/applications/ssr/src/utils/historique/mapToProps/raccordement/events/gestionnaire-réseau/mapToGestionnaireRéseauRaccordementModifiéTimelineItemProps.tsx
@@ -4,16 +4,8 @@ import { Lauréat } from '@potentiel-domain/projet';
 export const mapToGestionnaireRéseauRaccordementModifiéTimelineItemProps = (
   modification: Lauréat.ListerHistoriqueProjetReadModel['items'][number],
 ) => {
-  const { identifiantGestionnaireRéseau } =
-    modification.payload as Lauréat.Raccordement.GestionnaireRéseauRaccordementModifiéEvent['payload'];
   return {
     date: modification.createdAt as DateTime.RawType,
     title: <div>Nouveau gestionnaire de réseau de raccordement enregistré</div>,
-    content: (
-      <div>
-        Nouveau gestionnaire :{' '}
-        <span className="font-semibold">{identifiantGestionnaireRéseau}</span>
-      </div>
-    ),
   };
 };
