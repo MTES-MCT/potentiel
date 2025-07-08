b8b094711316c138f38a4585a076ab99c9916718
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-07-08 16:05:45 +0200

    ♻️  Meilleur gestion lorsqu'on ajoute des mois et que c'est le dernier jour du mois (#3233)

--- packages/domain/common/src/valueTypes/dateTime.valueType.spec.ts
+++ packages/domain/common/src/valueTypes/dateTime.valueType.spec.ts
@@ -25,6 +25,39 @@ describe('DateTime', () => {
       expect(date.ajouterNombreDeMois(1).formatter()).to.eq('2025-01-01T00:00:00.000Z');
       expect(date.ajouterNombreDeMois(1).formatter()).to.eq('2025-01-01T00:00:00.000Z');
     });
+
+    const fixtures = [
+      {
+        date: '2024-12-01T00:00:00.000Z',
+        nombreDeMois: 1,
+        expected: '2025-01-01T00:00:00.000Z',
+      },
+      {
+        date: '2024-02-29T00:00:00.000Z',
+        nombreDeMois: 1,
+        expected: '2024-03-31T00:00:00.000Z',
+      },
+      {
+        date: '2020-02-29T00:00:00.000Z',
+        nombreDeMois: 56,
+        expected: '2024-10-31T00:00:00.000Z',
+      },
+      {
+        date: '2023-04-21T00:00:00.000Z',
+        nombreDeMois: 46,
+        expected: '2027-02-21T00:00:00.000Z',
+      },
+    ];
+    fixtures.map((fixture) => {
+      it('should add correctly months to a date', () => {
+        const date = DateTime.convertirEnValueType(fixture.date);
+
+        const actual = date.ajouterNombreDeMois(fixture.nombreDeMois);
+
+        const expected = DateTime.convertirEnValueType(fixture.expected).formatter();
+        expect(actual.formatter()).to.equal(expected);
+      });
+    });
   });
   describe('retirerNombreDeMois', () => {
     it('is pure', () => {
