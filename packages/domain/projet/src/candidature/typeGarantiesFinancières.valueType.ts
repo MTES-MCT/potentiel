4641eee976f56bfc0e529639041e35de4220f782
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-07-11 09:54:03 +0200

    🎨 WIP

--- packages/domain/projet/src/candidature/typeGarantiesFinancières.valueType.ts
+++ packages/domain/projet/src/candidature/typeGarantiesFinancières.valueType.ts
@@ -5,12 +5,15 @@ export const types = [
   'avec-date-échéance',
   'six-mois-après-achèvement',
   'type-inconnu',
+  'garantie-bancaire',
 ] as const;
 
 export type RawType = (typeof types)[number];
 
 export type ValueType = ReadonlyValueType<{
   type: RawType;
+  estDisponiblePourCetAppelOffre({ value, typesDeAppelOffre });
+  estGarantiesBancaire: () => boolean;
   estConsignation: () => boolean;
   estAvecDateÉchéance: () => boolean;
   estSixMoisAprèsAchèvement: () => boolean;
@@ -19,12 +22,16 @@ export type ValueType = ReadonlyValueType<{
 
 export const convertirEnValueType = (value: string): ValueType => {
   estValide(value);
+  estDisponiblePourCetAppelOffre()
   return {
     get type() {
       return value;
     },
     estÉgaleÀ(valueType) {
       return this.type === valueType.type;
+    },
+    estGarantieBancaire() {
+      return this.type === 'garantie-bancaire';
     },
     estConsignation() {
       return this.type === 'consignation';
@@ -49,14 +56,37 @@ function estValide(value: string): asserts value is RawType {
   }
 }
 
+export const estDisponiblePourCetAppelOffre({
+  value,
+  typesDeAppelOffre,
+}: {
+  value: RawType;
+  typesDeAppelOffre: Array<RawType>;
+}) {
+  // on teste les types ou on donne un rawtype ?
+
+  if (!typesDeAppelOffre.includes(value)) {
+    throw new TypeGarantiesFinancièresNonDisponiblePourAppelOffreError(value);
+  }
+}
+
 export const consignation = convertirEnValueType('consignation');
 export const avecDateÉchéance = convertirEnValueType('avec-date-échéance');
 export const sixMoisAprèsAchèvement = convertirEnValueType('six-mois-après-achèvement');
+export const garantieBancaire = convertirEnValueType('garantie-bancaire');
 export const typeInconnu = convertirEnValueType('type-inconnu');
 
 class TypeGarantiesFinancièresInvalideError extends InvalidOperationError {
   constructor(value: string) {
     super(`Le type ne correspond à aucune valeur connue`, {
+      value,
+    });
+  }
+}
+
+class TypeGarantiesFinancièresNonDisponiblePourAppelOffreError extends InvalidOperationError {
+  constructor(value: string) {
+    super(`Ce type de garanties financières n'est pas disponible pour cet appel d'offre`, {
       value,
     });
   }
