b6d26024ee96383328f35af05d68536e0893dc81
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-07-18 17:19:27 +0200

    🚧 Ajout du value type DateAchevementPrévisionnel

---
+++ packages/domain/projet/src/lauréat/achèvement/dateAchèvementPrévisionnel.valueType.ts
@@ -0,0 +1,44 @@
+import { DateTime } from '@potentiel-domain/common';
+import { PlainType } from '@potentiel-domain/core';
+
+export type RawType = DateTime.RawType;
+
+export type ValueType = {
+  dateTime: DateTime.ValueType;
+  ajouterDélai: (nombreDeMois: number) => ValueType;
+  retirerDélai: (nombreDeMois: number) => ValueType;
+  formatter: () => DateTime.RawType;
+  formatterDate: () => string;
+};
+
+export const bind = ({ dateTime: date }: PlainType<ValueType>): ValueType => {
+  return convertirEnValueType(date.date);
+};
+
+const duréeInstructionEdfOaEnJours = 1;
+
+export const convertirEnValueType = (value: Date | string): ValueType => {
+  return {
+    dateTime: DateTime.convertirEnValueType(value),
+    formatter() {
+      return this.dateTime.formatter();
+    },
+    formatterDate() {
+      return this.dateTime.formatterDate();
+    },
+    ajouterDélai(nombreDeMois) {
+      const dateAvecDélai = this.dateTime
+        .ajouterNombreDeMois(nombreDeMois)
+        .retirerNombreDeJours(duréeInstructionEdfOaEnJours);
+
+      return convertirEnValueType(dateAvecDélai.date);
+    },
+    retirerDélai(nombreDeMois) {
+      const dateSansDélai = this.dateTime
+        .ajouterNombreDeJours(duréeInstructionEdfOaEnJours)
+        .retirerNombreDeMois(nombreDeMois);
+
+      return convertirEnValueType(dateSansDélai.date);
+    },
+  };
+};
