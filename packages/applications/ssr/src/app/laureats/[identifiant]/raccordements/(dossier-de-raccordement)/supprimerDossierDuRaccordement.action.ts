92a4bbe84641feabc90524cfdd05765ed039b896
Violette <27735540+VioMrqs@users.noreply.github.com>
2025-07-25 14:36:16 +0200

    🐛 Redirection quand suppression demande complete raccordement (#3334)

--- packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/(dossier-de-raccordement)/supprimerDossierDuRaccordement.action.ts
+++ packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/(dossier-de-raccordement)/supprimerDossierDuRaccordement.action.ts
@@ -5,6 +5,7 @@ import * as zod from 'zod';
 
 import { Routes } from '@potentiel-applications/routes';
 import { Lauréat } from '@potentiel-domain/projet';
+import { Option } from '@potentiel-libraries/monads';
 
 import { FormAction, formAction, FormState } from '@/utils/formAction';
 
@@ -17,6 +18,15 @@ const action: FormAction<FormState, typeof schema> = async (
   _,
   { identifiantProjet, referenceDossier },
 ) => {
+  const raccordement = await mediator.send<Lauréat.Raccordement.ConsulterRaccordementQuery>({
+    type: 'Lauréat.Raccordement.Query.ConsulterRaccordement',
+    data: {
+      identifiantProjetValue: identifiantProjet,
+    },
+  });
+
+  const estLeDernierDossier = Option.isSome(raccordement) && raccordement.dossiers.length === 1;
+
   await mediator.send<Lauréat.Raccordement.RaccordementUseCase>({
     type: 'Lauréat.Raccordement.UseCase.SupprimerDossierDuRaccordement',
     data: {
@@ -27,7 +37,11 @@ const action: FormAction<FormState, typeof schema> = async (
 
   return {
     status: 'success',
-    redirection: { url: Routes.Raccordement.détail(identifiantProjet) },
+    redirection: {
+      url: estLeDernierDossier
+        ? Routes.Projet.details(identifiantProjet)
+        : Routes.Raccordement.détail(identifiantProjet),
+    },
   };
 };
 
