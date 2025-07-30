41f455ebf3a94d7f0eab0a26b73de610512640ed
Julien Pavon <17512486+JulienPavon@users.noreply.github.com>
2025-07-30 16:37:31 +0200

    ♻️  Remove useless arg

--- packages/domain/projet/src/lauréat/délai/demande/supprimer/supprimerDemandeDélai.options.ts
+++ packages/domain/projet/src/lauréat/délai/demande/supprimer/supprimerDemandeDélai.options.ts
@@ -3,5 +3,4 @@ import { DateTime, Email } from '@potentiel-domain/common';
 export type SupprimerDemandeDélaiOptions = {
   dateSuppression: DateTime.ValueType;
   identifiantUtilisateur: Email.ValueType;
-  nePasLeverUneErreur?: true;
 };
