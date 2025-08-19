4d700a513c31beb1e73f33ebb9bb8900270e6e42
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-08-19 10:16:43 +0200

    🚚 Regrouper les composants relatifs au Header dans un meme dossier

---
+++ packages/applications/ssr/src/components/organisms/header/Header.tsx
@@ -0,0 +1,40 @@
+import DsfrHeader from '@codegouvfr/react-dsfr/Header';
+
+import { UserBasedRoleNavigation } from './UserBasedRoleNavigation';
+import { UserHeaderQuickAccessItem } from './UserHeaderQuickAccessItem';
+
+export const Header = () => (
+  <DsfrHeader
+    brandTop={
+      <>
+        République
+        <br />
+        Française
+      </>
+    }
+    serviceTitle="Potentiel"
+    serviceTagline={
+      <>
+        Facilite le parcours des producteurs
+        <br />
+        d'énergies renouvelables électriques
+      </>
+    }
+    homeLinkProps={{
+      href: '/',
+      title: "Retour à l'accueil",
+    }}
+    quickAccessItems={[
+      <UserHeaderQuickAccessItem key={0} />,
+      {
+        iconId: 'ri-question-line',
+        linkProps: {
+          target: '_blank',
+          href: 'https://docs.potentiel.beta.gouv.fr/guide-dutilisation/sommaire-du-guide-dutilisation',
+        },
+        text: 'Aide',
+      },
+    ]}
+    navigation={<UserBasedRoleNavigation />}
+  />
+);
