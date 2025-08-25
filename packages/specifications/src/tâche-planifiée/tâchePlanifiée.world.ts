4e0a640021e1834517adf9ec4d6aa5a544a2051f
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-08-25 12:03:03 +0200

    ✅ Clarifier les steps tâche planifiée

--- packages/specifications/src/tâche-planifiée/tâchePlanifiée.world.ts
+++ packages/specifications/src/tâche-planifiée/tâchePlanifiée.world.ts
@@ -10,8 +10,6 @@ export type TypeTâchePlanifiée =
   | "rappel d'instruction de la demande de changement de représentant légal à deux mois"
   | 'supprimer automatiquement le document à trois mois';
 
-export type RechercherStatutTâchePlanifiée = 'planifiée' | 'annulée' | 'exécutée';
-
 export class TâchePlanifiéeWorld {
   rechercherTypeTâchePlanifiée(value: TypeTâchePlanifiée) {
     return match(value)
@@ -48,14 +46,6 @@ export class TâchePlanifiéeWorld {
           Lauréat.ReprésentantLégal.TypeTâchePlanifiéeChangementReprésentantLégal
             .suppressionDocumentÀTroisMois,
       )
-      .exhaustive();
-  }
-
-  rechercherStatutTâchePlanifiée(value: RechercherStatutTâchePlanifiée) {
-    return match(value)
-      .with('planifiée', () => Lauréat.TâchePlanifiée.StatutTâchePlanifiée.enAttenteExécution)
-      .with('annulée', () => Lauréat.TâchePlanifiée.StatutTâchePlanifiée.annulée)
-      .with('exécutée', () => Lauréat.TâchePlanifiée.StatutTâchePlanifiée.exécutée)
       .exhaustive();
   }
 }
