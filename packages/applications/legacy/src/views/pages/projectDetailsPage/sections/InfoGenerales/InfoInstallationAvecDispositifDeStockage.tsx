45b49a2b527b46e8c17974655eacfa4a8db9557c
Hanaë <hanae.yamashita@lilo.org>
2025-09-12 21:34:39 +0200

    ✨ Modifier depuis le détail du projet

--- packages/applications/legacy/src/views/pages/projectDetailsPage/sections/InfoGenerales/InfoInstallationAvecDispositifDeStockage.tsx
+++ packages/applications/legacy/src/views/pages/projectDetailsPage/sections/InfoGenerales/InfoInstallationAvecDispositifDeStockage.tsx
@@ -15,6 +15,14 @@ export const InfoInstallationAvecDispositifDeStockage = ({
     <div className="flex flex-col gap-0">
       <Heading3 className="m-0">Installation couplée à un dispositif de stockage</Heading3>
       <span>{value === true ? 'oui' : value === false ? 'non' : 'Non renseigné'}</span>
+      {installationAvecDispositifDeStockage.affichage && (
+        <Link
+          href={installationAvecDispositifDeStockage.affichage.url}
+          aria-label={installationAvecDispositifDeStockage.affichage.label}
+        >
+          {installationAvecDispositifDeStockage.affichage.label}
+        </Link>
+      )}
     </div>
   );
 };
