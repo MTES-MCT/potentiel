3c54356504fe0775a6fc93c6dc76822c9f4d5067
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-09-18 09:42:00 +0200

    🎨 Fix: typo

--- packages/applications/ssr/src/app/laureats/[identifiant]/delai/InfoBoxDemandeDélai.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/delai/InfoBoxDemandeDélai.tsx
@@ -10,7 +10,7 @@ export const InfoBoxDemandeDélai: FC = () => (
         Cette demande concerne les délais de <span className="font-semibold">force majeure</span>{' '}
         laissés à l’appréciation du préfet ou du ministre chargé de l’énergie, et{' '}
         <span className="font-semibold">non les délais pour contentieux ou raccordement</span> qui
-        sont automatiquement attribués et vérifiés a posteriori par le co-contractant ..
+        sont automatiquement attribués et vérifiés a posteriori par le co-contractant.
       </div>
     }
   />
