50b0e893cb060c407b83dd12c632fabd61c54ed4
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-10-07 12:34:36 +0200

    ⚗️ Load tests

---
+++ packages/applications/cli/src/commands/load-test/importer-candidatures.ts
@@ -0,0 +1,178 @@
+import { Command, Flags } from '@oclif/core';
+import zod from 'zod';
+import { faker } from '@faker-js/faker';
+import { mediator } from 'mediateur';
+
+import { Candidature, IdentifiantProjet, registerProjetUseCases } from '@potentiel-domain/projet';
+import { getDépôtCandidature } from '@potentiel-infrastructure/ds-api-client';
+import { Option } from '@potentiel-libraries/monads';
+import { appelsOffreData } from '@potentiel-domain/inmemory-referential';
+import { DocumentAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { DateTime, Email } from '@potentiel-domain/common';
+import { registerDocumentProjetCommand } from '@potentiel-domain/document';
+import { getProjetAggregateRootAdapter } from '@potentiel-applications/bootstrap';
+
+const envSchema = zod.object({
+  APPLICATION_STAGE: zod.string(),
+  DATABASE_CONNECTION_STRING: zod.url(),
+  DS_API_URL: zod.url(),
+  DS_API_TOKEN: zod.string(),
+});
+
+export class ImporterCandidatures extends Command {
+  static flags = {
+    occurrences: Flags.integer({
+      default: 100,
+    }),
+    dossier: Flags.integer({
+      description: 'Numéro du dossier dans démarches simplifiées, importé N fois',
+      required: true,
+    }),
+    appelOffre: Flags.string({ options: appelsOffreData.map((ao) => ao.id), required: true }),
+    periode: Flags.integer({ max: 100, required: true }),
+    reuse: Flags.boolean(),
+    skipDocuments: Flags.boolean(),
+  };
+
+  async init() {
+    const { APPLICATION_STAGE } = envSchema.parse(process.env);
+    if (APPLICATION_STAGE === 'production') {
+      console.log(`This job can't be executed on ${APPLICATION_STAGE} environment`);
+      this.exit(1);
+    }
+    const { flags } = await this.parse(ImporterCandidatures);
+
+    if (flags.skipDocuments) {
+      registerDocumentProjetCommand({
+        enregistrerDocumentProjet: async () => {},
+        archiverDocumentProjet: async () => {},
+        déplacerDossierProjet: async () => {},
+      });
+    } else {
+      registerDocumentProjetCommand({
+        enregistrerDocumentProjet: DocumentAdapter.téléverserDocumentProjet,
+        déplacerDossierProjet: DocumentAdapter.déplacerDossierProjet,
+        archiverDocumentProjet: DocumentAdapter.archiverDocumentProjet,
+      });
+    }
+
+    registerProjetUseCases({
+      getProjetAggregateRoot: getProjetAggregateRootAdapter,
+      supprimerDocumentProjetSensible: DocumentAdapter.remplacerDocumentProjetSensible,
+    });
+  }
+
+  async run() {
+    const importId = faker.string.uuid().slice(0, 8);
+
+    console.log(`--- Début du job d'import de candidatures (Import ${importId}) ---`);
+    const { flags } = await this.parse(ImporterCandidatures);
+    const { occurrences, appelOffre, periode, dossier: numeroDossierDS, reuse } = flags;
+    const candidatures: Omit<
+      Candidature.ImporterCandidatureUseCase['data'],
+      'importéLe' | 'importéPar'
+    >[] = [];
+
+    const instructions = Array(occurrences)
+      .fill(null)
+      .map((_, i) => ({
+        numeroDossierDS,
+        identifiantProjet: IdentifiantProjet.bind({
+          appelOffre,
+          période: String(periode),
+          famille: '',
+          numéroCRE: `${numeroDossierDS}_${importId}_${i}`,
+        }).formatter(),
+        statut: faker.helpers.arrayElement(Candidature.StatutCandidature.statuts),
+        note: faker.number.int({ min: 0, max: 100 }),
+        motifElimination: faker.lorem.sentence(),
+      }));
+
+    const start = process.hrtime.bigint();
+
+    for (const {
+      numeroDossierDS,
+      identifiantProjet,
+      statut,
+      note,
+      motifElimination,
+    } of instructions) {
+      const dossier =
+        reuse && candidatures[0]
+          ? {
+              dépôt: candidatures[0].dépôtValue,
+              demarcheId: '',
+              fichiers: { garantiesFinancières: undefined },
+            }
+          : await getDépôtCandidature(numeroDossierDS);
+
+      if (Option.isNone(dossier)) {
+        throw new Error(`Le dossier ${numeroDossierDS} est introuvable`);
+      }
+
+      const dépôt = dossier.dépôt;
+      const instruction: Candidature.Instruction.RawType = {
+        statut,
+        noteTotale: note,
+        motifÉlimination: motifElimination,
+      };
+
+      candidatures.push({
+        identifiantProjetValue: identifiantProjet,
+        dépôtValue: { ...dépôt, puissanceALaPointe: false } as Candidature.Dépôt.RawType,
+        détailsValue: {
+          typeImport: 'démarches-simplifiées',
+          demarcheId: dossier.demarcheId,
+          pièceJustificativeGf: dossier.fichiers.garantiesFinancières?.url ?? '',
+        },
+        instructionValue: instruction,
+      });
+    }
+
+    const endApiCalls = process.hrtime.bigint();
+    const durationAPICalls = Number(endApiCalls - start) / 1_000_000;
+    this.log(
+      `Chargement de ${occurrences} candidatures depuis l'API DS en ${durationAPICalls.toFixed(2)}ms (${(
+        durationAPICalls / occurrences
+      ).toFixed(2)}ms/candidature)`,
+    );
+
+    let importées = 0;
+    setInterval(() => {
+      console.log(`${importées}/${occurrences} candidatures importées`);
+    }, 1000).unref();
+    for (const {
+      identifiantProjetValue,
+      dépôtValue,
+      instructionValue,
+      détailsValue,
+    } of candidatures) {
+      await mediator.send<Candidature.ImporterCandidatureUseCase>({
+        type: 'Candidature.UseCase.ImporterCandidature',
+        data: {
+          identifiantProjetValue,
+          dépôtValue,
+          instructionValue,
+          détailsValue,
+          importéLe: DateTime.now().formatter(),
+          importéPar: Email.system().formatter(),
+        },
+      });
+      importées++;
+    }
+    const end = process.hrtime.bigint();
+    const durationTotal = Number(end - start) / 1_000_000;
+    const duractionImport = Number(end - endApiCalls) / 1_000_000;
+    this.log(
+      `Import de ${occurrences} candidatures terminé en ${duractionImport.toFixed(2)}ms (${(
+        duractionImport / occurrences
+      ).toFixed(2)}ms/candidature)`,
+    );
+
+    this.log(
+      `Total (API+import): ${occurrences} candidatures en ${durationTotal.toFixed(2)}ms (${(
+        durationTotal / occurrences
+      ).toFixed(2)}ms/candidature)`,
+    );
+  }
+}
---
+++ packages/applications/cli/src/commands/load-test/notifier-candidatures.ts
@@ -0,0 +1,106 @@
+import { Command, Flags } from '@oclif/core';
+import zod from 'zod';
+import { mediator } from 'mediateur';
+
+import {
+  Candidature,
+  registerProjetQueries,
+  registerProjetUseCases,
+} from '@potentiel-domain/projet';
+import { appelsOffreData } from '@potentiel-domain/inmemory-referential';
+import { DocumentAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { DateTime, Email } from '@potentiel-domain/common';
+import { getProjetAggregateRootAdapter } from '@potentiel-applications/bootstrap';
+import {
+  countProjection,
+  findProjection,
+  listHistoryProjection,
+  listProjection,
+} from '@potentiel-infrastructure/pg-projection-read';
+import { Période } from '@potentiel-domain/periode';
+import { loadAggregate, loadAggregateV2 } from '@potentiel-infrastructure/pg-event-sourcing';
+import { registerUtilisateurUseCases } from '@potentiel-domain/utilisateur';
+
+const envSchema = zod.object({
+  APPLICATION_STAGE: zod.string(),
+  DATABASE_CONNECTION_STRING: zod.url(),
+});
+
+export class NotifierCandidatures extends Command {
+  static flags = {
+    appelOffre: Flags.string({ options: appelsOffreData.map((ao) => ao.id), required: true }),
+    periode: Flags.integer({ max: 100, required: true }),
+  };
+
+  async init() {
+    const { APPLICATION_STAGE } = envSchema.parse(process.env);
+    if (APPLICATION_STAGE === 'production') {
+      console.log(`This job can't be executed on ${APPLICATION_STAGE} environment`);
+      this.exit(1);
+    }
+
+    Période.registerPériodeUseCases({
+      getProjetAggregateRoot: getProjetAggregateRootAdapter,
+      loadAggregate: loadAggregateV2,
+    });
+
+    registerProjetQueries({
+      count: countProjection,
+      find: findProjection,
+      list: listProjection,
+      listHistory: listHistoryProjection,
+
+      consulterABénéficiéDuDélaiCDC2022: async () => false,
+      getScopeProjetUtilisateur: async () => ({ type: 'all' as const }),
+      récupérerProjetsEligiblesPreuveRecanditure: async () => [],
+    });
+
+    registerUtilisateurUseCases({
+      loadAggregate,
+    });
+
+    registerProjetUseCases({
+      getProjetAggregateRoot: getProjetAggregateRootAdapter,
+      supprimerDocumentProjetSensible: DocumentAdapter.remplacerDocumentProjetSensible,
+    });
+  }
+
+  async run() {
+    console.log(`--- Début du job de notifications de candidatures ---`);
+    const { flags } = await this.parse(NotifierCandidatures);
+    const { appelOffre, periode } = flags;
+    const candidatures = await mediator.send<Candidature.ListerCandidaturesQuery>({
+      type: 'Candidature.Query.ListerCandidatures',
+      data: {
+        appelOffre,
+        période: String(periode),
+        estNotifiée: false,
+      },
+    });
+
+    const start = process.hrtime.bigint();
+
+    await mediator.send<Période.NotifierPériodeUseCase>({
+      type: 'Période.UseCase.NotifierPériode',
+      data: {
+        identifiantCandidatureValues: candidatures.items.map((c) =>
+          c.identifiantProjet.formatter(),
+        ),
+        identifiantPériodeValue: `${appelOffre}#${periode}`,
+        notifiéeLeValue: DateTime.now().formatter(),
+        notifiéeParValue: Email.system().formatter(),
+        validateurValue: {
+          fonction: 'Système',
+          nomComplet: 'Système',
+        },
+      },
+    });
+    const end = process.hrtime.bigint();
+    const durationTotal = Number(end - start) / 1_000_000;
+    this.log(
+      `Notification de ${candidatures.items.length} candidatures terminé en ${durationTotal.toFixed(2)}ms (${(
+        durationTotal / candidatures.items.length
+      ).toFixed(2)}ms/candidature)`,
+    );
+  }
+}
