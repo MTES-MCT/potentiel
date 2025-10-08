621889e77cc835383847e77472d077efd2ad5233
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-10-08 10:57:37 +0200

    📝 Restaurer un dump de la DB sur un env de test (#3546)

---
+++ .database/scripts/restore-test-env-db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This script restores a database dump for a test environment or review app.
+# For Scalingo review apps, it is automatically triggered by the "first-deploy" script in scalingo.json
+
+set -e
+set -u
+
+if [[ "$APPLICATION_NAME" == *"production"* ]]; then
+  echo "❌ This script cannot be run in production environment"
+  exit 1
+fi
+
+dbclient-fetcher pgsql
+
+if [ -f "./.database/potentiel-dev.dump" ]; then
+	pg_restore --clean --no-acl --no-owner -d $SCALINGO_POSTGRESQL_URL < ./.database/potentiel-dev.dump
+	echo "✨ Potentiel Database has been restored with potentiel-dev dump file✨"
+else
+	echo "❌ Potentiel database dump file not found"
+fi
