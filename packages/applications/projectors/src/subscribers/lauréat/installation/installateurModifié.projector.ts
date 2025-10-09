82c03e55d763137ba36743c35e51658496a7ed96
Hanaë <hanae.yamashita@lilo.org>
2025-10-09 14:14:11 +0200

    🐛 Fix projector

--- packages/applications/projectors/src/subscribers/lauréat/installation/installateurModifié.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/installation/installateurModifié.projector.ts
@@ -1,15 +1,14 @@
 import { Lauréat } from '@potentiel-domain/projet';
-import { upsertProjection } from '@potentiel-infrastructure/pg-projection-write';
+import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const installateurModifiéProjector = async ({
   payload: { identifiantProjet, installateur, modifiéLe },
 }: Lauréat.Installation.InstallateurModifiéEvent) => {
-  await upsertProjection<Lauréat.Installation.InstallationEntity>(
+  await updateOneProjection<Lauréat.Installation.InstallationEntity>(
     `installation|${identifiantProjet}`,
     {
       identifiantProjet,
       installateur,
-      typologieInstallation: [],
       misÀJourLe: modifiéLe,
     },
   );
