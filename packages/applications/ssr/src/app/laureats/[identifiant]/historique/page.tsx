ece51465dd5188202e37611528443635b644a066
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-10-15 15:47:56 +0200

    🚚 Déplacement des items historique pour installateur

--- packages/applications/ssr/src/app/laureats/[identifiant]/historique/page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/historique/page.tsx
@@ -18,7 +18,6 @@ import { mapToRecoursTimelineItemProps } from '@/app/elimine/[identifiant]/recou
 import { mapToÉliminéTimelineItemProps } from '@/app/elimine/[identifiant]/(historique)/mapToÉliminéTimelineItemProps';
 
 import { getLauréatInfos } from '../_helpers/getLauréat';
-import { mapToInstallateurTimelineItemProps } from '../../../../utils/historique/mapToProps/installateur/mapToInstallateurTimelineItemProps';
 import { mapToNatureDeLExploitationTimelineItemProps } from '../../../../utils/historique/mapToProps/nature-de-l-exploitation/mapToNatureDeLExploitationTimelineItemProps';
 import { mapToAchèvementTimelineItemProps } from '../achevement/(historique)/mapToAchèvementTimelineItemProps';
 import { mapToActionnaireTimelineItemProps } from '../actionnaire/(historique)/mapToActionnaireTimelineItemProps';
@@ -31,6 +30,7 @@ import { mapToAbandonTimelineItemProps } from '../abandon/(historique)/mapToAban
 import { mapToRaccordementTimelineItemProps } from '../raccordements/(historique)/mapToRaccordementTimelineItemProps';
 import { mapToDélaiTimelineItemProps } from '../delai/(historique)/mapToDélaiTimelineItemProps';
 import { mapToFournisseurTimelineItemProps } from '../fournisseur/changement/(historique)';
+import { mapToInstallateurTimelineItemProps } from '../installateur/(historique)/mapToInstallateurTimelineItemProps';
 
 import { HistoriqueLauréatAction, HistoriqueLauréatPage } from './HistoriqueLauréat.page';
 
