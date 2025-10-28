d1d3760830e8c2f2ac9e63978e86bdfef4f079f8
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-10-28 11:41:46 +0100

    🎨 Ajout menu SSR

--- packages/applications/legacy/src/views/components/UI/organisms/menuLegacy/AdminMenuLegacy.tsx
+++ packages/applications/legacy/src/views/components/UI/organisms/menuLegacy/AdminMenuLegacy.tsx
@@ -99,7 +99,6 @@ export const AdminMenuLegacy = (
         Inviter un utilisateur
       </DropdownMenu.DropdownItem>
     </DropdownMenu>
-
     <DropdownMenu buttonChildren={'Outils'}>
       <DropdownMenu.DropdownItem href={'https://potentiel.e2.rie.gouv.fr/'} externe>
         Tableau de bord
