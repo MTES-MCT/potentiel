a4ed3cb86e7c4f4dde98d18944357cac13cae500
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-10-31 11:04:17 +0100

    ♻️ Carto_projet_statistic

--- packages/statistiques/statistiques-publiques/src/08-carte/projetLauréatParDépartement.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/08-carte/projetLauréatParDépartement.statistic.ts
@@ -6,18 +6,33 @@ export const computeProjetLauréatParDépartement = async () => {
     insert
     into 
       domain_public_statistic.carto_projet_statistic
-    select
-      concat("appelOffreId", '#', "periodeId", '#', "familleId", '#', "numeroCRE") as identifiant,
-      "appelOffreId" as "appelOffre",
-      TO_TIMESTAMP(("notifiedOn")::BIGINT / 1000)::DATE as "dateNotification",
-      "departementProjet",
-      "isFinancementParticipatif",
-      "isInvestissementParticipatif",
-      "puissance"
-    from "projects"
-    where 
-      classe = 'Classé'
-      and "abandonedOn" = 0
+    select laur.value->>'identifiantProjet' as "identifiant",
+      laur.value->>'appelOffre' as "appelOffre",
+      SUBSTRING(laur.value->>'notifiéLe', 0, 11) as "dateNotification",
+      laur.value->>'localité.département' as "departementProjet",
+      (
+        case
+          when candidature.value->>'actionnariat' = 'financement-participatif' then TRUE
+          else FALSE
+        end
+      ) as "isFinancementParticipatif",
+      (
+        case
+          when candidature.value->>'actionnariat' = 'investissement-participatif' then TRUE
+          else FALSE
+        end
+      ) as "isInvestissementParticipatif",
+      (puiss.value->>'puissance')::float as "puissance"
+    from domain_views.projection laur
+      inner join domain_views.projection puiss on puiss.key = format('puissance|%s', laur.value->>'identifiantProjet')
+      inner join domain_views.projection candidature on candidature.key = format(
+        'candidature|%s',
+        laur.value->>'identifiantProjet'
+      )
+      left join domain_views.projection abandon on abandon.key = format('abandon|%s', laur.value->>'identifiantProjet')
+      and abandon.value->>'statut' = 'accordé'
+    where laur.key like 'lauréat|%'
+      and abandon.key is null;
     `,
   );
 };
