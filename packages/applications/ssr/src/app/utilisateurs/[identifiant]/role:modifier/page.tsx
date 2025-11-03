109429d7d5d0d6994ee817f533345f908b012761
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-03 10:45:10 +0100

    🔀 Rôle

--- packages/applications/ssr/src/app/utilisateurs/[identifiant]/role:modifier/page.tsx
+++ packages/applications/ssr/src/app/utilisateurs/[identifiant]/role:modifier/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({
 }) {
   return PageWithErrorHandling(async () =>
     withUtilisateur(async (utilisateur) => {
-      utilisateur.role.peutExécuterMessage<ModifierRôleUtilisateurUseCase>(
+      utilisateur.rôle.peutExécuterMessage<ModifierRôleUtilisateurUseCase>(
         'Utilisateur.UseCase.ModifierRôleUtilisateur',
       );
 
