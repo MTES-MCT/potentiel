c44d684037fc5d5cc212112df6afae2d4083cae1
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-06 14:42:09 +0100

    🎨 Simplification projecteurs

--- packages/applications/projectors/src/subscribers/lauréat/installation/dispositifDeStockageModifié.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/installation/dispositifDeStockageModifié.projector.ts
@@ -1,28 +1,12 @@
 import { Lauréat } from '@potentiel-domain/projet';
-import { findProjection } from '@potentiel-infrastructure/pg-projection-read';
-import { upsertProjection } from '@potentiel-infrastructure/pg-projection-write';
-import { Option } from '@potentiel-libraries/monads';
-import { getLogger } from '@potentiel-libraries/monitoring';
+import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const dispositifDeStockageModifiéProjector = async ({
   payload: { identifiantProjet, modifiéLe, dispositifDeStockage },
 }: Lauréat.Installation.DispositifDeStockageModifiéEvent) => {
-  const projectionToUpsert = await findProjection<Lauréat.Installation.InstallationEntity>(
-    `installation|${identifiantProjet}`,
-  );
-
-  if (Option.isNone(projectionToUpsert)) {
-    getLogger().error(`Installation non trouvée`, {
-      identifiantProjet,
-      fonction: 'dispositifDeStockageModifiéProjector',
-    });
-    return;
-  }
-
-  await upsertProjection<Lauréat.Installation.InstallationEntity>(
+  await updateOneProjection<Lauréat.Installation.InstallationEntity>(
     `installation|${identifiantProjet}`,
     {
-      ...projectionToUpsert,
       dispositifDeStockage,
       miseÀJourLe: modifiéLe,
       identifiantProjet,
--- packages/applications/projectors/src/subscribers/tâche/tâche/tâcheRenouvellée.projector.ts
+++ packages/applications/projectors/src/subscribers/tâche/tâche/tâcheRenouvellée.projector.ts
@@ -1,34 +1,11 @@
-import { DateTime } from '@potentiel-domain/common';
 import { Lauréat } from '@potentiel-domain/projet';
-import { findProjection } from '@potentiel-infrastructure/pg-projection-read';
-import { upsertProjection } from '@potentiel-infrastructure/pg-projection-write';
-import { Option } from '@potentiel-libraries/monads';
+import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const tâcheRenouvelléeProjector = async ({
   payload: { identifiantProjet, typeTâche, ajoutéeLe },
 }: Lauréat.Tâche.TâcheRenouvelléeEvent) => {
-  const tâche = await récupérerTâche(typeTâche, identifiantProjet);
-
-  await upsertProjection<Lauréat.Tâche.TâcheEntity>(`tâche|${typeTâche}#${identifiantProjet}`, {
-    ...tâche,
+  await updateOneProjection<Lauréat.Tâche.TâcheEntity>(`tâche|${typeTâche}#${identifiantProjet}`, {
     typeTâche,
     miseÀJourLe: ajoutéeLe,
   });
-};
-
-const récupérerTâche = async (typeTâche: string, identifiantProjet: string) => {
-  const tâcheEntity = await findProjection<Lauréat.Tâche.TâcheEntity>(
-    `tâche|${typeTâche}#${identifiantProjet}`,
-  );
-
-  const tâcheDefaultEntity: Lauréat.Tâche.TâcheEntity = {
-    identifiantProjet,
-    typeTâche: Lauréat.Tâche.TypeTâche.inconnue.type,
-    miseÀJourLe: DateTime.now().formatter(),
-    type: 'tâche',
-  };
-
-  return Option.match(tâcheEntity)
-    .some((value) => value)
-    .none(() => tâcheDefaultEntity);
 };
