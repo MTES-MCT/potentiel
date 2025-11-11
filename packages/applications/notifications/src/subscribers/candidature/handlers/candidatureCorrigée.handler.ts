8966706dfbabb68e992752993f54a8639f7cc33e
Hubert MONCENIS <hubmoncenis@gmail.com>
2025-11-11 10:23:05 +0100

    🎨 Notifier les porteurs actifs ayant accès au projet en cas de correction de candidature

--- packages/applications/notifications/src/subscribers/candidature/handlers/candidatureCorrigée.handler.ts
+++ packages/applications/notifications/src/subscribers/candidature/handlers/candidatureCorrigée.handler.ts
@@ -1,5 +1,9 @@
-import { Candidature } from '@potentiel-domain/projet';
+import { mediator } from 'mediateur';
+
+import { Accès, Candidature } from '@potentiel-domain/projet';
 import { Routes } from '@potentiel-applications/routes';
+import { ListerUtilisateursQuery, Role } from '@potentiel-domain/utilisateur';
+import { Option } from '@potentiel-libraries/monads';
 
 import { getBaseUrl, NotificationHandlerProps } from '../../../_helpers';
 import { candidatureNotificationTemplateId } from '../constant';
@@ -12,15 +16,40 @@ export const handleCandidatureCorrigée = async ({
     return;
   }
 
+  const utilisateursAvecAccès = await mediator.send<Accès.ConsulterAccèsQuery>({
+    type: 'Projet.Accès.Query.ConsulterAccès',
+    data: {
+      identifiantProjet: event.payload.identifiantProjet,
+    },
+  });
+
+  if (Option.isNone(utilisateursAvecAccès)) {
+    return;
+  }
+
+  const identifiantsUtilisateur = utilisateursAvecAccès.utilisateursAyantAccès.map(
+    (utilisateur) => utilisateur.email,
+  );
+
+  const porteurActifAyantAccès = await mediator.send<ListerUtilisateursQuery>({
+    type: 'Utilisateur.Query.ListerUtilisateurs',
+    data: {
+      identifiantsUtilisateur,
+      actif: true,
+      roles: [Role.porteur.nom],
+    },
+  });
+
+  if (porteurActifAyantAccès.items.length === 0) {
+    return;
+  }
+
   await sendEmail({
     templateId: candidatureNotificationTemplateId.attestationRegénéréePorteur,
     messageSubject: `Potentiel - Une nouvelle attestation est disponible pour le projet ${event.payload.nomProjet}`,
-    recipients: [
-      {
-        email: event.payload.emailContact,
-        fullName: event.payload.nomReprésentantLégal,
-      },
-    ],
+    recipients: porteurActifAyantAccès.items.map((p) => ({
+      email: p.email,
+    })),
     variables: {
       nom_projet: event.payload.nomProjet,
       raison: 'Votre candidature a été modifiée',
