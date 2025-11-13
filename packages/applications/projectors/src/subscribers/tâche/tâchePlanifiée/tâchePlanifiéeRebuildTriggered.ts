dcaaf2831b452da2f31ccc3e3bf71053ea5723f1
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-13 17:28:42 +0100

    ♻️ Fogotten rebuilds

--- packages/applications/projectors/src/subscribers/tâche/tâchePlanifiée/tâchePlanifiéeRebuildTriggered.ts
+++ packages/applications/projectors/src/subscribers/tâche/tâchePlanifiée/tâchePlanifiéeRebuildTriggered.ts
@@ -1,7 +1,8 @@
 import { Lauréat } from '@potentiel-domain/projet';
 import { RebuildTriggered } from '@potentiel-infrastructure/pg-event-sourcing';
-import { removeProjection } from '@potentiel-infrastructure/pg-projection-write';
+
+import { clearProjection } from '../../../helpers';
 
 export const tâchePlanifiéeRebuildTriggered = async ({ payload: { id } }: RebuildTriggered) => {
-  await removeProjection<Lauréat.TâchePlanifiée.TâchePlanifiéeEntity>(`tâche-planifiée|${id}`);
+  await clearProjection<Lauréat.TâchePlanifiée.TâchePlanifiéeEntity>('tâche-planifiée', id);
 };
