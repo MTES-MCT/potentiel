7b00cef0655efc503f85c4b3fc01778872e0a917
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-13 17:28:42 +0100

    ♻️ Fogotten rebuilds

--- packages/applications/projectors/src/subscribers/lauréat/producteur/producteurRebuildTrigerred.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/producteur/producteurRebuildTrigerred.projector.ts
@@ -1,22 +1,17 @@
 import { RebuildTriggered } from '@potentiel-infrastructure/pg-event-sourcing';
-import { removeProjection } from '@potentiel-infrastructure/pg-projection-write';
+import { removeProjectionWhere } from '@potentiel-infrastructure/pg-projection-write';
 import { Lauréat } from '@potentiel-domain/projet';
 import { Where } from '@potentiel-domain/entity';
-import { listProjection } from '@potentiel-infrastructure/pg-projection-read';
+
+import { clearProjection } from '../../../helpers';
 
 export const producteurRebuildTriggeredProjector = async ({
   payload: { id },
 }: RebuildTriggered) => {
-  await removeProjection<Lauréat.Producteur.ProducteurEntity>(`producteur|${id}`);
+  await clearProjection<Lauréat.Producteur.ProducteurEntity>(`producteur`, id);
 
-  const demandesChangementProducteur =
-    await listProjection<Lauréat.Producteur.ChangementProducteurEntity>(`changement-producteur`, {
-      where: { identifiantProjet: Where.equal(id) },
-    });
-
-  for (const changement of demandesChangementProducteur.items) {
-    await removeProjection<Lauréat.Producteur.ChangementProducteurEntity>(
-      `changement-producteur|${id}#${changement.changement.enregistréLe}`,
-    );
-  }
+  await removeProjectionWhere<Lauréat.Producteur.ChangementProducteurEntity>(
+    `changement-producteur`,
+    { identifiantProjet: Where.equal(id) },
+  );
 };
