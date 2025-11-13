c7b2b54bcfeef6325b9b3e6af691dd15a26f6338
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-13 17:48:13 +0100

    ♻️ Optimisation appel-offre

--- packages/applications/projectors/src/subscribers/candidature/détailsFournisseursCandidatureImportés.projector.ts
+++ packages/applications/projectors/src/subscribers/candidature/détailsFournisseursCandidatureImportés.projector.ts
@@ -1,27 +1,12 @@
 import { Candidature } from '@potentiel-domain/projet';
-import { findProjection } from '@potentiel-infrastructure/pg-projection-read';
-import { upsertProjection } from '@potentiel-infrastructure/pg-projection-write';
-import { getLogger } from '@potentiel-libraries/monitoring';
-import { Option } from '@potentiel-libraries/monads';
+import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const détailsFournisseursCandidatureImportésProjector = async ({
   payload,
 }: Candidature.DétailsFournisseursCandidatureImportésEvent) => {
-  const candidature = await findProjection<Candidature.CandidatureEntity>(
-    `candidature|${payload.identifiantProjet}`,
-  );
-
-  if (Option.isNone(candidature)) {
-    getLogger().error(`Candidature non trouvée`, {
-      identifiantProjet: payload.identifiantProjet,
-      fonction: 'détailsFournisseursCandidatureImportésProjector',
-    });
-    return;
-  }
-  await upsertProjection<Candidature.CandidatureEntity>(
+  await updateOneProjection<Candidature.CandidatureEntity>(
     `candidature|${payload.identifiantProjet}`,
     {
-      ...candidature,
       fournisseurs: payload.fournisseurs,
     },
   );
