6981db687ebdab3ad1d92355a8cc9f572f666341
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-11-19 15:40:30 +0100

    🎨 Alignement bouton copier coller

--- packages/applications/ssr/src/components/molecules/CopyButton.tsx
+++ packages/applications/ssr/src/components/molecules/CopyButton.tsx
@@ -2,13 +2,10 @@
 
 import Button, { ButtonProps } from '@codegouvfr/react-dsfr/Button';
 import { ReactNode, useState } from 'react';
-import clsx from 'clsx';
 import Tooltip from '@codegouvfr/react-dsfr/Tooltip';
 
 export type CopyButtonProps = {
   textToCopy: string;
-  timeoutInMs?: number;
-  className?: string;
   priority?: ButtonProps['priority'];
 } & (
   | {
@@ -23,13 +20,12 @@ export type CopyButtonProps = {
 
 export const CopyButton = ({
   textToCopy,
-  timeoutInMs = 2000,
-  className,
   noChildren,
   children,
   priority = 'tertiary no outline',
 }: CopyButtonProps) => {
   const [hasCopied, setHasCopied] = useState<boolean>(false);
+  const timeoutInMs = 2000;
 
   const copyLink = () => {
     navigator.clipboard.writeText(textToCopy);
@@ -40,22 +36,20 @@ export const CopyButton = ({
   };
 
   return (
-    <Tooltip
-      kind="hover"
-      title="Copier"
-      className={clsx(`print:hidden flex flex-row gap-3 items-center`, className)}
-    >
+    <div className="print:hidden flex flex-row gap-1 items-center">
       {noChildren ? null : (children ?? <span className="italic">{textToCopy}</span>)}
-      <Button
-        iconId={hasCopied ? 'ri-check-fill' : 'ri-file-copy-2-line'}
-        aria-label="copier-coller"
-        priority={priority}
-        onClick={copyLink}
-        style={{ marginTop: 0 }}
-        size="small"
-      >
-        {hasCopied ? 'Copié !' : ''}
-      </Button>
-    </Tooltip>
+      <Tooltip kind="hover" title="Copier">
+        <Button
+          iconId={hasCopied ? 'ri-check-fill' : 'ri-file-copy-2-line'}
+          aria-label="copier-coller"
+          priority={priority}
+          onClick={copyLink}
+          style={{ marginTop: 0 }}
+          size="small"
+        >
+          {hasCopied ? 'Copié !' : ''}
+        </Button>
+      </Tooltip>
+    </div>
   );
 };
