23011e69bfb9d797f89268503d3b6144ce29f6ff
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-20 15:43:13 +0100

    ⬆️ Mise à jour React DSFR 1.29

---
+++ packages/applications/ssr/src/dsfr-bootstrap/index.tsx
@@ -0,0 +1,22 @@
+'use client';
+import {
+  DsfrProviderBase,
+  type DsfrProviderProps,
+  StartDsfrOnHydration,
+} from '@codegouvfr/react-dsfr/next-app-router';
+import Link from 'next/link';
+
+import { defaultColorScheme } from './defaultColorScheme';
+
+declare module '@codegouvfr/react-dsfr/next-app-router' {
+  interface RegisterLink {
+    Link: typeof Link;
+  }
+}
+
+export function DsfrProvider(props: DsfrProviderProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <DsfrProviderBase defaultColorScheme={defaultColorScheme} Link={Link} {...props} />;
+}
+
+export { StartDsfrOnHydration };
