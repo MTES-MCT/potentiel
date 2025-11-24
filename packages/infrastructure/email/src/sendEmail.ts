5770099db0d3a90130a37b239f22a66e29c6313d
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-11-24 17:19:10 +0100

    ✨ Notifier par lot + ajouter l'équipe potentiel

--- packages/infrastructure/email/src/sendEmail.ts
+++ packages/infrastructure/email/src/sendEmail.ts
@@ -1,9 +1,9 @@
 import { getLogger } from '@potentiel-libraries/monitoring';
 
-import { getMailjetClient } from './getMailjetClient';
 import { mapToSendEmailMode } from './sendEmailMode';
+import { getMailjetClient } from './getMailjetClient';
 
-type Receipt = {
+type Recipient = {
   email: string;
   fullName?: string;
 };
@@ -11,17 +11,21 @@ type Receipt = {
 type SendEmail = (email: {
   templateId: number;
   messageSubject: string;
-  recipients: Array<Receipt>;
-  cc?: Array<Receipt>;
-  bcc?: Array<Receipt>;
+  recipients: Array<Recipient>;
+  cc?: Array<Recipient>;
+  bcc?: Array<Recipient>;
   variables: Record<string, string>;
 }) => Promise<void>;
 
-const formatRecipients = (recipients: Array<Receipt>) =>
+const formatRecipients = (recipients: Array<Recipient>) =>
   recipients.map(({ email, fullName }) => ({
     Email: email,
     Name: fullName,
   }));
+
+// Mailjet has a limit of 50 recipients per email: https://dev.mailjet.com/email/guides/send-api-V3/#send-in-bulk
+// To be safe, we use a limit of 30 recipients per email.
+const MAX_RECIPIENTS = 30;
 
 export const sendEmail: SendEmail = async (sendEmailArgs) => {
   const {
@@ -31,42 +35,57 @@ export const sendEmail: SendEmail = async (sendEmailArgs) => {
     MAINTENANCE_MODE,
   } = process.env;
 
+  const logger = getLogger('sendEmail');
+
   const mode = mapToSendEmailMode(SEND_EMAIL_MODE);
 
   if (mode !== 'logging-only' && !MAINTENANCE_MODE) {
-    const { templateId, messageSubject, recipients, cc, bcc, variables } = sendEmailArgs;
+    if (!SEND_EMAILS_FROM) {
+      throw new Error('SEND_EMAILS_FROM must be set to send emails');
+    }
+    const {
+      templateId,
+      messageSubject,
+      recipients: allTo,
+      cc: allCc = [],
+      bcc: allBcc = [],
+      variables,
+    } = sendEmailArgs;
 
-    if (recipients.length === 0 && !cc?.length && !bcc?.length) {
-      getLogger().error('No recipients provided for email', sendEmailArgs);
+    if (allTo.length === 0 && allCc.length === 0 && allBcc.length === 0) {
+      logger.error('No recipients provided for email', sendEmailArgs);
       return;
     }
 
-    await getMailjetClient()
-      .post('send', {
-        version: 'v3.1',
-      })
-      .request({
-        Messages: [
-          {
-            From: {
-              Email: SEND_EMAILS_FROM,
-              Name: SEND_EMAILS_FROM_NAME,
+    while (allTo.length > 0 || allCc.length > 0 || allBcc.length > 0) {
+      const to = allTo.splice(0, MAX_RECIPIENTS);
+      const cc = allCc.splice(0, MAX_RECIPIENTS);
+      const bcc = allBcc.splice(0, MAX_RECIPIENTS);
+      await getMailjetClient()
+        .post('send', { version: 'v3.1' })
+        .request({
+          Messages: [
+            {
+              From: {
+                Email: SEND_EMAILS_FROM,
+                Name: SEND_EMAILS_FROM_NAME,
+              },
+              To: formatRecipients(to),
+              Cc: formatRecipients(cc),
+              Bcc: formatRecipients(bcc),
+              TemplateID: templateId,
+              TemplateLanguage: true,
+              Subject: messageSubject,
+              Variables: variables,
             },
-            To: formatRecipients(recipients),
-            ...(cc && { Cc: formatRecipients(cc) }),
-            ...(bcc && { Bcc: formatRecipients(bcc) }),
-            TemplateID: templateId,
-            TemplateLanguage: true,
-            Subject: messageSubject,
-            Variables: variables,
-          },
-        ],
-        SandboxMode: mode === 'sandbox' ? true : false,
-      });
+          ],
+          SandboxMode: mode === 'sandbox',
+        });
+    }
 
-    getLogger().info('Email sent', sendEmailArgs);
+    logger.info('Email sent', sendEmailArgs);
   } else {
-    getLogger().info(
+    logger.info(
       '📨 Emailing mode set to logging-only so no email was sent, but here are the args: ',
       sendEmailArgs,
     );
