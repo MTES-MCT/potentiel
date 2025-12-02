69460fba07bea1ce3cb15424a1f88b99b1355841
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-12-02 18:07:38 +0100

    🎨 Ancienne route pour historique

---
+++ packages/applications/ssr/src/app/laureats/[identifiant]/historique/HistoriqueLauréat.page.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+
+import { ImprimerPage } from '@/components/atoms/ImprimerPage';
+import { ListFilters } from '@/components/molecules/ListFilters';
+import { FiltersTagListProps, FiltersTagList } from '@/components/molecules/FiltersTagList';
+import { Timeline, TimelineItemProps } from '@/components/organisms/timeline';
+export type HistoriqueLauréatAction = 'imprimer';
+export type HistoriqueLauréatPageProps = {
+  identifiantProjet: string;
+  actions?: Array<HistoriqueLauréatAction>;
+  filters: FiltersTagListProps['filters'];
+  historique: Array<TimelineItemProps>;
+};
+export const HistoriqueLauréatPage: FC<HistoriqueLauréatPageProps> = ({
+  actions,
+  historique,
+  filters,
+}) => (
+  <>
+    <div className="flex flex-col gap-6">
+      <div className="flex print:hidden">
+        <FiltersTagList filters={filters} />
+        {actions?.includes('imprimer') && <ImprimerPage />}
+      </div>
+      <div className="flex flex-row gap-4">
+        {filters.length ? (
+          <div className="print:hidden">
+            <ListFilters filters={filters} />
+          </div>
+        ) : null}
+        {historique.length > 0 ? <Timeline items={historique} /> : <>Aucun élément à afficher</>}
+      </div>
+    </div>
+  </>
+);
