eb31d8bada49d9c8ac1d3dabd7c36675ae5752d8
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-03 17:10:45 +0100

    🥅 Error management

--- packages/applications/api/src/errors.ts
+++ packages/applications/api/src/errors.ts
@@ -1,3 +1,12 @@
+import { match, P } from 'ts-pattern';
+
+import {
+  AggregateNotFoundError,
+  DomainError,
+  InvalidOperationError,
+  OperationRejectedError,
+} from '@potentiel-domain/core';
+
 export class ApiError extends Error {
   constructor(
     message: string,
@@ -27,3 +36,25 @@ export class ForbiddenError extends ApiError {
     this.name = ForbiddenError.name;
   }
 }
+
+const mapDomainErrorToApiError = (error: DomainError): ApiError => {
+  const code = match(error)
+    .with(P.instanceOf(InvalidOperationError), () => 400)
+    .with(P.instanceOf(OperationRejectedError), () => 403)
+    .with(P.instanceOf(AggregateNotFoundError), () => 404)
+    .otherwise(() => 500);
+
+  return new ApiError(error.message, code);
+};
+
+export const handleError = (error: unknown): { statusCode: number; message: string } => {
+  if (error instanceof DomainError) {
+    return handleError(mapDomainErrorToApiError(error));
+  }
+
+  if (error instanceof ApiError) {
+    return { statusCode: error.code, message: error.message };
+  }
+
+  return { statusCode: 500, message: 'Internal Server Error' };
+};
--- packages/applications/api/src/server.ts
+++ packages/applications/api/src/server.ts
@@ -4,22 +4,21 @@ import { createPotentielApiRouter } from '@potentiel-applications/api-documentat
 import { getLogger } from '@potentiel-libraries/monitoring';
 
 import { raccordementHandlers } from './handlers/raccordement/index.js';
-import { ApiError } from './errors.js';
+import { handleError } from './errors.js';
 import { achevementHandlers } from './handlers/achevement/index.js';
 
 export const createApiServer = (basePath: string) => {
   const router = createPotentielApiRouter(raccordementHandlers, achevementHandlers, {
     basePath,
     onInternalError: ({ request, response }, error) => {
-      getLogger().error('Internal server error:', { error, url: request.url });
-      response.setHeader('Content-Type', 'text/plain;charset=utf-8');
-      if (error instanceof ApiError) {
-        response.statusCode = error.code;
-        response.end(error.message);
-        return;
+      const { statusCode, message } = handleError(error);
+      if (statusCode === 500) {
+        getLogger().error(message, { error, url: request.url });
+      } else {
+        getLogger().warn(message, { error, url: request.url });
       }
-      response.statusCode = 500;
-      response.end('Internal server error');
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ code: statusCode, error: message }));
     },
   });
   return (request: http.IncomingMessage, response: http.ServerResponse) => {
