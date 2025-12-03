5daf55092da98b990a41f3566ba811ec6ebf7bf6
Violette <27735540+VioMrqs@users.noreply.github.com>
2025-12-03 14:01:49 +0100

    ♻️ Clean old feature flags (#3759)

--- packages/domain/appel-offre/src/lister/listerAppelOffre.query.ts
+++ packages/domain/appel-offre/src/lister/listerAppelOffre.query.ts
@@ -24,15 +24,9 @@ export type ListerAppelOffreDependencies = {
 
 export const registerListerAppelOffreQuery = ({ list }: ListerAppelOffreDependencies) => {
   const handler: MessageHandler<ListerAppelOffreQuery> = async ({ cycle }) => {
-    const aosActif =
-      process.env.FEATURES?.split(',')
-        .map((s) => s.trim())
-        .includes('aos') ?? false;
-
     const result = await list<AppelOffreEntity>('appel-offre', {
       where: {
         cycleAppelOffre: Where.equal(cycle),
-        id: aosActif ? undefined : Where.notEqual('PPE2 - Petit PV Bâtiment'),
       },
       orderBy: {
         id: 'ascending',
