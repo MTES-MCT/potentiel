dff3440ef1a436792d5963160ac166dfd3221c0e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-03 17:31:25 +0100

    🥅 Error handling

--- packages/applications/api/src/server.ts
+++ packages/applications/api/src/server.ts
@@ -1,25 +1,36 @@
 import http from 'node:http';
 
-import { createPotentielApiRouter } from '@potentiel-applications/api-documentation';
+import { createPotentielApiRouter, HttpContext } from '@potentiel-applications/api-documentation';
 import { getLogger } from '@potentiel-libraries/monitoring';
 
 import { raccordementHandlers } from './handlers/raccordement/index.js';
 import { handleError } from './errors.js';
 import { achevementHandlers } from './handlers/achevement/index.js';
 
+export const writeErrorResponse = (
+  { response, request }: HttpContext,
+  statusCode: number,
+  error: string,
+  body?: Record<string, unknown>,
+) => {
+  if (statusCode === 500) {
+    getLogger().error(error, { error, url: request.url, body });
+  } else {
+    getLogger().warn(error, { error, url: request.url, body });
+  }
+  response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify({ code: statusCode, error, ...body }));
+};
+
 export const createApiServer = (basePath: string) => {
   const router = createPotentielApiRouter(raccordementHandlers, achevementHandlers, {
     basePath,
-    onInternalError: ({ request, response }, error) => {
+    onInternalError: (ctx, error) => {
       const { statusCode, message } = handleError(error);
-      if (statusCode === 500) {
-        getLogger().error(message, { error, url: request.url });
-      } else {
-        getLogger().warn(message, { error, url: request.url });
-      }
-      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify({ code: statusCode, error: message }));
+      writeErrorResponse(ctx, statusCode, message);
     },
+    onInvalidRequest: (ctx, errors) => writeErrorResponse(ctx, 400, 'Bad Request', { errors }),
+    onRequestNotFound: (ctx) => writeErrorResponse(ctx, 404, 'Not Found'),
   });
   return (request: http.IncomingMessage, response: http.ServerResponse) => {
     // Hack because `basePath` is not used by the router to strip the path
