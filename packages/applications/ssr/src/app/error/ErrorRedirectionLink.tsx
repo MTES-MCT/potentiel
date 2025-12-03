f2f984af83c4bb4ea7597ba24987615570297633
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-03 10:06:30 +0100

    🐛 Fix errorRedirectionLink page not found

--- packages/applications/ssr/src/app/error/ErrorRedirectionLink.tsx
+++ packages/applications/ssr/src/app/error/ErrorRedirectionLink.tsx
@@ -2,18 +2,17 @@
 
 import { fr } from '@codegouvfr/react-dsfr';
 import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 import { Routes } from '@potentiel-applications/routes';
 
 import { decodeParameter } from '@/utils/decodeParameter';
 
 export const ErrorRedirectionLink = () => {
-  const { identifiant } = useParams<{ identifiant?: string }>();
   const pathname = usePathname();
 
-  const identifiantProjet =
-    pathname.startsWith('/laureats/') || pathname.startsWith('/elimines/') ? identifiant : null;
+  const identifiantProjet = pathname.match(/^\/(laureats|elimines)\/(?<identifiant>[^/]+)\/.*/)
+    ?.groups?.identifiant;
 
   return (
     <ul className={fr.cx('fr-btns-group', 'fr-btns-group--inline-md')}>
