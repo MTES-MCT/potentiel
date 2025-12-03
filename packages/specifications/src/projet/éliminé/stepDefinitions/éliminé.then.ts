67f3bdf3895784e8a4753fb3bea09c921a507c89
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-03 11:29:42 +0100

    ✅ Ajout specs

--- packages/specifications/src/projet/éliminé/stepDefinitions/éliminé.then.ts
+++ packages/specifications/src/projet/éliminé/stepDefinitions/éliminé.then.ts
@@ -5,6 +5,7 @@ import waitForExpect from 'wait-for-expect';
 
 import { Éliminé } from '@potentiel-domain/projet';
 import { mapToPlainObject } from '@potentiel-domain/core';
+import { Option } from '@potentiel-libraries/monads';
 
 import { PotentielWorld } from '../../../potentiel.world';
 
@@ -21,5 +22,17 @@ Alors('le projet éliminé devrait être consultable', async function (this: Pot
     const expected = mapToPlainObject(this.éliminéWorld.mapToExpected());
 
     expect(actual).to.deep.eq(expected);
+  });
+});
+
+Alors('le projet éliminé ne devrait plus être consultable', async function (this: PotentielWorld) {
+  await waitForExpect(async () => {
+    const éliminé = await mediator.send<Éliminé.ConsulterÉliminéQuery>({
+      type: 'Éliminé.Query.ConsulterÉliminé',
+      data: {
+        identifiantProjet: this.éliminéWorld.identifiantProjet.formatter(),
+      },
+    });
+    expect(Option.isNone(éliminé), 'Le projet éliminé ne devrait plus être consultable').to.be.true;
   });
 });
