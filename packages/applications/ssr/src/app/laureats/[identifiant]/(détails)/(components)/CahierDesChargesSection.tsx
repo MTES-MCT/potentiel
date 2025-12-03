52f3835fba998445877b64efe949f1b77d8faf18
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2025-12-03 15:35:09 +0100

    🎨 Encart cahier des charges

---
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(components)/CahierDesChargesSection.tsx
@@ -0,0 +1,38 @@
+import Notice from '@codegouvfr/react-dsfr/Notice';
+import Link from 'next/link';
+
+import { CahierDesChargesData } from '../_helpers/getCahierDesChargesData';
+
+import { Section } from './Section';
+
+export type CahierDesChargesSectionProps = CahierDesChargesData;
+
+export const CahierDesChargesSection = ({ value, action }: CahierDesChargesSectionProps) =>
+  value === 'Champs non renseigné' ? null : (
+    <Section title="Cahier des charges">
+      <div>
+        Instruction selon le cahier des charges{' '}
+        {value.estInitial
+          ? 'initial (en vigueur à la candidature)'
+          : `${
+              value.estAlternatif ? 'alternatif' : ''
+            } modifié rétroactivement et publié le ${value.dateParution}}`}{' '}
+        <Link target="_blank" className="w-fit" href={value.cahierDesChargesURL}>
+          voir le cahier des charges
+        </Link>
+      </div>
+      {value.doitChoisirUnCahierDesChargesModificatif && (
+        <Notice
+          description="Votre cahier des charges actuel ne vous permet pas d'accéder aux fonctionnalités
+       dématérialisées d'information au Préfet et de modification de votre projet (abandon...), vous devez d'abord choisir un cahier des charges"
+          title="Modification de votre projet"
+          severity="warning"
+        />
+      )}
+      {action && (
+        <Link href={action.url} className="w-fit">
+          {action.label}
+        </Link>
+      )}
+    </Section>
+  );
--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(components)/EtapesProjetSection.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(components)/EtapesProjetSection.tsx
@@ -175,7 +175,7 @@ const PastIcon = () => (
   <div className="flex flex-col print:min-w-[90px]" title="étape validée">
     <div className="hidden print:block text-xs mb-2 whitespace-nowrap">étape validée</div>
     <span className="relative z-2 w-8 h-8 flex items-center justify-center bg-green-700 print:bg-transparent print:border-solid print:border-2 print:border-green-700 rounded-full group-hover:bg-green-900">
-      <Success color="green-emeraude" />
+      <Success color="green-emeraude" fontSize="medium" />
     </span>
   </div>
 );
