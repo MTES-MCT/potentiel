d5b343133e99a805e8a58ccb37456ff4d07d2477
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-04 10:26:02 +0100

    🐛 Fix openapi usage

--- packages/applications/api-documentation/src/index.ts
+++ packages/applications/api-documentation/src/index.ts
@@ -1,15 +1,6 @@
-import { readFile } from 'fs/promises';
-import { join } from 'path';
-
 import { openApiDocument } from './generated/http/openapi3.js';
 
-export const getOpenApiSpecs = async () => {
-  const openApiSpecs = await readFile(join(__dirname, '@typespec/openapi3/openapi.json'), {
-    encoding: 'utf-8',
-  });
-  return JSON.parse(openApiSpecs);
-};
-
+export const getOpenApiSpecs = () => openApiDocument;
 export { createPotentielApiRouter, PotentielApiRouter } from './generated/http/router.js';
 export { HttpContext, Policy } from './generated/helpers/router.js';
 export * from './generated/models/all/index.js';
--- packages/applications/ssr/src/app/api/openapi/route.ts
+++ packages/applications/ssr/src/app/api/openapi/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { getOpenApiSpecs } from '@potentiel-applications/api-documentation';
 
 export const GET = async () => {
-  return NextResponse.json(await getOpenApiSpecs(), {
+  return NextResponse.json(getOpenApiSpecs(), {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
