c302c5cd2ed2906b5ebf9c88bc5b384eb07a5a54
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-08 17:13:30 +0100

    ✅ Tests pagination

--- packages/applications/api/src/helpers/pagination.ts
+++ packages/applications/api/src/helpers/pagination.ts
@@ -1,13 +1,15 @@
 import { RangeOptions } from '@potentiel-domain/entity';
 
-export const defaultItemsPerPage = 50;
+export const defaultNbItems = 50;
 
-export const mapToRangeOptions = (
-  after: number | undefined,
-  itemsPerPage = defaultItemsPerPage,
-): RangeOptions => {
+export const mapToRangeOptions = (after: number | undefined): RangeOptions => {
+  if (!!after && after < 0) {
+    throw new Error(`Invalid "after" value`);
+  }
+
+  const startPosition = after ? after + 1 : 0;
   return {
-    startPosition: after ? after + 1 : 0,
-    endPosition: (after || 0) + itemsPerPage - 1,
+    startPosition,
+    endPosition: startPosition + defaultNbItems - 1,
   };
 };
