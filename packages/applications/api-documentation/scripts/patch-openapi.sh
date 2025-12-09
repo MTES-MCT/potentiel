474dd1572c587fb772c50175c9857555b0f1840b
Hubert Moncenis <hubmoncenis@gmail.com>
2025-12-09 11:39:20 +0100

    🐛 Fix script patch-openapi w/ sed command for macos (#3772)
    * 🐛 Fix script patch-openapi w/ sed command for macos
    
    * 💚 Fix build
    
    ---------
    
    Co-authored-by: Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>

--- packages/applications/api-documentation/scripts/patch-openapi.sh
+++ packages/applications/api-documentation/scripts/patch-openapi.sh
@@ -2,4 +2,4 @@
 
 # This script patches the generated code to add "as const", for better type inference
 target_file="src/generated/http/openapi3.ts"
-sed -i '$ s/};$/} as const;/' "$target_file"
+sed '$ s/};$/} as const;/' "$target_file" > "$target_file.tmp" && mv "$target_file.tmp" "$target_file"
