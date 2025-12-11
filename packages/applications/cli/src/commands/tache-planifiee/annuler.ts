12c5074dd6e2b60872ccb8b5c395888a675a67e5
Hubert Moncenis <hubmoncenis@gmail.com>
2025-12-11 17:48:24 +0100

    ♻️ Retravailler la command annuler tache planifiée pour être sur un modèle comme tache achever (#3781)

--- packages/applications/cli/src/commands/tache-planifiee/annuler.ts
+++ packages/applications/cli/src/commands/tache-planifiee/annuler.ts
@@ -1,60 +1,27 @@
-import { mediator } from 'mediateur';
-import { Args, Command, Flags } from '@oclif/core';
+import { Command, Flags } from '@oclif/core';
 
-import {
-  countProjection,
-  findProjection,
-  listHistoryProjection,
-  listProjection,
-} from '@potentiel-infrastructure/pg-projection-read';
-import { Lauréat, registerProjetQueries, registerProjetUseCases } from '@potentiel-domain/projet';
-import { DocumentAdapter, ProjetAdapter } from '@potentiel-infrastructure/domain-adapters';
+import { IdentifiantProjet } from '@potentiel-domain/projet';
+import { ProjetAdapter } from '@potentiel-infrastructure/domain-adapters';
 
 export class Annuler extends Command {
   static flags = {
+    projet: Flags.string({ char: 'p', description: 'identifiant du projet', required: true }),
     type: Flags.string({
-      desc: 'Type de la Tâche Planifiée à annuler',
+      char: 't',
+      description: 'type de tâche planifiée à annuler',
       required: true,
     }),
   };
-  static args = {
-    identifiantProjet: Args.string({
-      description: 'Identifiant du projet',
-      required: true,
-    }),
-  };
-
-  async init() {
-    registerProjetQueries({
-      list: listProjection,
-      find: findProjection,
-      count: countProjection,
-      listHistory: listHistoryProjection,
-      consulterABénéficiéDuDélaiCDC2022: () => {
-        throw new Error('notImplemented');
-      },
-      getScopeProjetUtilisateur: () => {
-        throw new Error('notImplemented');
-      },
-      récupérerProjetsEligiblesPreuveRecanditure: () => {
-        throw new Error('notImplemented');
-      },
-    });
-    registerProjetUseCases({
-      getProjetAggregateRoot: ProjetAdapter.getProjetAggregateRootAdapter,
-      enregistrerDocumentSubstitut: DocumentAdapter.enregistrerDocumentSubstitutAdapter,
-    });
-  }
 
   async run() {
-    const { flags, args } = await this.parse(Annuler);
+    const { flags } = await this.parse(Annuler);
 
-    await mediator.send<Lauréat.TâchePlanifiée.AnnulerTâchePlanifiéeUseCase>({
-      type: 'System.TâchePlanifiée.UseCase.AnnulerTâchePlanifiée',
-      data: {
-        identifiantProjetValue: args.identifiantProjet,
-        typeTâchePlanifiéeValue: flags.type,
-      },
-    });
+    const identifiantProjet = IdentifiantProjet.convertirEnValueType(flags.projet);
+
+    const projet = await ProjetAdapter.getProjetAggregateRootAdapter(identifiantProjet);
+
+    const tâchePlanifiée = await projet.lauréat.loadTâchePlanifiée(flags.type);
+
+    await tâchePlanifiée.annuler();
   }
 }
