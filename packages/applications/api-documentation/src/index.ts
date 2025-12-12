74f1aaa5902ce57e4edf402df37e2937b785ce0c
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-12 16:07:04 +0100

    ✨ API playground URL (#3787)

--- packages/applications/api-documentation/src/index.ts
+++ packages/applications/api-documentation/src/index.ts
@@ -1,6 +1,18 @@
 import { openApiDocument } from './generated/http/openapi3.js';
 
-export const getOpenApiSpecs = () => openApiDocument;
+export const getOpenApiSpecs = (baseUrl: string | undefined) => {
+  const server = openApiDocument.servers[0];
+
+  return {
+    ...openApiDocument,
+    servers: [
+      {
+        ...server,
+        url: baseUrl ? new URL(new URL(server.url).pathname, baseUrl).toString() : server.url,
+      },
+    ],
+  };
+};
 export { createPotentielApiRouter, PotentielApiRouter } from './generated/http/router.js';
 export { HttpContext, Policy } from './generated/helpers/router.js';
 export * from './generated/models/all/index.js';
--- packages/applications/ssr/src/app/api/openapi/route.ts
+++ packages/applications/ssr/src/app/api/openapi/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { getOpenApiSpecs } from '@potentiel-applications/api-documentation';
 
 export const GET = async () => {
-  return NextResponse.json(getOpenApiSpecs(), {
+  return NextResponse.json(getOpenApiSpecs(process.env.BASE_URL), {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
