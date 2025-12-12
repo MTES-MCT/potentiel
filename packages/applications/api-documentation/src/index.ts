fd326ced9f724007671c1ab2426d864bdcf3e5ae
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2025-12-12 14:35:23 +0100

    ✨ API playground URL

--- packages/applications/api-documentation/src/index.ts
+++ packages/applications/api-documentation/src/index.ts
@@ -1,6 +1,18 @@
 import { openApiDocument } from './generated/http/openapi3.js';
 
-export const getOpenApiSpecs = () => openApiDocument;
+export const getOpenApiSpecs = (baseUrl: string) => {
+  const server = openApiDocument.servers[0];
+
+  return {
+    ...openApiDocument,
+    servers: [
+      {
+        ...server,
+        url: new URL(new URL(server.url).pathname, baseUrl).toString(),
+      },
+    ],
+  };
+};
 export { createPotentielApiRouter, PotentielApiRouter } from './generated/http/router.js';
 export { HttpContext, Policy } from './generated/helpers/router.js';
 export * from './generated/models/all/index.js';
