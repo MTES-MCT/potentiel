28797b57fbb7b048edda3768b0392320456cc027
Hanaë <hanae.yamashita@lilo.org>
2025-12-22 16:01:55 +0100

    🔥 Suppression schema non utilisé

--- packages/applications/ssr/src/utils/candidature/index.ts
+++ packages/applications/ssr/src/utils/candidature/index.ts
@@ -1,5 +1,4 @@
 export * from './csv/candidatureCsv.schema';
-export * from './csv/candidature.schema';
 export * from './candidatureNotifiée.schema';
 export * from './dépôt.schema';
 export * from './instruction.schema';
