a2c5390236c3ef8bcccb335d53eabd37e256d96d
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-01-07 11:18:47 +0100

    🎨 Amélioration removeEmptyValues au lieu d'avoir un cleanDétails spécifique

--- packages/domain/projet/src/candidature/détail/_helpers/cleanDétails.ts
+++
@@ -1,9 +0,0 @@
-import { DétailCandidature, DétailCandidatureRaw } from '../détailCandidature.type';
-
-export const cleanDétails = (détails: DétailCandidatureRaw) =>
-  Object.entries(détails).reduce((détail, [key, value]) => {
-    if (key !== '' && value !== '' && value !== undefined) {
-      détail[key] = value;
-    }
-    return détail;
-  }, {} as DétailCandidature);
