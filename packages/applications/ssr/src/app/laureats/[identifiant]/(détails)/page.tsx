96f71dd74f758a8569bd066bd8a8e3a834856fb4
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-01-08 16:11:50 +0100

    remove feature flag page projet

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/page.tsx
@@ -5,17 +5,10 @@ import { IdentifiantParameter } from '@/utils/identifiantParameter';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 
 import { TableauDeBordPage } from './TableauDeBord.page';
-import { checkFeatureFlag } from './_helpers/checkFeatureFlag';
 
-type PageProps = IdentifiantParameter & {
-  searchParams?: Record<string, string>;
-};
-
-export default async function Page({ params: { identifiant }, searchParams }: PageProps) {
+export default async function Page({ params: { identifiant } }: IdentifiantParameter) {
   return PageWithErrorHandling(async () => {
     const identifiantProjet = IdentifiantProjet.convertirEnValueType(decodeParameter(identifiant));
-
-    checkFeatureFlag(identifiantProjet, searchParams);
 
     return <TableauDeBordPage identifiantProjet={identifiantProjet.formatter()} />;
   });
