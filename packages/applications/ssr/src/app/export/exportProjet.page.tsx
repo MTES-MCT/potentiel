0ddd98d75ddf11cc17a06dcc8755a5f7712e0bbd
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-01-10 09:00:32 +0100

    🎨 Création d'une route pour l'export projet raccordement

--- packages/applications/ssr/src/app/export/exportProjet.page.tsx
+++ packages/applications/ssr/src/app/export/exportProjet.page.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react';
 
+import { Routes } from '@potentiel-applications/routes';
+
 import { PageTemplate } from '@/components/templates/Page.template';
 import { Heading1 } from '@/components/atoms/headings';
-
-import { ExportProjetForm } from './exportProjet.form';
+import { LinkAction } from '@/components/atoms/LinkAction';
 
 export const ExportProjetPage: FC = () => (
   <PageTemplate banner={<Heading1>Exporter des données projets</Heading1>}>
-    <p>
-      Cette page permet d'exporter les données des projets, vous devez tout d'abord sélectionner le
-      type d'export désiré
-    </p>
-    <ExportProjetForm />
+    <div className="mb-4">
+      Cette page permet d'accèder à des liens pour exporter des données des projets
+    </div>
+    <LinkAction
+      label="Exporter les données projets liées au raccordement"
+      href={Routes.Raccordement.exporter}
+    />
   </PageTemplate>
 );
