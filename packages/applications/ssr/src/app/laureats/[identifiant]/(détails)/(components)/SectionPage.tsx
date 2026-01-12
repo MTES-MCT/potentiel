d74ac2ba977d148cc2a894a3924775af145ba762
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-01-12 12:00:14 +0100

    💄 Correction de l'affichage des flex box en impression

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(components)/SectionPage.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(components)/SectionPage.tsx
@@ -6,7 +6,7 @@ type SectionPageProps = {
 };
 
 export const SectionPage = ({ title, children }: SectionPageProps) => (
-  <div className="flex flex-col gap-4 w-full print:block">
+  <div className="flex flex-col gap-4 w-full print:block print:space-y-4">
     <Heading2 className="print:mb-2">{title}</Heading2>
     {children}
   </div>
