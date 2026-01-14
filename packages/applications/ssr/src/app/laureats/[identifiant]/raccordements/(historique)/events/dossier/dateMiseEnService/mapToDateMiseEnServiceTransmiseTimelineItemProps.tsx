6b1118b38df5dfcd462308f2d7dade938a9d18ce
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-01-14 17:57:21 +0100

    amelioration historique date mise en service

--- packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/(historique)/events/dossier/dateMiseEnService/mapToDateMiseEnServiceTransmiseTimelineItemProps.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/(historique)/events/dossier/dateMiseEnService/mapToDateMiseEnServiceTransmiseTimelineItemProps.tsx
@@ -1,6 +1,9 @@
 import { Lauréat } from '@potentiel-domain/projet';
+import { DateTime } from '@potentiel-domain/common';
 
 import { TimelineItemProps } from '@/components/organisms/timeline';
+
+import { FormattedDate } from '../../../../../../../../components/atoms/FormattedDate';
 
 export const mapToDateMiseEnServiceTransmiseTimelineItemProps = (
   event: (
@@ -13,12 +16,18 @@ export const mapToDateMiseEnServiceTransmiseTimelineItemProps = (
   const { référenceDossierRaccordement, dateMiseEnService } = event.payload;
 
   return {
-    date: dateMiseEnService,
+    date: event.createdAt as DateTime.RawType,
     title: (
       <div>
-        Date de mise en service pour le dossier de raccordement{' '}
+        La date de mise en service a été transmise pour le dossier de raccordement{' '}
         <span className="font-semibold">{référenceDossierRaccordement}</span>
       </div>
+    ),
+    content: (
+      <span>
+        Date de mise en service :{' '}
+        <FormattedDate className="font-semibold" date={dateMiseEnService} />
+      </span>
     ),
   };
 };
