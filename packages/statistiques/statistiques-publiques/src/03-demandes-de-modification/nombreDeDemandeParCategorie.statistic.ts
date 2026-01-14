538f5d942b2ab58c2de445f23fd49e781b238242
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-01-14 14:54:40 +0100

    🗃️ Vues SQL pour les statistiques (#3858)

--- packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreDeDemandeParCategorie.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/03-demandes-de-modification/nombreDeDemandeParCategorie.statistic.ts
@@ -8,71 +8,12 @@ export const computeNombreDeDemandeParCategorie = async () => {
     insert
     into 
       domain_public_statistic.camembert_statistic
-    values
-      (
-        $1,
-        'abandon',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'AbandonDemandé-V%'
-        )
-      ),
-      (
-        $1,
-        'actionnaire',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'ChangementActionnaireDemandé-V%' or type like 'ChangementActionnaireEnregistré-V%'
-        )
-      ),
-      (
-        $1,
-        'représentant légal',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'ChangementReprésentantLégalDemandé-V%'
-        )
-      ),
-      (
-        $1,
-        'recours',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'RecoursDemandé-V%'
-        )
-      ),
-      (
-        $1,
-        'puissance',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'ChangementPuissanceDemandé-V%' or type like 'ChangementPuissanceEnregistré-V%'
-        )
-      ),      
-      (
-        $1,
-        'producteur',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'ChangementProducteurEnregistré-V%'
-        )
-      ),
-      (
-        $1,
-        'fournisseur',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'ChangementFournisseurEnregistré-V%'
-        )
-      ),
-      (
-        $1,
-        'delai',
-        (
-          select count(*) from "event_store"."event_stream" 
-          where type like 'DélaiDemandé-V%'
-        )
-      )
+    select
+      $1,
+     categorie,
+     count(*)
+    from domain_views.stats_demandes
+    group by categorie;
     `,
     statisticType,
   );
