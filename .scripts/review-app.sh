456f24d82a5362ac0699210804118ba05b9a8051
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-01-15 16:43:08 +0100

    🧑‍💻 Script de déploiement d'une Review app (#3888)

---
+++ .scripts/review-app.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+# Crédits : https://github.com/betagouv/rdv-service-public/blob/production/scripts/create_review_app.rb
+
+target_app=potentiel-dev
+target_app_region=osc-fr1 
+
+# Vérifier que gh est installé
+if ! command -v gh &> /dev/null; then
+  echo "Erreur: gh (GitHub CLI) n'est pas installé."
+  exit 1
+fi
+
+# Vérifier que l'utilisateur est authentifié
+if ! gh auth status --hostname github.com --active &> /dev/null; then
+  echo "Erreur: vous n'êtes pas authentifié avec GitHub CLI. Exécutez 'gh auth login'."
+  exit 1
+fi
+
+
+pr_number=$(gh pr view --json number --jq '.number')
+
+review_app_name=$(echo "${target_app}-pr${pr_number}")
+review_app_url="https://${review_app_name}.${target_app_region}.scalingo.io/"
+
+
+if scalingo --app "$review_app_name" stats >/dev/null 2>&1; then
+  echo "La review app existe déjà: ${review_app_url}"
+  exit 0
+fi
+
+echo "Création de la review app pour la PR #${pr_number}..."
+scalingo --region "$target_app_region" --app "$target_app" integration-link-manual-review-app "$pr_number"
+
+gh pr comment -b "[Review app](${review_app_url})" >/dev/null 2>&1
+
+echo "Le déploiement de la review app sera déclenchée dès que la CI sera passée avec succès."
+echo "Logs du déploiement: https://dashboard.scalingo.com/apps/${target_app_region}/${review_app_name}/deploy/list"
+echo ""
+echo "Une fois le déploiement terminé (et pas avant, sinon la DB ne sera pas restaurée), il est nécessaire de scale les containers web et worker à 1 pour que l'application fonctionne correctement :"
+echo " scalingo --region ${target_app_region} --app ${review_app_name} scale web:1 worker:1"
+echo ""
+echo "L'application sera disponible à l'adresse ${review_app_url}."
