934ac1085fbc63a8d2885a1d457dab60538b9e2e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-01-19 14:54:19 +0100

    🎨 Simplifier

--- packages/applications/notifications/src/helpers/NotificationHandlerProps.type.ts
+++ packages/applications/notifications/src/helpers/NotificationHandlerProps.type.ts
@@ -1,13 +1,8 @@
 import { DomainEvent } from '@potentiel-domain/core';
 
-import { SendEmail, SendEmailV2 } from '#sendEmail';
+import { SendEmail } from '#sendEmail';
 
 export type NotificationHandlerProps<T extends DomainEvent> = {
   sendEmail: SendEmail;
-  event: T;
-};
-
-export type NotificationHandlerPropsV2<T extends DomainEvent> = {
-  sendEmail: SendEmailV2;
   event: T;
 };
--- packages/applications/notifications/src/subscribers/lauréat/fournisseur/type.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEvent } from '@potentiel-domain/core';
-
-import { NotificationHandlerPropsV2 } from '#helpers';
-
-export type FournisseurNotificationsProps<T extends DomainEvent> = NotificationHandlerPropsV2<T> & {
-  projet: { nom: string; région: string; département: string; url: string };
-};
