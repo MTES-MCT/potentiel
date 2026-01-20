8ea75b76533a94bc147b2fcdfa6beb8fac8c899b
Hanaë <hanae.yamashita@lilo.org>
2026-01-20 09:15:21 +0100

    🎨 Réutilisation logique formattage date

--- packages/applications/ssr/src/components/atoms/FormattedDate.tsx
+++ packages/applications/ssr/src/components/atoms/FormattedDate.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 
 import { Iso8601DateTime } from '@potentiel-libraries/iso8601-datetime';
 
+import { formatDateToText } from '@/app/_helpers';
+
 export type FormattedDateProps = {
   date: Iso8601DateTime;
   className?: string;
 };
 
 export const FormattedDate: FC<FormattedDateProps> = ({ className = '', date }) => (
-  <span className={className}>{Intl.DateTimeFormat('fr').format(new Date(date))}</span>
+  <span className={className}>{formatDateToText(date)}</span>
 );
