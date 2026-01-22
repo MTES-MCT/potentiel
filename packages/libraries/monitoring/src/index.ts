ed3bd93e35567ff7efc8643773d72403ce3734bb
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-01-22 17:25:59 +0100

    🎨 Fix logger export

--- packages/libraries/monitoring/src/index.ts
+++ packages/libraries/monitoring/src/index.ts
@@ -1,6 +1,3 @@
 export { Level } from './level';
 export { initLogger, resetLogger, Logger } from './logger';
 export { getLogger } from './getLogger';
-
-// TODO remove this export when specifications is an ESM module
-export * from './winston';
