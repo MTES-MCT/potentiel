2d6302497acc0664f50110b0c039081403bb9c5f
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-02-03 17:38:55 +0100

    details

--- packages/applications/ssr/src/components/organisms/document/DocumentsList.tsx
+++ packages/applications/ssr/src/components/organisms/document/DocumentsList.tsx
@@ -13,7 +13,7 @@ export const DocumentsList = ({ documents }: DocumentListItemProps) => {
         </div>
       ) : (
         <div>
-          <div className="flex flex-col gap-4 mt-4">
+          <div className="flex flex-col gap-4 mt-4 list-none">
             {documents.map(({ type, url, date, format, demande }) => (
               <li key={type + date}>
                 <Tile className="flex flex-col md:flex-row md:justify-between">
