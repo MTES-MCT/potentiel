ee8f7288a8ed6c1a6de36cbd3b25ec1c057663e7
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-05 14:28:12 +0100

    🎨 Fix all scripts

--- packages/applications/ssr/src/app/api/auth/[...nextauth]/route.ts
+++ packages/applications/ssr/src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,11 @@ import NextAuth from 'next-auth';
 
 import { authOptions } from '@potentiel-applications/request-context';
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  ...authOptions,
+  // hack à supprimer quand l'adapter auth-pg-adapter sera retiré
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  adapter: authOptions.adapter as any,
+});
 
 export { handler as GET, handler as POST };
