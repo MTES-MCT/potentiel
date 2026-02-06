14e92d909b1824836abd678dd3325778195b9c4c
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-06 17:23:28 +0100

    🎨 Import

--- packages/applications/document-builder/src/assets.ts
+++ packages/applications/document-builder/src/assets.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 
-export const assetsFolderPath = path.resolve(__dirname, 'assets');
+export const assetsFolderPath = path.resolve(import.meta.dirname, 'assets');
 export const docxFolderPath = path.resolve(assetsFolderPath, 'docx');
 export const fontsFolderPath = path.resolve(assetsFolderPath, 'fonts');
 export const imagesFolderPath = path.resolve(assetsFolderPath, 'images');
