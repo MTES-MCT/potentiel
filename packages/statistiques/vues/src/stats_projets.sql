83746e460d45b0e166e7cc125a402ab91278c5a5
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-09 17:52:57 +0100

    ✨ Évolution vue statistiques projets

--- packages/statistiques/vues/src/stats_projets.sql
+++ packages/statistiques/vues/src/stats_projets.sql
@@ -9,7 +9,7 @@ WITH projets_en_service as (
 )
 SELECT proj.value->>'identifiantProjet' AS id,
   SPLIT_PART(proj.value->>'identifiantProjet', '#', 1) as "appelOffre",
-  SPLIT_PART(proj.value->>'identifiantProjet', '#', 2) as "periode",
+  SPLIT_PART(proj.value->>'identifiantProjet', '#', 2)::float as "periode",
   SPLIT_PART(proj.value->>'identifiantProjet', '#', 3) as "famille",
   SPLIT_PART(proj.value->>'identifiantProjet', '#', 4) as "numeroCRE",
   COALESCE(
@@ -17,6 +17,7 @@ SELECT proj.value->>'identifiantProjet' AS id,
     cand.value->>'nomProjet'
   ) AS "nomProjet",
   cand.value->>'nomCandidat' AS "nomCandidat",
+  cand.value->>'technologieCalculée' AS "technologie",
   COALESCE(
     proj.value->>'localité.région',
     cand.value->>'localité.région'
@@ -25,20 +26,27 @@ SELECT proj.value->>'identifiantProjet' AS id,
     proj.value->>'localité.département',
     cand.value->>'localité.département'
   ) AS departement,
-  cand.value->>'prixReference' as prix,
+  (cand.value->'prixReference')::float as prix,
   COALESCE(
     puiss.value->>'puissance',
-    cand.value->>'puissanceProductionAnnuelle'
-  ) as puissance,
-  cand.value->>'unitéPuissance' as "unitéPuissance",
+    cand.value->>'puissance'
+  )::float as puissance,
+  cand.value->>'unitéPuissance' as "unitePuissance",
   CAST(proj.value->>'notifiéLe' as timestamp) AS "dateNotification",
+  CAST(ach.value->>'prévisionnel.date' as timestamp) AS "dateAchevementPrevisionnel",
+  CAST(ach.value->>'réel.date' as timestamp) AS "dateAchevementReel",
   CASE
     WHEN proj.key like 'éliminé|%' THEN 'éliminé'
     WHEN aban.key is NOT NULL THEN 'abandonné'
     WHEN ach.value->>'estAchevé' = 'true' THEN 'achevé'
     ELSE 'actif'
   END AS statut,
-  projets_en_service.id is not null as "enService"
+  projets_en_service.id is not null as "enService",
+  cand.value->>'actionnariat' as "typeActionnariat",
+  COALESCE(
+    four.value->'évaluationCarboneSimplifiée',
+    cand.value->'evaluationCarboneSimplifiée'
+  )::float as "evaluationCarbone"
 FROM domain_views.projection proj
   INNER JOIN domain_views.projection cand on cand.key = format(
     'candidature|%s',
@@ -46,6 +54,10 @@ FROM domain_views.projection proj
   )
   LEFT JOIN domain_views.projection puiss on puiss.key = format(
     'puissance|%s',
+    proj.value->>'identifiantProjet'
+  )
+  LEFT JOIN domain_views.projection four on four.key = format(
+    'fournisseur|%s',
     proj.value->>'identifiantProjet'
   )
   LEFT JOIN domain_views.projection ach on ach.key = format(
