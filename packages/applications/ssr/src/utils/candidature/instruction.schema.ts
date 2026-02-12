abeea90fe2a4f2363b611311c93299446b9f5bcd
Violette <27735540+VioMrqs@users.noreply.github.com>
2026-02-12 10:50:05 +0100

    ♻️ Instruction schema test (#3987)

--- packages/applications/ssr/src/utils/candidature/instruction.schema.ts
+++ packages/applications/ssr/src/utils/candidature/instruction.schema.ts
@@ -1,9 +1,27 @@
 import { z } from 'zod';
 
+import { conditionalRequiredError } from './schemaBase';
 import { motifEliminationSchema, noteTotaleSchema, statutSchema } from './candidatureFields.schema';
 
-export const instructionSchema = z.object({
-  statut: statutSchema,
-  motifÉlimination: motifEliminationSchema,
-  noteTotale: noteTotaleSchema,
-});
+export const instructionSchema = z
+  .object({
+    statut: statutSchema,
+    motifÉlimination: motifEliminationSchema,
+    noteTotale: noteTotaleSchema,
+  })
+  .superRefine((obj, ctx) => {
+    const actualStatut = obj.statut;
+    if (actualStatut === 'éliminé' && !obj.motifÉlimination) {
+      ctx.addIssue(conditionalRequiredError('motifÉlimination', 'statut', actualStatut));
+    }
+    if (actualStatut === 'classé' && obj.motifÉlimination) {
+      ctx.addIssue({
+        code: 'custom',
+        path: ['motifÉlimination'],
+        message:
+          '"motifÉlimination" ne devrait pas être renseigné lorsque "statut" a la valeur "classé"',
+      });
+    }
+  });
+
+export type InstructionSchemaShape = z.infer<typeof instructionSchema>;
