67a9583c4ae29e698916ae8a6c9da6baea4de67d
Hanaë <hanae.yamashita@lilo.org>
2026-02-17 16:50:00 +0100

    ✨ Récupération via DN

--- packages/infrastructure/ds-api-client/src/_helpers/getters/getAutorisationDUrbanismeOuEnvironnementale.ts
+++ packages/infrastructure/ds-api-client/src/_helpers/getters/getAutorisationDUrbanismeOuEnvironnementale.ts
@@ -1,15 +1,17 @@
 import { DossierAccessor } from '../../graphql/index.js';
 
-type GetAutorisationDUrbanismeProps<TDossier extends Record<string, string>> = {
+type GetAutorisationDUrbanismeOuEnvironnementaleProps<TDossier extends Record<string, string>> = {
   accessor: DossierAccessor<TDossier>;
   nomChampDate: keyof TDossier;
   nomChampNuméro: keyof TDossier;
 };
-export const getAutorisationDUrbanisme = <TDossier extends Record<string, string>>({
+export const getAutorisationDUrbanismeOuEnvironnementale = <
+  TDossier extends Record<string, string>,
+>({
   accessor,
   nomChampDate,
   nomChampNuméro,
-}: GetAutorisationDUrbanismeProps<TDossier>) => {
+}: GetAutorisationDUrbanismeOuEnvironnementaleProps<TDossier>) => {
   const numéro = accessor.getStringValue(nomChampNuméro);
   const date = accessor.getDateValue(nomChampDate);
 
