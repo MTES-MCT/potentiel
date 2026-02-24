fe339f7773cfd9e0004198d38c0f5cedc1433168
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-24 14:47:14 +0100

    🎨 Fix mw

--- packages/applications/ssr/src/middleware.ts
+++ packages/applications/ssr/src/middleware.ts
@@ -5,7 +5,6 @@ import { withCSRF } from './middlewares/withCSRF';
 export default chain([withAuth, withCSRF]);
 
 export const config = {
-  runtime: 'nodejs',
   // do not run middleware for paths matching one of following
   matcher: [
     '/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|illustrations|error|statistiques-publiques|$).*)',
--- packages/applications/ssr/src/middlewares/withAuth.ts
+++ packages/applications/ssr/src/middlewares/withAuth.ts
@@ -1,5 +1,4 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import { redirect } from 'next/navigation';
 import { getCookieCache } from 'better-auth/cookies';
 
 import { CustomMiddleware } from './middleware';
@@ -8,7 +7,9 @@ export function withAuth(nextMiddleware: CustomMiddleware) {
   return async (request: NextRequest, event: NextFetchEvent) => {
     const session = await getCookieCache(request, { isSecure: true, strategy: 'jwe' });
     if (!session) {
-      redirect('/auth/signIn?callbackUrl=' + encodeURIComponent(request.url));
+      return NextResponse.redirect(
+        new URL('/auth/signIn?callbackUrl=' + encodeURIComponent(request.url), request.url),
+      );
     }
 
     return nextMiddleware(request, event, NextResponse.next());
