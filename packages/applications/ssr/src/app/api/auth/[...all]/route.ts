8f5f446d446cbc74022a68949655ab2da4417f0b
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-24 15:49:30 +0100

    Revert "fixup! 🎨 Fix build"
    This reverts commit 90d1eea2b4ca3999b89039e5b59982cc2439fdc0.

--- packages/applications/ssr/src/app/api/auth/[...all]/route.ts
+++ packages/applications/ssr/src/app/api/auth/[...all]/route.ts
@@ -2,6 +2,4 @@ import { toNextJsHandler } from 'better-auth/next-js';
 
 import { auth } from '@/auth';
 
-export const dynamic = 'force-dynamic';
-
 export const { POST, GET } = toNextJsHandler(auth);
