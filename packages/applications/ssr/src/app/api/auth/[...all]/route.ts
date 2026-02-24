6f6026a9ab485a4e0146a3790919facd7b049876
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-24 15:48:19 +0100

    fixup! 🎨 Fix build

--- packages/applications/ssr/src/app/api/auth/[...all]/route.ts
+++ packages/applications/ssr/src/app/api/auth/[...all]/route.ts
@@ -2,4 +2,6 @@ import { toNextJsHandler } from 'better-auth/next-js';
 
 import { auth } from '@/auth';
 
+export const dynamic = 'force-dynamic';
+
 export const { POST, GET } = toNextJsHandler(auth);
