3828d0efb62c408616cf7b896e816f7ec604976a
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-02-24 15:49:40 +0100

    Revert "🎨 Fix build"
    This reverts commit 703997fb9aa69da0dcf95dbaaf6d67cb071d4411.

--- packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
+++ packages/applications/ssr/src/app/auth/verifyRequest/page.tsx
@@ -9,8 +9,6 @@ import { PageTemplate } from '@/components/templates/Page.template';
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 import { auth } from '@/auth';
 
-export const dynamic = 'force-dynamic';
-
 export default function VerifyRequest() {
   return PageWithErrorHandling(async () => {
     const utilisateur = getContext()?.utilisateur;
