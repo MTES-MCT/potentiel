299e542f05d60a7c0dcf6caf107fafa5c7c88e94
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-02-25 16:28:39 +0100

    migration achèvement

--- packages/applications/notifications/src/subscribers/lauréat/achèvement/handlers/attestationConformitéTransmise.handler.ts
+++ packages/applications/notifications/src/subscribers/lauréat/achèvement/handlers/attestationConformitéTransmise.handler.ts
@@ -1,42 +1,33 @@
-import { IdentifiantProjet, Lauréat } from '@potentiel-domain/projet';
+import { Lauréat } from '@potentiel-domain/projet';
 
-import { listerDrealsRecipients, listerPorteursRecipients } from '#helpers';
-
-import type { AchèvementNotificationsProps } from '../type.js';
-import { achèvementNotificationTemplateId } from '../constant.js';
+import { getLauréat, listerDrealsRecipients, listerPorteursRecipients } from '#helpers';
+import { sendEmail } from '#sendEmail';
 
 export const handleAttestationConformitéTransmise = async ({
-  sendEmail,
-  event,
-  projet,
-}: AchèvementNotificationsProps<Lauréat.Achèvement.AttestationConformitéTransmiseEvent>) => {
-  const identifiantProjet = IdentifiantProjet.convertirEnValueType(event.payload.identifiantProjet);
-
-  const porteurs = await listerPorteursRecipients(identifiantProjet);
-
-  await sendEmail({
-    templateId: achèvementNotificationTemplateId.transmettreAttestationConformité.porteur,
-    messageSubject: `Potentiel - Mise à jour de la date d'achèvement du projet ${projet.nom} dans le département ${projet.département}`,
-    recipients: porteurs,
-    variables: {
-      type: 'accord',
-      nom_projet: projet.nom,
-      departement_projet: projet.département,
-      url: projet.url,
-    },
-  });
+  payload: { identifiantProjet },
+}: Lauréat.Achèvement.AttestationConformitéTransmiseEvent) => {
+  const projet = await getLauréat(identifiantProjet);
 
   const dreals = await listerDrealsRecipients(projet.région);
+  const porteurs = await listerPorteursRecipients(projet.identifiantProjet);
+
+  const values = {
+    nom_projet: projet.nom,
+    departement_projet: projet.département,
+    appel_offre: projet.identifiantProjet.appelOffre,
+    période: projet.identifiantProjet.période,
+    url: projet.url,
+  };
 
   await sendEmail({
-    templateId: achèvementNotificationTemplateId.transmettreAttestationConformité.dreal,
-    messageSubject: `Potentiel - Une attestation de conformité a été transmise pour le projet ${projet.nom} dans le département ${projet.département}`,
+    key: 'lauréat/achèvement/transmettre_attestation_porteur',
+    recipients: porteurs,
+    values,
+  });
+
+  await sendEmail({
+    key: 'lauréat/achèvement/transmettre_attestation_dreal',
     recipients: dreals,
-    variables: {
-      type: 'accord',
-      nom_projet: projet.nom,
-      departement_projet: projet.département,
-      url: projet.url,
-    },
+    values,
   });
 };
--- packages/applications/notifications/src/subscribers/lauréat/achèvement/type.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEvent } from '@potentiel-domain/core';
-
-import { NotificationHandlerProps } from '#helpers';
-
-export type AchèvementNotificationsProps<T extends DomainEvent> = NotificationHandlerProps<T> & {
-  projet: { nom: string; région: string; département: string; url: string };
-};
