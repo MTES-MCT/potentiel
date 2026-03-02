7f9cdf0de98e01d6d089d31b67859ce4f69a1398
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-03-02 17:53:24 +0100

    🐛 Fix dev mode

--- packages/libraries/monitoring/src/sentry/sentry.transport.ts
+++ packages/libraries/monitoring/src/sentry/sentry.transport.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // CREDITS: largely inspired by https://github.com/aandrewww/winston-transport-sentry-node
 import TransportStream, { TransportStreamOptions } from 'winston-transport';
-import Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/node';
 
 const LEVELS_MAP: Record<string, Sentry.SeverityLevel> = {
   silly: 'debug',
