caa17bfcef4407b0d5d7f2f3880b16ef90e26ce2
Hanaë <hanae.yamashita@lilo.org>
2026-03-05 12:35:26 +0100

    🎨 Ajout count et cycle AO

--- packages/statistiques/statistiques-publiques/sql/indicateursProjetsStatistic.sql
+++ packages/statistiques/statistiques-publiques/sql/indicateursProjetsStatistic.sql
@@ -1,4 +1,5 @@
 create table if not exists domain_public_statistic.indicateurs_projets (
+  cycle_appel_offres varchar not null,
   appel_offres varchar not null,
   periode decimal not null,
   famille varchar,
@@ -6,8 +7,9 @@ create table if not exists domain_public_statistic.indicateurs_projets (
   region_projet varchar not null,
   departement_projet varchar not null,
   type_actionnariat varchar,
-  date_de_notification timestamp,
-  puissance_cumulee decimal,
-  puissance_moyenne decimal,
-  ecs_moyenne decimal
+  date_de_notification timestamp not null,
+  puissance_cumulee decimal not null,
+  puissance_moyenne decimal not null,
+  ecs_moyenne decimal not null, 
+  nombre_de_projets decimal not null
 );
--- packages/statistiques/statistiques-publiques/src/01-ensemble-des-projets/indicateursProjetsAgrégés.statistic.ts
+++ packages/statistiques/statistiques-publiques/src/01-ensemble-des-projets/indicateursProjetsAgrégés.statistic.ts
@@ -6,7 +6,8 @@ export const computeIndicateursProjetsAgrégés = async () => {
     insert
     into 
       domain_public_statistic.indicateurs_projets
-    select 
+     select 
+      "cycleAppelOffre" as "cycle_appel_offres",
       "appelOffre" as "appel_offres", 
       "periode", 
       "famille", 
@@ -17,13 +18,14 @@ export const computeIndicateursProjetsAgrégés = async () => {
       "dateNotification" as "date_de_notification", 
       SUM("puissance") as "puissance_cumulee",
       AVG("puissance") as "puissance_moyenne",
-      AVG("evaluationCarbone") as "ecs_moyenne"
+      AVG("evaluationCarbone") as "ecs_moyenne",
+      count(*) as "nombre_de_projets"
     from 
       domain_views.stats_projets
     group by 
-      "appelOffre", "periode", "famille", "statut", "region", "departement", "dateNotification", "typeActionnariat"
+       "cycleAppelOffre", "appelOffre", "periode", "famille", "statut", "region", "departement", "dateNotification", "typeActionnariat"
     order by 
-      "appelOffre", "periode", "famille", "statut", "region", "departement", "dateNotification", "typeActionnariat";
+       "cycleAppelOffre", "appelOffre", "periode", "famille", "statut", "region", "departement", "dateNotification", "typeActionnariat";
     `,
   );
 };
