ffd2f03386ce1d75f51597a44abfa3fd55d715b7
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-03-19 16:57:56 +0100

    🎨 Ajouter la possibilité de spécifier un created_at spécifique avant de publish un event

--- packages/infrastructure/pg-event-sourcing/src/publish/publish.ts
+++ packages/infrastructure/pg-event-sourcing/src/publish/publish.ts
@@ -5,12 +5,12 @@ import { Event } from '../event.js';
 const insertEvent =
   'insert into event_store.event_stream values ($1, $2, $3, (select count(stream_id) + 1 from event_store.event_stream WHERE stream_id = $5), $4)';
 
-export const publish = async (
-  streamId: string,
-  ...events: ReadonlyArray<Pick<Event, 'payload' | 'type'>>
-) => {
+export type PublishEvent = Pick<Event, 'payload' | 'type'> & { created_at?: string };
+
+export const publish = async (streamId: string, ...events: ReadonlyArray<PublishEvent>) => {
   const createdAt = new Date().toISOString();
-  for (const { type, payload } of events) {
-    await executeQuery(insertEvent, streamId, createdAt, type, payload, streamId);
+
+  for (const { type, payload, created_at = createdAt } of events) {
+    await executeQuery(insertEvent, streamId, created_at, type, payload, streamId);
   }
 };
