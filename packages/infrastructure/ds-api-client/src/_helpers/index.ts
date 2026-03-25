b805907150b246a257b6b07781fa62bcaa744ddd
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-03-25 10:05:26 +0100

    🎨 Amélioration suite à review

--- packages/infrastructure/ds-api-client/src/_helpers/index.ts
+++ packages/infrastructure/ds-api-client/src/_helpers/index.ts
@@ -1,6 +1,4 @@
 export * from './getters/index.js';
 export * from './mappers/index.js';
 export * from './reverseRecord.js';
-export * from './récupérerFichier.js';
-export * from './récupérerAttestationGarantiesFinancièresAvecPlusieursFichiers.js';
 export type * from './types.js';
--- packages/infrastructure/ds-api-client/src/getAttestationGarantiesFinancières.ts
+++ packages/infrastructure/ds-api-client/src/getAttestationGarantiesFinancières.ts
@@ -1,11 +1,12 @@
+import { extension } from 'mime-types';
+import PizZip from 'pizzip';
+
 import { getLogger } from '@potentiel-libraries/monitoring';
 import { Option } from '@potentiel-libraries/monads';
+import { DateTime } from '@potentiel-domain/common';
+import { mergeDocuments } from '@potentiel-libraries/pdf';
 
 import { getDossier } from './getDossier.js';
-import {
-  récupérerAttestationGarantiesFinancièresAvecPlusieursFichiers,
-  récupérerFichier,
-} from './_helpers/index.js';
 import { getGarantiesFinancièresFiles } from './getGarantiesFinancièresFiles.js';
 
 export const getAttestationGarantiesFinancières = async (dossierNumber: number) => {
@@ -22,10 +23,10 @@ export const getAttestationGarantiesFinancières = async (dossierNumber: number)
     }
 
     const {
-      dépôt: { dateConstitutionGf },
+      dépôt: { dateConstitutionGf: dateConstitution },
     } = dossier;
 
-    if (!dateConstitutionGf) {
+    if (!dateConstitution) {
       logger.warn(
         `Aucune date de constitution de garanties financières pour le dossier ${dossierNumber}`,
       );
@@ -39,25 +40,15 @@ export const getAttestationGarantiesFinancières = async (dossierNumber: number)
       return Option.none;
     }
 
-    if (fichiersGarantiesFinancières.length > 1) {
-      return await récupérerAttestationGarantiesFinancièresAvecPlusieursFichiers({
-        dossierNumber,
-        dateConstitution: dateConstitutionGf,
-        attestations: fichiersGarantiesFinancières,
-      });
-    }
-
-    const fichier = await récupérerFichier({
-      attestation: fichiersGarantiesFinancières[0],
+    const { attestation } = await récupérerAttestationGarantiesFinancières({
       dossierNumber,
+      dateConstitution,
+      attestations: fichiersGarantiesFinancières,
     });
 
     return {
-      attestation: {
-        content: (await fichier.blob()).stream(),
-        format: fichiersGarantiesFinancières[0].contentType,
-      },
-      dateConstitution: dateConstitutionGf,
+      attestation,
+      dateConstitution,
     };
   } catch (e) {
     logger.warn(
@@ -70,4 +61,106 @@ export const getAttestationGarantiesFinancières = async (dossierNumber: number)
     );
     return Option.none;
   }
+};
+
+type RécupérerAttestationGarantiesFinancièresProps = {
+  dateConstitution: DateTime.RawType;
+  dossierNumber: number;
+  attestations: Array<{ url: string; contentType: string }>;
+};
+
+const récupérerAttestationGarantiesFinancières = async ({
+  dossierNumber,
+  dateConstitution,
+  attestations,
+}: RécupérerAttestationGarantiesFinancièresProps) => {
+  if (attestations.length === 1) {
+    const [attestationFile] = attestations;
+    const fichier = await récupérerFichier({ attestation: attestationFile, dossierNumber });
+
+    return {
+      dateConstitution,
+      attestation: {
+        content: (await fichier.blob()).stream(),
+        format: attestationFile.contentType,
+      },
+    };
+  }
+
+  const documents = [];
+
+  for (let i = 0; i < attestations.length; i++) {
+    const attestation = attestations[i];
+    const fichier = await récupérerFichier({ attestation, dossierNumber });
+
+    const fileName =
+      new URL(attestation.url).searchParams.get('filename') ??
+      `attestation-${i}.${extension(attestation.contentType)}`;
+
+    documents.push({ fileName, content: await fichier.blob() });
+  }
+
+  const isPdfOnly = attestations.every((a) => a.contentType === 'application/pdf');
+
+  const attestation = isPdfOnly
+    ? {
+        content: (await mergeDocuments(documents.map((d) => d.content))).stream(),
+        format: 'application/pdf',
+      }
+    : {
+        content: (await zipDocuments(documents)).stream(),
+        format: 'application/zip',
+      };
+
+  return {
+    dateConstitution,
+    attestation,
+  };
+};
+
+const zipDocuments = async (
+  documents: Array<{
+    fileName: string;
+    content: Blob;
+  }>,
+  options: Omit<PizZip.GenerateOptions, 'type'> = {
+    compression: 'DEFLATE',
+    mimeType: 'application/zip',
+  },
+) => {
+  const zip = new PizZip();
+
+  for (let i = 0; i < documents.length; i++) {
+    const document = documents[i];
+    const arrayBuffer = await document.content.arrayBuffer();
+
+    zip.file(document.fileName, arrayBuffer);
+  }
+
+  return zip.generate({ ...options, type: 'blob' });
+};
+
+type RécupérerFichierProps = {
+  attestation: {
+    url: string;
+    contentType: string;
+  };
+  dossierNumber: number;
+};
+
+const récupérerFichier = async ({ attestation, dossierNumber }: RécupérerFichierProps) => {
+  const fichier = await fetch(attestation.url);
+
+  if (!fichier.ok) {
+    throw new Error(
+      `Impossible de récupérer l'attestation de garanties financières pour le dossier ${dossierNumber}`,
+    );
+  }
+
+  if (!fichier.body) {
+    throw new Error(
+      `Le fichier de l'attestation de garanties financières est introuvable pour le dossier ${dossierNumber}`,
+    );
+  }
+  return fichier;
 };
