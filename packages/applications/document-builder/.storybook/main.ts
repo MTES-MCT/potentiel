eb7aaa1e997b265962d8678935a7efe7c92aa626
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-03-25 11:59:49 +0100

    main following doc

--- packages/applications/document-builder/.storybook/main.ts
+++ packages/applications/document-builder/.storybook/main.ts
@@ -1,14 +1,19 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import webpack from 'webpack';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const getAbsolutePath = (packageName: string) =>
+  dirname(fileURLToPath(import.meta.resolve(join(packageName, 'package.json'))));
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-webpack5-compiler-swc',
-    '@storybook/addon-docs',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
+    getAbsolutePath('@storybook/addon-docs'),
   ],
-  framework: '@storybook/react-webpack5',
+  framework: getAbsolutePath('@storybook/react-webpack5'),
   staticDirs: ['../src/assets'],
   webpackFinal: (config) => {
     config.plugins?.push(
