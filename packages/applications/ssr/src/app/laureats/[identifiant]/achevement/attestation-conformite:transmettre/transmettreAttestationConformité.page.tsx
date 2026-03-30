a011434ed6ae1190014b35fa0a7915611ac2f5bc
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-03-30 17:56:45 +0200

    💄 Display columns

--- packages/applications/ssr/src/app/laureats/[identifiant]/achevement/attestation-conformite:transmettre/transmettreAttestationConformité.page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/achevement/attestation-conformite:transmettre/transmettreAttestationConformité.page.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Alert from '@codegouvfr/react-dsfr/Alert';
 
 import { Heading1 } from '@/components/atoms/headings';
+import { ColumnPageTemplate } from '@/components/templates/ColumnPage.template';
 
 import {
   AttestationConformitéForm,
@@ -19,32 +20,42 @@ export type TransmettreAttestationConformitéPageProps = Pick<
 export const TransmettreAttestationConformitéPage: FC<
   TransmettreAttestationConformitéPageProps
 > = ({ identifiantProjet, demanderMainlevée, lauréatNotifiéLe }) => (
-  <>
-    <Heading1>Transmettre l'attestation de conformité du projet</Heading1>
-    <InfoBoxAttestationConformité />
-    <Alert
-      severity="warning"
-      small
-      className="mb-4"
-      description={
-        <p className="p-3">
-          Avant de transmettre votre{' '}
-          <span className="font-semibold">attestation de conformité</span> sur la plateforme,
-          veuillez vérifier avec le cocontractant que tous les éléments attendus sur Potentiel sont
-          corrects. Une fois l'achèvement déclaré sur Potentiel, vous ne pourrez plus effectuer de
-          demandes ni de déclarations sur la plateforme. Vous aurez cependant toujours la
-          possibilité de modifier les rubriques :{' '}
-          <span className="font-semibold">Raccordement au réseau</span> et{' '}
-          <span className="font-semibold">Garanties financières</span>.
-        </p>
-      }
-    />
-    <AttestationConformitéForm
-      identifiantProjet={identifiantProjet}
-      action={transmettreAttestationConformitéAction}
-      submitLabel="Transmettre"
-      demanderMainlevée={demanderMainlevée}
-      lauréatNotifiéLe={lauréatNotifiéLe}
-    />
-  </>
+  <ColumnPageTemplate
+    heading={<Heading1>Transmettre l'attestation de conformité du projet</Heading1>}
+    leftColumn={{
+      children: (
+        <AttestationConformitéForm
+          identifiantProjet={identifiantProjet}
+          action={transmettreAttestationConformitéAction}
+          submitLabel="Transmettre"
+          demanderMainlevée={demanderMainlevée}
+          lauréatNotifiéLe={lauréatNotifiéLe}
+        />
+      ),
+    }}
+    rightColumn={{
+      children: (
+        <>
+          <InfoBoxAttestationConformité />
+          <Alert
+            severity="warning"
+            small
+            className="mb-4"
+            description={
+              <p className="p-3">
+                Avant de transmettre votre{' '}
+                <span className="font-semibold">attestation de conformité</span> sur la plateforme,
+                veuillez vérifier avec le cocontractant que tous les éléments attendus sur Potentiel
+                sont corrects. Une fois l'achèvement déclaré sur Potentiel, vous ne pourrez plus
+                effectuer de demandes ni de déclarations sur la plateforme. Vous aurez cependant
+                toujours la possibilité de modifier les rubriques :{' '}
+                <span className="font-semibold">Raccordement au réseau</span> et{' '}
+                <span className="font-semibold">Garanties financières</span>.
+              </p>
+            }
+          />
+        </>
+      ),
+    }}
+  />
 );
