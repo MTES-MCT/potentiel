e2721164eeeb9b815157148d01804bc51a22fbcc
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-03-30 17:51:24 +0200

    ✨ Ajout d'un bloc d'info lors de l'achèvement

--- packages/applications/ssr/src/app/laureats/[identifiant]/achevement/attestation-conformite:transmettre/transmettreAttestationConformité.page.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/achevement/attestation-conformite:transmettre/transmettreAttestationConformité.page.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import Alert from '@codegouvfr/react-dsfr/Alert';
 
 import { Heading1 } from '@/components/atoms/headings';
 
@@ -21,6 +22,23 @@ export const TransmettreAttestationConformitéPage: FC<
   <>
     <Heading1>Transmettre l'attestation de conformité du projet</Heading1>
     <InfoBoxAttestationConformité />
+    <Alert
+      severity="warning"
+      small
+      className="mb-4"
+      description={
+        <p className="p-3">
+          Avant de transmettre votre{' '}
+          <span className="font-semibold">attestation de conformité</span> sur la plateforme,
+          veuillez vérifier avec le cocontractant que tous les éléments attendus sur Potentiel sont
+          corrects. Une fois l'achèvement déclaré sur Potentiel, vous ne pourrez plus effectuer de
+          demandes ni de déclarations sur la plateforme. Vous aurez cependant toujours la
+          possibilité de modifier les rubriques :{' '}
+          <span className="font-semibold">Raccordement au réseau</span> et{' '}
+          <span className="font-semibold">Garanties financières</span>.
+        </p>
+      }
+    />
     <AttestationConformitéForm
       identifiantProjet={identifiantProjet}
       action={transmettreAttestationConformitéAction}
