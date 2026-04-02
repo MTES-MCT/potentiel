d097e92a7806654fd8618f9ec0ba228fde8834c9
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-02 15:03:33 +0200

    🎨 Ajout de la réponse signée dans l'historique

--- packages/applications/ssr/src/app/laureats/[identifiant]/garanties-financieres/(historique)/events/mainlevée/mapToDemandeMainlevéeGarantiesFinancièresRejetéeTimelineItemsProps.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/garanties-financieres/(historique)/events/mainlevée/mapToDemandeMainlevéeGarantiesFinancièresRejetéeTimelineItemsProps.tsx
@@ -1,15 +1,23 @@
 import { Lauréat } from '@potentiel-domain/projet';
 
 import { TimelineItemProps } from '@/components/organisms/timeline';
+import { formatDateToText } from '@/app/_helpers/formatDateToText';
 
 export const mapToDemandeMainlevéeGarantiesFinancièresRejetéeTimelineItemsProps = (
   event: Lauréat.GarantiesFinancières.DemandeMainlevéeGarantiesFinancièresRejetéeEvent,
 ): TimelineItemProps => {
   const { rejetéLe, rejetéPar } = event.payload;
 
+  const document = Lauréat.GarantiesFinancières.DocumentMainlevée.demandeRejetée(event.payload);
+
   return {
     date: rejetéLe,
     title: 'La demande de mainlevée des garanties financières a été rejetée',
     actor: rejetéPar,
+    file: {
+      document,
+      label: 'Télécharger la réponse signée',
+      ariaLabel: `Télécharger la réponse signée de la demande de mainlevée des garanties financières rejetée le ${formatDateToText(rejetéLe)}`,
+    },
   };
 };
