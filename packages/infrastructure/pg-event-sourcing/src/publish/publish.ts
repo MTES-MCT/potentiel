fa2955ce25d22d716413c28bb548cdfc95a2a2fc
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-09 10:57:42 +0200

    🎨 Ajouter la possibilité de spécifier une version pour un publish d'event

--- packages/infrastructure/pg-event-sourcing/src/publish/publish.ts
+++ packages/infrastructure/pg-event-sourcing/src/publish/publish.ts
@@ -2,15 +2,28 @@ import { executeQuery } from '@potentiel-libraries/pg-helpers';
 
 import { Event } from '../event.js';
 
-const insertEvent =
-  'insert into event_store.event_stream values ($1, $2, $3, (select count(stream_id) + 1 from event_store.event_stream WHERE stream_id = $5), $4)';
-
-export type PublishEvent = Pick<Event, 'payload' | 'type'> & { created_at?: string };
+const insertEvent = `
+  insert into event_store.event_stream
+  values (
+    $1,
+    $2,
+    $3,
+    coalesce(
+      $6,
+      (select count(stream_id) + 1 from event_store.event_stream where stream_id = $5)
+    ),
+    $4
+  )
+`;
+export type PublishEvent = Pick<Event, 'payload' | 'type'> & {
+  created_at?: string;
+  version?: number;
+};
 
 export const publish = async (streamId: string, ...events: ReadonlyArray<PublishEvent>) => {
   const createdAt = new Date().toISOString();
 
-  for (const { type, payload, created_at = createdAt } of events) {
-    await executeQuery(insertEvent, streamId, created_at, type, payload, streamId);
+  for (const { type, payload, created_at = createdAt, version } of events) {
+    await executeQuery(insertEvent, streamId, created_at, type, payload, streamId, version);
   }
 };
