1dd02f57f3275e112b82d9b9e7e41cae87bd6b5a
Hanaë <hanae.yamashita@lilo.org>
2026-04-22 11:58:18 +0200

    Update projector actuel

--- packages/applications/projectors/src/subscribers/lauréat/raccordement/raccordementSuppriméV1.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/raccordement/raccordementSuppriméV1.projector.ts
@@ -1,21 +1,11 @@
-import { Where } from '@potentiel-domain/entity';
 import { Lauréat } from '@potentiel-domain/projet';
-import {
-  removeProjection,
-  removeProjectionWhere,
-} from '@potentiel-infrastructure/pg-projection-write';
+import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const raccordementSuppriméV1Projector = async ({
   payload: { identifiantProjet },
 }: Lauréat.Raccordement.RaccordementSuppriméEvent) => {
-  await removeProjection<Lauréat.Raccordement.RaccordementEntity>(
+  await updateOneProjection<Lauréat.Raccordement.RaccordementEntity>(
     `raccordement|${identifiantProjet}`,
-  );
-
-  await removeProjectionWhere<Lauréat.Raccordement.DossierRaccordementEntity>(
-    'dossier-raccordement',
-    {
-      identifiantProjet: Where.equal(identifiantProjet),
-    },
+    { désactivé: true },
   );
 };
