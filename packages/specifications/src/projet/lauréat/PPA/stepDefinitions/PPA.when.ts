acf00b90fc966150c65191a3f449d7c965c4269e
Hanaë <hanae.yamashita@lilo.org>
2026-04-23 13:20:56 +0200

    Domain : cas limites

--- packages/specifications/src/projet/lauréat/PPA/stepDefinitions/PPA.when.ts
+++ packages/specifications/src/projet/lauréat/PPA/stepDefinitions/PPA.when.ts
@@ -6,18 +6,31 @@ import { Lauréat } from '@potentiel-domain/projet';
 import { PotentielWorld } from '../../../../potentiel.world.js';
 
 Quand(
-  `un utilisateur {string} signale un état PPA pour le projet lauréat`,
-  async function (this: PotentielWorld, rôle: string) {
+  `un utilisateur {string} signale un état PPA pour le projet {string}`,
+  async function (this: PotentielWorld, rôle: string, projet: string) {
     try {
       const utilisateur =
         rôle === 'dgec'
           ? this.utilisateurWorld.dgecFixture.email
-          : this.utilisateurWorld.drealFixture.email;
+          : rôle === 'dreal'
+            ? this.utilisateurWorld.drealFixture.email
+            : 'inconnu';
+      if (utilisateur === 'inconnu') {
+        throw new Error(`Rôle utilisateur inconnu : ${rôle}`);
+      }
+
+      const statut = ['lauréat', 'éliminé'].includes(projet) ? projet : 'inconnu';
+      if (statut === 'inconnu') {
+        throw new Error(`Statut du projet inconnu : ${projet}`);
+      }
 
       await mediator.send<Lauréat.SignalerPPAUseCase>({
         type: 'Lauréat.UseCase.SignalerPPA',
         data: {
-          identifiantProjetValue: this.lauréatWorld.identifiantProjet.formatter(),
+          identifiantProjetValue:
+            statut === 'lauréat'
+              ? this.lauréatWorld.identifiantProjet.formatter()
+              : this.éliminéWorld.identifiantProjet.formatter(),
           signaléLeValue: new Date().toISOString(),
           signaléParValue: utilisateur,
         },
