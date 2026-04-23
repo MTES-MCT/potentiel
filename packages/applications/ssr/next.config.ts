2730ddb198c5cefee3dc78bb33f55adcca22bd4a
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-23 16:34:22 +0200

    🎨 Fixes

--- packages/applications/ssr/next.config.ts
+++ packages/applications/ssr/next.config.ts
@@ -68,24 +68,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-// export default withSentryConfig(nextConfig, {
-//   release: { create: false, name: '' },
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options
-
-//   org: process.env.SENTRY_ORG,
-//   project: process.env.SENTRY_PROJECT,
-//   sentryUrl: process.env.SENTRY_URL,
-
-//   // Only print logs for uploading source maps in CI
-//   // silent: !process.env.CI,
-
-//   // Upload a larger set of source maps for prettier stack traces (increases build time)
-//   // widenClientFileUpload: true,
-
-//   sourcemaps: {
-//     deleteSourcemapsAfterUpload: true,
-//   },
-// });
-
 export default nextConfig;
