df31e35340b21b1daaa218fa2cd45374b9746fd9
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-23 11:33:03 +0200

    ⬆️ Node version 24.14.0 in package.json + readd withAuth middleware

--- packages/applications/ssr/src/proxy.ts
+++ packages/applications/ssr/src/proxy.ts
@@ -1,7 +1,8 @@
 import { chain } from './middlewares/chain';
+import { withAuth } from './middlewares/withAuth';
 import { withCSRF } from './middlewares/withCSRF';
 
-export default chain([withCSRF]);
+export default chain([withAuth, withCSRF]);
 
 export const config = {
   // do not run middleware for paths matching one of following
