e11e7a47fd572bdc3b2474d1ef75bc514569f575
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-24 15:12:39 +0200

    🐛 Fix url to get docx assets in generated folder

--- packages/applications/document-builder/src/assets.ts
+++ packages/applications/document-builder/src/assets.ts
@@ -1,6 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-export const assetsFolderPath = path.resolve(import.meta?.dirname ?? __dirname, 'assets');
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
+export const assetsFolderPath = path.resolve(currentDir, 'assets');
+
 export const docxFolderPath = path.resolve(assetsFolderPath, 'docx');
 export const fontsFolderPath = path.resolve(assetsFolderPath, 'fonts');
 export const imagesFolderPath = path.resolve(assetsFolderPath, 'images');
