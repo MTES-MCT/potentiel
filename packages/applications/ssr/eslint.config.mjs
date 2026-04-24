56021242ec8766f2a5e7b47232f4778842fe9141
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-04-24 10:59:37 +0200

    ⬆️ Upgrade react dsfr

--- packages/applications/ssr/eslint.config.mjs
+++ packages/applications/ssr/eslint.config.mjs
@@ -1,17 +1,11 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 
 const eslintConfig = [
   ...nextCoreWebVitals,
   ...nextTypescript,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
 ];
 
