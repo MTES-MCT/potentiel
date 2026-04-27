105311d8ef2783b2265d4d8ffbc958fbacdd225e
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-04-27 17:07:35 +0200

    fix: csv et affichage candidature / producteur

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/informations-generales/(sections)/ProducteurDétails.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/informations-generales/(sections)/ProducteurDétails.tsx
@@ -3,6 +3,7 @@ import { PlainType } from '@potentiel-domain/core';
 
 import { TertiaryLink } from '@/components/atoms/form/TertiaryLink';
 import { ChampObligatoireAvecAction } from '@/app/laureats/[identifiant]/_helpers';
+import { Heading6 } from '@/components/atoms/headings';
 
 export type ProducteurDétailsProps = ChampObligatoireAvecAction<
   PlainType<Lauréat.Producteur.ConsulterProducteurReadModel>
@@ -11,7 +12,21 @@ export type ProducteurDétailsProps = ChampObligatoireAvecAction<
 export const ProducteurDétails = ({ value, action }: ProducteurDétailsProps) => (
   <>
     <div className="flex flex-col gap-1">
-      <span>{value.producteur || 'Champ non renseigné'}</span>
+      <div>
+        <Heading6>Nom du producteur</Heading6>
+        <span>{value.producteur || 'Champ non renseigné'}</span>
+      </div>
+      {value.numéroImmatriculation !== undefined && (
+        <div>
+          <Heading6>Numéro d'immmatriculation</Heading6>
+          {value.numéroImmatriculation.siret && (
+            <span>Numéro SIRET : {value.numéroImmatriculation.siret}</span>
+          )}
+          {value.numéroImmatriculation.siren && (
+            <span>Numéro SIREN : {value.numéroImmatriculation.siren}</span>
+          )}
+        </div>
+      )}
       {action && <TertiaryLink href={action.url}>{action.label}</TertiaryLink>}
     </div>
   </>
