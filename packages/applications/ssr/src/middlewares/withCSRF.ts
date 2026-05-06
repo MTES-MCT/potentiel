5f50e065ee277f0de2142bdb86f4f85d71d072a3
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-05-06 11:57:51 +0200

    hack csrf

---
+++ packages/applications/ssr/src/middlewares/withCSRF.ts
@@ -0,0 +1,45 @@
+import { createCsrfProtect, CsrfError } from '@potentiel-libraries/csrf';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+import { CustomMiddleware } from './middleware';
+
+const csrfProtect = createCsrfProtect({
+  cookie: {
+    sameSite: true,
+    secure: true,
+    httpOnly: true,
+  },
+  token: {
+    responseHeader: 'csrf_token',
+  },
+});
+
+export function withCSRF(middleware: CustomMiddleware) {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    const response = NextResponse.next();
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      await csrfProtect(request as any, response as any);
+    } catch (err) {
+      if (err instanceof CsrfError) {
+        const isAction = request.method === 'POST' && request.headers.has('Next-Action');
+        if (isAction) {
+          return NextResponse.json(
+            {
+              status: 'failed',
+            },
+            {
+              status: 403,
+              statusText: 'Invalid CSRF token',
+            },
+          );
+        }
+        return NextResponse.redirect(new URL('/error', request.url));
+      }
+      throw err;
+    }
+
+    return middleware(request, event, response);
+  };
+}
