da64f3418204943d4e0ef62c54f0939973a82a31
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-05-08 14:36:31 +0200

    🎨 Mise à jour des CSP pour Crisp

--- packages/applications/ssr/src/utils/csp.ts
+++ packages/applications/ssr/src/utils/csp.ts
@@ -11,15 +11,19 @@ export const setCspHeader = (request: IncomingMessage, response: ServerResponse)
     'connect-src': [
       "'self'",
       'https://potentiel.beta.gouv.fr',
-      'https://client.crisp.chat',
+      'https://*.crisp.chat',
       'wss://client.relay.crisp.chat',
+      'wss://*.relay.crisp.chat',
+      'wss://*.relay.rescue.crisp.chat',
       'https://sentry.incubateur.net',
+
       process.env.NEXT_PUBLIC_GEO_API_URL ?? '',
     ],
-    'font-src': ["'self'", 'https://client.crisp.chat'],
-    'frame-src': ['https://metabase.potentiel.beta.gouv.fr', 'blob:'],
-    'img-src': ["'self'", 'data:', 'https://image.crisp.chat'],
-    'style-src': ["'self'", "'unsafe-inline'", 'data:', 'https://client.crisp.chat'],
+    'media-src  ': ["'self'", 'https://*.crisp.chat'],
+    'font-src': ["'self'", 'https://*.crisp.chat'],
+    'frame-src': ['https://metabase.potentiel.beta.gouv.fr', 'https://*.crisp.chat', 'blob:'],
+    'img-src': ["'self'", 'data:', 'https://*.crisp.chat'],
+    'style-src': ["'self'", "'unsafe-inline'", 'data:', 'https://*.crisp.chat'],
     'script-src': [
       "'self'",
       // every inline <script> must have this nonce, or will be forbidden to run
@@ -29,7 +33,7 @@ export const setCspHeader = (request: IncomingMessage, response: ServerResponse)
       // an alternative solution can be found here: https://react-dsfr.codegouv.studio/content-security-policy
       "'sha256-UEZfoO3SfsYbnIIAoHHUiIGOhT+nhTDv2gd4I5588HQ='",
       'https://metabase.potentiel.beta.gouv.fr',
-      'https://client.crisp.chat',
+      'https://*.crisp.chat',
       // whitelist scalar
       "'sha256-RG6G8sfmsknwGzYCV+Cc56LE1j6++Gv3RE1sevFccm8='",
       'https://cdn.jsdelivr.net/npm/@scalar/api-reference',
