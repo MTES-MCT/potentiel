25f3f87df1668ef75bf1ea6d2650203b205362b6
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-05-12 11:18:48 +0200

    🎨 Utilisation de getSessionUser à la place de getContext?.utilisateur

--- packages/applications/ssr/src/app/(homepage)/page.tsx
+++ packages/applications/ssr/src/app/(homepage)/page.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from 'next';
 
-import { getContext } from '@potentiel-applications/request-context';
-
 import { PageWithErrorHandling } from '@/utils/PageWithErrorHandling';
 
 import { HomePage } from './Home.page';
+import { getSessionUser } from '@/auth/getSessionUser';
+import { headers } from 'next/headers';
 
 export const metadata: Metadata = { title: 'Accueil' };
 
 export default async function Page() {
   return PageWithErrorHandling(async () => {
-    const utilisateur = getContext()?.utilisateur;
+    const utilisateur = await getSessionUser({ headers: await headers() });
 
     return <HomePage utilisateur={utilisateur} />;
   });
