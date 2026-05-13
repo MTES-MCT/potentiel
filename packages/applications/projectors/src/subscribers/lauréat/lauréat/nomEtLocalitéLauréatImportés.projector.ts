8f8580313b1b9e2d1c81c42bf03de23f556829cb
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-05-13 15:08:22 +0200

    Ajout des coordonnées à NomEtLocalitéLauréatImportés-V1

--- packages/applications/projectors/src/subscribers/lauréat/lauréat/nomEtLocalitéLauréatImportés.projector.ts
+++ packages/applications/projectors/src/subscribers/lauréat/lauréat/nomEtLocalitéLauréatImportés.projector.ts
@@ -2,9 +2,10 @@ import { Lauréat } from '@potentiel-domain/projet';
 import { updateOneProjection } from '@potentiel-infrastructure/pg-projection-write';
 
 export const nomEtLocalitéLauréatImportésProjector = async ({
-  payload: { identifiantProjet, nomProjet, localité },
+  payload: { identifiantProjet, nomProjet, localité, coordonnées },
 }: Lauréat.NomEtLocalitéLauréatImportésEvent) =>
   await updateOneProjection<Lauréat.LauréatEntity>(`lauréat|${identifiantProjet}`, {
     nomProjet,
     localité,
+    coordonnées,
   });
