63d49807195bede75b03d0c86dc96b54bcbf20cb
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-05-19 17:25:11 +0200

    fix download

--- packages/libraries/file-storage/src/download.ts
+++ packages/libraries/file-storage/src/download.ts
@@ -3,28 +3,14 @@ import { executeSelect } from '@potentiel-libraries/pg-helpers';
 import { FichierInexistant } from './fichierInexistant.error.js';
 
 export const download = async (filePath: string): Promise<ReadableStream> => {
-  try {
-    // const result = await getClient().send(
-    //   new GetObjectCommand({ Bucket: getBucketName(), Key: filePath }),
-    // );
+  const result = await executeSelect<{ content: Buffer }>(
+    `select content from document_store.files where key = $1`,
+    filePath,
+  );
 
-    // if (!result.Body) {
-    //   throw new FichierInexistant(filePath);
-    // }
-
-    const result = await executeSelect<{ content: Buffer }>(
-      `select content from document_store.files where key = $1`,
-      filePath,
-    );
-
-    if (!result?.length) {
-      throw new FichierInexistant(filePath);
-    }
-
-    return ReadableStream.from(result[0].content.toString('utf-8'));
-
-    // return result.Body.transformToWebStream();
-  } catch {
+  if (!result?.length) {
     throw new FichierInexistant(filePath);
   }
+
+  return ReadableStream.from([result[0].content]);
 };
