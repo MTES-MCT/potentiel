02e1089a21387b261ac1e228727b590d6d6a16c0
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-05-27 15:19:28 +0200

    ui tweaks

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(sections)/IdentifiantProjet.section.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(sections)/IdentifiantProjet.section.tsx
@@ -1,8 +1,7 @@
-import Tooltip from '@codegouvfr/react-dsfr/Tooltip';
+import Tooltip from '@mui/material/Tooltip';
 
 import { IdentifiantProjet } from '@potentiel-domain/projet';
 
-import { Icon } from '@/components/atoms/Icon';
 import { Section } from '@/components/atoms/menu/Section';
 import { SectionWithErrorHandling } from '@/components/atoms/menu/SectionWithErrorHandling';
 import { CopyButton } from '@/components/molecules/CopyButton';
@@ -25,19 +24,18 @@ export const IdentifiantProjetSection = ({
       const identifiantProjet = IdentifiantProjet.convertirEnValueType(rawIdentifiant);
       return (
         <Section title={sectionTitle}>
-          <ul className="flex flex-col gap-1">
-            <li>
-              <span>Identifiant Projet : </span>
-              <CopyButton textToCopy={identifiantProjet.formatterMétier()} />
+          <ul>
+            <li className="flex flex-row items-center gap-1">
+              <Tooltip title="Format d'identifiant utilisé dans les documents et courriers électroniques.">
+                <span>Identifiant Métier :</span>
+              </Tooltip>
+              <CopyButton textToCopy={identifiantProjet.formatterMétier()} className="text-sm" />
             </li>
-            <li>
-              <span>
-                Identifiant Technique :{' '}
-                <Tooltip title="Format d'identifiant utilisé dans les fichiers CSV et dans les échanges par API.">
-                  <Icon size="sm" id="fr-icon-question-line" />
-                </Tooltip>
-              </span>
-              <CopyButton textToCopy={identifiantProjet.formatter()} />
+            <li className="flex flex-row items-center gap-1">
+              <Tooltip title="Format d'identifiant utilisé dans les fichiers CSV et dans les échanges par API.">
+                <span>Identifiant Technique :</span>
+              </Tooltip>
+              <CopyButton textToCopy={identifiantProjet.formatter()} className="text-sm" />
             </li>
           </ul>
         </Section>
