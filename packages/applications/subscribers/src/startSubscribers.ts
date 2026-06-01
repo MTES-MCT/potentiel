bafecba787e596a71fb80f57cb488dc0ae32de49
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-06-01 17:10:39 +0200

    seed

--- packages/applications/subscribers/src/startSubscribers.ts
+++ packages/applications/subscribers/src/startSubscribers.ts
@@ -1,6 +1,7 @@
 import { mediator } from 'mediateur';
 
 import { logMiddleware } from '@potentiel-applications/bootstrap';
+import { seedAppelOffre, seedPériodes } from '@potentiel-applications/projectors';
 import { getLogger } from '@potentiel-libraries/monitoring';
 
 import { type SetupSubscribersProps, setupSubscribers } from './setup/index.js';
@@ -9,6 +10,9 @@ export type StartSubscribersProps = SetupSubscribersProps;
 
 export const startSubscribers = async ({ dependencies }: StartSubscribersProps) => {
   mediator.use({ middlewares: [logMiddleware] });
+
+  await seedAppelOffre();
+  await seedPériodes();
 
   const subscribers = setupSubscribers({ dependencies });
 
