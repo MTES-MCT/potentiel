4e9a6f2708043d46d9fb232ae5c23bad5b3cf41b
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-06-01 16:36:43 +0200

    ✨ Rebuild depuis la CLI

--- packages/infrastructure/pg-event-sourcing/src/index.ts
+++ packages/infrastructure/pg-event-sourcing/src/index.ts
@@ -2,10 +2,16 @@ export type { Event } from './event.js';
 export { loadAggregate } from './load/loadAggregate.js';
 export { loadFromStream } from './load/loadFromStream.js';
 export { publish } from './publish/publish.js';
-export type { RebuildTriggered } from './subscribe/rebuild/rebuildTriggered.event.js';
+export { rebuild } from './subscribe/rebuild/rebuild.js';
+export { rebuildAll } from './subscribe/rebuild/rebuildAll.js';
+export type {
+  RebuildAllTriggered,
+  RebuildOneTriggered,
+  RebuildTriggered,
+} from './subscribe/rebuild/rebuildTriggered.event.js';
 export {
   executeSubscribersRetry,
   listDanglingSubscribers,
   subscribe,
 } from './subscribe/subscribe.js';
-export type { Subscriber, Unsubscribe } from './subscribe/subscriber/subscriber.js';
+export type { Subscriber } from './subscribe/subscriber/subscriber.js';
