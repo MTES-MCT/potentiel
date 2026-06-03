1fb79ac1ccfa582a667d31bd2a023b46c957f914
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-06-03 17:32:08 +0200

    build

--- packages/applications/ssr/src/app/laureats/[identifiant]/representant-legal/_helpers/steps/index.ts
+++ packages/applications/ssr/src/app/laureats/[identifiant]/representant-legal/_helpers/steps/index.ts
@@ -2,4 +2,3 @@ export * from './SaisieNom.step';
 export * from './SaisiePièceJustificative.step';
 export * from './SaisieType.step';
 export * from './SaisieTypeSociété.step';
-export * from './Validation.step';
