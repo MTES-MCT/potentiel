0bf9107aec35ad88538ecce1f5a2e7589c43a3b6
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-06-09 09:37:14 +0200

    🐛 Rebuild history projection (#4318)

--- packages/applications/cli/src/commands/projection/rebuild.ts
+++ packages/applications/cli/src/commands/projection/rebuild.ts
@@ -41,72 +41,77 @@ export class RebuildProjectionCommand extends Command {
 
     const logger = getLogger('projection-rebuild');
 
-    const subscription = setupSubscribers({})
+    const subscriptions = setupSubscribers({})
       .listSubscriptions()
-      .find((x) => x.streamCategory === category && ['projector'].includes(x.name));
+      .filter((x) => x.streamCategory === category && ['projector', 'history'].includes(x.name))
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .reverse();
 
-    if (!subscription) {
+    if (!subscriptions.length) {
       this.error(`Aucun subscriber trouvé pour la catégorie ${category}`);
     }
 
-    logger.info('Starting inline rebuild', {
-      category,
-      streamId,
-      mode: streamId ? 'single-stream' : 'category',
-    });
+    for (const subscription of subscriptions) {
+      logger.info('Starting inline rebuild', {
+        category,
+        streamId,
+        subscription,
+        mode: streamId ? 'single-stream' : 'category',
+      });
 
-    // only used for a single stream, to throw when the stream is not found.
-    // start at -1 to ignore the RebuildTriggered event.
-    let count = -1;
+      // only used for a single stream, to throw when the stream is not found.
+      // start at -1 to ignore the RebuildTriggered event.
+      let count = -1;
 
-    const subscriber = {
-      eventHandler: async (event: DomainEvent) => {
-        count++;
-        await mediator.send({
-          type: subscription.messageType,
-          data: event,
-        });
-      },
-      eventType: 'all',
-      name: 'projector',
-      streamCategory: category,
-    };
+      const subscriber = {
+        eventHandler: async (event: DomainEvent) => {
+          count++;
+          await mediator.send({
+            type: subscription.messageType,
+            data: event,
+          });
+        },
+        eventType: 'all',
+        name: subscription.name,
+        streamCategory: category,
+      };
 
-    if (streamId) {
-      const rebuildOneEvent: RebuildOneTriggered = {
-        stream_id: `${category}|${streamId}`,
+      if (streamId) {
+        const rebuildOneEvent: RebuildOneTriggered = {
+          stream_id: `${category}|${streamId}`,
+          created_at: new Date().toISOString(),
+          version: 0,
+          type: 'RebuildTriggered',
+          payload: {
+            category,
+            id: streamId,
+          },
+        };
+        await rebuild(rebuildOneEvent, subscriber);
+
+        if (count > 0) {
+          logger.info('Stream rebuilt');
+          return;
+        }
+        logger.warn('Stream not found...');
+        this.exit(1);
+      }
+
+      logger.info('Triggering rebuild for subscription', {
+        category,
+        streamId,
+      });
+
+      const rebuildAllEvent: RebuildAllTriggered = {
+        stream_id: category,
         created_at: new Date().toISOString(),
         version: 0,
         type: 'RebuildTriggered',
         payload: {
           category,
-          id: streamId,
         },
       };
-      await rebuild(rebuildOneEvent, subscriber);
-
-      if (count > 0) {
-        logger.info('Stream rebuilt');
-        return;
-      }
-      logger.warn('Stream not found...');
-      this.exit(1);
+      await rebuildAll(rebuildAllEvent, subscriber);
     }
-
-    logger.info('Triggering rebuild for subscription', {
-      category,
-      streamId,
-    });
-
-    const rebuildAllEvent: RebuildAllTriggered = {
-      stream_id: category,
-      created_at: new Date().toISOString(),
-      version: 0,
-      type: 'RebuildTriggered',
-      payload: {
-        category,
-      },
-    };
-    await rebuildAll(rebuildAllEvent, subscriber);
   }
 }
