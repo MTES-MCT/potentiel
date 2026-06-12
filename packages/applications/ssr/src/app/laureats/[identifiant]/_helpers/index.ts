941a4fa2c2be48c1d1e4c04626e22648f2e30d9e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-06-12 10:50:56 +0200

    remove duplicate export

--- packages/applications/ssr/src/app/laureats/[identifiant]/_helpers/index.ts
+++ packages/applications/ssr/src/app/laureats/[identifiant]/_helpers/index.ts
@@ -2,7 +2,6 @@ export * from './getAchèvement';
 export * from './getAction';
 export * from './getGarantiesFinancières';
 export * from './getLauréat';
-export * from './getLauréat';
 export * from './getRaccordement';
 export * from './getRecours';
 export * from './peutEffectuerUnChangement';
