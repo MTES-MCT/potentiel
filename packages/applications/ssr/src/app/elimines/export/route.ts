8307f3f070908c857c7b0ddfe33b9c434b88180a
Hanaë <hanae.yamashita@lilo.org>
2026-06-29 15:16:53 +0200

    Détail candidature : ajout données pv

--- packages/applications/ssr/src/app/elimines/export/route.ts
+++ packages/applications/ssr/src/app/elimines/export/route.ts
@@ -110,6 +110,26 @@ export const GET = async (request: Request) =>
             label: 'Taux prévisionnel ACC (%)',
           },
           { value: 'composantsRésilients', label: 'Composants résilients' },
+          {
+            value: 'typeTerrainImplantation',
+            label: "Type de terrain d'implantation",
+          },
+          {
+            value: 'natureExacteDuTerrain',
+            label: 'Nature exacte du terrain',
+          },
+          {
+            value: 'dateObtentionCETI',
+            label: 'Date obtention CETI',
+          },
+          {
+            value: 'surfaceProjetéeAuSol',
+            label: 'Surface projetée au sol (ha)',
+          },
+          {
+            value: 'surfaceTotaleTerrainImplantation',
+            label: 'Surface terrain implantation (ha)',
+          },
           { value: 'technologieÉolien', label: 'Technologie (éolien)' },
           { value: 'diamètreRotorEnMètres', label: 'Diamètre rotor (m)' },
           { value: 'hauteurBoutDePâleEnMètres', label: 'Hauteur bout de pâle (m)' },
