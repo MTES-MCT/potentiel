4ef28be9cf52419bc7491b464f93e9e1cba317c1
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-06-30 11:59:33 +0200

    details

--- packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/test/(raccordement-du-projet)/DétailsRaccordementDuProjetPage.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/test/(raccordement-du-projet)/DétailsRaccordementDuProjetPage.tsx
@@ -1,4 +1,3 @@
-import Alert from '@codegouvfr/react-dsfr/Alert';
 import Button from '@codegouvfr/react-dsfr/Button';
 
 import type { IdentifiantProjet, Lauréat } from '@potentiel-domain/projet';
@@ -10,35 +9,20 @@ import { GestionnaireRéseauSection } from './GestionnaireRéseau.section';
 type Props = {
   identifiantProjet: IdentifiantProjet.RawType;
   lienRetour: string;
-  peutCréerNouveauDossier: boolean;
   statut: Lauréat.StatutLauréat.RawType;
 };
 
 export const DétailsRaccordementDuProjetPage = ({
   identifiantProjet,
   lienRetour,
-  peutCréerNouveauDossier,
-  statut
+  statut,
 }: Props) => {
   return (
     <>
       <TitrePageRaccordement />
       <div className="my-2 md:my-4 flex flex-col gap-4">
-        <div className="flex flex-row items-start gap-8">
+        <div className='w-fit'>
           <GestionnaireRéseauSection identifiantProjet={identifiantProjet} />
-          {peutCréerNouveauDossier && (
-            <Alert
-              severity="info"
-              small
-              className="flex-1"
-              description={
-                <div className="p-3">
-                  Si le raccordement comporte plusieurs points d'injection, vous devez ajouter un
-                  nouveau dossier de raccordement.
-                </div>
-              }
-            />
-          )}
         </div>
         <DossiersRaccordementSection identifiantProjet={identifiantProjet} statut={statut} />
         <Button
