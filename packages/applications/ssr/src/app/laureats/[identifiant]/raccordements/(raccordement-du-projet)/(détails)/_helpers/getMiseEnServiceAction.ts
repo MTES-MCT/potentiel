fac9183ce372958ed6cc1bb26a2ee09af790bce9
VioMrqs <27735540+VioMrqs@users.noreply.github.com>
2026-06-30 14:57:28 +0200

    migration almos done

---
+++ packages/applications/ssr/src/app/laureats/[identifiant]/raccordements/(raccordement-du-projet)/(détails)/_helpers/getMiseEnServiceAction.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@potentiel-applications/routes';
+import type { Lauréat } from '@potentiel-domain/projet';
+import type { Role } from '@potentiel-domain/utilisateur';
+
+import type { DossierEtapeAction } from '../../../test/(raccordement-du-projet)/Dossier';
+
+type GetMiseEnServiceAction = (args: {
+  rôle: Role.ValueType;
+  dossier: Lauréat.Raccordement.ConsulterDossierRaccordementReadModel;
+}) => DossierEtapeAction;
+
+export const getMiseEnServiceAction: GetMiseEnServiceAction = ({ rôle, dossier }) => {
+  const transmettreAction = {
+    href: Routes.Raccordement.transmettreDateMiseEnService(
+      dossier.identifiantProjet.formatter(),
+      dossier.référence.formatter(),
+    ),
+    label: 'Transmettre la proposition technique et financière',
+  };
+  const modifierAction = {
+    href: Routes.Raccordement.modifierDateMiseEnService(
+      dossier.identifiantProjet.formatter(),
+      dossier.référence.formatter(),
+    ),
+    label: 'Modifier',
+  };
+
+  if (dossier.miseEnService?.dateMiseEnService) {
+    return rôle.aLaPermission('raccordement.date-mise-en-service.transmettre')
+      ? transmettreAction
+      : undefined;
+  }
+
+  return rôle.aLaPermission('raccordement.date-mise-en-service.modifier')
+    ? modifierAction
+    : undefined;
+};
