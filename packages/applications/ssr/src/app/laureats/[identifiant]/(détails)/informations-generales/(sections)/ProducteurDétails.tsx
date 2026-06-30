a62e6113bea76b2efa413f1fc7076ab4c5e533c2
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-06-30 15:43:21 +0200

    🎨 Afficher lien action sous producteur + sous identification

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/informations-generales/(sections)/ProducteurDétails.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/informations-generales/(sections)/ProducteurDétails.tsx
@@ -1,38 +1,51 @@
 import type { PlainType } from '@potentiel-domain/core';
 import type { Lauréat } from '@potentiel-domain/projet';
 
-import type { ChampAvecMultiplesActions } from '@/app/laureats/[identifiant]/_helpers';
+import type { ChampObligatoireAvecAction } from '@/app/laureats/[identifiant]/_helpers';
 import { FormattedSIREN, FormattedSIRET } from '@/components/atoms/FormattedNuméroIdentification';
 import { TertiaryLink } from '@/components/atoms/form/TertiaryLink';
 import { TitreChamp } from '@/components/atoms/section/TitreChamp';
 
-export type ProducteurDétailsProps = ChampAvecMultiplesActions<
-  PlainType<Lauréat.Producteur.ConsulterProducteurReadModel>
->;
+export type ProducteurDétailsProps = {
+  producteur: ChampObligatoireAvecAction<
+    PlainType<Lauréat.Producteur.ConsulterProducteurReadModel['producteur']>
+  >;
+  numéroIdentification: ChampObligatoireAvecAction<
+    PlainType<Lauréat.Producteur.ConsulterProducteurReadModel['numéroIdentification']>
+  >;
+};
 
-export const ProducteurDétails = ({ value, actions }: ProducteurDétailsProps) => (
+export const ProducteurDétails = ({ producteur, numéroIdentification }: ProducteurDétailsProps) => (
   <>
     <div className="flex flex-col gap-1">
       <div>
         <TitreChamp>Nom du producteur</TitreChamp>
-        <span>{value.producteur || 'Champ non renseigné'}</span>
+        <span>{producteur.value || 'Champ non renseigné'}</span>
       </div>
+      {producteur.action && (
+        <TertiaryLink href={producteur.action.url} key={producteur.action.label}>
+          {producteur.action.label}
+        </TertiaryLink>
+      )}
       <div>
         <TitreChamp>Numéro d'identification</TitreChamp>
         <div className="flex flex-col">
           <span>
-            Numéro SIRET : <FormattedSIRET value={value.numéroIdentification?.siret} />
+            Numéro SIRET : <FormattedSIRET value={numéroIdentification.value?.siret} />
           </span>
           <span>
-            Numéro SIREN : <FormattedSIREN value={value.numéroIdentification?.siren} />
+            Numéro SIREN : <FormattedSIREN value={numéroIdentification.value?.siren} />
           </span>
         </div>
       </div>
-      {actions?.map((action) => (
-        <TertiaryLink href={action.url} key={action.label}>
-          {action.label}
+      {numéroIdentification.action && (
+        <TertiaryLink
+          href={numéroIdentification.action.url}
+          key={numéroIdentification.action.label}
+        >
+          {numéroIdentification.action.label}
         </TertiaryLink>
-      ))}
+      )}
     </div>
   </>
 );
