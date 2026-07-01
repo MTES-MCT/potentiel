c0945bfc07698d31eb58dd95de86e0bf11e6563b
Hanaë <hanae.yamashita@lilo.org>
2026-07-01 11:10:45 +0200

    petit clean

--- packages/applications/document-builder/src/index.ts
+++ packages/applications/document-builder/src/index.ts
@@ -1,5 +1,4 @@
 export { formatDateForDocument } from './_utils/index.js';
-export type { DonnéesDocument } from './abandon/accordAbandonAvecRecandidature/buildDocument.js';
-export { buildDocument } from './abandon/accordAbandonAvecRecandidature/buildDocument.js';
+export * as RéponseSignéeAccordAbandonAvecRecandidature from './abandon/accordAbandonAvecRecandidature/index.js';
 export { AttestationSaga, buildCertificate } from './candidature/attestation/index.js';
 export * as ModèleRéponseSignée from './modèleRéponseSignée/index.js';
