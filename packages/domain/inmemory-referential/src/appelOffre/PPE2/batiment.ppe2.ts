84151933603a9c6b5741cc6da0ba7803e21f4166
Hanaë <hanae.yamashita@lilo.org>
2026-07-09 11:57:26 +0200

    fix tests

--- packages/domain/inmemory-referential/src/appelOffre/PPE2/batiment.ppe2.ts
+++ packages/domain/inmemory-referential/src/appelOffre/PPE2/batiment.ppe2.ts
@@ -123,7 +123,7 @@ export const batimentPPE2: AppelOffre.AppelOffreReadModel = {
   },
   champsSupplémentaires: {
     typologieInstallation: { type: 'optionnel' as const },
-    volumeRéservé: { type: 'requis' },
+    volumeRéservé: { type: 'requis' as const },
     ...champsSupplémentairesÀPartirDeP10,
   },
   delaiRealisationTexte: 'trente (30) mois',
