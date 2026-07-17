0047c96163bc686af77baaf92d6c4a089c995cca
Hanaë <hanae.yamashita@lilo.org>
2026-07-17 17:01:24 +0200

    review commande CLI

--- packages/applications/cli/src/commands/candidature/migrer-volume-reserve.ts
+++ packages/applications/cli/src/commands/candidature/migrer-volume-reserve.ts
@@ -30,6 +30,7 @@ export class Migrer extends Command {
   }
   async run() {
     let projetsModifiés = 0;
+    const errors: Error[] = [];
     const appelsOffresAvecVolumeRéservé = ['PPE2 - Bâtiment', 'PPE2 - Sol'];
 
     await executeQuery(
@@ -37,52 +38,59 @@ export class Migrer extends Command {
     );
 
     for (const appelOffreId of appelsOffresAvecVolumeRéservé) {
-      const appelOffre = await mediator.send<AppelOffre.ConsulterAppelOffreQuery>({
-        type: 'AppelOffre.Query.ConsulterAppelOffre',
-        data: {
-          identifiantAppelOffre: appelOffreId,
-        },
-      });
-      if (Option.isNone(appelOffre)) {
-        throw new Error(`appel d'offres ${appelOffreId} non trouvé`);
-      }
+      try {
+        const appelOffre = await mediator.send<AppelOffre.ConsulterAppelOffreQuery>({
+          type: 'AppelOffre.Query.ConsulterAppelOffre',
+          data: {
+            identifiantAppelOffre: appelOffreId,
+          },
+        });
+        if (Option.isNone(appelOffre)) {
+          throw new Error(`appel d'offres ${appelOffreId} non trouvé`);
+        }
 
-      for (const période of appelOffre.periodes) {
-        const candidaturesLauréates = await executeSelect(
-          `
-            select count(key)
-            FROM domain_views.projection
-            WHERE key LIKE $1
-            AND value->>'statut' = 'classé';
+        for (const période of appelOffre.periodes) {
+          try {
+            const candidaturesLauréates = await executeSelect(
+              `
+              select count(key)
+              FROM domain_views.projection
+              WHERE key LIKE $1
+              AND value->>'statut' = 'classé';
             
          `,
-          `candidature|${appelOffreId}#${période.id}#%`,
-        );
-        if (!période.volumeRéservé) continue;
+              `candidature|${appelOffreId}#${période.id}#%`,
+            );
+            if (!période.volumeRéservé) continue;
 
-        const queryResult = await executeQuery(
-          `
-          update event_store.event_stream e
-          set payload = jsonb_set(    
-            e.payload,
-              '{volumeRéservé}',
-              to_jsonb(true)
-              )::jsonb
-          where e.stream_id like $1
-          and e.type like 'CandidatureImportée-V%'
-          and e.payload->>'statut' = 'classé'
-          and e.payload->>'puissance' ~ '^[0-9]+(.[0-9]+)?$'
-          and (e.payload->>'puissance')::numeric <= $2
-          and (e.payload->>'noteTotale')::numeric >= $3;
+            const queryResult = await executeQuery(
+              `
+              update event_store.event_stream e
+              set payload = jsonb_set(    
+                e.payload,
+                  '{volumeRéservé}',
+                  to_jsonb(true)
+                  )::jsonb
+              where e.stream_id like $1
+              and e.type like 'CandidatureImportée-V%'
+              and e.payload->>'statut' = 'classé'
+              and (e.payload->>'puissance')::numeric <= $2
+              and (e.payload->>'noteTotale')::numeric >= $3;
       `,
-          `candidature|${appelOffreId}#${période.id}#%`,
-          période.volumeRéservé.puissanceMax,
-          période.volumeRéservé.noteMin,
-        );
-        console.info(
-          `${appelOffreId} p${période.id} : volume réservé renseigné pour ${queryResult.rowCount} projets sur ${candidaturesLauréates[0]['count']} lauréats`,
-        );
-        projetsModifiés += queryResult.rowCount || 0;
+              `candidature|${appelOffreId}#${période.id}#%`,
+              période.volumeRéservé.puissanceMax,
+              période.volumeRéservé.noteMin,
+            );
+            console.info(
+              `${appelOffreId} p${période.id} : volume réservé renseigné pour ${queryResult.rowCount} projets sur ${candidaturesLauréates[0]['count']} lauréats`,
+            );
+            projetsModifiés += queryResult.rowCount || 0;
+          } catch (error) {
+            errors.push(error as Error);
+          }
+        }
+      } catch (error) {
+        errors.push(error as Error);
       }
     }
     await executeQuery(
@@ -91,6 +99,10 @@ export class Migrer extends Command {
         select event_store.throw_when_trying_to_update_event();
       `,
     );
+
     console.info(`${projetsModifiés} projets modifiés (volume réservé ajouté)`);
+    if (errors.length) {
+      console.error(`Erreurs : ${JSON.stringify(errors.map((e) => e.message))}`);
+    }
   }
 }
