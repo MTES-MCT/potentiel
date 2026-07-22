bf4316efd890b10c8ea75d619e16d4de8b950eef
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-07-22 10:23:48 +0200

    🚸 Ajout d'une indication sur l'environnement en cours (warning prod) pour les admin

--- packages/applications/ssr/src/components/organisms/header/Header.tsx
+++ packages/applications/ssr/src/components/organisms/header/Header.tsx
@@ -1,40 +1,75 @@
 import DsfrHeader from '@codegouvfr/react-dsfr/Header';
+import { headers } from 'next/headers';
 
+import type { PotentielUtilisateur } from '@potentiel-applications/request-context';
+
+import { getSessionUser } from '@/auth/getSessionUser';
 import { UserBasedRoleNavigation } from './UserBasedRoleNavigation';
 import { UserHeaderQuickAccessItem } from './UserHeaderQuickAccessItem';
 
-export const Header = () => (
-  <DsfrHeader
-    brandTop={
+const getServiceTitle = (utilisateur: PotentielUtilisateur | undefined) => {
+  if (!utilisateur?.rôle.estAdmin() || !process.env.APPLICATION_STAGE) {
+    return 'Potentiel';
+  }
+
+  if (process.env.APPLICATION_STAGE === 'production') {
+    return (
       <>
-        République
-        <br />
-        Française
+        Potentiel - ⚠️{' '}
+        <span className="text-dsfr-text-actionHigh-redMarianne-default">
+          {process.env.APPLICATION_STAGE.toUpperCase()}
+        </span>{' '}
+        ⚠️
       </>
-    }
-    serviceTitle="Potentiel"
-    serviceTagline={
-      <>
-        Facilite le parcours des producteurs
-        <br />
-        d'énergies renouvelables électriques
-      </>
-    }
-    homeLinkProps={{
-      href: '/',
-      title: "Retour à l'accueil",
-    }}
-    quickAccessItems={[
-      <UserHeaderQuickAccessItem key={0} />,
-      {
-        iconId: 'ri-question-line',
-        linkProps: {
-          target: '_blank',
-          href: 'https://docs.potentiel.beta.gouv.fr/guide-dutilisation/sommaire-du-guide-dutilisation',
+    );
+  }
+
+  return (
+    <>
+      Potentiel -{' '}
+      <span className="text-dsfr-text-actionHigh-blueFrance-default">
+        {process.env.APPLICATION_STAGE.toUpperCase()}
+      </span>
+    </>
+  );
+};
+
+export const Header = async () => {
+  const utilisateur = await getSessionUser({ headers: await headers() });
+
+  return (
+    <DsfrHeader
+      brandTop={
+        <>
+          République
+          <br />
+          Française
+        </>
+      }
+      serviceTitle={getServiceTitle(utilisateur)}
+      serviceTagline={
+        <>
+          Facilite le parcours des producteurs
+          <br />
+          d'énergies renouvelables électriques
+        </>
+      }
+      homeLinkProps={{
+        href: '/',
+        title: "Retour à l'accueil",
+      }}
+      quickAccessItems={[
+        <UserHeaderQuickAccessItem key={0} />,
+        {
+          iconId: 'ri-question-line',
+          linkProps: {
+            target: '_blank',
+            href: 'https://docs.potentiel.beta.gouv.fr/guide-dutilisation/sommaire-du-guide-dutilisation',
+          },
+          text: 'Aide',
         },
-        text: 'Aide',
-      },
-    ]}
-    navigation={<UserBasedRoleNavigation />}
-  />
-);
+      ]}
+      navigation={<UserBasedRoleNavigation />}
+    />
+  );
+};
