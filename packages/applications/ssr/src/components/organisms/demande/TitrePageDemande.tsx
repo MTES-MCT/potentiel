6370eef90b72a62841e2ec5db6a4c134f93bf6da
Hanaë <hanae.yamashita@lilo.org>
2026-07-31 15:46:15 +0200

    commentaire

--- packages/applications/ssr/src/components/organisms/demande/TitrePageDemande.tsx
+++ packages/applications/ssr/src/components/organisms/demande/TitrePageDemande.tsx
@@ -6,14 +6,6 @@ import { StatutDemandeBadge, type StatutDemandeBadgeProps } from './StatutDemand
 type Props = { titre: string; statut: StatutDemandeBadgeProps['statut'] };
 
 export const TitrePageDemande: FC<Props> = ({ titre, statut }) => (
-  // <div className="flex flex-row gap-4">
-  //   <div className="flex-shrink-0">
-  //     <Heading1>{titre}</Heading1>
-  //   </div>
-  //   <div className="flex-shrink-0">
-  //     <StatutDemandeBadge statut={statut} />
-  //   </div>
-  // </div>
   <div className="flex items-center gap-4">
     <Heading1>{titre}</Heading1>
     <StatutDemandeBadge statut={statut} />
