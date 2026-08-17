3fa231876ee01de9dd1bf06e4fffe018220b2593
Hubert MONCENIS <hubmoncenis@gmail.com>
2026-08-17 10:23:02 +0200

    🔥 Suppression des stats PTF obsolète

--- packages/statistiques/statistiques-publiques/src/04-mise-en-service/index.ts
+++ packages/statistiques/statistiques-publiques/src/04-mise-en-service/index.ts
@@ -1,26 +1,20 @@
 import { computeNombreTotalProjetEnService } from './nombreTotalProjetEnService.statistic.js';
 import { computeNombreTotalProjetPPE2AvecDCRDéposée } from './nombreTotalProjetPPE2AvecDCRDéposée.statistic.js';
 import { computeNombreTotalProjetPPE2AvecDossierRaccordementComplet } from './nombreTotalProjetPPE2AvecDossierRaccordementComplet.statistic.js';
-import { computeNombreTotalPTFDéposées } from './nombreTotalPTFDéposées.statistic.js';
 import { computeNombreTotalRéférencesRaccordement } from './nombreTotalRéférencesRaccordement.statistic.js';
-import { computepourcentageProjetAvecDCRQuiOntUnePTF } from './pourcentageProjetAvecDCRQuiOntUnePTF.statistic.js';
 import { computePourcentageProjetEnService } from './pourcentageProjetEnService.statistic.js';
 import { computePourcentageProjetPPE2AvecDCRDéposée } from './pourcentageProjetPPE2AvecDCRDéposée.js';
 import { computePourcentageProjetPPE2AvecDossierRaccordementComplet } from './pourcentageProjetPPE2AvecDossierRaccordementComplet.statistic.js';
-import { computePourcentagePTFDéposées } from './pourcentagePTFDéposées.statistic.js';
 import { computePourcentageRéférencesRaccordement } from './pourcentageRéférencesRaccordement.statistic.js';
 import { computePuissanceTotaleMiseEnService } from './puissanceTotaleMiseEnService.stastistic.js';
 
 export const computeMiseEnService = async () => {
   await computeNombreTotalRéférencesRaccordement();
-  await computeNombreTotalPTFDéposées();
   await computeNombreTotalProjetPPE2AvecDCRDéposée();
   await computeNombreTotalProjetEnService();
   await computeNombreTotalProjetPPE2AvecDossierRaccordementComplet();
-  await computepourcentageProjetAvecDCRQuiOntUnePTF();
   await computePourcentageProjetEnService();
   await computePuissanceTotaleMiseEnService();
-  await computePourcentagePTFDéposées();
   await computePourcentageProjetPPE2AvecDCRDéposée();
   await computePourcentageRéférencesRaccordement();
   await computePourcentageProjetPPE2AvecDossierRaccordementComplet();
