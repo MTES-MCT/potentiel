b6846c9b800c42e6e641b53f1e5e062dc8c0ff2c
HanaeY <72154904+HanaeY@users.noreply.github.com>
2026-09-03 16:33:16 +0200

    Fix affichage boutons action mainlevée (#4554)

--- packages/applications/ssr/src/app/laureats/[identifiant]/garanties-financieres/mainlevee/rejeter/RejeterDemandeMainlevée.form.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/garanties-financieres/mainlevee/rejeter/RejeterDemandeMainlevée.form.tsx
@@ -28,7 +28,7 @@ export const RejeterDemandeMainlevéeForm = ({
 
   return (
     <>
-      <Button priority="secondary" className="block w-1/2" onClick={() => setIsOpen(true)}>
+      <Button priority="secondary" className="block shrink-0" onClick={() => setIsOpen(true)}>
         Rejeter
       </Button>
 
