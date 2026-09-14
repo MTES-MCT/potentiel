b4caca445438594cae6b50b64254058d7eac5b19
Violette <27735540+VioMrqs@users.noreply.github.com>
2026-09-14 12:06:48 +0200

    ♻️ Suppression ancien comportement lors du téléchargement de attestation (#4587)

--- packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(sections)/ÉtapesProjet.tsx
+++ packages/applications/ssr/src/app/laureats/[identifiant]/(détails)/(sections)/ÉtapesProjet.tsx
@@ -1,6 +1,5 @@
 import Information from '@codegouvfr/react-dsfr/picto/Information';
 import Success from '@codegouvfr/react-dsfr/picto/Success';
-import type React from 'react';
 import type { FC } from 'react';
 import { match } from 'ts-pattern';
 
@@ -22,82 +21,74 @@ export type ÉtapeProjet = (
       type: 'mise-en-service' | 'achèvement-réel';
       date?: DateTime.RawType;
     }
-) & { hasNoDocument?: true };
+) & { document?: string };
 
 export type EtapesProjetProps = {
   identifiantProjet: IdentifiantProjet.RawType;
   étapes: Array<ÉtapeProjet>;
 };
 
-export const EtapesProjet: FC<EtapesProjetProps> = ({ identifiantProjet, étapes }) => {
-  return (
-    <aside aria-label="Progress">
-      <ul className="pl-0 overflow-hidden list-none print:flex print:justify-evenly print:flex-row">
-        {étapes.map((étape) =>
-          match(étape)
-            .with({ type: 'designation' }, ({ date }) => (
-              <ÉtapeProjet
-                key={étape.type}
-                titre="Notification"
-                date={date}
-                document={
-                  étape.hasNoDocument
-                    ? undefined
-                    : { url: Routes.Candidature.téléchargerAttestation(identifiantProjet) }
-                }
-              />
-            ))
-            .with({ type: 'recours' }, ({ date, dateDemande }) => (
-              <ÉtapeProjet
-                key={étape.type}
-                titre="Recours accordé"
-                date={date}
-                action={{
-                  href: Routes.Recours.détail(identifiantProjet, dateDemande),
-                  label: 'Voir les détails du recours',
-                }}
-              />
-            ))
-            .with({ type: 'abandon' }, ({ date, dateDemande }) => (
-              <ÉtapeProjet
-                key={étape.type}
-                titre="Abandon accordé"
-                date={date}
-                action={{
-                  href: Routes.Abandon.détail(identifiantProjet, dateDemande),
-                  label: "Voir les détails de l'abandon",
-                }}
-              />
-            ))
-            .with({ type: 'achèvement-prévisionel' }, ({ date }) => (
-              <ÉtapeProjet key={étape.type} titre="Achèvement prévisionnel" date={date} />
-            ))
-            .with({ type: 'mise-en-service' }, ({ date }) => (
-              <ÉtapeProjet key={étape.type} titre="Mise en service" date={date} />
-            ))
-            .with({ type: 'achèvement-réel' }, ({ date }) => (
-              <ÉtapeProjet key={étape.type} titre="Achèvement réel" date={date} />
-            ))
-            .exhaustive(),
-        )}
-      </ul>
-    </aside>
-  );
-};
+export const EtapesProjet: FC<EtapesProjetProps> = ({ identifiantProjet, étapes }) => (
+  <aside aria-label="Progress">
+    <ul className="pl-0 overflow-hidden list-none print:flex print:justify-evenly print:flex-row">
+      {étapes.map((étape) =>
+        match(étape)
+          .with({ type: 'designation' }, ({ date, document }) => (
+            <ÉtapeProjet
+              key={étape.type}
+              titre="Notification"
+              date={date}
+              documentUrl={document ? Routes.Document.télécharger(document) : undefined}
+            />
+          ))
+          .with({ type: 'recours' }, ({ date, dateDemande }) => (
+            <ÉtapeProjet
+              key={étape.type}
+              titre="Recours accordé"
+              date={date}
+              action={{
+                href: Routes.Recours.détail(identifiantProjet, dateDemande),
+                label: 'Voir les détails du recours',
+              }}
+            />
+          ))
+          .with({ type: 'abandon' }, ({ date, dateDemande }) => (
+            <ÉtapeProjet
+              key={étape.type}
+              titre="Abandon accordé"
+              date={date}
+              action={{
+                href: Routes.Abandon.détail(identifiantProjet, dateDemande),
+                label: "Voir les détails de l'abandon",
+              }}
+            />
+          ))
+          .with({ type: 'achèvement-prévisionel' }, ({ date }) => (
+            <ÉtapeProjet key={étape.type} titre="Achèvement prévisionnel" date={date} />
+          ))
+          .with({ type: 'mise-en-service' }, ({ date }) => (
+            <ÉtapeProjet key={étape.type} titre="Mise en service" date={date} />
+          ))
+          .with({ type: 'achèvement-réel' }, ({ date }) => (
+            <ÉtapeProjet key={étape.type} titre="Achèvement réel" date={date} />
+          ))
+          .exhaustive(),
+      )}
+    </ul>
+  </aside>
+);
 
 type ÉtapeProjetProps = {
   titre: string;
   date: DateTime.RawType | undefined;
-  document?: {
-    url: string;
-  };
+  documentUrl?: string;
   action?: {
     href: string;
     label: string;
   };
 };
 
-const ÉtapeProjet: FC<ÉtapeProjetProps> = ({ titre, date, document, action }) => {
+const ÉtapeProjet: FC<ÉtapeProjetProps> = ({ titre, date, documentUrl, action }) => {
   return (
     <TimelineItem>
       {date ? (
@@ -108,12 +99,12 @@ const ÉtapeProjet: FC<ÉtapeProjetProps> = ({ titre, date, document, action }) 
       <ContentArea>
         {date ? <FormattedDate date={date} /> : <span className="italic">À transmettre</span>}
         <ItemTitle title={titre} />
-        {document && (
+        {documentUrl && (
           <DownloadDocument
             className="mb-0"
             label="Télécharger l'attestation"
             format="pdf"
-            url={document.url}
+            url={documentUrl}
             small
           />
         )}
